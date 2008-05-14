f15680dd819cec52faf7dfb773d7573645210e4f
sefftinge <sefftinge>
2008-05-14 11:53:09 +0000

    added generation of token constants

---
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/ParseErrorHandlingTest.java
@@ -0,0 +1,26 @@
+package org.eclipse.xtext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.xtext.core.parser.IParseErrorHandler;
+import org.eclipse.xtext.generator.tests.AbstractGeneratorTest;
+
+public class ParseErrorHandlingTest extends AbstractGeneratorTest {
+	@Override
+	protected Class<?> getTheClass() {
+		return XtextGrammarTest.class;
+	}
+
+	public void testLexError() throws Exception {
+		final List<String> errors = new ArrayList<String>();
+		parse("import 'holla' % as foo", new IParseErrorHandler() {
+			public void handleParserError(int offset, int length, String text, String message) {
+				System.out.println(message + ":'" + text + "' (" + offset + "," + length + ")");
+				errors.add(text);
+			}
+		});
+		//TODO assertEquals("%",errors.get(0));
+		assertEquals(1, errors.size());
+	}
+}
