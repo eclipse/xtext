b50c12cef4679850bd2d3b592c11889df0497136
sefftinge <sefftinge>
2008-05-15 13:11:37 +0000

    introduced dummy language to be used for testing generic features (like e.g. whitespace handling in parse trees)

---
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/dummy/DummyLanguage.java
@@ -0,0 +1,4 @@
+package org.eclipse.xtext.dummy;
+
+// don't add tests here use AbstractGeneratorTest.getTheClass() { return DummyLanguage.class; } to use the dummy language grammar
+public abstract class DummyLanguage {}
