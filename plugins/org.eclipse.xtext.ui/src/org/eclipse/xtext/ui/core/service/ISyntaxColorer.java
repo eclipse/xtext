37e524b3c6fd280ad40eed71690bea9e6bcd5df5
dhubner <dhubner>
2008-05-15 08:07:31 +0000

    comments PDE: exported packages

--- plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/core/service/ISyntaxColorer.java
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/core/service/ISyntaxColorer.java
@@ -1,6 +1,12 @@
-/**
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2008 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+
 package org.eclipse.xtext.ui.core.service;
 
 import org.eclipse.jface.text.TextAttribute;
@@ -11,5 +17,5 @@ import org.eclipse.xtext.ui.core.language.ILanguageService;
  * 
  */
 public interface ISyntaxColorer extends ILanguageService {
-	TextAttribute color(int type, Object token);
+	TextAttribute color(ILexer lexer, Object token);
 }
