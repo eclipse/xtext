24d4126ab63306f2f2b8b71b807da515247ce636
pfriese <pfriese>
2008-05-15 07:52:48 +0000

    Xtext UI tests.

---
+++ tests/org.eclipse.xtext.ui.tests/src/org/eclipse/xtext/ui/core/language/SyntaxColorer1.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2008 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+package org.eclipse.xtext.ui.core.language;
+
+import org.eclipse.jface.text.TextAttribute;
+import org.eclipse.xtext.ui.core.service.ISyntaxColorer;
+
+/**
+ * @author Peter Friese - Initial contribution and API
+ *
+ */
+public class SyntaxColorer1 implements ISyntaxColorer {
+
+	/**
+	 * 
+	 */
+	public SyntaxColorer1() {
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.xtext.ui.core.language.service.ISyntaxColorer#color(int, java.lang.Object)
+	 */
+	public TextAttribute color(int type, Object token) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
