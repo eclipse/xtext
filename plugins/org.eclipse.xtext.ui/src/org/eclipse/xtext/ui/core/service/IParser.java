965c1efe19020ae7fa8be981a74dcb4f42b81141
dhubner <dhubner>
2008-05-15 08:07:31 +0000

    comments PDE: exported packages

--- plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/core/service/IParser.java
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/core/service/IParser.java
@@ -1,13 +1,19 @@
-/**
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2008 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+
 package org.eclipse.xtext.ui.core.service;
 
 /**
- * @generted
- *
+ * @author Dennis Hübner - Initial contribution and API
+ * 
  */
-public interface IParser
-		extends
-			org.eclipse.xtext.ui.core.language.ILanguageService {
+public interface IParser extends
+		org.eclipse.xtext.ui.core.language.ILanguageService {
+	Object parse(ILexer lexer);
 }
