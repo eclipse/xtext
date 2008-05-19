1fab5de243cfb7595fcf2509dc7255584393e811
pfriese <pfriese>
2008-05-19 07:46:35 +0000

    Catch up with latest development in .core.

--- devtools/org.eclipse.xtext.reference/src/org/eclipse/xtext/reference/ReferenceGrammar.java
+++ devtools/org.eclipse.xtext.reference/src/org/eclipse/xtext/reference/ReferenceGrammar.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.xtext.GeneratorFacade;
 import org.eclipse.xtext.Grammar;
-import org.eclipse.xtext.XtextEPackageAccess;
+import org.eclipse.xtext.XtextConstants;
 import org.eclipse.xtext.XtextPackage;
-import org.eclipse.xtext.parser.XtextFactory;
+import org.eclipse.xtext.parser.XtextASTFactory;
 import org.eclipse.xtext.parser.XtextParser;
 
 /**
@@ -21,15 +23,22 @@ public class ReferenceGrammar {
 	private static final String PATH = "./src-gen";
 	
 	public void generate() throws IOException {
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
+				"ecore", new XMIResourceFactoryImpl());
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
+				"xmi", new XMIResourceFactoryImpl());
+		
+		GeneratorFacade.cleanFolder(PATH);
+
 		String filename = this.getClass().getName().replace('.', '/') + ".xtext";
 
 		System.out.println("loading " + filename);
 		InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream(filename);
 		
 		//TODO make Xtext2Factory manual so one can overwrite 'getEPackages' in order to support generated epackages
-		EPackage.Registry.INSTANCE.put(XtextEPackageAccess.XTEXT_NS_URI, XtextPackage.eINSTANCE);
+		EPackage.Registry.INSTANCE.put(XtextConstants.XTEXT_NS_URI, XtextPackage.eINSTANCE);
 		XtextParser xtext2Parser= new XtextParser();
-		Grammar grammarModel = (Grammar) xtext2Parser.parse(resourceAsStream, new XtextFactory());
+		Grammar grammarModel = (Grammar) xtext2Parser.parse(resourceAsStream, new XtextASTFactory());
 		
 		GeneratorFacade.generate(grammarModel, this.getClass().getSimpleName(),this.getClass().getPackage().getName().replace('.', '/'), PATH);
 		System.out.println("Done.");
