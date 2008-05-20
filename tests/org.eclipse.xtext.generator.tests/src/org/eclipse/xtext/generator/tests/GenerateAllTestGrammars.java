2f3bdceffb91f322c4da1e0ffc743f8765b73589
jkohnlein <jkohnlein>
2008-05-20 11:47:52 +0000

    default values for tokenTypes

--- tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/generator/tests/GenerateAllTestGrammars.java
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/generator/tests/GenerateAllTestGrammars.java
@@ -23,13 +23,13 @@ import org.eclipse.xtext.XtextPackage;
 import org.eclipse.xtext.dummy.DummyLanguage;
 import org.eclipse.xtext.grammargen.tests.SimpleTest;
 import org.eclipse.xtext.grammargen.tests.SimpleTest2;
+import org.eclipse.xtext.lexer.LexerLanguage;
 import org.eclipse.xtext.metamodelreferencing.tests.MetamodelRefTest;
 import org.eclipse.xtext.parser.XtextASTFactory;
 import org.eclipse.xtext.parser.XtextParser;
 import org.eclipse.xtext.parsetree.reconstr.ComplexReconstrTest;
 import org.eclipse.xtext.parsetree.reconstr.SimpleReconstrTest;
 import org.eclipse.xtext.test.TestLanguage;
-import org.eclipse.xtext.testlanguages.SimpleExpressions;
 
 /**
  * @author Sven Efftinge - Initial contribution and API
@@ -41,7 +41,7 @@ public class GenerateAllTestGrammars {
 
 	private final static Class<?>[] testclasses = new Class[] { 
 		SimpleTest.class, SimpleTest2.class, XtextGrammarTest.class,
-		MetamodelRefTest.class, DummyLanguage.class, TestLanguage.class, SimpleReconstrTest.class, ComplexReconstrTest.class, SimpleExpressions.class};
+		MetamodelRefTest.class, DummyLanguage.class, TestLanguage.class, SimpleReconstrTest.class, ComplexReconstrTest.class, LexerLanguage.class };
 
 	public static void main(String[] args) throws Exception {
 		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
