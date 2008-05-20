d2f7d3ec9c624dd13df9b91032c476c48b6a3e0e
sefftinge <sefftinge>
2008-05-20 11:36:35 +0000

    cleaned up test infrastructure

--- tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/parsetree/HiddenTokensTest.java
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/parsetree/HiddenTokensTest.java
@@ -1,13 +1,8 @@
 package org.eclipse.xtext.parsetree;
 
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.xtext.XtextGrammarTest;
 import org.eclipse.xtext.core.parsetree.AbstractNode;
-import org.eclipse.xtext.core.parsetree.NodeAdapter;
 import org.eclipse.xtext.dummy.DummyLanguage;
-import org.eclipse.xtext.dummy.parser.DummyLanguageASTFactory;
 import org.eclipse.xtext.generator.tests.AbstractGeneratorTest;
-import org.eclipse.xtext.parser.XtextGrammarTestASTFactory;
 
 public class HiddenTokensTest extends AbstractGeneratorTest {
 
@@ -36,12 +31,6 @@ public class HiddenTokensTest extends AbstractGeneratorTest {
 		assertEquals(node.serialize(),model);
 	}
 	
-	private AbstractNode getRootNode(String model) throws Exception {
-		EObject object = (EObject) parse(model,new DummyLanguageASTFactory());
-		NodeAdapter adapter = (NodeAdapter) object.eAdapters().get(0);
-		AbstractNode node = adapter.getParserNode();
-		return node;
-	}
 	@Override
 	protected Class<?> getTheClass() {
 		return DummyLanguage.class;
