180d8605570364ac590a713fa5542c458a6bd84b
jkohnlein <jkohnlein>
2008-05-20 12:45:14 +0000

    create EReference also from assignments in actions

---
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/xtext2ecore/ActionTest.java
@@ -0,0 +1,55 @@
+package org.eclipse.xtext.xtext2ecore;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.xtext.core.parser.IElementFactory;
+import org.eclipse.xtext.generator.tests.AbstractGeneratorTest;
+import org.eclipse.xtext.testlanguages.ActionTestLanguage;
+import org.eclipse.xtext.testlanguages.ActionTestLanguageConstants;
+import org.eclipse.xtext.testlanguages.parser.ActionTestLanguageASTFactory;
+
+public class ActionTest extends AbstractGeneratorTest {
+
+	
+	public void testMetamodelCreation() throws Exception {
+		EClass model = checkEClassExists("Model");
+		EClass type = checkEClassExists("Type");
+		EClass item = checkEClassExists("Item");
+		EClass thing = checkEClassExists("Thing");
+		checkContainmentReferenceExists(model, type, "children");
+		checkContainmentReferenceExists(item, type, "items");
+		checkContainmentReferenceExists(thing, type, "content");
+		assertTrue(item.getESuperTypes().contains(type));
+		assertTrue(thing.getESuperTypes().contains(type));
+		assertNull(ActionTestLanguageConstants.getActionLangEPackage().getEClassifier("Element"));
+	}
+	
+	private EClass checkEClassExists(String name) {
+		EPackage actionLangEPackage = ActionTestLanguageConstants.getActionLangEPackage();
+		EClassifier classifier = actionLangEPackage.getEClassifier(name);
+		assertNotNull(classifier);
+		assertTrue(classifier instanceof EClass);
+		return (EClass) classifier;
+	}
+	
+	private void checkContainmentReferenceExists(EClass eClass, EClass type, String name) {
+		EStructuralFeature structuralFeature = eClass.getEStructuralFeature(name);
+		assertNotNull(structuralFeature);
+		assertTrue(structuralFeature instanceof EReference);
+		assertTrue(((EReference)structuralFeature).isContainment());
+		assertEquals(type, structuralFeature.getEType());
+	}
+	
+	@Override
+	protected IElementFactory getASTFactory() throws Exception {
+		return new ActionTestLanguageASTFactory();
+	}
+	
+	@Override
+	protected Class<?> getTheClass() {
+		return ActionTestLanguage.class;
+	}
+}
