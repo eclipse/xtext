34b9763f6a364625867048ba8eaf1b59b3590448
jkohnlein <jkohnlein>
2008-05-23 08:53:11 +0000

    Refctoring xtext.core -> xtext, part 3 - Updated build scripts, features and team project sets

--- plugins/org.eclipse.xtext.core/src-gen/org/eclipse/xtext/XtextTokenTypes.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.xtext;
-
-import org.eclipse.xtext.core.parser.ITokenTypes;
-
-public interface XtextTokenTypes extends ITokenTypes {
-
-	
-}
--- plugins/org.eclipse.xtext.core/src/org/eclipse/xtext/core/parser/IParsePostProcessor.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.xtext.core.parser;
-
-import org.eclipse.emf.ecore.EObject;
-
-public interface IParsePostProcessor {
-
-	EObject postProcess(EObject parseResult);
-}
