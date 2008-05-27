cfbebbdef6b0dc72ac6856ab0d6aefe66c568e75
jkohnlein <jkohnlein>
2008-05-27 14:35:41 +0000

    Extracted superclass for antlr parser bootstrap

--- tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/parsetree/EmptyModelTest.java
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/parsetree/EmptyModelTest.java
@@ -1,6 +1,7 @@
 package org.eclipse.xtext.parsetree;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.parser.IParseResult;
 import org.eclipse.xtext.testlanguages.OptionalEmptyLanguageLanguageFacade;
 import org.eclipse.xtext.testlanguages.OptionalEmptyLanguageStandaloneSetup;
 import org.eclipse.xtext.tests.AbstractGeneratorTest;
@@ -19,6 +20,24 @@ public class EmptyModelTest extends AbstractGeneratorTest {
 		assertNull(model);
 		model = getModel("hallo welt");
 		assertWithXtend("'welt'", "child.name", model);
+	}
+	
+	public void testParseCommentOnly() throws Exception {
+	    String model = "// some single line comment \n /* and \n a \n \n multiline \n comment */";
+        assertEmptyModel(model);
+	}
+
+    private void assertEmptyModel(String model) throws Exception {
+        IParseResult parseResult = parse(model);
+	    assertNull(parseResult.getRootASTElement());
+	    CompositeNode rootNode = parseResult.getRootNode();
+        assertNotNull(rootNode);
+        assertEquals(model, rootNode.serialize());
+    }
+	
+	public void testParseWhitespaceOnly() throws Exception {
+        String model = "    \t\n\r  \t\n\n ";
+	    assertEmptyModel(model);
 	}
 	
 }
