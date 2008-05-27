fad605b7b5198c90683e2192eff33dec0e5537ba
dhubner <dhubner>
2008-05-27 09:53:49 +0000

    pluginid changed

--- tests/org.eclipse.xtext.ui.tests/src/org/eclipse/xtext/ui/tests/Activator.java
+++ tests/org.eclipse.xtext.ui.tests/src/org/eclipse/xtext/ui/tests/Activator.java
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.xtext.ui.core.tests";
+	public static final String PLUGIN_ID = "org.eclipse.xtext.ui.tests";
 
 	// The shared instance
 	private static Activator plugin;
