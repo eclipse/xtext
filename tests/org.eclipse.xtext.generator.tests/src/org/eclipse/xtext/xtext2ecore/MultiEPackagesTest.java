1cbae727872e8e355dbbeba47ffce1154807f06b
sefftinge <sefftinge>
2008-05-27 14:46:53 +0000

    bug in xtext2ecore fixed

--- tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/xtext2ecore/MultiEPackagesTest.java
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/xtext2ecore/MultiEPackagesTest.java
@@ -1,16 +1,37 @@
 package org.eclipse.xtext.xtext2ecore;
 
+import java.util.List;
+
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.xtext.Grammar;
 import org.eclipse.xtext.XtextLanguageFacade;
 import org.eclipse.xtext.XtextStandaloneSetup;
 import org.eclipse.xtext.tests.AbstractGeneratorTest;
+import org.eclipse.xtext.xtextutil.XtextutilPackage;
 
-public abstract class MultiEPackagesTest extends AbstractGeneratorTest {
+public class MultiEPackagesTest extends AbstractGeneratorTest {
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
+		XtextutilPackage.eINSTANCE.getAbstractType();
 		XtextStandaloneSetup.doSetup();
 		with(XtextLanguageFacade.LANGUAGE_ID);
 	}
 	
+	@SuppressWarnings("unchecked")
+	public void testMultiEpackageGen() throws Exception {
+		Grammar model = (Grammar) getModel("generate foo 'foo' generate bar 'bar' as bar Foo : 'foo' bar=Bar; Bar returns bar::Bar : flag?='bar';");
+		List<EPackage> result = (List<EPackage>) invokeWithXtend("transform()", model);
+		assertEquals(2,result.size());
+		assertWithXtend("1", "get(0).eClassifiers.size", result);
+		assertWithXtend("'Foo'", "get(0).eClassifiers.first().name", result);
+		
+		assertWithXtend("1", "get(1).eClassifiers.size", result);
+		assertWithXtend("'Bar'", "get(1).eClassifiers.first().name", result);
+	}
 	
+	@Override
+	protected String[] importedExtensions() {
+		return new String[]{"org::eclipse::xtext::xtext2ecore::Xtext2Ecore"};
+	}
 }
