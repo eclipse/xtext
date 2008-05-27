e79819450cf346d4c2a34b199ca959391dd1c08d
dhubner <dhubner>
2008-05-27 09:04:20 +0000

    new parser interface compatibility new iserviceinterface preferences store initial

---
+++ devtools/org.eclipse.xtext.reference.ui/src/org/eclipse/xtext/reference/ui/services/ReferenceSyntaxColorer.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2008 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+
+package org.eclipse.xtext.reference.ui.services;
+
+import org.eclipse.jface.text.TextAttribute;
+import org.eclipse.xtext.parsetree.LeafNode;
+
+/**
+ * @author Dennis Hübner
+ * 
+ */
+public class ReferenceSyntaxColorer extends org.eclipse.xtext.ui.core.service.impl.SyntaxColorer {
+	@Override
+	public TextAttribute color(LeafNode leafNode) {
+		return super.color(leafNode);
+	}
+}
