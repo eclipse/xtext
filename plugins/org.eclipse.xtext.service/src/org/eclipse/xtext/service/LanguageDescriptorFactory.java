bc28ec095954d94d6b5ff28dd8aa624acc3c0596
sefftinge <sefftinge>
2008-06-03 14:23:36 +0000

    superfluos method removed

--- plugins/org.eclipse.xtext.service/src/org/eclipse/xtext/service/LanguageDescriptorFactory.java
+++ plugins/org.eclipse.xtext.service/src/org/eclipse/xtext/service/LanguageDescriptorFactory.java
@@ -28,15 +28,6 @@ public class LanguageDescriptorFactory {
 		return languageDescriptor;
 	}
 
-	public static ILanguageDescriptor getByNamespace(String namespace) {
-		for (ILanguageDescriptor languageDescriptor : id2descriptorMap.values()) {
-			if (namespace.equals(languageDescriptor.getNameSpace())) {
-				return languageDescriptor;
-			}
-		}
-		return null;
-	}
-
 	public static ILanguageDescriptor createLanguageDescriptor(String languageId, String languageName,
 			String namespace, ILanguageDescriptor superLanguage) {
 		synchronized (id2descriptorMap) {
