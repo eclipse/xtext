901cb699613bdef9b8dc1e87afde82a29e13e709
sefftinge <sefftinge>
2008-06-03 13:29:52 +0000

    bootstrapped

--- tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/parsetree/EmptyModelTest.java
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/parsetree/EmptyModelTest.java
@@ -1,12 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2008 itemis AG (http://www.itemis.eu) and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- *******************************************************************************/
-
 package org.eclipse.xtext.parsetree;
 
 import org.eclipse.emf.ecore.EObject;
@@ -14,11 +5,6 @@ import org.eclipse.xtext.parser.IParseResult;
 import org.eclipse.xtext.testlanguages.OptionalEmptyLanguageStandaloneSetup;
 import org.eclipse.xtext.tests.AbstractGeneratorTest;
 
-/**
- * 
- * @author Jan Köhnlein - Initial contribution and API
- *
- */
 public class EmptyModelTest extends AbstractGeneratorTest {
 	
 	@Override
