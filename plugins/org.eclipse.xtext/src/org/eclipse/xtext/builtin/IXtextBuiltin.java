ab048e364e4a6bedee04e9ae050c643bf159c6e7
jkohnlein <jkohnlein>
2008-06-11 10:42:48 +0000

    Updated manually written code to the current state of the generated code

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/builtin/IXtextBuiltin.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/builtin/IXtextBuiltin.java
@@ -1,5 +1,13 @@
 package org.eclipse.xtext.builtin;
 
 public interface IXtextBuiltin {
-	public static final String ID = "org.eclipse.xtext.builtin.XtextBuiltin";
+    /**
+     * the unique identifier of this language. 
+     * To be used to get the language descriptor and related services from the registry.
+     * Within ILanguageServices it is encouraged to use dependency injection instead
+     * of directly accessing the ServiceRegistry 
+     */
+    public static final String ID = "org.eclipse.xtext.builtin.XtextBuiltin";
+    public static final String NAME = "Xtext Builtin";
+    public static final String NAMESPACE = "org.eclipse.xtext.builtin";
 }
--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/builtin/XtextBuiltinStandaloneSetup.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/builtin/XtextBuiltinStandaloneSetup.java
@@ -11,30 +11,39 @@ import org.eclipse.xtext.service.LanguageDescriptorFactory;
 import org.eclipse.xtext.service.ServiceRegistry;
 
 public class XtextBuiltinStandaloneSetup {
-	public final static void doSetup() {
-		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
-				"ecore", new XMIResourceFactoryImpl());
-		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
-				"xmi", new XMIResourceFactoryImpl());
-		
-		ServiceRegistry.registerService(getLanguageDescriptor(), IGrammarAccess.class, XtextBuiltinGrammarAccess.class);
-		ServiceRegistry.registerService(getLanguageDescriptor(), IMetamodelAccess.class, XtextBuiltinMetamodelAccess.class);
-		ServiceRegistry.registerService(getLanguageDescriptor(), IValueConverterService.class, XtextBuiltInConverters.class);
-	}
-	
-	private static ILanguageDescriptor INSTANCE;
-	
-	public static final String LANGUAGE_ID = IXtextBuiltin.ID;
-	public static final String LANGUAGE_NAME = "XtextBuiltin";
-	public static final String NAMESPACE = "org/eclipse/xtext/builtin";
-    
+
+    private static boolean isInitialized = false;
+
+    public final static void doSetup() {
+        if (!isInitialized) {
+            Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("ecore", new XMIResourceFactoryImpl());
+            Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
+
+            ServiceRegistry.registerService(getLanguageDescriptor(), IGrammarAccess.class, XtextBuiltinGrammarAccess.class);
+            ServiceRegistry.registerService(getLanguageDescriptor(), IMetamodelAccess.class, XtextBuiltinMetamodelAccess.class);
+            ServiceRegistry.registerService(getLanguageDescriptor(), IValueConverterService.class, XtextBuiltInConverters.class);
+            isInitialized = true;
+        }
+    }
+
+  //TODO private constructor?
+//  private XtextStandaloneSetup() {
+//  }
+
+    private static class InstanceHolder {
+        private static ILanguageDescriptor INSTANCE;
+
+        static {
+            INSTANCE = LanguageDescriptorFactory.get(IXtextBuiltin.ID);
+            if (INSTANCE == null) {
+                // TODO put super grammar
+                INSTANCE = LanguageDescriptorFactory.createLanguageDescriptor(IXtextBuiltin.ID,
+                        IXtextBuiltin.NAME, IXtextBuiltin.NAMESPACE, null);
+            }
+        }
+    }
+
     public static ILanguageDescriptor getLanguageDescriptor() {
-    	if (INSTANCE == null) {
-    		INSTANCE = LanguageDescriptorFactory.get(LANGUAGE_ID);
-    		if(INSTANCE == null) {
-    			INSTANCE = LanguageDescriptorFactory.createLanguageDescriptor(LANGUAGE_ID, LANGUAGE_NAME, NAMESPACE, null);
-    		}
-    	}
-    	return INSTANCE;
-	}
+        return InstanceHolder.INSTANCE;
+    }
 }
