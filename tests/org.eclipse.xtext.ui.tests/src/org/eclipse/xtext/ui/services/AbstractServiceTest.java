33b3bea299669824ad8ce8df22419aa0b470ce42
dhubner <dhubner>
2008-06-18 08:21:25 +0000

    easymock integration

--- tests/org.eclipse.xtext.ui.tests/src/org/eclipse/xtext/ui/services/AbstractServiceTest.java
+++ tests/org.eclipse.xtext.ui.tests/src/org/eclipse/xtext/ui/services/AbstractServiceTest.java
@@ -8,16 +8,10 @@
  *******************************************************************************/
 package org.eclipse.xtext.ui.services;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-
 import junit.framework.TestCase;
 
 import org.eclipse.xtext.parser.IParseResult;
 import org.eclipse.xtext.parser.IParser;
-import org.eclipse.xtext.parsetree.LeafNode;
-import org.eclipse.xtext.parsetree.ParsetreeFactory;
 import org.eclipse.xtext.service.ILanguageDescriptor;
 import org.eclipse.xtext.service.ILanguageService;
 import org.eclipse.xtext.service.LanguageDescriptorFactory;
@@ -29,10 +23,10 @@ import org.eclipse.xtext.util.StringInputStream;
  * 
  */
 public abstract class AbstractServiceTest extends TestCase {
-
 	static {
 		Thread.currentThread().setContextClassLoader(AbstractServiceTest.class.getClassLoader());
 	}
+
 	private static final String DEFAULT_LANGUAGE = "org.eclipse.xtext.reference.ReferenceGrammar";
 	private ILanguageDescriptor languageDescriptor;
 
@@ -51,53 +45,4 @@ public abstract class AbstractServiceTest extends TestCase {
 		return parser.parse(new StringInputStream(model));
 	}
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-	}
-
-	protected LeafNode createLeafNode() {
-		return ParsetreeFactory.eINSTANCE.createLeafNode();
-	}
-
-	/**
-	 * use at your own risk
-	 * 
-	 * @param <T>
-	 * @param service
-	 * @param handler
-	 * @return mock
-	 */
-	@SuppressWarnings("unchecked")
-	protected <T> T createMock(Class<T> service, InvocationHandler handler) {
-		try {
-			if (service.isInterface())
-				return (T) Proxy.newProxyInstance(getClass().getClassLoader(), new Class[] { service }, handler);
-			else if (service.isPrimitive()) {
-				String name = service.getName();
-				if (name.equals(Integer.TYPE.getName())) {
-					return (T) Integer.valueOf(0);
-				}
-				else if (name.equals(Boolean.TYPE.getName())) {
-					return (T) Boolean.valueOf(false);
-				}
-				return (T) Class.forName(service.getName()).newInstance();
-			}
-			else {
-				return service.newInstance();
-			}
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	protected final class InvocationHandlerImpl implements InvocationHandler {
-		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-			if (!method.getReturnType().equals(Void.TYPE))
-				return createMock(method.getReturnType(), this);
-			return null;
-		}
-	}
 }
--- tests/org.eclipse.xtext.ui.tests/src/org/eclipse/xtext/ui/services/BISyntaxColorerTest.java
+++ tests/org.eclipse.xtext.ui.tests/src/org/eclipse/xtext/ui/services/BISyntaxColorerTest.java
@@ -8,14 +8,15 @@
  *******************************************************************************/
 package org.eclipse.xtext.ui.services;
 
-import org.eclipse.xtext.Grammar;
-import org.eclipse.xtext.GrammarUtil;
-import org.eclipse.xtext.LexerRule;
-import org.eclipse.xtext.builtin.XtextBuiltinGrammarAccess;
+import org.eclipse.jface.preference.IPersistentPreferenceStore;
+import org.eclipse.xtext.builtin.IXtextBuiltin;
+import org.eclipse.xtext.builtin.XtextBuiltinStandaloneSetup;
 import org.eclipse.xtext.parsetree.LeafNode;
 import org.eclipse.xtext.service.ILanguageDescriptor;
+import org.eclipse.xtext.service.LanguageDescriptorFactory;
+import org.eclipse.xtext.ui.AbstractEasyMockTest;
 import org.eclipse.xtext.ui.editor.utils.TextStyle;
-import org.eclipse.xtext.ui.service.IPreferenceStoreService;
+import org.eclipse.xtext.ui.service.impl.BuildInPreferenceStore;
 import org.eclipse.xtext.ui.service.impl.BuildInSyntaxColorer;
 import org.eclipse.xtext.ui.tokentype.BuildInTokenTypeDef;
 
@@ -23,17 +24,30 @@ import org.eclipse.xtext.ui.tokentype.BuildInTokenTypeDef;
  * @author Dennis Hübner - Initial contribution and API
  * 
  */
-public class BISyntaxColorerTest extends AbstractServiceTest {
-
+public class BISyntaxColorerTest extends AbstractEasyMockTest {
+	static {
+		Thread.currentThread().setContextClassLoader(AbstractServiceTest.class.getClassLoader());
+		if (LanguageDescriptorFactory.get(IXtextBuiltin.ID) == null)
+			XtextBuiltinStandaloneSetup.doSetup();
+	}
 	private BuildInSyntaxColorer colorer;
 
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 		colorer = new BuildInSyntaxColorer();
-		colorer.setLanguageDescriptor(createMock(ILanguageDescriptor.class, new InvocationHandlerImpl()));
+		final ILanguageDescriptor languageDescriptorMock = createMock(ILanguageDescriptor.class);
+		final IPersistentPreferenceStore persistablePrefStore = createMock(IPersistentPreferenceStore.class);
+
+		colorer.setLanguageDescriptor(languageDescriptorMock);
 		colorer.setTokenTypeDefService(new BuildInTokenTypeDef());
-		colorer.setPreferenceStoreService(createMock(IPreferenceStoreService.class, new InvocationHandlerImpl()));
+		colorer.setPreferenceStoreService(new BuildInPreferenceStore() {
+			@Override
+			public IPersistentPreferenceStore getPersitablePreferenceStore() {
+				return persistablePrefStore;
+			}
+		});
+		replay();
 	}
 
 	public void testTextDefaultStyle() throws Exception {
@@ -44,13 +58,9 @@ public class BISyntaxColorerTest extends AbstractServiceTest {
 	}
 
 	public void testKnownTokenTypeStyle() throws Exception {
-		LeafNode leafNode = createLeafNode();
-		new Object();
-		Grammar grammar = new XtextBuiltinGrammarAccess().getGrammar();
-		LexerRule lr = (LexerRule) GrammarUtil.findRuleForName(grammar, "SL_COMMENT");
-		leafNode.setGrammarElement(lr);
-		leafNode.setText("//single line comment\n");
+		LeafNode leafNode = createSLComment();
 		TextStyle textStyle = colorer.color(leafNode);
 		assertNotNull(textStyle);
 	}
+
 }
