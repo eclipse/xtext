295e829fbcfa42c35c7cc5ebfa20520752adfa74
dhubner <dhubner>
2008-06-18 13:07:41 +0000

    initial

---
+++ plugins/org.eclipse.xtext.xtext.ui/src/org/eclipse/xtext/xtext/ui/Activator.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * Copyright (c) 2008 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+package org.eclipse.xtext.xtext.ui;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author Dennis Hübner - Initial contribution and API
+ * 
+ */
+public class Activator extends AbstractUIPlugin {
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.eclipse.xtext.xtext.ui";
+
+	// The shared instance
+	private static Activator plugin;
+
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		setDefault(this);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void stop(BundleContext context) throws Exception {
+		setDefault(null);
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+	private static void setDefault(Activator activator) {
+		plugin = activator;
+	}
+}
---
+++ plugins/org.eclipse.xtext.xtext.ui/src/org/eclipse/xtext/xtext/ui/services/XtextTokenTypeDef.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2008 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+package org.eclipse.xtext.xtext.ui.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.xtext.parsetree.LeafNode;
+import org.eclipse.xtext.ui.editor.utils.TextStyle;
+import org.eclipse.xtext.ui.tokentype.BuildInTokenTypeDef;
+import org.eclipse.xtext.ui.tokentype.ITokenTypeDef;
+import org.eclipse.xtext.ui.tokentype.TokenTypeDef;
+
+/**
+ * @author Dennis Hübner - Initial contribution and API
+ * 
+ */
+public class XtextTokenTypeDef extends BuildInTokenTypeDef {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.xtext.ui.service.ITokenTypeDefService#allTokenTypes()
+	 */
+	@Override
+	public List<ITokenTypeDef> allTokenTypes() {
+		List<ITokenTypeDef> allTokenTypes = new ArrayList<ITokenTypeDef>();
+		allTokenTypes.add(special());
+		allTokenTypes.addAll(super.allTokenTypes());
+		return allTokenTypes;
+	}
+
+	private ITokenTypeDef special() {
+		TokenTypeDef ttd = new TokenTypeDef("special") {
+			@Override
+			public boolean match(LeafNode node) {
+				return "ID".equals(node.getText()) || "STRING".equals(node.getText());
+			}
+		};
+		TextStyle ts = keyWordTokenType().getTextStyle();
+		ttd.setTextStyle(ts);
+		return ttd;
+	}
+
+}
