0076f829118f646ef9f96419d882ab81fc6e74c6
jkohnlein <jkohnlein>
2008-07-08 08:44:08 +0000

    preparation for server test

---
+++ tests/org.eclipse.xtext.service.tests/src/org/eclipse/xtext/service/AllTests.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2008 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+package org.eclipse.xtext.service;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * @author Jan Köhnlein - Initial contribution and API
+ *
+ */
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test for org.eclipse.xtext.service");
+		//$JUnit-BEGIN$
+		suite.addTestSuite(LanguageDescriptorFactoryTest.class);
+		suite.addTestSuite(PriorityTest.class);
+		suite.addTestSuite(RegistrationTest.class);
+		suite.addTestSuite(LanguageInheritanceTest.class);
+		suite.addTestSuite(DependencyInjectionTest.class);
+		//$JUnit-END$
+		return suite;
+	}
+
+}
--- tests/org.eclipse.xtext.service.tests/src/org/eclipse/xtext/service/LanguageDescriptorFactoryTest.java
+++ tests/org.eclipse.xtext.service.tests/src/org/eclipse/xtext/service/LanguageDescriptorFactoryTest.java
@@ -6,7 +6,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  *******************************************************************************/
-package org.eclipse.xtext.service.examples;
+package org.eclipse.xtext.service;
 
 import junit.framework.TestCase;
 
