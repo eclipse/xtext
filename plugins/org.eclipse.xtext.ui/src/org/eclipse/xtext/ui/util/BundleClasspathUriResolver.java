a38077d032494a4067c1cb3c93b14cca10d79ddd
jkohnlein <jkohnlein>
2008-07-09 10:56:24 +0000

    removed dependency on internal API

--- plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/util/BundleClasspathUriResolver.java
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/util/BundleClasspathUriResolver.java
@@ -4,12 +4,10 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Plugin;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.osgi.baseadaptor.BaseData;
-import org.eclipse.osgi.framework.internal.core.BundleHost;
-import org.eclipse.osgi.framework.internal.core.BundleURLConnection;
 import org.eclipse.xtext.resource.ClasspathUriResolutionException;
 import org.eclipse.xtext.resource.ClasspathUriUtil;
 import org.eclipse.xtext.resource.IClasspathUriResolver;
@@ -36,7 +34,6 @@ public class BundleClasspathUriResolver implements IClasspathUriResolver {
     }
 
 
-    @SuppressWarnings("restriction")
     private URI findResourceInBundle(Bundle bundle, URI classpathUri)
             throws MalformedURLException, IOException {
         Path fullPath = new Path(classpathUri.path());
@@ -45,18 +42,9 @@ public class BundleClasspathUriResolver implements IClasspathUriResolver {
             String projectRelativePath = "/" + fullPath;
             URL resourceUrl = bundle.getResource(projectRelativePath);
             if (resourceUrl != null) {
-                String baseUrl = ((BaseData) ((BundleHost) bundle)
-                        .getBundleData()).getBundleFile().getBaseFile().toURL()
-                        .toString();
-                if (baseUrl.endsWith(".jar")) {
-                    baseUrl = "jar:" + baseUrl + "!/";
-                }
-                String localUrl = ((BundleURLConnection) resourceUrl
-                        .openConnection()).getLocalURL().toString();
-                String platformPluginUri = bundle.getSymbolicName() + "/"
-                        + localUrl.substring(baseUrl.length());
-                URI normalizedURI = URI.createPlatformPluginURI(
-                        platformPluginUri, true);
+            	URL resolvedUrl = FileLocator.resolve(resourceUrl);
+                URI normalizedURI = URI.createURI(
+                        resolvedUrl.toString(), true);
                 return normalizedURI;
             }
         }
