184d3e99610f9300928ac677e867c454b5ab9a0f
dhubner <dhubner>
2008-07-15 15:55:10 +0000

    syntaxcoloring pref page

--- plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/service/impl/BuiltInSyntaxColorer.java
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/service/impl/BuiltInSyntaxColorer.java
@@ -9,13 +9,10 @@
 
 package org.eclipse.xtext.ui.service.impl;
 
-import org.eclipse.jface.preference.IPersistentPreferenceStore;
 import org.eclipse.swt.SWT;
 import org.eclipse.xtext.parsetree.LeafNode;
-import org.eclipse.xtext.service.IServiceScope;
 import org.eclipse.xtext.service.Inject;
 import org.eclipse.xtext.ui.editor.utils.TextStyle;
-import org.eclipse.xtext.ui.service.IPreferenceStore;
 import org.eclipse.xtext.ui.service.ISyntaxColorer;
 import org.eclipse.xtext.ui.service.ITokenTypeDefProvider;
 import org.eclipse.xtext.ui.tokentype.ITokenTypeDef;
@@ -27,8 +24,6 @@ import org.eclipse.xtext.ui.tokentype.ITokenTypeDef;
 public class BuiltInSyntaxColorer implements ISyntaxColorer {
 
 	private ITokenTypeDefProvider tokenTypeDef;
-	private IPreferenceStore preferenceStore;
-	private IServiceScope languageDescriptor;
 
 	/*
 	 * (non-Javadoc)
@@ -40,15 +35,7 @@ public class BuiltInSyntaxColorer implements ISyntaxColorer {
 		TextStyle tsToSet = new TextStyle(null, null, SWT.NONE, null);
 		for (ITokenTypeDef ttd : tokenTypeDef.allTokenTypes()) {
 			if (ttd.match(leafNode)) {
-				String tokenTypeId = ttd.getId();
-				if (ttd.getTextStyle() != null) {
-					tsToSet = ttd.getTextStyle();
-				}
-				TextStyle ts = new TextStyle(getColorForTokenType(tokenTypeId, tsToSet.getColor()),
-						getBackgroundColorForTokenType(tokenTypeId, tsToSet.getBackgroundColor()),
-						getStyleForTokenType(tokenTypeId, tsToSet.getStyle()), getFontForTokenType(tokenTypeId, tsToSet
-								.getFontName()));
-				return ts;
+				return ttd.getTextStyle();
 			}
 		}
 		return tsToSet;
@@ -57,56 +44,6 @@ public class BuiltInSyntaxColorer implements ISyntaxColorer {
 	@Inject
 	public void setTokenTypeDefProvider(ITokenTypeDefProvider service) {
 		this.tokenTypeDef = service;
-	}
-
-	@Inject
-	public void setPreferenceStore(IPreferenceStore preferenceStore) {
-		this.preferenceStore = preferenceStore;
-	}
-
-	@Inject
-	public void setLanguageDescriptor(IServiceScope languageDescriptor) {
-		this.languageDescriptor = languageDescriptor;
-	}
-
-	private IPersistentPreferenceStore getPreferenceStore() {
-		return preferenceStore.getPersitablePreferenceStore();
-	}
-
-	// TODO set defaults somewhere else if possible, or check if a default
-	// already set
-	private int getStyleForTokenType(String tokenType, int defaultStyle) {
-		String tokenStylePreferenceKey = BuiltInPreferenceStore.getTokenStylePreferenceKey(languageDescriptor,
-				tokenType);
-		getPreferenceStore().setDefault(tokenStylePreferenceKey, defaultStyle);
-		return getPreferenceStore().getInt(tokenStylePreferenceKey);
-	}
-
-	private String getFontForTokenType(String tokenType, String defaultFont) {
-		String tokenFontPreferenceKey = BuiltInPreferenceStore.getTokenFontPreferenceKey(languageDescriptor, tokenType);
-		if (defaultFont != null)
-			getPreferenceStore().setDefault(tokenFontPreferenceKey, defaultFont);
-		return getPreferenceStore().getString(tokenFontPreferenceKey);
-	}
-
-	private String getBackgroundColorForTokenType(String tokenType, String defaultBackgroundColor) {
-		String tokenBackgroundColorPreferenceKey = BuiltInPreferenceStore.getTokenBackgroundColorPreferenceKey(
-				languageDescriptor, tokenType);
-		if (defaultBackgroundColor != null)
-			getPreferenceStore().setDefault(
-					BuiltInPreferenceStore.getTokenBackgroundColorPreferenceKey(languageDescriptor, tokenType),
-					defaultBackgroundColor);
-		String rgbString = getPreferenceStore().getString(tokenBackgroundColorPreferenceKey);
-		return rgbString;
-	}
-
-	private String getColorForTokenType(String tokenType, String defaultColor) {
-		String preferenceKey = BuiltInPreferenceStore.getTokenColorPreferenceKey(languageDescriptor, tokenType);
-		if (defaultColor != null)
-			getPreferenceStore().setDefault(
-					BuiltInPreferenceStore.getTokenColorPreferenceKey(languageDescriptor, tokenType), defaultColor);
-		String rgbString = getPreferenceStore().getString(preferenceKey);
-		return rgbString;
 	}
 
 }
