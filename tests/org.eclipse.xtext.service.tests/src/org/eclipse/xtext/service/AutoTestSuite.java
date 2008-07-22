775286a5b0a2260b4e14911bbb215df54a58df64
jkohnlein <jkohnlein>
2008-07-22 14:36:39 +0000

    Added text.xml and auto test suites

---
+++ tests/org.eclipse.xtext.service.tests/src/org/eclipse/xtext/service/AutoTestSuite.java
@@ -0,0 +1,18 @@
+package org.eclipse.xtext.service;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AutoTestSuite {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("org.eclipse.xtext.service.tests");
+		suite.addTestSuite(org.eclipse.xtext.service.NestedServiceScopesTest.class);
+		suite.addTestSuite(org.eclipse.xtext.service.DependencyInjectionTest.class);
+		suite.addTestSuite(org.eclipse.xtext.service.ServiceScopeFactoryTest.class);
+		suite.addTestSuite(org.eclipse.xtext.service.RegistrationTest.class);
+		suite.addTestSuite(org.eclipse.xtext.service.PriorityTest.class);
+		suite.addTestSuite(org.eclipse.xtext.service.fieldInjection.FieldInjectionTest.class);
+		return suite;
+	}
+}
