f2390b59bef11136147da2d89ad675eccf5a6905
jkohnlein <jkohnlein>
2008-07-23 08:36:25 +0000

    Sort tests in test suites Added UI tests

--- tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/generator/AutoTestSuite.java
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/generator/AutoTestSuite.java
@@ -3,6 +3,11 @@ package org.eclipse.xtext.generator;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+/**
+ * DO NOT MODIFY MANUALLY! This class has been automatically generated. 
+ * Install org.eclipse.xtext.testcollector and use the Xtext Tools/Create Test Suite
+ * action on the source folder to regenerate this.
+ */
 public class AutoTestSuite {
 
 	public static Test suite() {
@@ -11,25 +16,25 @@ public class AutoTestSuite {
 		suite.addTestSuite(org.eclipse.xtext.generator.resource.ResourceTest.class);
 		suite.addTestSuite(org.eclipse.xtext.grammarinheritance.InheritanceTest.class);
 		suite.addTestSuite(org.eclipse.xtext.grammarinheritance.ToEcoreTrafoTest.class);
-		suite.addTestSuite(org.eclipse.xtext.lexer.LexerSLComment.class);
 		suite.addTestSuite(org.eclipse.xtext.lexer.LexerErrorTest.class);
-		suite.addTestSuite(org.eclipse.xtext.metamodelreferencing.tests.MultiGenMMTest.class);
+		suite.addTestSuite(org.eclipse.xtext.lexer.LexerSLComment.class);
 		suite.addTestSuite(org.eclipse.xtext.metamodelreferencing.tests.MetamodelRefTest.class);
+		suite.addTestSuite(org.eclipse.xtext.metamodelreferencing.tests.MultiGenMMTest.class);
 		suite.addTestSuite(org.eclipse.xtext.parseerrorhandling.ParseErrorHandlingTest.class);
+		suite.addTestSuite(org.eclipse.xtext.parser.PartialParserReplaceTest.class);
 		suite.addTestSuite(org.eclipse.xtext.parser.PartialParserTest.class);
+		suite.addTestSuite(org.eclipse.xtext.parser.PartialParsingPerformanceTest.class);
 		suite.addTestSuite(org.eclipse.xtext.parser.PartialParsingPointerTest.class);
 		suite.addTestSuite(org.eclipse.xtext.parser.XtextParserTest.class);
-		suite.addTestSuite(org.eclipse.xtext.parser.PartialParsingPerformanceTest.class);
-		suite.addTestSuite(org.eclipse.xtext.parser.PartialParserReplaceTest.class);
-		suite.addTestSuite(org.eclipse.xtext.parsetree.HiddenTokensTest.class);
-		suite.addTestSuite(org.eclipse.xtext.parsetree.CommentTest.class);
 		suite.addTestSuite(org.eclipse.xtext.parsetree.ASTChangeTest.class);
-		suite.addTestSuite(org.eclipse.xtext.parsetree.SerializeTest.class);
-		suite.addTestSuite(org.eclipse.xtext.parsetree.NodeModelTest.class);
-		suite.addTestSuite(org.eclipse.xtext.parsetree.NodeContentAdapterTest.class);
-		suite.addTestSuite(org.eclipse.xtext.parsetree.LengthOffsetLineTest.class);
-		suite.addTestSuite(org.eclipse.xtext.parsetree.InvalidTokenTest.class);
+		suite.addTestSuite(org.eclipse.xtext.parsetree.CommentTest.class);
 		suite.addTestSuite(org.eclipse.xtext.parsetree.EmptyModelTest.class);
+		suite.addTestSuite(org.eclipse.xtext.parsetree.HiddenTokensTest.class);
+		suite.addTestSuite(org.eclipse.xtext.parsetree.InvalidTokenTest.class);
+		suite.addTestSuite(org.eclipse.xtext.parsetree.LengthOffsetLineTest.class);
+		suite.addTestSuite(org.eclipse.xtext.parsetree.NodeContentAdapterTest.class);
+		suite.addTestSuite(org.eclipse.xtext.parsetree.NodeModelTest.class);
+		suite.addTestSuite(org.eclipse.xtext.parsetree.SerializeTest.class);
 		suite.addTestSuite(org.eclipse.xtext.parsetree.reconstr.ComplexReconstrTest.class);
 		suite.addTestSuite(org.eclipse.xtext.parsetree.reconstr.SimpleReconstrTest.class);
 		suite.addTestSuite(org.eclipse.xtext.parsetree.reconstr.WhitespacePreservingCallbackTest.class);
