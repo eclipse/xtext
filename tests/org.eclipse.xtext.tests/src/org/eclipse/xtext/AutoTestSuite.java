8160760bbec27b6c91c1ef65af75baad7daa9937
jkohnlein <jkohnlein>
2008-07-23 08:36:25 +0000

    Sort tests in test suites Added UI tests

--- tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/AutoTestSuite.java
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/AutoTestSuite.java
@@ -3,6 +3,11 @@ package org.eclipse.xtext;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+/**
+ * DO NOT MODIFY MANUALLY! This class has been automatically generated. 
+ * Install org.eclipse.xtext.testcollector and use the Xtext Tools/Create Test Suite
+ * action on the source folder to regenerate this.
+ */
 public class AutoTestSuite {
 
 	public static Test suite() {
