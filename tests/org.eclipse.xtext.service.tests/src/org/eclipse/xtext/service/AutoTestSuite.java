4cbda08ddf22e03188e8386401183393edfb68c5
jkohnlein <jkohnlein>
2008-07-23 08:36:25 +0000

    Sort tests in test suites Added UI tests

--- tests/org.eclipse.xtext.service.tests/src/org/eclipse/xtext/service/AutoTestSuite.java
+++ tests/org.eclipse.xtext.service.tests/src/org/eclipse/xtext/service/AutoTestSuite.java
@@ -3,15 +3,20 @@ package org.eclipse.xtext.service;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+/**
+ * DO NOT MODIFY MANUALLY! This class has been automatically generated. 
+ * Install org.eclipse.xtext.testcollector and use the Xtext Tools/Create Test Suite
+ * action on the source folder to regenerate this.
+ */
 public class AutoTestSuite {
 
 	public static Test suite() {
 		TestSuite suite = new TestSuite("org.eclipse.xtext.service.tests");
-		suite.addTestSuite(org.eclipse.xtext.service.NestedServiceScopesTest.class);
 		suite.addTestSuite(org.eclipse.xtext.service.DependencyInjectionTest.class);
-		suite.addTestSuite(org.eclipse.xtext.service.ServiceScopeFactoryTest.class);
-		suite.addTestSuite(org.eclipse.xtext.service.RegistrationTest.class);
+		suite.addTestSuite(org.eclipse.xtext.service.NestedServiceScopesTest.class);
 		suite.addTestSuite(org.eclipse.xtext.service.PriorityTest.class);
+		suite.addTestSuite(org.eclipse.xtext.service.RegistrationTest.class);
+		suite.addTestSuite(org.eclipse.xtext.service.ServiceScopeFactoryTest.class);
 		suite.addTestSuite(org.eclipse.xtext.service.fieldInjection.FieldInjectionTest.class);
 		return suite;
 	}
---
+++ tests/org.eclipse.xtext.service.ui.tests/src/org/eclipse/xtext/service/ui/AutoTestSuite.java
@@ -0,0 +1,18 @@
+package org.eclipse.xtext.service.ui;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * DO NOT MODIFY MANUALLY! This class has been automatically generated. 
+ * Install org.eclipse.xtext.testcollector and use the Xtext Tools/Create Test Suite
+ * action on the source folder to regenerate this.
+ */
+public class AutoTestSuite {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("org.eclipse.xtext.service.ui.tests");
+		suite.addTestSuite(org.eclipse.xtext.service.ui.internal.LanguageDescriptorRegistrationTest.class);
+		return suite;
+	}
+}
