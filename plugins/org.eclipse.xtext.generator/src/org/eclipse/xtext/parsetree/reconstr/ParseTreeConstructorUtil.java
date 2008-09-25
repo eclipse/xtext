8e144c473af7b13dfde1877748e2e0020a5c8045
sefftinge <sefftinge>
2008-09-25 17:31:34 +0000

    applied patch from bug https://bugs.eclipse.org/bugs/show_bug.cgi?id=248463

--- plugins/org.eclipse.xtext.generator/src/org/eclipse/xtext/parsetree/reconstr/ParseTreeConstructorUtil.java
+++ plugins/org.eclipse.xtext.generator/src/org/eclipse/xtext/parsetree/reconstr/ParseTreeConstructorUtil.java
@@ -5,17 +5,19 @@ import java.util.List;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.AbstractElement;
 import org.eclipse.xtext.AbstractRule;
+import org.eclipse.xtext.Action;
 import org.eclipse.xtext.Alternatives;
 import org.eclipse.xtext.Assignment;
 import org.eclipse.xtext.Group;
 import org.eclipse.xtext.Keyword;
+import org.eclipse.xtext.RuleCall;
 
 public class ParseTreeConstructorUtil {
 
 	private static String toIDString(String val) {
 		if (val == null)
 			return "";
-		return val.replaceAll("[^a-zA-Z0-9]", "");
+		return val.replaceAll("[^a-zA-Z0-9_]", "");
 	}
 
 	public static String getUniqueElementName(AbstractElement ele) {
@@ -28,20 +30,27 @@ public class ParseTreeConstructorUtil {
 			r = ((Keyword) ele).getValue();
 		else if (ele instanceof Assignment)
 			r = ((Assignment) ele).getFeature();
+		else if (ele instanceof RuleCall)
+			r = ((RuleCall) ele).getName();
+		else if (ele instanceof Action) {
+			Action a = (Action) ele;
+			r = a.getTypeName().getName() + "_" + a.getFeature();
+		}
 		r = toIDString(r);
 		if (r.equals(""))
 			r = ele.eClass().getName();
 		else
 			r = ele.eClass().getName() + "_" + r;
 
-		while ((!(obj instanceof AbstractRule)) && obj.eContainer() != null) {
+		while ((!(obj.eContainer() instanceof AbstractRule))
+				&& obj.eContainer() != null) {
 			EObject tmp = obj.eContainer();
 			r = tmp.eContents().indexOf(obj) + "_" + r;
 			obj = tmp;
 		}
-		if (obj instanceof AbstractRule)
-			return ((AbstractRule) obj).getName() + "_" + r;
-		return "UnknownRule" + r;
+		if (obj.eContainer() instanceof AbstractRule)
+			return ((AbstractRule) obj.eContainer()).getName() + "_" + r;
+		return "UnknownRule_" + r;
 	}
 
 	// return the next sibling or the parent's next sibling
