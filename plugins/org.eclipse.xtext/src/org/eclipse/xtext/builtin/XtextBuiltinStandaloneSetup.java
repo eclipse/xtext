f19ce1fd53c7a1e9063b380f520a2b274608c7f5
sefftinge <sefftinge>
2008-09-25 11:52:46 +0000

    optional injection added, less verbose configuration mechanism introduced

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/builtin/XtextBuiltinStandaloneSetup.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/builtin/XtextBuiltinStandaloneSetup.java
@@ -1,41 +1,26 @@
 package org.eclipse.xtext.builtin;
 
+
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
-import org.eclipse.xtext.IGrammarAccess;
 import org.eclipse.xtext.XtextPackage;
-import org.eclipse.xtext.builtin.conversion.XtextBuiltInConverters;
-import org.eclipse.xtext.conversion.IValueConverterService;
-import org.eclipse.xtext.crossref.IFragmentProvider;
-import org.eclipse.xtext.crossref.ILinkProvider;
-import org.eclipse.xtext.crossref.ILinker;
-import org.eclipse.xtext.crossref.IURIChecker;
-import org.eclipse.xtext.crossref.impl.DefaultRuntimeURIChecker;
-import org.eclipse.xtext.crossref.impl.XtextBuiltinFragmentProvider;
-import org.eclipse.xtext.crossref.impl.XtextBuiltinLinkProvider;
-import org.eclipse.xtext.crossref.internal.Linker;
 import org.eclipse.xtext.service.ServiceRegistry;
+import org.eclipse.xtext.service.IServiceRegistrationFactory.IServiceRegistration;
 
 public class XtextBuiltinStandaloneSetup {
 
-    private static boolean isInitialized = false;
+	private static boolean isInitialized = false;
 
-    public synchronized final static void doSetup() {
-        if (!isInitialized) {
-            Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("ecore", new XMIResourceFactoryImpl());
-            Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
-            EPackage.Registry.INSTANCE.put(XtextPackage.eNS_URI,XtextPackage.eINSTANCE);
-            
-            ServiceRegistry.registerService(IXtextBuiltin.SCOPE, IGrammarAccess.class, XtextBuiltinGrammarAccess.class);
-            ServiceRegistry.registerService(IXtextBuiltin.SCOPE, IValueConverterService.class, XtextBuiltInConverters.class);
-            ServiceRegistry.registerService(IXtextBuiltin.SCOPE, ILinker.class, Linker.class);
-            ServiceRegistry.registerService(IXtextBuiltin.SCOPE, ILinkProvider.class, XtextBuiltinLinkProvider.class);
-            ServiceRegistry.registerService(IXtextBuiltin.SCOPE, IURIChecker.class, DefaultRuntimeURIChecker.class);
-            ServiceRegistry.registerService(IXtextBuiltin.SCOPE, IFragmentProvider.class, XtextBuiltinFragmentProvider.class);
-            
-            isInitialized = true;
-        }
-    }
-
+	public synchronized final static void doSetup() {
+		if (!isInitialized) {
+			Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("ecore", new XMIResourceFactoryImpl());
+			Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
+			EPackage.Registry.INSTANCE.put(XtextPackage.eNS_URI, XtextPackage.eINSTANCE);
+			for (IServiceRegistration reg :  new XtextBuiltinRuntimeConfig().registrations()) {
+				ServiceRegistry.registerFactory(reg.scope(), reg.serviceFactory(), reg.priority());
+			}
+			isInitialized = true;
+		}
+	}
 }
