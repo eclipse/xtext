38c7352aff62208e7495e144f3c0428e91d4ad52
jkohnlein <jkohnlein>
2008-10-01 13:40:11 +0000

    First shot at XtextMetamodelResource

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/resource/metamodel/XtextMetamodelResource.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/resource/metamodel/XtextMetamodelResource.java
@@ -12,20 +12,67 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.impl.ResourceImpl;
+import org.eclipse.xtext.Grammar;
+import org.eclipse.xtext.IGrammarAccess;
+import org.eclipse.xtext.service.IServiceScope;
+import org.eclipse.xtext.service.ServiceRegistry;
+import org.eclipse.xtext.service.ServiceScopeFactory;
+import org.eclipse.xtext.services.XtextResourceFactory;
 
 /**
  * @author Jan Köhnlein - Initial contribution and API
- *
+ * 
  */
 public class XtextMetamodelResource extends ResourceImpl {
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.emf.ecore.resource.impl.ResourceImpl#doLoad(java.io.InputStream, java.util.Map)
+	public XtextMetamodelResource(URI uri) {
+		super(uri);
+	}
+
+	private Grammar getGrammar(String languageID) {
+		IServiceScope serviceScope = ServiceScopeFactory.get(languageID);
+		IGrammarAccess service = ServiceRegistry.getService(serviceScope, IGrammarAccess.class);
+		return service.getGrammar();
+	}
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.emf.ecore.resource.impl.ResourceImpl#doLoad(java.io.InputStream
+	 * , java.util.Map)
 	 */
 	@Override
 	protected void doLoad(InputStream inputStream, Map<?, ?> options) throws IOException {
-		// TODO: implement
-		super.doLoad(inputStream, options);
+		XtextResourceFactory xtextResourceFactory = new XtextResourceFactory();
+		Resource xtextResource = xtextResourceFactory.createResource(getURI());
+//		xtextResource.load(inputStream, options);
+//		EList<EObject> grammarResourceContents = xtextResource.getContents();
+//		if (grammarResourceContents.size() != 1) {
+//			throw new IllegalStateException("XtextResource " + Strings.notNull(getURI().toString())
+//					+ " contains other than 1 elements");
+//		}
+//		EObject firstObject = grammarResourceContents.get(0);
+//		if (!(firstObject instanceof Grammar)) {
+//			throw new IllegalStateException("XtextResource " + Strings.notNull(getURI().toString())
+//					+ " does not contain a root element of type Grammar");
+//		}
+//		Grammar grammar = (Grammar) firstObject;
+//		Xtext2EcoreTransformer xtext2EcoreTransformer = new Xtext2EcoreTransformer();
+//		List<EPackage> ePackages = xtext2EcoreTransformer.transform(grammar);
+//		getContents().addAll(ePackages);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.emf.ecore.resource.impl.ResourceImpl#doUnload()
+	 */
+	@Override
+	protected void doUnload() {
+		// TODO Auto-generated method stub
+		super.doUnload();
 	}
 }
---
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/resource/metamodel/XtextMetamodelResourceFactory.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2008 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.resource.metamodel;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceFactoryImpl;
+
+/**
+ * @author Jan Köhnlein - Initial contribution and API
+ */
+public class XtextMetamodelResourceFactory extends ResourceFactoryImpl {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.emf.ecore.resource.impl.ResourceFactoryImpl#createResource(org.eclipse.emf.common.util.URI)
+	 */
+	@Override
+	public Resource createResource(URI uri) {
+		// TODO Auto-generated method stub
+		return new XtextMetamodelResource(uri);
+	}
+}
