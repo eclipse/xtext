7dcfde418f0b4ef22fe7527ff88f83c452045b29
jkohnlein <jkohnlein>
2008-10-23 15:14:08 +0000

    NEW - bug 251856: [Core] NodeContentAdapter does not work https://bugs.eclipse.org/bugs/show_bug.cgi?id=251856

---
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/parsetree/NodeContentAdapterTest.java
@@ -0,0 +1,175 @@
+/*******************************************************************************
+ * Copyright (c) 2008 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.parsetree;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Jan Köhnlein - Initial contribution and API
+ */
+public class NodeContentAdapterTest extends TestCase {
+	
+	private static final String TEXT0 = "Tests\n";
+	private static final String TEXT1 = "are\n";
+	private static final String TEXT2 = "so\n";
+	private static final String TEXT3 = "great\n";
+	private static final String TEXT4 = "not\n";
+
+	private CompositeNode root;
+	private CompositeNode comp0;
+	private CompositeNode comp1;
+	private LeafNode leaf0;
+	private LeafNode leaf1;
+	private LeafNode leaf2;
+	private LeafNode leaf3;
+	private NodeContentAdapter contentAdapter;
+
+	/**
+	 * root +- leaf0
+	 *      +- comp0 +- leaf1
+	 *      +- comp1 +- leaf2
+	 *      +- leaf3
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		root = ParsetreeFactory.eINSTANCE.createCompositeNode();
+		contentAdapter = new NodeContentAdapter();
+	}
+
+	private void createTree(CompositeNode root) {
+		comp0 = ParsetreeFactory.eINSTANCE.createCompositeNode();
+		comp1 = ParsetreeFactory.eINSTANCE.createCompositeNode();
+		leaf0 = ParsetreeFactory.eINSTANCE.createLeafNode();
+		leaf1 = ParsetreeFactory.eINSTANCE.createLeafNode();
+		leaf2 = ParsetreeFactory.eINSTANCE.createLeafNode();
+		leaf3 = ParsetreeFactory.eINSTANCE.createLeafNode();
+		
+		leaf0.setText(TEXT0);
+		leaf1.setText(TEXT1);
+		leaf2.setText(TEXT2);
+		leaf3.setText(TEXT3);
+		
+		root.getChildren().add(leaf0);
+		root.getChildren().add(comp0);
+		comp0.getChildren().add(leaf1);
+		root.getChildren().add(comp1);
+		comp1.getChildren().add(leaf2);
+		root.getChildren().add(leaf3);
+	}
+	
+	public void testAdaptContainment() throws Exception {
+		createTree(root);
+		root.eAdapters().add(contentAdapter);
+		assertInitialSetting();		
+	}
+
+	public void testAdaptOnAdd() throws Exception {
+		root.eAdapters().add(contentAdapter);
+		createTree(root);
+		assertInitialSetting();		
+	}
+
+	private void assertInitialSetting() {
+		assertTrue(comp0.eAdapters().contains(contentAdapter));
+		assertTrue(comp1.eAdapters().contains(contentAdapter));
+		assertTrue(leaf0.eAdapters().contains(contentAdapter));
+		assertTrue(leaf1.eAdapters().contains(contentAdapter));
+		assertTrue(leaf2.eAdapters().contains(contentAdapter));
+		assertTrue(leaf3.eAdapters().contains(contentAdapter));
+		
+		assertEquals(0, root.getOffset());
+		assertEquals(0, leaf0.getOffset());
+		assertEquals(TEXT0.length(), comp0.getOffset());
+		assertEquals(TEXT0.length(), leaf1.getOffset());
+		assertEquals(TEXT0.length() + TEXT1.length(), comp1.getOffset());
+		assertEquals(TEXT0.length() + TEXT1.length(), leaf2.getOffset());
+		assertEquals(TEXT0.length() + TEXT1.length() + TEXT2.length(), leaf3.getOffset());
+
+		assertEquals(TEXT0.length() + TEXT1.length() + TEXT2.length() + TEXT3.length(), root.getLength());
+		assertEquals(TEXT0.length(), leaf0.getLength());
+		assertEquals(TEXT1.length(), comp0.getLength());
+		assertEquals(TEXT1.length(), leaf1.getLength());
+		assertEquals(TEXT2.length(), comp1.getLength());
+		assertEquals(TEXT2.length(), leaf2.getLength());
+		assertEquals(TEXT3.length(), leaf3.getLength());
+		
+		assertEquals(1, root.getLine());
+		assertEquals(1, leaf0.getLine());
+		assertEquals(2, comp0.getLine());
+		assertEquals(2, leaf1.getLine());
+		assertEquals(3, comp1.getLine());
+		assertEquals(3, leaf2.getLine());
+		assertEquals(4, leaf3.getLine());
+	}
+
+	
+	public void testRemove() throws Exception {
+		createTree(root);
+		root.eAdapters().add(contentAdapter);
+		
+		comp0.getChildren().remove(leaf1);
+
+		assertEquals(0, root.getOffset());
+		assertEquals(0, leaf0.getOffset());
+		assertEquals(TEXT0.length(), comp0.getOffset());
+		assertEquals(TEXT0.length(), comp1.getOffset());
+		assertEquals(TEXT0.length(), leaf2.getOffset());
+		assertEquals(TEXT0.length() + TEXT2.length(), leaf3.getOffset());
+		
+		assertEquals(TEXT0.length() + TEXT2.length() + TEXT3.length(), root.getLength());
+		assertEquals(TEXT0.length(), leaf0.getLength());
+		assertEquals(0, comp0.getLength());
+		assertEquals(TEXT2.length(), comp1.getLength());		
+		assertEquals(TEXT2.length(), leaf2.getLength());		
+		assertEquals(TEXT3.length(), leaf3.getLength());		
+		
+		assertEquals(1, root.getLine());
+		assertEquals(1, leaf0.getLine());
+		assertEquals(2, comp0.getLine());
+		assertEquals(2, comp1.getLine());
+		assertEquals(2, leaf2.getLine());
+		assertEquals(3, leaf3.getLine());
+	}
+
+	public void testAdd() throws Exception {
+		createTree(root);
+		root.eAdapters().add(contentAdapter);
+		
+		LeafNode leaf4 = ParsetreeFactory.eINSTANCE.createLeafNode();
+		leaf4.setText(TEXT4);
+		
+		comp0.getChildren().add(0, leaf4);
+
+		assertEquals(0, root.getOffset());
+		assertEquals(0, leaf0.getOffset());
+		assertEquals(TEXT0.length(), comp0.getOffset());
+		assertEquals(TEXT0.length(), leaf4.getOffset());
+		assertEquals(TEXT0.length() + TEXT4.length(), leaf1.getOffset());
+		assertEquals(TEXT0.length() + TEXT1.length() + TEXT4.length(), comp1.getOffset());
+		assertEquals(TEXT0.length() + TEXT1.length() + TEXT4.length(), leaf2.getOffset());
+		assertEquals(TEXT0.length() + TEXT1.length() + TEXT2.length() + TEXT4.length(), leaf3.getOffset());
+
+		assertEquals(TEXT0.length() + TEXT1.length() + TEXT2.length() + TEXT3.length() + TEXT4.length(), root.getLength());
+		assertEquals(TEXT0.length(), leaf0.getLength());
+		assertEquals(TEXT4.length() + TEXT1.length(), comp0.getLength());
+		assertEquals(TEXT4.length(), leaf4.getLength());
+		assertEquals(TEXT1.length(), leaf1.getLength());
+		assertEquals(TEXT2.length(), comp1.getLength());
+		assertEquals(TEXT2.length(), leaf2.getLength());
+		assertEquals(TEXT3.length(), leaf3.getLength());
+		
+		assertEquals(1, root.getLine());
+		assertEquals(1, leaf0.getLine());
+		assertEquals(2, comp0.getLine());
+		assertEquals(2, leaf4.getLine());
+		assertEquals(3, leaf1.getLine());
+		assertEquals(4, comp1.getLine());
+		assertEquals(4, leaf2.getLine());
+		assertEquals(5, leaf3.getLine());
+	}
+}
