2fe23e0eb2bb5cd742ad94d97803792b6a8dd10e
mclay <mclay>
2008-10-27 08:28:57 +0000

    add: start working on bug https://bugs.eclipse.org/bugs/show_bug.cgi?id=250383 (Implementation of org.eclipse.xtext.ui.common.editor.codecompletion.DefaultContentAssistProcessor)

--- plugins/org.eclipse.xtext.ui.common/src/org/eclipse/xtext/ui/common/editor/codecompletion/IProposalProvider.java
+++ plugins/org.eclipse.xtext.ui.common/src/org/eclipse/xtext/ui/common/editor/codecompletion/IProposalProvider.java
@@ -16,7 +16,7 @@ import org.eclipse.xtext.RuleCall;
  * In addition to the declared methods, the framework tries to call grammar dependent methods for assignments using reflection.
  * The signature of such methods invoked reflectively follows the following pattern:
  * 
- * public List<ICompletionProposal> complete[Typename]_[featureName](Assignment ele, EObject model, String prefix)
+ * public List<ICompletionProposal> complete[Typename][featureName](Assignment ele, EObject model, String prefix)
  * 
  * <b>Example</b>
  *  Given the following grammar :
@@ -27,12 +27,12 @@ import org.eclipse.xtext.RuleCall;
  *
  * One could provide the following method in an implementation of this interface:
  *  <code>
- * 	public List<ICompletionProposal> completeMyType_name(Assignment ele, EObject model, String prefix, IDocument doc) {...}
+ * 	public List<ICompletionProposal> completeMyTypeName(Assignment ele, EObject model, String prefix, IDocument doc) {...}
  *  </code>
  *  Note that if you have generated Java classes for your domain model (meta model) you can alternatively declare the second parameter using 
  *  a specific type:
  *  <code>
- * 	public List<ICompletionProposal> completeMyType_name(Assignment ele, MyType model, String prefix, IDocument doc) {...}
+ * 	public List<ICompletionProposal> completeMyTypeName(Assignment ele, MyType model, String prefix, IDocument doc) {...}
  *  </code>
  *   
  */
@@ -48,7 +48,7 @@ public interface IProposalProvider {
 	 * @param doc - the IDocument instance used by the editor
 	 * @return
 	 */
-	public List<ICompletionProposal> completeKeyword(Keyword ele, EObject model, String prefix, IDocument doc);
+	public List<ICompletionProposal> completeKeyword(Keyword ele, EObject model, String prefix, IDocument doc,int offset);
 
 	/**
 	 * Is invoked by the framework if (with respect to the grammar) it is possible that the keyword passed as first parameter
