0164a2e1985a375aae5857e9f91568cd0664cbc0
jkohnlein <jkohnlein>
2008-10-29 09:03:49 +0000

    Applied patch https://bugs.eclipse.org/bugs/attachment.cgi?id=116272

---
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/crossrefs/LinkingErrorTest.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2008 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.crossrefs;
+
+import org.apache.log4j.Logger;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource.Diagnostic;
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.testlanguages.ReferenceGrammarStandaloneSetup;
+import org.eclipse.xtext.tests.AbstractGeneratorTest;
+
+/**
+ * @author Heiko Behrens - Initial contribution and API
+ */
+public class LinkingErrorTest extends AbstractGeneratorTest {
+
+	private static final Logger logger = Logger.getLogger(CrossRefTest.class);
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		with(LangAStandaloneSetup.class);
+	}
+	
+	public void testNoErrors() throws Exception {
+		XtextResource resource = getResourceFromString(" type A extends B \n type B extends A");
+		EObject model = getModel(resource);
+		logger.debug(invokeWithXtend("types.collect(e|e.name+'->'+e.extends.name).toString(',')", model));
+		assertWithXtend("2", "types.size", model);
+		assertEquals(0, resource.getErrors().size());
+		assertEquals(0, resource.getWarnings().size());
+	}
+	
+	public void testLinkError() throws Exception {
+		XtextResource resource = getResourceFromString(" type A extends B \n type B extends C");
+		EObject model = getModel(resource);
+		logger.debug(invokeWithXtend("types.collect(e|e.name+'->'+e.extends.name).toString(',')", model));
+		assertWithXtend("2", "types.size", model);
+		assertEquals(1, resource.getErrors().size());
+		assertEquals(0, resource.getWarnings().size());
+		
+		Diagnostic error = resource.getErrors().get(0);
+		assertEquals(2, error.getLine());
+		assertTrue(error instanceof XtextResource.Diagnostic);
+		XtextResource.Diagnostic verboseError = (XtextResource.Diagnostic)error;
+		assertEquals(35, verboseError.getOffset());
+		assertEquals(1, verboseError.getLength());
+	}
+
+}
