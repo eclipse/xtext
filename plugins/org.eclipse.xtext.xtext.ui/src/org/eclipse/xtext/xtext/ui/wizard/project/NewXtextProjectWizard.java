21e35149d2b7bbb1a1f5b4b422fec3a023ee977a
sefftinge <sefftinge>
2008-11-03 14:20:23 +0000

    removed direct dependency to log4j

--- plugins/org.eclipse.xtext.xtext.ui/src/org/eclipse/xtext/xtext/ui/wizard/project/NewXtextProjectWizard.java
+++ plugins/org.eclipse.xtext.xtext.ui/src/org/eclipse/xtext/xtext/ui/wizard/project/NewXtextProjectWizard.java
@@ -194,7 +194,7 @@ public class NewXtextProjectWizard extends Wizard implements INewWizard {
 			final IProject dslProject = EclipseResourceUtil.createProject(xtextProjectInfo.getProjectName(),
 					SRC_FOLDER_LIST, Collections.<IProject> emptyList(), new LinkedHashSet<String>(Arrays.asList(
 							"org.eclipse.xtext.log4j;bundle-version=\"1.2.15\"", "org.eclipse.xtext",
-							"org.eclipse.xtext.generator", "org.apache.log4j", "org.antlr.gen", "org.eclipse.xtend",
+							"org.eclipse.xtext.generator", "org.antlr.gen", "org.eclipse.xtend",
 							"org.eclipse.xtend.typesystem.emf", "org.eclipse.xpand", "org.apache.commons.logging",
 							"org.eclipse.xtend.util.stdlib")), exportedPackages, null, monitor, NewXtextProjectWizard.this.getShell());
 
