827c1bbc8716bc4e6777d8b3c14d90d24a5e8f20
pfriese <pfriese>
2008-11-05 14:43:05 +0000

    Improved performance for URI fragment construction.

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/crossref/impl/XtextBuiltinFragmentProvider.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/crossref/impl/XtextBuiltinFragmentProvider.java
@@ -1,21 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2008 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package org.eclipse.xtext.crossref.impl;
 
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.xtext.crossref.IFragmentProvider;
 
+/**
+ * @author Sven Efftinge - Initial contribution and API
+ * @author Peter Friese
+ */
 public class XtextBuiltinFragmentProvider implements IFragmentProvider {
 
 	public String getFragment(EObject obj) {
 		EClass eclass = obj.eClass();
-		EList<EAttribute> attributes = eclass.getEAllAttributes();
-		for (EAttribute a : attributes) {
-			if (a.getName().equals("name")) {
-				Object name = obj.eGet(a);
-				if (name instanceof String)
+		
+		EStructuralFeature structuralFeature = eclass.getEStructuralFeature("name");
+		if (structuralFeature != null) {
+			if (structuralFeature instanceof EAttribute) {
+				EAttribute attribute = (EAttribute) structuralFeature;
+				Object name = obj.eGet(attribute);
+				if (name instanceof String) {
 					return (String) name;
+				}
 			}
 		}
 		return null;
