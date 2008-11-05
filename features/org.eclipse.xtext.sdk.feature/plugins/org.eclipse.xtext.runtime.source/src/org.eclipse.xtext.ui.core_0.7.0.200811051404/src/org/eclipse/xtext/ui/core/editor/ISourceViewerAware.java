2ad8ba2676e12f3b4e7b9946bd377a400960c6d3
dhubner <dhubner>
2008-11-05 16:11:20 +0000

    unnecessary  content

--- features/org.eclipse.xtext.sdk.feature/plugins/org.eclipse.xtext.runtime.source/src/org.eclipse.xtext.ui.core_0.7.0.200811051404/src/org/eclipse/xtext/ui/core/editor/ISourceViewerAware.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.xtext.ui.core.editor;
-
-import org.eclipse.jface.text.source.ISourceViewer;
-
-public interface ISourceViewerAware {
-	void setSourceViewer(ISourceViewer sourceViewer);
-}
