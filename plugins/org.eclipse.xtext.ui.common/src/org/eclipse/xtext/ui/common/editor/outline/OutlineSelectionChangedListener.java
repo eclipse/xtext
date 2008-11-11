b6b6bd40edaebeb79a053274dc64713d177b1bcd
pfriese <pfriese>
2008-11-11 14:34:12 +0000

    Added missing copyright headers.

--- plugins/org.eclipse.xtext.ui.common/src/org/eclipse/xtext/ui/common/editor/outline/OutlineSelectionChangedListener.java
+++ plugins/org.eclipse.xtext.ui.common/src/org/eclipse/xtext/ui/common/editor/outline/OutlineSelectionChangedListener.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2008 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
 package org.eclipse.xtext.ui.common.editor.outline;
 
 import org.eclipse.emf.common.util.EList;
@@ -18,6 +26,8 @@ import org.eclipse.xtext.ui.core.editor.model.UnitOfWork;
 /**
  * This listener listens to selections in the outline and will update the
  * editor selection accordingly.
+ * 
+ * @author Peter Friese - Initial contribution and API
  */
 public final class OutlineSelectionChangedListener extends AbstractSelectionChangedListener {
 
