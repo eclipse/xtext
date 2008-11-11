fe5c771f67181e52ec243080f6790659a8857df0
pfriese <pfriese>
2008-11-11 14:34:12 +0000

    Added missing copyright headers.

--- plugins/org.eclipse.xtext.ui.common/src/org/eclipse/xtext/ui/common/editor/outline/AbstractSelectionChangedListener.java
+++ plugins/org.eclipse.xtext.ui.common/src/org/eclipse/xtext/ui/common/editor/outline/AbstractSelectionChangedListener.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2008 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
 package org.eclipse.xtext.ui.common.editor.outline;
 
 import org.eclipse.core.runtime.Assert;
@@ -8,6 +16,9 @@ import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.xtext.ui.core.editor.model.IXtextDocument;
 import org.eclipse.xtext.ui.core.editor.model.XtextDocumentUtil;
 
+/**
+ * @author Peter Friese - Initial contribution and API
+ */
 public abstract class AbstractSelectionChangedListener implements ISelectionChangedListener  {
 
 	protected final XtextContentOutlinePage outlinePage;
--- plugins/org.eclipse.xtext.ui.common/src/org/eclipse/xtext/ui/common/editor/outline/NodeBasedTreeContentProvider.java
+++ plugins/org.eclipse.xtext.ui.common/src/org/eclipse/xtext/ui/common/editor/outline/NodeBasedTreeContentProvider.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2008 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
 package org.eclipse.xtext.ui.common.editor.outline;
 
 import java.util.Arrays;
@@ -10,6 +18,9 @@ import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.xtext.parsetree.NodeAdapter;
 import org.eclipse.xtext.parsetree.NodeUtil;
 
+/**
+ * @author Peter Friese - Initial contribution and API
+ */
 public class NodeBasedTreeContentProvider implements ITreeContentProvider {
 
 	private static Logger log = Logger.getLogger(NodeBasedTreeContentProvider.class);
