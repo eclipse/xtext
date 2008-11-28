535d972e2e9d56b31374a8770f8503dd975ba07f
jkohnlein <jkohnlein>
2008-11-28 10:13:02 +0000

    Added support for external packages (e.g. plugins in the workspace the current workbench has been started from)

--- plugins/org.eclipse.xtext.ui.core/src/org/eclipse/xtext/ui/core/util/JdtClasspathUriResolver.java
+++ plugins/org.eclipse.xtext.ui.core/src/org/eclipse/xtext/ui/core/util/JdtClasspathUriResolver.java
@@ -14,6 +14,7 @@ import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.internal.core.ExternalPackageFragmentRoot;
 import org.eclipse.jdt.internal.core.JarPackageFragmentRoot;
 import org.eclipse.xtext.resource.ClasspathUriResolutionException;
 import org.eclipse.xtext.resource.ClasspathUriUtil;
@@ -22,59 +23,72 @@ import org.eclipse.xtext.resource.IClasspathUriResolver;
 @SuppressWarnings("restriction")
 public class JdtClasspathUriResolver implements IClasspathUriResolver {
 
-    private IJavaElement javaElement;
+	private IJavaElement javaElement;
 
-    public URI resolve(Object context, URI classpathUri) {
-        if (!(context instanceof IJavaElement)) {
-            throw new IllegalArgumentException("Context must implement IResource");
-        }
-        javaElement = (IJavaElement) context;
-        try {
-            if (ClasspathUriUtil.isClassapthUri(classpathUri)) {
-                IJavaProject javaProject = javaElement.getJavaProject();
-                return findResourceInWorkspace(javaProject, classpathUri);
-            }
-        } catch (Exception exc) {
-            throw new ClasspathUriResolutionException(exc);
-        }
-        return classpathUri;
-    }
+	public URI resolve(Object context, URI classpathUri) {
+		if (!(context instanceof IJavaElement)) {
+			throw new IllegalArgumentException("Context must implement IResource");
+		}
+		javaElement = (IJavaElement) context;
+		try {
+			if (ClasspathUriUtil.isClassapthUri(classpathUri)) {
+				IJavaProject javaProject = javaElement.getJavaProject();
+				return findResourceInWorkspace(javaProject, classpathUri);
+			}
+		}
+		catch (Exception exc) {
+			throw new ClasspathUriResolutionException(exc);
+		}
+		return classpathUri;
+	}
 
-    protected static URI findResourceInWorkspace(IJavaProject javaProject, URI classpathUri) throws JavaModelException, CoreException {
-        Path fullPath = new Path(classpathUri.path());
-        String projectRelativePath = fullPath.toString();
-        if (javaProject.exists()) {
-            IPackageFragmentRoot[] allPackageFragmentRoots = javaProject.getAllPackageFragmentRoots();
-            for (IPackageFragmentRoot packageFragmentRoot : allPackageFragmentRoots) {
-                IResource correspondingResource = packageFragmentRoot.getCorrespondingResource();
-                if (correspondingResource != null) {
-                    if (correspondingResource instanceof IFile) {
-                        // jar file
-                        JarPackageFragmentRoot jarPackageFragmentRoot = (JarPackageFragmentRoot) JavaCore
-                                .createJarPackageFragmentRootFrom((IFile) correspondingResource);
-                        if (jarPackageFragmentRoot != null) {
-                            ZipFile zipFile = jarPackageFragmentRoot.getJar();
-                            if (zipFile != null) {
-                                ZipEntry zipEntry = zipFile.getEntry(projectRelativePath.substring(1));
-                                if (zipEntry != null) {
-                                    return URI.createURI("jar:" + "platform:/resource" + correspondingResource.getFullPath() + "!"
-                                            + projectRelativePath, true);
-                                }
-                            }
-                        }
-                    } else {
-                        // plain file
-                        IFolder rootFolder = (IFolder) correspondingResource;
-                        IResource modelFile = rootFolder.findMember(projectRelativePath);
-                        if (modelFile != null && modelFile.exists() && modelFile instanceof IFile) {
-                            URI platformResourceUri = URI.createPlatformResourceURI(modelFile.getFullPath().toString(), true);
-                            return platformResourceUri;
-                        }
-                    }
-                }
-            }
-        }
-        return classpathUri;
-    }
+	protected static URI findResourceInWorkspace(IJavaProject javaProject, URI classpathUri) throws JavaModelException,
+			CoreException {
+		Path fullPath = new Path(classpathUri.path());
+		String projectRelativePath = fullPath.toString();
+		if (javaProject.exists()) {
+			IPackageFragmentRoot[] allPackageFragmentRoots = javaProject.getAllPackageFragmentRoots();
+			for (IPackageFragmentRoot packageFragmentRoot : allPackageFragmentRoots) {
+				IResource correspondingResource = packageFragmentRoot.getCorrespondingResource();
+				if (correspondingResource != null && correspondingResource instanceof IFile) {
+					// jar file
+					JarPackageFragmentRoot jarPackageFragmentRoot = (JarPackageFragmentRoot) JavaCore
+							.createJarPackageFragmentRootFrom((IFile) correspondingResource);
+					if (jarPackageFragmentRoot != null) {
+						ZipFile zipFile = jarPackageFragmentRoot.getJar();
+						if (zipFile != null) {
+							ZipEntry zipEntry = zipFile.getEntry(projectRelativePath.substring(1));
+							if (zipEntry != null) {
+								return URI.createURI("jar:" + "platform:/resource"
+										+ correspondingResource.getFullPath() + "!" + projectRelativePath, true);
+							}
+						}
+					}
+				}
+				else {
+					// folder
+					IFolder rootFolder = null;
+					if (correspondingResource instanceof IFolder) {
+						rootFolder = (IFolder) correspondingResource;
+					}
+					else if (packageFragmentRoot instanceof ExternalPackageFragmentRoot) {
+						IResource resource = ((ExternalPackageFragmentRoot) packageFragmentRoot).resource();
+						if (resource instanceof IFolder) {
+							rootFolder = (IFolder) resource;
+						}
+					}
+					if (rootFolder != null) {
+						IResource modelFile = rootFolder.findMember(projectRelativePath);
+						if (modelFile != null && modelFile.exists() && modelFile instanceof IFile) {
+							URI platformResourceUri = URI.createPlatformResourceURI(modelFile.getFullPath().toString(),
+									true);
+							return platformResourceUri;
+						}
+					}
+				}
+			}
+		}
+		return classpathUri;
+	}
 
 }
