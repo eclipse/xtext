456512af21547ecb6c809109919baf2c5ebeed25
meysholdt <meysholdt>
2009-01-12 17:42:35 +0000

    - improved error reporting of the ParseTreeConstructor - fixed a recursion bug in the ParseTreeConstructor

--- tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/parsetree/formatter/FormatterTest.java
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/parsetree/formatter/FormatterTest.java
@@ -20,6 +20,18 @@ public class FormatterTest extends AbstractGeneratorTest {
 		assertEquals(expected, res);
 	}
 
+	public void _testKeepComments() throws Exception {
+		// final String model = "test linewrap float val; int x; double y;";
+		final String model = "// begincomment \ntest linewrap // comment1\n"
+				+ "float val; //comment2\n" + "int x;\n"
+				+ "double y;//yoyoyo!\n// endcomment.";
+		EObject m = getModel(model);
+		// System.out.println(EmfFormater.objToStr(m, ""));
+		String res = serialize(m);
+		// System.out.println(res);
+		assertEquals(model, res);
+	}
+
 	public void testIndentation() throws Exception {
 		final String model = "test indentation { float val; indentation { int x; } double y; }";
 		final String expected = "test indentation {\n  indentation {\n    int x;\n  }\n  float val;\n  double y;\n}";
