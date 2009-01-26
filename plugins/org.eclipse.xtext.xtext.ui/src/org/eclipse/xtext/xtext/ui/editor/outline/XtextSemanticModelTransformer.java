1dee0693acbb9947eaea75ef5fcd9493711dd91e
pfriese <pfriese>
2009-01-26 01:06:20 +0000

    RESOLVED - bug 261974: DefaultSemanticModelTransformer.consumeSemanticNode is not NullSafe https://bugs.eclipse.org/bugs/show_bug.cgi?id=261974

--- plugins/org.eclipse.xtext.xtext.ui/src/org/eclipse/xtext/xtext/ui/editor/outline/XtextSemanticModelTransformer.java
+++ plugins/org.eclipse.xtext.xtext.ui/src/org/eclipse/xtext/xtext/ui/editor/outline/XtextSemanticModelTransformer.java
@@ -17,54 +17,65 @@ import org.eclipse.xtext.util.XtextSwitch;
 /**
  * @author Peter Friese - Initial contribution and API
  */
-public class XtextSemanticModelTransformer extends
-		DefaultSemanticModelTransformer {
-	
+public class XtextSemanticModelTransformer extends DefaultSemanticModelTransformer {
+
 	@Override
 	public boolean consumeSemanticChildNodes(EObject semanticNode) {
-		return new XtextSwitch<Boolean>() {
-			@Override
-			public Boolean caseGrammar(Grammar object) {
-				return true;
-			}
-			@Override
-			public Boolean caseParserRule(org.eclipse.xtext.ParserRule object) {
-				return false;
-			};
-			public Boolean defaultCase(EObject object) {
-				return false;
-			};
-		}.doSwitch(semanticNode);
+		if (semanticNode != null) {
+			return new XtextSwitch<Boolean>() {
+				@Override
+				public Boolean caseGrammar(Grammar object) {
+					return true;
+				}
+
+				@Override
+				public Boolean caseParserRule(org.eclipse.xtext.ParserRule object) {
+					return false;
+				};
+
+				public Boolean defaultCase(EObject object) {
+					return false;
+				};
+			}.doSwitch(semanticNode);
+		} else {
+			return true;
+		}
 
 	}
-	
+
 	@Override
-	protected ContentOutlineNode createOutlineNode(EObject semanticNode,
-			ContentOutlineNode outlineParentNode) {
+	protected ContentOutlineNode createOutlineNode(EObject semanticNode, ContentOutlineNode outlineParentNode) {
 		final ContentOutlineNode outlineNode = super.createOutlineNode(semanticNode, outlineParentNode);
-		
-		return new XtextSwitch<ContentOutlineNode>() {
-			@Override
-			public ContentOutlineNode caseGrammar(Grammar object) {
-				outlineNode.setLabel("Grammar " + object.getIdElements());
-				return outlineNode;
-			}
-			@Override
-			public ContentOutlineNode caseGeneratedMetamodel(org.eclipse.xtext.GeneratedMetamodel object) {
-				outlineNode.setLabel("GeneratedMM: " + object.getName());
-				return outlineNode;
-			};
-			@Override
-			public ContentOutlineNode caseReferencedMetamodel(org.eclipse.xtext.ReferencedMetamodel object) {
-				outlineNode.setLabel("ReferencedMM " + object.getAlias());
-				return outlineNode;
-			};
-			@Override
-			public ContentOutlineNode caseParserRule(org.eclipse.xtext.ParserRule object) {
-				outlineNode.setLabel(object.getName());
-				return outlineNode;
-			};
-		}.doSwitch(semanticNode);
+
+		if (semanticNode != null) {
+			return new XtextSwitch<ContentOutlineNode>() {
+				@Override
+				public ContentOutlineNode caseGrammar(Grammar object) {
+					outlineNode.setLabel("Grammar " + object.getIdElements());
+					return outlineNode;
+				}
+
+				@Override
+				public ContentOutlineNode caseGeneratedMetamodel(org.eclipse.xtext.GeneratedMetamodel object) {
+					outlineNode.setLabel("GeneratedMM: " + object.getName());
+					return outlineNode;
+				};
+
+				@Override
+				public ContentOutlineNode caseReferencedMetamodel(org.eclipse.xtext.ReferencedMetamodel object) {
+					outlineNode.setLabel("ReferencedMM " + object.getAlias());
+					return outlineNode;
+				};
+
+				@Override
+				public ContentOutlineNode caseParserRule(org.eclipse.xtext.ParserRule object) {
+					outlineNode.setLabel(object.getName());
+					return outlineNode;
+				};
+			}.doSwitch(semanticNode);
+		} else {
+			return outlineNode;
+		}
 	}
-	
+
 }
