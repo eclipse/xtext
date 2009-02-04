890843e1fd5a92961061e937da406b182fcd3f15
jkohnlein <jkohnlein>
2009-02-04 15:43:55 +0000

    Fixed test suites

--- tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/generator/PluginTestSuite.java
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/generator/PluginTestSuite.java
@@ -4,11 +4,12 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 /**
- * DO NOT MODIFY MANUALLY! This class has been automatically generated. 
- * Install org.eclipse.xtext.testcollector and use the Xtext Tools/Create Test Suite
- * action on the source folder to regenerate this.
+ * Runs all tests in this plug-in. Delegates to eclipse auto-generated test
+ * suites (New->TestSuite) in the contained packages.
+ * 
+ * @author Jan Koehnlein
  */
-public class AutoTestSuite {
+public class PluginTestSuite {
 
 	public static Test suite() {
 		TestSuite suite = new TestSuite("org.eclipse.xtext.generator.tests");
