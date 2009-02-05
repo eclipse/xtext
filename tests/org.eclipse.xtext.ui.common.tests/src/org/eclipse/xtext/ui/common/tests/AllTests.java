41f78ea9728ab73439d3bb36564682ece886d87c
dhubner <dhubner>
2009-02-05 09:07:06 +0000

    adding common.ui.tests

---
+++ tests/org.eclipse.xtext.ui.common.tests/src/org/eclipse/xtext/ui/common/tests/AllTests.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2008 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+package org.eclipse.xtext.ui.common.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.eclipse.xtext.ui.common.editor.contentassist.impl.DefaultContentAssistProcessorTest;
+import org.eclipse.xtext.ui.common.editor.outline.impl.DefaultSemanticModelTransformerTest;
+
+/**
+ * @author Dennis Hübner - Initial contribution and API
+ * 
+ */
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test for org.eclipse.xtext.ui.common.tests");
+		// $JUnit-BEGIN$
+		suite.addTestSuite(DefaultContentAssistProcessorTest.class);
+
+		suite.addTestSuite(DefaultSemanticModelTransformerTest.class);
+		// $JUnit-END$
+		return suite;
+	}
+
+}
