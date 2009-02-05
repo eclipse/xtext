87f4977fb1445943ca2e1bc31e7e0b65a8cfbf9f
szarnekow <szarnekow>
2009-02-05 08:18:39 +0000

    minor experimental beautifying of the xtext outline, added missing tests to xtext.test.plugintests, updated mapfile

--- plugins/org.eclipse.xtext.xtext.ui/src/org/eclipse/xtext/xtext/ui/editor/outline/XtextSemanticModelTransformer.java
+++ plugins/org.eclipse.xtext.xtext.ui/src/org/eclipse/xtext/xtext/ui/editor/outline/XtextSemanticModelTransformer.java
@@ -8,10 +8,18 @@
  *******************************************************************************/
 package org.eclipse.xtext.xtext.ui.editor.outline;
 
+import java.util.List;
+
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.Grammar;
+import org.eclipse.xtext.GrammarUtil;
+import org.eclipse.xtext.RuleCall;
+import org.eclipse.xtext.parsetree.LeafNode;
+import org.eclipse.xtext.parsetree.NodeAdapter;
+import org.eclipse.xtext.parsetree.NodeUtil;
 import org.eclipse.xtext.ui.common.editor.outline.ContentOutlineNode;
 import org.eclipse.xtext.ui.common.editor.outline.impl.DefaultSemanticModelTransformer;
+import org.eclipse.xtext.util.Strings;
 import org.eclipse.xtext.util.XtextSwitch;
 import org.eclipse.xtext.xtext.ui.Activator;
 
@@ -27,12 +35,12 @@ public class XtextSemanticModelTransformer extends DefaultSemanticModelTransform
 				@Override
 				public Boolean caseTypeRef(org.eclipse.xtext.TypeRef object) {
 					return false;
-				};
+				}
 
 				@Override
 				public Boolean defaultCase(EObject object) {
 					return true;
-				};
+				}
 			}.doSwitch(semanticNode);
 		} else {
 			return false;
@@ -51,17 +59,17 @@ public class XtextSemanticModelTransformer extends DefaultSemanticModelTransform
 				@Override
 				public Boolean caseParserRule(org.eclipse.xtext.ParserRule object) {
 					return true;
-				};
+				}
 
 				@Override
 				public Boolean caseAlternatives(org.eclipse.xtext.Alternatives object) {
 					return true;
-				};
+				}
 
 				@Override
 				public Boolean defaultCase(EObject object) {
 					return true;
-				};
+				}
 			}.doSwitch(semanticNode);
 		} else {
 			return true;
@@ -77,51 +85,76 @@ public class XtextSemanticModelTransformer extends DefaultSemanticModelTransform
 				@Override
 				public ContentOutlineNode caseGrammar(Grammar object) {
 					// create fully qualified ID string
-					Object[] idElements = object.getIdElements().toArray();
-					int size = idElements.length;
-					StringBuffer stringBuffer = new StringBuffer();
-					for (int i = 0; i < size;) {
-						stringBuffer.append(String.valueOf(idElements[i]));
-						if (++i < size) {
-							stringBuffer.append(".");
-						}
-					}
-					outlineNode.setLabel("language " + stringBuffer.toString());
+					
+//					Object[] idElements = object.getIdElements().toArray();
+//					int size = idElements.length;
+//					StringBuffer stringBuffer = new StringBuffer();
+//					for (int i = 0; i < size;) {
+//						stringBuffer.append(String.valueOf(idElements[i]));
+//						if (++i < size) {
+//							stringBuffer.append(".");
+//						}
+//					}
+					outlineNode.setLabel("language " + GrammarUtil.getLanguageId(object));
 					outlineNode.setImageDescriptor(Activator.getImageDescriptor("icons/language.gif"));
 					return outlineNode;
 				}
 
 				@Override
 				public ContentOutlineNode caseGeneratedMetamodel(org.eclipse.xtext.GeneratedMetamodel object) {
-					outlineNode.setLabel("generate " + object.getName());
+					outlineNode.setLabel("generate " + object.getName() + (!Strings.isEmpty(object.getAlias())? " as " + object.getAlias() :""));
 					outlineNode.setImageDescriptor(Activator.getImageDescriptor("icons/export.gif"));
 					return outlineNode;
-				};
+				}
 
 				@Override
 				public ContentOutlineNode caseReferencedMetamodel(org.eclipse.xtext.ReferencedMetamodel object) {
 					outlineNode.setLabel("import " + object.getAlias());
 					outlineNode.setImageDescriptor(Activator.getImageDescriptor("icons/import.gif"));
 					return outlineNode;
-				};
+				}
 
 				@Override
-				public ContentOutlineNode caseParserRule(org.eclipse.xtext.ParserRule object) {
+				public ContentOutlineNode caseAbstractRule(org.eclipse.xtext.AbstractRule object) {
 					outlineNode.setLabel(object.getName());
 					outlineNode.setImageDescriptor(Activator.getImageDescriptor("icons/rule.gif"));
 					return outlineNode;
-				};
+				}
+				
+				public ContentOutlineNode caseRuleCall(RuleCall ruleCall) {
+					if (ruleCall.getRule() != null)
+						outlineNode.setLabel("call " + ruleCall.getRule().getName());
+					else {
+						NodeAdapter nodeAdapter = NodeUtil.getNodeAdapter(ruleCall);
+						String ruleName = "???";
+						if (nodeAdapter != null) {
+							List<LeafNode> leafs = nodeAdapter.getParserNode().getLeafNodes();
+							for (LeafNode leaf: leafs) {
+								if (!leaf.isHidden()) {
+									ruleName = leaf.getText();
+									break;
+								}
+							}
+						}
+						outlineNode.setLabel("unresolved call " + ruleName);
+					}
+					// TODO show another icon, if unresolved
+					// TODO how to show text in another color? 
+					outlineNode.setImageDescriptor(Activator.getImageDescriptor("icons/rule.gif"));
+					return outlineNode;
+				}
 
 				@Override
 				public ContentOutlineNode caseAlternatives(org.eclipse.xtext.Alternatives object) {
 					outlineNode.setLabel("|");
 					return outlineNode;
-				};
+				}
 
 				@Override
 				public ContentOutlineNode defaultCase(EObject object) {
+					outlineNode.setLabel(object.eClass().getName());
 					return outlineNode;
-				};
+				}
 
 			}.doSwitch(semanticNode);
 		} else {
