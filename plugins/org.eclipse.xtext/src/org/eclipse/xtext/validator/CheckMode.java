afc12df1402194815127fcfaee3a8ee29b110068
sefftinge <sefftinge>
2009-02-10 16:20:39 +0000

    convenient, Java-based check API added. EValidator as primary hook remains.

---
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/validator/CheckMode.java
@@ -0,0 +1,9 @@
+package org.eclipse.xtext.validator;
+
+public interface CheckMode {
+	public final static String KEY = "check.mode";
+	
+	public final static int ALL = 1;
+	public final static int FAST = 2;
+	
+}
---
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/validator/AbstractDeclarativeValidatorTest.java
@@ -0,0 +1,112 @@
+/*******************************************************************************
+ * Copyright (c) 2008 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.validator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.eclipse.emf.common.util.DiagnosticChain;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.EcorePackage;
+
+/**
+ * @author Sven Efftinge - Initial contribution and API
+ *
+ */
+public class AbstractDeclarativeValidatorTest extends TestCase {
+	
+	public void testSimpleDispatch() throws Exception {
+		
+		AbstractDeclarativeValidator test = new TestValidator();
+		TestChain chain = chain();
+		test.validate(EcorePackage.eINSTANCE.getEClass(), chain, null);
+		assertMatch(chain, 1, 3);
+	}
+	
+	public void testDeeperHierarchyWithOverwrittenJavaMethods() throws Exception {
+		AbstractDeclarativeValidator test = new TestValidator() {
+			@Check
+			public void foo(Object x) {
+				error("fooObject",5);
+			}
+		};
+		TestChain chain = chain();
+		test.validate(EcorePackage.eINSTANCE.getEClass(), chain, null);
+		assertMatch(chain, 1, 5);
+	}
+	
+	public void testSkipExpensive() throws Exception {
+		AbstractDeclarativeValidator test = new TestValidator() {
+			@Check(CheckType.EXPENSIVE)
+			public void bar(Object x) {
+				error("fooObject",27);
+			}
+		};
+		TestChain chain = chain();
+		test.validate(EcorePackage.eINSTANCE.getEClass(), chain, Collections.singletonMap((Object)CheckMode.KEY, (Object)CheckMode.FAST));
+		assertMatch(chain, 1, 3);
+		
+		chain = chain();
+		test.validate(EcorePackage.eINSTANCE.getEClass(), chain, Collections.singletonMap((Object)CheckMode.KEY, (Object)CheckMode.ALL));
+		assertMatch(chain, 1, 3, 27);
+		
+		chain = chain();
+		test.validate(EcorePackage.eINSTANCE.getEClass(), chain, null);
+		assertMatch(chain, 1, 3, 27);
+	}
+
+	private void assertMatch(TestChain chain, Integer ... expectedFeatureIds) {
+		assertEquals(expectedFeatureIds.length, chain.integers.size());
+		List<Integer> asList = Arrays.asList(expectedFeatureIds);
+		assertTrue("expected : "+asList+" , but was "+chain.integers,chain.integers.containsAll(asList));
+	}
+
+
+	private TestChain chain() {
+		return new TestChain();
+	}
+	
+	@SuppressWarnings("unused")
+	private class TestValidator extends AbstractDeclarativeValidator {
+		@Check
+		private void foo(EClass x) {
+			error("fooString",1);
+		}
+
+		@Check
+		protected void foo(EStructuralFeature x) {
+			error("fooInteger",2);
+		}
+
+		@Check
+		public void foo(Object x) {
+			error("fooObject",3);
+		}
+	}
+
+	private final class TestChain implements DiagnosticChain {
+		public List<Integer> integers = new ArrayList<Integer>();
+
+		public void add(org.eclipse.emf.common.util.Diagnostic diagnostic) {
+			integers.add((Integer) diagnostic.getData().get(1));
+		}
+
+		public void addAll(org.eclipse.emf.common.util.Diagnostic diagnostic) {
+			throw new UnsupportedOperationException();
+		}
+
+		public void merge(org.eclipse.emf.common.util.Diagnostic diagnostic) {
+			throw new UnsupportedOperationException();
+		}
+	}
+}
