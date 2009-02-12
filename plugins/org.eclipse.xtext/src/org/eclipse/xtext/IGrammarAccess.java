ea7707a36f3d3026faa1797ea465dcb655feb48b
szarnekow <szarnekow>
2009-02-12 13:47:49 +0000

    GrammarAccess has accessors for inherited rules

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/IGrammarAccess.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/IGrammarAccess.java
@@ -1,19 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2009 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package org.eclipse.xtext;
 
 import org.eclipse.xtext.service.ILanguageService;
 
+/**
+ * TODO Javadoc
+ */
 public interface IGrammarAccess extends ILanguageService {
 
 	public interface IAbstractRuleAccess {
 		public AbstractRule getRule();
 	}
-	
+
 	public interface IParserRuleAccess extends IAbstractRuleAccess {
 		public ParserRule getRule();
 	}
 
 	public Grammar getGrammar();
-
-	public IGrammarAccess getSuperGrammar();
 
 }
