576c99966e595f227507b6a2ffc771b04fa4b33a
jkohnlein <jkohnlein>
2009-02-17 09:42:13 +0000

    organized imports

--- tests/org.eclipse.xtext.ui.integration.tests/src/org/eclipse/xtext/ui/integration/TestLanguageUIModule.java
+++ tests/org.eclipse.xtext.ui.integration.tests/src/org/eclipse/xtext/ui/integration/TestLanguageUIModule.java
@@ -3,7 +3,6 @@ Generated with Xtext
 */
 package org.eclipse.xtext.ui.integration;
 
-import com.google.inject.Binder;
 
 /**
  * used to manually extend and modify configuration for components used within the IDE.
