9fdd2528bec5b9a1332e070b0b45671c0a93ef5a
pfriese <pfriese>
2009-02-20 14:49:09 +0000

    RESOLVED - bug 265455: [Outline] Allow implementation of filter actions https://bugs.eclipse.org/bugs/show_bug.cgi?id=265455

---
+++ plugins/org.eclipse.xtext.xtext.ui/src/org/eclipse/xtext/xtext/ui/editor/outline/ParserRulesOutlineFilter.java
@@ -0,0 +1,9 @@
+package org.eclipse.xtext.xtext.ui.editor.outline;
+
+import org.eclipse.xtext.ui.common.editor.outline.IOutlineFilter;
+
+/**
+ * @author Peter Friese - Initial contribution and API
+ */
+public class ParserRulesOutlineFilter implements IOutlineFilter {
+}
