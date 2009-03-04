10aa21618ca0e919020880db400a44bccc5580d8
pschonbac <pschonbac>
2009-03-04 15:59:41 +0000

    Dummy activator added

---
+++ plugins/org.eclipse.xtext.activities/src/org/eclipse/xtext/activities/Activator.java
@@ -0,0 +1,23 @@
+/*
+ * <copyright>
+ *
+ * Copyright (c) 2005-2006 Sven Efftinge (http://www.efftinge.de) and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Sven Efftinge (http://www.efftinge.de) - Initial API and implementation
+ *
+ * </copyright>
+ */
+ 
+package org.eclipse.xtext.activities;
+
+import org.eclipse.core.runtime.Plugin;
+
+
+public class Activator extends Plugin {
+
+}
