1c7b25f469490954f744b83738374de8856b87d0
pschonbac <pschonbac>
2009-03-04 16:11:42 +0000

    Dummy activator added

--- plugins/org.eclipse.xtext.activities/src/org/eclipse/xtext/activities/Activator.java
+++ plugins/org.eclipse.xtext.activities/src/org/eclipse/xtext/activities/Activator.java
@@ -1,22 +1,17 @@
-/*
- * <copyright>
- *
- * Copyright (c) 2005-2006 Sven Efftinge (http://www.efftinge.de) and others.
- * All rights reserved.   This program and the accompanying materials
+/*******************************************************************************
+ * Copyright (c) 2005, 2007 committers of openArchitectureWare and others.
+ * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Sven Efftinge (http://www.efftinge.de) - Initial API and implementation
- *
- * </copyright>
- */
- 
+ *     committers of openArchitectureWare - initial API and implementation
+ *******************************************************************************/
+
 package org.eclipse.xtext.activities;
 
 import org.eclipse.core.runtime.Plugin;
-
 
 public class Activator extends Plugin {
 
