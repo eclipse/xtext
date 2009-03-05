53151264ef2fa28371675805ab570695070a2d5f
szarnekow <szarnekow>
2009-03-05 13:56:34 +0000

    Fix: Don't execute checks multiple times per validation Warning if someone will stumble accross https://bugs.eclipse.org/bugs/show_bug.cgi?id=266978

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/validator/CheckMode.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/validator/CheckMode.java
@@ -1,9 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2008 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package org.eclipse.xtext.validator;
 
 public interface CheckMode {
-	public final static String KEY = "check.mode";
-	
+	public final static String KEY = "org.eclipse.xtext.validator.CheckMode";
+
 	public final static int ALL = 1;
 	public final static int FAST = 2;
-	
+
 }
