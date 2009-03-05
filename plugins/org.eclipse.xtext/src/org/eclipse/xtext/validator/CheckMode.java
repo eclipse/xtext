5f8b9b032a0a5a1acc491db5bf1b20cf03eaf748
sefftinge <sefftinge>
2009-03-05 15:23:46 +0000

    https://bugs.eclipse.org/bugs/show_bug.cgi?id=263703

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/validator/CheckMode.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/validator/CheckMode.java
@@ -8,9 +8,38 @@
 package org.eclipse.xtext.validator;
 
 public interface CheckMode {
-	public final static String KEY = "org.eclipse.xtext.validator.CheckMode";
-
-	public final static int ALL = 1;
-	public final static int FAST = 2;
-
+	public final static String KEY = "check.mode";
+	
+	public final static CheckMode FAST_ONLY = new CheckMode(){
+		public boolean shouldCheck(CheckType type) {
+			return type == CheckType.FAST;
+		}
+	};
+	
+	public final static CheckMode NORMAL_ONLY = new CheckMode(){
+		public boolean shouldCheck(CheckType type) {
+			return type == CheckType.NORMAL;
+		}
+	};
+	
+	public final static CheckMode EXPENSIVE_ONLY = new CheckMode(){
+		public boolean shouldCheck(CheckType type) {
+			return type == CheckType.EXPENSIVE;
+		}
+	};
+	
+	public final static CheckMode NORMAL_AND_FAST = new CheckMode(){
+		public boolean shouldCheck(CheckType type) {
+			return type == CheckType.NORMAL || type == CheckType.FAST;
+		}
+	};
+	
+	public final static CheckMode ALL = new CheckMode(){
+		public boolean shouldCheck(CheckType type) {
+			return true;
+		}
+	};
+	
+	boolean shouldCheck(CheckType type);
+	
 }
