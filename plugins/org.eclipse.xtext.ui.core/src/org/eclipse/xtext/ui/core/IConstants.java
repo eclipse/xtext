febe9bc6dab98d42272492d54905652743134a9d
sefftinge <sefftinge>
2009-03-08 19:13:50 +0000

    - wizard clean up  - first attempt to use index in scoping

---
+++ plugins/org.eclipse.xtext.ui.core/src/org/eclipse/xtext/ui/core/IConstants.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2008 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+package org.eclipse.xtext.ui.core;
+
+/**
+ * @author Sven Efftinge - Initial contribution and API
+ *
+ */
+public interface IConstants {
+	
+	public final static String CROSS_REF_MARKER_ID = "org.eclipse.xtext.ui.core.danglingCrossReference";
+}
