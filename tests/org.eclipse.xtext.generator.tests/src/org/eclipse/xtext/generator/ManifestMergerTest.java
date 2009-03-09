042b5ad02a0de1bd85b82e9f9def3288c5d089c9
sefftinge <sefftinge>
2009-03-09 19:55:12 +0000

    - manifests ore only written when there are changes, fixed broken tests

--- tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/generator/ManifestMergerTest.java
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/generator/ManifestMergerTest.java
@@ -29,10 +29,22 @@ public class ManifestMergerTest extends TestCase {
 		String before = (String) attrs.get(MergeableManifest.REQUIRE_BUNDLE);
 		manifest.addRequiredBundles(Collections.singleton("foo.bar.baz"));
 		String after = (String) attrs.get(MergeableManifest.REQUIRE_BUNDLE);
-		assertEquals(before + ",foo.bar.baz", after);
+		assertEquals(before + ",foo.bar.baz", after.replaceAll("\\s",""));
 	}
 
 	public void testMergeExportedPackages() throws Exception {
+		String packageName = getClass().getPackage().getName().replace('.', '/');
+		InputStream resourceAsStream = getClass().getResourceAsStream("/" + packageName + "/Test_Manifest.MF");
+		MergeableManifest manifest = new MergeableManifest(resourceAsStream);
+
+		manifest.addExportedPackages(Collections.singleton("org.eclipse.xtext"));
+		assertFalse(manifest.isModified());
+		
+		manifest.addRequiredBundles(Collections.singleton("org.eclipse.xtext.log4j"));
+		assertFalse(manifest.isModified());
+	}
+	
+	public void testNoChanges() throws Exception {
 		String packageName = getClass().getPackage().getName().replace('.', '/');
 		InputStream resourceAsStream = getClass().getResourceAsStream("/" + packageName + "/Test_Manifest.MF");
 		MergeableManifest manifest = new MergeableManifest(resourceAsStream);
@@ -40,7 +52,7 @@ public class ManifestMergerTest extends TestCase {
 		String before = (String) attrs.get(MergeableManifest.EXPORT_PACKAGE);
 		manifest.addExportedPackages(Collections.singleton("foo.bar.baz"));
 		String after = (String) attrs.get(MergeableManifest.EXPORT_PACKAGE);
-		assertEquals(before + ",foo.bar.baz", after);
+		assertEquals(before + ",foo.bar.baz", after.replaceAll("\\s",""));
 	}
 
 	public void testMergeIntoCommaSeparatedList() throws Exception {
@@ -49,11 +61,19 @@ public class ManifestMergerTest extends TestCase {
 		toMerge.add("bar");
 		toMerge.add("baz");
 
-		assertEquals("bar,foo,baz", MergeableManifest.mergeIntoCommaSeparatedList("bar", toMerge));
-		assertEquals("bar,x,foo,baz", MergeableManifest.mergeIntoCommaSeparatedList("bar ,    x", toMerge));
-		assertEquals("baz,bar,foo", MergeableManifest.mergeIntoCommaSeparatedList("baz,bar,foo", toMerge));
-		assertEquals("foo,bar,baz", MergeableManifest.mergeIntoCommaSeparatedList("", toMerge));
-		assertEquals("foo,bar,baz", MergeableManifest.mergeIntoCommaSeparatedList(null, toMerge));
+		assertEquals("bar,foo,baz", removeWS(MergeableManifest.mergeIntoCommaSeparatedList("bar", toMerge)));
+		assertEquals("bar,x,foo,baz", removeWS(MergeableManifest.mergeIntoCommaSeparatedList("bar ,    x", toMerge)));
+		assertEquals("baz,bar,foo", removeWS(MergeableManifest.mergeIntoCommaSeparatedList("baz,bar,foo", toMerge)));
+		assertEquals("foo,bar,baz", removeWS(MergeableManifest.mergeIntoCommaSeparatedList("", toMerge)));
+		assertEquals("foo,bar,baz", removeWS(MergeableManifest.mergeIntoCommaSeparatedList(null, toMerge)));
+	}
+
+	/**
+	 * @param mergeIntoCommaSeparatedList
+	 * @return
+	 */
+	private String removeWS(String s) {
+		return s==null?null:s.replaceAll("\\s", "");
 	}
 
 	public void testMergeIntoCommaSeparatedListWithParams() throws Exception {
@@ -62,10 +82,10 @@ public class ManifestMergerTest extends TestCase {
 		toMerge.add("bar");
 		toMerge.add("baz");
 
-		assertEquals("bar;version=\"0.7.0\",foo,baz", MergeableManifest.mergeIntoCommaSeparatedList(
-				"bar;version=\"0.7.0\"", toMerge));
-		assertEquals("bar;special=foo,x,foo,baz", MergeableManifest.mergeIntoCommaSeparatedList(
-				"bar;special=foo ,    x", toMerge));
+		assertEquals("bar;version=\"0.7.0\",foo,baz", removeWS(MergeableManifest.mergeIntoCommaSeparatedList(
+				"bar;version=\"0.7.0\"", toMerge)));
+		assertEquals("bar;special=foo,x,foo,baz", removeWS(MergeableManifest.mergeIntoCommaSeparatedList(
+				"bar;special=foo ,    x", toMerge)));
 	}
 
 	public void testMergeIntoCommaSeparatedListInvalidParam() throws Exception {
