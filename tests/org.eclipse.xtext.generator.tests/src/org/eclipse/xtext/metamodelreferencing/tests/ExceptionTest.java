f1c676b35d4e2c501fe54061854f93fbd0d78388
szarnekow <szarnekow>
2009-03-10 11:16:13 +0000

    Fix: Another infinite loop in XtextDamagerRepairer Refactoring: AdaptingIterable existed already as MappingIterator - Collections.map(..) Fix: Exception when importing a generated metamodel Feature: error markers when referencing a metamodel twice

---
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/metamodelreferencing/tests/ExceptionTest.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2009 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.metamodelreferencing.tests;
+
+import org.eclipse.xtext.XtextStandaloneSetup;
+import org.eclipse.xtext.junit.AbstractXtextTests;
+import org.eclipse.xtext.resource.XtextResource;
+
+/**
+ * @author Sebastian Zarnekow - Initial contribution and API
+ */
+public class ExceptionTest extends AbstractXtextTests {
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		with(XtextStandaloneSetup.class);
+	}
+
+	public void testExceptionOnRegenerate() throws Exception {
+		String model = "grammar org.eclipse.xtext.ExceptionTest with org.eclipse.xtext.common.Terminals\n" +
+			"generate existing 'http://www.eclipse.org/xtext/tests/grammarinheritance'\n" +
+			"import 'classpath:/org/eclipse/xtext/grammarinheritance/ametamodel.ecore' as existing\n" +
+			"AType : name=ID;";
+		XtextResource resource = getResourceFromString(model);
+		assertTrue(resource.getErrors().toString(), resource.getErrors().isEmpty());
+	}
+
+}
