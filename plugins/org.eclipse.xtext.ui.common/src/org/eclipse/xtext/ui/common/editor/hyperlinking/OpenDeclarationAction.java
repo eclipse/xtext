a580428f3816b2603069236cde8bd3dac5e301a6
szarnekow <szarnekow>
2009-03-12 20:10:06 +0000

    Feature: Navigate into resources from jars, navigate inside these resources, too

--- plugins/org.eclipse.xtext.ui.common/src/org/eclipse/xtext/ui/common/editor/hyperlinking/OpenDeclarationAction.java
+++ plugins/org.eclipse.xtext.ui.common/src/org/eclipse/xtext/ui/common/editor/hyperlinking/OpenDeclarationAction.java
@@ -1,14 +1,21 @@
 package org.eclipse.xtext.ui.common.editor.hyperlinking;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
 import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.text.Region;
 import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
@@ -16,7 +23,10 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.IDE;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.core.ILocationInFileProvider;
+import org.eclipse.xtext.ui.core.editor.ReadonlyArchiveStorage;
+import org.eclipse.xtext.ui.core.editor.ReadonlyFileStorage;
 import org.eclipse.xtext.ui.core.editor.XtextEditor;
+import org.eclipse.xtext.ui.core.editor.XtextReadonlyEditorInput;
 import org.eclipse.xtext.ui.core.editor.model.UnitOfWork;
 
 /**
@@ -29,7 +39,7 @@ import org.eclipse.xtext.ui.core.editor.model.UnitOfWork;
 public class OpenDeclarationAction extends Action {
 
 	// logger available to subclasses
-	protected final Logger logger = Logger.getLogger(getClass());
+	private static final Logger logger = Logger.getLogger(OpenDeclarationAction.class);
 
 	private final URI uri;
 
@@ -53,36 +63,81 @@ public class OpenDeclarationAction extends Action {
 	public void doOpen(final URI uri) {
 		IFile file = getContainingResourceSetFile(uri);
 		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+		IEditorPart openEditor = null;
 		try {
-			IEditorPart openEditor = IDE.openEditor(page, file);
-			if (openEditor instanceof XtextEditor) {
-				final XtextEditor edit = (XtextEditor) openEditor;
-				if (uri.fragment()!=null) {
-				edit.getDocument().readOnly(new UnitOfWork<Object>(){
-
-					public Object exec(XtextResource resource) throws Exception {
-						EObject object = resource.getEObject(uri.fragment());
-						Region region = locationProvider.getLocation(object);
-						edit.selectAndReveal(region.getOffset(),region.getLength());
-						return null;
-					}});
-				}
-			} else if (openEditor instanceof ISelectionProvider) {
-				//TODO: use ISelectionProvider instead of ITextEditor
+			if (file != null) {
+				openEditor = IDE.openEditor(page, file);
+			} else if (uri.isArchive()) {
+				// TODO don't fall back to java.io
+				IEditorInput input = new XtextReadonlyEditorInput(new ReadonlyArchiveStorage(uri));
+				openEditor = IDE.openEditor(page, input, PlatformUI.getWorkbench().getEditorRegistry()
+						.getDefaultEditor(uri.lastSegment()).getId());
+			} else {
+				// fall back: URI is bundle resource uri and has to converted
+				URL url = FileLocator.toFileURL(new URL(uri.scheme()+ ":" +uri.devicePath()));
+				URI urlAsUri = URI.createURI(url.toString());
+				String path = urlAsUri.toFileString();
+				File ioFile = new File(path);
+				// TODO don't fall back to java.io
+				IEditorInput input = new XtextReadonlyEditorInput(new ReadonlyFileStorage(ioFile, uri));
+				openEditor = IDE.openEditor(page, input, PlatformUI.getWorkbench().getEditorRegistry()
+						.getDefaultEditor(uri.lastSegment()).getId());
 			}
-		}
-		catch (PartInitException partInitException) {
+		} catch (PartInitException partInitException) {
 			logger.error("Error while opening editor part for EMF URI '" + uri + "'",
 					partInitException);
+		} catch (IOException e) {
+			logger.error("Error while opening editor part for EMF URI '" + uri + "'", e);
+		}
+		if (openEditor != null && openEditor instanceof XtextEditor) {
+			final XtextEditor edit = (XtextEditor) openEditor;
+			if (uri.fragment()!=null) {
+				edit.getDocument().readOnly(new UnitOfWork<Object>(){
+				public Object exec(XtextResource resource) throws Exception {
+					EObject object = resource.getEObject(uri.fragment());
+					Region region = locationProvider.getLocation(object);
+					edit.selectAndReveal(region.getOffset(),region.getLength());
+					return null;
+				}});
+			}
+		} else if (openEditor instanceof ISelectionProvider) {
+			//TODO: use ISelectionProvider instead of ITextEditor
 		}
 	}
 
 	private IFile getContainingResourceSetFile(URI uri) {
-		IFile targetFile = uri.isPlatformResource() ?
-				ResourcesPlugin.getWorkspace().getRoot().getFile(
-						new Path(uri.toPlatformString(true)))
-				: ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(
-						new Path(uri.toFileString()));
-		return targetFile;
+		String path = null;
+		if (uri.isPlatformResource()) {
+			path = uri.toPlatformString(true);
+		} else if (uri.isPlatformPlugin()) {
+			path = uri.toPlatformString(true);
+		} else if (uri.isFile()) {
+			path = uri.toFileString();
+		} else if (uri.isArchive()) {
+			URI archiveUri = URI.createURI(uri.authority());
+			String archive = null;
+			if (archiveUri.isFile()) {
+				archive = archiveUri.toFileString();
+			} else if (archiveUri.isPlatformResource()) {
+				archive = archiveUri.toPlatformString(true);
+			} else {
+				archive = archiveUri.toString();
+			}
+			path = uri.scheme() + ":" + archive + uri.path();
+		} else {
+			path = uri.toString();
+		}
+		IFile result = null;
+		if (uri.isPlatformResource()) {
+			result = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(path));
+		} else {
+			result = ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(new Path(path));
+			if (result == null) {
+				IResource res = ResourcesPlugin.getWorkspace().getRoot().findMember(path);
+				if (res != null && (res instanceof IFile))
+					result = (IFile) res;
+			}
+		}
+		return result;
 	}
 }
