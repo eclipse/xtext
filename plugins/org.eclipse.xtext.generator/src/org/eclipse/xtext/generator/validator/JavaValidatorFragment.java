32eb5bf17f8fb8dd7415c868b10be2a40597537a
sefftinge <sefftinge>
2009-03-13 14:59:16 +0000

    java-based validator (to be implemented)

--- plugins/org.eclipse.xtext.generator/src/org/eclipse/xtext/generator/validator/JavaValidatorFragment.java
+++ plugins/org.eclipse.xtext.generator/src/org/eclipse/xtext/generator/validator/JavaValidatorFragment.java
@@ -3,4 +3,5 @@ package org.eclipse.xtext.generator.validator;
 import org.eclipse.xtext.generator.AbstractGeneratorFragment;
 
 public class JavaValidatorFragment extends AbstractGeneratorFragment {
+	
 }
