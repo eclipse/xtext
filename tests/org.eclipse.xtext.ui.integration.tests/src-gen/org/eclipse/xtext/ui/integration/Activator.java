41724c9737e672ba2650419624b1bce1d2d27a69
szarnekow <szarnekow>
2009-03-26 20:06:58 +0000

    Regenerated all languages, fixed some tests

--- tests/org.eclipse.xtext.ui.integration.tests/src-gen/org/eclipse/xtext/ui/integration/Activator.java
+++ tests/org.eclipse.xtext.ui.integration.tests/src-gen/org/eclipse/xtext/ui/integration/Activator.java
@@ -4,6 +4,8 @@
  */
 package org.eclipse.xtext.ui.integration;
 
+import org.eclipse.xtext.ui.common.service.UIPluginModule;
+
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -30,12 +32,20 @@ public class Activator extends AbstractUIPlugin {
 		super.start(context);
 		INSTANCE = this;
 		
-		injectors.put("org.eclipse.xtext.ui.integration.TestLanguage",Guice.createInjector(new org.eclipse.xtext.ui.integration.TestLanguageRuntimeModule(), new org.eclipse.xtext.ui.integration.TestLanguageUiModule()));
+		injectors.put("org.eclipse.xtext.ui.integration.TestLanguage", Guice.createInjector(
+			new org.eclipse.xtext.ui.integration.TestLanguageRuntimeModule(), 
+			new org.eclipse.xtext.ui.integration.TestLanguageUiModule(),
+			createUIPluginModule()
+		));
 		
 	}
 	
 	public static Activator getInstance() {
 		return INSTANCE;
+	}
+	
+	protected UIPluginModule createUIPluginModule() {
+		return new UIPluginModule(this);
 	}
 	
 }
