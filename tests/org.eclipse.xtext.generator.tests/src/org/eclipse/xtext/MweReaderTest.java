ee75b7a5b7405b7c1f1080c30693b4b3e20a2a7e
hbehrens <hbehrens>
2009-04-01 14:11:19 +0000

    * fix: MweReader now propagates errors from all resources of the resourceSet  * feature: Message text of mwe issues  displays resource URI and line number

--- tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/MweReaderTest.java
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/MweReaderTest.java
@@ -8,14 +8,15 @@
  *******************************************************************************/
 package org.eclipse.xtext;
 
+import junit.framework.TestCase;
+
 import org.eclipse.emf.mwe.core.WorkflowContextDefaultImpl;
 import org.eclipse.emf.mwe.core.issues.IssuesImpl;
 import org.eclipse.emf.mwe.core.monitor.NullProgressMonitor;
 
-import junit.framework.TestCase;
-
 /**
  * @author Sven Efftinge - Initial contribution and API
+ * @author Heiko Behrens
  *
  */
 public class MweReaderTest extends TestCase {
@@ -49,5 +50,24 @@ public class MweReaderTest extends TestCase {
 		IssuesImpl issues = new IssuesImpl();
 		reader.checkConfiguration(issues);
 		assertTrue(issues.hasErrors());
+	}
+	
+	public void testIssuesInOtherResource() throws Exception {
+		MweReader reader = new MweReader();
+		reader.setRegister(new XtextStandaloneSetup());
+		final String errorURI = "classpath:/"+(getClass().getName()+"ResourceWithError").replace('.', '/')+".xtext";
+		final String loadingURI = "classpath:/"+(getClass().getName()+"LoadingResourceWithError").replace('.', '/')+".xtext";
+		reader.setUri(loadingURI);
+		WorkflowContextDefaultImpl ctx = new WorkflowContextDefaultImpl();
+		IssuesImpl issues = new IssuesImpl();
+		reader.checkConfiguration(issues);
+		assertFalse(issues.hasErrors());
+		reader.invoke(ctx, new NullProgressMonitor(), issues);
+		assertTrue(ctx.get(MweReader.DEFAULT_OUTPUT_SLOT) instanceof Grammar);
+
+		assertEquals(1, issues.getErrors().length);
+		final String errorString = issues.getErrors()[0].toString();
+		assertFalse(errorString.startsWith("[ERROR]: " + loadingURI));
+		assertTrue(errorString.startsWith("[ERROR]: " + errorURI));
 	}
 }
