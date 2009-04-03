c17aedf67bcfb516620da3b3783ff33b905a5810
szarnekow <szarnekow>
2009-04-03 12:10:57 +0000

    Make AbstractDeclarativeScopeProvider more configurable

--- tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/crossrefs/DeclarativeScopeProviderTest.java
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/crossrefs/DeclarativeScopeProviderTest.java
@@ -10,13 +10,20 @@ package org.eclipse.xtext.crossrefs;
 
 import java.util.Collections;
 
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.xtext.crossref.IScope;
 import org.eclipse.xtext.crossref.IScopedElement;
 import org.eclipse.xtext.crossref.impl.AbstractDeclarativeScopeProvider;
+import org.eclipse.xtext.crossref.impl.DefaultScopeProvider;
 import org.eclipse.xtext.crossref.impl.SimpleScope;
 
 import junit.framework.TestCase;
@@ -81,5 +88,33 @@ public class DeclarativeScopeProviderTest extends TestCase {
 		EReference details = EcorePackage.eINSTANCE.getEClass_ESuperTypes();
 		assertEquals(b,provider.getScope(details, details));
 		assertEquals(c,provider.getScope(details.getEContainingClass(), details));
+	}
+	
+	@SuppressWarnings("unused")
+	public void testScopeByType() throws Exception {
+		final IScope a =  new SimpleScope(IScope.NULLSCOPE, Collections.<IScopedElement>emptySet());
+		final IScope b =  new SimpleScope(IScope.NULLSCOPE, Collections.<IScopedElement>emptySet());
+		final IScope c =  new SimpleScope(IScope.NULLSCOPE, Collections.<IScopedElement>emptySet());
+		AbstractDeclarativeScopeProvider provider = new AbstractDeclarativeScopeProvider() {
+			private IScope scope_EClass(EClassifier clazz, EReference ref) {
+				return a;
+			}
+			
+			private IScope scope_EClass(EClass clazz, EReference ref) {
+				return c;
+			}
+			
+			private IScope scope_EClass(EReference ctx, EReference ref) {
+				return b;
+			}
+		};
+		DefaultScopeProvider defaultScopeProvider = new DefaultScopeProvider();
+		provider.setGenericFallBack(defaultScopeProvider);
+		ResourceSet resourceSet = new ResourceSetImpl();
+		Resource res = new XMIResourceFactoryImpl().createResource(URI.createURI("test.uri"));
+		resourceSet.getResources().add(res);
+		EReference details = (EReference) EcoreUtil.copy(EcorePackage.eINSTANCE.getEClass_ESuperTypes());
+		res.getContents().add(details);
+		assertNotNull(provider.getScope(details, details.eClass()));
 	}
 }
