346840d51212880bcd3abd376fa5d903a92cbb5c
szarnekow <szarnekow>
2009-04-24 10:40:01 +0000

    Feature: Provide sample implementation for CamelCase Completion

--- plugins/org.eclipse.xtext.ui.core/src/org/eclipse/xtext/ui/core/editor/contentassist/PrefixMatcher.java
+++ plugins/org.eclipse.xtext.ui.core/src/org/eclipse/xtext/ui/core/editor/contentassist/PrefixMatcher.java
@@ -7,13 +7,34 @@
  *******************************************************************************/
 package org.eclipse.xtext.ui.core.editor.contentassist;
 
+import org.eclipse.ui.dialogs.SearchPattern;
+
+import com.google.inject.ImplementedBy;
+
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
  */
-public class PrefixMatcher {
+@ImplementedBy(PrefixMatcher.Simple.class)
+public abstract class PrefixMatcher {
 
-	public boolean isCandidateMatchingPrefix(String name, String prefix) {
-		return name.regionMatches(true, 0, prefix, 0,prefix.length());
+	public abstract boolean isCandidateMatchingPrefix(String name, String prefix);
+
+	public static class Simple extends PrefixMatcher {
+		@Override
+		public boolean isCandidateMatchingPrefix(String name, String prefix) {
+			return name.regionMatches(true, 0, prefix, 0, prefix.length());
+		}
 	}
-	
+
+	public static class CamelCase extends PrefixMatcher {
+		@Override
+		public boolean isCandidateMatchingPrefix(String name, String prefix) {
+			SearchPattern pattern = new SearchPattern(
+					SearchPattern.RULE_BLANK_MATCH | SearchPattern.RULE_CAMELCASE_MATCH | SearchPattern.RULE_PREFIX_MATCH);
+			pattern.setPattern(prefix);
+			boolean result = pattern.matches(name);
+			return result;
+		}
+	}
+
 }
