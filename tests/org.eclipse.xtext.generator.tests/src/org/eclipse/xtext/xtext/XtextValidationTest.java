6f29fb080cc3bfa4641f6da4fc7d59f9b5c55998
hbehrens <hbehrens>
2009-04-28 10:21:18 +0000

    * fix: Check that refuses enum with explicit empty literals ("") also works with imported enums

--- tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/xtext/XtextValidationTest.java
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/xtext/XtextValidationTest.java
@@ -106,7 +106,7 @@ public class XtextValidationTest extends AbstractGeneratorTest {
 		assertEquals("diag.isWarning", diag.getSeverity(), Diagnostic.WARNING);
 	}
 	
-	public void testEnumWithEmptyLiteral() throws Exception {
+	public void testEnumWithEmptyLiteralGenerated() throws Exception {
 		XtextResource resource = getResourceFromString(
 				"grammar org.foo.Bar with org.eclipse.xtext.common.Terminals\n" +
 				"generate testLanguage 'http://www.eclipse.org/2009/tmf/xtext/validation/literal/2'\n" +
@@ -121,4 +121,17 @@ public class XtextValidationTest extends AbstractGeneratorTest {
 		assertEquals("diag.isError", diag.getSeverity(), Diagnostic.ERROR);
 	}
 
+	public void testEnumWithEmptyLiteralImported() throws Exception {
+		XtextResource resource = getResourceFromString(
+				"grammar org.foo.Bar with org.eclipse.xtext.common.Terminals\n" +
+				"import 'classpath:/org/eclipse/xtext/enumrules/enums.ecore'\n" +
+				"enum ExistingEnum: SameName | DifferentName='Diff' | OverriddenLiteral='';");
+		assertTrue(resource.getErrors().toString(), resource.getErrors().isEmpty());
+		assertTrue(resource.getWarnings().toString(), resource.getWarnings().isEmpty());
+
+		Diagnostic diag = Diagnostician.INSTANCE.validate(resource.getContents().get(0));
+		assertNotNull("diag", diag);
+		assertEquals(diag.getChildren().toString(), 1, diag.getChildren().size());
+		assertEquals("diag.isError", diag.getSeverity(), Diagnostic.ERROR);
+	}
 }
