a38b60f9017dc60566484e4a9add6c59c742e26c
hbehrens <hbehrens>
2009-04-28 10:21:18 +0000

    * fix: Check that refuses enum with explicit empty literals ("") also works with imported enums

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/xtext/XtextValidator.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/xtext/XtextValidator.java
@@ -338,8 +338,6 @@ public class XtextValidator extends AbstractDeclarativeValidator {
 	
 	@Check
 	public void checkEnumLiteralIsValid(EnumLiteralDeclaration decl) {
-		EnumRule rule = GrammarUtil.containingEnumRule(decl);
-		guard(rule.getType().getMetamodel() instanceof GeneratedMetamodel);
 		if("".equals(decl.getLiteral().getValue()))
 			error("Enum literal must not be an empty string.", XtextPackage.ENUM_LITERAL_DECLARATION__LITERAL);
 	}
