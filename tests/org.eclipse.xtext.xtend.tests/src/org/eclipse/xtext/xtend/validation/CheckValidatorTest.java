6349c216830d609491a02b4f4d7faf66a08e4228
szarnekow <szarnekow>
2009-05-05 17:47:54 +0000

    Fixed failing tests

--- tests/org.eclipse.xtext.xtend.tests/src/org/eclipse/xtext/xtend/validation/CheckValidatorTest.java
+++ tests/org.eclipse.xtext.xtend.tests/src/org/eclipse/xtext/xtend/validation/CheckValidatorTest.java
@@ -13,14 +13,53 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.Diagnostician;
 import org.eclipse.xtext.junit.AbstractXtextTests;
 import org.eclipse.xtext.testlanguages.TreeTestLanguageStandaloneSetup;
+import org.eclipse.xtext.testlanguages.validation.TreeTestLanguageCheckValidator;
+
+import com.google.inject.Binder;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 
 /**
  * @author Sven Efftinge - Initial contribution and API
  */
 public class CheckValidatorTest extends AbstractXtextTests {
 
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		with(new TreeTestLanguageStandaloneSetup() {
+			@Override
+			public Injector createInjector() {
+				return Guice.createInjector(new org.eclipse.xtext.testlanguages.TreeTestLanguageRuntimeModule() {
+					@Override
+					public void configure(Binder binder) {
+						super.configure(binder);
+						binder.bind(TreeTestLanguageCheckValidator.class).asEagerSingleton();
+					}
+					
+					@Override
+					public Class<? extends org.eclipse.xtend.expression.ExecutionContext> bindExecutionContext() {
+						return org.eclipse.xtext.xtend.InjectableExecutionContext.class;
+					}
+
+					
+					@Override
+					public Class<? extends org.eclipse.xtend.expression.ResourceManager> bindResourceManager() {
+						return org.eclipse.xtext.xtend.InjectableResourceManager.class;
+					}
+
+					
+					@Override
+					public java.lang.ClassLoader bindClassLoader() {
+						return getClass().getClassLoader();
+					}
+				});
+			}
+		});
+	}
+	
 	public void testErrorValidation() throws Exception {
-		with(new TreeTestLanguageStandaloneSetup());
 		EObject model = getModel("foo ('A') { a('A'){}; b('A'){}; c('A'){}; d('A'){}; e('A'){}; };");
 		Diagnostic diagnostic = Diagnostician.INSTANCE.validate(model);
 		assertEquals(diagnostic.toString(), 1, diagnostic.getChildren().size());
@@ -30,7 +69,6 @@ public class CheckValidatorTest extends AbstractXtextTests {
 	}
 
 	public void testWarningValidation() throws Exception {
-		with(new TreeTestLanguageStandaloneSetup());
 		EObject model = getModel("foo ('A') { a('A'){}; b('A'){}; c('A'){}; };");
 		Diagnostic diagnostic = Diagnostician.INSTANCE.validate(model);
 		assertEquals(diagnostic.toString(), 1, diagnostic.getChildren().size());
