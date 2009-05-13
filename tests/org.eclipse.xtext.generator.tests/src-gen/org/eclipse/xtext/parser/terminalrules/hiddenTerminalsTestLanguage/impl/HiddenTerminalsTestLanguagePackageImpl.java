6626eafd855a78ccdd8a3894e17075366ff04ae4
szarnekow <szarnekow>
2009-05-13 13:06:28 +0000

    Deactivated GeneratorTestLanguage as it most likely breaks the Xext build on emft.eclipse.org

--- tests/org.eclipse.xtext.generator.tests/src-gen/org/eclipse/xtext/parser/terminalrules/hiddenTerminalsTestLanguage/impl/HiddenTerminalsTestLanguagePackageImpl.java
+++ tests/org.eclipse.xtext.generator.tests/src-gen/org/eclipse/xtext/parser/terminalrules/hiddenTerminalsTestLanguage/impl/HiddenTerminalsTestLanguagePackageImpl.java
@@ -123,20 +123,10 @@ public class HiddenTerminalsTestLanguagePackageImpl extends EPackageImpl impleme
   private static boolean isInited = false;
 
   /**
-   * Creates, registers, and initializes the <b>Package</b> for this
-   * model, and for any others upon which it depends.  Simple
-   * dependencies are satisfied by calling this method on all
-   * dependent packages before doing anything else.  This method drives
-   * initialization for interdependent packages directly, in parallel
-   * with this package, itself.
-   * <p>Of this package and its interdependencies, all packages which
-   * have not yet been registered by their URI values are first created
-   * and registered.  The packages are then initialized in two steps:
-   * meta-model objects for all of the packages are created before any
-   * are initialized, since one package's meta-model objects may refer to
-   * those of another.
-   * <p>Invocation of this method will not affect any packages that have
-   * already been initialized.
+   * Creates, registers, and initializes the <b>Package</b> for this model, and for any others upon which it depends.
+   * 
+   * <p>This method is used to initialize {@link HiddenTerminalsTestLanguagePackage#eINSTANCE} when that field is accessed.
+   * Clients should not invoke it directly. Instead, they should simply access that field to obtain the package.
    * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
    * @see #eNS_URI
@@ -162,6 +152,9 @@ public class HiddenTerminalsTestLanguagePackageImpl extends EPackageImpl impleme
     // Mark meta-data to indicate it can't be changed
     theHiddenTerminalsTestLanguagePackage.freeze();
 
+  
+    // Update the registry and return the package
+    EPackage.Registry.INSTANCE.put(HiddenTerminalsTestLanguagePackage.eNS_URI, theHiddenTerminalsTestLanguagePackage);
     return theHiddenTerminalsTestLanguagePackage;
   }
 
