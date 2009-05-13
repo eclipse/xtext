262d655788af6558f23fecd713f7ee0c223a57d9
jkohnlein <jkohnlein>
2009-05-13 15:18:59 +0000

    Cosmetical changes

--- plugins/org.eclipse.emf.index/src/org/eclipse/emf/index/ResourceDescriptor.java
+++ plugins/org.eclipse.emf.index/src/org/eclipse/emf/index/ResourceDescriptor.java
@@ -7,7 +7,6 @@
  *******************************************************************************/
 package org.eclipse.emf.index;
 
-import java.io.Serializable;
 import java.util.Date;
 import java.util.Map;
 
