55b5905a55be7ed1c86ee7a0cecdcd41d9becef9
szarnekow <szarnekow>
2009-05-13 13:06:28 +0000

    Deactivated GeneratorTestLanguage as it most likely breaks the Xext build on emft.eclipse.org

--- tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/generator/GeneratorTestLanguageRuntimeModule.java
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/generator/GeneratorTestLanguageRuntimeModule.java
@@ -6,6 +6,6 @@ package org.eclipse.xtext.generator;
 /**
  * Use this class to register components to be used within the IDE.
  */
-public class GeneratorTestLanguageRuntimeModule extends org.eclipse.xtext.generator.AbstractGeneratorTestLanguageRuntimeModule {
+public class GeneratorTestLanguageRuntimeModule { //extends org.eclipse.xtext.generator.AbstractGeneratorTestLanguageRuntimeModule {
 
 }
