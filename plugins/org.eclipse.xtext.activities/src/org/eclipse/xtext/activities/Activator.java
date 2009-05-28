44569ffb418a12f3a567366537322dc9f29323ef
pschonbac <pschonbac>
2009-05-28 13:42:27 +0000

    Copyright notice in license headers updated.

--- plugins/org.eclipse.xtext.activities/src/org/eclipse/xtext/activities/Activator.java
+++ plugins/org.eclipse.xtext.activities/src/org/eclipse/xtext/activities/Activator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2007 committers of openArchitectureWare and others.
+ * Copyright (c) 2005, 2009 committers of openArchitectureWare and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
