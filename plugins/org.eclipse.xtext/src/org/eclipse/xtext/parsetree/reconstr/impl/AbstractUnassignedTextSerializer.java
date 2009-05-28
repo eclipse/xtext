f5991cc90c987f95b56546384ff742a0977167dc
sefftinge <sefftinge>
2009-05-28 07:38:59 +0000

    made AbstractUnassigned... abstract, moved exception throwing up to DefautImpl

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/parsetree/reconstr/impl/AbstractUnassignedTextSerializer.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/parsetree/reconstr/impl/AbstractUnassignedTextSerializer.java
@@ -1,20 +1,8 @@
 package org.eclipse.xtext.parsetree.reconstr.impl;
 
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.xtext.GrammarUtil;
-import org.eclipse.xtext.RuleCall;
 import org.eclipse.xtext.parsetree.reconstr.IUnassignedTextSerializer;
 
-public class AbstractUnassignedTextSerializer implements
+public abstract class AbstractUnassignedTextSerializer implements
 		IUnassignedTextSerializer {
-
-	public String serializeUnassignedRuleCall(RuleCall ruleCall, EObject current) {
-		throw new RuntimeException(
-				"Could not determine the value for the unassigned rulecall of rule "
-						+ ruleCall.getRule().getName()
-						+ " from within rule "
-						+ GrammarUtil.containingRule(ruleCall).getName()
-						+ ". You might want to implement IUnassignedTextSerializer or modify your implementation to handle this rulecall.");
-	}
 
 }
