f84252401c37a182e62bf59ecbad4914a3b3863a
pfriese <pfriese>
2009-05-30 06:04:55 +0000

    [275490] [Outline] Outline tree should contain EObjects rather than ContentOutlineNode elements

---
+++ plugins/org.eclipse.xtext.generator/src/org/eclipse/xtext/ui/generator/outline/OutlineNodeAdapterFactoryFragment.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2009 itemis AG (http://www.itemis.com) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+package org.eclipse.xtext.ui.generator.outline;
+
+import java.util.Set;
+
+import org.eclipse.xtext.Grammar;
+import org.eclipse.xtext.GrammarUtil;
+import org.eclipse.xtext.generator.AbstractGeneratorFragment;
+import org.eclipse.xtext.generator.BindFactory;
+import org.eclipse.xtext.generator.Binding;
+
+/**
+ * @author Peter Friese - Initial contribution and API
+ */
+public class OutlineNodeAdapterFactoryFragment extends AbstractGeneratorFragment {
+	
+	@Override
+	public Set<Binding> getGuiceBindingsUi(Grammar grammar) {
+		return new BindFactory()
+			.addTypeToType("org.eclipse.xtext.ui.common.editor.outline.IContentOutlineNodeAdapterFactory",
+					getQualifiedName(grammar))
+			.getBindings();
+	}
+
+	public static String getQualifiedName(Grammar grammar) {
+		return GrammarUtil.getNamespace(grammar) + ".outline." + GrammarUtil.getName(grammar) + "OutlineNodeAdapterFactory";
+	}
+	
+	@Override
+	public String[] getRequiredBundlesUi(Grammar grammar) {
+		return new String[]{
+				"org.eclipse.xtext.ui.common",
+				"org.eclipse.xtext.ui.core"
+		};
+	}
+
+}
