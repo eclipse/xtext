0655794db300a8bbe1e711a8db2bf6c640125636
szarnekow <szarnekow>
2009-06-05 07:49:23 +0000

    Fix: IOOBE: Error tokens let the editor stuck

--- plugins/org.eclipse.xtext.ui.common/src/org/eclipse/xtext/ui/common/editor/syntaxcoloring/DefaultLexicalHighlightingConfiguration.java
+++ plugins/org.eclipse.xtext.ui.common/src/org/eclipse/xtext/ui/common/editor/syntaxcoloring/DefaultLexicalHighlightingConfiguration.java
@@ -23,6 +23,7 @@ public class DefaultLexicalHighlightingConfiguration implements ILexicalHighligh
 	public static final String STRING_ID = "string";
 	public static final String NUMBER_ID = "number";
 	public static final String DEFAULT_ID = "default";
+	public static final String INVALID_TOKEN_ID = "error";
 
 	public void configure(IHighlightingConfigurationAcceptor acceptor) {
 		acceptor.acceptDefaultHighlighting(KEYWORD_ID, "Keyword", keywordTextStyle());
@@ -31,12 +32,19 @@ public class DefaultLexicalHighlightingConfiguration implements ILexicalHighligh
 		acceptor.acceptDefaultHighlighting(STRING_ID, "String", stringTextStyle());
 		acceptor.acceptDefaultHighlighting(NUMBER_ID, "Number", numberTextStyle());
 		acceptor.acceptDefaultHighlighting(DEFAULT_ID, "Default", defaultTextStyle());
+		acceptor.acceptDefaultHighlighting(INVALID_TOKEN_ID, "Invalid Symbol", errorTextStyle());
 	}
 	
 	public TextStyle defaultTextStyle() {
 		TextStyle textStyle = new TextStyle();
 		textStyle.setBackgroundColor(new RGB(255, 255, 255));
 		textStyle.setColor(new RGB(0, 0, 0));
+		return textStyle;
+	}
+	
+	public TextStyle errorTextStyle() {
+		TextStyle textStyle = defaultTextStyle().copy();
+//		textStyle.setColor(new RGB(255, 0, 0));
 		return textStyle;
 	}
 	
