d9cedc5a215c660ae06ada3e0a7c33efffd74591
szarnekow <szarnekow>
2009-06-05 08:13:41 +0000

    Highlight type references italic by default

--- plugins/org.eclipse.xtext.xtext.ui/src/org/eclipse/xtext/xtext/ui/editor/syntaxcoloring/SemanticHighlightingConfiguration.java
+++ plugins/org.eclipse.xtext.xtext.ui/src/org/eclipse/xtext/xtext/ui/editor/syntaxcoloring/SemanticHighlightingConfiguration.java
@@ -7,6 +7,7 @@
  *******************************************************************************/
 package org.eclipse.xtext.xtext.ui.editor.syntaxcoloring;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.xtext.ui.common.editor.syntaxcoloring.IHighlightingConfigurationAcceptor;
 import org.eclipse.xtext.ui.common.editor.syntaxcoloring.ISemanticHighlightingConfiguration;
@@ -22,13 +23,19 @@ public class SemanticHighlightingConfiguration implements ISemanticHighlightingC
 	
 	public void configure(IHighlightingConfigurationAcceptor acceptor) {
 		acceptor.acceptDefaultHighlighting(RULE_DECLARATION_ID, "Rule Declaration", defaultTextStyle());
-		acceptor.acceptDefaultHighlighting(TYPE_REFERENCE_ID, "Type Reference", defaultTextStyle());
+		acceptor.acceptDefaultHighlighting(TYPE_REFERENCE_ID, "Type Reference", typeReference());
 	}
 
 	public TextStyle defaultTextStyle() {
 		TextStyle textStyle = new TextStyle();
 		textStyle.setBackgroundColor(new RGB(255, 255, 255));
 		textStyle.setColor(new RGB(0, 0, 0));
+		return textStyle;
+	}
+	
+	public TextStyle typeReference() {
+		TextStyle textStyle = defaultTextStyle().copy();
+		textStyle.setStyle(SWT.ITALIC);
 		return textStyle;
 	}
 	
