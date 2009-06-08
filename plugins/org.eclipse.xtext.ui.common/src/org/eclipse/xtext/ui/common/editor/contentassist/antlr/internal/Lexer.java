41541224d56eba78875cf1637d6c8632df51bd3e
szarnekow <szarnekow>
2009-06-08 08:29:55 +0000

    Antlr: Don't write error messages to System.err, use logger instead

--- plugins/org.eclipse.xtext.ui.common/src/org/eclipse/xtext/ui/common/editor/contentassist/antlr/internal/Lexer.java
+++ plugins/org.eclipse.xtext.ui.common/src/org/eclipse/xtext/ui/common/editor/contentassist/antlr/internal/Lexer.java
@@ -8,18 +8,29 @@
 package org.eclipse.xtext.ui.common.editor.contentassist.antlr.internal;
 
 import org.antlr.runtime.CharStream;
+import org.apache.log4j.Logger;
 
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
  */
 public abstract class Lexer extends org.antlr.runtime.Lexer {
 
+	private static final Logger logger = Logger.getLogger(Lexer.class);
+	
 	protected Lexer(CharStream input) {
 		super(input);
 	}
 
 	protected Lexer() {
 		super();
+	}
+	
+	@Override
+	public void emitErrorMessage(String msg) {
+		// don't call super, since it would do a plain vanilla
+		// System.err.println(msg);
+		if (logger.isTraceEnabled())
+			logger.trace(msg);
 	}
 	
 }
