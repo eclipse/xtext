338cbbbde2cc404081c0f1c39831216d49687dcd
pfriese <pfriese>
2009-06-11 23:12:33 +0000

    Made outline toolbar actions injectable.

--- plugins/org.eclipse.xtext.xtext.ui/src/org/eclipse/xtext/xtext/ui/editor/outline/XtextActionBarContributor.java
+++ plugins/org.eclipse.xtext.xtext.ui/src/org/eclipse/xtext/xtext/ui/editor/outline/XtextActionBarContributor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 itemis AG (http://www.itemis.eu) and others.
+ * Copyright (c) 2009 itemis AG (http://www.itemis.com) and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,21 +8,17 @@
  *******************************************************************************/
 package org.eclipse.xtext.xtext.ui.editor.outline;
 
-import org.eclipse.jface.action.IToolBarManager;
-import org.eclipse.ui.IActionBars;
+import org.eclipse.jface.action.Action;
 import org.eclipse.xtext.ui.common.editor.outline.XtextContentOutlinePage;
+import org.eclipse.xtext.ui.common.editor.outline.actions.DeclarativeActionBarContributor;
 
 /**
  * @author Peter Friese - Initial contribution and API
  */
-public class FilteringXtextOutlinePage extends XtextContentOutlinePage {
-
-	@Override
-	protected void registerToolbarActions(IActionBars actionBars) {
-		super.registerToolbarActions(actionBars);
-		IToolBarManager toolBarManager = actionBars.getToolBarManager();
-		if (toolBarManager != null) {
-			toolBarManager.add(new FilterParserRulesAction(this));
-		}
+public class XtextActionBarContributor extends DeclarativeActionBarContributor {
+	
+	public Action addFilterParserRulesToolbarAction(XtextContentOutlinePage page) {
+		return new FilterParserRulesAction(page);
 	}
+
 }
