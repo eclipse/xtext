9208f72c8c62964b7fa552022b92ea2cf26e527f
jkohnlein <jkohnlein>
2009-06-19 14:53:00 +0000

    Improved documentation for the generator

--- tests/org.eclipse.xtext.ui.common.tests/src/org/eclipse/xtext/ui/common/SimpleLabelProviderFragment.java
+++ tests/org.eclipse.xtext.ui.common.tests/src/org/eclipse/xtext/ui/common/SimpleLabelProviderFragment.java
@@ -14,37 +14,40 @@ import org.eclipse.xtext.Grammar;
 import org.eclipse.xtext.generator.AbstractGeneratorFragment;
 import org.eclipse.xtext.generator.BindFactory;
 import org.eclipse.xtext.generator.Binding;
+import org.eclipse.xtext.generator.IGeneratorFragment;
+import org.eclipse.xtext.ui.core.SimpleLabelProvider;
 
 /**
+ * An {@link IGeneratorFragment} to generate the binding of a {@link SimpleLabelProvider} for an Xtext language.
+ * 
  * @author Sebastian Zarnekow - Initial contribution and API
  */
 public class SimpleLabelProviderFragment extends AbstractGeneratorFragment {
 
 	@Override
 	public Set<Binding> getGuiceBindingsUi(Grammar grammar) {
-		return new BindFactory().addTypeToType(
-				"org.eclipse.jface.viewers.ILabelProvider",
+		return new BindFactory().addTypeToType("org.eclipse.jface.viewers.ILabelProvider",
 				"org.eclipse.xtext.ui.core.SimpleLabelProvider").getBindings();
 	}
-	
+
 	@Override
 	public void generate(Grammar grammar, XpandExecutionContext ctx) {
 		// noop
 	}
-	
+
 	@Override
 	public void addToStandaloneSetup(Grammar grammar, XpandExecutionContext ctx) {
 		// noop
 	}
-	
+
 	@Override
 	public void addToPluginXmlRt(Grammar grammar, XpandExecutionContext ctx) {
 		// noop
 	}
-	
+
 	@Override
 	public void addToPluginXmlUi(Grammar grammar, XpandExecutionContext ctx) {
 		// noop
 	}
-	
+
 }
