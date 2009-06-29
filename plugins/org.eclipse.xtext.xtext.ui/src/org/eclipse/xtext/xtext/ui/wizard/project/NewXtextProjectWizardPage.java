20368a16a6227db1bdd93d640a3ed3a6226a12f9
pschonbac <pschonbac>
2009-06-29 17:25:47 +0000

    bug 263763: [Wizard] Warn if input does not conform common conventions https://bugs.eclipse.org/bugs/show_bug.cgi?id=263763

--- plugins/org.eclipse.xtext.xtext.ui/src/org/eclipse/xtext/xtext/ui/wizard/project/NewXtextProjectWizardPage.java
+++ plugins/org.eclipse.xtext.xtext.ui/src/org/eclipse/xtext/xtext/ui/wizard/project/NewXtextProjectWizardPage.java
@@ -14,7 +14,11 @@ import java.net.URISyntaxException;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.jdt.core.JavaConventions;
+import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jface.dialogs.IDialogPage;
+import org.eclipse.jface.dialogs.IMessageProvider;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
@@ -47,28 +51,28 @@ public class NewXtextProjectWizardPage extends WizardPage {
 
 	private Button generateGenProject;
 
-	private XtextProjectInfo xtextProjectInfo = new XtextProjectInfo();
+	private final XtextProjectInfo xtextProjectInfo = new XtextProjectInfo();
 
-//	private ISelection selection;
+	// private ISelection selection;
 
 	/**
 	 * Constructor for NewXtextProjectWizardPage.
 	 * 
 	 * @param pageName
 	 */
-	public NewXtextProjectWizardPage(ISelection selection) {
+	public NewXtextProjectWizardPage(final ISelection selection) {
 		super("wizardPage"); //$NON-NLS-1$
 		setTitle(Messages.NewXtextProjectWizardPage_Title);
 		setDescription(Messages.NewXtextProjectWizardPage_Description);
-//		this.selection = selection;
+		// this.selection = selection;
 	}
 
 	/**
 	 * @see IDialogPage#createControl(Composite)
 	 */
-	public void createControl(Composite parent) {
-		Composite container = new Composite(parent, SWT.NULL);
-		GridLayout layout = new GridLayout();
+	public void createControl(final Composite parent) {
+		final Composite container = new Composite(parent, SWT.NULL);
+		final GridLayout layout = new GridLayout();
 		container.setLayout(layout);
 		layout.numColumns = 2;
 		layout.verticalSpacing = 9;
@@ -80,7 +84,7 @@ public class NewXtextProjectWizardPage extends WizardPage {
 		gd = new GridData(GridData.FILL_HORIZONTAL);
 		projectText.setLayoutData(gd);
 		projectText.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
+			public void modifyText(final ModifyEvent e) {
 				dialogChanged();
 			}
 		});
@@ -92,7 +96,7 @@ public class NewXtextProjectWizardPage extends WizardPage {
 		gd = new GridData(GridData.FILL_HORIZONTAL);
 		languageNameText.setLayoutData(gd);
 		languageNameText.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
+			public void modifyText(final ModifyEvent e) {
 				dialogChanged();
 			}
 		});
@@ -104,7 +108,7 @@ public class NewXtextProjectWizardPage extends WizardPage {
 		gd = new GridData(GridData.FILL_HORIZONTAL);
 		fileExtensionText.setLayoutData(gd);
 		fileExtensionText.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
+			public void modifyText(final ModifyEvent e) {
 				dialogChanged();
 			}
 		});
@@ -116,15 +120,15 @@ public class NewXtextProjectWizardPage extends WizardPage {
 		gd = new GridData(GridData.FILL_HORIZONTAL);
 		generateGenProject.setLayoutData(gd);
 		generateGenProject.addSelectionListener(new SelectionListener() {
-			public void widgetDefaultSelected(SelectionEvent e) {
+			public void widgetDefaultSelected(final SelectionEvent e) {
 				dialogChanged();
 			}
 
-			public void widgetSelected(SelectionEvent e) {
+			public void widgetSelected(final SelectionEvent e) {
 				dialogChanged();
 			}
 		});
-		
+
 		initializeDefaultValues();
 		dialogChanged();
 		setControl(container);
@@ -147,49 +151,68 @@ public class NewXtextProjectWizardPage extends WizardPage {
 		xtextProjectInfo.setFileExtension(fileExtensionText.getText());
 		xtextProjectInfo.setCreateGeneratorProject(generateGenProject.getSelection());
 
-		if (!xtextProjectInfo.getProjectName().matches("\\w+[\\w\\.]*\\w+")) { //$NON-NLS-1$
-			setErrorMessage(Messages.NewXtextProjectWizardPage_ProjectNameValidationError);
-			setPageComplete(false);
+		errorMessage(null);
+		warningMessage(null);
+		setPageComplete(true);
+
+		if (!validatePackageName(xtextProjectInfo.getProjectName())) { //$NON-NLS-1$
+			errorMessage(Messages.NewXtextProjectWizardPage_ProjectNameValidationError);
 			return;
 		}
 		try {
 			xtextProjectInfo.setEncoding(ResourcesPlugin.getWorkspace().getRoot().getDefaultCharset());
-		} catch (CoreException e1) {
+		}
+		catch (final CoreException e1) {
 			xtextProjectInfo.setEncoding(System.getProperty("file.encoding"));
 		}
 		// check whether project already exists
 		final IProject handle = ResourcesPlugin.getWorkspace().getRoot().getProject(xtextProjectInfo.getProjectName());
 		if (handle.exists()) {
-			setErrorMessage(Messages.NewXtextProjectWizardPage_DuplicateProjectName);
-			setPageComplete(false);
+			errorMessage(Messages.NewXtextProjectWizardPage_DuplicateProjectName);
 			return;
 		}
-		if (!xtextProjectInfo.getLanguageName().matches("\\w+[\\w\\.]*\\w+")) { //$NON-NLS-1$
-			setErrorMessage(Messages.NewXtextProjectWizardPage_LanguageNameMandatory);
-			setPageComplete(false);
+
+		final String projectName = xtextProjectInfo.getProjectName();
+		final String projectPrefix = projectName.substring(0, projectName.lastIndexOf("."));
+		final String languagePackage = xtextProjectInfo.getBasePackage();
+		final String languageName = xtextProjectInfo.getLanguageName();
+
+		if (languageName.length() > 0 && !validatePackageName(languagePackage)) {
+			errorMessage("The language name must start with a valid Java package name");
 			return;
 		}
+
+		if (!validateLanguageName(languageName)) {
+			if (languageName.length() == 0) {
+				errorMessage(Messages.NewXtextProjectWizardPage_LanguageNameMandatory);
+			}
+			else {
+				errorMessage("The last segment of the language name must start with an uppercase letter");
+			}
+			return;
+		}
+
+		if (!projectPrefix.equals(languagePackage)) {
+			warningMessage("The project name for this language should start with '" + languagePackage + "'");
+			return;
+		}
+
 		try {
 			new URI(xtextProjectInfo.getNsURI());
 		}
-		catch (URISyntaxException e) {
-			setErrorMessage(Messages.NewXtextProjectWizardPage_InvalidURI);
+		catch (final URISyntaxException e) {
+			errorMessage(Messages.NewXtextProjectWizardPage_InvalidURI);
 			setPageComplete(false);
 			return;
 		}
-		setErrorMessage(null);
-		setPageComplete(true);
 	}
 
-	private void initializeDefaultValues() {
-		String dslName = findNextValidDSLName("org.xtext.example", "MyDsl"); //$NON-NLS-1$ //$NON-NLS-2$
-		projectText.setText("org.xtext.example." + dslName.toLowerCase()); //$NON-NLS-1$
-		languageNameText.setText("org.xtext.example." + dslName); //$NON-NLS-1$
-		fileExtensionText.setText(dslName.toLowerCase());
-		generateGenProject.setSelection(true);
+	private void errorMessage(final String msg) {
+		setErrorMessage(msg);
+		setPageComplete((msg != null) ? false : true);
 	}
 
-	private String findNextValidDSLName(String prefix, String name) {
+	private String findNextValidDSLName(final String prefix, final String name) {
 		String candidate = name;
 		int suffix = 1;
 		while (ResourcesPlugin.getWorkspace().getRoot().getProject((prefix + "." + candidate).toLowerCase()).exists()) { //$NON-NLS-1$
@@ -197,5 +220,36 @@ public class NewXtextProjectWizardPage extends WizardPage {
 			suffix++;
 		}
 		return candidate;
+	}
+
+	private void initializeDefaultValues() {
+		final String dslName = findNextValidDSLName("org.xtext.example", "MyDsl"); //$NON-NLS-1$ //$NON-NLS-2$
+		projectText.setText("org.xtext.example." + dslName.toLowerCase()); //$NON-NLS-1$
+		languageNameText.setText("org.xtext.example." + dslName); //$NON-NLS-1$
+		fileExtensionText.setText(dslName.toLowerCase());
+		generateGenProject.setSelection(true);
+	}
+
+	private boolean validateLanguageName(final String languageName) {
+		if (languageName == null)
+			return false;
+
+		final IStatus status = JavaConventions.validateJavaTypeName(languageName, JavaCore.VERSION_1_3,
+				JavaCore.VERSION_1_3);
+		return status.isOK();
+	}
+
+	private boolean validatePackageName(final String projectName) {
+		if (projectName == null)
+			return false;
+
+		final IStatus status = JavaConventions.validatePackageName(projectName, JavaCore.VERSION_1_3,
+				JavaCore.VERSION_1_3);
+		return status.isOK();
+	}
+
+	private void warningMessage(final String msg) {
+		setMessage(msg, IMessageProvider.WARNING);
+		setPageComplete(true);
 	}
 }
