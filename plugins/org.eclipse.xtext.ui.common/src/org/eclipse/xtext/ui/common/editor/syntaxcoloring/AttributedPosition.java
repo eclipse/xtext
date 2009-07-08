5af08afc005c0e476c790425894e2f2a755c328f
szarnekow <szarnekow>
2009-07-08 13:00:47 +0000

    Fix: Highlighter ignores configured font - https://bugs.eclipse.org/bugs/show_bug.cgi?id=282180

--- plugins/org.eclipse.xtext.ui.common/src/org/eclipse/xtext/ui/common/editor/syntaxcoloring/AttributedPosition.java
+++ plugins/org.eclipse.xtext.ui.common/src/org/eclipse/xtext/ui/common/editor/syntaxcoloring/AttributedPosition.java
@@ -46,6 +46,7 @@ public class AttributedPosition extends Position {
 		StyleRange styleRange= new StyleRange(getOffset(), len, textAttribute.getForeground(), textAttribute.getBackground(), fontStyle);
 		styleRange.strikeout= (style & TextAttribute.STRIKETHROUGH) != 0;
 		styleRange.underline= (style & TextAttribute.UNDERLINE) != 0;
+		styleRange.font = textAttribute.getFont();
 
 		return styleRange;
 	}
