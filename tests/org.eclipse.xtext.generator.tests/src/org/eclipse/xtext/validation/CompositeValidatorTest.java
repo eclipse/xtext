34b69bd6b40e6fbd8d01aae74cd3ee806eb4b7f4
szarnekow <szarnekow>
2009-07-28 15:28:52 +0000

    Fix: Possible CME in CompositeEValidator.validate - see https://bugs.eclipse.org/bugs/show_bug.cgi?id=279962

--- tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/validation/CompositeValidatorTest.java
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/validation/CompositeValidatorTest.java
@@ -12,13 +12,16 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.emf.common.util.BasicDiagnostic;
 import org.eclipse.emf.common.util.DiagnosticChain;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EValidator;
 import org.eclipse.xtext.enumrules.EnumRulesTestLanguageStandaloneSetup;
+import org.eclipse.xtext.enumrules.enumRulesTestLanguage.EnumRulesTestLanguageFactory;
 import org.eclipse.xtext.enumrules.enumRulesTestLanguage.EnumRulesTestLanguagePackage;
+import org.eclipse.xtext.enumrules.enumRulesTestLanguage.Model;
 import org.eclipse.xtext.enumrules.validation.EnumRulesTestLanguageCheckValidator;
 import org.eclipse.xtext.junit.AbstractXtextTests;
 import org.eclipse.xtext.validation.CompositeEValidator.EValidatorEqualitySupport;
@@ -112,6 +115,82 @@ public abstract class CompositeValidatorTest extends AbstractXtextTests {
 		assertTrue(contents.contains(equalitySupport));
 		equalitySupport.setDelegate(second);
 		assertTrue(contents.contains(equalitySupport));
+	}
+	
+	public void testBug_279962() {
+		EValidator validator = registry.getEValidator(pack);
+		assertTrue(validator instanceof CompositeEValidator);
+		CompositeEValidator composite = (CompositeEValidator) validator;
+		
+		int prevSize = composite.getContents().size();
+		
+		get(Val_279962_01.class);
+		get(Val_279962_04.class);
+		assertEquals(prevSize + 2, composite.getContents().size());
+		assertNotNull(validator);
+		Model model = EnumRulesTestLanguageFactory.eINSTANCE.createModel();
+		// do not expect an exception
+		validator.validate(model, new BasicDiagnostic(), null);
+		assertEquals(prevSize + 4, composite.getContents().size());
+	}
+	
+	@ComposedChecks(validators = { Val_279962_02.class })
+	public static class Val_279962_01 extends AbstractDeclarativeValidator {
+		private final EnumRulesTestLanguagePackage pack;
+		
+		@Inject
+		public Val_279962_01(EnumRulesTestLanguagePackage pack) {
+			this.pack = pack;
+		}
+		
+		@Override
+		protected List<? extends EPackage> getEPackages() {
+			return Collections.singletonList(pack);
+		}
+	}
+	
+	@ComposedChecks(validators = { Val_279962_03.class })
+	public static class Val_279962_02 extends AbstractDeclarativeValidator {
+		private final EnumRulesTestLanguagePackage pack;
+		
+		@Inject
+		public Val_279962_02(EnumRulesTestLanguagePackage pack) {
+			this.pack = pack;
+		}
+		
+		@Override
+		protected List<? extends EPackage> getEPackages() {
+			return Collections.singletonList(pack);
+		}
+	}
+	
+	public static class Val_279962_03 extends AbstractDeclarativeValidator {
+		private final EnumRulesTestLanguagePackage pack;
+		
+		@Inject
+		public Val_279962_03(EnumRulesTestLanguagePackage pack) {
+			this.pack = pack;
+		}
+		
+		@Override
+		protected List<? extends EPackage> getEPackages() {
+			return Collections.singletonList(pack);
+		}
+	}
+	
+	@ComposedChecks(validators = { Val_279962_03.class })
+	public static class Val_279962_04 extends AbstractDeclarativeValidator {
+		private final EnumRulesTestLanguagePackage pack;
+		
+		@Inject
+		public Val_279962_04(EnumRulesTestLanguagePackage pack) {
+			this.pack = pack;
+		}
+		
+		@Override
+		protected List<? extends EPackage> getEPackages() {
+			return Collections.singletonList(pack);
+		}
 	}
 	
 	public abstract void testContentsSize();
