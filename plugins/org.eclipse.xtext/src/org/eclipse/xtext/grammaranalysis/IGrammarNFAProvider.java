b62368c5678e3e2703bef64a5546bf607d187ba1
meysholdt <meysholdt>
2009-07-30 18:56:12 +0000

    added missing copyright headers

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/grammaranalysis/IGrammarNFAProvider.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/grammaranalysis/IGrammarNFAProvider.java
@@ -1,7 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2009 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
 package org.eclipse.xtext.grammaranalysis;
 
 import org.eclipse.xtext.AbstractElement;
 
+/**
+ * @author Moritz Eysholdt - Initial contribution and API
+ */
 public interface IGrammarNFAProvider<S, T> {
 
 	public interface NFABuilder<S, T> {
--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/grammaranalysis/impl/AbstractNFATransition.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/grammaranalysis/impl/AbstractNFATransition.java
@@ -1,11 +1,19 @@
-/**
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2009 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
 package org.eclipse.xtext.grammaranalysis.impl;
 
 import org.eclipse.xtext.grammaranalysis.INFAState;
 import org.eclipse.xtext.grammaranalysis.INFATransition;
 
+/**
+ * @author Moritz Eysholdt - Initial contribution and API
+ */
 public class AbstractNFATransition<S,T> implements INFATransition<S,T> {
 
 	protected boolean ruleCall;
