6fa36c4d6153dd552e043af69d80d9d06598da1a
szarnekow <szarnekow>
2009-07-31 09:29:08 +0000

    Minor change: Bound type parameters to their required interfaces

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/parsetree/reconstr/impl/TreeConstState.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/parsetree/reconstr/impl/TreeConstState.java
@@ -1,4 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2009 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
 package org.eclipse.xtext.parsetree.reconstr.impl;
+
+import java.util.List;
 
 import org.eclipse.xtext.AbstractElement;
 import org.eclipse.xtext.grammaranalysis.IGrammarNFAProvider.NFABuilder;
@@ -11,4 +21,14 @@ public class TreeConstState extends AbstractNFAState<TreeConstState, TreeConstTr
 		super(element, builder);
 	}
 
+	@Override
+	public List<TreeConstTransition> getFollowers() {
+		return super.getFollowers();
+	}
+	
+	@Override
+	public List<TreeConstTransition> getParentFollowers() {
+		return super.getParentFollowers();
+	}
+	
 }
