5c58676b160fd5256d5e6224497cde7b5d1d2269
szarnekow <szarnekow>
2009-08-10 13:10:24 +0000

    Fix: Allow custom values in the context-map of the validator - see https://bugs.eclipse.org/bugs/show_bug.cgi?id=286114

--- plugins/org.eclipse.xtext.ui.core/src/org/eclipse/xtext/ui/core/editor/utils/ValidationJob.java
+++ plugins/org.eclipse.xtext.ui.core/src/org/eclipse/xtext/ui/core/editor/utils/ValidationJob.java
@@ -28,17 +28,19 @@ import org.eclipse.xtext.validation.CheckMode;
 /**
  * @author Dennis Hübner - Initial contribution and API
  */
-public final class ValidationJob extends Job {
+public class ValidationJob extends Job {
+
 	private static final Logger log = Logger.getLogger(ValidationJob.class);
 
 	private final CheckMode checkMode;
+	
 	private final IFile iFile;
 
 	private final boolean deleteOldMarkers;
 
-	private IXtextResourceChecker xtextResourceChecker;
+	private final IXtextResourceChecker xtextResourceChecker;
 
-	private IStateAccess<XtextResource> xtextDocument;
+	private final IStateAccess<XtextResource> xtextDocument;
 	
 	/**
 	 * Constructs a ValidationJob with a specified {@link CheckMode}
@@ -59,24 +61,52 @@ public final class ValidationJob extends Job {
 		if (monitor.isCanceled())
 			return Status.CANCEL_STATUS;
 		log.debug("Starting Xtext Validation with CheckMode: " + checkMode);
-		if (iFile == null) { // file may be null, if it was opend from an
+		if (iFile == null) { // file may be null, if it was opened from an
 			// IStorageEditorInput
 			log.debug("Aborting Xtext Validation with CheckMode: " + checkMode + " because file does not exist.");
 			return Status.OK_STATUS;
 		}
-		final List<Map<String, Object>> issues = xtextDocument
-				.readOnly(new IUnitOfWork<List<Map<String, Object>>, XtextResource>() {
-					public List<Map<String, Object>> exec(XtextResource resource) throws Exception {
-						return xtextResourceChecker.check(resource, Collections.singletonMap(CheckMode.KEY, checkMode),
-								monitor);
-					}
-				});
+		List<Map<String,Object>> issues = createIssues(monitor);
 		if (monitor.isCanceled())
 			return Status.CANCEL_STATUS;
 		MarkerUtil.addMarkers(iFile, issues, checkMode, deleteOldMarkers, monitor);
 		if (monitor.isCanceled())
 			return Status.CANCEL_STATUS;
 		return Status.OK_STATUS;
+	}
+
+	public List<Map<String,Object>> createIssues(final IProgressMonitor monitor) {
+		final List<Map<String, Object>> issues = xtextDocument.readOnly(
+				new IUnitOfWork<List<Map<String, Object>>, XtextResource>() {
+					public List<Map<String, Object>> exec(XtextResource resource) throws Exception {
+						return xtextResourceChecker.check(resource, getValidationContext(),	monitor);
+					}
+				});
+		return issues;
+	}
+
+	protected IXtextResourceChecker getXtextResourceChecker() {
+		return xtextResourceChecker;
+	}
+
+	protected IStateAccess<XtextResource> getXtextDocument() {
+		return xtextDocument;
+	}
+
+	protected CheckMode getCheckMode() {
+		return checkMode;
+	}
+
+	protected IFile getFile() {
+		return iFile;
+	}
+
+	protected boolean isDeleteOldMarkers() {
+		return deleteOldMarkers;
+	}
+
+	protected Map<Object, Object> getValidationContext() {
+		return Collections.<Object, Object>singletonMap(CheckMode.KEY, checkMode);
 	}
 
 	public static interface Factory {
