c714f345eb78f92c012156070b8e69d608a1fb61
szarnekow <szarnekow>
2009-08-18 17:08:48 +0000

    Fix: Wrong matching pattern for CVS dirs in CopyProjectSettings

--- releng/team/src/org/eclipse/xtext/releng/CopyProjectSettings.java
+++ releng/team/src/org/eclipse/xtext/releng/CopyProjectSettings.java
@@ -117,7 +117,7 @@ public class CopyProjectSettings {
 		File settingsDir = new File(".settings");
 		return settingsDir.listFiles(new FilenameFilter() {
 			public boolean accept(File dir, String name) {
-				return !name.toLowerCase().contains("CVS");
+				return !name.toLowerCase().contains("cvs");
 			}
 		});
 	}
