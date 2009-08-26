194d4722b371c6a8f253d0ec005b4dd9b0ec8123
szarnekow <szarnekow>
2009-08-26 19:48:59 +0000

    Enabling some "lost" tests

--- tests/org.eclipse.xtext.ui.common.tests/src/org/eclipse/xtext/ui/common/editor/outline/impl/DeclarativeSemanticModelTransformerTest.java
+++ tests/org.eclipse.xtext.ui.common.tests/src/org/eclipse/xtext/ui/common/editor/outline/impl/DeclarativeSemanticModelTransformerTest.java
@@ -23,7 +23,7 @@ import org.eclipse.xtext.ui.common.editor.outline.transformer.ISemanticModelTran
 /**
  * @author Peter Friese - Initial contribution and API
  */
-public class AbstractDeclarativeSemanticModelTransformerTest extends AbstractXtextTests {
+public class DeclarativeSemanticModelTransformerTest extends AbstractXtextTests {
 
 	private root theRoot;
 	private XtextResource resource;
