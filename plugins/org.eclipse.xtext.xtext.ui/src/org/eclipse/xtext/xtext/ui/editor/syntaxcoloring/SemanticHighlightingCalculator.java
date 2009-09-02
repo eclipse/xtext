306858261135bb455149faad27600f2488428856
szarnekow <szarnekow>
2009-09-02 15:29:18 +0000

    Feature: Highlight unused rules - see https://bugs.eclipse.org/bugs/show_bug.cgi?id=288366

--- plugins/org.eclipse.xtext.xtext.ui/src/org/eclipse/xtext/xtext/ui/editor/syntaxcoloring/SemanticHighlightingCalculator.java
+++ plugins/org.eclipse.xtext.xtext.ui/src/org/eclipse/xtext/xtext/ui/editor/syntaxcoloring/SemanticHighlightingCalculator.java
@@ -7,14 +7,21 @@
  *******************************************************************************/
 package org.eclipse.xtext.xtext.ui.editor.syntaxcoloring;
 
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.Set;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.xtext.AbstractElement;
 import org.eclipse.xtext.AbstractRule;
+import org.eclipse.xtext.Alternatives;
 import org.eclipse.xtext.Assignment;
+import org.eclipse.xtext.CrossReference;
 import org.eclipse.xtext.EcoreUtil2;
+import org.eclipse.xtext.Grammar;
 import org.eclipse.xtext.GrammarUtil;
+import org.eclipse.xtext.Group;
 import org.eclipse.xtext.ParserRule;
 import org.eclipse.xtext.RuleCall;
 import org.eclipse.xtext.TerminalRule;
@@ -28,6 +35,9 @@ import org.eclipse.xtext.parsetree.NodeUtil;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.common.editor.syntaxcoloring.IHighlightedPositionAcceptor;
 import org.eclipse.xtext.ui.common.editor.syntaxcoloring.ISemanticHighlightingCalculator;
+import org.eclipse.xtext.util.XtextSwitch;
+
+import com.google.common.collect.Sets;
 
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
@@ -39,13 +49,27 @@ public class SemanticHighlightingCalculator implements ISemanticHighlightingCalc
 			return;
 		
 		Iterator<EObject> iter = EcoreUtil.getAllContents(resource, true);
+		Set<AbstractRule> calledRules = Sets.newHashSet();
 		while(iter.hasNext()) {
 			EObject current = iter.next();
-			if (current instanceof AbstractRule) {
+			if (current instanceof Grammar) {
+				Grammar grammar = (Grammar) current;
+				if (!grammar.getRules().isEmpty()) {
+					AbstractRule firstRule = grammar.getRules().get(0);
+					if (firstRule instanceof ParserRule) {
+						UsedRulesFinder usedRulesFinder = new UsedRulesFinder(calledRules);
+						usedRulesFinder.doSwitch(firstRule);
+						usedRulesFinder.doSwitch(grammar);
+					}
+				}
+			} else if (current instanceof AbstractRule) {
 				AbstractNode node = getFirstFeatureNode(current, XtextPackage.Literals.ABSTRACT_RULE__NAME.getName());
 				highlightNode(node, SemanticHighlightingConfiguration.RULE_DECLARATION_ID, acceptor);
 				if (current instanceof ParserRule && GrammarUtil.isDatatypeRule((ParserRule) current)) {
 					highlightNode(node, SemanticHighlightingConfiguration.DATA_TYPE_RULE_ID, acceptor);	
+				}
+				if (!calledRules.isEmpty() && !calledRules.contains(current)) {
+					highlightNode(node, SemanticHighlightingConfiguration.NEVER_CALLED_RULE_ID, acceptor);
 				}
 			} else if (current instanceof TypeRef) {
 				AbstractNode node = getFirstFeatureNode(current, null);
@@ -63,6 +87,77 @@ public class SemanticHighlightingCalculator implements ISemanticHighlightingCalc
 				}
 			}
 		}
+	}
+	
+	public static class UsedRulesFinder extends XtextSwitch<Void> {
+		
+		private final Collection<AbstractRule> usedRules;
+		private final Collection<Grammar> visitedGrammars = Sets.newHashSet();
+
+		public UsedRulesFinder(Collection<AbstractRule> usedRules) {
+			this.usedRules = usedRules;
+		}
+		
+		@Override
+		public Void caseAbstractRule(AbstractRule object) {
+			usedRules.add(object);
+			return null;
+		}
+		
+		@Override
+		public Void caseParserRule(ParserRule object) {
+			if (usedRules.add(object) && object.getAlternatives() != null)
+				doSwitch(object.getAlternatives());
+			return null;
+		}
+		
+		@Override
+		public Void caseRuleCall(RuleCall object) {
+			if (object.getRule() != null)
+				return doSwitch(object.getRule());
+			return null;
+		}
+		
+		@Override
+		public Void caseCrossReference(CrossReference object) {
+			if (object.getTerminal() != null)
+				return doSwitch(object.getTerminal());
+			return null;
+		}
+		
+		@Override
+		public Void caseAssignment(Assignment object) {
+			if (object.getTerminal() != null)
+				return doSwitch(object.getTerminal());
+			return null;
+		}
+		
+		@Override
+		public Void caseAlternatives(Alternatives object) {
+			for(AbstractElement element: object.getGroups())
+				doSwitch(element);
+			return null;
+		}
+		
+		@Override
+		public Void caseGroup(Group object) {
+			for(AbstractElement element: object.getTokens())
+				doSwitch(element);
+			return null;
+		}
+		
+		@Override
+		public Void caseGrammar(Grammar object) {
+			if (!visitedGrammars.add(object))
+				return null;
+			if (object.isDefinesHiddenTokens())
+				usedRules.addAll(object.getHiddenTokens());
+			for(Grammar grammar: object.getUsedGrammars()) {
+				caseGrammar(grammar);
+			}
+			return null;
+		}
+		
 	}
 	
 	private void highlightNode(AbstractNode node, String id, IHighlightedPositionAcceptor acceptor) {
