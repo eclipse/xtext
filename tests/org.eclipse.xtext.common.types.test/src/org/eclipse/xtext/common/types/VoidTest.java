a88b476a338c0a95a2ffbc8940477db952a11f40
szarnekow <szarnekow>
2009-09-09 09:32:11 +0000

    Initial check in

---
+++ tests/org.eclipse.xtext.common.types.test/src/org/eclipse/xtext/common/types/VoidTest.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2009 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.common.types;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Sebastian Zarnekow - Initial contribution and API
+ */
+public class VoidTest extends TestCase {
+
+	private Void _void;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		_void = TypesFactory.eINSTANCE.createVoid();
+	}
+	
+	public void testCanonicalName_01() {
+		assertEquals("void", _void.getCanonicalName());
+	}
+	
+}
