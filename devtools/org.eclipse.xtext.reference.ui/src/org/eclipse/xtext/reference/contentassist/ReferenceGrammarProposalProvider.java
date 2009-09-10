aeb094f195a057b0f2796f8892852dcf04c84849
sefftinge <sefftinge>
2009-09-10 12:05:27 +0000

    deleted reference example

--- devtools/org.eclipse.xtext.reference.ui/src/org/eclipse/xtext/reference/contentassist/ReferenceGrammarProposalProvider.java
+++
@@ -1,5 +0,0 @@
-package org.eclipse.xtext.reference.contentassist;
-
-public class ReferenceGrammarProposalProvider extends org.eclipse.xtext.reference.contentassist.AbstractReferenceGrammarProposalProvider {
-
-}
