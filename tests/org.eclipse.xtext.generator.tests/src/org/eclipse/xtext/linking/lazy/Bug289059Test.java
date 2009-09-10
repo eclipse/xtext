579f07d25db5b154383844b6d53cd76ac5d2ed09
szarnekow <szarnekow>
2009-09-10 14:42:06 +0000

    Fix: Cross references were not found in some cases - see https://bugs.eclipse.org/bugs/show_bug.cgi?id=289054, removed some warnings

---
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/linking/lazy/Bug289059Test.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2009 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.linking.lazy;
+
+import org.eclipse.xtext.junit.AbstractXtextTests;
+import org.eclipse.xtext.linking.Bug289059TestLanguageStandaloneSetup;
+import org.eclipse.xtext.linking.bug289059Test.Model;
+
+/**
+ * @author Sebastian Zarnekow - Initial contribution and API
+ */
+public class Bug289059Test extends AbstractXtextTests {
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		with(Bug289059TestLanguageStandaloneSetup.class);
+	}
+	
+	public void testModelLinked_01() throws Exception {
+		String modelAsText = "model model";
+		Model model = (Model) getModel(modelAsText);
+		assertSame(model, model.getReference());
+	}
+	
+	public void testModelLinked_02() throws Exception {
+		String modelAsText = "model enabled model";
+		Model model = (Model) getModel(modelAsText);
+		assertSame(model, model.getReference());
+	}
+	
+}
