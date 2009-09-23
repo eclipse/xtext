635ba799d8a2495a577d310ffa0252796c88d62d
szarnekow <szarnekow>
2009-09-23 13:55:14 +0000

    Next try: another test to learn about the differences between client and server side execution

--- tests/org.eclipse.xtext.common.types.tests/src/org/eclipse/xtext/common/types/access/jdt/MockJavaProjectProviderTest.java
+++ tests/org.eclipse.xtext.common.types.tests/src/org/eclipse/xtext/common/types/access/jdt/MockJavaProjectProviderTest.java
@@ -9,17 +9,45 @@ package org.eclipse.xtext.common.types.access.jdt;
 
 import junit.framework.TestCase;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.IParent;
+import org.eclipse.jdt.internal.core.JavaElement;
+import org.eclipse.xtext.common.types.access.scenarios.ParameterizedTypes;
 
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
  */
+@SuppressWarnings("restriction")
 public class MockJavaProjectProviderTest extends TestCase {
 
-	public void testClasspathResolved() throws JavaModelException {
+	public void testClasspathResolved() throws CoreException {
 		IJavaProject javaProject = new MockJavaProjectProvider().getJavaProject(null);
 		javaProject.getResolvedClasspath(false);
+		assertTrue(expandAndLookFor(javaProject, 0, ParameterizedTypes.class.getSimpleName()));
+		
+	}
+	
+	public boolean expandAndLookFor(IJavaElement javaElement, int tab, String input) throws CoreException {
+		IJavaElement[] children = null;
+		// force opening of element by getting its children
+		if (javaElement instanceof IParent) {
+			IParent parent = (IParent)javaElement;
+			children = parent.getChildren();
+		}
+		StringBuffer buffer = new StringBuffer(16);
+		((JavaElement)javaElement).toStringInfo(tab, buffer);
+		System.out.println(buffer);
+		boolean result = buffer.indexOf(input) >= 0;
+		if (!result) {
+			if (children != null) {
+				for (int i = 0, length = children.length; i < length; i++) {
+					result |= expandAndLookFor(children[i], tab + 1, input);
+				}
+			}
+		}
+		return result;
 	}
 	
 }
