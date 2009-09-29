d280002fc1ee29d5fe846fac2ad8c34ae9b2997c
szarnekow <szarnekow>
2009-09-29 12:24:05 +0000

    Fix: Fields with reference to TypeParameter could not be resolved

---
+++ plugins/org.eclipse.xtext.common.types/src/org/eclipse/xtext/common/types/testSetups/Fields.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2009 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.common.types.testSetups;
+
+import java.util.List;
+
+/**
+ * @author Sebastian Zarnekow - Initial contribution and API
+ */
+public class Fields<T> extends TestScenario {
+
+	@SuppressWarnings("unused")
+	private T privateT;
+	
+	List<T> defaultListT;
+	
+	protected String protectedString;
+	
+	public int publicInt;
+	
+	public static class Inner extends TestScenario {
+		
+		public Fields<String> innerFields;
+		
+	}
+	
+}
