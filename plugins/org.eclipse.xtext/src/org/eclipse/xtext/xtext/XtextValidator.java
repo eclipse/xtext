950936acbc86354ae684624ca14f0c513fe85b74
szarnekow <szarnekow>
2009-10-04 21:37:52 +0000

    Fixed detection of left recursive grammar - see https://bugs.eclipse.org/bugs/show_bug.cgi?id=285605 and https://bugs.eclipse.org/bugs/show_bug.cgi?id=290919

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/xtext/XtextValidator.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/xtext/XtextValidator.java
@@ -7,17 +7,14 @@
  *******************************************************************************/
 package org.eclipse.xtext.xtext;
 
-import static org.eclipse.xtext.GrammarUtil.isOptionalCardinality;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EEnum;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.xtext.AbstractElement;
@@ -49,7 +46,6 @@ import org.eclipse.xtext.util.XtextSwitch;
 import org.eclipse.xtext.validation.AbstractDeclarativeValidator;
 import org.eclipse.xtext.validation.Check;
 import org.eclipse.xtext.validation.CheckType;
-import org.eclipse.xtext.validation.ValidationMessageAcceptor;
 
 import com.google.common.base.Function;
 import com.google.common.base.Predicate;
@@ -519,20 +515,20 @@ public class XtextValidator extends AbstractDeclarativeValidator {
 
 	@Check
 	public void checkForOverriddenValue(final ParserRule rule) {
-		OverriddenValueInspector inspection = new OverriddenValueInspector(this);
-		inspection.inspect(rule);
+		OverriddenValueInspector inspector = new OverriddenValueInspector(this);
+		inspector.inspect(rule);
 	}
 	
 	@Check
 	public void checkInstanceCreated(final ParserRule rule) {
-		RuleWithoutInstantiationInspector inspection = new RuleWithoutInstantiationInspector(this);
-		inspection.inspect(rule);
+		RuleWithoutInstantiationInspector inspector = new RuleWithoutInstantiationInspector(this);
+		inspector.inspect(rule);
 	}
 	
 	@Check
 	public void checkInstanceCreatedForEntryRule(final ParserRule rule) {
-		ValidEntryRuleInspector inspection = new ValidEntryRuleInspector(this);
-		inspection.inspect(rule);
+		ValidEntryRuleInspector inspector = new ValidEntryRuleInspector(this);
+		inspector.inspect(rule);
 	}
 	
 	@Check
@@ -543,109 +539,8 @@ public class XtextValidator extends AbstractDeclarativeValidator {
 	}
 
 	@Check
-	public void checkIfGrammarIsLeftRecursionFree(final Grammar grammar) {
-		new LeftRecursiveGrammarSwitch(this).doSwitch(grammar);
-	}
-
-	public static class LeftRecursiveGrammarSwitch extends XtextSwitch<Void> {
-		private final ValidationMessageAcceptor validationMessageAcceptor;
-		private ParserRule parserRule;
-		private Set<RuleCall> validatedRuleCalls = Sets.newHashSet();
-		private Set<RuleCall> leftRecursiveRuleCalls = Sets.newLinkedHashSet();
-
-		private LeftRecursiveGrammarSwitch(ValidationMessageAcceptor validationMessageAcceptor) {
-			this.validationMessageAcceptor = validationMessageAcceptor;
-		}
-
-		@Override
-		public Void caseGrammar(Grammar grammar) {
-			EList<AbstractRule> rules = grammar.getRules();
-			for (AbstractRule abstractRule : rules) {
-				doSwitch(abstractRule);
-			}
-			for (RuleCall ruleCall : leftRecursiveRuleCalls) {
-				this.validationMessageAcceptor.acceptError("The following rule call is left-recursive '"
-						+ ruleCall.getRule().getName() + "'", ruleCall, XtextPackage.RULE_CALL__RULE, null);
-			}
-			return null;
-		}
-
-		@Override
-		public Void caseParserRule(ParserRule parserRule) {
-			this.parserRule = parserRule;
-			this.validatedRuleCalls = Sets.newHashSet();
-			doSwitch(parserRule.getAlternatives());
-			return null;
-		}
-
-		@Override
-		public Void caseGroup(Group group) {
-			boolean isOptional = true;
-			for (Iterator<AbstractElement> iterator = group.getTokens().iterator(); iterator.hasNext() && isOptional;) {
-				AbstractElement groupElement = iterator.next();
-				doSwitch(groupElement);
-				isOptional = isOptionalGroupElement(groupElement);
-			}
-			return null;
-		}
-
-		@Override
-		public Void caseAlternatives(Alternatives alternatives) {
-			for (AbstractElement alternativeElement : alternatives.getGroups()) {
-				doSwitch(alternativeElement);
-			}
-			return null;
-		}
-
-		@Override
-		public Void caseAssignment(Assignment assignment) {
-			AbstractElement terminal = assignment.getTerminal();
-			if (terminal instanceof RuleCall) {
-				RuleCall ruleCall = (RuleCall) terminal;
-				doSwitch(ruleCall);
-			}
-			return null;
-		}
-
-		@Override
-		public Void caseRuleCall(RuleCall ruleCall) {
-			if (null == ruleCall.getRule() || null == parserRule) {
-				return null;
-			}
-			boolean isLeftRecursive = false;
-			if (ruleCall.getRule().equals(parserRule)) {
-				isLeftRecursive = true;
-			}
-			else {
-				if (validatedRuleCalls.contains(ruleCall)) {
-					isLeftRecursive = true;
-				}
-				else if (ruleCall.getRule() instanceof ParserRule) {
-					validatedRuleCalls.add(ruleCall);
-					doSwitch(ruleCall.getRule().getAlternatives());
-				}
-			}
-			if (isLeftRecursive) {
-				leftRecursiveRuleCalls.add(ruleCall);
-			}
-			return null;
-		}
-
-		private boolean isOptionalGroupElement(AbstractElement groupElement) {
-			boolean isOptional = true;
-			if ((groupElement instanceof Group || groupElement instanceof Alternatives)
-					&& !isOptionalCardinality(groupElement)) {
-				EList<AbstractElement> abstractTokens = groupElement instanceof Group ? ((Group) groupElement)
-						.getTokens() : ((Alternatives) groupElement).getGroups();
-				for (Iterator<AbstractElement> iterator = abstractTokens.iterator(); isOptional && iterator.hasNext();) {
-					AbstractElement abstractElement = iterator.next();
-					isOptional = isOptionalGroupElement(abstractElement);
-				}
-			}
-			else {
-				isOptional = isOptionalCardinality(groupElement);
-			}
-			return isOptional;
-		}
+	public void checkForLeftRecursion(final Grammar grammar) {
+		GrammarWithoutLeftRecursionInspector inspector = new GrammarWithoutLeftRecursionInspector(this);
+		inspector.inspect(grammar);
 	}
 }
