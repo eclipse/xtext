695ee0aa5032ed05d5c6df0b84a2c9644fe3b1fc
szarnekow <szarnekow>
2009-10-19 07:54:20 +0000

    Refactoring: Make it easier to mock / alter the lexer, refer to lexer as TokenSource in generated code instead of rich lexer interface to make it easier to implement manually

--- tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/service/GenericModuleTest.java
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/service/GenericModuleTest.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2009 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package org.eclipse.xtext.service;
 
 import java.lang.reflect.Type;
@@ -9,10 +16,8 @@ import junit.framework.TestCase;
 import org.eclipse.xtext.service.AbstractGenericModule.Binding;
 
 import com.google.inject.Guice;
-import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Provider;
-import com.google.inject.name.Named;
 
 public class GenericModuleTest extends TestCase {
 	
@@ -169,6 +174,7 @@ public class GenericModuleTest extends TestCase {
 	}
 	
 	public void testNamedBinding() throws Exception {
+		@SuppressWarnings("unused")
 		AbstractGenericModule module = new AbstractGenericModule() {
 			String foo() {
 				return "bar";
