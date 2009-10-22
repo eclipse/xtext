3c6c82636b42a9b37dfbc8abca6094a9a5338de9
mclay <mclay>
2009-10-22 21:18:12 +0000

    https://bugs.eclipse.org/bugs/show_bug.cgi?id=293022

--- plugins/org.eclipse.xtext.ui.core/src/org/eclipse/xtext/ui/core/editor/validation/AnnotationIssueProcessor.java
+++ plugins/org.eclipse.xtext.ui.core/src/org/eclipse/xtext/ui/core/editor/validation/AnnotationIssueProcessor.java
@@ -8,6 +8,9 @@
  *******************************************************************************/
 package org.eclipse.xtext.ui.core.editor.validation;
 
+import static com.google.common.collect.Collections2.*;
+
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -23,9 +26,11 @@ import org.eclipse.ui.texteditor.AnnotationTypeLookup;
 import org.eclipse.ui.texteditor.MarkerAnnotation;
 import org.eclipse.xtext.ui.core.editor.XtextEditor;
 
-import com.google.common.collect.HashBiMap;
+import com.google.common.base.Predicate;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
 
 /**
  * @author Sven Efftinge - Initial contribution and API
@@ -42,13 +47,14 @@ public class AnnotationIssueProcessor implements IValidationIssueProcessor {
 
 	public void processIssues(List<Map<String, Object>> issues, IProgressMonitor monitor) {
 		List<Annotation> toBeRemoved = getAnnotationsToRemove(monitor);
-		HashBiMap<Annotation, Position> annotationToPosition = getAnnotationsToAdd(issues, monitor);
-		updateMarkerAnnotations(annotationToPosition.inverse(),monitor);
+		Multimap<Position, Annotation> positionToAnnotations = Multimaps.newArrayListMultimap();
+		Map<Annotation, Position> annotationToPosition = getAnnotationsToAdd(positionToAnnotations,issues, monitor);
+		updateMarkerAnnotations(positionToAnnotations,monitor);
 		updateAnnotations(monitor, toBeRemoved, annotationToPosition);
 	}
 
 	private void updateAnnotations(IProgressMonitor monitor, List<Annotation> toBeRemoved,
-			HashBiMap<Annotation, Position> annotationToPosition) {
+			Map<Annotation, Position> annotationToPosition) {
 		if (monitor.isCanceled()) {
 			return;
 		}
@@ -93,12 +99,12 @@ public class AnnotationIssueProcessor implements IValidationIssueProcessor {
 		return toBeRemoved;
 	}
 
-	private HashBiMap<Annotation, Position> getAnnotationsToAdd(List<Map<String, Object>> issues,
+	private Map<Annotation, Position> getAnnotationsToAdd(Multimap<Position,Annotation> positionToAnnotations, List<Map<String, Object>> issues,
 			IProgressMonitor monitor) {
 		if (monitor.isCanceled()) {
 			return Maps.newHashBiMap();
 		}
-		HashBiMap<Annotation, Position> annotationToPosition = new HashBiMap<Annotation, Position>(issues.size());
+		Map<Annotation, Position> annotationToPosition = Maps.newHashMapWithExpectedSize(issues.size());
 		for (Map<String, Object> map : issues) {
 			if (monitor.isCanceled()) {
 				return annotationToPosition;
@@ -112,12 +118,13 @@ public class AnnotationIssueProcessor implements IValidationIssueProcessor {
 				Annotation annotation = new Annotation(type, false, message);
 				Position position = new Position(start, end - start);
 				annotationToPosition.put(annotation, position);
+				positionToAnnotations.put(position, annotation);
 			}
 		}
 		return annotationToPosition;
 	}
 
-	private void updateMarkerAnnotations(Map<Position, Annotation> annotationsToAdd, IProgressMonitor monitor) {
+	private void updateMarkerAnnotations(Multimap<Position, Annotation> positionToAnnotations, IProgressMonitor monitor) {
 		if (monitor.isCanceled()) {
 			return;
 		}
@@ -130,11 +137,17 @@ public class AnnotationIssueProcessor implements IValidationIssueProcessor {
 			Annotation annotation = annotationIterator.next();
 			if (isRelevantAnnotationType(annotation.getType()) && (annotation instanceof MarkerAnnotation)) {
 				Position markerAnnotationPosition = annotationModel.getPosition(annotation);
-				MarkerAnnotation markerAnnotation = (MarkerAnnotation) annotation;
-				Annotation sourceAnnotation = annotationsToAdd.get(markerAnnotationPosition);
-				boolean deleted = sourceAnnotation == null
-						|| !sourceAnnotation.getText().equals(markerAnnotation.getText());
-				markerAnnotation.markDeleted(deleted);
+				final MarkerAnnotation markerAnnotation = (MarkerAnnotation) annotation;
+				Collection<Annotation> sourceAnnotations = positionToAnnotations.get(markerAnnotationPosition);
+				boolean markAsDeleted=true;
+				if (null!=sourceAnnotations) {
+					markAsDeleted = filter(sourceAnnotations, new Predicate<Annotation>(){
+						public boolean apply(Annotation sourceAnnotation) {
+							return sourceAnnotation.getText().equals(markerAnnotation.getText()) && 
+								   sourceAnnotation.getType().equals(markerAnnotation.getType());
+					}}).isEmpty();
+				} 
+				markerAnnotation.markDeleted(markAsDeleted);
 			}
 		}
 	}
