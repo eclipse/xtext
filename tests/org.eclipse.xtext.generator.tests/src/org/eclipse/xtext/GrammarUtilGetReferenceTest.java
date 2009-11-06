2b255911956c18f657316940c689ec611efd9de1
szarnekow <szarnekow>
2009-11-06 16:18:18 +0000

    Fix: https://bugs.eclipse.org/bugs/show_bug.cgi?id=294309 - missing proposals when actions are involved

---
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/GrammarUtilGetReferenceTest.java
@@ -0,0 +1,110 @@
+/*******************************************************************************
+ * Copyright (c) 2009 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext;
+
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.xtext.junit.AbstractXtextTests;
+
+/**
+ * @author Sebastian Zarnekow - Initial contribution and API
+ */
+public class GrammarUtilGetReferenceTest extends AbstractXtextTests {
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		with(XtextStandaloneSetup.class);
+	}
+	
+	public void testGetReference_01() throws Exception {
+		String grammarAsString = "grammar foo.bar with org.eclipse.xtext.common.Terminals\n" +
+			"generate test 'http://test'\n" +
+			"Model: model=[Model];";
+		Grammar grammar = (Grammar) getModel(grammarAsString);
+		AbstractRule ruleModel = grammar.getRules().get(0);
+		CrossReference crossRef = (CrossReference) ((Assignment) ruleModel.getAlternatives()).getTerminal();
+		EReference reference = GrammarUtil.getReference(crossRef);
+		assertEquals("model", reference.getName());
+		assertEquals("Model", reference.getEContainingClass().getName());
+	}
+	
+	public void testGetReference_02() throws Exception {
+		String grammarAsString = "grammar foo.bar with org.eclipse.xtext.common.Terminals\n" +
+			"generate test 'http://test'\n" +
+			"Model: {SubModel} model=[Model];";
+		Grammar grammar = (Grammar) getModel(grammarAsString);
+		AbstractRule ruleModel = grammar.getRules().get(0);
+		Group group = (Group) ruleModel.getAlternatives();
+		Assignment assignment = (Assignment) group.getTokens().get(1);
+		CrossReference crossRef = (CrossReference) assignment.getTerminal();
+		EReference reference = GrammarUtil.getReference(crossRef);
+		assertEquals("model", reference.getName());
+		assertEquals("SubModel", reference.getEContainingClass().getName());
+	}
+	
+	public void testGetReference_03() throws Exception {
+		String grammarAsString = "grammar foo.bar with org.eclipse.xtext.common.Terminals\n" +
+			"generate test 'http://test'\n" +
+			"Model: {SubModel1} model=[Model] | {SubModel2} model=[Model];";
+		Grammar grammar = (Grammar) getModel(grammarAsString);
+		AbstractRule ruleModel = grammar.getRules().get(0);
+		Alternatives alternatives = (Alternatives) ruleModel.getAlternatives();
+		Group group = (Group) alternatives.getGroups().get(0);
+		Assignment assignment = (Assignment) group.getTokens().get(1);
+		CrossReference crossRef = (CrossReference) assignment.getTerminal();
+		EReference reference = GrammarUtil.getReference(crossRef);
+		assertEquals("model", reference.getName());
+		assertEquals("Model", reference.getEContainingClass().getName());
+	}
+	
+	public void testGetReference_04() throws Exception {
+		String grammarAsString = "grammar foo.bar with org.eclipse.xtext.common.Terminals\n" +
+			"generate test 'http://test'\n" +
+			"Model: {SubModel1} model=[Model] | {SubModel2} model=[Model];";
+		Grammar grammar = (Grammar) getModel(grammarAsString);
+		AbstractRule ruleModel = grammar.getRules().get(0);
+		Alternatives alternatives = (Alternatives) ruleModel.getAlternatives();
+		Group group = (Group) alternatives.getGroups().get(1);
+		Assignment assignment = (Assignment) group.getTokens().get(1);
+		CrossReference crossRef = (CrossReference) assignment.getTerminal();
+		EReference reference = GrammarUtil.getReference(crossRef);
+		assertEquals("model", reference.getName());
+		assertEquals("Model", reference.getEContainingClass().getName());
+	}
+	
+	public void testGetReference_05() throws Exception {
+		String grammarAsString = "grammar foo.bar with org.eclipse.xtext.common.Terminals\n" +
+			"generate test 'http://test'\n" +
+			"Model: {SubModel1} model1=[Model] | {SubModel2} model2=[Model];";
+		Grammar grammar = (Grammar) getModel(grammarAsString);
+		AbstractRule ruleModel = grammar.getRules().get(0);
+		Alternatives alternatives = (Alternatives) ruleModel.getAlternatives();
+		Group group = (Group) alternatives.getGroups().get(0);
+		Assignment assignment = (Assignment) group.getTokens().get(1);
+		CrossReference crossRef = (CrossReference) assignment.getTerminal();
+		EReference reference = GrammarUtil.getReference(crossRef);
+		assertEquals("model1", reference.getName());
+		assertEquals("SubModel1", reference.getEContainingClass().getName());
+	}
+
+	public void testGetReference_06() throws Exception {
+		String grammarAsString = "grammar foo.bar with org.eclipse.xtext.common.Terminals\n" +
+			"generate test 'http://test'\n" +
+			"Model: {SubModel1} model1=[Model] | {SubModel2} model2=[Model];";
+		Grammar grammar = (Grammar) getModel(grammarAsString);
+		AbstractRule ruleModel = grammar.getRules().get(0);
+		Alternatives alternatives = (Alternatives) ruleModel.getAlternatives();
+		Group group = (Group) alternatives.getGroups().get(1);
+		Assignment assignment = (Assignment) group.getTokens().get(1);
+		CrossReference crossRef = (CrossReference) assignment.getTerminal();
+		EReference reference = GrammarUtil.getReference(crossRef);
+		assertEquals("model2", reference.getName());
+		assertEquals("SubModel2", reference.getEContainingClass().getName());
+	}
+
+}
