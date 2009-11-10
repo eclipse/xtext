59ddf2fc9a8d7ca49173f274372d66b683e9e108
szarnekow <szarnekow>
2009-11-10 14:11:26 +0000

    Bootstrapped and regenerated all languages

--- plugins/org.eclipse.xtext.ui.common/src-gen/org/eclipse/xtext/common/AbstractTerminalsUiModule.java
+++ plugins/org.eclipse.xtext.ui.common/src-gen/org/eclipse/xtext/common/AbstractTerminalsUiModule.java
@@ -139,7 +139,12 @@ public abstract class AbstractTerminalsUiModule extends TerminalsRuntimeModule {
 
 	// contributed by org.eclipse.xtext.ui.generator.ImplicitUiFragment
 	public Class<? extends org.eclipse.xtext.ui.core.builder.ILanguageBuilder> bindILanguageBuilder() {
-		return org.eclipse.xtext.ui.core.builder.impl.AbstractLanguageBuilder.class;
+		return org.eclipse.xtext.ui.core.builder.impl.SimpleProjectLanguageBuilder.class;
+	}
+
+	// contributed by org.eclipse.xtext.ui.generator.ImplicitUiFragment
+	public Class<? extends org.eclipse.xtext.ui.core.editor.model.XtextDocumentProvider> bindXtextDocumentProvider() {
+		return org.eclipse.xtext.ui.core.editor.model.ResourceAwareXtextDocumentProvider.class;
 	}
 
 	// contributed by org.eclipse.xtext.ui.generator.contentAssist.JavaBasedContentAssistFragment
