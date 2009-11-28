8231556460fc1dfdb143455c7a878dc8f230b4de
sefftinge <sefftinge>
2009-11-28 17:49:36 +0000

    ShadowingResourceDescription can be configured via setContext(Notifier)

--- plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/impl/ShadowingResourceDescriptions.java
+++ plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/impl/ShadowingResourceDescriptions.java
@@ -7,51 +7,104 @@
  *******************************************************************************/
 package org.eclipse.xtext.builder.impl;
 
+import java.util.Collections;
 import java.util.Set;
 
+import org.eclipse.emf.common.notify.Notifier;
+import org.eclipse.emf.common.notify.impl.AdapterImpl;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.resource.IResourceDescription;
 import org.eclipse.xtext.resource.IResourceDescriptions;
 import org.eclipse.xtext.resource.IResourceDescription.Event.Listener;
+import org.eclipse.xtext.resource.impl.ResourceSetBasedResourceDescriptions;
 
 import com.google.common.base.Function;
 import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Maps;
+import com.google.inject.Inject;
 
 /**
  * @author Sven Efftinge - Initial contribution and API
  */
-public class ShadowingResourceDescriptions implements IResourceDescriptions {
-	
+public class ShadowingResourceDescriptions implements IResourceDescriptions, IResourceDescriptions.IContextAware {
+
+	public static class DeletedUrisAdapter extends AdapterImpl {
+
+		public DeletedUrisAdapter(Set<URI> deletedUris) {
+			super();
+			this.deletedUris = deletedUris;
+		}
+
+		private Set<URI> deletedUris = null;
+
+		public Set<URI> getDeletedUris() {
+			return deletedUris;
+		}
+
+		@Override
+		public boolean isAdapterForType(Object type) {
+			return DeletedUrisAdapter.class == type;
+		}
+	}
+
+	protected Set<URI> getDeletedUris(Notifier notifier) {
+		DeletedUrisAdapter adapter = (DeletedUrisAdapter) EcoreUtil.getAdapter(notifier.eAdapters(),
+				DeletedUrisAdapter.class);
+		if (adapter == null)
+			return Collections.emptySet();
+		return adapter.deletedUris;
+	}
+
+	public void setContext(Notifier ctx) {
+		ResourceSet resourceSet = EcoreUtil2.getResourceSet(ctx);
+		if (resourceSet == null)
+			throw new NullPointerException();
+		ResourceSetBasedResourceDescriptions resourceSetBasedResourceDescriptions = new ResourceSetBasedResourceDescriptions();
+		resourceSetBasedResourceDescriptions.setContext(resourceSet);
+		this.shadow = resourceSetBasedResourceDescriptions;
+		deleteUris = getDeletedUris(resourceSet);
+	}
+
 	private IResourceDescriptions descriptions;
 	private IResourceDescriptions shadow;
 	private Set<URI> deleteUris;
-	
-	public ShadowingResourceDescriptions(IResourceDescriptions descriptions, IResourceDescriptions shadow, Set<URI> deleteUris) {
+
+	@Inject
+	public ShadowingResourceDescriptions(IResourceDescriptions descriptions) {
 		this.descriptions = descriptions;
-		this.shadow=shadow;
-		this.deleteUris = deleteUris;
 	}
-	
+
+	public ShadowingResourceDescriptions(IResourceDescriptions descriptions, IResourceDescriptions shadow,
+			Set<URI> deletedUris) {
+		this.descriptions = descriptions;
+		this.shadow = shadow;
+		this.deleteUris = deletedUris;
+	}
+
 	public Iterable<IResourceDescription> getAllResourceDescriptions() {
-		final ImmutableMap<URI,IResourceDescription> shadowAsSet = Maps.uniqueIndex(shadow.getAllResourceDescriptions(), new Function<IResourceDescription, URI>() {
+		final ImmutableMap<URI, IResourceDescription> shadowAsSet = Maps.uniqueIndex(shadow
+				.getAllResourceDescriptions(), new Function<IResourceDescription, URI>() {
 			public URI apply(IResourceDescription from) {
 				return from.getURI();
 			}
 		});
-		return Iterables.concat(shadowAsSet.values(),Iterables.filter(descriptions.getAllResourceDescriptions(), new Predicate<IResourceDescription>() {
-			public boolean apply(IResourceDescription input) {
-				URI uri = input.getURI();
-				return !(deleteUris.contains(uri) || shadowAsSet.containsKey(uri));
-			}
-		}));
+		return Iterables.concat(shadowAsSet.values(), Iterables.filter(descriptions.getAllResourceDescriptions(),
+				new Predicate<IResourceDescription>() {
+					public boolean apply(IResourceDescription input) {
+						URI uri = input.getURI();
+						return !(deleteUris.contains(uri) || shadowAsSet.containsKey(uri));
+					}
+				}));
 	}
 
 	public IResourceDescription getResourceDescription(URI uri) {
 		IResourceDescription desc = shadow.getResourceDescription(uri);
-		if (desc!=null)
+		if (desc != null)
 			return desc;
 		if (deleteUris.contains(uri))
 			return null;
