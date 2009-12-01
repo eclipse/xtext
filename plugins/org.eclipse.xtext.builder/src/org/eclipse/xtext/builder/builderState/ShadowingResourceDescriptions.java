8340d01740e7eafc505f0fc25a4ab02847c30239
sefftinge <sefftinge>
2009-12-01 14:07:32 +0000

    introduced IStorageAwareResourceDescription, IMarkerUpdater in builder

--- plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/builderState/ShadowingResourceDescriptions.java
+++ plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/builderState/ShadowingResourceDescriptions.java
@@ -7,39 +7,82 @@
  *******************************************************************************/
 package org.eclipse.xtext.builder.builderState;
 
+import java.util.Map;
 import java.util.Set;
 
+import org.eclipse.core.resources.IStorage;
 import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.emf.common.notify.impl.AdapterImpl;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.xtext.EcoreUtil2;
+import org.eclipse.xtext.resource.IEObjectDescription;
 import org.eclipse.xtext.resource.IResourceDescription;
 import org.eclipse.xtext.resource.IResourceDescriptions;
 import org.eclipse.xtext.resource.IResourceDescription.Event.Listener;
 import org.eclipse.xtext.resource.impl.ResourceSetBasedResourceDescriptions;
+import org.eclipse.xtext.ui.core.resource.IStorageAwareResourceDescription;
 
-import com.google.common.base.Function;
-import com.google.common.base.Predicate;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Iterables;
 import com.google.common.collect.Maps;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
 
 /**
  * @author Sven Efftinge - Initial contribution and API
  */
 public class ShadowingResourceDescriptions implements IResourceDescriptions, IResourceDescriptions.IContextAware {
 
+	/**
+	 * @author Sven Efftinge - Initial contribution and API
+	 */
+	protected static class StorageAwareResourceDescription implements IStorageAwareResourceDescription {
+
+		private IResourceDescription delegate;
+		private IStorage storage;
+		
+		
+		public StorageAwareResourceDescription(IStorage storage, IResourceDescription delegate) {
+			super();
+			this.storage = storage;
+			this.delegate = delegate;
+		}
+		public Iterable<IEObjectDescription> getExportedObjects() {
+			return delegate.getExportedObjects();
+		}
+		public Iterable<IEObjectDescription> getExportedObjects(EClass clazz, String name) {
+			return delegate.getExportedObjects(clazz, name);
+		}
+		public Iterable<IEObjectDescription> getExportedObjects(EClass clazz) {
+			return delegate.getExportedObjects(clazz);
+		}
+		public Iterable<IEObjectDescription> getExportedObjectsForEObject(EObject object) {
+			return delegate.getExportedObjectsForEObject(object);
+		}
+		public Iterable<String> getImportedNames() {
+			return delegate.getImportedNames();
+		}
+		public URI getURI() {
+			return delegate.getURI();
+		}
+		public IStorage getStorage() {
+			return storage;
+		}
+	}
+
 	public static class Adapter extends AdapterImpl {
 
 		private IResourceDescriptions toBeShadowed;
 		private Set<URI> deletedUris;
+		private Map<URI, IStorage> toBeUpdated;
 
-		public Adapter(IResourceDescriptions toBeShadowed, Set<URI> deletedUris) {
+		public Adapter(IResourceDescriptions toBeShadowed, Map<URI,IStorage> toBeUpdated, Set<URI> deletedUris) {
 			super();
 			this.toBeShadowed = toBeShadowed;
 			this.deletedUris = deletedUris;
+			this.toBeUpdated = toBeUpdated;
 		}
 		
 		public IResourceDescriptions getToBeShadowed() {
@@ -49,15 +92,26 @@ public class ShadowingResourceDescriptions implements IResourceDescriptions, IRe
 		public Set<URI> getDeletedUris() {
 			return deletedUris;
 		}
+		
+		public Map<URI, IStorage> getToBeUpdated() {
+			return toBeUpdated;
+		}
 
 		@Override
 		public boolean isAdapterForType(Object type) {
 			return Adapter.class == type;
 		}
 	}
+	
+	@Inject
+	private Provider<ResourceSetBasedResourceDescriptions> resourceSetDescriptionsProvider;
+	
+	private Map<URI, IStorageAwareResourceDescription> resourceDescriptions;
 
 	public void setContext(Notifier ctx) {
 		ResourceSet resourceSet = EcoreUtil2.getResourceSet(ctx);
+		ResourceSetBasedResourceDescriptions resourceSetBasedResourceDescriptions = resourceSetDescriptionsProvider.get();
+		resourceSetBasedResourceDescriptions.setContext(resourceSet);
 		if (resourceSet == null)
 			throw new NullPointerException();
 		Adapter adapter = (Adapter) EcoreUtil.getAdapter(resourceSet.eAdapters(),
@@ -65,61 +119,41 @@ public class ShadowingResourceDescriptions implements IResourceDescriptions, IRe
 		if (adapter == null)
 			throw new IllegalArgumentException("No "+ShadowingResourceDescriptions.Adapter.class.getName()+" found");
 		
-		ResourceSetBasedResourceDescriptions resourceSetBasedResourceDescriptions = new ResourceSetBasedResourceDescriptions();
-		resourceSetBasedResourceDescriptions.setContext(resourceSet);
-		setShadowing(resourceSetBasedResourceDescriptions);
-		setShadowed(adapter.getToBeShadowed());
-		setDeleteUris(adapter.getDeletedUris());
+		initialize(resourceSetBasedResourceDescriptions, adapter);
 	}
 
-	private IResourceDescriptions shadowed;
-	private IResourceDescriptions shadowing;
-	private Set<URI> deleteUris;
-	public void setDeleteUris(Set<URI> deleteUris) {
-		this.deleteUris = deleteUris;
-	}
-	
-	public void setShadowing(IResourceDescriptions shadow) {
-		this.shadowing = shadow;
-	}
-	
-	public void setShadowed(IResourceDescriptions descriptions) {
-		this.shadowed = descriptions;
-	}
 
-	public Iterable<IResourceDescription> getAllResourceDescriptions() {
-		final ImmutableMap<URI, IResourceDescription> shadowAsSet = Maps.uniqueIndex(shadowing
-				.getAllResourceDescriptions(), new Function<IResourceDescription, URI>() {
-			public URI apply(IResourceDescription from) {
-				return from.getURI();
+	public void initialize(IResourceDescriptions shadowing, Adapter adapter) {
+		this.resourceDescriptions = Maps.newHashMap();
+		for (Map.Entry<URI,IStorage> toBeUpdated : adapter.getToBeUpdated().entrySet()) {
+			URI uri = toBeUpdated.getKey();
+			if (!adapter.getDeletedUris().contains(uri) && !resourceDescriptions.containsKey(uri)) {
+				IResourceDescription description = shadowing.getResourceDescription(uri);
+				resourceDescriptions.put(uri, new StorageAwareResourceDescription(toBeUpdated.getValue(),description));
 			}
-		});
-		return Iterables.concat(shadowAsSet.values(), Iterables.filter(shadowed.getAllResourceDescriptions(),
-				new Predicate<IResourceDescription>() {
-					public boolean apply(IResourceDescription input) {
-						URI uri = input.getURI();
-						return !(deleteUris.contains(uri) || shadowAsSet.containsKey(uri));
-					}
-				}));
+		}
+		Iterable<? extends IResourceDescription> descriptions = adapter.getToBeShadowed().getAllResourceDescriptions();
+		for (IResourceDescription desc : descriptions) {
+			URI uri = desc.getURI();
+			if (!adapter.getDeletedUris().contains(uri) && !resourceDescriptions.containsKey(uri)) {
+				resourceDescriptions.put(uri, (IStorageAwareResourceDescription) desc);
+			}
+		}
+	}
+
+	
+	public Iterable<? extends IResourceDescription> getAllResourceDescriptions() {
+		return resourceDescriptions.values();
 	}
 
 	public IResourceDescription getResourceDescription(URI uri) {
-		IResourceDescription desc = shadowing.getResourceDescription(uri);
-		if (desc != null)
-			return desc;
-		if (deleteUris.contains(uri))
-			return null;
-		return shadowed.getResourceDescription(uri);
+		return resourceDescriptions.get(uri);
 	}
 
 	public void addListener(Listener listener) {
-		this.shadowed.addListener(listener);
-		this.shadowing.addListener(listener);
 	}
 
 	public void removeListener(Listener listener) {
-		this.shadowed.removeListener(listener);
-		this.shadowing.removeListener(listener);
 	}
 
 }
