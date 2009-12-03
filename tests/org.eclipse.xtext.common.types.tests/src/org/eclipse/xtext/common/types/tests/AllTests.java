72c47ead36498fe6ab575e51807d9da1d0578c2c
sefftinge <sefftinge>
2009-12-03 17:55:39 +0000

    sequence of test cases in generated test suites is stable.

--- tests/org.eclipse.xtext.common.types.tests/src/org/eclipse/xtext/common/types/tests/AllTests.java
+++ tests/org.eclipse.xtext.common.types.tests/src/org/eclipse/xtext/common/types/tests/AllTests.java
@@ -11,24 +11,6 @@ import junit.framework.TestSuite;
 public class AllTests {
    public static Test suite() {
       TestSuite suite = new TestSuite("Tests for org.eclipse.xtext.common.types.tests");
-      suite.addTestSuite(org.eclipse.xtext.common.types.access.ClasspathTypeProviderFactoryTest.class);
-      suite.addTestSuite(org.eclipse.xtext.common.types.access.impl.ClassFinderTest.class);
-      suite.addTestSuite(org.eclipse.xtext.common.types.access.impl.ClassMirrorTest.class);
-      suite.addTestSuite(org.eclipse.xtext.common.types.access.impl.ClassNameUtilTest.class);
-      suite.addTestSuite(org.eclipse.xtext.common.types.access.impl.ClasspathTypeProviderTest.class);
-      suite.addTestSuite(org.eclipse.xtext.common.types.access.impl.ClassURIHelperTest.class);
-      suite.addTestSuite(org.eclipse.xtext.common.types.access.impl.PrimitiveMirrorTest.class);
-      suite.addTestSuite(org.eclipse.xtext.common.types.access.impl.PrimitivesTest.class);
-      suite.addTestSuite(org.eclipse.xtext.common.types.access.impl.PrimitiveTypeFactoryTest.class);
-      suite.addTestSuite(org.eclipse.xtext.common.types.access.jdt.JdtTypeProviderFactoryTest.class);
-      suite.addTestSuite(org.eclipse.xtext.common.types.access.jdt.JdtTypeProviderTest.class);
-      suite.addTestSuite(org.eclipse.xtext.common.types.access.jdt.MockJavaProjectProviderTest.class);
-      suite.addTestSuite(org.eclipse.xtext.common.types.access.jdt.TypeURIHelperTest.class);
-      suite.addTestSuite(org.eclipse.xtext.common.types.access.TypeResourceTest.class);
-      suite.addTestSuite(org.eclipse.xtext.common.types.access.xtext.ClasspathBasedTypeScopeTest.class);
-      suite.addTestSuite(org.eclipse.xtext.common.types.access.xtext.ui.JdtBasedSimpleTypeScopeProviderTest.class);
-      suite.addTestSuite(org.eclipse.xtext.common.types.access.xtext.ui.JdtBasedSimpleTypeScopeTest.class);
-      suite.addTestSuite(org.eclipse.xtext.common.types.access.xtext.ui.XtextResourceSetBasedProjectProviderTest.class);
       suite.addTestSuite(org.eclipse.xtext.common.types.AnnotationTypeTest.class);
       suite.addTestSuite(org.eclipse.xtext.common.types.ArrayTypeTest.class);
       suite.addTestSuite(org.eclipse.xtext.common.types.ConstructorTest.class);
@@ -43,11 +25,29 @@ public class AllTests {
       suite.addTestSuite(org.eclipse.xtext.common.types.ReferenceTypeArgumentTest.class);
       suite.addTestSuite(org.eclipse.xtext.common.types.TypeParameterTest.class);
       suite.addTestSuite(org.eclipse.xtext.common.types.UpperBoundTest.class);
-      suite.addTestSuite(org.eclipse.xtext.common.types.util.ClasspathSuperTypeCollectorTest.class);
-      suite.addTestSuite(org.eclipse.xtext.common.types.util.JdtSuperTypeCollectorTest.class);
       suite.addTestSuite(org.eclipse.xtext.common.types.VoidTest.class);
       suite.addTestSuite(org.eclipse.xtext.common.types.WildcardTest.class);
       suite.addTestSuite(org.eclipse.xtext.common.types.WildcardTypeArgumentTest.class);
+      suite.addTestSuite(org.eclipse.xtext.common.types.access.ClasspathTypeProviderFactoryTest.class);
+      suite.addTestSuite(org.eclipse.xtext.common.types.access.TypeResourceTest.class);
+      suite.addTestSuite(org.eclipse.xtext.common.types.access.impl.ClassFinderTest.class);
+      suite.addTestSuite(org.eclipse.xtext.common.types.access.impl.ClassMirrorTest.class);
+      suite.addTestSuite(org.eclipse.xtext.common.types.access.impl.ClassNameUtilTest.class);
+      suite.addTestSuite(org.eclipse.xtext.common.types.access.impl.ClassURIHelperTest.class);
+      suite.addTestSuite(org.eclipse.xtext.common.types.access.impl.ClasspathTypeProviderTest.class);
+      suite.addTestSuite(org.eclipse.xtext.common.types.access.impl.PrimitiveMirrorTest.class);
+      suite.addTestSuite(org.eclipse.xtext.common.types.access.impl.PrimitiveTypeFactoryTest.class);
+      suite.addTestSuite(org.eclipse.xtext.common.types.access.impl.PrimitivesTest.class);
+      suite.addTestSuite(org.eclipse.xtext.common.types.access.jdt.JdtTypeProviderFactoryTest.class);
+      suite.addTestSuite(org.eclipse.xtext.common.types.access.jdt.JdtTypeProviderTest.class);
+      suite.addTestSuite(org.eclipse.xtext.common.types.access.jdt.MockJavaProjectProviderTest.class);
+      suite.addTestSuite(org.eclipse.xtext.common.types.access.jdt.TypeURIHelperTest.class);
+      suite.addTestSuite(org.eclipse.xtext.common.types.access.xtext.ClasspathBasedTypeScopeTest.class);
+      suite.addTestSuite(org.eclipse.xtext.common.types.access.xtext.ui.JdtBasedSimpleTypeScopeProviderTest.class);
+      suite.addTestSuite(org.eclipse.xtext.common.types.access.xtext.ui.JdtBasedSimpleTypeScopeTest.class);
+      suite.addTestSuite(org.eclipse.xtext.common.types.access.xtext.ui.XtextResourceSetBasedProjectProviderTest.class);
+      suite.addTestSuite(org.eclipse.xtext.common.types.util.ClasspathSuperTypeCollectorTest.class);
+      suite.addTestSuite(org.eclipse.xtext.common.types.util.JdtSuperTypeCollectorTest.class);
       return suite;
    }
 }
