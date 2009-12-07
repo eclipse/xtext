dfa3c3d9417fcd978bf6fe0403c98060828a4187
sefftinge <sefftinge>
2009-12-07 16:22:43 +0000

    fixed problem with DefaultReferenceDescription (no longer EMF settings based)

--- tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/generator/tests/AllTests.java
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/generator/tests/AllTests.java
@@ -135,7 +135,7 @@ public class AllTests {
       suite.addTestSuite(org.eclipse.xtext.resource.ResourceSetReferencingResourceSetTest.class);
       suite.addTestSuite(org.eclipse.xtext.resource.XtextResourcePerformanceTest.class);
       suite.addTestSuite(org.eclipse.xtext.resource.XtextResourceTest.class);
-      suite.addTestSuite(org.eclipse.xtext.resource.impl.DefaultImportedNamesProviderTest.class);
+      suite.addTestSuite(org.eclipse.xtext.resource.impl.DefaultResourceDescription2Test.class);
       suite.addTestSuite(org.eclipse.xtext.resource.impl.DefaultReferenceDescriptionTest.class);
       suite.addTestSuite(org.eclipse.xtext.resource.impl.DefaultResourceDescriptionDeltaTest.class);
       suite.addTestSuite(org.eclipse.xtext.resource.impl.DefaultResourceDescriptionTest.class);
