7de873fe6bb9101265b8f1876bf7b71b52cf0c36
szarnekow <szarnekow>
2009-12-14 09:41:07 +0000

    Fix: NPE in EcoreUti2.containsCompatibleFeature - see https://bugs.eclipse.org/bugs/show_bug.cgi?id=296497

--- tests/org.eclipse.xtext.generator.tests/emf-gen/org/eclipse/xtext/enumrules/enums/EnumsPackage.java
+++ tests/org.eclipse.xtext.generator.tests/emf-gen/org/eclipse/xtext/enumrules/enums/EnumsPackage.java
@@ -2,10 +2,12 @@
  * <copyright>
  * </copyright>
  *
- * $Id: EnumsPackage.java,v 1.1 2009/03/18 16:58:37 szarnekow Exp $
+ * $Id: EnumsPackage.java,v 1.2 2009/12/14 09:41:09 szarnekow Exp $
  */
 package org.eclipse.xtext.enumrules.enums;
 
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EEnum;
 import org.eclipse.emf.ecore.EPackage;
 
@@ -58,6 +60,43 @@ public interface EnumsPackage extends EPackage {
 	EnumsPackage eINSTANCE = org.eclipse.xtext.enumrules.enums.impl.EnumsPackageImpl.init();
 
 	/**
+	 * The meta object id for the '{@link org.eclipse.xtext.enumrules.enums.impl.ExistingTypeImpl <em>Existing Type</em>}' class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see org.eclipse.xtext.enumrules.enums.impl.ExistingTypeImpl
+	 * @see org.eclipse.xtext.enumrules.enums.impl.EnumsPackageImpl#getExistingType()
+	 * @generated
+	 */
+	int EXISTING_TYPE = 0;
+
+	/**
+	 * The feature id for the '<em><b>Enum Feature</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 * @ordered
+	 */
+	int EXISTING_TYPE__ENUM_FEATURE = 0;
+
+	/**
+	 * The feature id for the '<em><b>String Feature</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 * @ordered
+	 */
+	int EXISTING_TYPE__STRING_FEATURE = 1;
+
+	/**
+	 * The number of structural features of the '<em>Existing Type</em>' class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 * @ordered
+	 */
+	int EXISTING_TYPE_FEATURE_COUNT = 2;
+
+	/**
 	 * The meta object id for the '{@link org.eclipse.xtext.enumrules.enums.ExistingEnum <em>Existing Enum</em>}' enum.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
@@ -65,8 +104,40 @@ public interface EnumsPackage extends EPackage {
 	 * @see org.eclipse.xtext.enumrules.enums.impl.EnumsPackageImpl#getExistingEnum()
 	 * @generated
 	 */
-	int EXISTING_ENUM = 0;
+	int EXISTING_ENUM = 1;
 
+
+	/**
+	 * Returns the meta object for class '{@link org.eclipse.xtext.enumrules.enums.ExistingType <em>Existing Type</em>}'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the meta object for class '<em>Existing Type</em>'.
+	 * @see org.eclipse.xtext.enumrules.enums.ExistingType
+	 * @generated
+	 */
+	EClass getExistingType();
+
+	/**
+	 * Returns the meta object for the attribute '{@link org.eclipse.xtext.enumrules.enums.ExistingType#getEnumFeature <em>Enum Feature</em>}'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the meta object for the attribute '<em>Enum Feature</em>'.
+	 * @see org.eclipse.xtext.enumrules.enums.ExistingType#getEnumFeature()
+	 * @see #getExistingType()
+	 * @generated
+	 */
+	EAttribute getExistingType_EnumFeature();
+
+	/**
+	 * Returns the meta object for the attribute '{@link org.eclipse.xtext.enumrules.enums.ExistingType#getStringFeature <em>String Feature</em>}'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the meta object for the attribute '<em>String Feature</em>'.
+	 * @see org.eclipse.xtext.enumrules.enums.ExistingType#getStringFeature()
+	 * @see #getExistingType()
+	 * @generated
+	 */
+	EAttribute getExistingType_StringFeature();
 
 	/**
 	 * Returns the meta object for enum '{@link org.eclipse.xtext.enumrules.enums.ExistingEnum <em>Existing Enum</em>}'.
@@ -100,6 +171,29 @@ public interface EnumsPackage extends EPackage {
 	 * @generated
 	 */
 	interface Literals {
+		/**
+		 * The meta object literal for the '{@link org.eclipse.xtext.enumrules.enums.impl.ExistingTypeImpl <em>Existing Type</em>}' class.
+		 * <!-- begin-user-doc -->
+		 * <!-- end-user-doc -->
+		 * @see org.eclipse.xtext.enumrules.enums.impl.ExistingTypeImpl
+		 * @see org.eclipse.xtext.enumrules.enums.impl.EnumsPackageImpl#getExistingType()
+		 * @generated
+		 */
+		EClass EXISTING_TYPE = eINSTANCE.getExistingType();
+		/**
+		 * The meta object literal for the '<em><b>Enum Feature</b></em>' attribute feature.
+		 * <!-- begin-user-doc -->
+		 * <!-- end-user-doc -->
+		 * @generated
+		 */
+		EAttribute EXISTING_TYPE__ENUM_FEATURE = eINSTANCE.getExistingType_EnumFeature();
+		/**
+		 * The meta object literal for the '<em><b>String Feature</b></em>' attribute feature.
+		 * <!-- begin-user-doc -->
+		 * <!-- end-user-doc -->
+		 * @generated
+		 */
+		EAttribute EXISTING_TYPE__STRING_FEATURE = eINSTANCE.getExistingType_StringFeature();
 		/**
 		 * The meta object literal for the '{@link org.eclipse.xtext.enumrules.enums.ExistingEnum <em>Existing Enum</em>}' enum.
 		 * <!-- begin-user-doc -->
