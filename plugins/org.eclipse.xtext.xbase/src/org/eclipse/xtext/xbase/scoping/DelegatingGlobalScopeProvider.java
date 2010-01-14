408e90d96b3a0f2835a1c73516284fd336ddf49d
sefftinge <sefftinge>
2010-01-14 16:07:14 +0000

    added linking to TypeParameters

--- plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/scoping/DelegatingGlobalScopeProvider.java
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/scoping/DelegatingGlobalScopeProvider.java
@@ -3,22 +3,34 @@ package org.eclipse.xtext.xbase.scoping;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
+import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.common.types.TypesPackage;
 import org.eclipse.xtext.common.types.xtext.AbstractTypeScopeProvider;
 import org.eclipse.xtext.scoping.IScope;
+import org.eclipse.xtext.scoping.Scopes;
 import org.eclipse.xtext.scoping.impl.DefaultGlobalScopeProvider;
+import org.eclipse.xtext.xbase.Function;
 
 import com.google.inject.Inject;
 
 public class DelegatingGlobalScopeProvider extends DefaultGlobalScopeProvider {
 	@Inject
 	private AbstractTypeScopeProvider typesScopeProvider;
-	
 
 	public IScope getScope(EObject context, EReference reference) {
-		if (TypesPackage.eINSTANCE.getType().isSuperTypeOf((EClass) reference.getEType())) {
-			return typesScopeProvider.getScope(context, reference);
+		if (TypesPackage.eINSTANCE.getType().isSuperTypeOf(
+				(EClass) reference.getEType())) {
+			return localTypes(context, typesScopeProvider.getScope(context,
+					reference));
 		}
 		return super.getScope(context, reference);
+	}
+
+	protected IScope localTypes(EObject context, IScope outerScope) {
+		Function function = EcoreUtil2.getContainerOfType(context,
+				Function.class);
+		if (function == null || function.getTypeParams().isEmpty())
+			return outerScope;
+		return Scopes.scopeFor(function.getTypeParams(), outerScope);
 	}
 }
--- tests/org.eclipse.xtext.xbase.tests/src/org/eclipse/xtext/xbase/tests/linking/LinkingTest.java
+++ tests/org.eclipse.xtext.xbase.tests/src/org/eclipse/xtext/xbase/tests/linking/LinkingTest.java
@@ -5,6 +5,7 @@ import org.eclipse.xtext.xbase.Function;
 import org.eclipse.xtext.xbase.tests.AbstractXbaseTestCase;
 import org.eclipse.xtext.xtype.FunctionTypeRef;
 import org.eclipse.xtext.xtype.SimpleTypeRef;
+import org.eclipse.xtext.xtype.TypeParamDeclaration;
 
 public class LinkingTest extends AbstractXbaseTestCase {
 	
@@ -24,6 +25,42 @@ public class LinkingTest extends AbstractXbaseTestCase {
 		assertEquals("java.lang.Boolean", returnType.getType().getCanonicalName());
 		SimpleTypeRef paramType = (SimpleTypeRef) type.getParamTypes().get(0);
 		assertEquals("java.lang.String", paramType.getType().getCanonicalName());
+	}
+	
+	public void testTypeParameterReference() throws Exception {
+		File file = file("<X> X foo(X x) : x;");
+		Function func = file.getFunctions().get(0);
+		SimpleTypeRef returnType = (SimpleTypeRef) func.getReturnType();
+		TypeParamDeclaration typeParamDecl = (TypeParamDeclaration) returnType.getType();
+		assertEquals("X", typeParamDecl.getCanonicalName());
+		assertEquals("X", typeParamDecl.getName());
+		
+		SimpleTypeRef paramType = (SimpleTypeRef) func.getDeclaredParams().get(0).getType();
+		assertEquals(typeParamDecl,paramType.getType());
+	}
+	
+	public void testTypeParameterReference_0() throws Exception {
+		File file = file("import java.lang.* <String> String foo(String x) : x;");
+		Function func = file.getFunctions().get(0);
+		SimpleTypeRef returnType = (SimpleTypeRef) func.getReturnType();
+		TypeParamDeclaration typeParamDecl = (TypeParamDeclaration) returnType.getType();
+		assertEquals("String", typeParamDecl.getCanonicalName());
+		assertEquals("String", typeParamDecl.getName());
+		
+		SimpleTypeRef paramType = (SimpleTypeRef) func.getDeclaredParams().get(0).getType();
+		assertEquals(typeParamDecl,paramType.getType());
+	}
+	
+	public void testTypeParameterReference_1() throws Exception {
+		File file = file("import java.lang.* <String> String foo(java.lang.String x) : x;");
+		Function func = file.getFunctions().get(0);
+		SimpleTypeRef returnType = (SimpleTypeRef) func.getReturnType();
+		TypeParamDeclaration typeParamDecl = (TypeParamDeclaration) returnType.getType();
+		assertEquals("String", typeParamDecl.getCanonicalName());
+		assertEquals("String", typeParamDecl.getName());
+		
+		SimpleTypeRef paramType = (SimpleTypeRef) func.getDeclaredParams().get(0).getType();
+		assertNotSame(typeParamDecl,paramType.getType());
 	}
 	
 }
