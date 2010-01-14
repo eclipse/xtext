4eb2d815c8ab01cde10c361347bd51fa417c8750
sefftinge <sefftinge>
2010-01-14 15:24:22 +0000

    enabled basic type references

---
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/scoping/DelegatingGlobalScopeProvider.java
@@ -0,0 +1,24 @@
+package org.eclipse.xtext.xbase.scoping;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.xtext.common.types.TypesPackage;
+import org.eclipse.xtext.common.types.xtext.AbstractTypeScopeProvider;
+import org.eclipse.xtext.scoping.IScope;
+import org.eclipse.xtext.scoping.impl.DefaultGlobalScopeProvider;
+
+import com.google.inject.Inject;
+
+public class DelegatingGlobalScopeProvider extends DefaultGlobalScopeProvider {
+	@Inject
+	private AbstractTypeScopeProvider typesScopeProvider;
+	
+
+	public IScope getScope(EObject context, EReference reference) {
+		if (TypesPackage.eINSTANCE.getType().isSuperTypeOf((EClass) reference.getEType())) {
+			return typesScopeProvider.getScope(context, reference);
+		}
+		return super.getScope(context, reference);
+	}
+}
