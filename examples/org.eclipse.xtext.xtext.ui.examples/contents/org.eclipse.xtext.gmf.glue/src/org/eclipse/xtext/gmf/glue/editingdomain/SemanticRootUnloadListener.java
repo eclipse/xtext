5b9712e95c7f3bf2cb053f82c02d1d01b9f838ff
jkohnlein <jkohnlein>
2010-01-15 09:33:17 +0000

    moving plugin to examples

---
+++ examples/org.eclipse.xtext.xtext.ui.examples/contents/org.eclipse.xtext.gmf.glue/src/org/eclipse/xtext/gmf/glue/editingdomain/SemanticRootUnloadListener.java
@@ -0,0 +1,88 @@
+/*******************************************************************************
+ * Copyright (c) 2009 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.gmf.glue.editingdomain;
+
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.transaction.NotificationFilter;
+import org.eclipse.emf.transaction.ResourceSetChangeEvent;
+import org.eclipse.emf.transaction.ResourceSetListener;
+import org.eclipse.emf.transaction.RollbackException;
+import org.eclipse.gef.EditPart;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramEditPart;
+import org.eclipse.xtext.resource.XtextResource;
+
+/**
+ * Reloads the semantic root element (the element of the diagram) on changes and
+ * refreshes the diagram. GMF does by default not listen to such events which
+ * can occur in an {@link XtextResource}Êif the document changes close to the
+ * root element.
+ * 
+ * Activate an instance of this in the {@link EditPart#activate()} method of the
+ * {@link DiagramEditPart}.
+ * 
+ * @author koehnlein
+ */
+public class SemanticRootUnloadListener implements ResourceSetListener {
+
+	private DiagramEditPart rootEditPart;
+	private EObject semanticRootElement;
+
+	public SemanticRootUnloadListener(DiagramEditPart rootEditPart) {
+		this.rootEditPart = rootEditPart;
+		this.semanticRootElement = rootEditPart.resolveSemanticElement();
+	}
+
+	public void activate() {
+		rootEditPart.getEditingDomain().addResourceSetListener(this);
+	}
+
+	public void deactivate() {
+		rootEditPart.getEditingDomain().removeResourceSetListener(this);
+	}
+
+	public NotificationFilter getFilter() {
+		return new NotificationFilter.Custom() {
+			@Override
+			public boolean matches(Notification notification) {
+				int featureID = notification.getFeatureID(Resource.class);
+				Object notifier = notification.getNotifier();
+				int eventType = notification.getEventType();
+				return notification.getOldValue() == semanticRootElement
+						&& featureID == Resource.RESOURCE__CONTENTS
+						&& (eventType == Notification.REMOVE || eventType == Notification.SET)
+						&& notifier instanceof Resource;
+			}
+		};
+	}
+
+	public boolean isAggregatePrecommitListener() {
+		return false;
+	}
+
+	public boolean isPostcommitOnly() {
+		return true;
+	}
+
+	public boolean isPrecommitOnly() {
+		return false;
+	}
+
+	public void resourceSetChanged(ResourceSetChangeEvent event) {
+		semanticRootElement = rootEditPart.resolveSemanticElement();
+		rootEditPart.refresh();
+	}
+
+	public Command transactionAboutToCommit(ResourceSetChangeEvent event)
+			throws RollbackException {
+		return null;
+	}
+
+}
---
+++ examples/org.eclipse.xtext.xtext.ui.examples/contents/org.eclipse.xtext.gmf.glue/src/org/eclipse/xtext/gmf/glue/editingdomain/XtextNodeModelReconciler.java
@@ -0,0 +1,147 @@
+/*******************************************************************************
+ * Copyright (c) 2009 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.gmf.glue.editingdomain;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.operations.IOperationHistory;
+import org.eclipse.core.commands.operations.IOperationHistoryListener;
+import org.eclipse.core.commands.operations.OperationHistoryEvent;
+import org.eclipse.emf.common.command.CommandStack;
+import org.eclipse.emf.common.notify.impl.AdapterImpl;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.emf.transaction.TransactionalEditingDomainEvent;
+import org.eclipse.emf.transaction.TransactionalEditingDomainListener;
+import org.eclipse.emf.transaction.TransactionalEditingDomain.Lifecycle;
+import org.eclipse.emf.transaction.util.TransactionUtil;
+import org.eclipse.emf.workspace.IWorkspaceCommandStack;
+import org.eclipse.gmf.runtime.common.core.command.ICommand;
+import org.eclipse.xtext.gmf.glue.Activator;
+import org.eclipse.xtext.parsetree.CompositeNode;
+import org.eclipse.xtext.parsetree.NodeAdapter;
+import org.eclipse.xtext.parsetree.NodeUtil;
+import org.eclipse.xtext.parsetree.reconstr.SerializerUtil;
+import org.eclipse.xtext.resource.XtextResource;
+
+/**
+ * Reconciles the node models of all XtextResources in a
+ * TransactionalEditingDomain with semantic changes.
+ * 
+ * @author koehnlein
+ */
+public class XtextNodeModelReconciler extends AdapterImpl implements
+		TransactionalEditingDomainListener, IOperationHistoryListener {
+
+	private TransactionalEditingDomain editingDomain;
+
+	private ChangeAggregatorAdapter changeAggregator;
+
+	private XtextNodeModelReconciler(TransactionalEditingDomain editingDomain) {
+		this.editingDomain = editingDomain;
+		Lifecycle lifecycle = TransactionUtil.getAdapter(editingDomain,
+				Lifecycle.class);
+		lifecycle.addTransactionalEditingDomainListener(this);
+		changeAggregator = new ChangeAggregatorAdapter();
+		editingDomain.getResourceSet().eAdapters().add(changeAggregator);
+		CommandStack commandStack = editingDomain.getCommandStack();
+		if (commandStack instanceof IWorkspaceCommandStack) {
+			IOperationHistory operationHistory = ((IWorkspaceCommandStack) commandStack)
+					.getOperationHistory();
+			operationHistory.addOperationHistoryListener(this);
+		}
+		changeAggregator.beginRecording();
+	}
+
+	public void editingDomainDisposing(TransactionalEditingDomainEvent event) {
+		changeAggregator.endRecording();
+		CommandStack commandStack = editingDomain.getCommandStack();
+		if (commandStack instanceof IWorkspaceCommandStack) {
+			IOperationHistory operationHistory = ((IWorkspaceCommandStack) commandStack)
+					.getOperationHistory();
+			operationHistory.removeOperationHistoryListener(this);
+		}
+		editingDomain.getResourceSet().eAdapters().remove(changeAggregator);
+		Lifecycle lifecycle = TransactionUtil.getAdapter(editingDomain,
+				Lifecycle.class);
+		lifecycle
+				.removeTransactionalEditingDomainListener(XtextNodeModelReconciler.this);
+	}
+
+	public void transactionClosed(TransactionalEditingDomainEvent event) {
+		// ignore
+	}
+
+	public void transactionClosing(TransactionalEditingDomainEvent event) {
+		// ignore
+	}
+
+	public void transactionInterrupted(TransactionalEditingDomainEvent event) {
+		// ignore
+	}
+
+	public void transactionStarted(TransactionalEditingDomainEvent event) {
+		// ignore
+	}
+
+	public void transactionStarting(TransactionalEditingDomainEvent event) {
+		// ignore
+	}
+
+	public static XtextNodeModelReconciler adapt(
+			TransactionalEditingDomain editingDomain) {
+		XtextNodeModelReconciler adapter = (XtextNodeModelReconciler) EcoreUtil
+				.getAdapter(editingDomain.getResourceSet().eAdapters(),
+						XtextNodeModelReconciler.class);
+		if (adapter == null) {
+			adapter = new XtextNodeModelReconciler(editingDomain);
+		}
+		return adapter;
+	}
+
+	public void historyNotification(OperationHistoryEvent event) {
+		int eventType = event.getEventType();
+		switch (eventType) {
+		case OperationHistoryEvent.DONE:
+		case OperationHistoryEvent.UNDONE:
+		case OperationHistoryEvent.REDONE:
+			changeAggregator.endRecording();
+			ICommand updateXtextResourceTextCommand = null;
+			for (EObject modificationRoot : changeAggregator
+					.getModificationRoots()) {
+				XtextResource xtextResource = (XtextResource) modificationRoot
+						.eResource();
+				NodeAdapter nodeAdapter = NodeUtil
+						.getNodeAdapter(modificationRoot);
+				CompositeNode parserNode = nodeAdapter.getParserNode();
+				SerializerUtil serializer = xtextResource.getSerializer();
+				String newText = serializer.serialize(modificationRoot);
+				ICommand newCommand = UpdateXtextResourceTextCommand
+						.createICommand(xtextResource, parserNode.getOffset(),
+								parserNode.getLength(), newText);
+				if (updateXtextResourceTextCommand == null) {
+					updateXtextResourceTextCommand = newCommand;
+				} else {
+					updateXtextResourceTextCommand.compose(newCommand);
+				}
+			}
+			try {
+				if (updateXtextResourceTextCommand != null) {
+					updateXtextResourceTextCommand.execute(null, null);
+				}
+			} catch (ExecutionException exc) {
+				Activator.logError(exc);
+			}
+			changeAggregator.beginRecording();
+			break;
+		default:
+			// ignore
+		}
+
+	}
+}
