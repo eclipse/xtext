01b97b440f1f11c8c4014b08f6cc4e8f07786e0e
sefftinge <sefftinge>
2010-01-20 07:56:42 +0000

    renaming

--- tests/org.eclipse.xtext.xbase.tests/src/org/eclipse/xtext/xbase/tests/typing/TypesServiceTest.java
+++ tests/org.eclipse.xtext.xbase.tests/src/org/eclipse/xtext/xbase/tests/typing/TypesServiceTest.java
@@ -5,7 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-package org.eclipse.xtext.xbase.tests.types;
+package org.eclipse.xtext.xbase.tests.typing;
 
 import org.eclipse.xtext.xbase.tests.AbstractXbaseTestCase;
 
