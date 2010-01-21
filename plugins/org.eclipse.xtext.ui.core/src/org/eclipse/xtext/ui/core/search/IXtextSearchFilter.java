6ec069420ce920ff69acb8be3a00c76b38345e97
jkohnlein <jkohnlein>
2010-01-21 09:30:54 +0000

    https://bugs.eclipse.org/bugs/show_bug.cgi?id=299907 Added support for unqualified names

--- plugins/org.eclipse.xtext.ui.core/src/org/eclipse/xtext/ui/core/search/IXtextSearchFilter.java
+++ plugins/org.eclipse.xtext.ui.core/src/org/eclipse/xtext/ui/core/search/IXtextSearchFilter.java
@@ -7,8 +7,9 @@
  *******************************************************************************/
 package org.eclipse.xtext.ui.core.search;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.CoreException;
@@ -18,10 +19,11 @@ import org.eclipse.xtext.resource.IEObjectDescription;
 import org.eclipse.xtext.ui.core.internal.Activator;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 
 /**
- * A filter to reject elements form the candidates in the global "Open Xtext Element" dialog.
- * Filters can be registered to the extension point <code>org.eclipse.xtext.ui.core.searchFilter</code>.  
+ * A filter to reject elements form the candidates in the global "Open Xtext Element" dialog. Filters can be registered
+ * to the extension point <code>org.eclipse.xtext.ui.core.searchFilter</code>.
  * 
  * @author koehnlein - Initial contribution and API
  */
@@ -32,8 +34,9 @@ public interface IXtextSearchFilter {
 	 * therefore be as specific as possible in the implementation of this method in order not to interfer with filters
 	 * from other languages.
 	 * 
-	 * @param element the element to be checked
-	 * @return <code>true</code> if the element should NOT appear in the dialog. 
+	 * @param element
+	 *            the element to be checked
+	 * @return <code>true</code> if the element should NOT appear in the dialog.
 	 */
 	boolean reject(IEObjectDescription element);
 
@@ -43,22 +46,49 @@ public interface IXtextSearchFilter {
 
 		public static final String EXTENSION_POINT_ID = Activator.PLUGIN_ID + ".searchFilter";
 
+		public static final String FILTER = "filter";
+
+		public static final String NAMESPACE_DELIMITER = "namespace_delimiter";
+
 		public static final String ATTRIBUTE = "class";
 
-		public static Collection<IXtextSearchFilter> allRegisteredFilters() {
+		public static final String VALUE = "value";
+		
+		public static final String DEFAULT_NAMESPACE_DELIMITER = ".";
+
+		public static Collection<IXtextSearchFilter> allFilters() {
 			IConfigurationElement[] configurationElements = Platform.getExtensionRegistry()
 					.getConfigurationElementsFor(EXTENSION_POINT_ID);
-			ArrayList<IXtextSearchFilter> filters = Lists.newArrayList();
+			List<IXtextSearchFilter> filters = Lists.newArrayList();
 			for (IConfigurationElement configurationElement : configurationElements) {
 				try {
-					IXtextSearchFilter filter = (IXtextSearchFilter) configurationElement
-							.createExecutableExtension(ATTRIBUTE);
-					filters.add(filter);
+					if (FILTER.equals(configurationElement.getName())) {
+						IXtextSearchFilter filter = (IXtextSearchFilter) configurationElement
+								.createExecutableExtension(ATTRIBUTE);
+						filters.add(filter);
+					}
 				} catch (CoreException e) {
 					LOG.error("Error instantiating IXtextSerachFilter from registry", e);
 				}
 			}
 			return filters;
+		}
+
+		public static Collection<String> allNamespaceDelimiters() {
+			IConfigurationElement[] configurationElements = Platform.getExtensionRegistry()
+					.getConfigurationElementsFor(EXTENSION_POINT_ID);
+			Set<String> delimiters = Sets.newHashSet();
+			delimiters.add(DEFAULT_NAMESPACE_DELIMITER);
+			for (IConfigurationElement configurationElement : configurationElements) {
+				if (NAMESPACE_DELIMITER.equals(configurationElement.getName())) {
+					String delimiter = configurationElement.getAttribute(VALUE);
+					if(delimiter.contains("*") || delimiter.contains("?")) {
+						LOG.error("Invalid namespace delimiter in " + configurationElement.getContributor().getName() + ": '*' and '?' not allowed.");
+					}
+					delimiters.add(delimiter);
+				}
+			}
+			return delimiters;
 		}
 	}
 }
