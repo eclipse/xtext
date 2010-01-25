a0bd38a150ada3c0f424abe2dacf863b521937fe
sefftinge <sefftinge>
2010-01-25 14:11:10 +0000

    avoid trigger parsing from bracket matching.

--- plugins/org.eclipse.xtext.ui.core/src/org/eclipse/xtext/ui/core/editor/bracketmatching/CharacterPairMatcher.java
+++ plugins/org.eclipse.xtext.ui.core/src/org/eclipse/xtext/ui/core/editor/bracketmatching/CharacterPairMatcher.java
@@ -7,39 +7,71 @@
  *******************************************************************************/
 package org.eclipse.xtext.ui.core.editor.bracketmatching;
 
-
+import org.eclipse.jface.text.DocumentEvent;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IDocumentListener;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.source.ICharacterPairMatcher;
+import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.core.editor.model.IXtextDocument;
+import org.eclipse.xtext.ui.core.editor.model.IXtextModelListener;
 
 import com.google.inject.Inject;
-
 
 /**
  * @author Sven Efftinge - Initial contribution and API
  */
-public class CharacterPairMatcher implements ICharacterPairMatcher {
+public class CharacterPairMatcher implements ICharacterPairMatcher, IDocumentListener, IXtextModelListener {
 
 	public void dispose() {
+		clear();
 	}
 
 	public void clear() {
+		this.document.removeDocumentListener(this);
+		this.document.removeModelListener(this);
+		this.changes = false;
 	}
-	
+
+	private IXtextDocument document;
+
 	@Inject
-	private IBracketMatcher bracketMatcher; 
-	
+	private IBracketMatcher bracketMatcher;
+
 	public IRegion match(IDocument iDocument, final int offset) {
-		if (iDocument instanceof IXtextDocument) {
-			return BracketMatchingUnitOfWork.match((IXtextDocument) iDocument, bracketMatcher, offset);
+		IXtextDocument doc = getDocument(iDocument);
+		if (!changes) {
+			return BracketMatchingUnitOfWork.match(doc, bracketMatcher, offset);
 		}
 		return null;
 	}
 
+	protected IXtextDocument getDocument(IDocument iDocument) {
+		if (this.document == null) {
+			this.document = (IXtextDocument) iDocument;
+			this.document.addDocumentListener(this);
+			this.document.addModelListener(this);
+		}
+		if (this.document != iDocument)
+			throw new IllegalStateException();
+		return this.document;
+	}
 
 	public int getAnchor() {
 		return ICharacterPairMatcher.LEFT;
+	}
+
+	private boolean changes = false;
+
+	public void documentAboutToBeChanged(DocumentEvent event) {
+	}
+
+	public void documentChanged(DocumentEvent event) {
+		changes = true;
+	}
+
+	public void modelChanged(XtextResource resource) {
+		changes = false;
 	}
 
 }
