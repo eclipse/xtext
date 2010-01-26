4873dc16b2be29c0076567e49257e2c8a1fc9f21
sefftinge <sefftinge>
2010-01-26 04:50:50 +0000

    added default behavior to ResourceSetBasedAllContainerStateProvider

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/resource/containers/ResourceSetBasedAllContainersStateProvider.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/resource/containers/ResourceSetBasedAllContainersStateProvider.java
@@ -7,26 +7,57 @@
  *******************************************************************************/
 package org.eclipse.xtext.resource.containers;
 
+import java.util.Collections;
+
+import org.apache.log4j.Logger;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.xtext.resource.IResourceDescriptions;
 import org.eclipse.xtext.resource.impl.ResourceSetBasedResourceDescriptions;
 
+import com.google.common.base.Function;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Multimaps;
+
 /**
  * @author Sven Efftinge - Initial contribution and API
  */
-public class ResourceSetBasedAllContainersStateProvider implements IAllContainersState.Provider{
+public class ResourceSetBasedAllContainersStateProvider implements IAllContainersState.Provider {
+
+	private final static Logger log = Logger.getLogger(ResourceSetBasedAllContainersStateProvider.class);
 
 	public IAllContainersState get(IResourceDescriptions context) {
 		if (context instanceof ResourceSetBasedResourceDescriptions) {
 			ResourceSetBasedResourceDescriptions resourceDescs = (ResourceSetBasedResourceDescriptions) context;
 			ResourceSet resourceSet = resourceDescs.getResourceSet();
-			IAllContainersState adapter = (IAllContainersState) EcoreUtil.getAdapter(resourceSet.eAdapters(), IAllContainersState.class);
-			if (adapter == null)
-				throw new IllegalStateException("No "+IAllContainersState.class.getName()+" installed.");
+			IAllContainersState adapter = (IAllContainersState) EcoreUtil.getAdapter(resourceSet.eAdapters(),
+					IAllContainersState.class);
+			if (adapter == null) {
+				return handleAdapterNotFound(resourceSet);
+			}
 			return adapter;
 		}
-		throw new IllegalStateException("Passed "+IResourceDescriptions.class.getName()+" not of type "+ResourceSetBasedResourceDescriptions.class.getName());
+		throw new IllegalStateException("Passed " + IResourceDescriptions.class.getName() + " not of type "
+				+ ResourceSetBasedResourceDescriptions.class.getName());
+	}
+
+	protected IAllContainersState handleAdapterNotFound(final ResourceSet resourceSet) {
+		if (log.isInfoEnabled())
+			log.info("No explicit container state set. Falling back to default.");
+		ResourceSetBasedAllContainersState containersState = new ResourceSetBasedAllContainersState();
+		String containerName = "all";
+		HashMultimap<String, URI> newHashMultimap = Multimaps.newHashMultimap();
+		newHashMultimap.putAll(containerName, Iterables.transform(resourceSet.getResources(),
+				new Function<Resource, URI>() {
+					public URI apply(Resource from) {
+						return resourceSet.getURIConverter().normalize(from.getURI());
+					}
+				}));
+		containersState.configure(Collections.singletonList(containerName), newHashMultimap);
+		return containersState;
 	}
 
 }
