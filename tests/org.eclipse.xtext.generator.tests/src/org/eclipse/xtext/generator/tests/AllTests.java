a69bc0d00fd5e0c65121a3bc6e264b9094a0c2a2
jkohnlein <jkohnlein>
2010-01-27 13:55:33 +0000

    https://bugs.eclipse.org/bugs/show_bug.cgi?id=300961 Find References on cross references and minor fixes

--- tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/generator/tests/AllTests.java
+++ tests/org.eclipse.xtext.generator.tests/src/org/eclipse/xtext/generator/tests/AllTests.java
@@ -155,7 +155,6 @@ public class AllTests {
       suite.addTestSuite(org.eclipse.xtext.scoping.ScopeTest.class);
       suite.addTestSuite(org.eclipse.xtext.scoping.ScopesTest.class);
       suite.addTestSuite(org.eclipse.xtext.scoping.impl.DeclarativeScopeProviderTest.class);
-      suite.addTestSuite(org.eclipse.xtext.scoping.impl.ProfilingTest.class);
       suite.addTestSuite(org.eclipse.xtext.scoping.impl.QualifiedNameScopeProviderTest.class);
       suite.addTestSuite(org.eclipse.xtext.scoping.impl.SimpleNameScopeProviderTest.class);
       suite.addTestSuite(org.eclipse.xtext.service.GenericModuleTest.class);
