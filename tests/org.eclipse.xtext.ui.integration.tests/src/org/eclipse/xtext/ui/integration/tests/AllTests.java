1a7324f7286c574df9d7bf49b9ab682600f0ea00
sefftinge <sefftinge>
2010-01-29 15:30:25 +0000

    removed unused class

--- tests/org.eclipse.xtext.ui.integration.tests/src/org/eclipse/xtext/ui/integration/tests/AllTests.java
+++ tests/org.eclipse.xtext.ui.integration.tests/src/org/eclipse/xtext/ui/integration/tests/AllTests.java
@@ -16,7 +16,6 @@ public class AllTests {
       suite.addTestSuite(org.eclipse.xtext.ui.core.resource.Storage2UriMapperTest.class);
       suite.addTestSuite(org.eclipse.xtext.ui.core.util.BundleClasspathUriResolverTest.class);
       suite.addTestSuite(org.eclipse.xtext.ui.core.util.JdtClasspathUriResolverTest.class);
-      suite.addTestSuite(org.eclipse.xtext.ui.core.util.JdtUtilTest.class);
       suite.addTestSuite(org.eclipse.xtext.ui.core.util.WorkspaceClasspathUriResolverTest.class);
       suite.addTestSuite(org.eclipse.xtext.ui.integration.tests.editor.autoedit.AutoEditTest.class);
       suite.addTestSuite(org.eclipse.xtext.ui.integration.tests.editor.bracketmatching.BracketMatcherTest.class);
