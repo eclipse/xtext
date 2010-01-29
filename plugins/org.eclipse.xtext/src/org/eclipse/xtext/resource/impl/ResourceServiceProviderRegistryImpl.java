bac4b26e4d6acdde2bcfae7ed70d98abccb16e68
sefftinge <sefftinge>
2010-01-29 18:26:52 +0000

    exclude too greedy contenthandlers

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/resource/impl/ResourceServiceProviderRegistryImpl.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/resource/impl/ResourceServiceProviderRegistryImpl.java
@@ -7,13 +7,18 @@
  *******************************************************************************/
 package org.eclipse.xtext.resource.impl;
 
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.ContentHandler;
+import org.eclipse.emf.ecore.resource.URIConverter;
+import org.eclipse.emf.ecore.resource.URIHandler;
+import org.eclipse.emf.ecore.resource.impl.ExtensibleURIConverterImpl;
 import org.eclipse.emf.ecore.resource.impl.ResourceFactoryRegistryImpl;
 import org.eclipse.xtext.resource.IResourceServiceProvider;
 
+import com.google.common.collect.Lists;
 import com.google.inject.Provider;
 
 /**
@@ -26,20 +31,46 @@ public class ResourceServiceProviderRegistryImpl implements IResourceServiceProv
 	 * @author Sven Efftinge - Initial contribution and API
 	 */
 	public static class InternalData extends ResourceFactoryRegistryImpl {
+
 		@SuppressWarnings({ "unchecked", "rawtypes" })
 		public IResourceServiceProvider getServiceProvider(URI uri, String contentType) {
-			Object object = getFactory(uri, protocolToFactoryMap, extensionToFactoryMap, contentTypeIdentifierToFactoryMap, contentType, false);
+			Object object = getFactory(uri, protocolToFactoryMap, extensionToFactoryMap,
+					contentTypeIdentifierToFactoryMap, contentType, false);
 			if (object instanceof Provider) {
-				return ((Provider<IResourceServiceProvider>)object).get();
+				return ((Provider<IResourceServiceProvider>) object).get();
 			}
 			return (IResourceServiceProvider) object;
+		}
+
+		private ExtensibleURIConverterImpl uriConverter;
+
+		@Override
+		protected synchronized URIConverter getURIConverter() {
+			if (this.uriConverter == null) {
+				List<ContentHandler> withoutPlatformDelegate = Lists.newArrayList();
+				for (ContentHandler contentHandler : ContentHandler.Registry.INSTANCE.contentHandlers()) {
+					if (!isTooEager(contentHandler))
+						withoutPlatformDelegate.add(contentHandler);
+				}
+				this.uriConverter = new ExtensibleURIConverterImpl(URIHandler.DEFAULT_HANDLERS, withoutPlatformDelegate);
+			}
+			return this.uriConverter;
+		}
+
+		private boolean isTooEager(ContentHandler contentHandler) {
+			try {
+				return contentHandler.canHandle(null);
+			} catch (RuntimeException e) {
+			} catch (AssertionError e) {
+			}
+			return false;
 		}
 	}
 
 	private final InternalData data = new InternalData();
 
 	public IResourceServiceProvider getResourceServiceProvider(URI uri, String contentType) {
-		if (uri==null)
+		if (uri == null)
 			return null;
 		return data.getServiceProvider(uri, contentType);
 	}
@@ -59,7 +90,5 @@ public class ResourceServiceProviderRegistryImpl implements IResourceServiceProv
 	public IResourceServiceProvider getResourceServiceProvider(URI uri) {
 		return getResourceServiceProvider(uri, ContentHandler.UNSPECIFIED_CONTENT_TYPE);
 	}
-	
-	
-	
+
 }
