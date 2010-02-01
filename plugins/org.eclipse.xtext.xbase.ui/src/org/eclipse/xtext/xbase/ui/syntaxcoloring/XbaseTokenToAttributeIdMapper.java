45e768f1353a349ea25395a10d911355ec2d90b8
sefftinge <sefftinge>
2010-02-01 14:26:25 +0000

    added syntax coloring and auto edit strategy

---
+++ plugins/org.eclipse.xtext.xbase.ui/src/org/eclipse/xtext/xbase/ui/syntaxcoloring/XbaseTokenToAttributeIdMapper.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2010 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.xbase.ui.syntaxcoloring;
+
+import org.eclipse.xtext.ui.common.editor.syntaxcoloring.DefaultHighlightingConfiguration;
+import org.eclipse.xtext.ui.common.editor.syntaxcoloring.antlr.DefaultAntlrTokenToAttributeIdMapper;
+
+/**
+ * @author Sven Efftinge - Initial contribution and API
+ */
+public class XbaseTokenToAttributeIdMapper extends DefaultAntlrTokenToAttributeIdMapper {
+	
+	@Override
+	protected String calculateId(String tokenName, int tokenType) {
+		if ("RULE_RICH_STRING".equals(tokenName)) {
+			return DefaultHighlightingConfiguration.STRING_ID;
+		}
+		return super.calculateId(tokenName, tokenType);
+	}
+}
