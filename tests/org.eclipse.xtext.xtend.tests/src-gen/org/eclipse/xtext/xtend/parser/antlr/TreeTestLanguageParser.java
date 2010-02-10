84ffa67a2956be8620dc2a645a7bf40a48455d8f
szarnekow <szarnekow>
2010-02-10 22:30:48 +0000

    Fix: Improve error messages when using separate lexer grammar - see https://bugs.eclipse.org/bugs/show_bug.cgi?id=299237

--- tests/org.eclipse.xtext.xtend.tests/src-gen/org/eclipse/xtext/xtend/parser/antlr/TreeTestLanguageParser.java
+++ tests/org.eclipse.xtext.xtend.tests/src-gen/org/eclipse/xtext/xtend/parser/antlr/TreeTestLanguageParser.java
@@ -25,6 +25,7 @@ public class TreeTestLanguageParser extends org.eclipse.xtext.parser.antlr.Abstr
 		tokenStream.setInitialHiddenTokens("RULE_WS", "RULE_ML_COMMENT", "RULE_SL_COMMENT");
 		org.eclipse.xtext.xtend.parser.antlr.internal.InternalTreeTestLanguageParser parser = createParser(tokenStream);
 		parser.setTokenTypeMap(getTokenDefProvider().getTokenDefMap());
+		parser.setSyntaxErrorProvider(getSyntaxErrorProvider());
 		try {
 			if(ruleName != null)
 				return parser.parse(ruleName);
