c883aee426228783e217c7083efb5019e5d88016
sefftinge <sefftinge>
2010-02-15 12:31:45 +0000

    improved error messages when forgot configuring references Genmodels (https://bugs.eclipse.org/bugs/show_bug.cgi?id=302150)

--- tests/org.eclipse.xtext.xtext.ui.tests/src/org/eclipse/xtext/xtext/ui/ecore2xtext/Ecore2XtextGenerator.java
+++ tests/org.eclipse.xtext.xtext.ui.tests/src/org/eclipse/xtext/xtext/ui/ecore2xtext/Ecore2XtextGenerator.java
@@ -5,7 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-package org.eclipse.xtext.xtext.ui;
+package org.eclipse.xtext.xtext.ui.ecore2xtext;
 
 import java.util.ArrayList;
 import java.util.Iterator;
