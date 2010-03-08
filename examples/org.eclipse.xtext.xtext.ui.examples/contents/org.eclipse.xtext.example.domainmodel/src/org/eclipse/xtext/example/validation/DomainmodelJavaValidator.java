961f17d4d0e7d2ec92b2466f419ec97985d3d498
jkohnlein <jkohnlein>
2010-03-08 13:47:10 +0000

    added example for semantic modification

--- examples/org.eclipse.xtext.xtext.ui.examples/contents/org.eclipse.xtext.example.domainmodel/src/org/eclipse/xtext/example/validation/DomainmodelJavaValidator.java
+++ examples/org.eclipse.xtext.xtext.ui.examples/contents/org.eclipse.xtext.example.domainmodel/src/org/eclipse/xtext/example/validation/DomainmodelJavaValidator.java
@@ -8,6 +8,7 @@ import org.eclipse.xtext.validation.Check;
 public class DomainmodelJavaValidator extends AbstractDomainmodelJavaValidator {
 
     public static final String INVALID_TYPE_NAME = "org.eclipse.xtext.DomainModel.InvalidTypeName";
+    public static final String INVALID_FEATURE_NAME = "org.eclipse.xtext.DomainModel.InvalidFeatureName";
 
     @Check
     public void checkTypeNameStartsWithCapital(Type type) {
@@ -19,7 +20,7 @@ public class DomainmodelJavaValidator extends AbstractDomainmodelJavaValidator {
     @Check
     public void checkFeatureNameStartsWithLowercase(Feature feature) {
         if (!Character.isLowerCase(feature.getName().charAt(0))) {
-            warning("Name should start with a lowercase", DomainmodelPackage.FEATURE__NAME);
+            warning("Name should start with a lowercase", DomainmodelPackage.FEATURE__NAME, INVALID_FEATURE_NAME, feature.getName());
         }
     }
 }
