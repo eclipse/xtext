f8c282b33fc3e794408d2ffb1db478de074ff2c2
pfriese <pfriese>
2010-03-12 11:09:21 +0000

    RESOLVED - bug 282387: [Outline] Provide "quick outline" https://bugs.eclipse.org/bugs/show_bug.cgi?id=282387

---
+++ plugins/org.eclipse.xtext.generator/src/org/eclipse/xtext/ui/generator/outline/QuickOutlineFragment.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2010 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.ui.generator.outline;
+
+import org.eclipse.xtext.Grammar;
+import org.eclipse.xtext.generator.AbstractGeneratorFragment;
+
+/**
+ * @author Peter Friese - Initial contribution and API
+ */
+public class QuickOutlineFragment extends AbstractGeneratorFragment {
+	
+	@Override
+	public String[] getRequiredBundlesUi(Grammar grammar) {
+		return new String[]{
+				"org.eclipse.xtext.ui"
+		};
+	}
+
+}
