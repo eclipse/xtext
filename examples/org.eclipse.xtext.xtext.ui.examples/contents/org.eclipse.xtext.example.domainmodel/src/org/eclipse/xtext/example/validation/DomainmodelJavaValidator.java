f62f07dc5c59be197192ec1541e1fed96eb63ba1
sefftinge <sefftinge>
2010-03-30 12:05:38 +0000

    improved layout

--- examples/org.eclipse.xtext.xtext.ui.examples/contents/org.eclipse.xtext.example.domainmodel/src/org/eclipse/xtext/example/validation/DomainmodelJavaValidator.java
+++ examples/org.eclipse.xtext.xtext.ui.examples/contents/org.eclipse.xtext.example.domainmodel/src/org/eclipse/xtext/example/validation/DomainmodelJavaValidator.java
@@ -8,19 +8,23 @@ import org.eclipse.xtext.validation.Check;
 public class DomainmodelJavaValidator extends AbstractDomainmodelJavaValidator {
 
     public static final String INVALID_TYPE_NAME = "org.eclipse.xtext.DomainModel.InvalidTypeName";
-    public static final String INVALID_FEATURE_NAME = "org.eclipse.xtext.DomainModel.InvalidFeatureName";
-
     @Check
     public void checkTypeNameStartsWithCapital(Type type) {
         if (!Character.isUpperCase(type.getName().charAt(0))) {
-            warning("Name should start with a capital", DomainmodelPackage.TYPE__NAME, INVALID_TYPE_NAME, type.getName());
+            warning("Name should start with a capital", 
+            		DomainmodelPackage.TYPE__NAME, 
+            		INVALID_TYPE_NAME, 
+            		type.getName());
         }
     }
 
+    public static final String INVALID_FEATURE_NAME = "org.eclipse.xtext.DomainModel.InvalidFeatureName";
     @Check
     public void checkFeatureNameStartsWithLowercase(Feature feature) {
         if (!Character.isLowerCase(feature.getName().charAt(0))) {
-            warning("Name should start with a lowercase", DomainmodelPackage.FEATURE__NAME, INVALID_FEATURE_NAME, feature.getName());
+            warning("Name should start with a lowercase", 
+            		DomainmodelPackage.FEATURE__NAME, 
+            		INVALID_FEATURE_NAME, feature.getName());
         }
     }
 }
