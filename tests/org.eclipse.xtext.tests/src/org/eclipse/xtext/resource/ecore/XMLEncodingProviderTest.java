6d3ee617817b21a66ffc8371f7e3938e44e40a06
jkohnlein <jkohnlein>
2010-04-27 08:43:47 +0000

    refactored XML encoding test

--- tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/resource/ecore/XMLEncodingProviderTest.java
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/resource/ecore/XMLEncodingProviderTest.java
@@ -7,20 +7,37 @@
  *******************************************************************************/
 package org.eclipse.xtext.resource.ecore;
 
+import junit.framework.TestCase;
+
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.xtext.ecore.XMLEncodingProvider;
-
-import junit.framework.TestCase;
+import org.eclipse.xtext.resource.ClassloaderClasspathUriResolver;
+import org.eclipse.xtext.resource.IClasspathUriResolver;
 
 /**
  * @author Jan Koehnlein - Initial contribution and API
  */
 public class XMLEncodingProviderTest extends TestCase {
 	
+	private String packagePath;
+	private IClasspathUriResolver resolver;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		packagePath = getClass().getPackage().getName().replaceAll("\\.", "/");
+		resolver = new ClassloaderClasspathUriResolver();
+	}
+
 	public void testName() throws Exception {
 		XMLEncodingProvider xmlEncodingProvider = new XMLEncodingProvider();
-		assertEquals("UTF-8", xmlEncodingProvider.getEncoding(URI.createFileURI("resources/utf8.ecore")));
-		assertEquals("ISO-8859-1", xmlEncodingProvider.getEncoding(URI.createFileURI("resources/iso-8859-1.ecore")));
-		assertEquals("UTF-16", xmlEncodingProvider.getEncoding(URI.createFileURI("resources/utf16.ecore")));
+		assertEquals("UTF-8", xmlEncodingProvider.getEncoding(getURI("utf8.ecore")));
+		assertEquals("ISO-8859-1", xmlEncodingProvider.getEncoding(getURI("iso-8859-1.ecore")));
+		assertEquals("UTF-16", xmlEncodingProvider.getEncoding(getURI("utf16.ecore")));
+	}
+	
+	protected URI getURI(String fileName) {
+		URI classpathURI = URI.createURI("classpath:/" + packagePath + "/" + fileName);
+		return resolver.resolve(this, classpathURI);
 	}
 }
