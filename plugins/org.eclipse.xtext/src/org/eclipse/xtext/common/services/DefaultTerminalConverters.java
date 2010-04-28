1a02a0258525ab338b33afe441435363156b2258
meysholdt <meysholdt>
2010-04-28 12:44:56 +0000

    toString() of the DefaultValueConverters now throws appropriate exceptions for invalid values.

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/common/services/DefaultTerminalConverters.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/common/services/DefaultTerminalConverters.java
@@ -14,76 +14,127 @@ import org.eclipse.xtext.conversion.IValueConverter;
 import org.eclipse.xtext.conversion.ValueConverter;
 import org.eclipse.xtext.conversion.ValueConverterException;
 import org.eclipse.xtext.conversion.impl.AbstractDeclarativeValueConverterService;
-import org.eclipse.xtext.conversion.impl.AbstractNullSafeConverter;
+import org.eclipse.xtext.conversion.impl.AbstractValueConverter;
 import org.eclipse.xtext.parsetree.AbstractNode;
 import org.eclipse.xtext.util.Strings;
 
+import com.google.common.base.Function;
+import com.google.common.base.Join;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Sets;
 
 /**
  * Simple converters for Strings, Integers and IDs.
  */
 public class DefaultTerminalConverters extends AbstractDeclarativeValueConverterService {
 
-	@ValueConverter(rule = "ID")
-	public IValueConverter<String> ID() {
-		return new AbstractNullSafeConverter<String>() {
-			
-			private Set<String> allKeywords = ImmutableSet.copyOf(GrammarUtil.getAllKeywords(getGrammar()));
-			
-			@Override
-			protected String internalToValue(String string, AbstractNode node) {
-				return string.startsWith("^") ? string.substring(1) : string;
-			}
+	public static class IDValueConverter extends AbstractValueConverter<String> {
 
-			@Override
-			protected String internalToString(String value) {
-				if (allKeywords.contains(value)) {
-					return "^" + value;
-				}
-				return value;
+		private Set<String> toEscape;
+
+		public IDValueConverter(Set<String> toEscape) {
+			super();
+			this.toEscape = toEscape;
+		}
+
+		protected Set<String> getToEscape() {
+			return toEscape;
+		}
+
+		public String toString(String value) {
+			if (value == null)
+				throw new ValueConverterException("ID may not be null.", null, null);
+			if (value.length() == 0)
+				throw new ValueConverterException("ID may not be empty.", null, null);
+			Set<Character> invalidChars = null;
+			for (int i = 0; i < value.length(); i++) {
+				char c = value.charAt(i);
+				if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+					continue;
+				if (i != 0 && c >= '0' && c <= '9')
+					continue;
+				if (c == '_')
+					continue;
+				if (invalidChars == null)
+					invalidChars = Sets.newHashSet();
+				invalidChars.add(c);
 			}
-		};
+			if (invalidChars != null) {
+				String chars = Join.join(", ", Iterables.transform(invalidChars, new Function<Character, String>() {
+					public String apply(Character from) {
+						return "'" + from + "' (0x" + Integer.toHexString(from) + ")";
+					}
+				}));
+				throw new ValueConverterException("ID '" + value + "' contains invalid characters: " + chars, null,
+						null);
+			}
+			if (toEscape.contains(value)) {
+				return "^" + value;
+			}
+			return value;
+		}
+
+		public String toValue(String string, AbstractNode node) {
+			if (string == null)
+				return null;
+			return string.startsWith("^") ? string.substring(1) : string;
+		}
 	}
 
-	@ValueConverter(rule = "STRING")
-	public IValueConverter<String> STRING() {
-		return new AbstractNullSafeConverter<String>() {
-			@Override
-			protected String internalToValue(String string, AbstractNode node) {
-				try {
-					return Strings.convertFromJavaString(string.substring(1, string.length() - 1), false);
-				} catch(IllegalArgumentException e) {
-					throw new ValueConverterException(e.getMessage(), node, e);
-				}
-			}
+	public static class INTValueConverter extends AbstractValueConverter<Integer> {
 
-			@Override
-			protected String internalToString(String value) {
-				return '"' + Strings.convertToJavaString(value, false) + '"';
+		public String toString(Integer value) {
+			if (value == null)
+				throw new ValueConverterException("INT-value may not be null. (null indeed, zero is ok)", null, null);
+			if (value < 0)
+				throw new ValueConverterException("INT-value may not be negative. (value:" + value + ").", null, null);
+			return value.toString();
+		}
+
+		public Integer toValue(String string, AbstractNode node) {
+			if (Strings.isEmpty(string))
+				throw new ValueConverterException("Couldn't convert empty string to int.", node, null);
+			try {
+				return Integer.valueOf(string);
+			} catch (NumberFormatException e) {
+				throw new ValueConverterException("Couldn't convert '" + string + "' to int.", node, e);
 			}
-		};
+		}
+
+	}
+
+	public static class STRINGValueConverter extends AbstractValueConverter<String> {
+		public String toString(String value) {
+			if (value == null)
+				throw new ValueConverterException("STRING-value may not be null.", null, null);
+			return '"' + Strings.convertToJavaString(value, false) + '"';
+		}
+
+		public String toValue(String string, AbstractNode node) {
+			if (string == null)
+				return null;
+			try {
+				return Strings.convertFromJavaString(string.substring(1, string.length() - 1), false);
+			} catch (IllegalArgumentException e) {
+				throw new ValueConverterException(e.getMessage(), node, e);
+			}
+		}
+	}
+
+	@ValueConverter(rule = "ID")
+	public IValueConverter<String> ID() {
+		return new IDValueConverter(ImmutableSet.copyOf(GrammarUtil.getAllKeywords(getGrammar())));
 	}
 
 	@ValueConverter(rule = "INT")
 	public IValueConverter<Integer> INT() {
-		return new IValueConverter<Integer>() {
-			
-			public Integer toValue(String string, AbstractNode node) {
-				if (Strings.isEmpty(string))
-					throw new ValueConverterException("Couldn't convert empty string to int", node, null);
-				try {
-					return Integer.valueOf(string);
-				} catch (NumberFormatException e) {
-					throw new ValueConverterException("Couldn't convert '"+string+"' to int", node, e);
-				}
-			}
+		return new INTValueConverter();
+	}
 
-			public String toString(Integer value) {
-				return value.toString();
-			}
-
-		};
+	@ValueConverter(rule = "STRING")
+	public IValueConverter<String> STRING() {
+		return new STRINGValueConverter();
 	}
 
 }
