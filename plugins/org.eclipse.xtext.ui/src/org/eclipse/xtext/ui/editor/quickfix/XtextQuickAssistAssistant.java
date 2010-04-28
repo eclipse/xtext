99c064c6b85e275ef91d44ae4ef84be9e43a9e8d
szarnekow <szarnekow>
2010-04-28 10:09:21 +0000

    Fix: Strange selection when QuickAssist is invoked - see https://bugs.eclipse.org/bugs/show_bug.cgi?id=309939

--- plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/quickfix/XtextQuickAssistAssistant.java
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/quickfix/XtextQuickAssistAssistant.java
@@ -1,34 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2010 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package org.eclipse.xtext.ui.editor.quickfix;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 import org.eclipse.jface.text.AbstractReusableInformationControlCreator;
-import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.DefaultInformationControl;
-import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.IInformationControl;
-import org.eclipse.jface.text.IInformationControlCreator;
-import org.eclipse.jface.text.Position;
 import org.eclipse.jface.text.DefaultInformationControl.IInformationPresenter;
+import org.eclipse.jface.text.IInformationControl;
+import org.eclipse.jface.text.Position;
+import org.eclipse.jface.text.contentassist.ContentAssistEvent;
+import org.eclipse.jface.text.contentassist.ICompletionListener;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
-import org.eclipse.jface.text.contentassist.ICompletionProposalExtension3;
-import org.eclipse.jface.text.contentassist.IContextInformation;
-import org.eclipse.jface.text.quickassist.IQuickAssistInvocationContext;
-import org.eclipse.jface.text.quickassist.IQuickAssistProcessor;
 import org.eclipse.jface.text.quickassist.QuickAssistAssistant;
-import org.eclipse.jface.text.source.Annotation;
-import org.eclipse.jface.text.source.IAnnotationModel;
-import org.eclipse.swt.graphics.Image;
+import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.texteditor.MarkerAnnotation;
-import org.eclipse.xtext.ui.MarkerUtil;
-import org.eclipse.xtext.ui.editor.model.IXtextDocument;
-import org.eclipse.xtext.ui.editor.model.edit.IssueUtil;
-import org.eclipse.xtext.ui.editor.validation.XtextAnnotation;
-import org.eclipse.xtext.validation.Issue;
 
 import com.google.inject.Inject;
 
@@ -39,162 +30,63 @@ import com.google.inject.Inject;
  */
 public class XtextQuickAssistAssistant extends QuickAssistAssistant {
 
-	public static class XtextCompletionProposalAdapter implements ICompletionProposal, ICompletionProposalExtension3 {
-
-		private Position pos;
-		private IssueResolution resolution;
-		private Image image;
-
-		public XtextCompletionProposalAdapter(Position pos, IssueResolution resolution, Image image) {
-			this.pos = pos;
-			this.resolution = resolution;
-			this.image = image;
-		}
-
-		public void apply(IDocument document) {
-			resolution.apply();
-		}
-
-		public Point getSelection(IDocument document) {
-			return new Point(pos.offset, 0);
-		}
-
-		public String getAdditionalProposalInfo() {
-			return resolution.getDescription();
-		}
-
-		public String getDisplayString() {
-			return resolution.getLabel();
-		}
-
-		public Image getImage() {
-			return image;
-		}
-
-		public IContextInformation getContextInformation() {
-			return null;
-		}
-
-		public IInformationControlCreator getInformationControlCreator() {
-			return null;
-		}
-
-		public int getPrefixCompletionStart(IDocument document, int completionOffset) {
-			return 0;
-		}
-
-		public CharSequence getPrefixCompletionText(IDocument document, int completionOffset) {
-			return null;
-		}
-
-	}
-
-	public static class XtextQuickAssistProcessor extends AbstractIssueResolutionProviderAdapter implements
-			IQuickAssistProcessor {
-
-		private final IssueUtil issueUtil;
-
-		private final MarkerUtil markerUtil;
-
-		@Inject
-		public XtextQuickAssistProcessor(IssueUtil issueUtil, MarkerUtil markerUtil) {
-			this.issueUtil = issueUtil;
-			this.markerUtil = markerUtil;
-		}
-
-		private String errorMessage;
-
-		public String getErrorMessage() {
-			return errorMessage;
-		}
-
-		public boolean canFix(Annotation annotation) {
-			if (annotation.isMarkedDeleted())
-				return false;
-
-			// non-persisted annotation
-			if (annotation instanceof XtextAnnotation) {
-				XtextAnnotation a = (XtextAnnotation) annotation;
-				return getResolutionProvider().hasResolutionFor(a.getIssueCode());
-			}
-
-			// persisted markerAnnotation
-			if (annotation instanceof MarkerAnnotation) {
-				MarkerAnnotation markerAnnotation = (MarkerAnnotation) annotation;
-				if (!markerAnnotation.isQuickFixableStateSet())
-					markerAnnotation.setQuickFixable(getResolutionProvider().hasResolutionFor(
-							markerUtil.getCode(markerAnnotation)));
-
-				return markerAnnotation.isQuickFixable();
-			}
-
-			return false;
-		}
-
-		public boolean canAssist(IQuickAssistInvocationContext invocationContext) {
-			return false;
-		}
-
-		public ICompletionProposal[] computeQuickAssistProposals(IQuickAssistInvocationContext invocationContext) {
-			if (invocationContext.getSourceViewer() == null)
-				return new ICompletionProposal[0];
-			final IDocument d = invocationContext.getSourceViewer().getDocument();
-			if (!(d instanceof IXtextDocument))
-				return new ICompletionProposal[0];
-			final IXtextDocument document = (IXtextDocument) d;
-
-			final IAnnotationModel amodel = invocationContext.getSourceViewer().getAnnotationModel();
-
-			List<ICompletionProposal> result = new ArrayList<ICompletionProposal>();
-			final int offset = invocationContext.getOffset();
-			for (Iterator<?> it = amodel.getAnnotationIterator(); it.hasNext();) {
-				Object key = it.next();
-				if (!(key instanceof Annotation))
-					continue;
-
-				Annotation annotation = (Annotation) key;
-				if (!canFix(annotation))
-					continue;
-
-				final Issue issue = issueUtil.getIssueFromAnnotation(annotation);
-				if (issue == null)
-					continue;
-
-				Iterable<IssueResolution> resolutions = getResolutions(issue, document);
-				if (!resolutions.iterator().hasNext())
-					continue;
-
-				Position pos = amodel.getPosition(annotation);
-				if (pos != null) {
-					try {
-						int line = document.getLineOfOffset(pos.getOffset());
-						int start = pos.getOffset();
-						String delim = document.getLineDelimiter(line);
-						int delimLength = delim != null ? delim.length() : 0;
-						int end = document.getLineLength(line) + start - delimLength;
-						if (offset >= start && offset <= end) {
-							for (IssueResolution resolution : resolutions)
-								result.add(new XtextCompletionProposalAdapter(pos, resolution, getImage(resolution)));
-						}
-					} catch (BadLocationException e) {
-						errorMessage = e.getMessage();
-					}
-				}
-			}
-			return result.toArray(new ICompletionProposal[result.size()]);
-		}
-
-	}
+	protected ISourceViewer viewer;
+	protected Position storedPosition;
 
 	@Inject
 	public XtextQuickAssistAssistant(XtextQuickAssistProcessor processor) {
 		setQuickAssistProcessor(processor);
-		setInformationControlCreator(new AbstractReusableInformationControlCreator() {
-			@Override
-			public IInformationControl doCreateInformationControl(Shell parent) {
-				return new DefaultInformationControl(parent, (IInformationPresenter) null);
+		setInformationControlCreator(createInformationControlCreator());
+		setRestoreSelection();
+	}
+
+	protected void setRestoreSelection() {
+		addCompletionListener(new ICompletionListener() {
+
+			public void assistSessionStarted(ContentAssistEvent event) {
+				Point selectedRange = viewer.getSelectedRange();
+				storePosition(selectedRange.x, selectedRange.y);
+			}
+
+			public void assistSessionEnded(ContentAssistEvent event) {
+				restorePosition();
+			}
+
+			public void selectionChanged(ICompletionProposal proposal, boolean smartToggle) {
 			}
 		});
 	}
 
+	protected AbstractReusableInformationControlCreator createInformationControlCreator() {
+		return new AbstractReusableInformationControlCreator() {
+			@Override
+			public IInformationControl doCreateInformationControl(Shell parent) {
+				return new DefaultInformationControl(parent, (IInformationPresenter) null);
+			}
+		};
+	}
+
+	@Override
+	public void install(ISourceViewer sourceViewer) {
+		super.install(sourceViewer);
+		viewer = sourceViewer;
+	}
+	
+	@Override
+	public void uninstall() {
+		viewer = null;
+		super.uninstall();
+	}
+
+	private void restorePosition() {
+		if (storedPosition != null && !storedPosition.isDeleted() && viewer.getDocument() != null) {
+			viewer.setSelectedRange(storedPosition.offset, storedPosition.length);
+			viewer.revealRange(storedPosition.offset, storedPosition.length);
+		}
+		storedPosition = null;
+	}
+
+	private void storePosition(int currOffset, int currLength) {
+		storedPosition = new Position(currOffset, currLength);
+	}
 }
---
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/quickfix/XtextQuickAssistProcessor.java
@@ -0,0 +1,179 @@
+/*******************************************************************************
+ * Copyright (c) 2010 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.ui.editor.quickfix;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.Position;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.jface.text.quickassist.IQuickAssistInvocationContext;
+import org.eclipse.jface.text.quickassist.IQuickAssistProcessor;
+import org.eclipse.jface.text.source.Annotation;
+import org.eclipse.jface.text.source.IAnnotationModel;
+import org.eclipse.jface.text.source.IAnnotationModelExtension2;
+import org.eclipse.jface.text.source.ISourceViewer;
+import org.eclipse.ui.texteditor.MarkerAnnotation;
+import org.eclipse.xtext.ui.MarkerUtil;
+import org.eclipse.xtext.ui.editor.model.IXtextDocument;
+import org.eclipse.xtext.ui.editor.model.edit.IssueUtil;
+import org.eclipse.xtext.ui.editor.validation.XtextAnnotation;
+import org.eclipse.xtext.validation.Issue;
+
+import com.google.inject.Inject;
+import com.google.inject.internal.Lists;
+
+public class XtextQuickAssistProcessor extends AbstractIssueResolutionProviderAdapter implements
+		IQuickAssistProcessor {
+
+	@Inject
+	private IssueUtil issueUtil;
+
+	@Inject
+	private MarkerUtil markerUtil;
+
+	private String errorMessage;
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public boolean canFix(Annotation annotation) {
+		if (annotation.isMarkedDeleted())
+			return false;
+
+		// non-persisted annotation
+		if (annotation instanceof XtextAnnotation) {
+			XtextAnnotation a = (XtextAnnotation) annotation;
+			return getResolutionProvider().hasResolutionFor(a.getIssueCode());
+		}
+
+		// persisted markerAnnotation
+		if (annotation instanceof MarkerAnnotation) {
+			MarkerAnnotation markerAnnotation = (MarkerAnnotation) annotation;
+			if (!markerAnnotation.isQuickFixableStateSet())
+				markerAnnotation.setQuickFixable(getResolutionProvider().hasResolutionFor(
+						markerUtil.getCode(markerAnnotation)));
+			return markerAnnotation.isQuickFixable();
+		}
+		return false;
+	}
+
+	public boolean canAssist(IQuickAssistInvocationContext invocationContext) {
+		return false;
+	}
+
+	public ICompletionProposal[] computeQuickAssistProposals(IQuickAssistInvocationContext invocationContext) {
+		ISourceViewer sourceViewer = invocationContext.getSourceViewer();
+		if (sourceViewer == null)
+			return new ICompletionProposal[0];
+		final IDocument document = sourceViewer.getDocument();
+		if (!(document instanceof IXtextDocument))
+			return new ICompletionProposal[0];
+		final IXtextDocument xtextDocument = (IXtextDocument) document;
+		final IAnnotationModel annotationModel = sourceViewer.getAnnotationModel();
+		List<ICompletionProposal> result = Lists.newArrayList();
+		try {
+			final int offset = invocationContext.getOffset();
+			Annotation annotation = getApplicableAnnotation(xtextDocument, annotationModel, offset);
+			if (annotation != null) {
+				final Issue issue = issueUtil.getIssueFromAnnotation(annotation);
+				if (issue != null) {
+					Iterable<IssueResolution> resolutions = getResolutions(issue, xtextDocument);
+					if (resolutions.iterator().hasNext()) {
+						Position pos = annotationModel.getPosition(annotation);
+						for (IssueResolution resolution : resolutions)
+							result.add(create(pos, resolution));
+						sourceViewer.setSelectedRange(pos.getOffset(), pos.getLength());
+					}
+				}
+			}
+		} catch (BadLocationException e) {
+			errorMessage = e.getMessage();
+		}
+		sortQuickfixes(result);
+		return result.toArray(new ICompletionProposal[result.size()]);
+	}
+	
+	protected ICompletionProposal create(Position posisition, IssueResolution resolution) {
+		return new QuickAssistCompletionProposal(posisition, resolution, getImage(resolution));
+	}
+	
+	protected void sortQuickfixes(List<ICompletionProposal> quickFixes) {
+		Collections.sort(quickFixes, new Comparator<ICompletionProposal>() {
+			public int compare(ICompletionProposal o1, ICompletionProposal o2) {
+				return o1.getDisplayString().compareTo(o2.getDisplayString());
+			}
+		});
+	}
+
+	protected Annotation getApplicableAnnotation(final IXtextDocument document, final IAnnotationModel annotationModel,
+			final int offset) throws BadLocationException {
+		final int line = document.getLineOfOffset(offset);
+		final String delim = document.getLineDelimiter(line);
+		final int delimLength = delim != null ? delim.length() : 0;
+		final int lineLength = document.getLineLength(line) - delimLength;
+		final int lineOffset = document.getLineOffset(line);
+
+		Iterator<?> iterator;
+		if (annotationModel instanceof IAnnotationModelExtension2)
+			iterator = ((IAnnotationModelExtension2) annotationModel).getAnnotationIterator(lineOffset, lineLength, true,
+					true);
+		else
+			iterator = annotationModel.getAnnotationIterator();
+
+		Annotation actualAnnotation = null;
+		int nearestAnnotationOffset = Integer.MAX_VALUE;
+		Annotation firstAnnotation = null;
+		int offsetOfFirstAnnotation = Integer.MAX_VALUE;
+
+		while (iterator.hasNext()) {
+			Object key = iterator.next();
+			if (!(key instanceof Annotation))
+				continue;
+
+			Annotation annotationTemp = (Annotation) key;
+			if (!canFix(annotationTemp))
+				continue;
+
+			Position pos = annotationModel.getPosition(annotationTemp);
+			if (pos == null)
+				continue;
+
+			if (pos.overlapsWith(lineOffset, lineLength)) {
+				if (pos.getOffset() < offsetOfFirstAnnotation) {
+					offsetOfFirstAnnotation = pos.getOffset();
+					firstAnnotation = annotationTemp;
+				}
+
+				int distanceToOffset = offset - pos.getOffset();
+				if (distanceToOffset >= 0 && distanceToOffset < nearestAnnotationOffset) {
+					actualAnnotation = annotationTemp;
+					nearestAnnotationOffset = distanceToOffset;
+				}
+			}
+		}
+		// choose the first annotation if there is no better selection available
+		if (actualAnnotation == null) {
+			actualAnnotation = firstAnnotation;
+		}
+		return actualAnnotation;
+	}
+	
+	public IssueUtil getIssueUtil() {
+		return issueUtil;
+	}
+	
+	public MarkerUtil getMarkerUtil() {
+		return markerUtil;
+	}
+}
