4344cb221c7b8fd85ec0de381cb359ec43abda73
szarnekow <szarnekow>
2010-04-28 10:09:21 +0000

    Fix: Strange selection when QuickAssist is invoked - see https://bugs.eclipse.org/bugs/show_bug.cgi?id=309939

---
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/quickfix/QuickAssistCompletionProposal.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * Copyright (c) 2010 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.ui.editor.quickfix;
+
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IInformationControlCreator;
+import org.eclipse.jface.text.Position;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.jface.text.contentassist.ICompletionProposalExtension3;
+import org.eclipse.jface.text.contentassist.IContextInformation;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
+
+public class QuickAssistCompletionProposal implements ICompletionProposal, ICompletionProposalExtension3 {
+
+	private Position pos;
+	private IssueResolution resolution;
+	private Image image;
+
+	public QuickAssistCompletionProposal(Position pos, IssueResolution resolution, Image image) {
+		this.pos = pos;
+		this.resolution = resolution;
+		this.image = image;
+	}
+
+	public void apply(IDocument document) {
+		resolution.apply();
+	}
+
+	public Point getSelection(IDocument document) {
+		return new Point(pos.offset, 0);
+	}
+
+	public String getAdditionalProposalInfo() {
+		return resolution.getDescription();
+	}
+
+	public String getDisplayString() {
+		return resolution.getLabel();
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+	public IContextInformation getContextInformation() {
+		return null;
+	}
+
+	public IInformationControlCreator getInformationControlCreator() {
+		return null;
+	}
+
+	public int getPrefixCompletionStart(IDocument document, int completionOffset) {
+		return 0;
+	}
+
+	public CharSequence getPrefixCompletionText(IDocument document, int completionOffset) {
+		return null;
+	}
+
+}
