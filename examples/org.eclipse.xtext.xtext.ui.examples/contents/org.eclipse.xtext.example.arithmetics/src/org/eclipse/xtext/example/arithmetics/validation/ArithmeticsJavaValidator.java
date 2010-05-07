520e41302c40059cc1b6d28357212b361d1b4a4a
sefftinge <sefftinge>
2010-05-07 12:57:46 +0000

    added div by zero check, use injection

--- examples/org.eclipse.xtext.xtext.ui.examples/contents/org.eclipse.xtext.example.arithmetics/src/org/eclipse/xtext/example/arithmetics/validation/ArithmeticsJavaValidator.java
+++ examples/org.eclipse.xtext.xtext.ui.examples/contents/org.eclipse.xtext.example.arithmetics/src/org/eclipse/xtext/example/arithmetics/validation/ArithmeticsJavaValidator.java
@@ -13,19 +13,33 @@ import java.math.BigDecimal;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.EcoreUtil2;
+import org.eclipse.xtext.example.arithmetics.arithmetics.ArithmeticsPackage;
+import org.eclipse.xtext.example.arithmetics.arithmetics.Div;
 import org.eclipse.xtext.example.arithmetics.arithmetics.Evaluation;
 import org.eclipse.xtext.example.arithmetics.arithmetics.Expression;
 import org.eclipse.xtext.example.arithmetics.arithmetics.FunctionCall;
 import org.eclipse.xtext.example.arithmetics.arithmetics.NumberLiteral;
 import org.eclipse.xtext.example.arithmetics.interpreter.Calculator;
 import org.eclipse.xtext.validation.Check;
+
+import com.google.inject.Inject;
  
 
 /**
  * @author Sven Efftinge - initial contribution and API
  */
 public class ArithmeticsJavaValidator extends AbstractArithmeticsJavaValidator {
+	
+	@Inject
+	private Calculator calculator;
 
+	@Check
+	public void checkDivByZero(Div div) {
+		BigDecimal bigDecimal = calculator.evaluate(div.getRight());
+		if (bigDecimal.doubleValue()==0.0) 
+			error("Division by zero detected.",ArithmeticsPackage.DIV__RIGHT);
+	}
+	
 	public final static String NORMALIZABLE = "normalizable-expression";
 	@Check
 	public void checkNormalizable(Expression expr) {
@@ -45,7 +59,7 @@ public class ArithmeticsJavaValidator extends AbstractArithmeticsJavaValidator {
 				return;
 			}
 		}
-		BigDecimal decimal = new Calculator().evaluate(expr);
+		BigDecimal decimal = calculator.evaluate(expr);
 		if (decimal.toString().length()<=8) {
 			warning("Expression could be normalized to constant '"+decimal+"'", -1,NORMALIZABLE,decimal.toString());
 		}
