c62d0a35e96c2143812a2f89438c88f50d2d581c
sefftinge <sefftinge>
2010-05-07 12:59:42 +0000

    added test case for bug https://bugs.eclipse.org/bugs/show_bug.cgi?id=311337

---
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/linking/lazy/Bug311337Test.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2010 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.linking.lazy;
+
+import org.eclipse.xtext.junit.AbstractXtextTests;
+import org.eclipse.xtext.linking.lazy.bug311337.Model;
+
+/**
+ * @author Sven Efftinge - Initial contribution and API
+ */
+public class Bug311337Test extends AbstractXtextTests {
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		with(new Bug311337TestLanguageStandaloneSetup());
+	}
+	
+	public void testNoCyclicLinkingException() throws Exception {
+		Model model = (Model) getModel(
+			"/************************************/\n"+
+			"(def) local :\n"+
+			"   (child) local_Child : def_depth1\n"+
+			"   (ref) local_Child:depth1_Child\n"+
+			"\n"+
+			"/************************************/\n"+
+			"\n"+
+			"(def) def_depth1 :\n"+
+			"   (child) depth1_Child : def_depth2\n"+
+			"\n"+
+			"/************************************/\n"+
+			"\n"+
+			"(def) def_depth2 :\n"+
+			"   (child) depth2_Child :\n");
+		assertTrue(model.eResource().getErrors().isEmpty());
+	}
+}
