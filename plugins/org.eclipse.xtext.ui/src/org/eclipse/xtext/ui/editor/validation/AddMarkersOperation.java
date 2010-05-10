7eb0ccf518ed55428fe220791ff2df2fd63966c6
szarnekow <szarnekow>
2010-05-10 16:50:43 +0000

    Fix: Use MarkerTypes... instead of EValidator.MARKER - see https://bugs.eclipse.org/bugs/show_bug.cgi?id=312297

--- plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/validation/AddMarkersOperation.java
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/validation/AddMarkersOperation.java
@@ -9,7 +9,9 @@
 package org.eclipse.xtext.ui.editor.validation;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IResource;
@@ -17,7 +19,10 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.ui.actions.WorkspaceModifyOperation;
+import org.eclipse.xtext.ui.MarkerTypes;
 import org.eclipse.xtext.validation.Issue;
+
+import com.google.common.collect.ImmutableList;
 
 /**
  * @author Sven Efftinge - Initial contribution and API
@@ -31,41 +36,59 @@ public class AddMarkersOperation extends WorkspaceModifyOperation {
 	private final List<Issue> issues;
 	private final IResource resource;
 	private final boolean deleteMarkers;
-	private final String markerId;
+	private final ImmutableList<String> markerIds;
 
 	private MarkerCreator markerCreator;
 
+	@Deprecated
 	public AddMarkersOperation(IResource resource, List<Issue> issues, String markerId,
+			boolean deleteMarkers, MarkerCreator markerCreator) {
+		this(resource, issues, Collections.singleton(markerId), deleteMarkers, markerCreator);
+	}
+	
+	public AddMarkersOperation(IResource resource, List<Issue> issues, Set<String> markerIds,
 			boolean deleteMarkers, MarkerCreator markerCreator) {
 		super(ResourcesPlugin.getWorkspace().getRuleFactory().markerRule(resource));
 		this.issues = issues;
-		this.markerId = markerId;
+		this.markerIds = ImmutableList.copyOf(markerIds);
 		this.resource = resource;
 		this.deleteMarkers = deleteMarkers;
 		this.markerCreator = markerCreator;
 	}
 
+	/**
+	 * @return the first markerID if any. 
+	 * @deprecated use {@link #getMarkerIds()} instead.
+	 */
+	@Deprecated
 	public String getMarkerId() {
-		return markerId;
+		if (markerIds.isEmpty())
+			return null;
+		return markerIds.iterator().next();
 	}
 
+	public ImmutableList<String> getMarkerIds() {
+		return markerIds;
+	}
+	
 	@Override
 	protected void execute(final IProgressMonitor monitor) throws CoreException, InvocationTargetException,
 			InterruptedException {
 		if (!resource.exists())
 			return;
-		if (deleteMarkers)
-			resource.deleteMarkers(getMarkerId(), false, IResource.DEPTH_INFINITE);
+		if (deleteMarkers) {
+			for(String markerId: getMarkerIds()) {
+				resource.deleteMarkers(markerId, false, IResource.DEPTH_INFINITE);	
+			}
+		}
 		if (!issues.isEmpty()) {
 			// update
 			for (Issue issue : issues) {
 				if (monitor.isCanceled())
 					return;
-				markerCreator.createMarker(issue,resource,getMarkerId());
+				markerCreator.createMarker(issue, resource, MarkerTypes.forCheckType(issue.getType()));
 			}
 		}
 	}
-
-	
 
 }
