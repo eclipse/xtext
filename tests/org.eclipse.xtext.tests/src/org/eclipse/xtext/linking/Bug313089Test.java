6de4a862631c8ff5a9a38587e45d551045798db2
jkohnlein <jkohnlein>
2010-05-17 11:53:34 +0000

    Parent node should only be examined if it can contain crossrefs to the same element https://bugs.eclipse.org/bugs/show_bug.cgi?id=313089

---
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/linking/Bug313089Test.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2010 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.linking;
+
+import org.eclipse.xtext.junit.AbstractXtextTests;
+
+/**
+ * @author koehnlein - Initial contribution and API
+ */
+public class Bug313089Test extends AbstractXtextTests {
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		with(Bug313089TestLanguageStandaloneSetup.class);
+	}
+	
+	public void testUnassignedAction() throws Exception {
+		getModelAndExpect("foo bar foo", 0);
+		
+	}
+	
+	public void testAssignedAction() throws Exception {
+		getModelAndExpect("foo baz baz0 baz baz1 foo",0);
+	}
+}
+
