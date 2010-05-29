9248cd8ffbbded507b8a9f6a149ec803ac483a05
meysholdt <meysholdt>
2010-05-29 13:23:29 +0000

    Fixed 314851: Property files for language configuration are now deprecated; RuntimeModules are the place to go. This commit - adapts the code generator for AbstractRuntimeModules to only load a properties file if it exists and to do the configuration via configure...() methods instead - removed all language specific .property files - adapts the wizards for new projects and xtext2ecore to not create property files. - regenerates all languages and bootstraps.

--- plugins/org.eclipse.xtext.ecore/src/org/eclipse/xtext/ecore/EcoreRuntimeModule.java
+++ plugins/org.eclipse.xtext.ecore/src/org/eclipse/xtext/ecore/EcoreRuntimeModule.java
@@ -1,10 +1,12 @@
 package org.eclipse.xtext.ecore;
 
+import org.eclipse.xtext.Constants;
 import org.eclipse.xtext.parser.IEncodingProvider;
 import org.eclipse.xtext.resource.IResourceServiceProvider;
 import org.eclipse.xtext.service.AbstractGenericModule;
 
 import com.google.inject.Binder;
+import com.google.inject.name.Names;
 
 public class EcoreRuntimeModule extends AbstractGenericModule {
 
@@ -13,7 +15,15 @@ public class EcoreRuntimeModule extends AbstractGenericModule {
 		super.configure(binder);
 		binder.bind(IResourceServiceProvider.class).to(EcoreResourceServiceProviderImpl.class);
 		binder.bind(IEncodingProvider.class).to(XMLEncodingProvider.class);
-		bindProperties(binder, "org/eclipse/xtext/ecore/Ecore.properties");
 	}
-	
+
+	public void configureLanguageName(Binder binder) {
+		binder.bind(String.class).annotatedWith(Names.named(Constants.LANGUAGE_NAME))
+				.toInstance("org.eclipse.emf.ecore.presentation.EcoreEditorID");
+	}
+
+	public void configureFileExtensions(Binder binder) {
+		binder.bind(String.class).annotatedWith(Names.named(Constants.FILE_EXTENSIONS)).toInstance("ecore");
+	}
+
 }
