8f892e8625384bce908a9abac696899e4789f8f1
sefftinge <sefftinge>
2010-05-30 19:14:20 +0000

    fixed issue with languageName (see https://bugs.eclipse.org/bugs/show_bug.cgi?id=314851)

--- tests/org.eclipse.xtext.xtend.tests/src-gen/org/eclipse/xtext/xtend/AbstractTreeTestLanguageRuntimeModule.java
+++ tests/org.eclipse.xtext.xtend.tests/src-gen/org/eclipse/xtext/xtend/AbstractTreeTestLanguageRuntimeModule.java
@@ -15,7 +15,6 @@ import com.google.inject.name.Names;
  @SuppressWarnings("all")
 public abstract class AbstractTreeTestLanguageRuntimeModule extends DefaultRuntimeModule {
 
-	// Support for property files is deprecated. Please use configure...() methods instead. 
 	protected boolean useProperties = getClass().getResource("org/eclipse/xtext/xtend/TreeTestLanguage.properties") != null;
 
 	@Override
@@ -30,8 +29,7 @@ public abstract class AbstractTreeTestLanguageRuntimeModule extends DefaultRunti
 	}
 	
 	public void configureLanguageName(Binder binder) {
-		if(!useProperties)
-			binder.bind(String.class).annotatedWith(Names.named(Constants.LANGUAGE_NAME)).toInstance("org.eclipse.xtext.xtend.TreeTestLanguage");
+		binder.bind(String.class).annotatedWith(Names.named(Constants.LANGUAGE_NAME)).toInstance("org.eclipse.xtext.xtend.TreeTestLanguage");
 	}
 	
 	public void configureFileExtensions(Binder binder) {
