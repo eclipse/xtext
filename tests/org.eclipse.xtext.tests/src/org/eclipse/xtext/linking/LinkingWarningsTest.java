7cebf57fabf5a37966cdd521c58c3d17345761c0
szarnekow <szarnekow>
2010-07-21 14:51:45 +0000

    Fix: Warnings not cleared on XtextResource#update - see https://bugs.eclipse.org/bugs/show_bug.cgi?id=320490

---
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/linking/LinkingWarningsTest.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * Copyright (c) 2010 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.linking;
+
+import org.eclipse.xtext.diagnostics.Diagnostic;
+import org.eclipse.xtext.diagnostics.DiagnosticMessage;
+import org.eclipse.xtext.diagnostics.DiagnosticSeverity;
+import org.eclipse.xtext.junit.AbstractXtextTests;
+import org.eclipse.xtext.resource.XtextResource;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+/**
+ * @author Sebastian Zarnekow - Initial contribution and API
+ */
+public class LinkingWarningsTest extends AbstractXtextTests implements ILinkingDiagnosticMessageProvider {
+
+	private final String expected = "Expected Message";
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		with(new LangATestLanguageStandaloneSetup() {
+			@Override
+			public Injector createInjector() {
+				return Guice.createInjector(new org.eclipse.xtext.linking.LangATestLanguageRuntimeModule() {
+					@SuppressWarnings("unused")
+					public ILinkingDiagnosticMessageProvider bindLinkingMessageProvider() {
+						return LinkingWarningsTest.this;
+					}
+				});
+			}
+		});
+	}
+	
+	public DiagnosticMessage getUnresolvedProxyMessage(ILinkingDiagnosticContext context) {
+		return new DiagnosticMessage(expected, DiagnosticSeverity.WARNING, Diagnostic.LINKING_DIAGNOSTIC);
+	}
+
+	public void testNoErrors() throws Exception {
+		XtextResource resource = getResourceFromString(" type A extends B \n type B extends A");
+		assertTrue(resource.getErrors().isEmpty());
+		assertTrue(resource.getWarnings().isEmpty());
+	}
+
+	public void testLinkingProblemAsWarning() throws Exception {
+		String modelAsText = "type A extends B \n type B extends C";
+		XtextResource resource = getResourceFromStringAndExpect(modelAsText, 0);
+		assertTrue(resource.getErrors().isEmpty());
+		assertEquals(1, resource.getWarnings().size());
+
+		Diagnostic warning = (Diagnostic) resource.getWarnings().get(0);
+		assertEquals(2, warning.getLine());
+		assertEquals(expected, warning.getMessage());
+	}
+	
+	public void testFixedLinkingProblem() throws Exception {
+		String modelAsText = "type A extends B \n type B extends C";
+		XtextResource resource = getResourceFromStringAndExpect(modelAsText, 0);
+		assertTrue(resource.getErrors().isEmpty());
+		assertEquals(1, resource.getWarnings().size());
+
+		resource.update(modelAsText.indexOf('C'), 1, "A");
+		
+		assertTrue(resource.getErrors().isEmpty());
+		assertTrue(resource.getWarnings().isEmpty());
+	}
+	
+
+}
