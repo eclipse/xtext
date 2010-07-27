b9fd34c0dd1eedfbfce0ae218cd697fb57825655
szarnekow <szarnekow>
2010-07-27 12:56:11 +0000

    Fix: Reverted some WS-changes in the grammar templates to reduce unnecessary noise, Tests for backtracking + unordered groups - see https://bugs.eclipse.org/bugs/show_bug.cgi?id=315335

---
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/parser/unorderedGroups/AbstractBacktrackingParserTest.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2010 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.parser.unorderedGroups;
+
+import org.eclipse.xtext.junit.AbstractXtextTests;
+
+/**
+ * @author Sebastian Zarnekow - Initial contribution and API
+ */
+public abstract class AbstractBacktrackingParserTest extends AbstractXtextTests {
+
+	public void testNoException_01() throws Exception {
+		getModel("unit { provides : unit { name : FooBar;	}; }");
+	}
+	
+	public void testNoException_02() throws Exception {
+		getModelAndExpect("unit { provides : unit { name : FooBar	}; }", 
+				/* usually expect 1 error (missing ;), but got a second due to backtracking */ 2);
+	}
+}
