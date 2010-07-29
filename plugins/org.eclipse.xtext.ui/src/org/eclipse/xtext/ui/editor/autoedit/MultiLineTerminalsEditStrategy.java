a905055f5763932995b18edf833b518d10eac5f0
szarnekow <szarnekow>
2010-07-29 14:54:14 +0000

    Fix: MultiLineTerminalsEditStrategy - see https://bugs.eclipse.org/bugs/show_bug.cgi?id=305475 and https://bugs.eclipse.org/bugs/show_bug.cgi?id=316360

--- plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/autoedit/MultiLineTerminalsEditStrategy.java
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/autoedit/MultiLineTerminalsEditStrategy.java
@@ -12,6 +12,7 @@ import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.DocumentCommand;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.TextUtilities;
 import org.eclipse.xtext.formatting.IIndentationInformation;
 
 import com.google.inject.Inject;
@@ -28,28 +29,93 @@ public class MultiLineTerminalsEditStrategy extends AbstractEditStrategy {
 	@Inject
 	private IIndentationInformation indentationInformation;
 
+	private boolean explicitIndentation;
+
+	private String significantRightTerminalPart;
+
+	private String significantInsertableIndentationString;
+	private String significantLookupIndentationString;
+
 	public MultiLineTerminalsEditStrategy configure(String leftTerminal, String indentationString, String rightTerminal) {
+		String insignificantPrefix = getInsignificantPart(indentationString, rightTerminal);
+		return configure(leftTerminal, indentationString, rightTerminal, insignificantPrefix);
+	}
+	
+	public MultiLineTerminalsEditStrategy configure(String leftTerminal, String indentationString, String rightTerminal, String insignificantPrefix) {
 		this.leftTerminal = leftTerminal;
 		this.rightTerminal = rightTerminal;
-		this.indentationString = indentationString == null ? indentationInformation.getIndentString()
+		this.significantRightTerminalPart = rightTerminal.substring(insignificantPrefix.length());
+		this.indentationString = indentationString == null 
+				? indentationInformation.getIndentString()
 				: indentationString;
+		this.explicitIndentation = indentationString != null;
+		if (this.explicitIndentation) {
+			this.significantInsertableIndentationString = this.indentationString.substring(insignificantPrefix.length());
+			this.significantLookupIndentationString = this.indentationString.trim();
+		}
 		return this;
 	}
 
 	public void customizeDocumentCommand(IDocument document, DocumentCommand command) {
 		try {
+			if (command.length != 0)
+				return;
 			String originalText = command.text;
-			if (originalText.startsWith(getNewLine())) {
-				if (isAfter(document, command.offset, leftTerminal)
-						|| (lineStartsWith(document, command.offset, indentationString))) {
-					command.text = concat(originalText, indentationString);
-					command.caretOffset = command.offset + command.text.length();
-					String documentContent = getDocumentContent(document, command);
-					int opening = count(leftTerminal, documentContent);
-					int closing = count(rightTerminal, documentContent);
-					if (opening > closing)
-						command.text = command.text + originalText + rightTerminal;
-					command.shiftsCaret = false;
+			String[] lineDelimiters = document.getLegalLineDelimiters();
+			int delimiterIndex = TextUtilities.startsWith(lineDelimiters, originalText);
+			if (delimiterIndex != -1) {
+				IRegion currentLineRegion = document.getLineInformationOfOffset(command.offset);
+				String currentLine = document.get(currentLineRegion.getOffset(), command.offset - currentLineRegion.getOffset());
+//				currentLine = currentLine.substring(0, command.offset - currentLineRegion.getOffset());
+				int lastLeftTerminal = currentLine.lastIndexOf(leftTerminal);
+				if (lastLeftTerminal != -1) {
+					// open left terminal
+					int lastRightTerminal = currentLine.lastIndexOf(significantRightTerminalPart);
+					if (lastRightTerminal == -1 || lastLeftTerminal > lastRightTerminal + leftTerminal.length()) {
+						StringBuilder commandString = new StringBuilder(lastLeftTerminal + indentationString.length() + command.text.length());
+						commandString.append(command.text);
+						int firstNonWsCharacter = findFirstNonWsCharacter(currentLine, lastLeftTerminal);
+						int numberOfSpaces = lastLeftTerminal - firstNonWsCharacter;
+						appendSpaces(commandString, numberOfSpaces);
+						commandString.append(indentationString);
+						
+						command.text = commandString.toString();
+						command.caretOffset = command.offset + command.text.length();
+						command.shiftsCaret = false;
+						String documentContent = getDocumentContent(document, command);
+						int opening = count(leftTerminal, documentContent);
+						int closing = count(significantRightTerminalPart, documentContent);
+						if (opening > closing) {
+							commandString.append(originalText);
+							appendSpaces(commandString, numberOfSpaces);
+							commandString.append(rightTerminal);
+							command.text = commandString.toString();
+						} else {
+							if (currentLine.length() < currentLineRegion.getLength()) {
+								String trailingCurrentLine = document.get(currentLineRegion.getOffset(), currentLineRegion.getLength());
+								trailingCurrentLine = trailingCurrentLine.substring(currentLine.length());
+								int trailingRightTerminalIndex = trailingCurrentLine.indexOf(significantRightTerminalPart);
+								if (trailingRightTerminalIndex != -1) {
+									if (hasNonWsBefore(trailingCurrentLine, trailingRightTerminalIndex))
+										return;
+									commandString.append(originalText);
+									appendSpaces(commandString, numberOfSpaces);
+									commandString.append(rightTerminal);
+									command.text = commandString.toString();
+									command.length += trailingRightTerminalIndex + significantRightTerminalPart.length();
+								}
+							}
+						}
+					}
+				} else if (explicitIndentation) {
+					// continuous auto edit
+					int indentationStringIndex = currentLine.indexOf(significantLookupIndentationString);
+					if (shouldAppendIndentationString(document, command, currentLine, indentationStringIndex)) {
+						StringBuilder commandString = new StringBuilder(indentationStringIndex + significantInsertableIndentationString.length() + command.text.length());
+						commandString.append(command.text);
+						commandString.append(significantInsertableIndentationString);
+						command.text = commandString.toString();
+					}
 				}
 			}
 		} catch (BadLocationException ex) {
@@ -57,31 +123,72 @@ public class MultiLineTerminalsEditStrategy extends AbstractEditStrategy {
 		}
 	}
 
-	private String concat(String originalText, String indentationString) {
-		if (originalText.endsWith(indentationString.substring(0, 1))) {
-			return originalText + indentationString.substring(1);
-		} else {
-			return originalText + indentationString;
-		}
+	protected boolean shouldAppendIndentationString(IDocument document, DocumentCommand command, String currentLine,
+			int indentationStringIndex) {
+		if (indentationStringIndex == -1) 
+			return false;
+		String prefixInDocument = getDocumentContent(document, command).substring(0, command.offset);
+		int previousLeftTerminalInDocument = prefixInDocument.lastIndexOf(leftTerminal);
+		if (previousLeftTerminalInDocument == -1)
+			return false;
+		int previousRightTerminalInDocument = prefixInDocument.lastIndexOf(significantRightTerminalPart);
+		if (previousRightTerminalInDocument != -1 && previousLeftTerminalInDocument < previousRightTerminalInDocument + leftTerminal.length())
+			return false;
+		if (hasNonWsBefore(currentLine, indentationStringIndex))
+			return false;
+		return true;
 	}
 
-	private boolean lineStartsWith(IDocument document, int offset, String indentationString)
-			throws BadLocationException {
-		IRegion region = document.getLineInformationOfOffset(offset);
-		String line = document.get(region.getOffset(), region.getLength());
-		if (line.startsWith(indentationString)) {
-			return true;
+	protected boolean hasNonWsBefore(String line, int index) {
+		int idx = 0;
+		while(idx < index) {
+			if (!Character.isWhitespace(line.charAt(idx)))
+				return true;
+			idx++;
 		}
 		return false;
 	}
 
+	protected void appendSpaces(StringBuilder commandString, int count) {
+		if (explicitIndentation) {
+			int i = 0;
+			while(i < count) {
+				commandString.append(' ');
+				i++;
+			}
+		}
+	}
+
+	protected int findFirstNonWsCharacter(String currentLine, int maxIndex) {
+		int i = 0;
+		while(i < maxIndex) {
+			if (Character.isWhitespace(currentLine.charAt(i))) {
+				i++;								
+			} else {
+				break;
+			}
+		}
+		return i;
+	}
+
+	protected String getInsignificantPart(String first, String second) {
+		if (first == null || second == null)
+			return "";
+		int minLen = Math.min(first.length(), second.length());
+		for(int i = 0; i < minLen; i++) {
+			if (!Character.isWhitespace(first.charAt(i)) || first.charAt(i) != second.charAt(i)) {
+				return first.substring(0, i);
+			}
+		}
+		return "";
+	}
+	
+	/**
+	 * @deprecated use {@link IDocument#getLegalLineDelimiters()} instead
+	 */
+	@Deprecated
 	protected String getNewLine() {
 		return "\n";
 	}
-
-	private boolean isAfter(IDocument doc, int offset, String leftTerminal) throws BadLocationException {
-		return doc.getLength() >= offset && offset >= leftTerminal.length() &&
-			doc.get(offset - leftTerminal.length(), leftTerminal.length()).equals(leftTerminal);
-	}
-
+	
 }
