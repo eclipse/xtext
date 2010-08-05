5750387fcc532db3d4bb39bb4c1aac4abc7813b8
szarnekow <szarnekow>
2010-08-05 09:42:58 +0000

    Fix: Illegal grammar when unassigned datatype rule calls are involved - see https://bugs.eclipse.org/bugs/show_bug.cgi?id=321580

--- tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/xtext/ecoreInference/UnassignedRuleCallTest.java
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/xtext/ecoreInference/UnassignedRuleCallTest.java
@@ -8,8 +8,11 @@
 package org.eclipse.xtext.xtext.ecoreInference;
 
 import org.eclipse.xtext.junit.AbstractXtextTests;
+import org.eclipse.xtext.parsetree.CompositeNode;
+import org.eclipse.xtext.parsetree.NodeUtil;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.xtext.ecoreInference.unassignedRuleCallTestLanguage.Model;
+import org.eclipse.xtext.xtext.ecoreInference.unassignedRuleCallTestLanguage.ModelFeatures;
 
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
@@ -48,5 +51,31 @@ public class UnassignedRuleCallTest extends AbstractXtextTests {
 		String modelAsText = "model 7 13 137 feature Foo 1 keyword 1 'bar';";
 		XtextResource resource = getResourceFromString(modelAsText);
 		assertTrue(resource.getErrors().isEmpty());
+	}
+	
+	public void testNodeModel_01() throws Exception {
+		String modelAsText = "model 7 feature Zonk 1 keyword 1;";
+		Model model = (Model) getModel(modelAsText);
+		CompositeNode node = NodeUtil.getNode(model);
+		assertEquals(4, node.getChildren().size());
+		ModelFeatures modelFeatures = model.getModelFeatures();
+		CompositeNode featuresNode = NodeUtil.getNode(modelFeatures);
+		assertSame(featuresNode, node.getChildren().get(3));
+		assertEquals(6, featuresNode.getChildren().size());
+		CompositeNode dataTypeNode = (CompositeNode) featuresNode.getChildren().get(4);
+		assertEquals(6, dataTypeNode.getChildren().size());
+	}
+	
+	public void testNodeModel_02() throws Exception {
+		String modelAsText = "model 7 13 137 feature Foo 1 keyword 1 'bar';";
+		Model model = (Model) getModel(modelAsText);
+		CompositeNode node = NodeUtil.getNode(model);
+		assertEquals(8, node.getChildren().size());
+		ModelFeatures modelFeatures = model.getModelFeatures();
+		CompositeNode featuresNode = NodeUtil.getNode(modelFeatures);
+		assertSame(featuresNode, node.getChildren().get(7));
+		assertEquals(6, featuresNode.getChildren().size());
+		CompositeNode dataTypeNode = (CompositeNode) featuresNode.getChildren().get(4);
+		assertEquals(8, dataTypeNode.getChildren().size());
 	}
 }
