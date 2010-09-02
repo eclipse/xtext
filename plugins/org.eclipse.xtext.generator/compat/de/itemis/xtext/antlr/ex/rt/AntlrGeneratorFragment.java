d22bcac9dddcc952492a66c9b4e70c0899250967
Sven Efftinge <sven.efftinge@itemis.de>
2010-09-02 14:04:16 +0200

    removed IAntlrParser and deprecated API

--- plugins/org.eclipse.xtext.generator/compat/de/itemis/xtext/antlr/ex/rt/AntlrGeneratorFragment.java
+++
@@ -1,9 +0,0 @@
-package de.itemis.xtext.antlr.ex.rt;
-
-@Deprecated
-public class AntlrGeneratorFragment extends org.eclipse.xtext.generator.parser.antlr.ex.rt.AntlrGeneratorFragment {
-	@Override
-	protected String getTemplate() {
-		return getClass().getSuperclass().getName().replaceAll("\\.", "::");
-	}
-}
