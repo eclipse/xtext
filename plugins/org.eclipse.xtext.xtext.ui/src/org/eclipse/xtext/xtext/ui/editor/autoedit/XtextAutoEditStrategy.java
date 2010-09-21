c8a75132787b9e30e5e84ef19fcb2e41afaa55b6
Sven Efftinge <sven.efftinge@itemis.de>
2010-09-21 20:56:44 +0200

    do not add closing bracket before anything than whitespace in grammar language (see https://bugs.eclipse.org/bugs/show_bug.cgi?id=325729#c2)

--- plugins/org.eclipse.xtext.xtext.ui/src/org/eclipse/xtext/xtext/ui/editor/autoedit/XtextAutoEditStrategy.java
+++ plugins/org.eclipse.xtext.xtext.ui/src/org/eclipse/xtext/xtext/ui/editor/autoedit/XtextAutoEditStrategy.java
@@ -7,8 +7,10 @@
  *******************************************************************************/
 package org.eclipse.xtext.xtext.ui.editor.autoedit;
 
+import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.xtext.ui.editor.autoedit.DefaultAutoEditStrategyProvider;
+import org.eclipse.xtext.ui.editor.autoedit.SingleLineTerminalsStrategy;
 
 /**
  * @author Sven Efftinge - Initial contribution and API
@@ -20,5 +22,19 @@ public class XtextAutoEditStrategy extends DefaultAutoEditStrategyProvider {
 		super.configure(acceptor);
 		acceptor.accept(singleLineTerminals.get().configure(":", ";"),IDocument.DEFAULT_CONTENT_TYPE);
 		acceptor.accept(multiLineTerminals.get().configure(":", null, ";"),IDocument.DEFAULT_CONTENT_TYPE);
+	}
+	
+	@Override
+	protected void configureParenthesis(IEditStrategyAcceptor acceptor) {
+		acceptor.accept(new SingleLineTerminalsStrategy(){
+			@Override
+			protected boolean isIdentifierPart(IDocument doc, int offset) throws BadLocationException {
+				if (doc.getLength()>offset+1) {
+					return !Character.isWhitespace(doc.getChar(offset));
+				}
+				return super.isIdentifierPart(doc, offset);
+			}
+			
+		}.configure("(", ")"), IDocument.DEFAULT_CONTENT_TYPE);
 	}
 }
