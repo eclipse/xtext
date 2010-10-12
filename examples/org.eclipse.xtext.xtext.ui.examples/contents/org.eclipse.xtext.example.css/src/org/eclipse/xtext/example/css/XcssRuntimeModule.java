7a2dc818a8f48c1283fc0fd3f1fca04bdd8282eb
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2010-10-12 21:32:15 +0200

    Initial tests for Xcss

--- examples/org.eclipse.xtext.xtext.ui.examples/contents/org.eclipse.xtext.example.css/src/org/eclipse/xtext/example/css/XcssRuntimeModule.java
+++ examples/org.eclipse.xtext.xtext.ui.examples/contents/org.eclipse.xtext.example.css/src/org/eclipse/xtext/example/css/XcssRuntimeModule.java
@@ -3,9 +3,19 @@
  */
 package org.eclipse.xtext.example.css;
 
+import org.eclipse.xtext.common.types.JvmTypeReference;
+import org.eclipse.xtext.common.types.util.IJvmTypeConformanceComputer;
+import org.eclipse.xtext.xbase.typing.ExpressionsTypeResolver;
+
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
 public class XcssRuntimeModule extends org.eclipse.xtext.example.css.AbstractXcssRuntimeModule {
-
+	public Class<? extends org.eclipse.xtext.typing.ITypeConformanceComputer<JvmTypeReference>> bindITypeService() {
+		return IJvmTypeConformanceComputer.class;
+	}
+	
+	public Class<? extends org.eclipse.xtext.typing.ITypeProvider<JvmTypeReference>> bindITypeProvider() {
+		return ExpressionsTypeResolver.class;
+	}
 }
