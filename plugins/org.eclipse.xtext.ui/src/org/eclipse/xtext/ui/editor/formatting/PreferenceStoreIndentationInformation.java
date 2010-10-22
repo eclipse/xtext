f908862e2a47c2809bc3790ce009d45a29aa5454
Knut Wannheden <knut.wannheden@paranor.ch>
2010-10-22 14:58:25 +0200

    fix: added caching of eclipse tab width preferences - see https://bugs.eclipse.org/bugs/show_bug.cgi?id=328225

--- plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/formatting/PreferenceStoreIndentationInformation.java
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/formatting/PreferenceStoreIndentationInformation.java
@@ -7,6 +7,8 @@
  *******************************************************************************/
 package org.eclipse.xtext.ui.editor.formatting;
 
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.ui.texteditor.AbstractDecoratedTextEditorPreferenceConstants;
 import org.eclipse.xtext.formatting.IIndentationInformation;
 import org.eclipse.xtext.ui.editor.preferences.IPreferenceStoreAccess;
@@ -15,28 +17,46 @@ import com.google.inject.Inject;
 
 /**
  * @author Sven Efftinge - Initial contribution and API
+ * @author Knut Wannheden
  */
-public class PreferenceStoreIndentationInformation implements IIndentationInformation {
-	@Inject
+public class PreferenceStoreIndentationInformation implements IIndentationInformation, IPropertyChangeListener {
+
 	private IPreferenceStoreAccess storeAccess;
-	
+
+	@Inject
+	public void setPreferenceStoreAccess(IPreferenceStoreAccess storeAccess) {
+		this.storeAccess = storeAccess;
+		storeAccess.getPreferenceStore().addPropertyChangeListener(this);
+	}
+
 	public int getTabWidth() {
 		return storeAccess.getPreferenceStore().getInt(AbstractDecoratedTextEditorPreferenceConstants.EDITOR_TAB_WIDTH);
 	}
-	
+
 	public boolean isSpacesForTab() {
 		return storeAccess.getPreferenceStore().getBoolean(AbstractDecoratedTextEditorPreferenceConstants.EDITOR_SPACES_FOR_TABS);
 	}
-	
+
 	private final String WS = "                                     ";
-	
-	public String getIndentString() {
-		if (isSpacesForTab()) {
-			if (getTabWidth()>WS.length()) {
-				return WS;
+
+	private String indentString = null;
+
+	public synchronized String getIndentString() {
+		if (indentString == null) {
+			if (isSpacesForTab()) {
+				if (getTabWidth() > WS.length()) {
+					indentString = WS;
+				}
+				indentString = WS.substring(0, getTabWidth());
+			} else {
+				indentString = "\t";
 			}
-			return WS.substring(0, getTabWidth());
 		}
-		return "\t";
+		return indentString;
 	}
+
+	public synchronized void propertyChange(PropertyChangeEvent event) {
+		indentString = null;
+	}
+
 }
