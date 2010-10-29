3ebec67880ece2874011cb7153142561c364bf9f
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2010-10-29 17:55:27 +0200

    [Xtext] Remove unused code (thanks to warnings in JDT 3.7)

--- plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/toggleComments/ToggleSLCommentAction.java
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/toggleComments/ToggleSLCommentAction.java
@@ -121,7 +121,6 @@ public final class ToggleSLCommentAction extends TextEditorAction {
 			IRegion block= getTextBlockFromSelection(textSelection, document);
 			ITypedRegion[] regions= TextUtilities.computePartitioning(document, fDocumentPartitioning, block.getOffset(), block.getLength(), false);
 
-			int lineCount= 0;
 			int[] lines= new int[regions.length * 2]; // [startline, endline, startline, endline, ...]
 			for (int i= 0, j= 0; i < regions.length; i++, j+= 2) {
 				// start line of region
@@ -132,7 +131,6 @@ public final class ToggleSLCommentAction extends TextEditorAction {
 				if (length > 0)
 					offset--;
 				lines[j + 1]= (lines[j] == -1 ? -1 : document.getLineOfOffset(offset));
-				lineCount += lines[j + 1] - lines[j] + 1;
 			}
 
 			// Perform the check
