38440d21a3ce473da8c6f57bba4273ba1e12bdac
Jan Koehnlein <jan.koehnlein@itemis.de>
2010-11-15 17:25:30 +0100

    First test for refactoring

---
+++ plugins/org.eclipse.xtext.generator/src/org/eclipse/xtext/ui/generator/refactoring/RefactorElementNameFragment.java
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2010 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.ui.generator.refactoring;
+
+import org.eclipse.xtext.generator.AbstractGeneratorFragment;
+
+/**
+ * @author Jan Koehnlein - Initial contribution and API
+ */
+public class RefactorElementNameFragment extends AbstractGeneratorFragment {
+}
