15dfa4de5ebb7d629ab44fe043d109230851f2bb
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2010-11-25 21:32:08 +0100

    [xtext] lightweight node model: moved some test classes, fix fallback algorithm for CompositeNode#getTotalOffset

--- tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/nodemodel/CompositeNodeTest.java
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/nodemodel/CompositeNodeTest.java
@@ -38,6 +38,42 @@ public class CompositeNodeTest extends AbstractCompositeNodeTest {
 		assertEquals(completeContent.trim().length(), composite.getTotalLength());
 	}
 	
+	public void testGetTotalOffsetWithoutChildren() {
+		RootNode rootNode = new RootNode();
+		rootNode.basicSetCompleteContent("my string");
+		LeafNode leafNode = new LeafNode();
+		leafNode.basicSetTotalOffset(0);
+		leafNode.basicSetTotalLength(3);
+		addChild(rootNode, leafNode);
+		CompositeNodeWithSemanticElement firstChild = createCompositeNode();
+		CompositeNodeWithSemanticElement firstGrandChild = createCompositeNode();
+		CompositeNodeWithSemanticElement secondChild = createCompositeNode();
+		CompositeNodeWithSemanticElement secondGrandChild = createCompositeNode();
+		CompositeNodeWithSemanticElement thirdChild = createCompositeNode();
+		CompositeNodeWithSemanticElement thirdGrandChild = createCompositeNode();
+		addChild(rootNode, firstChild);
+		addChild(firstChild, firstGrandChild);
+		addChild(rootNode, secondChild);
+		addChild(secondChild, secondGrandChild);
+		addChild(rootNode, thirdChild);
+		addChild(thirdChild, thirdGrandChild);
+		
+		assertEquals(0, rootNode.getTotalOffset());		
+		assertEquals(0, rootNode.getOffset());		
+		assertEquals(3, firstChild.getTotalOffset());		
+		assertEquals(3, firstChild.getOffset());		
+		assertEquals(3, firstGrandChild.getTotalOffset());		
+		assertEquals(3, firstGrandChild.getOffset());		
+		assertEquals(3, secondChild.getTotalOffset());		
+		assertEquals(3, secondChild.getOffset());		
+		assertEquals(3, secondGrandChild.getTotalOffset());		
+		assertEquals(3, secondGrandChild.getOffset());		
+		assertEquals(3, thirdChild.getTotalOffset());		
+		assertEquals(3, thirdChild.getOffset());		
+		assertEquals(3, thirdGrandChild.getTotalOffset());		
+		assertEquals(3, thirdGrandChild.getOffset());		
+	}
+	
 	@Override
 	protected AbstractNode getFirstChild(ICompositeNode node) {
 		return ((CompositeNodeWithSemanticElement) node).basicGetFirstChild();
