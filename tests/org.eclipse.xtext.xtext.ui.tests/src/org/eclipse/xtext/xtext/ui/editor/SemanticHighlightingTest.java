4f63a6fb31fb000ce97f204a6dc5d62b9a9f560d
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2010-12-06 16:04:15 +0100

    [xtext.xtext.ui] Prevent NPE on illegal return keyword in semantic highlighter

---
+++ tests/org.eclipse.xtext.xtext.ui.tests/src/org/eclipse/xtext/xtext/ui/editor/SemanticHighlightingTest.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2010 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.xtext.ui.editor;
+
+import org.eclipse.xtext.ISetup;
+import org.eclipse.xtext.XtextRuntimeModule;
+import org.eclipse.xtext.XtextStandaloneSetup;
+import org.eclipse.xtext.junit.AbstractXtextTests;
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.ui.XtextUiModule;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightedPositionAcceptor;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.ISemanticHighlightingCalculator;
+import org.eclipse.xtext.ui.shared.SharedStateModule;
+import org.eclipse.xtext.util.Modules2;
+import org.eclipse.xtext.xtext.ui.Activator;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+/**
+ * @author Sebastian Zarnekow - Initial contribution and API
+ */
+public class SemanticHighlightingTest extends AbstractXtextTests {
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		with(new XtextStandaloneSetup() {
+			@Override
+			public Injector createInjector() {
+				return Guice.createInjector(Modules2.mixin(new XtextRuntimeModule(),new XtextUiModule(Activator.getDefault()), new SharedStateModule()));
+			}
+		});
+	}
+
+	public void testNullPointerOnInvalidReturnKeyword() throws Exception {
+		XtextResource resource = getResourceFromStringAndExpect(
+				"grammar test with org.eclipse.xtext.common.Terminals\n" +
+				"import 'http://www.eclipse.org/emf/2002/Ecore'\n" +
+				"terminal fragment FOO returns EString: 'a';", 1);
+		ISemanticHighlightingCalculator calculator = get(ISemanticHighlightingCalculator.class);
+		calculator.provideHighlightingFor(resource, new IHighlightedPositionAcceptor() {
+			public void addPosition(int offset, int length, String... id) {
+				// ignore
+			}
+		});
+	}
+
+}
