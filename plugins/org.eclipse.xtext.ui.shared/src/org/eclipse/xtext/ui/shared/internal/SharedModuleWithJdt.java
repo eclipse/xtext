182693fc94220eb72507fd3237807e45741ce686
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2010-12-10 10:12:01 +0100

    [xtext.shared] Renamed ElementChangeListener to ProjectClasspathChangeListener [common.types] Removed unused import

--- plugins/org.eclipse.xtext.ui.shared/src/org/eclipse/xtext/ui/shared/internal/SharedModuleWithJdt.java
+++ plugins/org.eclipse.xtext.ui.shared/src/org/eclipse/xtext/ui/shared/internal/SharedModuleWithJdt.java
@@ -8,8 +8,9 @@
 package org.eclipse.xtext.ui.shared.internal;
 
 import org.eclipse.xtext.builder.impl.ToBeBuiltComputer;
-import org.eclipse.xtext.builder.impl.javasupport.ElementChangeListener;
+import org.eclipse.xtext.builder.impl.javasupport.ProjectClasspathChangeListener;
 import org.eclipse.xtext.builder.impl.javasupport.JdtToBeBuiltComputer;
+import org.eclipse.xtext.common.types.ui.notification.TypeResourceUnloader;
 import org.eclipse.xtext.ui.resource.IResourceSetProvider;
 import org.eclipse.xtext.ui.resource.IStorage2UriMapper;
 import org.eclipse.xtext.ui.resource.Storage2UriMapperJavaImpl;
@@ -25,9 +26,10 @@ public class SharedModuleWithJdt extends AbstractModule {
 	@Override
 	protected void configure() {
 		bind(ToBeBuiltComputer.class).to(JdtToBeBuiltComputer.class);
-		bind(ElementChangeListener.class).asEagerSingleton();
+		bind(ProjectClasspathChangeListener.class).asEagerSingleton();
 		bind(IStorage2UriMapper.class).to(Storage2UriMapperJavaImpl.class);
 		bind(IResourceSetProvider.class).to(XtextResourceSetProvider.class);
+		bind(TypeResourceUnloader.class).asEagerSingleton();
 	}
 
 }
