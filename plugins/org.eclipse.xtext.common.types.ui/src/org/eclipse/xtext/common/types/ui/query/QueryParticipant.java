0d2ada2abf23d669504ac435cfe06ce948203969
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2010-12-10 10:10:14 +0100

    [common.types] Refactored JDT Query Participant and moved registration to SharedCommonTypes

--- plugins/org.eclipse.xtext.common.types.ui/src/org/eclipse/xtext/common/types/ui/query/QueryParticipant.java
+++ plugins/org.eclipse.xtext.common.types.ui/src/org/eclipse/xtext/common/types/ui/query/QueryParticipant.java
@@ -7,55 +7,23 @@
  *******************************************************************************/
 package org.eclipse.xtext.common.types.ui.query;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IStorage;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EReference;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.jdt.core.IJarEntryResource;
 import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.search.IJavaSearchConstants;
 import org.eclipse.jdt.ui.search.ElementQuerySpecification;
 import org.eclipse.jdt.ui.search.IMatchPresentation;
 import org.eclipse.jdt.ui.search.IQueryParticipant;
 import org.eclipse.jdt.ui.search.ISearchRequestor;
 import org.eclipse.jdt.ui.search.QuerySpecification;
-import org.eclipse.jface.viewers.ILabelProvider;
-import org.eclipse.jface.viewers.ILabelProviderListener;
-import org.eclipse.search.ui.text.Match;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.xtext.common.types.access.jdt.TypeURIHelper;
-import org.eclipse.xtext.nodemodel.INode;
-import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
-import org.eclipse.xtext.resource.IEObjectDescription;
-import org.eclipse.xtext.resource.IExternalContentSupport;
-import org.eclipse.xtext.resource.IReferenceDescription;
-import org.eclipse.xtext.resource.IResourceDescription;
 import org.eclipse.xtext.resource.IResourceDescriptions;
-import org.eclipse.xtext.ui.editor.IDirtyStateManager;
-import org.eclipse.xtext.ui.editor.IURIEditorOpener;
-import org.eclipse.xtext.ui.label.GlobalDescriptionLabelProvider;
-import org.eclipse.xtext.ui.resource.IResourceSetProvider;
-import org.eclipse.xtext.ui.resource.IStorage2UriMapper;
-import org.eclipse.xtext.ui.shared.Access;
-import org.eclipse.xtext.util.ITextRegion;
-import org.eclipse.xtext.util.TextRegion;
 
-import com.google.common.base.Predicate;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Maps;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
 
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
@@ -67,11 +35,15 @@ public class QueryParticipant implements IQueryParticipant {
 	private final TypeURIHelper typeURIHelper;
 
 	private final UIParticipant uiParticipant;
+	
+	@Inject
+	private Provider<JavaSearchHelper> javaSearchHelperProvider;
 
-	public QueryParticipant() {
-		resourceDescriptions = Access.getIResourceDescriptions().get();
+	@Inject
+	public QueryParticipant(IResourceDescriptions resourceDescriptions, UIParticipant uiParticipant) {
+		this.resourceDescriptions = resourceDescriptions;
+		this.uiParticipant = uiParticipant;
 		typeURIHelper = new TypeURIHelper();
-		uiParticipant = new UIParticipant(resourceDescriptions);
 	}
 
 	public void search(ISearchRequestor requestor, QuerySpecification query, IProgressMonitor monitor)
@@ -96,134 +68,9 @@ public class QueryParticipant implements IQueryParticipant {
 	}
 
 	protected JavaSearchHelper createSearchHelper(ISearchRequestor requestor) {
-		return new JavaSearchHelper(requestor, resourceDescriptions);
-	}
-
-	public static class JavaSearchHelper {
-
-		private final ISearchRequestor requestor;
-		private final Map<IProject, ResourceSet> projectToResourceSet;
-		private final IStorage2UriMapper storage2UriMapper;
-
-		private final IResourceSetProvider resourceSetProvider;
-
-		private final IExternalContentSupport externalContentSupport;
-
-		private final IDirtyStateManager dirtyStateManager;
-		private final IResourceDescriptions resourceDescriptions;
-
-		public JavaSearchHelper(ISearchRequestor requestor, IResourceDescriptions resourceDescriptions) {
-			this.requestor = requestor;
-			this.resourceDescriptions = resourceDescriptions;
-			projectToResourceSet = Maps.newHashMap();
-			storage2UriMapper = Access.getIStorage2UriMapper().get();
-			resourceSetProvider = Access.provider(IResourceSetProvider.class).get();
-			externalContentSupport = Access.provider(IExternalContentSupport.class).get();
-			dirtyStateManager = Access.getIDirtyStateManager().get();
-		}
-
-		public void search(URI uri, IProgressMonitor monitor) {
-			int numResources = Iterables.size(resourceDescriptions.getAllResourceDescriptions());
-			SubMonitor subMonitor = SubMonitor.convert(monitor, numResources);
-			subMonitor.subTask("Find references in EMF resources");
-			try {
-				for (IResourceDescription resourceDescription : resourceDescriptions.getAllResourceDescriptions()) {
-					searchIn(uri, resourceDescription);
-					if (subMonitor.isCanceled()) {
-						return;
-					}
-					subMonitor.worked(1);
-				}
-				for(ResourceSet resourceSet: projectToResourceSet.values()) {
-					resourceSet.getResources().clear();
-					resourceSet.eAdapters().clear();
-				}
-			} finally {
-				subMonitor.done();
-			}
-		}
-
-		protected void searchIn(final URI uri, IResourceDescription resourceDescription) {
-			Iterable<IReferenceDescription> matchingReferenceDescriptors = Iterables.filter(resourceDescription
-					.getReferenceDescriptions(), new Predicate<IReferenceDescription>() {
-				public boolean apply(IReferenceDescription input) {
-					return uri.equals(input.getTargetEObjectUri());
-				}
-			});
-			for (IReferenceDescription matchingReferenceDescription : matchingReferenceDescriptors) {
-				accept(matchingReferenceDescription);
-			}
-		}
-
-		protected void accept(IReferenceDescription referenceDescription) {
-			URI sourceResourceURI = referenceDescription.getSourceEObjectUri().trimFragment();
-			Iterable<IStorage> storages = storage2UriMapper.getStorages(sourceResourceURI);
-			Iterator<IStorage> iterator = storages.iterator();
-			while (iterator.hasNext()) {
-				IStorage storage = iterator.next();
-				IProject project = getProject(storage);
-				if (project != null) {
-					ResourceSet resourceSet = getResourceSet(project);
-					EObject sourceEObject = resourceSet.getEObject(referenceDescription.getSourceEObjectUri(), true);
-					if (sourceEObject != null) {
-						ITextRegion region = getLocation(
-								sourceEObject, referenceDescription.getEReference(), 
-								referenceDescription.getIndexInList());
-						acceptMatch(storage, region);
-					} else {
-						acceptMatch(referenceDescription, null);
-					}
-				}
-			}
-		}
-
-		protected ITextRegion getLocation(EObject sourceEObject, EReference eReference, int indexInList) {
-			List<INode> result = NodeModelUtils.findNodesForFeature(sourceEObject, eReference);
-			if (result.isEmpty())
-				return ITextRegion.EMPTY_REGION;
-			if (result.size() == 1) {
-				INode node = result.get(0);
-				return new TextRegion(node.getOffset(), node.getLength());
-			}
-			if (indexInList == -1 || indexInList > result.size())
-				return ITextRegion.EMPTY_REGION;
-			INode node = result.get(indexInList);
-			return new TextRegion(node.getOffset(), node.getLength());
-		}
-		
-		protected ResourceSet getResourceSet(IProject project) {
-			ResourceSet resourceSet = projectToResourceSet.get(project);
-			if (resourceSet == null) {
-				resourceSet = createResourceSet(project);
-				projectToResourceSet.put(project, resourceSet);
-			}
-			return resourceSet;
-		}
-
-		protected IProject getProject(IStorage storage) {
-			if (storage instanceof IResource) {
-				return ((IResource) storage).getProject();
-			} else if (storage instanceof IJarEntryResource) {
-				IPackageFragmentRoot packageFragmentRoot = ((IJarEntryResource) storage)
-						.getPackageFragmentRoot();
-				IJavaProject javaProject = packageFragmentRoot.getJavaProject();
-				return javaProject.getProject();
-			}
-			return null;
-		}
-
-		protected void acceptMatch(Object element, ITextRegion region) {
-			if (region != null)
-				requestor.reportMatch(new Match(element, region.getOffset(), region.getLength()));
-			else
-				requestor.reportMatch(new Match(element, 0, 0));
-		}
-		
-		protected ResourceSet createResourceSet(IProject project) {
-			ResourceSet result = resourceSetProvider.get(project);
-			externalContentSupport.configureResourceSet(result, dirtyStateManager);
-			return result;
-		}
+		JavaSearchHelper searchHelper = javaSearchHelperProvider.get();
+		searchHelper.init(requestor, resourceDescriptions);
+		return searchHelper;
 	}
 
 	public int estimateTicks(QuerySpecification query) {
@@ -265,82 +112,5 @@ public class QueryParticipant implements IQueryParticipant {
 		//			}
 		//		}
 		return false;
-	}
-
-	protected static class UIParticipant implements IMatchPresentation {
-
-		private final GlobalDescriptionLabelProvider globalDescriptionsLabelProvider;
-
-		private final IURIEditorOpener uriEditorOpener;
-
-		private final IResourceDescriptions resourceDescriptions;
-
-		public UIParticipant(IResourceDescriptions resourceDescriptions) {
-			this.resourceDescriptions = resourceDescriptions;
-			uriEditorOpener = Access.getIURIEditorOpener().get();
-			this.globalDescriptionsLabelProvider = Access.provider(GlobalDescriptionLabelProvider.class).get();
-		}
-
-		public ILabelProvider createLabelProvider() {
-			return new ILabelProvider() {
-				public void removeListener(ILabelProviderListener listener) {
-				}
-
-				public boolean isLabelProperty(Object element, String property) {
-					return false;
-				}
-
-				public void dispose() {
-				}
-
-				public void addListener(ILabelProviderListener listener) {
-				}
-
-				public String getText(Object element) {
-					IReferenceDescription referenceDescription = (IReferenceDescription) element;
-					URI containerEObjectURI = referenceDescription.getContainerEObjectURI();
-					final URI eObjectURI = (containerEObjectURI == null) ? referenceDescription.getSourceEObjectUri()
-							: containerEObjectURI;
-					IResourceDescription resourceDescription = resourceDescriptions.getResourceDescription(eObjectURI
-							.trimFragment());
-					if (resourceDescription != null) {
-						for (IEObjectDescription eObjectDescription : resourceDescription.getExportedObjects()) {
-							if (eObjectDescription.getEObjectURI().equals(eObjectURI)) {
-								return globalDescriptionsLabelProvider.getText(eObjectDescription);
-							}
-						}
-					}
-					return "unknown";
-				}
-
-				public Image getImage(Object element) {
-					IReferenceDescription referenceDescription = (IReferenceDescription) element;
-					URI containerEObjectURI = referenceDescription.getContainerEObjectURI();
-					final URI eObjectURI = (containerEObjectURI == null) ? referenceDescription.getSourceEObjectUri()
-							: containerEObjectURI;
-					IResourceDescription resourceDescription = resourceDescriptions.getResourceDescription(eObjectURI
-							.trimFragment());
-					if (resourceDescription != null) {
-						for (IEObjectDescription eObjectDescription : resourceDescription.getExportedObjects()) {
-							if (eObjectDescription.getEObjectURI().equals(eObjectURI)) {
-								return globalDescriptionsLabelProvider.getImage(eObjectDescription);
-							}
-						}
-					}
-					return null;
-				}
-			};
-		}
-
-		public void showMatch(Match match, int currentOffset, int currentLength, boolean activate) {
-			Object o = match.getElement();
-			if (o instanceof IReferenceDescription) {
-				IReferenceDescription descr = (IReferenceDescription) o;
-				if (activate) {
-					uriEditorOpener.open(descr.getSourceEObjectUri(), descr.getEReference(), descr.getIndexInList(),
-							true);
-				}
-			}
-		}
 	}
 }
