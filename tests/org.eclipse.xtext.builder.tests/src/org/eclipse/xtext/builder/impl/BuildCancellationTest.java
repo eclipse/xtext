63a0e72af667112fbb68fd4209e2cda693d6261d
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2010-12-13 10:06:39 +0100

    [builder.tests] Refactored builder participant tests

--- tests/org.eclipse.xtext.builder.tests/src/org/eclipse/xtext/builder/impl/BuildCancellationTest.java
+++ tests/org.eclipse.xtext.builder.tests/src/org/eclipse/xtext/builder/impl/BuildCancellationTest.java
@@ -16,35 +16,18 @@ import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.xtext.builder.IXtextBuilderParticipant;
-import org.eclipse.xtext.builder.IXtextBuilderParticipant.BuildType;
-import org.eclipse.xtext.builder.tests.Activator;
-import org.eclipse.xtext.builder.tests.LoggingBuilderParticipant;
 import org.eclipse.xtext.ui.XtextProjectHelper;
 import org.eclipse.xtext.util.StringInputStream;
 
 /**
  * @author Knut Wannheden - Initial contribution and API
  */
-public class BuildCancellationTest extends AbstractBuilderTest {
-
-	private LoggingBuilderParticipant participant;
+public class BuildCancellationTest extends AbstractParticipatingBuilderTest {
 
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		IXtextBuilderParticipant instance = Activator.getInstance()
-				.getInjector("org.eclipse.xtext.builder.tests.BuilderTestLanguage")
-				.getInstance(IXtextBuilderParticipant.class);
-		participant = (LoggingBuilderParticipant) instance;
-		participant.startLogging();
-	}
-
-	@Override
-	protected void tearDown() throws Exception {
-		participant.stopLogging();
-		participant = null;
-		super.tearDown();
+		startLogging();
 	}
 
 	/** see https://bugs.eclipse.org/bugs/show_bug.cgi?id=325814 */
@@ -55,11 +38,11 @@ public class BuildCancellationTest extends AbstractBuilderTest {
 		IFile file = folder.getFile("Foo" + F_EXT);
 		file.create(new StringInputStream("object Foo"), true, monitor());
 		waitForAutoBuild();
-		participant.reset();
+		reset();
 
 		OperationCanceledException expected = new OperationCanceledException();
 		try {
-			participant.cancel(expected);
+			cancel(expected);
 			project.getProject().build(IncrementalProjectBuilder.FULL_BUILD, monitor());
 			fail("Expected OperationCanceledException");
 		} catch (OperationCanceledException e) {
@@ -67,13 +50,13 @@ public class BuildCancellationTest extends AbstractBuilderTest {
 		} catch (Exception e) {
 			fail("Expected OperationCanceledException");
 		}
-		participant.reset();
+		reset();
 		ResourcesPlugin.getWorkspace().build(
 				IncrementalProjectBuilder.AUTO_BUILD, monitor());
 		waitForAutoBuild();
-		assertEquals(1, participant.getInvocationCount());
-		assertSame(BuildType.FULL, participant.getContext().getBuildType());
-		participant.reset();
+		assertEquals(1, getInvocationCount());
+		assertSame(BuildType.FULL, getContext().getBuildType());
+		reset();
 	}
 
 }
--- tests/org.eclipse.xtext.builder.tests/src/org/eclipse/xtext/builder/impl/BuilderParticipantTest.java
+++ tests/org.eclipse.xtext.builder.tests/src/org/eclipse/xtext/builder/impl/BuilderParticipantTest.java
@@ -10,66 +10,118 @@ package org.eclipse.xtext.builder.impl;
 import static org.eclipse.xtext.ui.junit.util.IResourcesSetupUtil.*;
 import static org.eclipse.xtext.ui.junit.util.JavaProjectSetupUtil.*;
 
+import java.util.Collection;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.xtext.builder.IXtextBuilderParticipant;
-import org.eclipse.xtext.builder.IXtextBuilderParticipant.BuildType;
-import org.eclipse.xtext.builder.tests.Activator;
-import org.eclipse.xtext.builder.tests.LoggingBuilderParticipant;
+import org.eclipse.xtext.resource.IResourceDescription;
 import org.eclipse.xtext.ui.XtextProjectHelper;
 import org.eclipse.xtext.util.StringInputStream;
+
+import com.google.common.collect.Lists;
 
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
  */
-public class BuilderParticipantTest extends AbstractBuilderTest {
+public class BuilderParticipantTest extends AbstractParticipatingBuilderTest {
 
-	private LoggingBuilderParticipant participant;
+	private Collection<IBuildContext> contexts;
 	
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		IXtextBuilderParticipant instance = Activator.getInstance().getInjector(
-				"org.eclipse.xtext.builder.tests.BuilderTestLanguage").getInstance(IXtextBuilderParticipant.class);
-		participant = (LoggingBuilderParticipant) instance;
-		participant.startLogging();
+		contexts = Lists.newArrayList();
+	}
+
+	@Override
+	public void reset() {
+		contexts.clear();
+		super.reset();
 	}
 	
 	@Override
-	protected void tearDown() throws Exception {
-		participant.stopLogging();
-		participant = null;
-		super.tearDown();
+	public void build(IBuildContext context, IProgressMonitor monitor) throws CoreException {
+		super.build(context, monitor);
+		if (isLogging()) {
+			contexts.add(context);
+		}
+	}
+
+	protected void validateContext(IBuildContext context) {
+		IProject project = context.getBuiltProject();
+		String prefix = "platform:/resource/" + project.getName();
+		for(IResourceDescription.Delta delta: context.getDeltas()) {
+			assertTrue("Exptected uri '" + delta.getUri() + "' starts with '" + prefix + "'", 
+					delta.getUri().toString().startsWith(prefix));
+		}
 	}
 	
 	public void testParticipantInvoked() throws Exception {
+		startLogging();
 		IJavaProject project = createJavaProject("foo");
 		addNature(project.getProject(), XtextProjectHelper.NATURE_ID);
 		IFolder folder = project.getProject().getFolder("src");
 		IFile file = folder.getFile("Foo" + F_EXT);
 		file.create(new StringInputStream("object Foo"), true, monitor());
 		waitForAutoBuild();
-		assertTrue(0 < participant.getInvocationCount());
-		participant.reset();
+		assertTrue(0 < getInvocationCount());
+		validateContexts();
+		reset();
 		
 		file.delete(true, monitor());
 		waitForAutoBuild();
-		assertEquals(1, participant.getInvocationCount());
-		assertSame(BuildType.INCREMENTAL, participant.getContext().getBuildType());
-		participant.reset();
+		assertEquals(1, getInvocationCount());
+		assertSame(BuildType.INCREMENTAL, getContext().getBuildType());
+		validateContexts();
+		reset();
 		
 		project.getProject().build(IncrementalProjectBuilder.CLEAN_BUILD, monitor());
-		assertSame(BuildType.CLEAN, participant.getContext().getBuildType());
+		assertSame(BuildType.CLEAN, getContext().getBuildType());
 		waitForAutoBuild();
-		assertEquals(2, participant.getInvocationCount());
-		assertSame(BuildType.FULL, participant.getContext().getBuildType());
-		participant.reset();
+		assertEquals(2, getInvocationCount());
+		assertSame(BuildType.FULL, getContext().getBuildType());
+		validateContexts();
+		reset();
 		
 		project.getProject().build(IncrementalProjectBuilder.FULL_BUILD, monitor());
-		assertSame(BuildType.FULL, participant.getContext().getBuildType());
-		participant.reset();
+		assertSame(BuildType.FULL, getContext().getBuildType());
+		validateContexts();
+		reset();
+	}
+	
+	private void validateContexts() {
+		for(IBuildContext context: contexts) {
+			validateContext(context);
+		}
+	}
+
+	public void testTwoFilesInTwoReferencedProjects() throws Exception {
+		createTwoReferencedProjects();
+		IFile firstFile = createFile("first/src/first"+F_EXT, "object First ");
+		createFile("second/src/second"+F_EXT, "object Second references First");
+		waitForAutoBuild();
+		startLogging();
+		firstFile.setContents(new StringInputStream("object Modified "), true, true, monitor());
+		waitForAutoBuild();
+		validateContexts();
+		assertEquals(2, getInvocationCount());
+	}
+
+	protected void createTwoReferencedProjects() throws CoreException {
+		IJavaProject firstProject = createJavaProjectWithRootSrc("first");
+		IJavaProject secondProject = createJavaProjectWithRootSrc("second");
+		addProjectReference(secondProject, firstProject);
+	}
+	
+	protected IJavaProject createJavaProjectWithRootSrc(String string) throws CoreException {
+		IJavaProject project = createJavaProject(string);
+		addNature(project.getProject(), XtextProjectHelper.NATURE_ID);
+		return project;
 	}
 	
 }
