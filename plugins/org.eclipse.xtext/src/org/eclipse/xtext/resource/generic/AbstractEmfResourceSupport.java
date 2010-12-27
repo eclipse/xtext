af98bd2c65959d5ab912c6b8e63b2de203c5319f
Jan Koehnlein <jan.koehnlein@itemis.de>
2010-12-27 18:21:19 +0100

    Extracted generic EMF ResourceServices

---
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/resource/generic/AbstractEmfResourceSupport.java
@@ -0,0 +1,87 @@
+/*******************************************************************************
+ * Copyright (c) 2010 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.resource.generic;
+
+import org.eclipse.emf.mwe2.runtime.workflow.IWorkflowComponent;
+import org.eclipse.emf.mwe2.runtime.workflow.IWorkflowContext;
+import org.eclipse.xtext.Constants;
+import org.eclipse.xtext.resource.IResourceServiceProvider;
+
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.google.inject.name.Named;
+
+/**
+ * 
+ * This class is intended to be used from an MWE workflow.
+ * 
+ * It instantiates and registers the Ecore support for Xtext, which allows for referencing ecore models from any Xtext
+ * language.
+ * 
+ * Usage:
+ * 
+ * <pre>
+ *    component = org.eclipse.xtext.ecore.EcoreSupport{}
+ * </pre>
+ * 
+ * If you want to provide a different guice guiceModule than the default one ({@link EcoreRuntimeModule}) in order to
+ * change any implementation classes, you can make use of the property guiceModule. E.g. :
+ * 
+ * <pre>
+ *    component = org.eclipse.xtext.ecore.EcoreSupport{
+ *       guiceModule = my.special.CustomizedEcoreRuntimeModule {}
+ *    }
+ * </pre>
+ * 
+ * @author Sven Efftinge - Initial contribution and API
+ */
+public abstract class AbstractEmfResourceSupport implements IWorkflowComponent {
+
+	@Inject
+	@Named(Constants.FILE_EXTENSIONS)
+	private String fileExtension;
+
+	@Inject
+	private IResourceServiceProvider resourceServiceProvider;
+
+	@Inject
+	private IResourceServiceProvider.Registry registry;
+
+	public void preInvoke() {
+		Injector injector = Guice.createInjector(getGuiceModule());
+		injector.injectMembers(this);
+		registerInRegistry();
+	}
+
+	private Module guiceModule;
+
+	public void setGuiceModule(Module module) {
+		this.guiceModule = module;
+	}
+
+	protected Module getGuiceModule() {
+		if (guiceModule == null)
+			guiceModule = createGuiceModule();
+		return guiceModule;
+	}
+
+	protected abstract Module createGuiceModule();
+
+	protected void registerInRegistry() {
+		registry.getExtensionToFactoryMap().put(fileExtension, resourceServiceProvider);
+	}
+
+	public void invoke(IWorkflowContext ctx) {
+	}
+
+	public void postInvoke() {
+	}
+
+}
--- tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/resource/ecore/EmfResourceDescriptionManagerTest.java
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/resource/ecore/EmfResourceDescriptionManagerTest.java
@@ -13,26 +13,40 @@ import java.util.Map;
 
 import junit.framework.TestCase;
 
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.ENamedElement;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EcoreFactory;
 import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.eclipse.xtext.ecore.EcoreResourceDescriptionManager;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
+import org.eclipse.xtext.ecore.EcoreRuntimeModule;
 import org.eclipse.xtext.naming.QualifiedName;
 import org.eclipse.xtext.resource.IEObjectDescription;
 import org.eclipse.xtext.resource.IResourceDescription;
+import org.eclipse.xtext.resource.IResourceDescription.Delta;
+import org.eclipse.xtext.resource.generic.EmfResourceDescriptionManager;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 
 /**
  * @author Sven Efftinge - Initial contribution and API
  */
-public class EcoreResourceDescriptionManagerTest extends TestCase {
+public class EmfResourceDescriptionManagerTest extends TestCase {
+	
+	private EmfResourceDescriptionManager getEmfResourceDescriptionsManager() {
+		Injector injector = Guice.createInjector(new EcoreRuntimeModule());
+		return injector.getInstance(EmfResourceDescriptionManager.class);
+	}
 	
 	public void testNaming() throws Exception {
-		EcoreResourceDescriptionManager underTest = new EcoreResourceDescriptionManager();
+		EmfResourceDescriptionManager underTest = getEmfResourceDescriptionsManager();
 		IResourceDescription description = underTest.getResourceDescription(EcorePackage.eINSTANCE.eResource());
 		
 		Map<QualifiedName,IEObjectDescription> index = Maps.newHashMap();
@@ -43,9 +57,9 @@ public class EcoreResourceDescriptionManagerTest extends TestCase {
 		assertEquals(EcorePackage.eINSTANCE, index.get(QualifiedName.create("ecore")).getEObjectOrProxy());
 		assertEquals(EcorePackage.Literals.ECLASS, index.get(QualifiedName.create("EClass")).getEObjectOrProxy());
 	}
-
+	
 	public void testPerformance() throws Exception {
-		EcoreResourceDescriptionManager manager = new EcoreResourceDescriptionManager();
+		EmfResourceDescriptionManager manager = getEmfResourceDescriptionsManager();
 		Collection<String> uris = ImmutableList.copyOf(EPackage.Registry.INSTANCE.keySet());
 		for(String uri: uris) {
 			EPackage pack = EPackage.Registry.INSTANCE.getEPackage(uri);
@@ -64,5 +78,27 @@ public class EcoreResourceDescriptionManagerTest extends TestCase {
 				}
 			}
 		}
+	}
+	
+	public void testCreateDeltaAndIsAffected() {
+		EmfResourceDescriptionManager manager = getEmfResourceDescriptionsManager();
+		EClass eClass = EcoreFactory.eINSTANCE.createEClass();
+		eClass.setName("Test");
+		eClass.getESuperTypes().add(EcorePackage.Literals.EPACKAGE);
+		Resource resource = new XMIResourceImpl(URI.createFileURI("test.ecore"));
+		resource.getContents().add(eClass);
+
+		EPackage copyOfEPackage = (EPackage) EcoreUtil.copy(EcorePackage.eINSTANCE);
+		Resource ecoreResource = new XMIResourceImpl(URI.createURI(copyOfEPackage.getNsURI()));
+		ecoreResource.getContents().add(copyOfEPackage);
+		IResourceDescription oldDescription = manager.getResourceDescription(ecoreResource);
+		copyOfEPackage.setName("ecore_new");
+		IResourceDescription newDescription = manager.getResourceDescription(ecoreResource);
+		
+		Delta delta = manager.createDelta(oldDescription, newDescription);
+		assertTrue(delta.haveEObjectDescriptionsChanged());
+		
+		IResourceDescription referrerDescription = manager.getResourceDescription(resource);
+		assertTrue(manager.isAffected(delta, referrerDescription));
 	}
 }
