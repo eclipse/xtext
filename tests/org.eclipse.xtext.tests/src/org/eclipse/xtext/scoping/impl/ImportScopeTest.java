8f8a43f9e93a4ecc15733627b0970f217c3b3c87
Sven Efftinge <sven.efftinge@itemis.de>
2011-01-03 20:22:24 +0100

    [Xtext] ImportScope supports selection of multiple elements by name, fixed broken toString()

--- tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/scoping/impl/ImportScopeTest.java
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/scoping/impl/ImportScopeTest.java
@@ -7,15 +7,21 @@
  *******************************************************************************/
 package org.eclipse.xtext.scoping.impl;
 
+import static com.google.common.collect.Iterables.*;
 import static com.google.common.collect.Lists.*;
 import static org.eclipse.xtext.scoping.Selectors.*;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.xtext.naming.QualifiedName;
 import org.eclipse.xtext.resource.EObjectDescription;
 import org.eclipse.xtext.resource.IEObjectDescription;
+import org.eclipse.xtext.resource.ISelectable;
+import org.eclipse.xtext.resource.impl.AliasedEObjectDescription;
 import org.eclipse.xtext.scoping.IScope;
 import org.eclipse.xtext.scoping.ISelector;
 
@@ -26,13 +32,38 @@ import junit.framework.TestCase;
  */
 public class ImportScopeTest extends TestCase {
 	
+	/**
+	 * subclassed just to make protected members accessible from within this OSGi bundle
+	 */
+	static class TestableImportScope extends ImportScope {
+		public TestableImportScope(List<ImportNormalizer> namespaceResolvers, IScope parent, ISelectable importFrom,
+				EClass type, boolean ignoreCase) {
+			super(namespaceResolvers, parent, importFrom, type, ignoreCase);
+		}
+		
+		@Override
+		protected Iterable<IEObjectDescription> getLocalElementsByName(QualifiedName name) {
+			return super.getLocalElementsByName(name);
+		}
+		
+		@Override
+		protected Iterable<IEObjectDescription> getAllLocalElements() {
+			return super.getAllLocalElements();
+		}
+		
+		@Override
+		protected Iterable<IEObjectDescription> getAliasedElements(Iterable<IEObjectDescription> candidates) {
+			return super.getAliasedElements(candidates);
+		}
+	}
+	
 	public void testImports_01() throws Exception {
 		final IEObjectDescription desc1 = EObjectDescription.create(QualifiedName.create("com","foo","bar"), EcorePackage.Literals.EANNOTATION);
 		final IEObjectDescription desc2 = EObjectDescription.create(QualifiedName.create("de","foo"), EcorePackage.Literals.EATTRIBUTE);
 		SimpleScope outer = new SimpleScope(newArrayList(desc1,desc2), false);
 		ImportNormalizer n1 = new ImportNormalizer(QualifiedName.create("com"), true);
 		ImportNormalizer n2 = new ImportNormalizer(QualifiedName.create("de","foo"), false);
-		ImportScope scope = new ImportScope(newArrayList(n1,n2), outer, new ScopeBasedSelectable(outer), EcorePackage.Literals.EOBJECT, false);
+		TestableImportScope scope = new TestableImportScope(newArrayList(n1,n2), outer, new ScopeBasedSelectable(outer), EcorePackage.Literals.EOBJECT, false);
 		
 		final Iterable<IEObjectDescription> elements = scope.getElements(ISelector.SELECT_ALL);
 		Iterator<IEObjectDescription> iterator = elements.iterator();
@@ -49,11 +80,11 @@ public class ImportScopeTest extends TestCase {
 		IScope outer = new SimpleScope(newArrayList(desc1,desc2), false);
 		ImportNormalizer n1 = new ImportNormalizer(QualifiedName.create("com"), true);
 		ImportNormalizer n2 = new ImportNormalizer(QualifiedName.create("de"), true);
-		outer = new ImportScope(newArrayList(n1,n2), outer, new ScopeBasedSelectable(outer), EcorePackage.Literals.EOBJECT, false);
+		outer = new TestableImportScope(newArrayList(n1,n2), outer, new ScopeBasedSelectable(outer), EcorePackage.Literals.EOBJECT, false);
 		
 		n1 = new ImportNormalizer(QualifiedName.create("foo"), true);
 		n2 = new ImportNormalizer(QualifiedName.create("foo"), false);
-		ImportScope scope = new ImportScope(newArrayList(n1,n2), outer, new ScopeBasedSelectable(outer), EcorePackage.Literals.EOBJECT, false);
+		TestableImportScope scope = new TestableImportScope(newArrayList(n1,n2), outer, new ScopeBasedSelectable(outer), EcorePackage.Literals.EOBJECT, false);
 		
 		final Iterable<IEObjectDescription> elements = scope.getElements(ISelector.SELECT_ALL);
 		Iterator<IEObjectDescription> iterator = elements.iterator();
@@ -71,7 +102,7 @@ public class ImportScopeTest extends TestCase {
 		SimpleScope outer = new SimpleScope(newArrayList(desc1,desc2), false);
 		ImportNormalizer n1 = new ImportNormalizer(QualifiedName.create("com"), true);
 		ImportNormalizer n2 = new ImportNormalizer(QualifiedName.create("de"), true);
-		ImportScope scope = new ImportScope(newArrayList(n1,n2), outer, new ScopeBasedSelectable(outer), EcorePackage.Literals.EOBJECT, false);
+		TestableImportScope scope = new TestableImportScope(newArrayList(n1,n2), outer, new ScopeBasedSelectable(outer), EcorePackage.Literals.EOBJECT, false);
 		
 		Iterator<IEObjectDescription> iterator = scope.getElements(ISelector.SELECT_ALL).iterator();
 		assertSame(desc1,iterator.next());
@@ -85,7 +116,7 @@ public class ImportScopeTest extends TestCase {
 		SimpleScope outer = new SimpleScope(newArrayList(desc1,desc2), true);
 		ImportNormalizer n1 = new ImportNormalizer(QualifiedName.create("com"), true);
 		ImportNormalizer n2 = new ImportNormalizer(QualifiedName.create("de"), true);
-		ImportScope scope = new ImportScope(newArrayList(n1,n2), outer, new ScopeBasedSelectable(outer), EcorePackage.Literals.EOBJECT, true);
+		TestableImportScope scope = new TestableImportScope(newArrayList(n1,n2), outer, new ScopeBasedSelectable(outer), EcorePackage.Literals.EOBJECT, true);
 		
 		Iterator<IEObjectDescription> iterator = scope.getElements(ISelector.SELECT_ALL).iterator();
 		assertSame(desc1,iterator.next());
@@ -99,7 +130,7 @@ public class ImportScopeTest extends TestCase {
 		SimpleScope outer = new SimpleScope(newArrayList(desc1,desc2), false);
 		ImportNormalizer n1 = new ImportNormalizer(QualifiedName.create("com"), true);
 		ImportNormalizer n2 = new ImportNormalizer(QualifiedName.create("de"), true);
-		ImportScope scope = new ImportScope(newArrayList(n1,n2), outer, new ScopeBasedSelectable(outer), EcorePackage.Literals.EOBJECT, false);
+		TestableImportScope scope = new TestableImportScope(newArrayList(n1,n2), outer, new ScopeBasedSelectable(outer), EcorePackage.Literals.EOBJECT, false);
 		
 		assertNull(scope.getSingleElement(selectByName(QualifiedName.create("foo"))));
 		assertNotNull(scope.getSingleElement(selectByName(QualifiedName.create("com","foo"))));
@@ -112,10 +143,72 @@ public class ImportScopeTest extends TestCase {
 		SimpleScope outer = new SimpleScope(newArrayList(desc1,desc2), true);
 		ImportNormalizer n1 = new ImportNormalizer(QualifiedName.create("com"), true);
 		ImportNormalizer n2 = new ImportNormalizer(QualifiedName.create("de"), true);
-		ImportScope scope = new ImportScope(newArrayList(n1,n2), outer, new ScopeBasedSelectable(outer), EcorePackage.Literals.EOBJECT, true);
+		TestableImportScope scope = new TestableImportScope(newArrayList(n1,n2), outer, new ScopeBasedSelectable(outer), EcorePackage.Literals.EOBJECT, true);
 		
 		assertNull(scope.getSingleElement(selectByName(QualifiedName.create("Foo"))));
 		assertNotNull(scope.getSingleElement(selectByName(QualifiedName.create("com","Foo"))));
 		assertNotNull(scope.getSingleElement(selectByName(QualifiedName.create("de","Foo"))));
+	}
+	
+	public void testMultipleElementsByName_00() throws Exception {
+		final IEObjectDescription desc1 = new EObjectDescription(QualifiedName.create("com","foo"), EcorePackage.Literals.EANNOTATION, null);
+		final IEObjectDescription desc2 = new EObjectDescription(QualifiedName.create("com","foo"), EcorePackage.Literals.EATTRIBUTE, null);
+		SimpleScope outer = new SimpleScope(newArrayList(desc1,desc2), true);
+		ImportNormalizer n1 = new ImportNormalizer(QualifiedName.create("com"), true);
+		ImportNormalizer n2 = new ImportNormalizer(QualifiedName.create("de"), true);
+		TestableImportScope scope = new TestableImportScope(newArrayList(n1,n2), outer, new ScopeBasedSelectable(outer), EcorePackage.Literals.EOBJECT, true);
+		
+		assertNotNull(scope.getSingleElement(selectByName(QualifiedName.create("Foo"))));
+		Iterable<IEObjectDescription> elements = scope.getLocalElementsByName(QualifiedName.create("Foo"));
+		assertEquals(2,size(elements));
+	}
+	
+	public void testGetAllLocalElements_00() throws Exception {
+		final IEObjectDescription desc1 = new EObjectDescription(QualifiedName.create("com","foo"), EcorePackage.Literals.EANNOTATION, null);
+		final IEObjectDescription desc2 = new EObjectDescription(QualifiedName.create("com","foo"), EcorePackage.Literals.EATTRIBUTE, null);
+		SimpleScope outer = new SimpleScope(newArrayList(desc1,desc2), true);
+		ImportNormalizer n1 = new ImportNormalizer(QualifiedName.create("com"), true);
+		ImportNormalizer n2 = new ImportNormalizer(QualifiedName.create("de"), true);
+		TestableImportScope scope = new TestableImportScope(newArrayList(n1,n2), outer, new ScopeBasedSelectable(outer), EcorePackage.Literals.EOBJECT, true);
+		
+		Iterable<IEObjectDescription> elements = scope.getAllLocalElements();
+		assertEquals(2,size(elements));
+	}
+	
+	public void testAllAliasedElements_00() throws Exception {
+		final IEObjectDescription desc1 = new EObjectDescription(QualifiedName.create("com","foo"), EcorePackage.Literals.EANNOTATION, null);
+		final IEObjectDescription desc2 = new EObjectDescription(QualifiedName.create("com","foo"), EcorePackage.Literals.EATTRIBUTE, null);
+		final ArrayList<IEObjectDescription> newArrayList = newArrayList(desc1,desc2);
+		ImportNormalizer n1 = new ImportNormalizer(QualifiedName.create("com"), true);
+		ImportNormalizer n2 = new ImportNormalizer(QualifiedName.create("de"), true);
+		TestableImportScope scope = new TestableImportScope(newArrayList(n1,n2), IScope.NULLSCOPE, null, EcorePackage.Literals.EOBJECT, true);
+		Iterable<IEObjectDescription> elements = scope.getAliasedElements(newArrayList);
+		assertEquals(2,size(elements));
+		Iterator<IEObjectDescription> iterator = elements.iterator();
+		assertSame(desc1,((AliasedEObjectDescription)iterator.next()).getAliasedEObjectDescription());
+		assertSame(desc2,((AliasedEObjectDescription)iterator.next()).getAliasedEObjectDescription());
+	}
+	
+	public void testAllAliasedElements_01() throws Exception {
+		final IEObjectDescription desc1 = new EObjectDescription(QualifiedName.create("com","foo"), EcorePackage.Literals.EANNOTATION, null);
+		final IEObjectDescription desc2 = new EObjectDescription(QualifiedName.create("de","foo"), EcorePackage.Literals.EATTRIBUTE, null);
+		final ArrayList<IEObjectDescription> newArrayList = newArrayList(desc1,desc2);
+		ImportNormalizer n1 = new ImportNormalizer(QualifiedName.create("com"), true);
+		ImportNormalizer n2 = new ImportNormalizer(QualifiedName.create("de"), true);
+		TestableImportScope scope = new TestableImportScope(newArrayList(n1,n2), IScope.NULLSCOPE, null, EcorePackage.Literals.EOBJECT, true);
+		Iterable<IEObjectDescription> elements = scope.getAliasedElements(newArrayList);
+		assertEquals(0,size(elements));
+	}
+	public void testAllAliasedElements_02() throws Exception {
+		final IEObjectDescription desc1 = new EObjectDescription(QualifiedName.create("com","foo"), EcorePackage.Literals.EANNOTATION, null);
+		final IEObjectDescription desc2 = new EObjectDescription(QualifiedName.create("com","bar"), EcorePackage.Literals.EANNOTATION, null);
+		final IEObjectDescription desc3 = new EObjectDescription(QualifiedName.create("de","foo"), EcorePackage.Literals.EATTRIBUTE, null);
+		final ArrayList<IEObjectDescription> newArrayList = newArrayList(desc1,desc2,desc3);
+		ImportNormalizer n1 = new ImportNormalizer(QualifiedName.create("com"), true);
+		ImportNormalizer n2 = new ImportNormalizer(QualifiedName.create("de"), true);
+		TestableImportScope scope = new TestableImportScope(newArrayList(n1,n2), IScope.NULLSCOPE, null, EcorePackage.Literals.EOBJECT, true);
+		Iterable<IEObjectDescription> elements = scope.getAliasedElements(newArrayList);
+		assertEquals(1,size(elements));
+		assertSame(desc2,((AliasedEObjectDescription)elements.iterator().next()).getAliasedEObjectDescription());
 	}
 }
