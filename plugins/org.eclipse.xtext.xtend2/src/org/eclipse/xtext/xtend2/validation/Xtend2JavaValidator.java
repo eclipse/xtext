50e7dbd42160d00ae187752e2c83544c12cc7a1b
Jan Koehnlein <jan.koehnlein@itemis.de>
2011-01-13 18:05:47 +0100

    Fixed Xtend2 validation

--- plugins/org.eclipse.xtext.xtend2/src/org/eclipse/xtext/xtend2/validation/Xtend2JavaValidator.java
+++ plugins/org.eclipse.xtext.xtend2/src/org/eclipse/xtext/xtend2/validation/Xtend2JavaValidator.java
@@ -1,17 +1,19 @@
 package org.eclipse.xtext.xtend2.validation;
 
-import static java.util.Collections.*;
+import static com.google.common.collect.Lists.*;
 
 import java.util.List;
 
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.xtext.xbase.XbasePackage;
+import org.eclipse.xtext.xbase.validation.XbaseJavaValidator;
 import org.eclipse.xtext.xtend2.xtend2.Xtend2Package;
  
-public class Xtend2JavaValidator extends AbstractXtend2JavaValidator {
+public class Xtend2JavaValidator extends XbaseJavaValidator {
 
 	@Override
 	protected List<EPackage> getEPackages() {
-		return singletonList((EPackage)Xtend2Package.eINSTANCE);
+		return newArrayList(Xtend2Package.eINSTANCE, XbasePackage.eINSTANCE);
 	}
 
 }
