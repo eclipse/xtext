0075eeed55a4b6d68095d057d713973b3b967c98
Jan Koehnlein <jan.koehnlein@itemis.de>
2011-01-21 13:49:45 +0100

    Naming conventions for Xtend files, packages and classes

--- plugins/org.eclipse.xtext.xtend2/src/org/eclipse/xtext/xtend2/validation/Xtend2JavaValidator.java
+++ plugins/org.eclipse.xtext.xtend2/src/org/eclipse/xtext/xtend2/validation/Xtend2JavaValidator.java
@@ -1,19 +1,61 @@
 package org.eclipse.xtext.xtend2.validation;
 
 import static com.google.common.collect.Lists.*;
+import static org.eclipse.xtext.util.Strings.*;
+import static org.eclipse.xtext.xtend2.xtend2.Xtend2Package.*;
 
 import java.util.List;
 
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.URIConverter;
+import org.eclipse.xtext.resource.ClasspathUriResolutionException;
+import org.eclipse.xtext.resource.ClasspathUriUtil;
+import org.eclipse.xtext.validation.Check;
 import org.eclipse.xtext.xbase.XbasePackage;
 import org.eclipse.xtext.xbase.validation.XbaseJavaValidator;
 import org.eclipse.xtext.xtend2.xtend2.Xtend2Package;
- 
+import org.eclipse.xtext.xtend2.xtend2.XtendClass;
+import org.eclipse.xtext.xtend2.xtend2.XtendFile;
+
 public class Xtend2JavaValidator extends XbaseJavaValidator {
+
+	public static final String WRONG_PACKAGE = Xtend2JavaValidator.class.getName() + ".wrong_package";
+	public static final String WRONG_FILE = Xtend2JavaValidator.class.getName() + ".wrong_file";
 
 	@Override
 	protected List<EPackage> getEPackages() {
 		return newArrayList(Xtend2Package.eINSTANCE, XbasePackage.eINSTANCE);
 	}
 
+	@Check
+	public void checkFileNamingConventions(XtendFile xtendFile) {
+		Resource resource = xtendFile.eResource();
+		URI resourceURI = resource.getURI();
+		String packageName = xtendFile.getPackage();
+		StringBuilder classpathURIBuilder = new StringBuilder(ClasspathUriUtil.CLASSPATH_SCHEME);
+		classpathURIBuilder.append(":/");
+		if (packageName != null)
+			classpathURIBuilder.append(packageName.replace(".", "/")).append("/");
+		classpathURIBuilder.append(resourceURI.lastSegment());
+		URI classpathURI = URI.createURI(classpathURIBuilder.toString());
+		URIConverter uriConverter = resource.getResourceSet().getURIConverter();
+		try {
+			URI normalizedURI = uriConverter.normalize(classpathURI);
+			if(!resourceURI.equals(normalizedURI))
+				reportInvalidPackage(packageName, classpathURI);
+		} catch(ClasspathUriResolutionException e) {
+			reportInvalidPackage(packageName, classpathURI);
+		}
+		XtendClass xtendClass = xtendFile.getClasses().get(0);
+		if (xtendClass != null && xtendClass.getName() != null
+				&& !equal(resourceURI.trimFileExtension().lastSegment(), xtendClass.getName()))
+			error("The class '" + notNull(packageName) + "." + xtendClass.getName() + "' must be defined in its own file",
+					xtendClass, XTEND_CLASS__NAME, WRONG_FILE);
+	}
+
+	protected void reportInvalidPackage(String packageName, URI classpathURI) {
+		error("The declared package '" + notNull(packageName) + "' does not match the expected package", XTEND_FILE__PACKAGE, WRONG_PACKAGE);
+	}
 }
--- tests/org.eclipse.xtext.xtend2.tests/src/org/eclipse/xtext/xtend2/tests/validation/Xtend2JavaValidatorTest.java
+++ tests/org.eclipse.xtext.xtend2.tests/src/org/eclipse/xtext/xtend2/tests/validation/Xtend2JavaValidatorTest.java
@@ -1,21 +1,59 @@
 package org.eclipse.xtext.xtend2.tests.validation;
 
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.xtext.junit.validation.ValidationTestHelper;
+import org.eclipse.xtext.resource.XtextResourceSet;
 import org.eclipse.xtext.xbase.XbasePackage;
 import org.eclipse.xtext.xbase.validation.XbaseJavaValidator;
 import org.eclipse.xtext.xtend2.tests.AbstractXtend2TestCase;
+import org.eclipse.xtext.xtend2.validation.Xtend2JavaValidator;
+import org.eclipse.xtext.xtend2.xtend2.Xtend2Package;
+import org.eclipse.xtext.xtend2.xtend2.XtendFile;
 import org.eclipse.xtext.xtend2.xtend2.XtendFunction;
 
 import com.google.inject.Inject;
 
 public class Xtend2JavaValidatorTest extends AbstractXtend2TestCase {
-	
+
 	@Inject
 	protected ValidationTestHelper helper;
-	
+
 	public void testAssignmentToFunctionParameter() throws Exception {
 		XtendFunction function = function("void foo(int bar) bar=7");
-		helper.assertError(function, XbasePackage.Literals.XASSIGNMENT, XbaseJavaValidator.ASSIGNMENT_TO_FINAL, "Assignment", "final", "parameter");
+		helper.assertError(function, XbasePackage.Literals.XASSIGNMENT, XbaseJavaValidator.ASSIGNMENT_TO_FINAL,
+				"Assignment", "final", "parameter");
+	}
+
+	public void testFileNamingConventions_0() throws Exception {
+		XtendFile xtendFile = loadExampleXtendFile();
+		helper.assertNoError(xtendFile, Xtend2JavaValidator.WRONG_PACKAGE);
+		helper.assertNoError(xtendFile, Xtend2JavaValidator.WRONG_FILE);
 	}
 	
+	public void testFileNamingConventions_1() throws Exception {
+		XtendFile xtendFile = loadExampleXtendFile();
+			xtendFile.getClasses().get(0).setName("Bar");
+		helper.assertNoError(xtendFile, Xtend2JavaValidator.WRONG_PACKAGE);
+		helper.assertError(xtendFile, Xtend2Package.Literals.XTEND_CLASS, Xtend2JavaValidator.WRONG_FILE);
+	}
+		
+	public void testFileNamingConventions_2() throws Exception {
+		XtendFile xtendFile = loadExampleXtendFile();
+		Resource resource = xtendFile.eResource();
+		URI resourceURI = resource.getURI();
+		URI invalidPackageURI = resourceURI.trimSegments(1).appendSegment("invalid").appendSegment(resourceURI.lastSegment());
+		resource.setURI(invalidPackageURI);
+		helper.assertError(xtendFile, Xtend2Package.Literals.XTEND_FILE, Xtend2JavaValidator.WRONG_PACKAGE);
+		helper.assertNoError(xtendFile, Xtend2JavaValidator.WRONG_FILE);
+	}
+	
+	protected XtendFile loadExampleXtendFile() {
+		XtextResourceSet resourceSet = new XtextResourceSet();
+		resourceSet.setClasspathURIContext(this);
+		URI classpathURI = URI.createURI("classpath:/test/Foo.xtend");
+		URI normalizedURI = resourceSet.getURIConverter().normalize(classpathURI);
+		Resource resource = resourceSet.getResource(normalizedURI, true);
+		return (XtendFile) resource.getContents().get(0);
+	}
 }
