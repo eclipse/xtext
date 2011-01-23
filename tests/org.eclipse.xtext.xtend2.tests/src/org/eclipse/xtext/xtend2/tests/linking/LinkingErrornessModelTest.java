c11bf8efa8588d48dd3479b421377a940801183d
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2011-01-23 11:52:55 +0100

    [xtend2.tests] Enhanced the brute force exception test

--- tests/org.eclipse.xtext.xtend2.tests/src/org/eclipse/xtext/xtend2/tests/linking/LinkingErrornessModelTest.java
+++ tests/org.eclipse.xtext.xtend2.tests/src/org/eclipse/xtext/xtend2/tests/linking/LinkingErrornessModelTest.java
@@ -8,38 +8,91 @@
 package org.eclipse.xtext.xtend2.tests.linking;
 
 import java.io.InputStream;
+import java.util.List;
 
+import org.eclipse.emf.common.util.BasicDiagnostic;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource.Diagnostic;
+import org.eclipse.xtext.diagnostics.ExceptionDiagnostic;
+import org.eclipse.xtext.diagnostics.Severity;
 import org.eclipse.xtext.linking.lazy.LazyLinkingResource;
 import org.eclipse.xtext.resource.XtextResourceSet;
 import org.eclipse.xtext.util.CancelIndicator;
 import org.eclipse.xtext.util.Files;
+import org.eclipse.xtext.util.IAcceptor;
 import org.eclipse.xtext.util.StringInputStream;
 import org.eclipse.xtext.validation.CheckMode;
+import org.eclipse.xtext.validation.IDiagnosticConverter;
+import org.eclipse.xtext.validation.Issue;
+import org.eclipse.xtext.validation.ResourceValidatorImpl;
 import org.eclipse.xtext.xtend2.tests.AbstractXtend2TestCase;
 
 /**
+ * A brute-force test class to ensure that parsing, linking and validation 
+ * don't throw exceptions on invalid models.
  * @author Sven Efftinge - Initial contribution and API
+ * @author Sebastian Zarnekow
  */
 public class LinkingErrornessModelTest extends AbstractXtend2TestCase {
 	
-	/**
-	 * just ensures that parsing, linking and validation don't throw exceptions on invlaid models.
-	 */
-	public void testParseErrornessModel() throws Exception {
-		final String name = "/"+getClass().getName().replace('.', '/')+"Data.xtend";
-		final InputStream resourceAsStream = getClass().getResourceAsStream(name);
-		String string = Files.readStreamIntoString(resourceAsStream);
-		for (int i = 1; i < string.length(); i=i+4) {
+	public void testParseErrornessModel_01() throws Exception {
+		String string = readXtendFile();
+		for (int i = 0; i < string.length(); i++) {
 			doParseLinkAndValidate(string.substring(0, i));
 		}
 	}
 
-	protected void doParseLinkAndValidate(String string) throws Exception {
+	protected String readXtendFile() {
+		final String name = "/"+getClass().getName().replace('.', '/')+"Data.xtend";
+		final InputStream resourceAsStream = getClass().getResourceAsStream(name);
+		String string = Files.readStreamIntoString(resourceAsStream);
+		return string;
+	}
+	
+	public void testParseErrornessModel_02() throws Exception {
+		String string = readXtendFile();
+		for (int i = 0; i < string.length(); i++) {
+			doParseLinkAndValidate(string.substring(i));
+		}
+	}
+	
+	public void testParseErrornessModel_03() throws Exception {
+		String string = readXtendFile();
+		for (int i = 0; i < string.length() - 1; i++) {
+			doParseLinkAndValidate(string.substring(0, i) + string.substring(i+1));
+		}
+	}
+
+	protected void doParseLinkAndValidate(final String string) throws Exception {
 		XtextResourceSet set = get(XtextResourceSet.class);
 		LazyLinkingResource resource = (LazyLinkingResource) set.createResource(URI.createURI("Test.xtend"));
 		resource.load(new StringInputStream(string), null);
 		resource.resolveLazyCrossReferences(CancelIndicator.NullImpl);
-		resource.getResourceServiceProvider().getResourceValidator().validate(resource, CheckMode.ALL, CancelIndicator.NullImpl);
+		ResourceValidatorImpl validator = new ResourceValidatorImpl();
+		assertNotSame(validator, resource.getResourceServiceProvider().getResourceValidator());
+		getInjector().injectMembers(validator);
+		validator.setDiagnosticConverter(new IDiagnosticConverter() {
+			public void convertValidatorDiagnostic(org.eclipse.emf.common.util.Diagnostic diagnostic, IAcceptor<Issue> acceptor) {
+				if (diagnostic instanceof BasicDiagnostic) {
+					List<?> data = diagnostic.getData();
+					if (!data.isEmpty() && data.get(0) instanceof Throwable) {
+						Throwable t = (Throwable) data.get(0);
+						// the framework catches runtime exception
+						// and AssertionError does not take a throwable as argument
+						throw new Error(new RuntimeException("Input was: " + string, t));
+					}
+				}
+			}
+			
+			public void convertResourceDiagnostic(Diagnostic diagnostic, Severity severity, IAcceptor<Issue> acceptor) {
+				if (diagnostic instanceof ExceptionDiagnostic) {
+					Exception e = ((ExceptionDiagnostic) diagnostic).getException();
+					// the framework catches runtime exception
+					// and AssertionError does not take a throwable as argument
+					throw new Error(new RuntimeException("Input was: " + string, e));
+				}
+			}
+		});
+		validator.validate(resource, CheckMode.ALL, CancelIndicator.NullImpl);
 	}
 }
