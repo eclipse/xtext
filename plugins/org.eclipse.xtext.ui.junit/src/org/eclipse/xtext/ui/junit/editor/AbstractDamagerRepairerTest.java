7a2d3c77f44ac2b05b3284cb55753d1d7318dec5
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2011-01-28 16:37:25 +0100

    [xtend2.ui] Some fixes for presentation damager

--- plugins/org.eclipse.xtext.ui.junit/src/org/eclipse/xtext/ui/junit/editor/AbstractDamagerRepairerTest.java
+++ plugins/org.eclipse.xtext.ui.junit/src/org/eclipse/xtext/ui/junit/editor/AbstractDamagerRepairerTest.java
@@ -5,7 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-package org.eclipse.xtext.ui.tests.editor;
+package org.eclipse.xtext.ui.junit.editor;
 
 import junit.framework.TestCase;
 
@@ -18,6 +18,12 @@ import org.eclipse.jface.text.TypedRegion;
 import org.eclipse.jface.text.presentation.IPresentationDamager;
 import org.eclipse.jface.text.rules.IToken;
 import org.eclipse.jface.text.rules.ITokenScanner;
+import org.eclipse.xtext.parser.antlr.Lexer;
+import org.eclipse.xtext.ui.editor.PresentationDamager;
+import org.eclipse.xtext.ui.editor.model.DocumentTokenSource;
+import org.eclipse.xtext.ui.editor.model.XtextDocument;
+
+import com.google.inject.Provider;
 
 /**
  * @author Sven Efftinge - Initial contribution and API
@@ -50,77 +56,27 @@ public abstract class AbstractDamagerRepairerTest extends TestCase implements ID
 		return lastRegion;
 	}
 
-	protected Document createDocument(String before) throws Exception{
-		return new Document(before);
+	protected IPresentationDamager createRegionDamager() {
+		return new PresentationDamager();
 	}
+
+	protected abstract Lexer createLexer();
 	
-	protected abstract IPresentationDamager createRegionDamager();
+	protected Document createDocument(String before) throws Exception {
+		DocumentTokenSource source = new DocumentTokenSource();
+		source.setLexer(new Provider<Lexer>() {
+			public Lexer get() {
+				return createLexer();
+			}
+		});
+		XtextDocument document = new XtextDocument(source, null);
+		document.set(before);
+		return document;
+	}
 	
 	protected void assertEquals(int expStart, int expLength, IRegion actual) {
 		assertEquals("offset", expStart, actual.getOffset());
 		assertEquals("length", expLength, actual.getLength());
-	}
-	
-	public void testEmptyDocs() throws Exception {
-		assertEquals(0,0,check("",0,0,""));
-	}
-	
-	public void testAddToEmptyDoc() throws Exception {
-		assertEquals(0,3,check("",0,0,"foo"));
-	}
-	
-	public void testRemoveToEmptyDoc() throws Exception {
-		assertEquals(0,0,check("foo",0,3,""));
-	}
-	
-	public void testRemoveFullElement() throws Exception {
-		assertEquals(4,0,check("foo bar",4,3,""));
-	}
-	
-	public void testRemovePartlyElement() throws Exception {
-		assertEquals(4,1,check("foo bar",5,2,""));
-	}
-	
-	public void testAddElement() throws Exception {
-		assertEquals(7,9,check("foo bar",7,0," honolulu"));
-	}
-	
-	public void testAddElement2() throws Exception {
-		assertEquals(4,11,check("foo bar",7,0,"honolulu"));
-	}
-	
-	public void testChangeInTheMiddleElement() throws Exception {
-		assertEquals(4,3,check("foo bar import",6,1,"z"));
-	}
-	
-	public void testComplicatedStuff1() throws Exception {
-		assertEquals(0,22,check("test : 'mo*/in' foo;",0,0,"/*"));
-	}
-	
-	public void testComplicatedStuff2() throws Exception {
-		assertEquals(0,8,check("foo bar",0,0,"foo bar "));
-	}
-	
-	public void testBug276628() throws Exception {
-		assertEquals(7,10,check("Rule2: 'keyword';",8,9,"keyword';"));
-	}
-	
-	public void testBug279061() throws Exception {
-		assertEquals(3,2, check("foo(x", 3, 2, "(x"));
-	}
-	
-	public void testMlEndOfLine() throws Exception {
-		String model = "/* foo ";
-		String end = "*/";
-		
-		assertEquals(0,(model+end).length(), check(model+end,0,model.length(),model));
-	}
-	
-	public void testMlEndOfLine2() throws Exception {
-		String model = "foo /* foo ";
-		String end = "*/";
-		
-		assertEquals(0,(model+end).length(), check(model+end,0,model.length(),model));
 	}
 	
 	public void documentChanged(DocumentEvent event) {
--- tests/org.eclipse.xtext.ui.tests/tests/org/eclipse/xtext/ui/tests/editor/PresentationDamagerTest.java
+++ tests/org.eclipse.xtext.ui.tests/tests/org/eclipse/xtext/ui/tests/editor/PresentationDamagerTest.java
@@ -7,36 +7,85 @@
  *******************************************************************************/
 package org.eclipse.xtext.ui.tests.editor;
 
-import org.eclipse.jface.text.Document;
-import org.eclipse.jface.text.presentation.IPresentationDamager;
 import org.eclipse.xtext.parser.antlr.Lexer;
-import org.eclipse.xtext.ui.editor.PresentationDamager;
-import org.eclipse.xtext.ui.editor.model.DocumentTokenSource;
-import org.eclipse.xtext.ui.editor.model.XtextDocument;
-
-import com.google.inject.Provider;
+import org.eclipse.xtext.parser.antlr.internal.InternalXtextLexer;
+import org.eclipse.xtext.ui.junit.editor.AbstractDamagerRepairerTest;
 
 /**
  * @author Sven Efftinge - Initial contribution and API
  */
 public class PresentationDamagerTest extends AbstractDamagerRepairerTest {
 
-	@Override
-	protected IPresentationDamager createRegionDamager() {
-		return new PresentationDamager();
+	public void testEmptyDocs() throws Exception {
+		assertEquals(0,0,check("",0,0,""));
+	}
+	
+	public void testAddToEmptyDoc() throws Exception {
+		assertEquals(0,3,check("",0,0,"foo"));
+	}
+	
+	public void testRemoveToEmptyDoc() throws Exception {
+		assertEquals(0,0,check("foo",0,3,""));
+	}
+	
+	public void testRemoveFullElement() throws Exception {
+		assertEquals(4,0,check("foo bar",4,3,""));
+	}
+	
+	public void testRemovePartlyElement() throws Exception {
+		assertEquals(4,1,check("foo bar",5,2,""));
+	}
+	
+	public void testAddElement() throws Exception {
+		assertEquals(7,9,check("foo bar",7,0," honolulu"));
+	}
+	
+	public void testAddElement2() throws Exception {
+		assertEquals(4,11,check("foo bar",7,0,"honolulu"));
+	}
+	
+	public void testChangeInTheMiddleElement() throws Exception {
+		assertEquals(4,3,check("foo bar import",6,1,"z"));
+	}
+	
+	public void testComplicatedStuff1() throws Exception {
+		assertEquals(0,22,check("test : 'mo*/in' foo;",0,0,"/*"));
+	}
+	
+	public void testComplicatedStuff2() throws Exception {
+		assertEquals(0,8,check("foo bar",0,0,"foo bar "));
+	}
+	
+	public void testBug276628() throws Exception {
+		assertEquals(7,10,check("Rule2: 'keyword';",8,9,"keyword';"));
+	}
+	
+	public void testBug279061() throws Exception {
+		assertEquals(3,2, check("foo(x", 3, 2, "(x"));
+	}
+	
+	public void testPartitionsWithoutSpace() throws Exception {
+		String text = "Rule2: 'keyword'DoesNotExist'keyword';";
+		assertEquals(text.indexOf("Does"), "DoesNotExistx".length(), check(text, text.indexOf('t') + 1, 0, "x"));
+	}
+	
+	public void testMlEndOfLine() throws Exception {
+		String model = "/* foo ";
+		String end = "*/";
+		
+		assertEquals(0,(model+end).length(), check(model+end,0,model.length(),model));
+	}
+	
+	public void testMlEndOfLine2() throws Exception {
+		String model = "foo /* foo ";
+		String end = "*/";
+		
+		assertEquals(0,(model+end).length(), check(model+end,0,model.length(),model));
 	}
 
 	@Override
-	protected Document createDocument(String before) throws Exception {
-		DocumentTokenSource source = new DocumentTokenSource();
-		source.setLexer(new Provider<Lexer>() {
-			public Lexer get() {
-				return new org.eclipse.xtext.parser.antlr.internal.InternalXtextLexer();
-			}
-		});
-		XtextDocument document = new XtextDocument(source, null);
-		document.set(before);
-		return document;
+	protected Lexer createLexer() {
+		return new InternalXtextLexer();
 	}
 
 }
