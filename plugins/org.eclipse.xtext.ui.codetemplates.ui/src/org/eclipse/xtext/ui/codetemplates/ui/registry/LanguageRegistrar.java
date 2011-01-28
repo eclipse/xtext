3ea47d64f8b9333bf09106f48a61abe96764506a
Jan Koehnlein <jan.koehnlein@itemis.de>
2011-01-28 17:01:52 +0100

    Introduced FileExtensionProvider

--- plugins/org.eclipse.xtext.ui.codetemplates.ui/src/org/eclipse/xtext/ui/codetemplates/ui/registry/LanguageRegistrar.java
+++ plugins/org.eclipse.xtext.ui.codetemplates.ui/src/org/eclipse/xtext/ui/codetemplates/ui/registry/LanguageRegistrar.java
@@ -8,15 +8,14 @@
 package org.eclipse.xtext.ui.codetemplates.ui.registry;
 
 import org.eclipse.jface.text.templates.ContextTypeRegistry;
-import org.eclipse.xtext.Constants;
 import org.eclipse.xtext.IGrammarAccess;
+import org.eclipse.xtext.resource.FileExtensionProvider;
 import org.eclipse.xtext.ui.codetemplates.ui.highlighting.TemplateBodyHighlighter;
 import org.eclipse.xtext.ui.codetemplates.ui.partialEditing.PartialContentAssistContextFactory;
 import org.eclipse.xtext.ui.editor.templates.ContextTypeIdHelper;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
-import com.google.inject.name.Named;
 
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
@@ -25,27 +24,26 @@ public class LanguageRegistrar {
 
 	@Inject
 	private IGrammarAccess grammarAccess;
-	
+
 	@Inject
 	private Provider<TemplateBodyHighlighter> highlighter;
 
 	@Inject
 	private ContextTypeRegistry contextTypeRegistry;
-	
+
 	@Inject
 	private ContextTypeIdHelper helper;
-	
+
 	@Inject
 	private Provider<PartialContentAssistContextFactory> contentAssistContextFactory;
-	
+
 	@Inject
-	@Named(Constants.FILE_EXTENSIONS)
-	private String fileExtensions;
-	
+	private FileExtensionProvider fileExtensionProvider;
+
 	@Inject
 	public void register(LanguageAcceptor acceptor) {
-		String[] extensions = fileExtensions.split(",");
-		acceptor.accept(grammarAccess.getGrammar(), highlighter, contextTypeRegistry, helper, contentAssistContextFactory, extensions[0]);
+		acceptor.accept(grammarAccess.getGrammar(), highlighter, contextTypeRegistry, helper,
+				contentAssistContextFactory, fileExtensionProvider.getFileExtensions().iterator().next());
 	}
-	
+
 }
