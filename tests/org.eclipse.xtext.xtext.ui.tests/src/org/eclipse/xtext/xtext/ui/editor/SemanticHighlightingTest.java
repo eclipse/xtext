caf054d08bdc36bcc91213ca00544bece8fe6ee0
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2011-02-01 11:12:18 +0100

    [xtext.xtext.ui] Regression: Semantic highlighting is broken (see https://bugs.eclipse.org/bugs/show_bug.cgi?id=335937)

--- tests/org.eclipse.xtext.xtext.ui.tests/src/org/eclipse/xtext/xtext/ui/editor/SemanticHighlightingTest.java
+++ tests/org.eclipse.xtext.xtext.ui.tests/src/org/eclipse/xtext/xtext/ui/editor/SemanticHighlightingTest.java
@@ -7,6 +7,9 @@
  *******************************************************************************/
 package org.eclipse.xtext.xtext.ui.editor;
 
+import java.util.Set;
+
+import org.eclipse.jface.text.TypedRegion;
 import org.eclipse.xtext.XtextRuntimeModule;
 import org.eclipse.xtext.XtextStandaloneSetup;
 import org.eclipse.xtext.junit.AbstractXtextTests;
@@ -17,14 +20,18 @@ import org.eclipse.xtext.ui.editor.syntaxcoloring.ISemanticHighlightingCalculato
 import org.eclipse.xtext.ui.shared.SharedStateModule;
 import org.eclipse.xtext.util.Modules2;
 import org.eclipse.xtext.xtext.ui.Activator;
+import org.eclipse.xtext.xtext.ui.editor.syntaxcoloring.SemanticHighlightingConfiguration;
 
+import com.google.common.collect.Sets;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
  */
-public class SemanticHighlightingTest extends AbstractXtextTests {
+public class SemanticHighlightingTest extends AbstractXtextTests implements IHighlightedPositionAcceptor {
+	
+	private Set<TypedRegion> expectedRegions;
 	
 	@Override
 	protected void setUp() throws Exception {
@@ -35,6 +42,7 @@ public class SemanticHighlightingTest extends AbstractXtextTests {
 				return Guice.createInjector(Modules2.mixin(new XtextRuntimeModule(),new XtextUiModule(Activator.getDefault()), new SharedStateModule()));
 			}
 		});
+		expectedRegions = Sets.newLinkedHashSet();
 	}
 
 	public void testNullPointerOnInvalidReturnKeyword() throws Exception {
@@ -48,6 +56,55 @@ public class SemanticHighlightingTest extends AbstractXtextTests {
 				// ignore
 			}
 		});
+	}
+	
+	public void testHighlightGrammar() {
+		String testGrammar = "grammar org.xtext.example.mydsl1.MyDsl with org.eclipse.xtext.common.Terminals\n" + 
+			"\n" + 
+			"generate myDsl \"http://www.xtext.org/example/mydsl1/MyDsl\"\n" + 
+			"\n" + 
+			"Model returns Zonk:\n" + 
+			"	greetings+=Greeting*;\n" + 
+			"	\n" + 
+			"Greeting:\n" + 
+			"	'Hello' name=Identifier STRING '!';\n" + 
+			"	\n" + 
+			"Unused: name=Identifier;\n" + 
+			"\n" + 
+			"Identifier: ID;";
+		
+		expect(testGrammar.indexOf("STRING"), "STRING".length(), SemanticHighlightingConfiguration.UNUSED_VALUE_ID);
+		expect(testGrammar.indexOf("Identifier:"), "Identifier".length(), SemanticHighlightingConfiguration.DATA_TYPE_RULE_ID);
+		expect(testGrammar.indexOf("Unused"), "Unused".length(), SemanticHighlightingConfiguration.NEVER_CALLED_RULE_ID);
+		
+		expect(testGrammar.indexOf("Zonk"), "Zonk".length(), SemanticHighlightingConfiguration.TYPE_REFERENCE_ID);
+		expect(testGrammar.indexOf("Model"), "Model".length(), SemanticHighlightingConfiguration.RULE_DECLARATION_ID);
+		expect(testGrammar.indexOf("Greeting:"), "Greeting".length(), SemanticHighlightingConfiguration.RULE_DECLARATION_ID);
+		expect(testGrammar.indexOf("Identifier:"), "Identifier".length(), SemanticHighlightingConfiguration.RULE_DECLARATION_ID);
+		expect(testGrammar.indexOf("Unused:"), "Unused".length(), SemanticHighlightingConfiguration.RULE_DECLARATION_ID);
+		highlight(testGrammar);
+	}
+	
+	protected void highlight(String grammar) {
+		try {
+			XtextResource resource = getResourceFromString(grammar);
+			ISemanticHighlightingCalculator calculator = get(ISemanticHighlightingCalculator.class);
+			calculator.provideHighlightingFor(resource, this);
+			assertTrue(expectedRegions.toString(), expectedRegions.isEmpty());
+		} catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected void expect(int offset, int length, String type) {
+		expectedRegions.add(new TypedRegion(offset, length, type));
+	}
+
+	public void addPosition(int offset, int length, String... id) {
+		assertEquals(1, id.length);
+		TypedRegion region = new TypedRegion(offset, length, id[0]);
+		assertFalse(region.toString(), expectedRegions.isEmpty());
+		assertTrue("expected: " + expectedRegions.toString() + " but was: " + region, expectedRegions.remove(region));
 	}
 
 }
