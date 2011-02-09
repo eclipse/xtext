c5a498cfee691adbc49a00467fbeb909010a2241
Jan Koehnlein <jan.koehnlein@itemis.de>
2011-02-09 16:22:30 +0100

    [xtend2] Moved 'name' to XtendMember

--- plugins/org.eclipse.xtext.xtend2/emf-gen/org/eclipse/xtext/xtend2/xtend2/XtendFunction.java
+++ plugins/org.eclipse.xtext.xtend2/emf-gen/org/eclipse/xtext/xtend2/xtend2/XtendFunction.java
@@ -21,7 +21,6 @@ import org.eclipse.xtext.xbase.XExpression;
  * <p>
  * The following features are supported:
  * <ul>
- *   <li>{@link org.eclipse.xtext.xtend2.xtend2.XtendFunction#getName <em>Name</em>}</li>
  *   <li>{@link org.eclipse.xtext.xtend2.xtend2.XtendFunction#getExpression <em>Expression</em>}</li>
  *   <li>{@link org.eclipse.xtext.xtend2.xtend2.XtendFunction#getReturnType <em>Return Type</em>}</li>
  *   <li>{@link org.eclipse.xtext.xtend2.xtend2.XtendFunction#getParameters <em>Parameters</em>}</li>
@@ -35,32 +34,6 @@ import org.eclipse.xtext.xbase.XExpression;
  */
 public interface XtendFunction extends XtendMember, JvmTypeParameterDeclarator
 {
-	/**
-	 * Returns the value of the '<em><b>Name</b></em>' attribute.
-	 * <!-- begin-user-doc -->
-	 * <p>
-	 * If the meaning of the '<em>Name</em>' attribute isn't clear,
-	 * there really should be more of a description here...
-	 * </p>
-	 * <!-- end-user-doc -->
-	 * @return the value of the '<em>Name</em>' attribute.
-	 * @see #setName(String)
-	 * @see org.eclipse.xtext.xtend2.xtend2.Xtend2Package#getXtendFunction_Name()
-	 * @model
-	 * @generated
-	 */
-	String getName();
-
-	/**
-	 * Sets the value of the '{@link org.eclipse.xtext.xtend2.xtend2.XtendFunction#getName <em>Name</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Name</em>' attribute.
-	 * @see #getName()
-	 * @generated
-	 */
-	void setName(String value);
-
 	/**
 	 * Returns the value of the '<em><b>Expression</b></em>' containment reference.
 	 * <!-- begin-user-doc -->
