11a8f47782618c42b3705b950cc4c921903f6e8f
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2011-02-16 15:41:15 +0100

    [common.types] Fixed JvmConstructor#getQualifiedName to return java.lang.Object instead of java.lang.Object.Object

--- tests/org.eclipse.xtext.common.types.tests/src/org/eclipse/xtext/common/types/JvmConstructorTest.java
+++ tests/org.eclipse.xtext.common.types.tests/src/org/eclipse/xtext/common/types/JvmConstructorTest.java
@@ -30,4 +30,67 @@ public class JvmConstructorTest extends JvmExecutableTest {
 		return "DoesNotExist";
 	}
 	
+	@Override
+	public void testGetQualifiedName_02() {
+		JvmGenericType outerType = TypesFactory.eINSTANCE.createJvmGenericType();
+		outerType.setSimpleName("DoesNotExist");
+		outerType.getMembers().add(getObjectUnderTest());
+		getObjectUnderTest().setSimpleName("DoesNotExist");
+		assertEquals("DoesNotExist", getObjectUnderTest().getQualifiedName());
+	}
+	
+	@Override
+	public void testGetQualifiedName_03() {
+		JvmGenericType outerType = TypesFactory.eINSTANCE.createJvmGenericType();
+		outerType.setSimpleName("DoesNotExist");
+		outerType.setPackageName("java.lang");
+		outerType.getMembers().add(getObjectUnderTest());
+		getObjectUnderTest().setSimpleName("DoesNotExist");
+		assertEquals("java.lang.DoesNotExist", getObjectUnderTest().getQualifiedName());
+	}
+	
+	@Override
+	public void testGetQualifiedName_04() {
+		JvmGenericType outerOuterType = TypesFactory.eINSTANCE.createJvmGenericType();
+		outerOuterType.setSimpleName("DoesNotExist");
+		outerOuterType.setPackageName("java.lang");
+		JvmGenericType outerType = TypesFactory.eINSTANCE.createJvmGenericType();
+		outerOuterType.getMembers().add(outerType);
+		outerType.setSimpleName("Outer");
+		outerType.getMembers().add(getObjectUnderTest());
+		getObjectUnderTest().setSimpleName("Outer");
+		assertEquals("java.lang.DoesNotExist$Outer", getObjectUnderTest().getQualifiedName());
+	}
+	
+	@Override
+	public void testGetQualifiedNameWithDelimiter_02() {
+		JvmGenericType type = TypesFactory.eINSTANCE.createJvmGenericType();
+		type.setSimpleName("DoesNotExist");
+		type.getMembers().add(getObjectUnderTest());
+		getObjectUnderTest().setSimpleName("DoesNotExist");
+		assertEquals("DoesNotExist", getObjectUnderTest().getQualifiedName('.'));
+	}
+	
+	@Override
+	public void testGetQualifiedNameWithDelimiter_03() {
+		JvmGenericType type = TypesFactory.eINSTANCE.createJvmGenericType();
+		type.setSimpleName("DoesNotExist");
+		type.setPackageName("java.lang");
+		type.getMembers().add(getObjectUnderTest());
+		getObjectUnderTest().setSimpleName("DoesNotExist");
+		assertEquals("java.lang.DoesNotExist", getObjectUnderTest().getQualifiedName('.'));
+	}
+	
+	@Override
+	public void testGetQualifiedNameWithDelimiter_04() {
+		JvmGenericType outerType = TypesFactory.eINSTANCE.createJvmGenericType();
+		outerType.setSimpleName("DoesNotExist");
+		outerType.setPackageName("java.lang");
+		JvmGenericType type = TypesFactory.eINSTANCE.createJvmGenericType();
+		outerType.getMembers().add(type);
+		type.setSimpleName("Inner");
+		type.getMembers().add(getObjectUnderTest());
+		getObjectUnderTest().setSimpleName("Inner");
+		assertEquals("java.lang.DoesNotExist.Inner", getObjectUnderTest().getQualifiedName('.'));
+	}
 }
