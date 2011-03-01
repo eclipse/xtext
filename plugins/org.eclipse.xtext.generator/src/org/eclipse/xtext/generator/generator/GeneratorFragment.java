829159bbd6f9433b8172195015584182cf6b215d
Sven Efftinge <sven.efftinge@itemis.de>
2011-03-01 13:32:51 +0100

    [Xtext] first draft of generator API

---
+++ plugins/org.eclipse.xtext.generator/src/org/eclipse/xtext/generator/generator/GeneratorFragment.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2008 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+package org.eclipse.xtext.generator.generator;
+
+import java.util.Set;
+
+import org.eclipse.xtext.Grammar;
+import org.eclipse.xtext.GrammarUtil;
+import org.eclipse.xtext.generator.AbstractGeneratorFragment;
+import org.eclipse.xtext.generator.BindFactory;
+import org.eclipse.xtext.generator.Binding;
+import org.eclipse.xtext.generator.IGenerator;
+import org.eclipse.xtext.generator.Naming;
+
+/**
+ * An {@link IGeneratorFragment} to create a formatter for an Xtext language.
+ * 
+ * @author Sven Efftinge - Initial contribution and API
+ */
+public class GeneratorFragment extends AbstractGeneratorFragment {
+	@Override
+	public Set<Binding> getGuiceBindingsRt(Grammar grammar) {
+		return new BindFactory().addTypeToType(IGenerator.class.getName(), getGeneratorName(grammar, getNaming())).getBindings();
+	}
+	
+	@Override
+	public String[] getImportedPackagesRt(Grammar grammar) {
+		return new String[] {
+				"org.eclipse.xtext.xbase.lib",
+				"org.eclipse.xtext.xtend2.lib"
+		};
+	}
+
+	public static String getGeneratorName(Grammar grammar, Naming naming) {
+		return naming.basePackageRuntime(grammar) + ".generator." + GrammarUtil.getName(grammar) + "Generator";
+	}
+
+	@Override
+	public Set<Binding> getGuiceBindingsUi(Grammar grammar) {
+		return new BindFactory()
+		.addTypeToType("org.eclipse.xtext.builder.IXtextBuilderParticipant", "org.eclipse.xtext.builder.JavaProjectBasedBuilderParticipant")
+		.addTypeToInstance("org.eclipse.core.resources.IWorkspaceRoot", "org.eclipse.core.resources.ResourcesPlugin.getWorkspace().getRoot()")
+		.getBindings();
+	}
+}
