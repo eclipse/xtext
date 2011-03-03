8646ea60ca95c0adee34a60cd68c2382d9cc58aa
Jan Koehnlein <jan.koehnlein@itemis.de>
2011-03-03 16:16:13 +0100

    [dmodel] Added JvmModel inferrence

--- examples/org.eclipse.xtext.xtext.ui.examples/contents/org.eclipse.xtext.example.domainmodel/src/org/eclipse/xtext/example/domainmodel/scoping/DomainmodelScopeProvider.java
+++ examples/org.eclipse.xtext.xtext.ui.examples/contents/org.eclipse.xtext.example.domainmodel/src/org/eclipse/xtext/example/domainmodel/scoping/DomainmodelScopeProvider.java
@@ -3,17 +3,88 @@
  */
 package org.eclipse.xtext.example.domainmodel.scoping;
 
-import org.eclipse.xtext.scoping.impl.AbstractDeclarativeScopeProvider;
+import static com.google.common.collect.Iterables.*;
+import static java.util.Collections.*;
+
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.xtext.EcoreUtil2;
+import org.eclipse.xtext.common.types.JvmFormalParameter;
+import org.eclipse.xtext.common.types.JvmType;
+import org.eclipse.xtext.common.types.TypesPackage;
+import org.eclipse.xtext.example.domainmodel.domainmodel.Entity;
+import org.eclipse.xtext.example.domainmodel.domainmodel.Operation;
+import org.eclipse.xtext.naming.QualifiedName;
+import org.eclipse.xtext.resource.EObjectDescription;
+import org.eclipse.xtext.resource.IEObjectDescription;
+import org.eclipse.xtext.scoping.IScope;
+import org.eclipse.xtext.scoping.impl.MapBasedScope;
+import org.eclipse.xtext.scoping.impl.SimpleScope;
+import org.eclipse.xtext.util.Strings;
+import org.eclipse.xtext.xbase.jvmmodel.IJvmModelAssociations;
+import org.eclipse.xtext.xbase.scoping.XbaseScopeProvider;
+
+import com.google.common.collect.Lists;
+import com.google.inject.Inject;
 
 /**
  * This class contains custom scoping description.
  * 
- * see : http://www.eclipse.org/Xtext/documentation/latest/xtext.html#scoping on how and when to
- * use it
- * 
+ * see : http://www.eclipse.org/Xtext/documentation/latest/xtext.html#scoping
+ * on how and when to use it 
+ *
  */
-public class DomainmodelScopeProvider extends AbstractDeclarativeScopeProvider {
-	
-	
+public class DomainmodelScopeProvider extends XbaseScopeProvider {
 
+	@Inject
+	private IJvmModelAssociations associations;
+	
+	@Override
+	public IScope getScope(EObject context, EReference reference) {
+		IScope parent = super.getScope(context, reference);
+		if(TypesPackage.Literals.JVM_TYPE.isSuperTypeOf(reference.getEReferenceType())) {
+			Entity entity = EcoreUtil2.getContainerOfType(context, Entity.class);
+			if (entity != null) {
+				JvmType jvmType = getJvmType(entity);
+				if (jvmType != null && !Strings.isEmpty(jvmType.getSimpleName())) {
+					return new SimpleScope(parent, singleton(EObjectDescription.create(QualifiedName.create(jvmType.getSimpleName()), jvmType)));
+				}
+			}
+		}
+		return parent;
+	}
+	
+	@Override
+	protected IScope createLocalVarScope(EObject context, EReference reference, IScope parent,
+			boolean includeCurrentBlock, int idx) {
+		if (context instanceof Entity) {
+			JvmType jvmType = getJvmType((Entity) context);
+			if(jvmType != null)
+				return new SimpleScope(parent, singleton(EObjectDescription.create(THIS, jvmType)));
+		} else if (context instanceof Operation) {
+			Operation operation  = (Operation) context;
+			List<JvmFormalParameter> list = operation.getParams();
+			List<IEObjectDescription> descriptions = Lists.newArrayList();
+			for (JvmFormalParameter jvmFormalParameter : list) {
+				if (!Strings.isEmpty(jvmFormalParameter.getName())) {
+					IEObjectDescription desc = createIEObjectDescription(jvmFormalParameter);
+					descriptions.add(desc);
+				}
+			}
+			return MapBasedScope.createScope(
+					super.createLocalVarScope(context, reference, parent, includeCurrentBlock, idx), descriptions);
+		}
+		return super.createLocalVarScope(context, reference, parent, includeCurrentBlock, idx);
+	}
+	
+	protected IEObjectDescription createIEObjectDescription(JvmFormalParameter jvmFormalParameter) {
+		return EObjectDescription.create(QualifiedName.create(jvmFormalParameter.getName()), jvmFormalParameter, null);
+	}
+
+	protected JvmType getJvmType(Entity entity) {
+		Iterable<JvmType> jvmElements = filter(associations.getJvmElements(entity, true), JvmType.class);
+		return (isEmpty(jvmElements) ? null : jvmElements.iterator().next());
+	}
 }
