ba49608fe696a8a69a209f2ccd05af08c211dcac
Jan Koehnlein <jan.koehnlein@itemis.de>
2011-03-04 18:51:51 +0100

    [generator] Create missing directories in IFileSystemAccess

--- plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/EclipseResourceFileSystemAccess.java
+++ plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/EclipseResourceFileSystemAccess.java
@@ -7,11 +7,16 @@
  *******************************************************************************/
 package org.eclipse.xtext.builder;
 
+import static org.eclipse.xtext.util.Strings.*;
+
 import java.io.UnsupportedEncodingException;
 
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.xtext.generator.AbstractFileSystemAccess;
 import org.eclipse.xtext.util.StringInputStream;
@@ -20,6 +25,7 @@ import com.google.inject.Inject;
 
 /**
  * @author Sven Efftinge - Initial contribution and API
+ * @author Jan Koehnlein
  */
 public class EclipseResourceFileSystemAccess extends AbstractFileSystemAccess {
 
@@ -34,6 +40,7 @@ public class EclipseResourceFileSystemAccess extends AbstractFileSystemAccess {
 		String outletPath = getPathes().get(slot);
 		IFile file = root.getFile(new Path(outletPath + "/" + fileName));
 		try {
+			createFolder(file.getParent());
 			final String defaultCharset = file.getCharset();
 			if (file.exists())
 				file.setContents(new StringInputStream(contents.toString(), defaultCharset), true, true, null);
@@ -46,4 +53,12 @@ public class EclipseResourceFileSystemAccess extends AbstractFileSystemAccess {
 		}
 	}
 
+	protected void createFolder(IContainer parent) throws CoreException {
+		if(!parent.exists()) {
+			if(!(parent instanceof IFolder)) 
+				throw new RuntimeException("IContainer " + notNull(parent) + " does not exist");
+			createFolder(parent.getParent());
+			((IFolder)parent).create(true, false, new NullProgressMonitor());
+		}
+	}
 }
