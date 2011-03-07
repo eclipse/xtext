e52b85ebff256b309449898a513dd8f55198e1db
Sven Efftinge <sven.efftinge@itemis.de>
2011-03-07 10:24:11 +0100

    [Xtext] add platform:/resource mappings for all jared OSGi bundles on the classpath

--- plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/resource/XtextResourceSetProvider.java
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/resource/XtextResourceSetProvider.java
@@ -7,11 +7,25 @@
  *******************************************************************************/
 package org.eclipse.xtext.ui.resource;
 
+import static com.google.common.collect.Maps.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
+import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.plugin.EcorePlugin;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.xtext.resource.XtextResourceSet;
 import org.eclipse.xtext.ui.util.JdtClasspathUriResolver;
 
@@ -23,18 +37,61 @@ import com.google.inject.Provider;
  */
 public class XtextResourceSetProvider implements IResourceSetProvider {
 	
+	private final static Logger LOG = Logger.getLogger(XtextResourceSetProvider.class);
+	
 	@Inject
 	private Provider<XtextResourceSet> resourceSetProvider;
 
 	public ResourceSet get(IProject project) {
 		XtextResourceSet set = resourceSetProvider.get();
-		set.getURIConverter().getURIMap().putAll(EcorePlugin.computePlatformURIMap());
 		IJavaProject javaProject = JavaCore.create(project);
+		set.getURIConverter().getURIMap().putAll(computePlatformURIMap(javaProject));
 		if (javaProject!=null && javaProject.exists()) {
 			set.setClasspathURIContext(javaProject);
 			set.setClasspathUriResolver(new JdtClasspathUriResolver());
 		}
 		return set;
+	}
+
+	protected Map<URI, URI> computePlatformURIMap(IJavaProject javaProject) {
+		long now = System.currentTimeMillis();
+		HashMap<URI,URI> hashMap = newHashMap(EcorePlugin.computePlatformURIMap());
+		try {
+			if (!javaProject.exists())
+				return hashMap;
+			IClasspathEntry[] classpath = javaProject.getResolvedClasspath(true);
+			for (IClasspathEntry classPathEntry : classpath) {
+				IPath path = classPathEntry.getPath();
+				if (path!=null && "jar".equals(path.getFileExtension())) {
+					try {
+						final File file = path.toFile();
+						JarFile jarFile = new JarFile(file);
+						Manifest manifest = jarFile.getManifest();
+						if (manifest!=null) {
+							String name = manifest.getMainAttributes().getValue("Bundle-SymbolicName");
+							if (name != null) {
+								final int indexOf = name.indexOf(';');
+								if (indexOf>0) 
+									name = name.substring(0, indexOf);
+								if (!EcorePlugin.getPlatformResourceMap().containsKey(name)) {
+									String p = "archive:file:"+file.getCanonicalPath() + "!/";
+									URI uri = URI.createURI(p);
+									final URI key = URI.createPlatformResourceURI(name + "/", false);
+									hashMap.put(key, uri);
+									System.out.println(key+" -> "+uri);
+								}
+							}
+						}
+					} catch (IOException e) {
+						LOG.error(e.getMessage(), e);
+					}
+				}
+			}
+		} catch (JavaModelException e) {
+			LOG.error(e.getMessage(), e);
+		}
+		System.out.println("computing patform resource map took ms "+((System.currentTimeMillis() - now)));
+		return hashMap;
 	}
 
 }
