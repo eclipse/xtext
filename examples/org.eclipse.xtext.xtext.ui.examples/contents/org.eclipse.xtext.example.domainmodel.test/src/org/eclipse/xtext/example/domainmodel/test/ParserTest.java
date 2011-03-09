333880496558a4abec6a491514acc5c1dd7be5c1
Sven Efftinge <sven.efftinge@itemis.de>
2011-03-09 18:46:58 +0100

    [Domainmodel Example] added Junit fragment and wrote a first example unit test.

---
+++ examples/org.eclipse.xtext.xtext.ui.examples/contents/org.eclipse.xtext.example.domainmodel.test/src/org/eclipse/xtext/example/domainmodel/test/ParserTest.java
@@ -0,0 +1,47 @@
+
+package org.eclipse.xtext.example.domainmodel.test;
+
+import static junit.framework.Assert.*;
+import org.eclipse.xtext.example.domainmodel.DomainmodelInjectorProvider;
+import org.eclipse.xtext.example.domainmodel.domainmodel.DomainModel;
+import org.eclipse.xtext.example.domainmodel.domainmodel.Entity;
+import org.eclipse.xtext.example.domainmodel.domainmodel.PackageDeclaration;
+import org.eclipse.xtext.example.domainmodel.domainmodel.Property;
+import org.eclipse.xtext.junit4.InjectWith;
+import org.eclipse.xtext.junit4.XtextRunner;
+import org.eclipse.xtext.junit4.util.ParseHelper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.google.inject.Inject;
+
+/**
+ * Example test
+ */
+@RunWith(XtextRunner.class)
+@InjectWith(DomainmodelInjectorProvider.class)
+public class ParserTest {
+	
+	@Inject
+	private ParseHelper<DomainModel> parser;
+
+	@Test
+	public void testParsing() throws Exception {
+		DomainModel model = parser.parse(
+				"package example {" +
+				"  entity MyEntity {" +
+				"    property : String" +
+				"  }" +
+				"}");
+		
+		PackageDeclaration pack = (PackageDeclaration) model.getElements().get(0);
+		assertEquals("example", pack.getName());
+		
+		Entity entity = (Entity) pack.getElements().get(0);
+		assertEquals("MyEntity", entity.getName());
+		
+		Property property = (Property) entity.getFeatures().get(0);
+		assertEquals("property", property.getName());
+		assertEquals("java.lang.String", property.getType().getIdentifier());
+	}
+}
