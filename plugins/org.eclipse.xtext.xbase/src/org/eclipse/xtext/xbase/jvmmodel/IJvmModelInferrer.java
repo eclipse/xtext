aa9236eda902ca95848b6c1c2b32d052a56675f5
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2011-03-10 23:38:14 +0100

    [xbase.jvmmodel] IJvmModelInferrer returns List of JvmDeclaredTypes instead of EObjects [domainmodel] Fix in ScopeProvider - associator returned unloaded proxy JvmTypes which broke the linking of implicit receivers - fix is only a workaround [xbase.ui] NPE in content assist for incomplete JvmOperations

--- plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/jvmmodel/IJvmModelInferrer.java
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/jvmmodel/IJvmModelInferrer.java
@@ -12,20 +12,35 @@ import static java.util.Collections.*;
 import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.common.types.JvmDeclaredType;
 
 import com.google.inject.ImplementedBy;
 
 /**
+ * <p>The model inferrer adapts a given source object with a {@link JvmDeclaredType}
+ * that mimics the interface of the source object and its children.</p>
+ * <p>Implementors have to traverse the given source object in {@link #inferJvmModel(EObject)}
+ * and decide which types to create according to the state of the model.</p>
+ * 
+ * @see IJvmModelAssociations
+ * @see IJvmModelAssociator
  * @author Jan Koehnlein - Initial contribution and API
  */
 @ImplementedBy(IJvmModelInferrer.NullImpl.class)
 public interface IJvmModelInferrer {
 
-	List<? extends EObject> inferJvmModel(EObject sourceObject);
+	/**
+	 * Returns the list of inferred declared types for a given source object.
+	 * @return the list of inferred types. May not be <code>null</code>. 
+	 */
+	List<? extends JvmDeclaredType> inferJvmModel(EObject sourceObject);
 	
+	/**
+	 * A null-implementation. Returns an empty list. 
+	 */
 	public static class NullImpl implements IJvmModelInferrer {
 
-		public List<? extends EObject> inferJvmModel(EObject sourceObject) {
+		public List<? extends JvmDeclaredType> inferJvmModel(EObject sourceObject) {
 			return emptyList();
 		}
 		
