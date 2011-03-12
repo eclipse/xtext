add6a49b35b63ee0bbd8c87148e3800d589eb02e
Sven Efftinge <sven.efftinge@itemis.de>
2011-03-12 04:12:20 +0100

    [examples] updated and improved the Xbase tutorial

---
+++ examples/org.eclipse.xtext.xtext.ui.examples/contents/org.eclipse.xtext.example.xbase.tutorial/src/helper/Circle.java
@@ -0,0 +1,11 @@
+package helper;
+
+public class Circle implements Shape {
+	public int diameter;
+
+	public Circle(int diameter) {
+		super();
+		this.diameter = diameter;
+	}
+	
+}
---
+++ examples/org.eclipse.xtext.xtext.ui.examples/contents/org.eclipse.xtext.example.xbase.tutorial/src/helper/Rectangle.java
@@ -0,0 +1,13 @@
+package helper;
+
+public class Rectangle implements Shape {
+	
+	public int height, width;
+
+	public Rectangle(int height, int width) {
+		super();
+		this.height = height;
+		this.width = width;
+	}
+	
+}
