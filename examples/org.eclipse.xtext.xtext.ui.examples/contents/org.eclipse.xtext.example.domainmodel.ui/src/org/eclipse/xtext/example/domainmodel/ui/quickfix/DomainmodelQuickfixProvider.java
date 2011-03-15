eaa5640fa863bda4924ca37a383c8c86705ff58f
Jan Koehnlein <jan.koehnlein@itemis.de>
2011-03-15 15:00:10 +0100

    [dmodel] Removed quickfix that collides with JVM model

--- examples/org.eclipse.xtext.xtext.ui.examples/contents/org.eclipse.xtext.example.domainmodel.ui/src/org/eclipse/xtext/example/domainmodel/ui/quickfix/DomainmodelQuickfixProvider.java
+++ examples/org.eclipse.xtext.xtext.ui.examples/contents/org.eclipse.xtext.example.domainmodel.ui/src/org/eclipse/xtext/example/domainmodel/ui/quickfix/DomainmodelQuickfixProvider.java
@@ -1,14 +1,8 @@
 package org.eclipse.xtext.example.domainmodel.ui.quickfix;
 
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.text.BadLocationException;
-import org.eclipse.xtext.example.domainmodel.domainmodel.AbstractElement;
-import org.eclipse.xtext.example.domainmodel.domainmodel.DomainModel;
-import org.eclipse.xtext.example.domainmodel.domainmodel.DomainmodelFactory;
-import org.eclipse.xtext.example.domainmodel.domainmodel.Entity;
 import org.eclipse.xtext.example.domainmodel.domainmodel.Feature;
-import org.eclipse.xtext.example.domainmodel.domainmodel.PackageDeclaration;
 import org.eclipse.xtext.example.domainmodel.validation.IssueCodes;
 import org.eclipse.xtext.ui.editor.model.IXtextDocument;
 import org.eclipse.xtext.ui.editor.model.edit.IModification;
@@ -46,36 +40,4 @@ public class DomainmodelQuickfixProvider extends DefaultQuickfixProvider {
 				});
 	}
 
-	@Fix(IssueCodes.MISSING_TYPE)
-	public void createReferenceType(final Issue issue, IssueResolutionAcceptor acceptor) {
-		final String linkText = issue.getData()[0];
-		acceptor.accept(issue, "Create entity '" + linkText + "'", "Create entity '" + linkText + "'", null,
-				new ISemanticModification() {
-					public void apply(final EObject element, IModificationContext context) {
-						createNewEntity((Entity) element.eContainer().eContainer(), linkText);
-					}
-				});
-		createLinkingIssueResolutions(issue, acceptor);
-	}
-
-	protected boolean createNewEntity(Entity sibling, String name) {
-		Entity newEntity = DomainmodelFactory.eINSTANCE.createEntity();
-		newEntity.setName(name);
-		return addEntityAsSibling(sibling, newEntity);
-	}
-
-	protected boolean addEntityAsSibling(Entity sibling, Entity newType) {
-		EObject container = sibling.eContainer();
-		EList<AbstractElement> elements = null;
-		if (container instanceof PackageDeclaration) {
-			elements = ((PackageDeclaration) container).getElements();
-		} else if (container instanceof DomainModel) {
-			elements = ((DomainModel) container).getElements();
-		} else {
-			return false;
-		}
-		int index = elements.indexOf(sibling) + 1;
-		elements.add(index, newType);
-		return true;
-	}
 }
