42a46bfe4af8326e729357c35178c9f244a327a5
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2011-03-29 16:42:14 +0200

    [xtext.ui] Improve performance of TokenScanner - see https://bugs.eclipse.org/bugs/show_bug.cgi?id=326524

--- plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/syntaxcoloring/TokenScanner.java
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/syntaxcoloring/TokenScanner.java
@@ -8,27 +8,108 @@
 package org.eclipse.xtext.ui.editor.syntaxcoloring;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.rules.IToken;
 import org.eclipse.jface.text.rules.Token;
 import org.eclipse.xtext.ui.editor.model.ILexerTokenRegion;
 import org.eclipse.xtext.ui.editor.model.Regions;
 import org.eclipse.xtext.ui.editor.model.XtextDocument;
 
-import com.google.common.collect.Iterables;
+import com.google.common.base.Predicate;
+import com.google.common.collect.UnmodifiableIterator;
 import com.google.inject.Inject;
 
 /**
- * 
- * TokenScanner implementation based on {@link XtextDocument#getTokens(int)}
+ * TokenScanner implementation based on {@link XtextDocument#getTokens()}
  * 
  * @author Sven Efftinge - Initial contribution and API
+ * @author Sebastian Zarnekow - Use reentrant iterator to reduce runtime complexity
  */
 public class TokenScanner extends AbstractTokenScanner {
 
-	private Iterator<ILexerTokenRegion> iterator;
+	private RangedReentrantIterator reentrantIterator = new RangedReentrantIterator();
 	private ILexerTokenRegion currentToken;
+	
+	/**
+	 * <p>A reentrant iterator allows to reuse and filter the elements another iterator
+	 * and may update its filter criteria continuously.</p>
+	 * <p>It is assumed that {@link #setRange(IDocument, int, int)} will be called with
+	 * ascending order most of the time thus a sorted iterator may be reused and traversed
+	 * further after the range has been updated.</p> 
+	 */
+	protected class RangedReentrantIterator extends UnmodifiableIterator<ILexerTokenRegion> {
+		
+		private Iterator<ILexerTokenRegion> delegate;
+		private ILexerTokenRegion current = null;
+		private boolean computedHasNext = false;
+		private boolean hasNext = false;
+		private Predicate<? super IRegion> overlapFilter;
+		private int regionOffset;
+		
+		public boolean hasNext() {
+			// we already know whether we have a next element
+			if (computedHasNext)
+				return hasNext;
+			
+			computedHasNext = true;
+			while (delegate.hasNext()) {
+				current = delegate.next();
+				if (overlapFilter.apply(current)) {
+					hasNext = true;
+					break;
+				} else {
+					// filter until we leave the valid region - no need to iterate
+					// the delegate until it's done
+					if (current.getOffset() > regionOffset) {
+						hasNext = false;
+						break;
+					}
+				}
+			} 
+			return hasNext;
+		}
+
+		public ILexerTokenRegion next() {
+			if (!computedHasNext) {
+				// compute hasNext on demand to make sure the internal state is valid
+				hasNext();
+			}
+			if (!hasNext) {
+				throw new NoSuchElementException("You should check for #hasNext prior to calling #next");
+			}
+			ILexerTokenRegion result = current;
+			current = null;
+			computedHasNext = false;
+			hasNext = false;
+			return result;
+		}
+		
+		public void setRange(IDocument document, final int offset, final int length) {
+			this.regionOffset = offset;
+			overlapFilter = Regions.overlaps(offset, length);
+			if (current == null) {
+				delegate = getTokens(document).iterator();
+			} else {
+				if (current.getOffset() <= offset && current.getOffset() + current.getLength() > offset) {
+					// offset is inside of current - return current as next
+					computedHasNext = true;
+					hasNext = true;
+				} else if (current.getOffset() + current.getLength() <= offset) {
+					// keep the delegate but compute hasNext on demand
+					computedHasNext = false;
+					hasNext = false;
+				} else {
+					// restart - use a new delegate
+					computedHasNext = false;
+					hasNext = false;
+					delegate = getTokens(document).iterator();
+				}
+			}
+		}
+	}
 
 	@Inject
 	private AbstractAntlrTokenToAttributeIdMapper tokenIdMapper;
@@ -39,7 +120,7 @@ public class TokenScanner extends AbstractTokenScanner {
 
 	public void setRange(IDocument document, final int offset, final int length) {
 		currentToken=null;
-		iterator = Iterables.filter(getTokens(document),Regions.overlaps(offset, length)).iterator();
+		reentrantIterator.setRange(document, offset, length);
 	}
 
 	protected Iterable<ILexerTokenRegion> getTokens(IDocument document) {
@@ -48,9 +129,9 @@ public class TokenScanner extends AbstractTokenScanner {
 	}
 
 	public IToken nextToken() {
-		if (!iterator.hasNext())
+		if (!reentrantIterator.hasNext())
 			return Token.EOF;
-		currentToken = iterator.next();
+		currentToken = reentrantIterator.next();
 		return createToken(currentToken);
 	}
 
@@ -69,7 +150,7 @@ public class TokenScanner extends AbstractTokenScanner {
 	}
 	
 	protected Iterator<ILexerTokenRegion> getIterator() {
-		return iterator;
+		return reentrantIterator;
 	}
 	
 	protected ILexerTokenRegion getCurrentToken() {
