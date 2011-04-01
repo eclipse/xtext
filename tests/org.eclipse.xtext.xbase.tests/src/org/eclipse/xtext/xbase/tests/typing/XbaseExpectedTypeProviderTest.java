23ab4a93207f869441096c600ee54a11462e03bf
Sven Efftinge <sven.efftinge@itemis.de>
2011-04-01 16:58:39 +0200

    [Xbase] fixed test expectations

--- tests/org.eclipse.xtext.xbase.tests/src/org/eclipse/xtext/xbase/tests/typing/XbaseExpectedTypeProviderTest.java
+++ tests/org.eclipse.xtext.xbase.tests/src/org/eclipse/xtext/xbase/tests/typing/XbaseExpectedTypeProviderTest.java
@@ -152,7 +152,7 @@ public class XbaseExpectedTypeProviderTest extends AbstractXbaseTestCase {
 	public void testIfExpression() throws Exception {
 		XIfExpression target = (XIfExpression) expressionWithExpectedType("if (null) null else null", "String");
 
-		assertExpected("java.lang.Boolean", target.getIf());
+		assertExpected("boolean", target.getIf());
 		assertExpected("java.lang.String", target.getThen());
 		assertExpected("java.lang.String", target.getElse());
 	}
@@ -173,14 +173,14 @@ public class XbaseExpectedTypeProviderTest extends AbstractXbaseTestCase {
 	public void testWhileExpression_0() throws Exception {
 		XWhileExpression exp = (XWhileExpression) expression("while (null) null");
 
-		assertExpected("java.lang.Boolean", exp.getPredicate());
+		assertExpected("boolean", exp.getPredicate());
 		assertExpected(null, exp.getBody());
 	}
 
 	public void testWhileExpression_1() throws Exception {
 		XDoWhileExpression exp = (XDoWhileExpression) expression("do null while (null)");
 
-		assertExpected("java.lang.Boolean", exp.getPredicate());
+		assertExpected("boolean", exp.getPredicate());
 		assertExpected(null, exp.getBody());
 	}
 
