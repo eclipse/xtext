d8052fb522d2cac7ace93f6a9e28f1b98889adaf
Jan Koehnlein <jan.koehnlein@itemis.de>
2011-04-08 11:44:12 +0200

    [ecore] Index ecore elements with nsURI and name https://bugs.eclipse.org/bugs/show_bug.cgi?id=332557

--- plugins/org.eclipse.xtext.ecore/src/org/eclipse/xtext/ecore/EcoreResourceDescriptionStrategy.java
+++ plugins/org.eclipse.xtext.ecore/src/org/eclipse/xtext/ecore/EcoreResourceDescriptionStrategy.java
@@ -9,8 +9,9 @@ package org.eclipse.xtext.ecore;
 
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.xtext.naming.IQualifiedNameProvider;
 import org.eclipse.xtext.naming.QualifiedName;
 import org.eclipse.xtext.resource.EObjectDescription;
 import org.eclipse.xtext.resource.IEObjectDescription;
@@ -18,6 +19,7 @@ import org.eclipse.xtext.resource.impl.DefaultResourceDescriptionStrategy;
 import org.eclipse.xtext.util.IAcceptor;
 
 import com.google.common.collect.Maps;
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 /**
@@ -26,24 +28,35 @@ import com.google.inject.Singleton;
 @Singleton
 public class EcoreResourceDescriptionStrategy extends DefaultResourceDescriptionStrategy {
 
+	private static final Logger LOG = Logger.getLogger(EcoreResourceDescriptionStrategy.class);
+
 	public static final String NS_URI_INDEX_ENTRY = "nsURI";
-	public static final String NS_URI_INDEX_ENTRY_NAME = "name";
-	
+
+	@Inject
+	private NsURIQualifiedNameProvider nsURIQualifiedNameProvider;
+
 	@Override
 	public boolean createEObjectDescriptions(EObject eObject, IAcceptor<IEObjectDescription> acceptor) {
-		if (eObject instanceof EPackage) {
-			EPackage pack = (EPackage) eObject;
-			if (pack.getNsURI() != null && pack.getNsURI().length() != 0) {
-				QualifiedName qualifiedName = QualifiedName.create(pack.getNsURI());
-				Map<String, String> userData = Maps.newHashMapWithExpectedSize(2);
-				userData.put(NS_URI_INDEX_ENTRY, "true");
-				userData.put(NS_URI_INDEX_ENTRY_NAME, pack.getName());
-				IEObjectDescription description = EObjectDescription.create(
-						qualifiedName, pack, userData);
-				acceptor.accept(description);
-			}
-		}
-		return super.createEObjectDescriptions(eObject, acceptor);
+		boolean isProcessContents = createEObjectDescriptions(getQualifiedNameProvider(), false, eObject, acceptor);
+		isProcessContents |= createEObjectDescriptions(nsURIQualifiedNameProvider, true, eObject, acceptor);
+		return isProcessContents;
 	}
-	
+
+	protected boolean createEObjectDescriptions(IQualifiedNameProvider qualifiedNameProvider, boolean isNsURI,
+			EObject eObject, IAcceptor<IEObjectDescription> acceptor) {
+		try {
+			QualifiedName qualifiedName = qualifiedNameProvider.getFullyQualifiedName(eObject);
+			if (qualifiedName != null) {
+				Map<String, String> userData = Maps.newHashMapWithExpectedSize(1);
+				userData.put(NS_URI_INDEX_ENTRY, Boolean.toString(isNsURI));
+				IEObjectDescription description = EObjectDescription.create(qualifiedName, eObject, userData);
+				acceptor.accept(description);
+				return true;
+			}
+		} catch (Exception exc) {
+			LOG.error(exc.getMessage());
+		}
+		return false;
+	}
+
 }
