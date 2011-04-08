700d1dfd63d0fd63238b6b300e2ba68b633bff33
Jan Koehnlein <jan.koehnlein@itemis.de>
2011-04-08 12:04:18 +0200

    [ecore] Added a search filter  for nsURI based QNs

---
+++ plugins/org.eclipse.xtext.ui.ecore/src/org/eclipse/xtext/ui/ecore/search/EcoreSearchFilter.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2011 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.ui.ecore.search;
+
+import org.eclipse.xtext.ecore.EcoreResourceDescriptionStrategy;
+import org.eclipse.xtext.resource.IEObjectDescription;
+import org.eclipse.xtext.ui.search.IXtextSearchFilter;
+
+public class EcoreSearchFilter implements IXtextSearchFilter {
+
+	public boolean reject(IEObjectDescription element) {
+		return "true".equals(element.getUserData(EcoreResourceDescriptionStrategy.NS_URI_INDEX_ENTRY));
+	}
+
+}
