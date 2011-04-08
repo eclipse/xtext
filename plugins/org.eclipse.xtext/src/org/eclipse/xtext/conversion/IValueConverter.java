93c67ca51242a8e4fb7aa4c3f98f15c6d5ad51f4
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2011-04-08 14:03:00 +0200

    [xtext] Updated API problem filters to reflect the latest changes (e.g. introduction of EOF token) [xtext][converters] Updated JavaDoc

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/conversion/IValueConverter.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/conversion/IValueConverter.java
@@ -12,9 +12,10 @@ import org.eclipse.xtext.AbstractRule;
 import org.eclipse.xtext.nodemodel.INode;
 
 /**
+ * A {@link ValueConverter} is used to create a semantic value from 
+ * an input string or node and vice versa.
+ * 
  * @author Sven Efftinge - Initial contribution and API
- *
- * @param <Type>
  */
 public interface IValueConverter<Type> {
 	
@@ -28,16 +29,29 @@ public interface IValueConverter<Type> {
 	};
 	
 	/**
-	 * @param string
-	 * @param node
-	 * @return
-	 * @throws ValueConverterException
+	 * <p>Creates a value from the given input. The input is conformant
+	 * to a data type or terminal rule.</p> 
+	 * 
+	 * <p>The given <code>string</code>
+	 * or <code>node</code> may be null but not both of them.</p>
+	 * 
+	 * @param string the string that was inferred from the node. Usually the node's text
+	 * but may be reduced to the parts of the node that are not 
+	 * {@link org.eclipse.xtext.nodemodel.ILeafNode#isHidden() hidden}.
+	 * 
+	 * @param node the parsed node including hidden parts.
+	 * @return the new value or <code>null</code>.
+	 * @throws ValueConverterException indicates that the string or node did not fulfil
+	 * the expected format.
 	 */
 	Type toValue(String string, INode node) throws ValueConverterException;
 	
 	/**
-	 * @param value
-	 * @return
+	 * Transforms the given value to a string that is conformant to the expected
+	 * terminal or data type rule.
+	 * @param value the to-be-transformed value
+	 * @return a string represenation for that value.
+	 * @throws ValueConverterException indicates that the value cannot be converted to a valid string.
 	 */
 	String toString(Type value) throws ValueConverterException;
 	
