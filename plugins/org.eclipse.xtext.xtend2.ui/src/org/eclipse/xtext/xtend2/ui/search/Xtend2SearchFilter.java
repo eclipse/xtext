a7ad45de1443f9e54d2da6006baa47c6965e020c
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2011-04-09 14:32:45 +0200

    [xtend] Added some missing copyright headers

--- plugins/org.eclipse.xtext.xtend2.ui/src/org/eclipse/xtext/xtend2/ui/search/Xtend2SearchFilter.java
+++ plugins/org.eclipse.xtext.xtend2.ui/src/org/eclipse/xtext/xtend2/ui/search/Xtend2SearchFilter.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2011 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package org.eclipse.xtext.xtend2.ui.search;
 
 import org.eclipse.xtext.common.types.TypesPackage;
@@ -7,6 +14,9 @@ import org.eclipse.xtext.ui.search.IXtextSearchFilter;
 
 import com.google.inject.Inject;
 
+/**
+ * Filters inferred JVM elements from open model element dialog.
+ */
 public class Xtend2SearchFilter implements IXtextSearchFilter {
 
 	@Inject
