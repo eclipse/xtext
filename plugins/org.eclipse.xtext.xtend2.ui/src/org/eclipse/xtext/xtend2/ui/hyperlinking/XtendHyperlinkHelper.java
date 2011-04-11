865ac4244e44b23bf46ddb9c36ad2405c03391b5
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2011-04-11 17:31:52 +0200

    [xtend][hyperlinking] HyperlinkHelper should be aware of JVM type references (see https://bugs.eclipse.org/bugs/show_bug.cgi?id=342423)
    Solution was to extend the TypesAwareHyperlinkHelper instead of replacing it

--- plugins/org.eclipse.xtext.xtend2.ui/src/org/eclipse/xtext/xtend2/ui/hyperlinking/XtendHyperlinkHelper.java
+++ plugins/org.eclipse.xtext.xtend2.ui/src/org/eclipse/xtext/xtend2/ui/hyperlinking/XtendHyperlinkHelper.java
@@ -11,8 +11,8 @@ import java.util.Set;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.text.Region;
+import org.eclipse.xtext.common.types.xtext.ui.TypeAwareHyperlinkHelper;
 import org.eclipse.xtext.resource.XtextResource;
-import org.eclipse.xtext.ui.editor.hyperlinking.HyperlinkHelper;
 import org.eclipse.xtext.ui.editor.hyperlinking.IHyperlinkAcceptor;
 import org.eclipse.xtext.xbase.jvmmodel.IJvmModelAssociations;
 
@@ -21,7 +21,7 @@ import com.google.inject.Inject;
 /**
  * @author Jan Koehnlein - Initial contribution and API
  */
-public class XtendHyperlinkHelper extends HyperlinkHelper {
+public class XtendHyperlinkHelper extends TypeAwareHyperlinkHelper {
 
 	@Inject
 	private IJvmModelAssociations associations;
