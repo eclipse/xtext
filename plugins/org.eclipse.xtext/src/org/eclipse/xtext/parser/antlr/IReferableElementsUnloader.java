77526dd5faae2fc7dcdcd0c649af575ffdc1f24a
Jan Koehnlein <jan.koehnlein@itemis.de>
2011-04-11 14:21:42 +0200

    [xtext] Make unloader work with EContentAdapters as well as with Ecore https://bugs.eclipse.org/bugs/show_bug.cgi?id=340985

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/parser/antlr/IReferableElementsUnloader.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/parser/antlr/IReferableElementsUnloader.java
@@ -7,42 +7,55 @@
  *******************************************************************************/
 package org.eclipse.xtext.parser.antlr;
 
-import java.util.Iterator;
+import static com.google.common.collect.Lists.*;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.xtext.util.Pair;
+import org.eclipse.xtext.util.Tuples;
 
 import com.google.inject.ImplementedBy;
 
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
+ * @author Jan Koehnlein
  */
 @ImplementedBy(IReferableElementsUnloader.NullUnloader.class)
 public interface IReferableElementsUnloader {
 
 	void unloadRoot(EObject root);
-	
+
 	final class NullUnloader implements IReferableElementsUnloader {
 
 		public void unloadRoot(EObject root) {
+			// do nothing
 		}
-		
+
 	}
-	
+
 	class GenericUnloader implements IReferableElementsUnloader {
 
 		public void unloadRoot(EObject root) {
-			for(Iterator<EObject> i = EcoreUtil.getAllProperContents(root, false); i.hasNext();) {
-				final EObject next = i.next();
-				next.eAdapters().clear();
-				((InternalEObject) next).eSetProxyURI(EcoreUtil.getURI(next));
+			// Content adapters should be removed the same way as they are added: top-down. 
+			// Fragments are recursive, so we need them to be calculated before proxifying the container.
+			// OTOH, some model elements resolve their children when being proxified (e.g. EPackageImpl).
+			// => first calculate all fragments, then proxify elements starting form root.
+			List<Pair<EObject, URI>> elementsToUnload = newArrayList();
+			elementsToUnload.add(Tuples.create(root, EcoreUtil.getURI(root)));
+			for (Iterator<EObject> i = EcoreUtil.getAllProperContents(root, false); i.hasNext();) {
+				EObject element = i.next();
+				elementsToUnload.add(Tuples.create(element, EcoreUtil.getURI(element)));
+				element.eAdapters().clear();
 			}
-			((InternalEObject) root).eSetProxyURI(EcoreUtil.getURI(root));
-			root.eAdapters().clear();
-
+			for (Pair<EObject,URI> elementToUnload : elementsToUnload) {
+				((InternalEObject) elementToUnload.getFirst()).eSetProxyURI(elementToUnload.getSecond());
+			}
 		}
-		
 	}
-	
+
 }
