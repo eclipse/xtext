e76bae4fb46f00a4f977a45d4ba04c0d7bb45cf6
Jan Koehnlein <jan.koehnlein@itemis.de>
2011-04-13 15:30:46 +0200

    [refactoring] Common superclass for refactoring strategies

---
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/refactoring/impl/AbstractRenameStrategy.java
@@ -0,0 +1,82 @@
+/*******************************************************************************
+ * Copyright (c) 2011 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.ui.refactoring.impl;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.ltk.core.refactoring.RefactoringStatus;
+import org.eclipse.xtext.EcoreUtil2;
+import org.eclipse.xtext.ui.refactoring.IRenameStrategy;
+import org.eclipse.xtext.util.CancelIndicator;
+import org.eclipse.xtext.util.SimpleAttributeResolver;
+import org.eclipse.xtext.util.Strings;
+
+/**
+ * @author Jan Koehnlein - Initial contribution and API
+ */
+public abstract class AbstractRenameStrategy implements IRenameStrategy {
+
+	protected String originalName;
+	protected URI targetElementOriginalURI;
+	protected URI targetElementNewURI;
+
+	protected AbstractRenameStrategy(EObject targetElement) {
+		this.targetElementOriginalURI = EcoreUtil.getURI(targetElement);
+		EAttribute nameAttribute = getNameAttribute(targetElement);
+		this.originalName = targetElement.eGet(nameAttribute).toString();
+		if (Strings.isEmpty(originalName))
+			throw new RefactoringStatusException("Target element does not have a name", false);
+	}
+
+	public String getOriginalName() {
+		return originalName;
+	}
+
+	public RefactoringStatus validateNewName(String newName) {
+		//TODO: Validate if name is valid in the meaning of the DataTypeRule / Terminal
+		RefactoringStatus newRefactoringStatus = new RefactoringStatus();
+		if (Strings.equal(newName, originalName))
+			newRefactoringStatus.addWarning("Name should be different");
+		return newRefactoringStatus;
+	}
+
+	public void applyDeclarationChange(String newName, ResourceSet resourceSet) {
+		resolveProxies(resourceSet);
+		EObject renamedElement = setName(targetElementOriginalURI, newName, resourceSet);
+		targetElementNewURI = EcoreUtil.getURI(renamedElement);
+	}
+
+	protected void resolveProxies(ResourceSet resourceSet) {
+		for(int i=0; i< resourceSet.getResources().size(); ++i) 
+			EcoreUtil2.resolveLazyCrossReferences(resourceSet.getResources().get(i), CancelIndicator.NullImpl);
+	}
+
+	public void revertDeclarationChange(ResourceSet resourceSet) {
+		if(targetElementNewURI == null)
+			return;
+		setName(targetElementNewURI, originalName, resourceSet);
+	}
+	
+	protected EObject setName(URI targetElementURI, String newName, ResourceSet resourceSet) {
+		EObject targetElement = resourceSet.getEObject(targetElementURI, false);
+		if(targetElement == null) {
+			throw new RefactoringStatusException("Target element not loaded.", true);
+		}
+		EAttribute nameAttribute = getNameAttribute(targetElement);
+		targetElement.eSet(nameAttribute, newName);
+		return targetElement;
+	}
+	
+	protected EAttribute getNameAttribute(EObject eObject) {
+		return SimpleAttributeResolver.NAME_RESOLVER.getAttribute(eObject);
+	}
+
+}
--- plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/refactoring/impl/DefaultRenameStrategy.java
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/refactoring/impl/DefaultRenameStrategy.java
@@ -7,23 +7,15 @@
  *******************************************************************************/
 package org.eclipse.xtext.ui.refactoring.impl;
 
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.eclipse.ltk.core.refactoring.RefactoringStatus;
 import org.eclipse.text.edits.ReplaceEdit;
 import org.eclipse.text.edits.TextEdit;
-import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.resource.ILocationInFileProvider;
 import org.eclipse.xtext.ui.refactoring.IRefactoringUpdateAcceptor;
 import org.eclipse.xtext.ui.refactoring.IRenameStrategy;
 import org.eclipse.xtext.ui.refactoring.ui.IRenameElementContext;
-import org.eclipse.xtext.util.CancelIndicator;
 import org.eclipse.xtext.util.ITextRegion;
-import org.eclipse.xtext.util.SimpleAttributeResolver;
-import org.eclipse.xtext.util.Strings;
 
 import com.google.inject.Inject;
 
@@ -31,7 +23,7 @@ import com.google.inject.Inject;
  * @author Jan Koehnlein - Initial contribution and API
  * @author Holger Schill
  */
-public class DefaultRenameStrategy implements IRenameStrategy {
+public class DefaultRenameStrategy extends AbstractRenameStrategy{
 
 	public static class Provider implements IRenameStrategy.Provider {
 
@@ -47,68 +39,15 @@ public class DefaultRenameStrategy implements IRenameStrategy {
 		}
 	}
 
-	protected String originalName;
-	protected URI targetElementOriginalURI;
 	protected ITextRegion originalNameRegion;
-	protected URI targetElementNewURI;
 
 	protected DefaultRenameStrategy(EObject targetElement, ILocationInFileProvider locationInFileProvider) {
-		this.targetElementOriginalURI = EcoreUtil.getURI(targetElement);
-		EAttribute nameAttribute = getNameAttribute(targetElement);
-		this.originalName = targetElement.eGet(nameAttribute).toString();
-		if (Strings.isEmpty(originalName))
-			throw new RefactoringStatusException("Target element does not have a name", false);
-		originalNameRegion = locationInFileProvider.getFullTextRegion(targetElement, nameAttribute, 0);
+		super(targetElement);
+		originalNameRegion = locationInFileProvider.getFullTextRegion(targetElement, getNameAttribute(targetElement), 0);
 	}
 
-	public String getOriginalName() {
-		return originalName;
-	}
-
-	public RefactoringStatus validateNewName(String newName) {
-		//TODO: Validate if name is valid in the meaning of the DataTypeRule / Terminal
-		RefactoringStatus newRefactoringStatus = new RefactoringStatus();
-		if (Strings.equal(newName, originalName))
-			newRefactoringStatus.addWarning("Name should be different");
-		return newRefactoringStatus;
-	}
-
-	public void applyDeclarationChange(String newName, ResourceSet resourceSet) {
-		resolveProxies(resourceSet);
-		EObject renamedElement = setName(targetElementOriginalURI, newName, resourceSet);
-		targetElementNewURI = EcoreUtil.getURI(renamedElement);
-	}
-
-	protected void resolveProxies(ResourceSet resourceSet) {
-		for(int i=0; i< resourceSet.getResources().size(); ++i) 
-			EcoreUtil2.resolveLazyCrossReferences(resourceSet.getResources().get(i), CancelIndicator.NullImpl);
-	}
-
-	public void revertDeclarationChange(ResourceSet resourceSet) {
-		if(targetElementNewURI == null)
-			return;
-		setName(targetElementNewURI, originalName, resourceSet);
-	}
-	
-	/**
-	 * @since 2.0
-	 */
 	public void createDeclarationUpdates(String newName, ResourceSet resourceSet, IRefactoringUpdateAcceptor updateAcceptor) {
 		updateAcceptor.accept(targetElementOriginalURI.trimFragment(), getDeclarationTextEdit(newName));
-	}
-
-	protected EObject setName(URI targetElementURI, String newName, ResourceSet resourceSet) {
-		EObject targetElement = resourceSet.getEObject(targetElementURI, false);
-		if(targetElement == null) {
-			throw new RefactoringStatusException("Target element not loaded.", true);
-		}
-		EAttribute nameAttribute = getNameAttribute(targetElement);
-		targetElement.eSet(nameAttribute, newName);
-		return targetElement;
-	}
-	
-	protected EAttribute getNameAttribute(EObject eObject) {
-		return SimpleAttributeResolver.NAME_RESOLVER.getAttribute(eObject);
 	}
 
 	protected TextEdit getDeclarationTextEdit(String newName) {
