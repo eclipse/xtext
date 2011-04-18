8b817d4f575e5e1a8ec800289647df4058111626
Jan Koehnlein <jan.koehnlein@itemis.de>
2011-04-18 14:40:03 +0200

    [builder] Test for EMF binary resource bugfix

---
+++ tests/org.eclipse.xtext.builder.tests/src/org/eclipse/xtext/builder/builderState/BinaryGuillemetBugfixTest.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2011 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.builder.builderState;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.eclipse.emf.ecore.resource.impl.BinaryResourceImpl;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Jan Koehnlein - Initial contribution and API
+ */
+public class BinaryGuillemetBugfixTest extends TestCase {
+
+	public void testEncodeGuillemet() throws IOException {
+		String s = "«";
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		new BinaryResourceImpl.EObjectOutputStream(out, null).writeString(s);
+		byte[] byteArray = out.toByteArray();
+		ByteArrayInputStream in = new ByteArrayInputStream(byteArray);
+		String newS = new BinaryResourceImpl.EObjectInputStream(in, null) {
+			@Override
+			public String readString() throws IOException {
+				int length = readCompressedInt();
+				if (length == -1) {
+					return null;
+				} else {
+					if (characters == null || characters.length < length) {
+						characters = new char[length];
+					}
+					LOOP: for (int i = 0; i < length; ++i) {
+						byte value = readByte();
+						if (value == 0) {
+							do {
+								characters[i] = readChar();
+							} while (++i < length);
+							break LOOP;
+						} else {
+							// here's the fix
+							char charValue = (char) (value & 0xff);
+							characters[i] = charValue;
+						}
+					}
+					return new String(characters, 0, length);
+				}
+			}
+
+		}.readString();
+		assertEquals(s, newS);
+	}
+}
