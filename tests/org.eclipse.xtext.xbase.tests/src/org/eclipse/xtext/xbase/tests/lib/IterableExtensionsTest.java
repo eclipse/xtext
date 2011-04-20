053c233270b6c76925ba22c9d6882290ff8a87fd
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2011-04-20 18:16:23 +0200

    [xbase][lib] JavaDoc for IterableExtensions, some fixes in implementation

---
+++ tests/org.eclipse.xtext.xbase.tests/src/org/eclipse/xtext/xbase/tests/lib/IterableExtensionsTest.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2011 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.xbase.tests.lib;
+
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.xtext.xbase.lib.Functions;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Sebastian Zarnekow - Initial contribution and API
+ */
+public class IterableExtensionsTest extends TestCase {
+
+	public void testFindLast() {
+		List<String> list = Lists.newArrayList("a", "b", "c");
+		String last = IterableExtensions.findLast(list, new Functions.Function1<String, Boolean>() {
+			public Boolean apply(String p) {
+				return true;
+			}
+		});
+		assertEquals("c", last);
+	}
+	
+	public void testLast() {
+		Set<String> emptySet = Sets.newTreeSet();
+		String last = IterableExtensions.last(emptySet);
+		assertNull(last);
+	}
+	
+	public void testJoinWithNull() {
+		List<String> list = Lists.newArrayList("a", null, "c");
+		String string = IterableExtensions.join(list, ",");
+		assertEquals("a,null,c", string);
+	}
+	
+}
