3946bb528419c3e55089c1a45e791c82f168920e
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2011-04-27 10:13:59 +0200

    [xbase][lib] JavaDoc for BooleanExtensions and CollectionExtensions

--- plugins/org.eclipse.xtext.xbase.lib/src/org/eclipse/xtext/xbase/lib/CollectionExtensions.java
+++ plugins/org.eclipse.xtext.xbase.lib/src/org/eclipse/xtext/xbase/lib/CollectionExtensions.java
@@ -23,71 +23,183 @@ import com.google.common.collect.ImmutableSortedMap;
 import com.google.common.collect.ImmutableSortedSet;
 
 /**
+ * This is an extension library for {@link Collection collections}.
+ * 
  * @author Sven Efftinge - Initial contribution and API
  * @author Sebastian Zarnekow
  */
 public class CollectionExtensions {
 
 	/**
-	 * operator mapping for {@link Collection#add(Object)} to +=
+	 * The operator mapping from {@code +=} to {@link Collection#add(Object)}. Returns <code>true</code> if the
+	 * collection changed due to this operation.
+	 * 
+	 * @param collection
+	 *            the to-be-changed collection. May not be <code>null</code>.
+	 * @param value
+	 *            the value that should be added to the collection.
+	 * @return <code>true</code> if the collection changed due to this operation.
+	 * @see Collection#add(Object)
 	 */
-	public static <E> boolean operator_add(Collection<? super E> a, E b) {
-		return a.add(b);
+	public static <E> boolean operator_add(Collection<? super E> collection, E value) {
+		return collection.add(value);
 	}
-	
+
 	/**
-	 * operator mapping for {@link Collection#addAll(Collection)} to +=
+	 * The operator mapping from {@code +=} to {@link Collection#addAll(Collection)}. Returns <code>true</code> if the
+	 * collection changed due to this operation.
+	 * 
+	 * @param collection
+	 *            the to-be-changed collection. May not be <code>null</code>.
+	 * @param newElements
+	 *            elements to be inserted into the collection. May not be <code>null</code> but may contain
+	 *            <code>null</code> elements if the the target collection supports <code>null</code> elements.
+	 * @return <code>true</code> if the collection changed due to this operation.
+	 * @see Collection#addAll(Collection)
 	 */
-	public static <E> boolean operator_add(Collection<? super E> a, Collection<? extends E> b) {
-		return a.addAll(b);
+	public static <E> boolean operator_add(Collection<E> collection, Collection<? extends E> newElements) {
+		return collection.addAll(newElements);
 	}
-	
+
+	/**
+	 * Returns an unmodifiable view of the specified {@code list}.
+	 * 
+	 * @param list
+	 *            the list for which an unmodifiable view is to be returned. May not be <code>null</code>.
+	 * @return an unmodifiable view of the specified list.
+	 * @see Collections#unmodifiableList(List)
+	 */
 	public static <T> List<T> unmodifiableView(List<? extends T> list) {
 		return Collections.unmodifiableList(list);
 	}
-	
+
+	/**
+	 * Returns an unmodifiable view of the specified {@code collection}.
+	 * 
+	 * @param collection
+	 *            the collection for which an unmodifiable view is to be returned. May not be <code>null</code>.
+	 * @return an unmodifiable view of the specified collection.
+	 * @see Collections#unmodifiableCollection(Collection)
+	 */
 	public static <T> Collection<T> unmodifiableView(Collection<? extends T> collection) {
 		return Collections.unmodifiableCollection(collection);
 	}
-	
+
+	/**
+	 * Returns an unmodifiable view of the specified {@code set}.
+	 * 
+	 * @param set
+	 *            the set for which an unmodifiable view is to be returned. May not be <code>null</code>.
+	 * @return an unmodifiable view of the specified set.
+	 * @see Collections#unmodifiableSet(Set)
+	 */
 	public static <T> Set<T> unmodifiableView(Set<? extends T> set) {
 		return Collections.unmodifiableSet(set);
 	}
-	
+
+	/**
+	 * Returns an unmodifiable view of the specified sorted {@code set}.
+	 * 
+	 * @param set
+	 *            the sorted set for which an unmodifiable view is to be returned. May not be <code>null</code>.
+	 * @return an unmodifiable view of the specified sorted set.
+	 * @see Collections#unmodifiableSortedSet(SortedSet)
+	 */
 	public static <T> SortedSet<T> unmodifiableView(SortedSet<T> set) {
 		return Collections.unmodifiableSortedSet(set);
 	}
-	
+
+	/**
+	 * Returns an unmodifiable view of the specified {@code map}.
+	 * 
+	 * @param map
+	 *            the map for which an unmodifiable view is to be returned. May not be <code>null</code>.
+	 * @return an unmodifiable view of the specified map.
+	 * @see Collections#unmodifiableMap(Map)
+	 */
 	public static <K, V> Map<K, V> unmodifiableView(Map<? extends K, ? extends V> map) {
 		return Collections.unmodifiableMap(map);
 	}
-	
+
+	/**
+	 * Returns an unmodifiable view of the specified sorted {@code map}.
+	 * 
+	 * @param map
+	 *            the sorted map for which an unmodifiable view is to be returned. May not be <code>null</code>.
+	 * @return an unmodifiable view of the specified sorted map.
+	 * @see Collections#unmodifiableSortedMap(SortedMap)
+	 */
 	public static <K, V> SortedMap<K, V> unmodifiableView(SortedMap<K, ? extends V> map) {
 		return Collections.unmodifiableSortedMap(map);
 	}
-	
+
+	/**
+	 * Returns an immutable copy of the specified {@code list}.
+	 * 
+	 * @param list
+	 *            the list for which an immutable copy should be created. May not be <code>null</code>.
+	 * @return an immutable copy of the specified list.
+	 */
 	public static <T> List<T> immutableCopy(List<? extends T> list) {
 		return ImmutableList.copyOf(list);
 	}
-	
+
+	/**
+	 * Returns an immutable copy of the specified {@code set}.
+	 * 
+	 * @param set
+	 *            the set for which an immutable copy should be created. May not be <code>null</code>.
+	 * @return an immutable copy of the specified set.
+	 */
 	public static <T> Set<T> immutableCopy(Set<? extends T> set) {
 		return ImmutableSet.copyOf(set);
 	}
-	
+
+	/**
+	 * Returns an immutable copy of the specified sorted {@code set}.
+	 * 
+	 * @param set
+	 *            the sorted set for which an immutable copy should be created. May not be <code>null</code>.
+	 * @return an immutable copy of the specified sorted set.
+	 */
 	public static <T> SortedSet<T> immutableCopy(SortedSet<T> set) {
 		return ImmutableSortedSet.copyOfSorted(set);
 	}
-	
+
+	/**
+	 * Returns an immutable copy of the specified {@code map}.
+	 * 
+	 * @param map
+	 *            the map for which an immutable copy should be created. May not be <code>null</code>.
+	 * @return an immutable copy of the specified map.
+	 */
 	public static <K, V> Map<K, V> immutableCopy(Map<? extends K, ? extends V> map) {
 		return ImmutableMap.copyOf(map);
 	}
-	
+
+	/**
+	 * Returns an immutable copy of the specified sorted {@code map}.
+	 * 
+	 * @param map
+	 *            the sorted map for which an immutable copy should be created. May not be <code>null</code>.
+	 * @return an immutable copy of the specified sorted map.
+	 */
 	public static <K, V> SortedMap<K, V> immutableCopy(SortedMap<K, ? extends V> map) {
 		return ImmutableSortedMap.copyOfSorted(map);
 	}
-	
+
+	/**
+	 * Adds all of the specified elements to the specified collection.
+	 * 
+	 * @param collection
+	 *            the collection into which the {@code elements} are to be inserted. May not be <code>null</code>.
+	 * @param elements
+	 *            the elements to insert into the {@code collection}. May not be <code>null</code> but may contain
+	 *            <code>null</code> entries if the {@code collection} allows that.
+	 * @return <code>true</code> if the collection changed as a result of the call
+	 */
 	public static <T> boolean addAll(Collection<? super T> collection, T... elements) {
 		return collection.addAll(Arrays.asList(elements));
 	}
-	
+
 }
