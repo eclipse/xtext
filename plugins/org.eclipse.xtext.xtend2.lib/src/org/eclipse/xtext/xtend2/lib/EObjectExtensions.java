037adf5432964df7795335e21707648a58aa20f5
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2011-04-28 23:07:18 +0200

    [xtend][lib] Added JavaDoc, reformatted the class StringConcatenation

--- plugins/org.eclipse.xtext.xtend2.lib/src/org/eclipse/xtext/xtend2/lib/EObjectExtensions.java
+++ plugins/org.eclipse.xtext.xtend2.lib/src/org/eclipse/xtext/xtend2/lib/EObjectExtensions.java
@@ -12,11 +12,23 @@ import java.util.Iterator;
 import org.eclipse.emf.ecore.EObject;
 
 /**
+ * This is an extension library for {@link EObject EMF objects}.
+ * 
  * @author Jan Koehnlein - Initial contribution and API
  */
 public class EObjectExtensions {
 
+	/**
+	 * Returns all the contents of {@code eObject} as an {@link Iterable}. That is, the {@code eObject} will not be
+	 * provided by the results {@link Iterable#iterator() iterator}.
+	 * 
+	 * @param eObject
+	 *            the object whose contents should be traversed. May not be <code>null</code>.
+	 * @return an iterable that yields all the contents of the {@code eObject}. Never <code>null</code>.
+	 */
 	public static Iterable<EObject> allContentsIterable(final EObject eObject) {
+		if (eObject == null)
+			throw new NullPointerException("eObject");
 		return new Iterable<EObject>() {
 			public Iterator<EObject> iterator() {
 				return eObject.eAllContents();
