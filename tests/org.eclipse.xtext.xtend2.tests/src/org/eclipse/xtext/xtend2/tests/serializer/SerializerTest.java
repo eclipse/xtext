a417ee823007efc5ef1bc241aeaff535f8ffdc8e
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2011-05-04 10:23:33 +0200

    [xtend] eXtended the serializer tests

--- tests/org.eclipse.xtext.xtend2.tests/src/org/eclipse/xtext/xtend2/tests/serializer/SerializerTest.java
+++ tests/org.eclipse.xtext.xtend2.tests/src/org/eclipse/xtext/xtend2/tests/serializer/SerializerTest.java
@@ -9,7 +9,6 @@ package org.eclipse.xtext.xtend2.tests.serializer;
 
 import org.eclipse.xtext.serializer.ISerializer;
 import org.eclipse.xtext.serializer.impl.Serializer;
-import org.eclipse.xtext.xtend2.tests.AbstractXtend2TestCase;
 
 import com.google.inject.Inject;
 
@@ -17,7 +16,7 @@ import com.google.inject.Inject;
  * @author Moritz Eysholdt - Initial contribution and API
  */
 @SuppressWarnings("restriction")
-public class SerializerTest extends AbstractXtend2TestCase {
+public class SerializerTest extends AbstractXtend2TestData {
 
 	@Inject
 	private ISerializer serializer;
@@ -26,28 +25,8 @@ public class SerializerTest extends AbstractXtend2TestCase {
 		assertTrue(serializer instanceof Serializer);
 	}
 
-	public void testSimple() throws Exception {
-		StringBuilder expected = new StringBuilder();
-		expected.append("package org.eclipse.xtext.xtend2.tests.compiler\n");
-		expected.append("\n");
-		expected.append("class TestClass1 {\n");
-		expected.append("\n");
-		expected.append("	testFunction1() {\n");
-		expected.append("		42\n");
-		expected.append("	}\n");
-		expected.append("	\n");
-		expected.append("	testFunction2() {\n");
-		expected.append("		return 42\n");
-		expected.append("	}\n");
-		expected.append("		\n");
-		expected.append("	Integer testFunction3() {\n");
-		expected.append("		40 + 2\n");
-		expected.append("	}\n");
-		expected.append("	\n");
-		expected.append("	Integer testFunction4() {\n");
-		expected.append("		return 6 * 7\n");
-		expected.append("	}	\n");
-		expected.append("}");
-		assertEquals(expected.toString(), serializer.serialize(file(expected.toString())));
+	@Override
+	protected void doTest(String fileContents) throws Exception {
+		assertEquals(fileContents, serializer.serialize(file(fileContents)));
 	}
 }
