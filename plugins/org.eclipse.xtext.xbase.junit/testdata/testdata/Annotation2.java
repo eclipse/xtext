467824408cef5da7a9b94ec8fd75d62ecc8bd99d
Sven Efftinge <sven.efftinge@itemis.de>
2011-05-09 11:32:50 +0200

    [Xbase / Xtend] added validation for annotations

---
+++ plugins/org.eclipse.xtext.xbase.junit/testdata/testdata/Annotation2.java
@@ -0,0 +1,12 @@
+package testdata;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Annotation2 {
+	String[] value();
+}
