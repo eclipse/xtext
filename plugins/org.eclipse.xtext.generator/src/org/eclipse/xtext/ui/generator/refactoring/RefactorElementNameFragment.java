9c52e66cf7a88f6b309a8b7f598fbe859505edb9
Jan Koehnlein <jan.koehnlein@itemis.de>
2011-05-18 08:53:14 +0200

    [refactoring] Extracted JDT refactoring participation into generator fragment

--- plugins/org.eclipse.xtext.generator/src/org/eclipse/xtext/ui/generator/refactoring/RefactorElementNameFragment.java
+++ plugins/org.eclipse.xtext.generator/src/org/eclipse/xtext/ui/generator/refactoring/RefactorElementNameFragment.java
@@ -7,10 +7,48 @@
  *******************************************************************************/
 package org.eclipse.xtext.ui.generator.refactoring;
 
+import static java.util.Collections.*;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.xtext.Grammar;
 import org.eclipse.xtext.generator.AbstractGeneratorFragment;
+import org.eclipse.xtext.generator.BindFactory;
+import org.eclipse.xtext.generator.Binding;
 
 /**
  * @author Jan Koehnlein - Initial contribution and API
  */
 public class RefactorElementNameFragment extends AbstractGeneratorFragment {
+
+	private boolean useJdtRefactoring = false;
+
+	public void setUseJdtRefactoring(boolean useInferredJvmModel) {
+		this.useJdtRefactoring = useInferredJvmModel;
+	}
+
+	@Override
+	public Set<Binding> getGuiceBindingsUi(Grammar grammar) {
+		if (!useJdtRefactoring)
+			return emptySet();
+		return new BindFactory()
+				.addTypeToType("org.eclipse.xtext.ui.refactoring.ui.RenameElementHandler",
+						"org.eclipse.xtext.xbase.ui.jvmmodel.refactoring.jdt.JvmRenameElementHandler")
+				.addTypeToType("org.eclipse.xtext.ui.refactoring.IRenameRefactoringProvider",
+						"org.eclipse.xtext.xbase.ui.jvmmodel.refactoring.jdt.JvmRenameRefactoringProvider")
+				.addTypeToType("org.eclipse.xtext.ui.refactoring.IRenameProcessorAdapter$Factory",
+						"org.eclipse.xtext.xbase.ui.jvmmodel.refactoring.jdt.JavaRenameProcessorAdapter.Factory")
+				.addTypeToType("org.eclipse.xtext.ui.refactoring.ui.IRenameSupport.Factory",
+						"org.eclipse.xtext.xbase.ui.jvmmodel.refactoring.jdt.JdtAwareRenameSupportFactory")
+				.addTypeToType("org.eclipse.xtext.ui.refactoring.impl.RefactoringResourceSetProvider",
+						"org.eclipse.xtext.xbase.ui.jvmmodel.refactoring.JvmModelRefactoringResourceSetProvider")
+				.getBindings();
+	}
+	
+	@Override
+	protected List<Object> getParameters(Grammar grammar) {
+		return Collections.<Object>singletonList(useJdtRefactoring);
+	}
 }
