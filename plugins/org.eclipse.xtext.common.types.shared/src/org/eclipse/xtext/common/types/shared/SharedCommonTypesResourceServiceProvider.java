a5e02fa3b3fc7d1fdfd803b43aee4329d013ef0c
Sven Efftinge <sven.efftinge@itemis.de>
2011-06-03 16:05:39 +0200

    made SharedCommonTypesResourceServiceProvider always return null when asked for an IResourceDescriptionManager

--- plugins/org.eclipse.xtext.common.types.shared/src/org/eclipse/xtext/common/types/shared/SharedCommonTypesResourceServiceProvider.java
+++ plugins/org.eclipse.xtext.common.types.shared/src/org/eclipse/xtext/common/types/shared/SharedCommonTypesResourceServiceProvider.java
@@ -1,9 +1,15 @@
 package org.eclipse.xtext.common.types.shared;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.xtext.resource.IResourceDescription.Manager;
 import org.eclipse.xtext.resource.impl.DefaultResourceServiceProvider;
 
 public class SharedCommonTypesResourceServiceProvider extends DefaultResourceServiceProvider {
+	
+	@Override
+	public Manager getResourceDescriptionManager() {
+		return null;
+	}
 	
 	@Override
 	public boolean canHandle(URI uri) {
