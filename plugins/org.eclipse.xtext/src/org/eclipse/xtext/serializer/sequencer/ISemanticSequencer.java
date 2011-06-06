723487f6a95c63501399a9bcc2eab8543c25416d
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2011-06-06 06:35:51 -0700

    [serializer] removed invalid comments

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/serializer/sequencer/ISemanticSequencer.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/serializer/sequencer/ISemanticSequencer.java
@@ -14,41 +14,5 @@ public interface ISemanticSequencer {
 	void init(ISemanticSequencer sequencer, ISemanticSequenceAcceptor sequenceAcceptor,
 			ISerializationDiagnostic.Acceptor errorAcceptor);
 
-	/**
-	 * An implementation of this interface converts all direct children of a semantic object into a sequence of
-	 * (grammarElement, Value)-pairs. Direct children are all values from the EObject's EStructuralFeatures that are not
-	 * transient. The sequence of pairs must comply with the order of grammar element as it is defined by the grammar.
-	 * The sequence of pairs must be complete in the sense that all rule calls (assigned and unassigned) and all
-	 * assigned keywords must appear. The appearance of unassigned keywords is optional.
-	 * 
-	 * @see ITransientValueService: Is called by the ISequencer to determine values that must not (transient) or should
-	 *      not (optional) appear in the output sequence.
-	 * 
-	 * @param context
-	 *            can be an ParserRule or an assigned Action
-	 * @param semanticObject
-	 *            the EObject from the semantic model that is supposed to be sequenced
-	 * @return The following pairs are allowed:
-	 * 
-	 *         (ParserRuleCall, EObject) for assigned parser rule calls. Unassigned parser rule calls are inlined. The
-	 *         ParserRuleCall does not need to be the RuleCall that is contained by the assignment. There area also rule
-	 *         calls allowed that this rule call directly delegates to.
-	 * 
-	 *         (AssignedAction, EObject) for EObjects that have been created by AssignedActions.
-	 * 
-	 *         (TerminalRuleCall, Value) for assigned and unassigned terminal rule calls.
-	 * 
-	 *         (DatatypeRuleCall, Value)for assigned and unassigned value rule calls.
-	 * 
-	 *         (EnumRuleCall, EnumLiteral) for for assigned and unassigned enum rule calls.
-	 * 
-	 *         (GrammarKeyword, KeywordString) for assigned keywords. Optionally, this is also allowed for unassigned
-	 *         keywords.
-	 * 
-	 *         (TerminalRuleCall, ReferencedEObject) for assigned cross references. Unassigned cross references are not
-	 *         allowed by Xtext at all.
-	 * 
-	 *         (DatatypeRuleCall, ReferencedEObject) for assigned cross references.
-	 */
 	void createSequence(EObject context, EObject semanticObject);
 }
--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/serializer/sequencer/ISyntacticSequencer.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/serializer/sequencer/ISyntacticSequencer.java
@@ -7,14 +7,6 @@ import org.eclipse.xtext.serializer.diagnostic.ISerializationDiagnostic;
 import com.google.inject.ImplementedBy;
 
 /**
- * The ISequenceParser (1) recursively traverses the semanticRoot, (2) creates a token sequence for each EObject using
- * the ISequencer, (3) uses a state machine to determine the missing grammar elements (such as keywords, unassigned
- * actions and unassigned rule calls), (4) and then feeds all grammar elements and values to the IParseEventsAcceptor.
- * 
- * @see ISequencer is used to create the intial sequence of (grammarElement, Value)-pairs for each EObject.
- * 
- * @see IParseEventsAcceptor is used to capture the output of this algorithm.
- * 
  * @author Moritz Eysholdt - Initial contribution and API
  */
 @ImplementedBy(GenericSyntacticSequencer.class)
@@ -24,33 +16,6 @@ public interface ISyntacticSequencer {
 		void setSyntacticSequencer(ISyntacticSequencer sequencer);
 	}
 
-//	public class PassThroughSyntacticSequencer implements ISyntacticSequencer, ISemanticSequencerOwner {
-//
-//		protected ISemanticSequencer sequencer;
-//
-//		@Inject
-//		public void setSemanticSequencer(ISemanticSequencer sequencer) {
-//			this.sequencer = sequencer;
-//		}
-//
-//		public void createSequence(EObject context, EObject semanticObject, INode previousNode,
-//				ISyntacticSequenceAcceptor sequenceAcceptor, Acceptor errorAcceptor) {
-//			sequencer.init(sequenceAcceptor, errorAcceptor);
-//			sequencer.createSequence(context, semanticObject);
-//		}
-//	}
-
-	/**
-	 * Serializes the semanticRoot.
-	 * 
-	 * @param rootRule
-	 *            The ParserRule that should be used to serialize semanitcRoot.
-	 * @param semanticRoot
-	 *            The EObject that should be serialized.
-	 * @param constructor
-	 *            A handler that accepts a sequence of parse-events and does something useful with it. For example: Mix
-	 *            in white-spaces and comments and compose all the tokens to a document.
-	 */
 	void init(EObject context, EObject semanticObject, ISyntacticSequenceAcceptor sequenceAcceptor,
 			ISerializationDiagnostic.Acceptor errorAcceptor);
 }
