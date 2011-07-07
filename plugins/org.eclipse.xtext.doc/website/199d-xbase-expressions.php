4583535ee9934332e35bceae0acd89b1843d95a0
Sven Efftinge <sven.efftinge@itemis.de>
2011-07-07 14:09:59 +0200

    integrated xtend2 and xbase doc into xtext.doc

--- plugins/org.eclipse.xtext.doc/website/199d-xbase-expressions.php
+++ plugins/org.eclipse.xtext.doc/website/199d-xbase-expressions.php
@@ -1,6 +1,6 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	include("include.php");
-	$pageTitle 		= "Leveraging Xbase Expressions In Your DSL";
+	$pageTitle 		= "Using Xbase Expressions";
 	
 	$html = file_get_contents('199d-xbase-expressions.html');
 
