278b840636635db5edd731897d7020bb980797f7
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2011-07-12 23:26:14 +0200

    [xtext][builder] Cosmetics: Removed unused import

--- plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/clustering/CopiedResourceDescription.java
+++ plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/clustering/CopiedResourceDescription.java
@@ -7,7 +7,6 @@
  *******************************************************************************/
 package org.eclipse.xtext.builder.clustering;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
