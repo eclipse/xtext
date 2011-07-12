094fcdde5810c5690dda41f338374877ba0a587a
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2011-07-12 18:11:17 +0200

    [serializer] work on the context finder

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/serializer/sequencer/ContextFinder.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/serializer/sequencer/ContextFinder.java
@@ -7,26 +7,34 @@
  *******************************************************************************/
 package org.eclipse.xtext.serializer.sequencer;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.xtext.IGrammarAccess;
 import org.eclipse.xtext.serializer.analysis.IGrammarConstraintProvider;
 import org.eclipse.xtext.serializer.analysis.IGrammarConstraintProvider.IConstraint;
 import org.eclipse.xtext.serializer.analysis.IGrammarConstraintProvider.IConstraintContext;
+import org.eclipse.xtext.serializer.analysis.IGrammarConstraintProvider.IFeatureInfo;
 import org.eclipse.xtext.util.Pair;
 import org.eclipse.xtext.util.Tuples;
 
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.google.inject.Inject;
+import com.google.inject.Singleton;
 
 /**
  * @author Moritz Eysholdt - Initial contribution and API
  */
+@Singleton
 public class ContextFinder implements IContextFinder {
 
 	protected List<IConstraintContext> constraintContexts;
@@ -39,8 +47,74 @@ public class ContextFinder implements IContextFinder {
 	@Inject
 	protected IGrammarConstraintProvider grammarConstraintProvider;
 
-	protected Iterable<EObject> findContexts(EObject semanitcObject) {
-		EClass cls = semanitcObject.eClass();
+	@Inject
+	protected ITransientValueService transientValues;
+
+	protected boolean allChildrenHaveContexts(EObject context, EObject semanicObj) {
+		IConstraint constraint = constraints.get(Tuples.create(context, semanicObj.eClass()));
+		if (constraint == null)
+			return false;
+		for (EReference ref : semanicObj.eClass().getEAllContainments())
+			if (!isTransient(semanicObj, ref)) {
+				IFeatureInfo fi = constraint.getFeatures()[semanicObj.eClass().getFeatureID(ref)];
+				List<EObject> calledContexts = fi.getCalledContexts();
+				if (ref.isMany()) {
+					for (EObject child : getAllNonTransientValues(semanicObj, ref))
+						if (Iterables.isEmpty(findContextsByContents(child, calledContexts)))
+							return false;
+				} else {
+					EObject child = (EObject) semanicObj.eGet(ref);
+					if (Iterables.isEmpty(findContextsByContents(child, calledContexts)))
+						return false;
+				}
+			}
+		return true;
+	}
+
+	protected boolean allFeaturesHaveAssignments(EObject context, EObject semanicObj) {
+		IConstraint constraint = constraints.get(Tuples.create(context, semanicObj.eClass()));
+		if (constraint == null)
+			return false;
+		for (int featureID = 0; featureID < semanicObj.eClass().getFeatureCount(); featureID++)
+			if (!isTransient(semanicObj, semanicObj.eClass().getEStructuralFeature(featureID))
+					&& constraint.getFeatures()[featureID] == null)
+				return false;
+		return true;
+	}
+
+	public Iterable<EObject> findContextsByContents(EObject semanitcObject, Iterable<EObject> contextCandidates) {
+		initConstraints();
+		if (contextCandidates != null) {
+			List<EObject> cands = Lists.newArrayList();
+			for (EObject ctx : contextCandidates)
+				if (constraints.get(Tuples.create(ctx, semanitcObject.eClass())) != null)
+					cands.add(ctx);
+			contextCandidates = cands;
+		} else
+			contextCandidates = getAllContextsForType(semanitcObject.eClass());
+		//		if (Iterables.size(contextCandidates) < 2)
+		//			return contextCandidates;
+		List<EObject> filtered1 = Lists.newArrayList();
+		for (EObject cand : contextCandidates)
+			if (allFeaturesHaveAssignments(cand, semanitcObject))
+				filtered1.add(cand);
+		//		if (filtered1.size() < 2)
+		//			return filtered1;
+		List<EObject> filtered2 = Lists.newArrayList();
+		for (EObject cand : filtered1)
+			if (allChildrenHaveContexts(cand, semanitcObject))
+				filtered2.add(cand);
+		//		if (filtered2.size() < 2)
+		//			return filtered2;
+		return filtered2;
+	}
+
+	public Iterable<EObject> findContextsByContentsAndContainer(EObject semanitcObject,
+			Iterable<EObject> contextCandidates) {
+		return findContextsByContents(semanitcObject, contextCandidates);
+	}
+
+	protected Set<EObject> getAllContextsForType(EClass cls) {
 		Set<EObject> result = Sets.newHashSet();
 		for (IConstraintContext cc : constraintContexts)
 			for (IConstraint c : cc.getConstraints())
@@ -51,26 +125,22 @@ public class ContextFinder implements IContextFinder {
 		return result;
 	}
 
-	public Iterable<EObject> findContextsByContents(EObject semanitcObject, Iterable<EObject> contextCandidates) {
-		initConstraints();
-		// TODO: actually validate if the constraints match
-		if (contextCandidates == null)
-			return findContexts(semanitcObject);
-		EClass cls = semanitcObject.eClass();
-		Set<EObject> result = Sets.newHashSet();
-		for (EObject candidate : contextCandidates)
-			if (getConstraint(candidate, cls) != null)
-				result.add(candidate);
-		return result;
-	}
-
-	public Iterable<EObject> findContextsByContentsAndContainer(EObject semanitcObject,
-			Iterable<EObject> contextCandidates) {
-		return findContextsByContents(semanitcObject, contextCandidates);
-	}
-
-	protected IConstraint getConstraint(EObject context, EClass type) {
-		return constraints.get(Tuples.create(context, type));
+	@SuppressWarnings("unchecked")
+	protected List<EObject> getAllNonTransientValues(EObject container, EReference ref) {
+		switch (transientValues.isListTransient(container, ref)) {
+			case SOME:
+				List<EObject> result = Lists.newArrayList();
+				List<?> values = (List<?>) container.eGet(ref);
+				for (int i = 0; i < values.size(); i++)
+					if (!transientValues.isValueInListTransient(container, i, ref))
+						result.add((EObject) values.get(i));
+				return result;
+			case NO:
+				return (List<EObject>) container.eGet(ref);
+			case YES:
+				return Collections.emptyList();
+		}
+		return Collections.emptyList();
 	}
 
 	protected void initConstraints() {
@@ -82,6 +152,33 @@ public class ContextFinder implements IContextFinder {
 				for (IConstraint constraint : ctx.getConstraints())
 					constraints.put(Tuples.create(ctx.getContext(), constraint.getType()), constraint);
 		}
+	}
+
+	protected boolean isTransient(EObject obj, EStructuralFeature feature) {
+		if (feature.isMany())
+			switch (transientValues.isListTransient(obj, feature)) {
+				case NO:
+					return false;
+				case YES:
+					return true;
+				case SOME:
+					List<?> values = (List<?>) obj.eGet(feature);
+					for (int i = 0; i < values.size(); i++)
+						if (!transientValues.isValueInListTransient(obj, i, feature))
+							return false;
+					return true;
+			}
+		else
+			switch (transientValues.isValueTransient(obj, feature)) {
+				case NO:
+					return false;
+				case YES:
+					return true;
+				case PREFERABLY:
+					return true;
+
+			}
+		return true;
 	}
 
 }
