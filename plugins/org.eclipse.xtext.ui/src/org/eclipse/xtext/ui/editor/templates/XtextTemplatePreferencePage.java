10f7c9b7540e3e4e85d4b146efff5553f4b752fe
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2011-07-17 21:30:38 +0200

    [xtext][preferences] Fix: Template proposals: Column 'name' is not wide enough
    see https://bugs.eclipse.org/bugs/show_bug.cgi?id=338044

--- plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/templates/XtextTemplatePreferencePage.java
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/templates/XtextTemplatePreferencePage.java
@@ -10,6 +10,8 @@ package org.eclipse.xtext.ui.editor.templates;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.text.templates.ContextTypeRegistry;
 import org.eclipse.jface.text.templates.persistence.TemplateStore;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.texteditor.templates.TemplatePreferencePage;
 
 import com.google.inject.Inject;
@@ -30,4 +32,20 @@ public class XtextTemplatePreferencePage extends TemplatePreferencePage {
 	protected boolean isShowFormatterSetting() {
 		return false;
 	}
+	
+	@Override
+	protected Control createContents(Composite ancestor) {
+		Control result = super.createContents(ancestor);
+		ancestor.layout();
+		return result;
+	}
+	
+	@Override
+	public void createControl(Composite parent) {
+		super.createControl(parent);
+		// set the with of the leftmost column ('name')
+		getTableViewer().getTable().getColumns()[0].pack();
+	}
+	
+	
 }
