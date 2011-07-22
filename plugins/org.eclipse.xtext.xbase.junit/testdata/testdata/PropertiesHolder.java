dd9ccbba9454b0e59a6b1e633192870cce8a842c
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2011-07-22 11:45:18 +0200

    [xbase][linking] Fix: Unresolvable feature in closure context
    see https://bugs.eclipse.org/bugs/show_bug.cgi?id=352705

---
+++ plugins/org.eclipse.xtext.xbase.junit/testdata/testdata/PropertiesHolder.java
@@ -0,0 +1,12 @@
+package testdata;
+
+import java.util.Collections;
+import java.util.List;
+
+public class PropertiesHolder {
+
+	public List<Properties1> getProperties() {
+		return Collections.emptyList();
+	}
+	
+}
