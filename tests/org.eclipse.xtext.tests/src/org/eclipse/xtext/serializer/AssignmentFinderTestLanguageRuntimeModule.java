4ec09d4c18c7236cc311790b1c0fddd00d1909f3
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2011-07-25 20:15:52 +0200

    [serializer/AssignmentFinder] support terminal/datatype-rules that return enums

--- tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/serializer/AssignmentFinderTestLanguageRuntimeModule.java
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/serializer/AssignmentFinderTestLanguageRuntimeModule.java
@@ -3,9 +3,43 @@
  */
 package org.eclipse.xtext.serializer;
 
+import org.eclipse.xtext.common.services.DefaultTerminalConverters;
+import org.eclipse.xtext.conversion.IValueConverter;
+import org.eclipse.xtext.conversion.IValueConverterService;
+import org.eclipse.xtext.conversion.ValueConverter;
+import org.eclipse.xtext.conversion.ValueConverterException;
+import org.eclipse.xtext.nodemodel.INode;
+import org.eclipse.xtext.serializer.assignmentFinderTest.TestEnum;
+
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
-public class AssignmentFinderTestLanguageRuntimeModule extends org.eclipse.xtext.serializer.AbstractAssignmentFinderTestLanguageRuntimeModule {
+public class AssignmentFinderTestLanguageRuntimeModule extends AbstractAssignmentFinderTestLanguageRuntimeModule {
 
+	public static class AFValueConverter extends DefaultTerminalConverters {
+		@ValueConverter(rule = "DatEnum")
+		public IValueConverter<TestEnum> DatEnum() {
+			return new IValueConverter<TestEnum>() {
+
+				public TestEnum toValue(String string, INode node) throws ValueConverterException {
+					if ("lit3".equals(string))
+						return TestEnum.LIT3;
+					else
+						throw new ValueConverterException(null, null, null);
+				}
+
+				public String toString(TestEnum value) throws ValueConverterException {
+					if (value == TestEnum.LIT3)
+						return TestEnum.LIT3.getName();
+					else
+						throw new ValueConverterException(null, null, null);
+				}
+			};
+		}
+	}
+
+	@Override
+	public Class<? extends IValueConverterService> bindIValueConverterService() {
+		return AFValueConverter.class;
+	}
 }
