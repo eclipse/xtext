819d3dfffe1e40ce446b4c27ecb8b28847d98f46
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2011-07-29 14:06:39 +0200

    [xbase/serializer] migrated custom implementation of hooks

--- plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/serializer/XbaseSyntacticSequencer.java
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/serializer/XbaseSyntacticSequencer.java
@@ -1,11 +1,14 @@
 package org.eclipse.xtext.xbase.serializer;
 
+import java.util.List;
+
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.Keyword;
 import org.eclipse.xtext.nodemodel.INode;
-import org.eclipse.xtext.serializer.analysis.ISyntacticSequencerPDAProvider.ISynTransition;
+import org.eclipse.xtext.serializer.analysis.ISyntacticSequencerPDAProvider.ISynNavigable;
 import org.eclipse.xtext.xbase.XBlockExpression;
 import org.eclipse.xtext.xbase.XExpression;
+import org.eclipse.xtext.xbase.XForLoopExpression;
 import org.eclipse.xtext.xbase.XIfExpression;
 
 @SuppressWarnings("restriction")
@@ -16,18 +19,19 @@ public class XbaseSyntacticSequencer extends AbstractXbaseSyntacticSequencer {
 	 */
 	@Override
 	protected void emit_XParenthesizedExpression_LeftParenthesisKeyword_0_a(EObject semanticObject,
-			ISynTransition transition, INode fromNode, INode toNode) {
+			ISynNavigable transition, List<INode> nodes) {
 
 		Keyword kw = grammarAccess.getXParenthesizedExpressionAccess().getLeftParenthesisKeyword_0();
 
-		if (fromNode == null) {
+		if (nodes == null) {
 			if (semanticObject instanceof XIfExpression) {
 				EObject cnt = semanticObject.eContainer();
-				if (cnt instanceof XExpression && !(cnt instanceof XBlockExpression))
+				if (cnt instanceof XExpression && !(cnt instanceof XBlockExpression)
+						&& !(cnt instanceof XForLoopExpression))
 					acceptUnassignedKeyword(kw, null);
 			}
 		}
 
-		acceptNodes(transition, fromNode, toNode);
+		acceptNodes(transition, nodes);
 	}
 }
