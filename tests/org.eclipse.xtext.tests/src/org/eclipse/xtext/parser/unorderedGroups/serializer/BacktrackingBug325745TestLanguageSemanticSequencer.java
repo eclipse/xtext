efbca16cb7029b1ea3d2c99c9151be678f810198
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2011-07-31 17:16:45 +0200

    [serializer/tests] added newly generated serializer files

---
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/parser/unorderedGroups/serializer/BacktrackingBug325745TestLanguageSemanticSequencer.java
@@ -0,0 +1,4 @@
+package org.eclipse.xtext.parser.unorderedGroups.serializer;
+
+public class BacktrackingBug325745TestLanguageSemanticSequencer extends AbstractBacktrackingBug325745TestLanguageSemanticSequencer {
+}
