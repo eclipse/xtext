5aea2f3f786bddfd43e413d6b19f07bfd5e59745
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2011-08-27 15:35:05 +0200

    [xtext] Fix: NodeModelUtils#findActualNodeFor(EObject) returns wrong node
    It did not handle cases like 
    (A|B) c=C 
    for C correctly
    
    see https://bugs.eclipse.org/bugs/show_bug.cgi?id=353969

--- tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/resource/LocationInFileProviderTest.java
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/resource/LocationInFileProviderTest.java
@@ -9,10 +9,15 @@ package org.eclipse.xtext.resource;
 
 import java.util.List;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.xtext.junit.AbstractXtextTests;
+import org.eclipse.xtext.resource.locationprovidertest.Bus;
+import org.eclipse.xtext.resource.locationprovidertest.Component;
 import org.eclipse.xtext.resource.locationprovidertest.Element;
 import org.eclipse.xtext.resource.locationprovidertest.LocationprovidertestPackage;
+import org.eclipse.xtext.resource.locationprovidertest.Mode;
 import org.eclipse.xtext.resource.locationprovidertest.Model;
+import org.eclipse.xtext.resource.locationprovidertest.Processor;
 import org.eclipse.xtext.util.ITextRegion;
 
 /**
@@ -29,10 +34,30 @@ public class LocationInFileProviderTest extends AbstractXtextTests {
 	protected void setUp() throws Exception {
 		super.setUp();
 		with(LocationProviderTestLanguageStandaloneSetup.class);
-		modelAsString = "element x singleref y multiref y element y";
+		modelAsString = 
+				"element x singleref y multiref y element y\n" +
+				"processor p1\n" + 
+				"modes\n" + 
+				"mode m1; \n" + 
+				"mode m2; \n" + 
+				"end;\n" +
+				"bus b1\n" +
+				"modes\n" +
+				"mode mb1;\n" +
+				"mode mb2;\n" +
+				"end;";
 		model = (Model) getModel(modelAsString);
 		elements = model.getElements();
 		locationInFileProvider = new DefaultLocationInFileProvider();
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		elements = null;
+		modelAsString = null;
+		model = null;
+		locationInFileProvider = null;
+		super.tearDown();
 	}
 
 	public void testContainmentRefSignificantLocation() throws Exception {
@@ -101,7 +126,7 @@ public class LocationInFileProviderTest extends AbstractXtextTests {
 		assertEquals(modelAsString.indexOf(" element y"), location.getLength());
 		location = locationInFileProvider.getFullTextRegion(model, LocationprovidertestPackage.Literals.MODEL__ELEMENTS, 1);
 		assertEquals(modelAsString.lastIndexOf("element"), location.getOffset());
-		assertEquals(modelAsString.length() - modelAsString.lastIndexOf("element"), location.getLength());		
+		assertEquals("element y".length(), location.getLength());		
 		// multi feature index out of bounds -> return owner's full region 
 		location = locationInFileProvider.getFullTextRegion(model, LocationprovidertestPackage.Literals.MODEL__ELEMENTS, 2);
 		assertEquals(0, location.getOffset());
@@ -151,5 +176,64 @@ public class LocationInFileProviderTest extends AbstractXtextTests {
 		location = locationInFileProvider.getFullTextRegion(elements.get(1), LocationprovidertestPackage.Literals.ELEMENT__NAME, 1);
 		assertEquals(modelAsString.lastIndexOf("y"), location.getOffset());
 		assertEquals("y".length(), location.getLength());		
+	}
+	
+	public void testBug353969_Setup() throws Exception {
+		EList<Component> components = model.getComponents();
+		assertEquals(2, components.size());
+		assertTrue(components.get(0) instanceof Processor);
+		assertTrue(components.get(1) instanceof Bus);
+	}
+	
+	public void testBug353969_01() throws Exception {
+		Processor p = (Processor) model.getComponents().get(0);
+		EList<Mode> modes = p.getMode();
+		assertEquals(2, modes.size());
+		Mode m1 = modes.get(0);
+		ITextRegion m1FullRegion = locationInFileProvider.getFullTextRegion(m1);
+		assertEquals(modelAsString.indexOf("mode m1;"), m1FullRegion.getOffset());
+		assertEquals("mode m1;".length(), m1FullRegion.getLength());
+		ITextRegion m1SignificantRegion = locationInFileProvider.getSignificantTextRegion(m1);
+		assertEquals(modelAsString.indexOf("m1;"), m1SignificantRegion.getOffset());
+		assertEquals("m1".length(), m1SignificantRegion.getLength());
+	}
+	
+	public void testBug353969_02() throws Exception {
+		Processor p = (Processor) model.getComponents().get(0);
+		EList<Mode> modes = p.getMode();
+		assertEquals(2, modes.size());
+		Mode m2 = modes.get(1);
+		ITextRegion m2FullRegion = locationInFileProvider.getFullTextRegion(m2);
+		assertEquals(modelAsString.indexOf("mode m2;"), m2FullRegion.getOffset());
+		assertEquals("mode m2;".length(), m2FullRegion.getLength());
+		ITextRegion m2SignificantRegion = locationInFileProvider.getSignificantTextRegion(m2);
+		assertEquals(modelAsString.indexOf("m2;"), m2SignificantRegion.getOffset());
+		assertEquals("m2".length(), m2SignificantRegion.getLength());
+	}
+	
+	public void testBug353969_03() throws Exception {
+		Bus b = (Bus) model.getComponents().get(1);
+		EList<Mode> modes = b.getMode();
+		assertEquals(2, modes.size());
+		Mode mb1 = modes.get(0);
+		ITextRegion mb1FullRegion = locationInFileProvider.getFullTextRegion(mb1);
+		assertEquals(modelAsString.indexOf("mode mb1;"), mb1FullRegion.getOffset());
+		assertEquals("mode mb1;".length(), mb1FullRegion.getLength());
+		ITextRegion mb1SignificantRegion = locationInFileProvider.getSignificantTextRegion(mb1);
+		assertEquals(modelAsString.indexOf("mb1;"), mb1SignificantRegion.getOffset());
+		assertEquals("mb1".length(), mb1SignificantRegion.getLength());
+	}
+	
+	public void testBug353969_04() throws Exception {
+		Bus b = (Bus) model.getComponents().get(1);
+		EList<Mode> modes = b.getMode();
+		assertEquals(2, modes.size());
+		Mode mb2 = modes.get(1);
+		ITextRegion mb2FullRegion = locationInFileProvider.getFullTextRegion(mb2);
+		assertEquals(modelAsString.indexOf("mode mb2;"), mb2FullRegion.getOffset());
+		assertEquals("mode mb2;".length(), mb2FullRegion.getLength());
+		ITextRegion mb2SignificantRegion = locationInFileProvider.getSignificantTextRegion(mb2);
+		assertEquals(modelAsString.indexOf("mb2;"), mb2SignificantRegion.getOffset());
+		assertEquals("mb2".length(), mb2SignificantRegion.getLength());
 	}
 }
