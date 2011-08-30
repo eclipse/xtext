8a410e31ff68da6f7b305bcfbb19250aebf2a669
Michael Clay <clay@codeworkz.at>
2011-08-30 18:58:20 +0200

    Bug 351963 - Support customized markers

--- plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/builderState/MarkerUpdaterImpl.java
+++ plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/builderState/MarkerUpdaterImpl.java
@@ -7,48 +7,34 @@
  *******************************************************************************/
 package org.eclipse.xtext.builder.builderState;
 
-import java.util.List;
-
-import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IStorage;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.SubMonitor;
-import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.xtext.resource.IResourceServiceProvider;
 import org.eclipse.xtext.resource.IResourceDescription.Delta;
-import org.eclipse.xtext.ui.MarkerTypes;
-import org.eclipse.xtext.ui.editor.validation.MarkerCreator;
+import org.eclipse.xtext.resource.IResourceServiceProvider;
 import org.eclipse.xtext.ui.resource.IStorage2UriMapper;
-import org.eclipse.xtext.util.CancelIndicator;
+import org.eclipse.xtext.ui.validation.IResourceUIValidatorExtension;
 import org.eclipse.xtext.util.Pair;
 import org.eclipse.xtext.validation.CheckMode;
-import org.eclipse.xtext.validation.IResourceValidator;
-import org.eclipse.xtext.validation.Issue;
 
 import com.google.common.collect.ImmutableList;
 import com.google.inject.Inject;
 
 /**
  * @author Sven Efftinge - Initial contribution and API
+ * @author Michael Clay
  */
 public class MarkerUpdaterImpl implements IMarkerUpdater {
-
 	@Inject
 	private IResourceServiceProvider.Registry resourceServiceProviderRegistry;
 
 	@Inject
-	private MarkerCreator markerCreator;
-
-	@Inject
 	private IStorage2UriMapper mapper;
-
-	private final static Logger log = Logger.getLogger(MarkerUpdaterImpl.class);
 
 	public void updateMarker(ResourceSet resourceSet, ImmutableList<Delta> resourceDescriptionDeltas,
 			IProgressMonitor monitor) {
@@ -58,18 +44,17 @@ public class MarkerUpdaterImpl implements IMarkerUpdater {
 		for (Delta delta : resourceDescriptionDeltas) {
 			if (subMonitor.isCanceled())
 				throw new OperationCanceledException();
+			SubMonitor child = subMonitor.newChild(1);
 			if (delta.getNew() != null) {
-				Iterable<Pair<IStorage, IProject>> storages = mapper.getStorages(delta.getNew().getURI());
-				SubMonitor child = subMonitor.newChild(1);
+				URI uri = delta.getNew().getURI();
+				Iterable<Pair<IStorage, IProject>> storages = mapper.getStorages(uri);
 				child.setWorkRemaining(3);
 				for (Pair<IStorage, IProject> pair : storages) {
 					child.setWorkRemaining(3);
 					if (pair.getFirst() instanceof IFile) {
-						IFile file = (IFile) pair.getFirst();
-						if (file.isAccessible() && pair.getSecond().isAccessible() && !pair.getSecond().isHidden()) {
-							Resource resource = resourceSet.getResource(delta.getNew().getURI(), true);
-							addMarkers(file, resource, child.newChild(2));
-						}
+						IResourceUIValidatorExtension resourceUIValidatorExtension = getResourceUIValidatorExtension(uri);
+						resourceUIValidatorExtension.updateValidationMarkers((IFile) pair.getFirst(),
+								resourceSet.getResource(uri, true), CheckMode.NORMAL_AND_FAST, child.newChild(2));
 					} else {
 						child.worked(1);
 					}
@@ -78,15 +63,10 @@ public class MarkerUpdaterImpl implements IMarkerUpdater {
 				Iterable<Pair<IStorage, IProject>> storages = mapper.getStorages(delta.getOld().getURI());
 				for (Pair<IStorage, IProject> pair : storages) {
 					if (pair.getFirst() instanceof IFile) {
-						IFile file = (IFile) pair.getFirst();
-						if (file.isAccessible() && pair.getSecond().isAccessible() && !pair.getSecond().isHidden()) {
-							try {
-								file.deleteMarkers(MarkerTypes.FAST_VALIDATION, true, IResource.DEPTH_ZERO);
-								file.deleteMarkers(MarkerTypes.NORMAL_VALIDATION, true, IResource.DEPTH_ZERO);
-							} catch (CoreException ex) {
-								log.error(ex.getMessage(), ex);
-							}
-						}
+						IResourceUIValidatorExtension resourceUIValidatorExtension = getResourceUIValidatorExtension(delta
+								.getOld().getURI());
+						resourceUIValidatorExtension.deleteValidationMarkers((IFile) pair.getFirst(),
+								CheckMode.NORMAL_AND_FAST, child);
 					}
 				}
 				subMonitor.worked(1);
@@ -94,35 +74,10 @@ public class MarkerUpdaterImpl implements IMarkerUpdater {
 		}
 	}
 
-	protected void addMarkers(IFile file, Resource resource, final IProgressMonitor monitor) {
-		SubMonitor subMonitor = SubMonitor.convert(monitor, 1);
-		try {
-			IResourceServiceProvider provider = 
-				resourceServiceProviderRegistry.getResourceServiceProvider(resource.getURI());
-			if (provider == null)
-				return;
-
-			IResourceValidator resourceValidator = provider.getResourceValidator();
-			List<Issue> list = resourceValidator.validate(resource, CheckMode.NORMAL_AND_FAST, getCancelIndicator(subMonitor));
-			if (subMonitor.isCanceled())
-				throw new OperationCanceledException();
-			subMonitor.worked(1);
-			file.deleteMarkers(MarkerTypes.FAST_VALIDATION, true, IResource.DEPTH_ZERO);
-			file.deleteMarkers(MarkerTypes.NORMAL_VALIDATION, true, IResource.DEPTH_ZERO);
-			for (Issue issue : list) {
-				markerCreator.createMarker(issue, file, MarkerTypes.forCheckType(issue.getType()));
-			}
-		} catch (CoreException e) {
-			log.error(e.getMessage(), e);
-		}
-	}
-
-	private CancelIndicator getCancelIndicator(final IProgressMonitor monitor) {
-		return new CancelIndicator() {
-			public boolean isCanceled() {
-				return monitor.isCanceled();
-			}
-		};
+	protected IResourceUIValidatorExtension getResourceUIValidatorExtension(URI uri) {
+		IResourceServiceProvider provider = resourceServiceProviderRegistry.getResourceServiceProvider(uri);
+		IResourceUIValidatorExtension resourceUIValidatorExtension = provider.get(IResourceUIValidatorExtension.class);
+		return resourceUIValidatorExtension;
 	}
 
 }
---
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/validation/IResourceUIValidatorExtension.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2011 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.ui.validation;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.validation.CheckMode;
+
+import com.google.inject.ImplementedBy;
+
+/**
+ * @author Michael Clay - Initial contribution and API
+ * @since 2.1
+ */
+@ImplementedBy(DefaultResourceUIValidatorExtension.class)
+public interface IResourceUIValidatorExtension {
+
+	/**
+	 * Update the validation markers of the given file
+	 * 
+	 * @param file
+	 *            the file to create validation markers for
+	 * @param resource
+	 *            the resource corresponding to the given file
+	 * @param mode
+	 *            the mode (normal,fast,expensive) of the validation
+	 */
+	void updateValidationMarkers(IFile file, Resource resource, CheckMode mode, IProgressMonitor monitor);
+
+	/**
+	 * Delete all validation markers corresponding to the given checkMode on the passed in file
+	 * 
+	 * @param file
+	 *            the file to create validation markers for
+	 * @param mode
+	 *            the mode (normal,fast,expensive) of the validation
+	 */
+	void deleteValidationMarkers(IFile file, CheckMode checkMode, IProgressMonitor monitor);
+}
