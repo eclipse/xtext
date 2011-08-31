1a49f158432c310098841c450cadb9504c15e760
Sven Efftinge <sven.efftinge@itemis.de>
2011-08-31 09:07:24 +0200

    [pure xbase] fixed generation of pure xbase files

--- plugins/org.eclipse.xtext.purexbase/xtend-gen/org/eclipse/xtext/purexbase/generator/PureXbaseGenerator.java
+++ plugins/org.eclipse.xtext.purexbase/xtend-gen/org/eclipse/xtext/purexbase/generator/PureXbaseGenerator.java
@@ -1,6 +1,7 @@
 package org.eclipse.xtext.purexbase.generator;
 
 import com.google.inject.Inject;
+import java.util.Collection;
 import java.util.List;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
@@ -12,9 +13,12 @@ import org.eclipse.xtext.generator.IFileSystemAccess;
 import org.eclipse.xtext.generator.IGenerator;
 import org.eclipse.xtext.purexbase.pureXbase.Model;
 import org.eclipse.xtext.xbase.XBlockExpression;
+import org.eclipse.xtext.xbase.XExpression;
 import org.eclipse.xtext.xbase.compiler.ImportManager;
 import org.eclipse.xtext.xbase.compiler.StringBuilderBasedAppendable;
 import org.eclipse.xtext.xbase.compiler.XbaseCompiler;
+import org.eclipse.xtext.xbase.controlflow.IEarlyExitComputer;
+import org.eclipse.xtext.xbase.controlflow.IEarlyExitComputer.ExitPoint;
 import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.IntegerExtensions;
 import org.eclipse.xtext.xbase.lib.IterableExtensions;
@@ -30,6 +34,9 @@ public class PureXbaseGenerator implements IGenerator {
   
   @Inject
   private TypeReferences typeReferences;
+  
+  @Inject
+  private IEarlyExitComputer computer;
   
   public void doGenerate(final Resource resource, final IFileSystemAccess fsa) {
     String _name = this.name(resource);
@@ -63,56 +70,126 @@ public class PureXbaseGenerator implements IGenerator {
       XBlockExpression _block = m.getBlock();
       JvmTypeReference _typeForName = this.typeReferences.getTypeForName("void", m);
       this.compiler.compile(_block, appendable, _typeForName);
-      StringConcatenation _builder = new StringConcatenation();
-      List<String> _imports = impMnr.getImports();
-      final Function1<String,String> _function = new Function1<String,String>() {
-          public String apply(final String e) {
-            String _operator_plus = StringExtensions.operator_plus("import ", e);
-            String _operator_plus_1 = StringExtensions.operator_plus(_operator_plus, ";\n");
-            return _operator_plus_1;
-          }
-        };
-      List<String> _map = ListExtensions.<String, String>map(_imports, _function);
-      String _join = IterableExtensions.join(_map);
-      _builder.append(_join, "");
-      _builder.newLineIfNotEmpty();
-      _builder.newLine();
-      _builder.append("@SuppressWarnings(\"all\")");
-      _builder.newLine();
-      _builder.append("public class ");
-      Resource _eResource = m.eResource();
-      String _name = this.name(_eResource);
-      _builder.append(_name, "");
-      _builder.append(" {");
-      _builder.newLineIfNotEmpty();
-      _builder.append("\t");
-      _builder.append("public static void main(String[] args) {");
-      _builder.newLine();
-      _builder.append("\t\t");
-      _builder.append("try {");
-      _builder.newLine();
-      _builder.append("\t\t\t");
-      _builder.append("xbaseExpression();");
-      _builder.newLine();
-      _builder.append("\t\t");
-      _builder.append("} catch (Throwable t) {}");
-      _builder.newLine();
-      _builder.append("\t");
-      _builder.append("}");
-      _builder.newLine();
-      _builder.append("\t");
-      _builder.append("public static Object xbaseExpression() throws Throwable {");
-      _builder.newLine();
-      _builder.append("\t\t");
-      _builder.append(appendable, "		");
-      _builder.newLineIfNotEmpty();
-      _builder.append("\t");
-      _builder.append("}");
-      _builder.newLine();
-      _builder.append("}");
-      _builder.newLine();
-      _xblockexpression = (_builder);
+      StringConcatenation _xifexpression = null;
+      XBlockExpression _block_1 = m.getBlock();
+      boolean _containsReturn = this.containsReturn(_block_1);
+      if (_containsReturn) {
+        StringConcatenation _builder = new StringConcatenation();
+        List<String> _imports = impMnr.getImports();
+        final Function1<String,String> _function = new Function1<String,String>() {
+            public String apply(final String e) {
+              String _operator_plus = StringExtensions.operator_plus("import ", e);
+              String _operator_plus_1 = StringExtensions.operator_plus(_operator_plus, ";\n");
+              return _operator_plus_1;
+            }
+          };
+        List<String> _map = ListExtensions.<String, String>map(_imports, _function);
+        String _join = IterableExtensions.join(_map);
+        _builder.append(_join, "");
+        _builder.newLineIfNotEmpty();
+        _builder.newLine();
+        _builder.append("@SuppressWarnings(\"all\")");
+        _builder.newLine();
+        _builder.append("public class ");
+        Resource _eResource = m.eResource();
+        String _name = this.name(_eResource);
+        _builder.append(_name, "");
+        _builder.append(" {");
+        _builder.newLineIfNotEmpty();
+        _builder.append("\t");
+        _builder.append("public static void main(String[] args) {");
+        _builder.newLine();
+        _builder.append("\t\t");
+        _builder.append("try {");
+        _builder.newLine();
+        _builder.append("\t\t\t");
+        _builder.append("xbaseExpression();");
+        _builder.newLine();
+        _builder.append("\t\t");
+        _builder.append("} catch (Throwable t) {}");
+        _builder.newLine();
+        _builder.append("\t");
+        _builder.append("}");
+        _builder.newLine();
+        _builder.append("\t");
+        _builder.append("public static Object xbaseExpression() throws Throwable {");
+        _builder.newLine();
+        _builder.append("\t\t");
+        _builder.append("if (Boolean.TRUE) {");
+        _builder.newLine();
+        _builder.append("\t\t\t");
+        _builder.append(appendable, "			");
+        _builder.newLineIfNotEmpty();
+        _builder.append("\t\t");
+        _builder.append("}");
+        _builder.newLine();
+        _builder.append("\t\t");
+        _builder.append("return null;");
+        _builder.newLine();
+        _builder.append("\t");
+        _builder.append("}");
+        _builder.newLine();
+        _builder.append("}");
+        _builder.newLine();
+        _xifexpression = _builder;
+      } else {
+        StringConcatenation _builder_1 = new StringConcatenation();
+        List<String> _imports_1 = impMnr.getImports();
+        final Function1<String,String> _function_1 = new Function1<String,String>() {
+            public String apply(final String e_1) {
+              String _operator_plus_2 = StringExtensions.operator_plus("import ", e_1);
+              String _operator_plus_3 = StringExtensions.operator_plus(_operator_plus_2, ";\n");
+              return _operator_plus_3;
+            }
+          };
+        List<String> _map_1 = ListExtensions.<String, String>map(_imports_1, _function_1);
+        String _join_1 = IterableExtensions.join(_map_1);
+        _builder_1.append(_join_1, "");
+        _builder_1.newLineIfNotEmpty();
+        _builder_1.newLine();
+        _builder_1.append("@SuppressWarnings(\"all\")");
+        _builder_1.newLine();
+        _builder_1.append("public class ");
+        Resource _eResource_1 = m.eResource();
+        String _name_1 = this.name(_eResource_1);
+        _builder_1.append(_name_1, "");
+        _builder_1.append(" {");
+        _builder_1.newLineIfNotEmpty();
+        _builder_1.append("\t");
+        _builder_1.append("public static void main(String[] args) {");
+        _builder_1.newLine();
+        _builder_1.append("\t\t");
+        _builder_1.append("try {");
+        _builder_1.newLine();
+        _builder_1.append("\t\t\t");
+        _builder_1.append(appendable, "			");
+        _builder_1.newLineIfNotEmpty();
+        _builder_1.append("\t\t");
+        _builder_1.append("} catch (Throwable t) {}");
+        _builder_1.newLine();
+        _builder_1.append("\t");
+        _builder_1.append("}");
+        _builder_1.newLine();
+        _builder_1.append("}");
+        _builder_1.newLine();
+        _xifexpression = _builder_1;
+      }
+      _xblockexpression = (_xifexpression);
     }
     return _xblockexpression;
+  }
+  
+  public boolean containsReturn(final XExpression expr) {
+    {
+      Collection<ExitPoint> _exitPoints = this.computer.getExitPoints(expr);
+      final Collection<ExitPoint> exitPoints = _exitPoints;
+      for (final ExitPoint point : exitPoints) {
+        XExpression _expression = point.getExpression();
+        if ((_expression instanceof org.eclipse.xtext.xbase.XReturnExpression)) {
+          return true;
+        }
+      }
+      return false;
+    }
   }
 }
