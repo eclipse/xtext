63348db32832cf1a85bffb73c41ffe9be9abffed
Jan Koehnlein <jan.koehnlein@itemis.de>
2011-09-06 19:42:47 +0200

    [refactoring] Eliminated IRefactoringProcessorAdapter and Strict LinkedPositionGroupCalculator

--- plugins/org.eclipse.xtext.xtend2.ui/src-gen/org/eclipse/xtext/xtend2/ui/AbstractXtend2UiModule.java
+++ plugins/org.eclipse.xtext.xtend2.ui/src-gen/org/eclipse/xtext/xtend2/ui/AbstractXtend2UiModule.java
@@ -119,11 +119,6 @@ public abstract class AbstractXtend2UiModule extends DefaultUiModule {
 	}
 
 	// contributed by org.eclipse.xtext.ui.generator.refactoring.RefactorElementNameFragment
-	public Class<? extends org.eclipse.xtext.ui.refactoring.IRenameProcessorAdapter.Factory> bindIRenameProcessorAdapter$Factory() {
-		return org.eclipse.xtext.common.types.ui.refactoring.JavaRenameProcessorAdapter.Factory.class;
-	}
-
-	// contributed by org.eclipse.xtext.ui.generator.refactoring.RefactorElementNameFragment
 	public Class<? extends org.eclipse.xtext.ui.refactoring.ui.IRenameSupport.Factory> bindIRenameSupport$Factory() {
 		return org.eclipse.xtext.common.types.ui.refactoring.JdtAwareRenameSupportFactory.class;
 	}
