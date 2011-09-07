d6567c2a6606414b116d773cb552167ee2db8959
Jan Koehnlein <jan.koehnlein@itemis.de>
2011-09-07 16:13:09 +0200

    [refactoring] simplified linked position calculation API

--- plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/refactoring/ui/DefaultLinkedPositionGroupCalculator.java
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/refactoring/ui/DefaultLinkedPositionGroupCalculator.java
@@ -12,15 +12,19 @@ import static com.google.common.collect.Lists.*;
 import static org.eclipse.xtext.util.Strings.*;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.link.LinkedPosition;
 import org.eclipse.jface.text.link.LinkedPositionGroup;
 import org.eclipse.ltk.core.refactoring.Change;
 import org.eclipse.text.edits.ReplaceEdit;
@@ -31,8 +35,10 @@ import org.eclipse.xtext.ui.editor.XtextEditor;
 import org.eclipse.xtext.ui.editor.findrefs.IReferenceFinder;
 import org.eclipse.xtext.ui.editor.findrefs.IReferenceFinder.IQueryData;
 import org.eclipse.xtext.ui.editor.findrefs.SimpleLocalResourceAccess;
+import org.eclipse.xtext.ui.editor.model.IXtextDocument;
 import org.eclipse.xtext.ui.refactoring.ElementRenameArguments;
 import org.eclipse.xtext.ui.refactoring.IDependentElementsCalculator;
+import org.eclipse.xtext.ui.refactoring.ILinkedPositionGroupCalculator;
 import org.eclipse.xtext.ui.refactoring.IRefactoringUpdateAcceptor;
 import org.eclipse.xtext.ui.refactoring.IReferenceUpdater;
 import org.eclipse.xtext.ui.refactoring.IRenameStrategy;
@@ -44,14 +50,20 @@ import org.eclipse.xtext.ui.refactoring.impl.RefactoringResourceSetProvider;
 import org.eclipse.xtext.ui.refactoring.impl.StatusWrapper;
 import org.eclipse.xtext.util.IAcceptor;
 
+import com.google.common.base.Function;
+import com.google.common.base.Predicates;
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.collect.Iterables;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 
 /**
  * @author Jan Koehnlein - Initial contribution and API
  */
-public class SimpleLinkedPositionGroupCalculator extends AbstractLinkedPositionGroupCalculator {
+public class DefaultLinkedPositionGroupCalculator implements ILinkedPositionGroupCalculator {
 
+	private static final Logger LOG = Logger.getLogger(DefaultLinkedPositionGroupCalculator.class);
+	
 	@Inject
 	private ProjectUtil projectUtil;
 
@@ -126,7 +138,63 @@ public class SimpleLinkedPositionGroupCalculator extends AbstractLinkedPositionG
 				renameStrategy.getOriginalName(), progress.newChild(10));
 		return linkedGroup;
 	}
+	
+	protected LinkedPositionGroup createLinkedGroupFromReplaceEdits(List<ReplaceEdit> edits, XtextEditor xtextEditor,
+			final String originalName, SubMonitor progress) {
+		if (edits == null)
+			return null;
+		final IXtextDocument document = xtextEditor.getDocument();
+		LinkedPositionGroup group = new LinkedPositionGroup();
+		Iterable<LinkedPosition> linkedPositions = filter(
+				Iterables.transform(edits, new Function<ReplaceEdit, LinkedPosition>() {
+					public LinkedPosition apply(ReplaceEdit edit) {
+						try {
+							String textToReplace = document.get(edit.getOffset(), edit.getLength());
+							int indexOf = textToReplace.indexOf(originalName);
+							if (indexOf != -1) {
+								int calculatedOffset = edit.getOffset() + indexOf;
+								return new LinkedPosition(document, calculatedOffset, originalName.length());
+							}
+						} catch (BadLocationException exc) {
+							LOG.error("Skipping invalid text edit " + notNull(edit), exc);
+						}
+						return null;
+					}
+				}), Predicates.notNull());
+		progress.worked(10);
+		final int invocationOffset = xtextEditor.getInternalSourceViewer().getSelectedRange().x;
+		int i = 0;
+		for (LinkedPosition position : sortPositions(linkedPositions, invocationOffset)) {
+			try {
+				position.setSequenceNumber(i);
+				i++;
+				group.addPosition(position);
+			} catch (BadLocationException e) {
+				LOG.error(e.getMessage(), e);
+				return null;
+			}
+		}
+		return group;
+	}
 
+	protected Iterable<LinkedPosition> sortPositions(Iterable<LinkedPosition> linkedPositions,
+			final int invocationOffset) {
+		Comparator<LinkedPosition> comparator = new Comparator<LinkedPosition>() {
+
+			public int compare(LinkedPosition left, LinkedPosition right) {
+				return rank(left) - rank(right);
+			}
+
+			private int rank(LinkedPosition o1) {
+				int relativeRank = o1.getOffset() + o1.length - invocationOffset;
+				if (relativeRank < 0)
+					return Integer.MAX_VALUE + relativeRank;
+				else
+					return relativeRank;
+			}
+		};
+		return ImmutableSortedSet.copyOf(comparator, linkedPositions);
+	}
 	public static class LocalResourceRefactoringUpdateAcceptor implements IRefactoringUpdateAcceptor {
 
 		@Inject
