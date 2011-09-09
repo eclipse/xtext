8fbe6ff4760a7e05a50591feab1ade1b723cccd1
Jan Koehnlein <jan.koehnlein@itemis.de>
2011-09-09 11:30:16 +0200

    [refactoring] more efficient index search for JVM elements

--- plugins/org.eclipse.xtext.common.types.ui/src/org/eclipse/xtext/common/types/ui/refactoring/participant/JvmElementFinder.java
+++ plugins/org.eclipse.xtext.common.types.ui/src/org/eclipse/xtext/common/types/ui/refactoring/participant/JvmElementFinder.java
@@ -7,6 +7,8 @@
  *******************************************************************************/
 package org.eclipse.xtext.common.types.ui.refactoring.participant;
 
+import java.util.Iterator;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
@@ -17,6 +19,7 @@ import org.eclipse.xtext.common.types.access.jdt.TypeURIHelper;
 import org.eclipse.xtext.naming.IQualifiedNameProvider;
 import org.eclipse.xtext.naming.QualifiedName;
 import org.eclipse.xtext.resource.IEObjectDescription;
+import org.eclipse.xtext.resource.IResourceDescription;
 import org.eclipse.xtext.resource.IResourceDescriptions;
 import org.eclipse.xtext.util.Strings;
 
@@ -24,7 +27,7 @@ import com.google.inject.Inject;
 
 /**
  * Helps to resolve {@link JvmIdentifiableElement}s corresponding to {@link IJavaElement}s.
- *  
+ * 
  * @author Jan Koehnlein - Initial contribution and API
  */
 public class JvmElementFinder {
@@ -40,28 +43,28 @@ public class JvmElementFinder {
 
 	@Inject
 	private IQualifiedNameProvider qualifiedNameProvider;
-	
+
 	public EObject getCorrespondingJvmElement(IJavaElement javaElement, ResourceSet resourceSet) {
 		typeProviderFactory.findOrCreateTypeProvider(resourceSet);
 		URI jvmElementURI = typeURIHelper.getFullURI(javaElement);
 		return (jvmElementURI != null) ? resourceSet.getEObject(jvmElementURI, true) : null;
 	}
-	
+
 	public EObject findJvmElementDeclarationInIndex(EObject jvmElement, IProject project) {
 		// TODO lookup top level type container in index instead? 
 		QualifiedName qualifiedName = qualifiedNameProvider.getFullyQualifiedName(jvmElement);
 		if (qualifiedName != null) {
-			for (IEObjectDescription candidate : resourceDescriptions.getExportedObjects(jvmElement.eClass(), qualifiedName,
-					true)) {
-				if (candidate.getEObjectURI().isPlatformResource()
-						&& Strings.equal(project.getName(), candidate.getEObjectURI().segment(1))) {
-					return jvmElement.eResource().getResourceSet().getEObject(candidate.getEObjectURI(), true);
+			for (IResourceDescription resourceDescription : resourceDescriptions.getAllResourceDescriptions()) {
+				if (resourceDescription.getURI().isPlatformResource()
+						&& Strings.equal(project.getName(), resourceDescription.getURI().segment(1))) {
+					Iterator<IEObjectDescription> matches = resourceDescription.getExportedObjects(jvmElement.eClass(),
+							qualifiedName, true).iterator();
+					if(matches.hasNext())
+						return jvmElement.eResource().getResourceSet().getEObject(matches.next().getEObjectURI(), true);
 				}
 			}
-			return null;
 		}
 		return null;
 	}
-	
 
 }
