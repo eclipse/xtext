44492ad6d3526afc07ca79a55aa52399e07c2b66
Sven Efftinge <sven.efftinge@itemis.de>
2011-09-09 10:08:56 +0200

    [Xtext] added late initializing resource

---
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/resource/ILateInitialization.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2011 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.resource;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * @author Sven Efftinge - Initial contribution and API
+ * @since 2.1
+ */
+public interface ILateInitialization {
+	
+	/**
+	 * callback to do modifications when and before getContents is called the first time on a resource.
+	 * {@link org.eclipse.emf.ecore.resource.Resource#eDeliver()} will be set to <code>false</code> during initialization,
+	 * such that modifications on the list don't fire any {@link org.eclipse.emf.common.notify.Notification}s.
+	 */
+	void doLateInitialization(EList<EObject> resourcesContentsList);
+}
---
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/resource/LateInitializationLazyLinkingResourceTest.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (c) 2011 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.resource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.impl.AdapterImpl;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EcoreFactory;
+import org.eclipse.xtext.junit.AbstractXtextTests;
+
+/**
+ * @author Sven Efftinge - Initial contribution and API
+ */
+public class LateInitializationLazyLinkingResourceTest extends AbstractXtextTests {
+
+	/**
+	 * @author efftinge - Initial contribution and API
+	 */
+	public static final class TestedResource extends LateInitializingLazyLinkingResource {
+		public void setIsLoaded() {
+			this.isLoaded = true;
+		}
+	}
+
+	public void testInitialization() throws Exception {
+		TestedResource resource = new TestedResource();
+		assertTrue(resource.getContents().isEmpty());
+		resource.setLateInitialization(new ILateInitialization() {
+			public void doLateInitialization(EList<EObject> resourcesContentsList) {
+				fail("shouldn't be called after initialization");
+			}
+		});
+		resource.getContents();
+		resource.setIsLoaded();
+		resource.eAdapters().add(new AdapterImpl(){
+			@Override
+			public void notifyChanged(Notification msg) {
+				fail("notification received");
+				super.notifyChanged(msg);
+			}
+		});
+		resource.setLateInitialization(new ILateInitialization() {
+			public void doLateInitialization(EList<EObject> resourcesContentsList) {
+				resourcesContentsList.add(EcoreFactory.eINSTANCE.createEObject());
+			}
+		});
+		assertEquals(1, resource.getContents().size());
+	}
+}
