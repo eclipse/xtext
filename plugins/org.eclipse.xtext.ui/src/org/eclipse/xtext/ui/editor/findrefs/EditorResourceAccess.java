31628cbff99c527bb74c30a61335392b2489f732
Jan Koehnlein <jan.koehnlein@itemis.de>
2011-09-26 16:54:06 +0200

    [mark occurrences] Avoid deadlock https://bugs.eclipse.org/bugs/show_bug.cgi?id=358396

--- plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/findrefs/EditorResourceAccess.java
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/findrefs/EditorResourceAccess.java
@@ -12,8 +12,8 @@ import static com.google.common.collect.Iterables.*;
 import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IStorage;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.common.util.WrappedException;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
@@ -43,7 +43,7 @@ public class EditorResourceAccess implements ILocalResourceAccess {
 
 	private static final Logger LOG = Logger.getLogger(EditorResourceAccess.class);
 
-	@Inject(optional=true)
+	@Inject(optional = true)
 	@Nullable
 	private IWorkbench workbench;
 
@@ -54,44 +54,38 @@ public class EditorResourceAccess implements ILocalResourceAccess {
 	private LoadingResourceAccess delegate;
 
 	public <R> R readOnly(final URI targetURI, final IUnitOfWork<R, ResourceSet> work) {
-		try {
-			R result = new DisplayRunnableWithResult<R>() {
-				@Override
-				protected R run() throws Exception {
-					IXtextDocument document = getOpenDocument(targetURI.trimFragment());
-					if (document != null) {
-						return document.readOnly(new IUnitOfWork<R, XtextResource>() {
-							public R exec(XtextResource state) throws Exception {
-								ResourceSet localContext = state.getResourceSet();
-								if (localContext != null)
-									return work.exec(localContext);
-								return null;
-							}
-						});
-					}
+		R result = null;
+		IXtextDocument document = getOpenDocument(targetURI.trimFragment());
+		if (document != null) {
+			result = document.readOnly(new IUnitOfWork<R, XtextResource>() {
+				public R exec(XtextResource state) throws Exception {
+					ResourceSet localContext = state.getResourceSet();
+					if (localContext != null)
+						return work.exec(localContext);
 					return null;
 				}
-			}.syncExec();
-			if (result != null)
-				return result;
-			else
-				return delegate.readOnly(targetURI, work);
-		} catch (Exception e) {
-			throw new WrappedException(e);
+			});
 		}
+		return result != null ? result : delegate.readOnly(targetURI, work);
 	}
 
-	protected IXtextDocument getOpenDocument(URI targetURI) throws Exception {
-		if (workbench != null) {
-			IWorkbenchPage activePage = workbench.getActiveWorkbenchWindow().getActivePage();
-			if (activePage != null) {
-				Iterable<Pair<IStorage, IProject>> storagesToProject = storage2UriMapper.getStorages(targetURI.trimFragment());
-				Iterable<IStorage> storages = Iterables.transform(storagesToProject, new Function<Pair<IStorage, IProject>, IStorage>() {
+	protected IXtextDocument getOpenDocument(final URI targetURI) {
+		if (workbench == null)
+			return null;
+		Iterable<Pair<IStorage, IProject>> storagesToProject = storage2UriMapper.getStorages(targetURI.trimFragment());
+		final Iterable<IStorage> storages = Iterables.transform(storagesToProject,
+				new Function<Pair<IStorage, IProject>, IStorage>() {
 					public IStorage apply(Pair<IStorage, IProject> from) {
 						return from.getFirst();
 					}
 				});
-				if (!isEmpty(storages)) {
+		if (!isEmpty(storages)) {
+			return new DisplayRunnableWithResult<IXtextDocument>() {
+				@Override
+				protected IXtextDocument run() throws Exception {
+					IWorkbenchPage activePage = workbench.getActiveWorkbenchWindow().getActivePage();
+					if (activePage == null)
+						return null;
 					for (IEditorReference editorReference : activePage.getEditorReferences()) {
 						try {
 							IEditorInput editorInput = editorReference.getEditorInput();
@@ -103,14 +97,14 @@ public class EditorResourceAccess implements ILocalResourceAccess {
 									return xtextEditor.getDocument();
 								}
 							}
-						} catch (PartInitException e) {
-							LOG.error(e);
+						} catch (Exception e) {
+							LOG.error("Error accessing document", e);
 						}
 					}
+					return null;
 				}
-			}
+			}.syncExec();
 		}
 		return null;
 	}
-
 }
