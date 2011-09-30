7ecd2da26bb9cc48c46e40281d690da62c50aa78
Holger Schill <Holger.Schill@itemis.de>
2011-09-30 20:53:45 +0200

    [Xtend] Semantic Syntax Coloring see https://bugs.eclipse.org/bugs/show_bug.cgi?id=358200 added support for deprecation

---
+++ plugins/org.eclipse.xtext.common.types/src/org/eclipse/xtext/common/types/util/DeprecationUtil.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2011 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.common.types.util;
+
+import org.eclipse.xtext.common.types.JvmAnnotationReference;
+import org.eclipse.xtext.common.types.JvmAnnotationTarget;
+import org.eclipse.xtext.common.types.JvmAnnotationType;
+
+/**
+ * @author Holger Schill - Initial contribution and API
+ */
+public class DeprecationUtil {
+	public static boolean isDeprecated(JvmAnnotationTarget jvmAnnotationTarget) {
+		for (JvmAnnotationReference ref : jvmAnnotationTarget.getAnnotations()) {
+			if (ref != null && !ref.eIsProxy()) {
+				JvmAnnotationType annotation = ref.getAnnotation();
+				return annotation != null && !annotation.eIsProxy() && isDeprecated(annotation);
+			}
+		}
+		return false;
+	}
+
+	public static boolean isDeprecated(JvmAnnotationType jvmAnnotationType) {
+		return jvmAnnotationType.getQualifiedName().equals(Deprecated.class.getName());
+	}
+}
