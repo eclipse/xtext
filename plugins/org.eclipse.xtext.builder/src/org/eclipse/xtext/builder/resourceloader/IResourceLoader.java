2b77af51ce792c607da11e475ad7a9e6b1865e82
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2011-10-11 16:08:22 +0200

    [xtext][builder] The builder's loading strategy has to be aware of the currently built project to configure the resource set's class path context
    Change Provider<XtextResourceSet> to XtextResourceSetProvider

--- plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/resourceloader/IResourceLoader.java
+++ plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/resourceloader/IResourceLoader.java
@@ -12,6 +12,7 @@ package org.eclipse.xtext.builder.resourceloader;
 
 import java.util.Collection;
 
+import org.eclipse.core.resources.IProject;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.common.util.WrappedException;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -36,7 +37,7 @@ public interface IResourceLoader {
 	 * @param parent the parent ResourceSet
 	 * @return a LoadOperation
 	 */
-	LoadOperation create(ResourceSet parent);
+	LoadOperation create(ResourceSet parent, IProject project);
 
 	/**
 	 * The Interface LoadOperation.
