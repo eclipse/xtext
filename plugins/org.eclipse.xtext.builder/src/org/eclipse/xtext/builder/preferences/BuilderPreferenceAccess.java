65e621e077156dea88fc65834c033cba5b1ba89c
Michael Clay <clay@codeworkz.at>
2011-10-20 00:24:38 +0200

    [builder] simplify preference handling of builder configuration ui and remove WorkingCopyPreferenceAccess

--- plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/preferences/BuilderPreferenceAccess.java
+++ plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/preferences/BuilderPreferenceAccess.java
@@ -9,23 +9,14 @@ package org.eclipse.xtext.builder.preferences;
 
 import static org.eclipse.xtext.builder.EclipseOutputConfigurationProvider.*;
 
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.runtime.preferences.IEclipsePreferences.IPreferenceChangeListener;
-import org.eclipse.core.runtime.preferences.IEclipsePreferences.PreferenceChangeEvent;
 import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.jface.util.IPropertyChangeListener;
-import org.eclipse.jface.util.PropertyChangeEvent;
-import org.eclipse.xtext.builder.DerivedResourceCleanerJob;
 import org.eclipse.xtext.generator.OutputConfiguration;
 import org.eclipse.xtext.generator.OutputConfigurationProvider;
 import org.eclipse.xtext.ui.editor.preferences.IPreferenceStoreAccess;
 import org.eclipse.xtext.ui.editor.preferences.IPreferenceStoreInitializer;
 import org.eclipse.xtext.ui.editor.preferences.PreferenceConstants;
-import org.osgi.service.prefs.Preferences;
 
 import com.google.inject.Inject;
-import com.google.inject.Provider;
 import com.google.inject.Singleton;
 
 /**
@@ -40,7 +31,6 @@ public class BuilderPreferenceAccess {
 	public static final String PREF_AUTO_BUILDING = "autobuilding"; //$NON-NLS-1$
 
 	public static class Initializer implements IPreferenceStoreInitializer {
-		private ChangeListener changeListener;
 		private OutputConfigurationProvider outputConfigurationProvider;
 
 		public OutputConfigurationProvider getOutputConfigurationProvider() {
@@ -52,20 +42,10 @@ public class BuilderPreferenceAccess {
 			this.outputConfigurationProvider = outputConfigurationProvider;
 		}
 
-		public ChangeListener getChangeListener() {
-			return changeListener;
-		}
-
-		@Inject
-		public void setChangeListener(ChangeListener changeListener) {
-			this.changeListener = changeListener;
-		}
-
 		public void initialize(IPreferenceStoreAccess preferenceStoreAccess) {
 			IPreferenceStore store = preferenceStoreAccess.getWritablePreferenceStore();
 			intializeBuilderPreferences(store);
 			initializeOutputPreferences(store);
-			store.addPropertyChangeListener(getChangeListener());
 		}
 
 		private void intializeBuilderPreferences(IPreferenceStore store) {
@@ -93,51 +73,6 @@ public class BuilderPreferenceAccess {
 	public static String getKey(OutputConfiguration outputConfiguration, String preferenceName) {
 		return OUTPUT_PREFERENCE_TAG + PreferenceConstants.SEPARATOR + outputConfiguration.getName()
 				+ PreferenceConstants.SEPARATOR + preferenceName;
-	}
-
-	public static class ChangeListener implements IPropertyChangeListener, IPreferenceChangeListener {
-
-		@Inject
-		private Provider<DerivedResourceCleanerJob> cleanerProvider;
-		
-		@Inject
-		private IWorkspace workspace;
-		
-		public ChangeListener() {
-//			new Exception("New ChangeListener").printStackTrace();
-		}
-
-		public void propertyChange(PropertyChangeEvent event) {
-			if (isOutputDirectoryKey(event.getProperty())) {
-				String oldValue = (String) event.getOldValue();
-				scheduleCleanerJob(null, oldValue);
-			}
-		}
-
-		public void preferenceChange(PreferenceChangeEvent preferenceChangeEvent) {
-			if (isOutputDirectoryKey(preferenceChangeEvent.getKey())) {
-				Preferences node = preferenceChangeEvent.getNode();
-				IProject project = null;
-				// TODO: this is a hack
-				if (node.absolutePath().startsWith("/project/")) {
-					String projectName = node.parent().name();
-					project = workspace.getRoot().getProject(projectName);
-				}
-				String oldValue = (String) preferenceChangeEvent.getOldValue();
-				scheduleCleanerJob(project, oldValue);
-			}
-		}
-
-		private void scheduleCleanerJob(IProject project, String folderNameToClean) {
-			DerivedResourceCleanerJob cleaner = cleanerProvider.get();
-			cleaner.setUser(true);
-			cleaner.initialize(project, folderNameToClean);
-			cleaner.schedule();
-		}
-
-		private boolean isOutputDirectoryKey(String key) {
-			return key.matches("^" + OUTPUT_PREFERENCE_TAG + "\\.\\w+\\." + OUTPUT_DIRECTORY + "$");
-		}
 	}
 
 	private IPreferenceStoreAccess preferenceStoreAccess;
--- plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/preferences/OptionsConfigurationBlock.java
+++ plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/preferences/OptionsConfigurationBlock.java
@@ -7,18 +7,17 @@
  *******************************************************************************/
 package org.eclipse.xtext.builder.preferences;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.ProjectScope;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.preferences.DefaultScope;
-import org.eclipse.core.runtime.preferences.IScopeContext;
-import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.preference.IPersistentPreferenceStore;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
@@ -39,10 +38,10 @@ import org.eclipse.ui.forms.events.ExpansionEvent;
 import org.eclipse.ui.forms.widgets.ExpandableComposite;
 import org.eclipse.ui.preferences.IWorkbenchPreferenceContainer;
 import org.eclipse.xtext.builder.internal.Activator;
-import org.eclipse.xtext.util.Strings;
-import org.osgi.service.prefs.BackingStoreException;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.MapDifference;
+import com.google.common.collect.MapDifference.ValueDifference;
 import com.google.common.collect.Maps;
 
 /**
@@ -52,6 +51,10 @@ import com.google.common.collect.Maps;
  * @since 2.1
  */
 public abstract class OptionsConfigurationBlock {
+
+	private static final String IS_PROJECT_SPECIFIC = "is_project_specific"; //$NON-NLS-1$
+	private static final String SETTINGS_EXPANDED = "expanded"; //$NON-NLS-1$
+	private static final String REBUILD_COUNT_KEY = "preferences_build_requested"; //$NON-NLS-1$
 
 	protected static class ControlData {
 		private String key;
@@ -87,8 +90,6 @@ public abstract class OptionsConfigurationBlock {
 		}
 	}
 
-	private static final String REBUILD_COUNT_KEY = "preferences_build_requested"; //$NON-NLS-1$
-	private static final String SETTINGS_EXPANDED = "expanded"; //$NON-NLS-1$
 	protected final List<Button> checkBoxes = Lists.newArrayList();
 	protected final List<Text> textBoxes = Lists.newArrayList();
 	protected final Map<Control, Label> labels = Maps.newHashMap();
@@ -100,8 +101,9 @@ public abstract class OptionsConfigurationBlock {
 	protected final String[] keys;
 	private Shell shell;
 	private Map<String, String> disabledProjectSettings;
+	private Map<String, String> originalSettings;
 	private int rebuildCount;
-	private WorkingCopyPreferencesAccess workingCopyPreferencesAccess;
+	private IPreferenceStore preferenceStore;
 	private IWorkbenchPreferenceContainer workbenchPreferenceContainer;
 
 	public IProject getProject() {
@@ -112,33 +114,25 @@ public abstract class OptionsConfigurationBlock {
 		this.statusChangeListener = fContext;
 	}
 
-	public OptionsConfigurationBlock(IProject project, String[] allKeys,
-			WorkingCopyPreferencesAccess workingCopyPreferencesAccess, IWorkbenchPreferenceContainer container) {
+	public OptionsConfigurationBlock(IProject project, String[] allKeys, IPreferenceStore preferenceStore,
+			IWorkbenchPreferenceContainer container) {
 		this.project = project;
 		this.keys = allKeys;
-		this.workingCopyPreferencesAccess = workingCopyPreferencesAccess;
+		this.preferenceStore = preferenceStore;
 		this.workbenchPreferenceContainer = container;
 		if (project == null || hasProjectSpecificOptions(project)) {
 			disabledProjectSettings = null;
 		} else {
 			disabledProjectSettings = Maps.newHashMap();
 			for (String key : allKeys) {
-				disabledProjectSettings.put(key, workingCopyPreferencesAccess.getValue(key));
+				disabledProjectSettings.put(key, preferenceStore.getString(key));
 			}
 		}
-		rebuildCount = getRebuildCount();
+		captureOriginalSettings();
 	}
 
 	public boolean hasProjectSpecificOptions(IProject project) {
-		if (project != null) {
-			IScopeContext projectContext = new ProjectScope(project);
-			for (int i = 0; i < keys.length; i++) {
-				if (workingCopyPreferencesAccess.getValue(keys[i], projectContext) != null) {
-					return true;
-				}
-			}
-		}
-		return false;
+		return preferenceStore.getBoolean(IS_PROJECT_SPECIFIC);
 	}
 
 	protected void setShell(Shell shell) {
@@ -299,7 +293,7 @@ public abstract class OptionsConfigurationBlock {
 		if (disabledProjectSettings != null) {
 			return disabledProjectSettings.get(key);
 		}
-		return workingCopyPreferencesAccess.getValue(key);
+		return preferenceStore.getString(key);
 	}
 
 	protected String setValue(String key, String value) {
@@ -307,33 +301,8 @@ public abstract class OptionsConfigurationBlock {
 			return disabledProjectSettings.put(key, value);
 		}
 		String oldValue = getValue(key);
-		workingCopyPreferencesAccess.setValue(key, value);
+		preferenceStore.setValue(key, value);
 		return oldValue;
-	}
-
-	private boolean getChanges(IScopeContext currContext, List<String> changedSettings) {
-		boolean completeSettings = project != null && disabledProjectSettings == null; // complete when project settings are enabled
-		boolean needsBuild = false;
-		for (int i = 0; i < keys.length; i++) {
-			String key = keys[i];
-			String oldVal = workingCopyPreferencesAccess.getOriginalValue(key, currContext);
-			String val = workingCopyPreferencesAccess.getValue(key, currContext);
-			String defaultValue = workingCopyPreferencesAccess.getDefaultValue(key);
-			if (val == null) {
-				if (oldVal != null) {
-					changedSettings.add(key);
-					needsBuild |= !oldVal.equals(defaultValue);
-				} else if (completeSettings) {
-					workingCopyPreferencesAccess.setValue(key, currContext, defaultValue);
-					changedSettings.add(key);
-					// no build needed
-				}
-			} else if (!val.equals(oldVal)) {
-				changedSettings.add(key);
-				needsBuild |= oldVal != null || !val.equals(defaultValue);
-			}
-		}
-		return needsBuild;
 	}
 
 	public void useProjectSpecificSettings(boolean enable) {
@@ -343,61 +312,52 @@ public abstract class OptionsConfigurationBlock {
 				for (int i = 0; i < keys.length; i++) {
 					String curr = keys[i];
 					String val = disabledProjectSettings.get(curr);
-					workingCopyPreferencesAccess.setValue(curr, val);
+					preferenceStore.setValue(curr, val);
 				}
 				disabledProjectSettings = null;
 				updateControls();
 				validateSettings(null, null, null);
+				preferenceStore.setValue(IS_PROJECT_SPECIFIC, true);
 			} else {
 				disabledProjectSettings = Maps.newHashMap();
 				for (int i = 0; i < keys.length; i++) {
 					String curr = keys[i];
-					String oldSetting = workingCopyPreferencesAccess.getValue(curr);
+					String oldSetting = preferenceStore.getString(curr);
 					disabledProjectSettings.put(curr, oldSetting);
-					workingCopyPreferencesAccess.setValue(curr, null); // clear project settings
+					preferenceStore.setToDefault(curr);
 				}
+				preferenceStore.setToDefault(IS_PROJECT_SPECIFIC);
 			}
 		}
 	}
 
 	public boolean performOk() {
-		return processChanges(false);
+		return processChanges(workbenchPreferenceContainer);
 	}
 
 	public boolean performApply() {
-		return processChanges(true);
+		return processChanges(null);
 	}
 
 	private int getRebuildCount() {
-		int result = 0;
-		String value = workingCopyPreferencesAccess.getValue(REBUILD_COUNT_KEY, new DefaultScope());
-		if (!Strings.isEmpty(value)) {
-			result = Integer.valueOf(value);
-		}
-		return result;
+		return preferenceStore.getDefaultInt(REBUILD_COUNT_KEY);
 	}
 
 	private void incrementRebuildCount() {
 		rebuildCount++;
-		workingCopyPreferencesAccess.setValue(REBUILD_COUNT_KEY, new DefaultScope(), String.valueOf(rebuildCount));
+		preferenceStore.setDefault(REBUILD_COUNT_KEY, rebuildCount);
 	}
 
-	protected boolean processChanges(boolean applyDirect) {
-		IScopeContext currContext = project != null ? new ProjectScope(project) : new InstanceScope();
-		List<String> changedOptions = Lists.newArrayList();
-		boolean needsBuild = getChanges(currContext, changedOptions);
-		if (changedOptions.isEmpty()) {
-			return true;
-		}
+	protected boolean processChanges(IWorkbenchPreferenceContainer container) {
+		boolean needsBuild = !getPreferenceChanges().isEmpty();
+		boolean doBuild = false;
 		if (needsBuild) {
 			int count = getRebuildCount();
 			if (count > rebuildCount) {
-				needsBuild = false; // build already requested
+				needsBuild = false;
 				rebuildCount = count;
 			}
 		}
-
-		boolean doBuild = false;
 		if (needsBuild) {
 			String[] strings = getFullBuildDialogStrings(project == null);
 			if (strings != null) {
@@ -412,24 +372,39 @@ public abstract class OptionsConfigurationBlock {
 				}
 			}
 		}
-		if (!applyDirect) {
+		savePreferences();
+		if (container != null) {
 			if (doBuild) {
 				incrementRebuildCount();
-				workbenchPreferenceContainer.registerUpdateJob(getBuildJob(getProject()));
+				container.registerUpdateJob(getBuildJob(getProject()));
 			}
 		} else {
-			try {
-				workingCopyPreferencesAccess.applyChanges();
-			} catch (BackingStoreException e) {
-				Activator.log(e);
-				return false;
-			}
 			if (doBuild) {
 				getBuildJob(getProject()).schedule();
 			}
-
 		}
+		captureOriginalSettings();
 		return true;
+	}
+
+	protected void savePreferences() {
+		try {
+			if (preferenceStore instanceof IPersistentPreferenceStore) {
+				((IPersistentPreferenceStore) preferenceStore).save();
+			}
+		} catch (IOException e) {
+			Activator.log(e);
+		}
+	}
+
+	public Map<String, ValueDifference<String>> getPreferenceChanges() {
+		Map<String, String> currentSettings = Maps.newHashMapWithExpectedSize(keys.length);
+		for (String key : keys) {
+			currentSettings.put(key, preferenceStore.getString(key));
+		}
+		MapDifference<String, String> mapDifference = Maps.difference(currentSettings, originalSettings);
+		Map<String, ValueDifference<String>> entriesDiffering = mapDifference.entriesDiffering();
+		return entriesDiffering;
 	}
 
 	protected abstract Job getBuildJob(IProject project);
@@ -439,7 +414,7 @@ public abstract class OptionsConfigurationBlock {
 	public void performDefaults() {
 		for (int i = 0; i < keys.length; i++) {
 			String curr = keys[i];
-			String defValue = workingCopyPreferencesAccess.getDefaultValue(curr);
+			String defValue = preferenceStore.getDefaultString(curr);
 			setValue(curr, defValue);
 		}
 		updateControls();
@@ -449,9 +424,6 @@ public abstract class OptionsConfigurationBlock {
 	public void dispose() {
 	}
 
-	/**
-	 * Updates the UI from the current settings. Must be called whenever a setting has been changed by code.
-	 */
 	protected void updateControls() {
 		for (int i = checkBoxes.size() - 1; i >= 0; i--) {
 			updateCheckBox(checkBoxes.get(i));
@@ -476,5 +448,12 @@ public abstract class OptionsConfigurationBlock {
 	}
 
 	protected abstract void validateSettings(String changedKey, String oldValue, String newValue);
+
+	private void captureOriginalSettings() {
+		originalSettings = Maps.newHashMapWithExpectedSize(keys.length);
+		for (String key : keys) {
+			originalSettings.put(key, preferenceStore.getString(key));
+		}
+	}
 
 }
