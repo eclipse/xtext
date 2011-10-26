c3f2de2167c0f5a2b5b288f712d56c46561b0f6d
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2011-10-26 05:47:41 +0200

    [junit4] improved ParameterizedXtextRunner

--- plugins/org.eclipse.xtext.junit4/src/org/eclipse/xtext/junit4/util/ParseHelper.java
+++ plugins/org.eclipse.xtext.junit4/src/org/eclipse/xtext/junit4/util/ParseHelper.java
@@ -16,56 +16,62 @@ import org.eclipse.emf.common.util.WrappedException;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.xtext.Constants;
 import org.eclipse.xtext.resource.IResourceFactory;
 import org.eclipse.xtext.resource.XtextResourceSet;
 import org.eclipse.xtext.util.StringInputStream;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
+import com.google.inject.name.Named;
 
 /**
- * Some convenience methods for parsing (i.e. testing, etc.) 
+ * Some convenience methods for parsing (i.e. testing, etc.)
  * 
  * @author Sven Efftinge - Initial contribution and API
  */
 public class ParseHelper<T extends EObject> {
-	
+
 	@Inject
 	private Provider<XtextResourceSet> resourceSetProvider;
-	
+
 	@Inject
 	private IResourceFactory resourceFactory;
 
+	@Inject
+	@Named(Constants.FILE_EXTENSIONS)
+	public String fileExtension;
+
 	@SuppressWarnings("unchecked")
-	public T parse(InputStream in, URI uriToUse, Map<?,?> options, ResourceSet resourceSet) {
+	public T parse(InputStream in, URI uriToUse, Map<?, ?> options, ResourceSet resourceSet) {
 		Resource resource = resourceFactory.createResource(uriToUse);
 		resourceSet.getResources().add(resource);
 		try {
 			resource.load(in, options);
-			final T root = (T) (resource.getContents().isEmpty()? null : resource.getContents().get(0));
+			final T root = (T) (resource.getContents().isEmpty() ? null : resource.getContents().get(0));
 			return root;
 		} catch (IOException e) {
 			throw new WrappedException(e);
 		}
 	}
-	
+
 	public T parse(CharSequence text) throws Exception {
 		return parse(text, resourceSetProvider.get());
 	}
-	
+
 	public T parse(CharSequence text, ResourceSet resourceSetToUse) throws Exception {
 		return parse(getAsStream(text), computeUnusedUri(resourceSetToUse), null, resourceSetToUse);
 	}
-	
+
 	public T parse(CharSequence text, URI uriToUse, ResourceSet resourceSetToUse) throws Exception {
-        return parse(getAsStream(text), uriToUse, null, resourceSetToUse);
-    }
-	
+		return parse(getAsStream(text), uriToUse, null, resourceSetToUse);
+	}
+
 	protected URI computeUnusedUri(ResourceSet resourceSet) {
 		String name = "__synthetic";
 		for (int i = 0; i < Integer.MAX_VALUE; i++) {
-			URI syntheticUri = URI.createURI(name+i+".uri");
-			if (resourceSet.getResource(syntheticUri, false)==null)
+			URI syntheticUri = URI.createURI(name + i + "." + fileExtension);
+			if (resourceSet.getResource(syntheticUri, false) == null)
 				return syntheticUri;
 		}
 		throw new IllegalStateException();
