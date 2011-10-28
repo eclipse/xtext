54258f9c4fc0dd9f79f6d11aae9c4503f3c8cadd
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2011-10-28 14:59:40 +0200

    [xtend.ui][tests] Disabled UISmokeTests on server for now

--- tests/org.eclipse.xtext.xtend2.ui.tests/src-longrunning/org/eclipse/xtext/xtend2/ui/tests/smoke/UISmokeTest.java
+++ tests/org.eclipse.xtext.xtend2.ui.tests/src-longrunning/org/eclipse/xtext/xtend2/ui/tests/smoke/UISmokeTest.java
@@ -33,63 +33,63 @@ import com.google.inject.Injector;
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
  */
-public class UISmokeTest extends AbstractSmokeTest {
+public abstract class UISmokeTest extends AbstractSmokeTest {
 
-	public static Test suite() {
-		return WorkbenchTestHelper.suite(UISmokeTest.class);
-	}
-	
-	@Inject
-	private WorkbenchTestHelper testHelper;
-	
-	@Inject
-	private XtendHighlightingCalculator highlighter;
-	
-	@Inject
-	private IOutlineTreeProvider outlineTreeProvider;
-	
-	@Override
-	public Injector doGetInjector() {
-		return Xtend2Activator.getInstance().getInjector("org.eclipse.xtext.xtend2.Xtend2");
-	}
-	
-	@Override
-	protected void checkForSmoke(final String model, final LazyLinkingResource resource) {
-		highlighter.provideHighlightingFor(resource, new IHighlightedPositionAcceptor() {
-			public void addPosition(int offset, int length, String... id) {
-			}
-		});
-		IXtextDocument document = (IXtextDocument) Proxy.newProxyInstance(
-				getClass().getClassLoader(), new Class[] { IXtextDocument.class }, new InvocationHandler() {
-			
-			public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-				if ("readOnly".equals(method.getName())) {
-					@SuppressWarnings("unchecked")
-					IUnitOfWork<?, Resource> unit = (IUnitOfWork<?, Resource>) args[0];
-					Object result = unit.exec(resource);
-					return result;
-				}
-				if ("getLength".equals(method.getName())) {
-					return model.length();
-				}
-				return null;
-			}
-		});
-		IOutlineNode node = outlineTreeProvider.createRoot(document);
-		traverseNode(node);
-	}
-	
-	private void traverseNode(IOutlineNode node) {
-		node.getText();
-		for(IOutlineNode child: node.getChildren()) {
-			traverseNode(child);
-		}
-	}
-
-	@Override
-	protected XtextResourceSet getResourceSet() {
-		ResourceSet resourceSet = testHelper.getResourceSet();
-		return (XtextResourceSet) resourceSet;
-	}
+//	public static Test suite() {
+//		return WorkbenchTestHelper.suite(UISmokeTest.class);
+//	}
+//	
+//	@Inject
+//	private WorkbenchTestHelper testHelper;
+//	
+//	@Inject
+//	private XtendHighlightingCalculator highlighter;
+//	
+//	@Inject
+//	private IOutlineTreeProvider outlineTreeProvider;
+//	
+//	@Override
+//	public Injector doGetInjector() {
+//		return Xtend2Activator.getInstance().getInjector("org.eclipse.xtext.xtend2.Xtend2");
+//	}
+//	
+//	@Override
+//	protected void checkForSmoke(final String model, final LazyLinkingResource resource) {
+//		highlighter.provideHighlightingFor(resource, new IHighlightedPositionAcceptor() {
+//			public void addPosition(int offset, int length, String... id) {
+//			}
+//		});
+//		IXtextDocument document = (IXtextDocument) Proxy.newProxyInstance(
+//				getClass().getClassLoader(), new Class[] { IXtextDocument.class }, new InvocationHandler() {
+//			
+//			public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+//				if ("readOnly".equals(method.getName())) {
+//					@SuppressWarnings("unchecked")
+//					IUnitOfWork<?, Resource> unit = (IUnitOfWork<?, Resource>) args[0];
+//					Object result = unit.exec(resource);
+//					return result;
+//				}
+//				if ("getLength".equals(method.getName())) {
+//					return model.length();
+//				}
+//				return null;
+//			}
+//		});
+//		IOutlineNode node = outlineTreeProvider.createRoot(document);
+//		traverseNode(node);
+//	}
+//	
+//	private void traverseNode(IOutlineNode node) {
+//		node.getText();
+//		for(IOutlineNode child: node.getChildren()) {
+//			traverseNode(child);
+//		}
+//	}
+//
+//	@Override
+//	protected XtextResourceSet getResourceSet() {
+//		ResourceSet resourceSet = testHelper.getResourceSet();
+//		return (XtextResourceSet) resourceSet;
+//	}
 	
 }
