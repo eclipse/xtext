9b1bddfc6de94ced36409cb360bb895670ec0df9
Jan Koehnlein <jan.koehnlein@itemis.de>
2011-10-28 12:19:30 +0200

    [docs] Used latest Xdoc version to fix TOC Fixed levels in Xbase reference docs https://bugs.eclipse.org/bugs/show_bug.cgi?id=356081

--- plugins/org.eclipse.xtext.doc/website/035-domainmodel-java.php
+++ plugins/org.eclipse.xtext.doc/website/035-domainmodel-java.php
@@ -1,6 +1,6 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	include("include.php");
-	$pageTitle 		= "5 simple steps to your JVM language";
+	$pageTitle 		= "Five simple steps to your JVM language";
 	
 	$html = file_get_contents('035-domainmodel-java.html');
 
