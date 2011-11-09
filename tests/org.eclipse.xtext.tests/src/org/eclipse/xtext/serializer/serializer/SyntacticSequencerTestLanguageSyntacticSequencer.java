c6e9930e7a182b8518e970801cbaad3b844b88c3
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2011-11-09 13:03:32 +0100

    [serializer] fixed bug 362581
    Syntactic sequencer extension for serialization
    of optional keywords doesn't work

--- tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/serializer/serializer/SyntacticSequencerTestLanguageSyntacticSequencer.java
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/serializer/serializer/SyntacticSequencerTestLanguageSyntacticSequencer.java
@@ -1,4 +1,70 @@
 package org.eclipse.xtext.serializer.serializer;
 
-public class SyntacticSequencerTestLanguageSyntacticSequencer extends AbstractSyntacticSequencerTestLanguageSyntacticSequencer {
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.IGrammarAccess;
+import org.eclipse.xtext.RuleCall;
+import org.eclipse.xtext.nodemodel.ICompositeNode;
+import org.eclipse.xtext.nodemodel.INode;
+import org.eclipse.xtext.serializer.analysis.ISyntacticSequencerPDAProvider.ISynNavigable;
+import org.eclipse.xtext.serializer.services.SyntacticSequencerTestLanguageGrammarAccess;
+
+import com.google.inject.Inject;
+
+public class SyntacticSequencerTestLanguageSyntacticSequencer extends
+		AbstractSyntacticSequencerTestLanguageSyntacticSequencer {
+
+	private SyntacticSequencerTestLanguageGrammarAccess ga;
+
+	@Inject
+	protected void asetGa(IGrammarAccess ga) {
+		this.ga = (SyntacticSequencerTestLanguageGrammarAccess) ga;
+	}
+
+	@Override
+	protected String getKW1Token(EObject semanticObject, RuleCall ruleCall, INode node) {
+		return "matched 1";
+	}
+
+	/**
+	 * Syntax: 'kw2' | KW1
+	 */
+	@Override
+	protected void emit_AlternativeTransition_KW1ParserRuleCall_1_0_or_Kw2Keyword_1_1(EObject semanticObject,
+			ISynNavigable transition, List<INode> nodes) {
+		ICompositeNode node = (ICompositeNode) nodes.get(0);
+		acceptUnassignedDatatype(ga.getAlternativeTransitionAccess().getKW1ParserRuleCall_1_0(), "matched 5", node);
+	}
+
+	/**
+	 * Syntax: KW1+
+	 */
+	@Override
+	protected void emit_MandatoryManyTransition_KW1ParserRuleCall_1_p(EObject semanticObject, ISynNavigable transition,
+			List<INode> nodes) {
+		ICompositeNode node = (ICompositeNode) nodes.get(0);
+		acceptUnassignedDatatype(ga.getMandatoryManyTransitionAccess().getKW1ParserRuleCall_1(), "matched 4", node);
+	}
+
+	/**
+	 * Syntax: KW1*
+	 */
+	@Override
+	protected void emit_OptionalManyTransition_KW1ParserRuleCall_1_a(EObject semanticObject, ISynNavigable transition,
+			List<INode> nodes) {
+		ICompositeNode node = (ICompositeNode) nodes.get(0);
+		acceptUnassignedDatatype(ga.getOptionalManyTransitionAccess().getKW1ParserRuleCall_1(), "matched 3", node);
+	}
+
+	/**
+	 * Syntax: KW1?
+	 */
+	@Override
+	protected void emit_OptionalSingleTransition_KW1ParserRuleCall_1_q(EObject semanticObject,
+			ISynNavigable transition, List<INode> nodes) {
+		ICompositeNode node = (ICompositeNode) nodes.get(0);
+		acceptUnassignedDatatype(ga.getOptionalSingleTransitionAccess().getKW1ParserRuleCall_1(), "matched 2", node);
+	}
+
 }
