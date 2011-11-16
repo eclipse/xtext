c842ea329c9fea8806774af506f909e06a712a6b
Jan Koehnlein <jan.koehnlein@itemis.de>
2011-11-16 17:58:51 +0100

    [xtend][quickfix] Allow surround with try/catch in functions returning rich strings

--- plugins/org.eclipse.xtext.xtend2.ui/src/org/eclipse/xtext/xtend2/ui/quickfix/Xtend2QuickfixProvider.java
+++ plugins/org.eclipse.xtext.xtend2.ui/src/org/eclipse/xtext/xtend2/ui/quickfix/Xtend2QuickfixProvider.java
@@ -62,6 +62,7 @@ import org.eclipse.xtext.xtend2.formatting.OverrideFunction;
 import org.eclipse.xtext.xtend2.services.Xtend2GrammarAccess;
 import org.eclipse.xtext.xtend2.ui.edit.OrganizeImportsHandler;
 import org.eclipse.xtext.xtend2.validation.IssueCodes;
+import org.eclipse.xtext.xtend2.xtend2.RichString;
 import org.eclipse.xtext.xtend2.xtend2.XtendClass;
 import org.eclipse.xtext.xtend2.xtend2.XtendFile;
 import org.eclipse.xtext.xtend2.xtend2.XtendFunction;
@@ -413,7 +414,7 @@ public class Xtend2QuickfixProvider extends DefaultQuickfixProvider {
 							if (exception instanceof JvmType) {
 								JvmType exceptionType = (JvmType) exception;
 								EObject childThrowingException = xtextResource.getResourceSet().getEObject(childURI, true);
-								XExpression toBeSurrounded = findContainerExpressionInBlock(childThrowingException);
+								XExpression toBeSurrounded = findContainerExpressionInBlockOrRichString(childThrowingException);
 								IXtextDocument xtextDocument = context.getXtextDocument();
 								if(toBeSurrounded != null) {
 									ICompositeNode toBeSurroundedNode = NodeModelUtils.getNode(toBeSurrounded);
@@ -436,13 +437,14 @@ public class Xtend2QuickfixProvider extends DefaultQuickfixProvider {
 	}
 	
 	
-	protected XExpression findContainerExpressionInBlock(EObject expr) {
+	protected XExpression findContainerExpressionInBlockOrRichString(EObject expr) {
 		if(expr==null)
 			return null;
-		if(expr.eContainer() instanceof XBlockExpression)
+		EObject container = expr.eContainer();
+		if(container instanceof XBlockExpression || container instanceof RichString)
 			return (XExpression) expr;
 		else 
-			return findContainerExpressionInBlock(expr.eContainer());
+			return findContainerExpressionInBlockOrRichString(container);
 	}
 
 	protected String getIndentationAtOffset(int offset, IDocument document) throws BadLocationException {
