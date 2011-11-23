378d9112e5a5573ab363a2db6962f5860d7f8f71
Holger Schill <Holger.Schill@itemis.de>
2011-11-23 14:46:28 +0100

    [Tests] Filtering of issues to see only errors. Reverted fixed tests with warnings but no errors.

--- tests/org.eclipse.xtext.xtend2.tests/src/org/eclipse/xtext/xtend2/tests/AbstractXtend2TestCase.java
+++ tests/org.eclipse.xtext.xtend2.tests/src/org/eclipse/xtext/xtend2/tests/AbstractXtend2TestCase.java
@@ -10,6 +10,7 @@ import junit.framework.TestCase;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.diagnostics.Severity;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.resource.XtextResourceSet;
 import org.eclipse.xtext.util.CancelIndicator;
@@ -25,6 +26,9 @@ import org.eclipse.xtext.xtend2.xtend2.XtendConstructor;
 import org.eclipse.xtext.xtend2.xtend2.XtendFile;
 import org.eclipse.xtext.xtend2.xtend2.XtendFunction;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
@@ -95,8 +99,12 @@ public abstract class AbstractXtend2TestCase extends TestCase {
 		resource.load(new StringInputStream(string), null);
 		assertEquals(resource.getErrors().toString(), 0, resource.getErrors().size());
 		if (validate) {
-			List<Issue> issues = ((XtextResource) resource).getResourceServiceProvider().getResourceValidator()
-					.validate(resource, CheckMode.ALL, CancelIndicator.NullImpl);
+			List<Issue> issues = Lists.newArrayList(Iterables.filter(((XtextResource) resource).getResourceServiceProvider().getResourceValidator()
+					.validate(resource, CheckMode.ALL, CancelIndicator.NullImpl), new Predicate<Issue>() {
+						public boolean apply(Issue issue) {
+							return issue.getSeverity() == Severity.ERROR;
+						}
+					}));
 			assertTrue("Resource contained errors : " + issues.toString(), issues.isEmpty());
 		}
 		XtendFile file = (XtendFile) resource.getContents().get(0);
