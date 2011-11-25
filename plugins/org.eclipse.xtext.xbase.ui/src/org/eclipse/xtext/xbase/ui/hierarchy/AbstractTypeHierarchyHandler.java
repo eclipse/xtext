511551ba46f0ebde34eef0abad5ec7432c8f171b
Holger Schill <Holger.Schill@itemis.de>
2011-11-25 13:36:43 +0100

    [Xbase] fixed some spelling mistakes in type hierarchy feature

--- plugins/org.eclipse.xtext.xbase.ui/src/org/eclipse/xtext/xbase/ui/hierarchy/AbstractTypeHierarchyHandler.java
+++ plugins/org.eclipse.xtext.xbase.ui/src/org/eclipse/xtext/xbase/ui/hierarchy/AbstractTypeHierarchyHandler.java
@@ -5,7 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-package org.eclipse.xtext.xbase.ui.hierachy;
+package org.eclipse.xtext.xbase.ui.hierarchy;
 
 import java.util.Set;
 
