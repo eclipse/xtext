e9e2db891f955515bad571180d7adc92c31ba028
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2011-11-28 14:19:16 +0100

    [Xtext] fixed 363914 - Check that you cannot append a null segment to a QualifiedName

--- tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/naming/QualifiedNameTest.java
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/naming/QualifiedNameTest.java
@@ -25,6 +25,13 @@ public class QualifiedNameTest extends TestCase {
 			fail("Exception expected");
 		} catch(IllegalArgumentException e) {}
 	}
+	
+	public void testAppendNull() {
+		try {
+			QualifiedName.create().append((String) null);
+			fail("Exception expected");
+		} catch (IllegalArgumentException e) {}
+	}
 
 	public void testSegments() {
 		QualifiedName qn = QualifiedName.create("foo", "bar", "baz");
