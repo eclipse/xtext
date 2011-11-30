3fb0898e2cde4e76c013b53b99cf9eddfb4426a1
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2011-11-30 16:49:03 +0100

    [xtext][ui] Attempt to fix the dangling error annotation problem in Xtext editors
    see https://bugs.eclipse.org/bugs/show_bug.cgi?id=364911

--- plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/XtextSourceViewer.java
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/XtextSourceViewer.java
@@ -8,9 +8,16 @@
 package org.eclipse.xtext.ui.editor;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.Assert;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.DocumentEvent;
+import org.eclipse.jface.text.ITextListener;
 import org.eclipse.jface.text.ITextPresentationListener;
+import org.eclipse.jface.text.SlaveDocumentEvent;
+import org.eclipse.jface.text.TextEvent;
 import org.eclipse.jface.text.contentassist.IContentAssistant;
 import org.eclipse.jface.text.source.IOverviewRuler;
 import org.eclipse.jface.text.source.IVerticalRuler;
@@ -23,6 +30,8 @@ import com.google.inject.ImplementedBy;
  * @author Sebastian Zarnekow - Initial contribution and API
  */
 public class XtextSourceViewer extends ProjectionViewer {
+	
+	private static final Logger log = Logger.getLogger(XtextSourceViewer.class);
 
 	@ImplementedBy(DefaultFactory.class)
 	public interface Factory {
@@ -56,6 +65,49 @@ public class XtextSourceViewer extends ProjectionViewer {
 
 		fTextPresentationListeners.remove(listener);
 		fTextPresentationListeners.add(0, listener);
+	}
+	
+	private int lengthDiff = Integer.MIN_VALUE;
+	
+	/**
+	 * Informs all registered text listeners about the change specified by the
+	 * widget command. This method does not use a robust iterator.
+	 *
+	 * @param cmd the widget command translated into a text event sent to all text listeners
+	 */
+	@Override
+	protected void updateTextListeners(WidgetCommand cmd) {
+		@SuppressWarnings("unchecked")
+		List<ITextListener> textListeners= fTextListeners;
+		if (textListeners != null) {
+			textListeners= new ArrayList<ITextListener>(textListeners);
+			DocumentEvent event= cmd.event;
+			if (event instanceof SlaveDocumentEvent)
+				event= ((SlaveDocumentEvent) event).getMasterEvent();
+			int usedDiff = 0;
+			if (event == null) {
+				if (lengthDiff > 0)
+					usedDiff = lengthDiff;
+				lengthDiff = Integer.MIN_VALUE; 
+			} else {
+				lengthDiff = event.fText.length() - event.fLength;
+			}
+			int length = cmd.length + usedDiff;
+			String text = cmd.text;
+			if (usedDiff != 0) {
+				try {
+					text = getDocument().get(cmd.start, length);
+				} catch(BadLocationException e) {
+					length = cmd.length;
+					log.debug("Ignored BadLocationException when fixing document events", e);
+				}
+			}
+			TextEvent e= new TextEvent(cmd.start, length, text, cmd.preservedText, event, redraws()) {};
+			for (int i= 0; i < textListeners.size(); i++) {
+				ITextListener l= textListeners.get(i);
+				l.textChanged(e);
+			}
+		}
 	}
 	
 	public IContentAssistant getContentAssistant() {
