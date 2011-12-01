9d6f9f8f39218e46fef68894efbc6f49011caa11
Holger Schill <Holger.Schill@itemis.de>
2011-12-01 11:34:24 +0100

    [Xtend] Removed validation for Exceptions regarding the subtype check see https://bugs.eclipse.org/bugs/show_bug.cgi?id=365174

--- plugins/org.eclipse.xtext.xtend2/src/org/eclipse/xtext/xtend2/validation/IssueCodes.java
+++ plugins/org.eclipse.xtext.xtend2/src/org/eclipse/xtext/xtend2/validation/IssueCodes.java
@@ -57,7 +57,6 @@ public final class IssueCodes {
 	public static final String FUNCTION_LOCALLY_NEVER_USED = "function_locally_never_used";
 	public static final String EXCEPTION_NOT_THROWABLE = "exception_not_throwable";
 	public static final String EXCEPTION_DECLARED_TWICE = "exception_declared_twice";
-	public static final String EXCEPTION_SUPTYPE_OF_DECLARED_EXCEPTION = "exception_is_subtype_of_declared_exception";
 	
 	public static final String MISSING_CONSTRUCTOR = ISSUE_CODE_PREFIX + "missing_constructor";
 	public static final String MUST_INVOKE_SUPER_CONSTRUCTOR = ISSUE_CODE_PREFIX + "must_invoke_super_constructor";
