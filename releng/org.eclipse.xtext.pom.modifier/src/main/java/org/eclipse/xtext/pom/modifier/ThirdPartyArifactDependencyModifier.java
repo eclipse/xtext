dc6ba4eb8223b69176b5654aebfec55fcfee69dc
Dennis Huebner <dennis.huebner@itemis.de>
2011-12-02 14:03:56 +0100

    [mvn] we will deploy our own antlr-runtime

--- releng/org.eclipse.xtext.pom.modifier/src/main/java/org/eclipse/xtext/pom/modifier/ThirdPartyArifactDependencyModifier.java
+++ releng/org.eclipse.xtext.pom.modifier/src/main/java/org/eclipse/xtext/pom/modifier/ThirdPartyArifactDependencyModifier.java
@@ -21,7 +21,6 @@ public class ThirdPartyArifactDependencyModifier extends
 	static {
 		R_MAP.put("com.google.guava", new String[] { "guava" });
 		R_MAP.put("com.google.inject", new String[] { "guice" });
-		R_MAP.put("org.antlr.runtime", new String[] { "antlr-runtime", "org.antlr" });
 		R_MAP.put("org.apache.log4j", new String[] { "log4j", "log4j", "1.2.15" });
 		R_MAP.put("org.apache.commons.cli", new String[] { "commons-cli", "commons-cli", "1.2" });
 		R_MAP.put("org.apache.commons.lang", new String[] { "commons-lang", "commons-lang", "2.4" });
