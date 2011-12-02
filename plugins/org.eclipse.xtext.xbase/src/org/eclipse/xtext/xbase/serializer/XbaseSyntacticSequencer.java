94258849ff6762c7bdda91367919b7d00d96fec6
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2011-12-02 20:18:17 +0100

    [xbase/serializer] put a semicolon before unary operator expressions

--- plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/serializer/XbaseSyntacticSequencer.java
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/serializer/XbaseSyntacticSequencer.java
@@ -4,15 +4,28 @@ import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.Keyword;
+import org.eclipse.xtext.RuleCall;
+import org.eclipse.xtext.nodemodel.ICompositeNode;
+import org.eclipse.xtext.nodemodel.ILeafNode;
 import org.eclipse.xtext.nodemodel.INode;
 import org.eclipse.xtext.serializer.analysis.ISyntacticSequencerPDAProvider.ISynNavigable;
+import org.eclipse.xtext.xbase.XBinaryOperation;
 import org.eclipse.xtext.xbase.XBlockExpression;
 import org.eclipse.xtext.xbase.XExpression;
 import org.eclipse.xtext.xbase.XForLoopExpression;
 import org.eclipse.xtext.xbase.XIfExpression;
+import org.eclipse.xtext.xbase.XUnaryOperation;
+import org.eclipse.xtext.xbase.services.XbaseGrammarAccess;
+
+import com.google.inject.Inject;
 
 @SuppressWarnings("restriction")
 public class XbaseSyntacticSequencer extends AbstractXbaseSyntacticSequencer {
+	
+	@Inject
+	protected XbaseGrammarAccess grammarAccess;
+	
+	boolean semicolonBeforeNextExpressionRequired = false;
 	
 	/**
 	 * Syntax: '('*
@@ -33,6 +46,37 @@ public class XbaseSyntacticSequencer extends AbstractXbaseSyntacticSequencer {
 		}
 
 		acceptNodes(transition, nodes);
+	}
+	
+	protected boolean startsWithUnaryOperator(EObject obj) {
+		if(obj instanceof XUnaryOperation)
+			return true;
+		if(obj instanceof XBinaryOperation)
+			return startsWithUnaryOperator(((XBinaryOperation)obj).getLeftOperand());
+		return false;
+	}
+	
+	@Override
+	public boolean enterAssignedParserRuleCall(RuleCall rc, EObject semanticChild, ICompositeNode node) {
+		if (rc == grammarAccess.getXBlockExpressionAccess().getExpressionsXExpressionInsideBlockParserRuleCall_2_0_0()) {
+			semicolonBeforeNextExpressionRequired = startsWithUnaryOperator(semanticChild);
+		} else
+			semicolonBeforeNextExpressionRequired = false;
+		return super.enterAssignedParserRuleCall(rc, semanticChild, node);
+	}
+	
+	/**
+	 * Syntax:
+	 *     ';'?
+	 */
+	@Override
+	protected void emit_XBlockExpression_SemicolonKeyword_2_1_q(EObject semanticObject, ISynNavigable transition, List<INode> nodes) {
+		if (semicolonBeforeNextExpressionRequired) {
+			ILeafNode node = nodes != null && nodes.size() == 1 && nodes.get(0) instanceof ILeafNode ? (ILeafNode) nodes.get(0) : null;
+			Keyword kw = grammarAccess.getXBlockExpressionAccess().getSemicolonKeyword_2_1();
+			acceptUnassignedKeyword(kw, kw.getValue(), node);
+		} else
+			acceptNodes(transition, nodes);
 	}
 	
 }
