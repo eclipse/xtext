400f486cce4d6635fc5e0de71f23c1f3e6f8d9af
Michael Clay <clay@codeworkz.at>
2011-12-04 21:34:17 +0100

    [xtend2.maven] re-enabled tests, fix empty package issue, make build continue on warnings and abort on jdt or xtend validation errors, add integration tests for builds with errors and warnings

--- plugins/org.eclipse.xtext.xtend2.maven/src/test/java/org/eclipse/xtext/xtend2/maven/Xtend2BatchCompilerPluginIT.java
+++ plugins/org.eclipse.xtext.xtend2.maven/src/test/java/org/eclipse/xtext/xtend2/maven/Xtend2BatchCompilerPluginIT.java
@@ -3,31 +3,65 @@ package org.eclipse.xtext.xtend2.maven;
 import java.io.File;
 import java.io.IOException;
 
+import junit.framework.Assert;
+
 import org.apache.maven.it.VerificationException;
 import org.apache.maven.it.Verifier;
 import org.apache.maven.it.util.ResourceExtractor;
 import org.junit.Test;
 
 public class Xtend2BatchCompilerPluginIT {
-//	
-//	@Test
-//	public void simpleProject() throws Exception {
-//		verifyErrorFreeLog("/it/simple");
-//	}
-//
-//	@Test
-//	public void pluginPrefix() throws Exception {
-//		verifyErrorFreeLog("/it/simple", "xtend2:compile");
-//	}
-//
-//	@Test
-//	public void simpleProjectXtend2Tests() throws Exception {
-//		verifyErrorFreeLog("/it/withtestsrc");
-//	}
+
+	@Test
+	public void simpleProject() throws Exception {
+		verifyErrorFreeLog("/it/simple");
+	}
+
+	@Test
+	public void pluginPrefix() throws Exception {
+		verifyErrorFreeLog("/it/simple", "xtend2:compile");
+	}
+
+	@Test
+	public void simpleProjectXtend2Tests() throws Exception {
+		verifyErrorFreeLog("/it/withtestsrc");
+	}
 
 	@Test
 	public void aggregation() throws Exception {
 		verifyErrorFreeLog("/it/aggregation");
+	}
+
+	@Test
+	public void haltOnErrorsFromJdtCompiler() throws Exception {
+		Verifier verifier = newVerifier("/it/jdtcerrors");
+		try {
+			verifier.executeGoal("verify");
+			Assert.fail("expected org.apache.maven.plugin.MojoExecutionException");
+		} catch (Exception e) {
+			verifier.verifyTextInLog("2: Couldn't resolve reference to JvmType 'JavaB'.");
+			verifier.verifyTextInLog("BUILD FAILURE");
+		}
+	}
+
+	@Test
+	public void haltOnXtendValidationErrors() throws Exception {
+		Verifier verifier = newVerifier("/it/xtenderrors");
+		try {
+			verifier.executeGoal("verify");
+			Assert.fail("expected org.apache.maven.plugin.MojoExecutionException");
+		} catch (Exception e) {
+			verifier.verifyTextInLog("3: Superclass must be a class");
+			verifier.verifyTextInLog("BUILD FAILURE");
+		}
+	}
+
+	@Test
+	public void continueOnXtendWarnings() throws Exception {
+		Verifier verifier = newVerifier("/it/xtendwarnings");
+		verifier.executeGoal("verify");
+		verifier.verifyTextInLog("3: The import 'java.util.Collections' is never used.");
+		verifier.verifyTextInLog("[INFO] BUILD SUCCESS");
 	}
 
 	private void verifyErrorFreeLog(String pathToTestProject) throws IOException, VerificationException {
@@ -35,11 +69,15 @@ public class Xtend2BatchCompilerPluginIT {
 	}
 
 	private void verifyErrorFreeLog(String pathToTestProject, String goal) throws IOException, VerificationException {
-		File testDir = ResourceExtractor.simpleExtractResources(getClass(), pathToTestProject);
-		Verifier verifier = new Verifier(testDir.getAbsolutePath());
+		Verifier verifier = newVerifier(pathToTestProject);
 		verifier.executeGoal(goal);
 		verifier.verifyErrorFreeLog();
 		verifier.setDebug(true);
 		verifier.resetStreams();
+	}
+
+	private Verifier newVerifier(String pathToTestProject) throws IOException, VerificationException {
+		File testDir = ResourceExtractor.simpleExtractResources(getClass(), pathToTestProject);
+		return new Verifier(testDir.getAbsolutePath());
 	}
 }
---
+++ plugins/org.eclipse.xtext.xtend2.maven/src/test/resources/it/jdtcerrors/src/main/java/test/XtendA.xtend
@@ -0,0 +1,4 @@
+
+class XtendA extends JavaB {
+	
+}
