57303e412a98ca870e3af858d30ccb92637971e5
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2012-01-09 14:34:54 +0100

    [tests] Migration to JUnit 4.x - some remaining clean-up

--- tests/org.eclipse.xtext.junit4.tests/src/org/eclipse/xtext/junit4/parameterized/XpectParameterProviderTest.java
+++ tests/org.eclipse.xtext.junit4.tests/src/org/eclipse/xtext/junit4/parameterized/XpectParameterProviderTest.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import junit.framework.Assert;
+import org.junit.Assert;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.xtext.Grammar;
--- tests/org.eclipse.xtext.xtend2.ui.tests/src-longrunning/org/eclipse/xtext/xtend2/ui/tests/autoedit/AutoEditInFirstMethodTest.java
+++ tests/org.eclipse.xtext.xtend2.ui.tests/src-longrunning/org/eclipse/xtext/xtend2/ui/tests/autoedit/AutoEditInFirstMethodTest.java
@@ -1,71 +1,61 @@
-///*******************************************************************************
-// * Copyright (c) 2011 itemis AG (http://www.itemis.eu) and others.
-// * All rights reserved. This program and the accompanying materials
-// * are made available under the terms of the Eclipse Public License v1.0
-// * which accompanies this distribution, and is available at
-// * http://www.eclipse.org/legal/epl-v10.html
-// *******************************************************************************/
-//package org.eclipse.xtext.xtend2.ui.tests.autoedit;
-//
-//import junit.framework.Test;
-//
-//import org.eclipse.xtext.ui.editor.XtextEditor;
-//
-///**
-// * @author Sebastian Zarnekow - Initial contribution and API
-// */
-//public class AutoEditInFirstMethodTest extends AutoEditTest {
-//
-//	public static Test suite() {
-//		return suite(AutoEditInFirstMethodTest.class);
-//	}
-//	
-//	private static String PREFIX = "class Foo {\n\tfoo()\n";
-//	
-//	private static String SUFFIX = "\n\tbar() '''} zonk'''\n}";
-//	
-//	@Override
-//	protected XtextEditor openEditor(String string) throws Exception {
-//		return super.openEditor(PREFIX + string + SUFFIX);
-//	}
-//	
-//	@Override
-//	protected void assertState(String string, XtextEditor editor) {
-//		super.assertState(PREFIX + string + SUFFIX, editor);
-//	}
-//	
-//	/**
-//	 * FIXME: The test should pass as defined in the base class 
-//	 */
-//	@Override
-//	@Test public void testCurlyBracesBlockInRichString_1() throws Exception {
-//		XtextEditor editor = openEditor("'''|'''");
-//		pressKey(editor, '{');
-//		assertState("'''{|'''", editor);
-//		pressKey(editor, '\n');
-//		assertState("'''{\n\t|'''", editor);
-//		pressKey(editor, '\n');
-//		assertState("'''{\n\t\n\t|'''", editor);
-//	}
-//	
-//	/**
-//	 * FIXME: The test should pass as defined in the base class 
-//	 */
-//	@Override
-//	@Test public void testCurlyBracesBlockInRichString_4() throws Exception {
-//		XtextEditor editor = openEditor("'''foo {|'''");
-//		pressKey(editor, '\n');
-//		assertState("'''foo {\n\t|'''", editor);
-//	}
-//	
-//	/**
-//	 * FIXME: The test should pass as defined in the base class 
-//	 */
-//	@Override
-//	@Test public void testCurlyBracesBlockInRichString_17() throws Exception {
-//		XtextEditor editor = openEditor("'''{\n|'''");
-//		pressKey(editor, '\n');
-//		assertState("'''{\n\n|'''", editor);
-//	}
-//	
-//}
+/*******************************************************************************
+ * Copyright (c) 2011 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.xtend2.ui.tests.autoedit;
+
+import org.eclipse.xtext.ui.editor.XtextEditor;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author Sebastian Zarnekow - Initial contribution and API
+ */
+@SuppressWarnings("restriction")
+public class AutoEditInFirstMethodTest extends AutoEditTest {
+
+	private static String PREFIX = "class Foo {\n\tfoo()\n";
+	
+	private static String SUFFIX = "\n\tbar() '''} zonk'''\n}";
+	
+	@Override
+	protected XtextEditor openEditor(String string) throws Exception {
+		return super.openEditor(PREFIX + string + SUFFIX);
+	}
+	
+	@Override
+	protected void assertState(String string, XtextEditor editor) {
+		super.assertState(PREFIX + string + SUFFIX, editor);
+	}
+	
+	/**
+	 * FIXME: The test should pass as defined in the base class 
+	 */
+	@Override
+	@Ignore("FIXME: The test should pass as defined in the base class")
+	@Test public void testCurlyBracesBlockInRichString_1() throws Exception {
+		super.testCurlyBracesBlockInRichString_1();
+	}
+	
+	/**
+	 * FIXME: The test should pass as defined in the base class 
+	 */
+	@Override
+	@Ignore("FIXME: The test should pass as defined in the base class")
+	@Test public void testCurlyBracesBlockInRichString_4() throws Exception {
+		super.testCurlyBracesBlockInRichString_4();
+	}
+	
+	/**
+	 * FIXME: The test should pass as defined in the base class 
+	 */
+	@Override
+	@Ignore("FIXME: The test should pass as defined in the base class")
+	@Test public void testCurlyBracesBlockInRichString_17() throws Exception {
+		super.testCurlyBracesBlockInRichString_17();
+	}
+	
+}
