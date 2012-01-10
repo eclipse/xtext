ebc9f4e932d72467e67c2226868aea59e6a86798
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2012-01-10 15:14:05 +0100

    [navigation][java] Support for NewSearchUI with reusable editors

--- plugins/org.eclipse.xtext.common.types.shared.jdt38/src/org/eclipse/xtext/common/types/shared/jdt38/OriginalJavaElementOpener.java
+++ plugins/org.eclipse.xtext.common.types.shared.jdt38/src/org/eclipse/xtext/common/types/shared/jdt38/OriginalJavaElementOpener.java
@@ -9,18 +9,25 @@ package org.eclipse.xtext.common.types.shared.jdt38;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IStorage;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IMember;
 import org.eclipse.jdt.ui.IJavaElementEditorOpener;
+import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.xtext.common.types.ui.navigation.IDerivedMemberAwareEditorOpener;
 import org.eclipse.xtext.generator.IDerivedResourceMarkers;
 import org.eclipse.xtext.resource.IResourceServiceProvider;
 import org.eclipse.xtext.ui.editor.XtextEditor;
+import org.eclipse.xtext.ui.editor.XtextReadonlyEditorInput;
+import org.eclipse.xtext.ui.resource.IStorage2UriMapper;
+import org.eclipse.xtext.util.Pair;
 
 import com.google.inject.Inject;
 
@@ -35,7 +42,10 @@ public class OriginalJavaElementOpener implements IJavaElementEditorOpener {
 	@Inject
 	private IDerivedResourceMarkers derivedResourceMarkers;
 	
-	public static class EditorHandle implements IJavaElementEditorOpener.IOpenableEditor, IRevealable {
+	@Inject
+	private IStorage2UriMapper storage2UriMapper;
+	
+	public static class EditorHandle implements IJavaElementEditorOpener.IOpenableEditor, IJavaElementEditorOpener.IRevealable {
 
 		private final IDerivedMemberAwareEditorOpener opener;
 		private final URI sourceResourceURI;
@@ -101,16 +111,56 @@ public class OriginalJavaElementOpener implements IJavaElementEditorOpener {
 		return null;
 	}
 
-	public IRevealable revealInEditor(IEditorPart part, IJavaElement element) {
+	public IJavaElementEditorOpener.IRevealable revealInEditor(IEditorPart part, IJavaElement element) {
 		if (part instanceof XtextEditor) {
 			return tryCreateHandle(element, true);
 		}
 		return null;
 	}
 	
-	public IRevealable revealInEditor(IEditorPart part, int offset,
+	public IJavaElementEditorOpener.IRevealable revealInEditor(IEditorPart part, int offset,
 			int length) {
 		// TODO implement as soon as trace information is available.
+		return null;
+	}
+
+	public IEditorInput getEditorInputForReusableEditorInSearchResult(
+			IJavaElement element) {
+		if (element instanceof IMember) {
+			try {
+				// TODO stay in same editor if local navigation
+				IResource resource = element.getResource();
+				if (resource instanceof IFile) {
+					IMarker[] markers = derivedResourceMarkers.findDerivedResourceMarkers((IFile) resource);
+					if (markers.length != 1)
+						return null;
+					IMarker marker = markers[0];
+					String source = derivedResourceMarkers.getSource(marker);
+					if (source != null) {
+						URI sourceResourceURI = URI.createURI(source).trimFragment();
+						Iterable<Pair<IStorage, IProject>> storages = storage2UriMapper.getStorages(sourceResourceURI);
+						for(Pair<IStorage, IProject> storage: storages) {
+							if (storage.getSecond().equals(resource.getProject())) {
+								IStorage originalStorage = storage.getFirst();
+								if (originalStorage instanceof IFile) {
+									return new FileEditorInput((IFile) originalStorage);
+								} else {
+									return new XtextReadonlyEditorInput(originalStorage);
+								}
+							}
+						}
+						for(Pair<IStorage, IProject> storage: storages) {
+							IStorage originalStorage = storage.getFirst();
+							if (originalStorage instanceof IFile) {
+								return new FileEditorInput((IFile) originalStorage);
+							}
+						}
+					}
+				}
+			} catch(CoreException e) { 
+				// ignore
+			}
+		}
 		return null;
 	}
 }
