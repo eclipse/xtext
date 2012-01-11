8b519a5bb095a840be56763fd624e22752cafa70
dhubner <dennis.huebner@itemis.de>
2012-01-11 13:57:23 +0100

    [tests] organize imports

--- tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/xtext/ResourceLoadTest.java
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/xtext/ResourceLoadTest.java
@@ -25,8 +25,6 @@ import org.eclipse.xtext.junit4.AbstractXtextTests;
 import org.eclipse.xtext.nodemodel.ICompositeNode;
 import org.eclipse.xtext.nodemodel.INode;
 import org.eclipse.xtext.resource.XtextResource;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
