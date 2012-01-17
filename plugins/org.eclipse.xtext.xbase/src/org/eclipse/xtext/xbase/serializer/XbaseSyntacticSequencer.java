89264abab7b003ab1f9797fc3eb0d8f65a97f155
Sven Efftinge <sven.efftinge@itemis.de>
2012-01-17 11:35:54 +0100

    [Xbase] added comments and removed an unnecessary field declaration.

--- plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/serializer/XbaseSyntacticSequencer.java
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/serializer/XbaseSyntacticSequencer.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2011 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package org.eclipse.xtext.xbase.serializer;
 
 import java.util.List;
@@ -15,15 +22,9 @@ import org.eclipse.xtext.xbase.XExpression;
 import org.eclipse.xtext.xbase.XForLoopExpression;
 import org.eclipse.xtext.xbase.XIfExpression;
 import org.eclipse.xtext.xbase.XUnaryOperation;
-import org.eclipse.xtext.xbase.services.XbaseGrammarAccess;
-
-import com.google.inject.Inject;
 
 @SuppressWarnings("restriction")
 public class XbaseSyntacticSequencer extends AbstractXbaseSyntacticSequencer {
-	
-	@Inject
-	protected XbaseGrammarAccess grammarAccess;
 	
 	boolean semicolonBeforeNextExpressionRequired = false;
 	
