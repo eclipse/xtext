a29d5c5f15ca8cf1a10eee3447c56fc7a7dd6fc5
Sven Efftinge <sven.efftinge@itemis.de>
2012-01-31 14:14:52 +0100

    integrated standalone projects into core

---
+++ tests/org.eclipse.xtend.core.tests/src/org/eclipse/xtend/core/tests/compiler/batch/XtendInjectorProvider.java
@@ -0,0 +1,36 @@
+package org.eclipse.xtend.core.tests.compiler.batch;
+
+import org.eclipse.xtend.core.XtendStandaloneSetup;
+import org.eclipse.xtext.junit4.GlobalRegistries;
+import org.eclipse.xtext.junit4.GlobalRegistries.GlobalStateMemento;
+import org.eclipse.xtext.junit4.IInjectorProvider;
+import org.eclipse.xtext.junit4.IRegistryConfigurator;
+
+import com.google.inject.Injector;
+
+public class XtendInjectorProvider implements IInjectorProvider, IRegistryConfigurator {
+	protected GlobalStateMemento globalStateMemento;
+	protected Injector injector;
+
+	static {
+		GlobalRegistries.initializeDefaults();
+	}
+	
+	public Injector getInjector() {
+		if (injector == null) {
+			this.injector = new XtendStandaloneSetup().createInjectorAndDoEMFRegistration();
+		}
+		return injector;
+	}
+	
+	public void restoreRegistry() {
+		globalStateMemento.restoreGlobalState();
+	}
+
+	public void setupRegistry() {
+		globalStateMemento = GlobalRegistries.makeCopyOfGlobalState();
+		if (injector != null)
+			new XtendStandaloneSetup().register(injector);
+	}
+	
+}
