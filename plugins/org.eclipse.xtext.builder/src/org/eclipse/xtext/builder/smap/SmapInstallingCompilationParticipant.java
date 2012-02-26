c088c781811ac9eb3ed5b7edf8edac17ac062a36
Sven Efftinge <sven.efftinge@itemis.de>
2012-02-26 21:42:02 +0100

    moved the trace to snap logic to Xtext core, such that it can be reused without any workspace model dependencies.

--- plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/smap/SmapInstallingCompilationParticipant.java
+++ plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/smap/SmapInstallingCompilationParticipant.java
@@ -1,30 +1,20 @@
 package org.eclipse.xtext.builder.smap;
 
-import static com.google.common.collect.Maps.*;
-
-import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.Map;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.emf.common.util.URI;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.ITypeRoot;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.compiler.BuildContext;
 import org.eclipse.jdt.core.compiler.CompilationParticipant;
 import org.eclipse.jdt.internal.core.Region;
-import org.eclipse.xtext.LanguageInfo;
 import org.eclipse.xtext.generator.trace.AbstractTraceRegion;
-import org.eclipse.xtext.generator.trace.ILocationData;
 import org.eclipse.xtext.generator.trace.ITrace;
 import org.eclipse.xtext.generator.trace.ITraceInformation;
+import org.eclipse.xtext.generator.trace.SmapSupport;
 import org.eclipse.xtext.resource.IResourceServiceProvider;
-import org.eclipse.xtext.smap.SDEInstaller;
-import org.eclipse.xtext.smap.SmapGenerator;
-import org.eclipse.xtext.smap.SmapStratum;
-import org.eclipse.xtext.util.Files;
 
 import com.google.inject.Inject;
 
@@ -34,6 +24,8 @@ public class SmapInstallingCompilationParticipant extends CompilationParticipant
 	private ITraceInformation traceInformation;
 	@Inject
 	private IResourceServiceProvider.Registry serviceProviderRegistry;
+	
+	@Inject private SmapSupport smapSupport;
 
 	@Override
 	public boolean isActive(IJavaProject project) {
@@ -62,15 +54,7 @@ public class SmapInstallingCompilationParticipant extends CompilationParticipant
 				final AbstractTraceRegion rootTraceRegion = ((ITrace.Internal) information).getRootTraceRegion();
 				if (rootTraceRegion == null)
 					continue;
-				SmapGenerator generator = new SmapGenerator();
-				generator.setOutputFileName(information.getLocalURI().lastSegment());
-				Map<String,SmapStratum> strata = newHashMap(); 
-				createSmapInfo(rootTraceRegion, strata, -1);
-
-				for (SmapStratum stratum : strata.values()) {
-					generator.addStratum(stratum, true);
-				}
-				String smap = generator.getString();
+				String smap = smapSupport.generateSmap(rootTraceRegion, information.getLocalURI().lastSegment());
 				System.out.println(smap);
 				
 				// install into bytecode
@@ -80,9 +64,7 @@ public class SmapInstallingCompilationParticipant extends CompilationParticipant
 				for (IResource res : resources) {
 					if (res instanceof IFile) {
 						IFile file = (IFile) res;
-						byte[] byteCode = Files.readStreamIntoByteArray(file.getContents());
-						InputStream newByteCode = new ByteArrayInputStream(
-								new SDEInstaller(byteCode, smap.getBytes()).getUpdatedByteCode());
+						InputStream newByteCode = smapSupport.getModifiedByteCode(smap, file.getContents());
 						file.setContents(newByteCode, IResource.KEEP_HISTORY, null);
 					}
 				}
@@ -93,42 +75,4 @@ public class SmapInstallingCompilationParticipant extends CompilationParticipant
 		}
 	}
 
-	
-	public void createSmapInfo(AbstractTraceRegion targetRegion, Map<String,SmapStratum> strata, int mappedLine) {
-		if (mappedLine == targetRegion.getMyLineNumber()) {
-			if (mappedLine == targetRegion.getMyEndLineNumber())
-				return; //SKIP
-			for (AbstractTraceRegion nested: targetRegion.getNestedRegions()) {
-				createSmapInfo(nested, strata, mappedLine);
-			}
-		}
-		ILocationData location = targetRegion.getMergedAssociatedLocation();
-		if (location != null) {
-			final URI path = targetRegion.getAssociatedPath();
-			if (path != null) {
-				final String fileName = path.trimFragment().lastSegment();
-				SmapStratum stratum = strata.get(fileName);
-				if (stratum == null) {
-					IResourceServiceProvider provider = serviceProviderRegistry.getResourceServiceProvider(path.trimFragment());
-					String name = provider.get(LanguageInfo.class).getShortName();
-					stratum = new SmapStratum(name);
-					strata.put(fileName, stratum);
-				}
-				stratum.addFile(fileName, getPath(path));
-				stratum.addLineData(location.getLineNumber()+1, fileName, 1, targetRegion.getMyLineNumber()+1, 1);
-			}
-		}
-		if (targetRegion.getMyLineNumber() != targetRegion.getMyEndLineNumber()) {
-			for (AbstractTraceRegion nested: targetRegion.getNestedRegions()) {
-				createSmapInfo(nested, strata, targetRegion.getMyLineNumber());
-			}
-		}
-	}
-
-	protected String getPath(URI path) {
-		String fullPath = path.trimFragment().toPlatformString(true);
-		final String substring = fullPath.substring(fullPath.substring(1).indexOf('/')+1);
-		System.out.println(substring);
-		return substring;
-	}
 }
