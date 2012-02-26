deddf45c6cab482864c95e8db29235c12df3704b
Michael Clay <clay@codeworkz.at>
2012-02-26 19:31:46 +0100

    [UI] Generated Source View
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=370270

--- plugins/org.eclipse.xtend.ide/src/org/eclipse/xtend/ide/view/DerivedSourceView.java
+++ plugins/org.eclipse.xtend.ide/src/org/eclipse/xtend/ide/view/DerivedSourceView.java
@@ -7,282 +7,118 @@
  *******************************************************************************/
 package org.eclipse.xtend.ide.view;
 
+import org.eclipse.core.internal.utils.WrappedRuntimeException;
 import org.eclipse.core.resources.IStorage;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jdt.internal.ui.JavaPlugin;
 import org.eclipse.jdt.internal.ui.javaeditor.JavaSourceViewer;
 import org.eclipse.jdt.internal.ui.text.SimpleJavaSourceViewerConfiguration;
 import org.eclipse.jdt.ui.text.IJavaPartitions;
 import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.jface.resource.JFaceResources;
-import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextSelection;
-import org.eclipse.jface.text.source.AnnotationModel;
-import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.text.TextSelection;
+import org.eclipse.jface.text.source.SourceViewer;
 import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.StyleRange;
-import org.eclipse.swt.custom.StyledText;
-import org.eclipse.swt.graphics.Font;
-import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.part.ViewPart;
 import org.eclipse.xtext.generator.trace.ILocationInResource;
 import org.eclipse.xtext.generator.trace.ITrace;
 import org.eclipse.xtext.generator.trace.ITraceInformation;
-import org.eclipse.xtext.resource.IResourceDescription;
 import org.eclipse.xtext.ui.editor.XtextEditor;
+import org.eclipse.xtext.ui.views.AbstractSourceView;
 import org.eclipse.xtext.util.Files;
-import org.eclipse.xtext.util.ITextRegionWithLineInformation;
+import org.eclipse.xtext.util.ITextRegion;
 import org.eclipse.xtext.util.TextRegion;
 
 import com.google.inject.Inject;
 
 /**
  * @author Sven Efftinge - Initial contribution and API
+ * @author Michael Clay
  */
-public class DerivedSourceView extends ViewPart implements ISelectionListener {
-
+public class DerivedSourceView extends AbstractSourceView {
 	@Inject
 	private ITraceInformation traceInformation;
-	@Inject
-	private IResourceDescription.Manager manager;
-
-	private IStorage currentlyShown = null;
-	private JavaSourceViewer fViewer;
-	private SimpleJavaSourceViewerConfiguration fViewerConfiguration;
-
-	public void selectionChanged(IWorkbenchPart part, ISelection selection) {
-		if (part.equals(this))
-			return;
-		if (!(part instanceof XtextEditor)) {
-			setInput(null);
-			return;
-		}
-		XtextEditor editor = (XtextEditor) part;
-		try {
-			Control control = fViewer.getControl();
-			if (control instanceof StyledText) {
-				ITrace generatedFile = traceInformation.getTraceToTarget((IStorage)editor.getResource());
-				if (generatedFile == null) {
-					setInput(null);
-					return;
-				}
-				ITextSelection textSelection = (ITextSelection) selection;
-				ILocationInResource location = generatedFile.getBestAssociatedLocation(new TextRegion(textSelection.getOffset(), textSelection.getLength()));
-				if (location != null) {
-					final ITextRegionWithLineInformation region = location.getTextRegion();
-					if (region != null)
-						highlightRange(location.getStorage(), region.getOffset(), region.getLength());
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	protected void highlightRange(IStorage iStorage, int myOffset, int myLength) {
-		if (currentlyShown == null || !currentlyShown.equals(iStorage)) {
-			setInput(iStorage);
-		} else {
-			fViewer.setDocument(fViewer.getDocument());
-		}
-		StyledText text = fViewer.getTextWidget();
-		if (text.getText().length() >= myOffset + myLength) {
-			text.setStyleRange(new StyleRange(myOffset, myLength, null, Display.getDefault().getSystemColor(SWT.COLOR_GRAY)));
-			text.redrawRange(myOffset, myLength, true);
-			fViewer.revealRange(myOffset, myLength);
-		}
-	}
-
-
-	private void setInput(IStorage iStorage) {
-		currentlyShown = iStorage;
-		if (iStorage == null) {
-			fViewer.setInput(new Document("")); //$NON-NLS-1$
-			return;
-		}
-		try {
-			final String source = Files.readStreamIntoString(iStorage.getContents());
-			Shell shell = getSite().getShell();
-			if (shell.isDisposed())
-				return;
-
-			Display display = shell.getDisplay();
-			if (display.isDisposed())
-				return;
-
-			display.asyncExec(new Runnable() {
-				public void run() {
-					if (getViewSite().getShell().isDisposed())
-						return;
-					IDocument document = new Document(source);
-					JavaPlugin.getDefault().getJavaTextTools()
-							.setupJavaDocumentPartitioner(document, IJavaPartitions.JAVA_PARTITIONING);
-					fViewer.setDocument(document, new AnnotationModel());
-				}
-			});
-		} catch (Exception ex) {
-			return;
-		}
-
-	}
-
-	static class Range implements Comparable<Range> {
-		public int offSet, length;
-
-		public Range(int offSet, int length) {
-			this.offSet = offSet;
-			this.length = length;
-		}
-
-		public int compareTo(Range o) {
-			return length - o.length;
-		}
-
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + length;
-			result = prime * result + offSet;
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Range other = (Range) obj;
-			if (length != other.length)
-				return false;
-			if (offSet != other.offSet)
-				return false;
-			return true;
-		}
-
-	}
-
-	public void selectionChanged(SelectionChangedEvent event) {
-		// TODO Auto-generated method stub
-
-	}
-
-	/** The viewer's font properties change listener. */
-	private IPropertyChangeListener fFontPropertyChangeListener = new FontPropertyChangeListener();
-	/**
-	 * The editor's property change listener.
-	 * 
-	 * @since 3.0
-	 */
-	private IPropertyChangeListener fPropertyChangeListener = new PropertyChangeListener();
+	private JavaSourceViewer javaSourceViewer;
+	private SimpleJavaSourceViewerConfiguration javaSourceViewerConfiguration;
 
 	@Override
-	public void createPartControl(Composite parent) {
+	protected SourceViewer createSourceViewer(Composite parent) {
 		IPreferenceStore store = JavaPlugin.getDefault().getCombinedPreferenceStore();
-		fViewer = new JavaSourceViewer(parent, null, null, false, SWT.V_SCROLL | SWT.H_SCROLL, store);
-		fViewerConfiguration = new SimpleJavaSourceViewerConfiguration(JavaPlugin.getDefault().getJavaTextTools()
-				.getColorManager(), store, null, IJavaPartitions.JAVA_PARTITIONING, false);
-		fViewer.configure(fViewerConfiguration);
-		fViewer.setEditable(false);
-
-		setViewerFont();
-		JFaceResources.getFontRegistry().addListener(fFontPropertyChangeListener);
-
-		store.addPropertyChangeListener(fPropertyChangeListener);
-
-		getSite().getPage().addPostSelectionListener(this);
+		javaSourceViewer = new JavaSourceViewer(parent, null, null, false, SWT.V_SCROLL | SWT.H_SCROLL, store);
+		javaSourceViewerConfiguration = new SimpleJavaSourceViewerConfiguration(JavaPlugin.getDefault()
+				.getJavaTextTools().getColorManager(), store, null, IJavaPartitions.JAVA_PARTITIONING, false);
+		javaSourceViewer.configure(javaSourceViewerConfiguration);
+		javaSourceViewer.setEditable(false);
+		return javaSourceViewer;
 	}
 
-	private static final String SYMBOLIC_FONT_NAME = "org.eclipse.jdt.ui.editors.textfont"; //$NON-NLS-1$
-
-	private void setViewerFont() {
-		Font font = JFaceResources.getFont(SYMBOLIC_FONT_NAME);
-
-		if (fViewer.getDocument() != null) {
-
-			Point selection = fViewer.getSelectedRange();
-			int topIndex = fViewer.getTopIndex();
-
-			StyledText styledText = fViewer.getTextWidget();
-			Control parent = fViewer.getControl();
-
-			parent.setRedraw(false);
-
-			styledText.setFont(font);
-
-			fViewer.setSelectedRange(selection.x, selection.y);
-			fViewer.setTopIndex(topIndex);
-
-			if (parent instanceof Composite) {
-				Composite composite = (Composite) parent;
-				composite.layout(true);
-			}
-
-			parent.setRedraw(true);
-
-		} else {
-			StyledText styledText = fViewer.getTextWidget();
-			styledText.setFont(font);
-		}
+	@Override
+	protected boolean isValidSelection(IWorkbenchPart workbenchPart, ISelection selection) {
+		return workbenchPart instanceof XtextEditor && selection instanceof TextSelection;
 	}
 
-	/**
-	 * Internal property change listener for handling changes in the editor's preferences.
-	 * 
-	 * @since 3.0
-	 */
-	class PropertyChangeListener implements IPropertyChangeListener {
-		/*
-		 * @see IPropertyChangeListener#propertyChange(org.eclipse.jface.util.PropertyChangeEvent)
-		 */
-		public void propertyChange(PropertyChangeEvent event) {
-			if (fViewer == null)
-				return;
-
-			if (fViewerConfiguration.affectsTextPresentation(event)) {
-				fViewerConfiguration.handlePropertyChangeEvent(event);
-				fViewer.invalidateTextPresentation();
-			}
-		}
+	@Override
+	protected String getBackgroundColorKey() {
+		return "org.eclipse.jdt.ui.DeclarationView.backgroundColor"; //$NON-NLS-1$
 	}
 
-	/**
-	 * Internal property change listener for handling workbench font changes.
-	 */
-	class FontPropertyChangeListener implements IPropertyChangeListener {
-		/*
-		 * @see IPropertyChangeListener#propertyChange(org.eclipse.jface.util.PropertyChangeEvent)
-		 */
-		public void propertyChange(PropertyChangeEvent event) {
-			if (fViewer == null)
-				return;
+	@Override
+	protected String getViewerFontName() {
+		return "org.eclipse.jdt.ui.editors.textfont"; //$NON-NLS-1$;
+	}
 
-			String property = event.getProperty();
-
-			if (SYMBOLIC_FONT_NAME.equals(property))
-				setViewerFont();
+	@Override
+	protected String computeInput(IWorkbenchPart workbenchPart, ISelection selection, EObject eObject) {
+		ILocationInResource location = getBestAssociatedLocation(workbenchPart, selection);
+		try {
+			return null != location ? Files.readStreamIntoString(location.getStorage().getContents()) : null;
+		} catch (CoreException e) {
+			throw new WrappedRuntimeException(e);
 		}
 	}
 
 	@Override
-	public void setFocus() {
-		// TODO Auto-generated method stub
+	public void propertyChange(PropertyChangeEvent event) {
+		super.propertyChange(event);
+		if (javaSourceViewerConfiguration.affectsTextPresentation(event)) {
+			javaSourceViewerConfiguration.handlePropertyChangeEvent(event);
+			javaSourceViewer.invalidateTextPresentation();
+		}
 	}
 
 	@Override
-	public void dispose() {
-		getSite().getPage().removePostSelectionListener(this);
+	protected IDocument createDocument(String input) {
+		IDocument document = super.createDocument(input);
+		JavaPlugin.getDefault().getJavaTextTools()
+				.setupJavaDocumentPartitioner(document, IJavaPartitions.JAVA_PARTITIONING);
+		return document;
 	}
+
+	@Override
+	protected ITextRegion computeSelectedTextRegion(IWorkbenchPart workbenchPart, ISelection selection, EObject eObject) {
+		ITextRegion textRegion = null;
+		ILocationInResource locationInResource = getBestAssociatedLocation(workbenchPart, selection);
+		if (locationInResource != null) {
+			textRegion = locationInResource.getTextRegion();
+		}
+		return textRegion;
+	}
+
+	private ILocationInResource getBestAssociatedLocation(IWorkbenchPart workbenchPart, ISelection selection) {
+		IStorage storage = (IStorage) ((XtextEditor) workbenchPart).getResource();
+		ITrace trace = traceInformation.getTraceToTarget(storage);
+		if (trace == null) {
+			return null;
+		}
+		ITextSelection textSelection = (ITextSelection) selection;
+		ILocationInResource location = trace.getBestAssociatedLocation(new TextRegion(textSelection.getOffset(),
+				textSelection.getLength()));
+		return location;
+	}
+
 }
