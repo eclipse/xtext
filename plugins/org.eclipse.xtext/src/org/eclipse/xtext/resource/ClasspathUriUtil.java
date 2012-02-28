6b0e63225234e46e281395621ef5572baa34000c
Sven Efftinge <sven.efftinge@itemis.de>
2012-02-28 13:47:11 +0100

    [Xbase] introduced JvmModelCompleter to complete a JvmModel according to Java defaults.

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/resource/ClasspathUriUtil.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/resource/ClasspathUriUtil.java
@@ -22,6 +22,8 @@ public class ClasspathUriUtil {
     public static final String CLASSPATH_SCHEME = "classpath";
 
     public static boolean isClasspathUri(URI uri) {
+    	if (uri == null)
+    		return false;
         String scheme = uri.scheme();
         return CLASSPATH_SCHEME.equals(scheme);
     }
