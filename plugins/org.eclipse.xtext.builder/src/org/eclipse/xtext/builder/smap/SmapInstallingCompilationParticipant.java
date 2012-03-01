dabce4e9a16a642db89e01cb9291f2067f255ba3
Sven Efftinge <sven.efftinge@itemis.de>
2012-03-01 13:58:49 +0100

    [debugging] moved creation of snap to builder participant. The compilation participant now only installs and deletes any existing snap files.

--- plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/smap/SmapInstallingCompilationParticipant.java
+++ plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/smap/SmapInstallingCompilationParticipant.java
@@ -2,19 +2,20 @@ package org.eclipse.xtext.builder.smap;
 
 import java.io.InputStream;
 
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.ITypeRoot;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.compiler.BuildContext;
 import org.eclipse.jdt.core.compiler.CompilationParticipant;
 import org.eclipse.jdt.internal.core.Region;
-import org.eclipse.xtext.generator.trace.AbstractTraceRegion;
-import org.eclipse.xtext.generator.trace.ITrace;
 import org.eclipse.xtext.generator.trace.ITraceInformation;
 import org.eclipse.xtext.generator.trace.SmapSupport;
 import org.eclipse.xtext.resource.IResourceServiceProvider;
+import org.eclipse.xtext.util.Files;
 
 import com.google.inject.Inject;
 
@@ -47,31 +48,39 @@ public class SmapInstallingCompilationParticipant extends CompilationParticipant
 			if (files == null)
 				return;
 			for (BuildContext ctx : files) {
-				ITypeRoot javaElement = (ITypeRoot) JavaCore.create(ctx.getFile());
-				ITrace information = traceInformation.getTraceToSource(ctx.getFile());
-				if (information == null)
-					continue;
-				final AbstractTraceRegion rootTraceRegion = ((ITrace.Internal) information).getRootTraceRegion();
-				if (rootTraceRegion == null)
-					continue;
-				String smap = smapSupport.generateSmap(rootTraceRegion, information.getLocalURI().lastSegment());
-				
-				// install into bytecode
-				Region region = new Region();
-				region.add(javaElement);
-				IResource[] resources = JavaCore.getGeneratedResources(region, false);
-				for (IResource res : resources) {
-					if (res instanceof IFile) {
-						IFile file = (IFile) res;
-						InputStream newByteCode = smapSupport.getModifiedByteCode(smap, file.getContents());
-						file.setContents(newByteCode, IResource.KEEP_HISTORY, null);
+				IJavaElement element = JavaCore.create(ctx.getFile());
+				if (element != null && element.exists()) {
+					IFile smapFile = findSmapFile(ctx);
+					if (smapFile != null && smapFile.exists()) {
+						try {
+							String smapAsString = Files.readStreamIntoString(smapFile.getContents());
+							// install into bytecode
+							Region region = new Region();
+							region.add(element);
+							IResource[] resources = JavaCore.getGeneratedResources(region, false);
+							for (IResource res : resources) {
+								if (res instanceof IFile) {
+									IFile file = (IFile) res;
+									InputStream newByteCode = smapSupport.getModifiedByteCode(smapAsString, file.getContents());
+									file.setContents(newByteCode, IResource.KEEP_HISTORY, null);
+								}
+							}
+						} finally {
+							smapFile.delete(true, null);
+						}
 					}
 				}
 			}
-			files = null;
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			files = null;
 		}
+	}
+
+	protected IFile findSmapFile(BuildContext ctx) {
+		final IContainer container = ctx.getFile().getParent();
+		return container.getFile(new Path(ctx.getFile().getName().toString().replace(".java", ".smap")));
 	}
 
 }
