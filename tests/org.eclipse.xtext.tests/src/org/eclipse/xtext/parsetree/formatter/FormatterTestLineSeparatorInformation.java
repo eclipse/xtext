7c5d2cd8318c57c6c82172aca72962ee5983e8a3
Jan Koehnlein <jan.koehnlein@itemis.de>
2012-03-01 14:47:28 +0100

    [formatter] fixing tests on Windows

---
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/parsetree/formatter/FormatterTestLineSeparatorInformation.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2012 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.parsetree.formatter;
+
+import org.eclipse.xtext.formatting.ILineSeparatorInformation;
+
+import com.google.inject.Singleton;
+
+/**
+ * @author Jan Koehnlein - Initial contribution and API
+ */
+@Singleton
+public class FormatterTestLineSeparatorInformation implements ILineSeparatorInformation {
+
+	private String lineSeparator;
+	
+	public void setLineSeparator(String lineSeparator) {
+		this.lineSeparator = lineSeparator;
+	}
+	
+	public String getLineSeparator() {
+		return lineSeparator;
+	}
+
+}
