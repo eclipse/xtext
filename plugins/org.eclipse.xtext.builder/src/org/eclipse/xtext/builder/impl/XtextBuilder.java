1e61b00e55ce3855686d7d145d3b703fd2335066
Knut Wannheden <knut.wannheden@paranor.ch>
2012-03-19 14:21:00 +0100

    [builder] tweaked progress monitor reporting

--- plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/impl/XtextBuilder.java
+++ plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/impl/XtextBuilder.java
@@ -113,7 +113,7 @@ public class XtextBuilder extends IncrementalProjectBuilder {
 	 *        reported and that the operation cannot be cancelled.
 	 */
 	protected void incrementalBuild(IResourceDelta delta, final IProgressMonitor monitor) throws CoreException {
-		final SubMonitor progress = SubMonitor.convert(monitor, Messages.XtextBuilder_CollectingResources, 2);
+		final SubMonitor progress = SubMonitor.convert(monitor, Messages.XtextBuilder_CollectingResources, 10);
 		progress.subTask(Messages.XtextBuilder_CollectingResources);
 
 		final ToBeBuilt toBeBuilt = new ToBeBuilt();
@@ -137,8 +137,8 @@ public class XtextBuilder extends IncrementalProjectBuilder {
 		delta.accept(visitor);
 		if (progress.isCanceled())
 			throw new OperationCanceledException();
-		progress.worked(1);
-		doBuild(toBeBuilt, progress.newChild(1), BuildType.INCREMENTAL);
+		progress.worked(2);
+		doBuild(toBeBuilt, progress.newChild(8), BuildType.INCREMENTAL);
 	}
 
 	/**
@@ -174,14 +174,14 @@ public class XtextBuilder extends IncrementalProjectBuilder {
 	 *        reported and that the operation cannot be cancelled.
 	 */
 	protected void fullBuild(final IProgressMonitor monitor, boolean isRecoveryBuild) throws CoreException {
-		SubMonitor progress = SubMonitor.convert(monitor, 2);
+		SubMonitor progress = SubMonitor.convert(monitor, 10);
 
 		IProject project = getProject();
 		ToBeBuilt toBeBuilt = 
 			isRecoveryBuild
-				? toBeBuiltComputer.updateProjectNewResourcesOnly(project, progress.newChild(1)) 
-				: toBeBuiltComputer.updateProject(project, progress.newChild(1));
-		doBuild(toBeBuilt, progress.newChild(1), 
+				? toBeBuiltComputer.updateProjectNewResourcesOnly(project, progress.newChild(2)) 
+				: toBeBuiltComputer.updateProject(project, progress.newChild(2));
+		doBuild(toBeBuilt, progress.newChild(8), 
 			isRecoveryBuild 
 				? BuildType.RECOVERY 
 				: BuildType.FULL);
@@ -200,10 +200,10 @@ public class XtextBuilder extends IncrementalProjectBuilder {
 	 */
 	@Override
 	protected void clean(IProgressMonitor monitor) throws CoreException {
-		SubMonitor progress = SubMonitor.convert(monitor, 2);
+		SubMonitor progress = SubMonitor.convert(monitor, 10);
 		try {
-			ToBeBuilt toBeBuilt = toBeBuiltComputer.removeProject(getProject(), progress.newChild(1));
-			doClean(toBeBuilt, progress.newChild(1));
+			ToBeBuilt toBeBuilt = toBeBuiltComputer.removeProject(getProject(), progress.newChild(2));
+			doClean(toBeBuilt, progress.newChild(8));
 		} finally {
 			if (monitor != null)
 				monitor.done();
