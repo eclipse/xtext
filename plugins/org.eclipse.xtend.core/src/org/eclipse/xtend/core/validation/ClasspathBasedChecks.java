e6c53db5d5be486adc1a1b94ff8e27c31aebdca2
Sven Efftinge <sven.efftinge@itemis.de>
2012-04-17 07:51:40 +0200

    reenabled package check

--- plugins/org.eclipse.xtend.core/src/org/eclipse/xtend/core/validation/ClasspathBasedChecks.java
+++ plugins/org.eclipse.xtend.core/src/org/eclipse/xtend/core/validation/ClasspathBasedChecks.java
@@ -10,8 +10,14 @@ package org.eclipse.xtend.core.validation;
 import static org.eclipse.xtext.util.Strings.*;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.URIConverter;
+import org.eclipse.xtend.core.xtend.XtendFile;
 import org.eclipse.xtend.core.xtend.XtendPackage;
+import org.eclipse.xtext.resource.ClasspathUriResolutionException;
+import org.eclipse.xtext.resource.ClasspathUriUtil;
 import org.eclipse.xtext.validation.AbstractDeclarativeValidator;
+import org.eclipse.xtext.validation.Check;
 import org.eclipse.xtext.validation.EValidatorRegistrar;
 import org.eclipse.xtext.validation.ValidationMessageAcceptor;
 
@@ -25,34 +31,34 @@ public class ClasspathBasedChecks extends AbstractDeclarativeValidator {
 		// do nothing
 	}
 
-//	@Check
-//	public void checkFileNamingConventions(XtendFile xtendFile) {
-//		Resource resource = xtendFile.eResource();
-//		// editor already closed
-//		if (resource == null || resource.getResourceSet() == null)
-//			return;
-//		URI resourceURI = resource.getURI();
-//		String packageName = xtendFile.getPackage();
-//		StringBuilder classpathURIBuilder = new StringBuilder(ClasspathUriUtil.CLASSPATH_SCHEME);
-//		classpathURIBuilder.append(":/");
-//		if (packageName != null)
-//			classpathURIBuilder.append(packageName.replace(".", "/")).append("/");
-//		classpathURIBuilder.append(resourceURI.lastSegment());
-//		URI classpathURI = URI.createURI(classpathURIBuilder.toString());
-//		URIConverter uriConverter = resource.getResourceSet().getURIConverter();
-//		try {
-//			URI normalizedURI = uriConverter.normalize(classpathURI);
-//			if(!resourceURI.equals(normalizedURI))
-//				reportInvalidPackage(packageName, classpathURI);
-//		} catch(ClasspathUriResolutionException e) {
-//			reportInvalidPackage(packageName, classpathURI);
-//		}
+	@Check
+	public void checkFileNamingConventions(XtendFile xtendFile) {
+		Resource resource = xtendFile.eResource();
+		// editor already closed
+		if (resource == null || resource.getResourceSet() == null)
+			return;
+		URI resourceURI = resource.getURI();
+		String packageName = xtendFile.getPackage();
+		StringBuilder classpathURIBuilder = new StringBuilder(ClasspathUriUtil.CLASSPATH_SCHEME);
+		classpathURIBuilder.append(":/");
+		if (packageName != null)
+			classpathURIBuilder.append(packageName.replace(".", "/")).append("/");
+		classpathURIBuilder.append(resourceURI.lastSegment());
+		URI classpathURI = URI.createURI(classpathURIBuilder.toString());
+		URIConverter uriConverter = resource.getResourceSet().getURIConverter();
+		try {
+			URI normalizedURI = uriConverter.normalize(classpathURI);
+			if(!resourceURI.equals(normalizedURI))
+				reportInvalidPackage(packageName, classpathURI);
+		} catch(ClasspathUriResolutionException e) {
+			reportInvalidPackage(packageName, classpathURI);
+		}
 //		XtendClass xtendClass = xtendFile.getXtendClass();
 //		if (xtendClass != null && xtendClass.getName() != null
 //				&& !equal(resourceURI.trimFileExtension().lastSegment(), xtendClass.getName()))
 //			error("The class '" + (packageName!=null ? notNull(packageName) + ".":"") + xtendClass.getName() + "' must be defined in its own file",
 //					xtendClass, XtendPackage.Literals.XTEND_CLASS__NAME, ValidationMessageAcceptor.INSIGNIFICANT_INDEX, IssueCodes.WRONG_FILE);
-//	}
+	}
 
 	protected void reportInvalidPackage(String packageName, URI classpathURI) {
 		error("The declared package '" + notNull(packageName) + "' does not match the expected package", 
