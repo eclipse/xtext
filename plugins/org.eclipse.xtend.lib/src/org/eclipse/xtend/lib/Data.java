60d92b436aa4b1caeae9769e4a04313fd9ba3a99
Sven Efftinge <sven.efftinge@itemis.de>
2012-04-25 14:03:03 +0200

    [@Data] some tests and documentation

--- plugins/org.eclipse.xtend.lib/src/org/eclipse/xtend/lib/Data.java
+++ plugins/org.eclipse.xtend.lib/src/org/eclipse/xtend/lib/Data.java
@@ -5,6 +5,18 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation is used by the Xtend compiler.
+ * It turns an annotated class into an immutable value object.
+ * 
+ * All fields become final and a getter method is added.
+ * The fields' name are prepended with an underscore (i.e. myField becomes _myField)
+ * One constructor for initializing the fields is added.
+ * {@link Object#equals(Object)} and {@link Object#hashCode()} implementations are added.
+ * {@link Object#toString()} implementation is added.  
+ * 
+ * @author Sven Efftinge
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Data {
