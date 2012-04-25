e323a330f86aeecfb939614afec424336eb7b65f
Sven Efftinge <sven.efftinge@itemis.de>
2012-04-25 15:29:24 +0200

    in the outline use label and image of the jvm feature

--- plugins/org.eclipse.xtend.lib/src/org/eclipse/xtend/lib/Data.java
+++ plugins/org.eclipse.xtend.lib/src/org/eclipse/xtend/lib/Data.java
@@ -10,8 +10,8 @@ import java.lang.annotation.Target;
  * It turns an annotated class into an immutable value object.
  * 
  * All fields become final and a getter method is added.
- * The fields' name are prepended with an underscore (i.e. myField becomes _myField)
- * One constructor for initializing the fields is added.
+ * The fields' name are prepended with an underscore (i.e. <tt>myField</tt> becomes <tt>_myField</tt>)
+ * One constructor for initializing the uninitialized fields is added.
  * {@link Object#equals(Object)} and {@link Object#hashCode()} implementations are added.
  * {@link Object#toString()} implementation is added.  
  * 
