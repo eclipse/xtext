7dfca3f0875765acb6b2dc2f5700104e30c3b76f
Holger Schill <Holger.Schill@itemis.de>
2012-05-04 14:04:35 +0200

    [Xtend] overwritten method with type params not recognized see https://bugs.eclipse.org/bugs/show_bug.cgi?id=364974

---
+++ tests/org.eclipse.xtend.core.tests/testdata/test/GenericSuperTypeClass.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.io.Serializable;
+import java.util.List;
+
+public abstract class GenericSuperTypeClass<T> {
+	public abstract <T1 extends Object> T1[] getValue1(final List<T1> t);
+
+	public abstract <T2 extends Object> T2[] getValue2(final T2[] t);
+
+	public abstract <T3 extends Object> List<T3> getValue3(final List<T3> t);
+
+	public abstract <T3 extends Object> List<T3> getValue4(final List<T3[]> t);
+
+	public abstract <T3 extends Object> List<T3> getValue5(final List<List<T3>[]> t);
+
+	public abstract <T3 extends Object> List<T3> getValue6(final List<? extends T3> t);
+
+	public abstract <T3 extends Object> List<T3> getValue7(final List<? super T3> t);
+
+	public abstract <T1 extends Object, T2 extends T1> T1[] getValue8(final List<T1> t, final List<T2> t2);
+
+	public abstract <T2 extends Object, T1 extends Serializable & CharSequence> T1[] getValue9(final List<T1> t);
+}
