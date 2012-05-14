7d0211045d1b24c5c7da95902c052b59a4bd0d39
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2012-05-14 11:59:03 +0200

    [common.types] Improved SuperTypeCollector for arrays

--- tests/org.eclipse.xtext.common.types.tests/tests/org/eclipse/xtext/common/types/util/JdtSuperTypeCollectorTest.java
+++ tests/org.eclipse.xtext.common.types.tests/tests/org/eclipse/xtext/common/types/util/JdtSuperTypeCollectorTest.java
@@ -9,10 +9,18 @@ package org.eclipse.xtext.common.types.util;
 
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.xtext.common.types.TypesFactory;
+import org.eclipse.xtext.common.types.access.AbstractTypeProviderFactory;
+import org.eclipse.xtext.common.types.access.IJvmTypeProvider;
 import org.eclipse.xtext.common.types.access.jdt.JdtTypeProvider;
 import org.eclipse.xtext.common.types.access.jdt.MockJavaProjectProvider;
 import org.junit.After;
 import org.junit.Before;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Provider;
 
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
@@ -22,12 +30,25 @@ public class JdtSuperTypeCollectorTest extends AbstractSuperTypeCollectorTest {
 	private ResourceSet resourceSet;
 	private JdtTypeProvider typeProvider;
 	private MockJavaProjectProvider projectProvider;
+	private Provider<SuperTypeCollector> collectorProvider;
 
 	@Before
 	public void setUp() throws Exception {
 		resourceSet = new ResourceSetImpl();
 		projectProvider = new MockJavaProjectProvider();
 		typeProvider = new JdtTypeProvider(projectProvider.getJavaProject(resourceSet), resourceSet);
+		Injector injector = Guice.createInjector(new AbstractModule() {
+			@Override
+			protected void configure() {
+				bind(IJvmTypeProvider.Factory.class).toInstance(new AbstractTypeProviderFactory() {
+					public IJvmTypeProvider createTypeProvider(ResourceSet resourceSet) {
+						return typeProvider;
+					}
+				});
+				bind(TypesFactory.class).toInstance(TypesFactory.eINSTANCE);
+			}
+		});
+		collectorProvider = injector.getProvider(SuperTypeCollector.class);
 	}
 	
 	@After
@@ -39,5 +60,10 @@ public class JdtSuperTypeCollectorTest extends AbstractSuperTypeCollectorTest {
 	@Override
 	protected JdtTypeProvider getTypeProvider() {
 		return typeProvider;
+	}
+	
+	@Override
+	protected SuperTypeCollector getCollector() {
+		return collectorProvider.get();
 	}
 }
