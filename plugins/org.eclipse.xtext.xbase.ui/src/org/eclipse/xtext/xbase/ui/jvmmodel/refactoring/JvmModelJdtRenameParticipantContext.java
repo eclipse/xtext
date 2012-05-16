22edcb3e5620b4d4cb13116d7e549354d647ba5f
Jan Koehnlein <jan.koehnlein@itemis.de>
2012-05-16 13:38:11 +0200

    [refactoring] Better constructor and declaration target handling

--- plugins/org.eclipse.xtext.xbase.ui/src/org/eclipse/xtext/xbase/ui/jvmmodel/refactoring/JvmModelJdtRenameParticipantContext.java
+++ plugins/org.eclipse.xtext.xbase.ui/src/org/eclipse/xtext/xbase/ui/jvmmodel/refactoring/JvmModelJdtRenameParticipantContext.java
@@ -16,6 +16,7 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.EcoreUtil2;
+import org.eclipse.xtext.common.types.JvmConstructor;
 import org.eclipse.xtext.common.types.ui.refactoring.participant.JdtRenameParticipant;
 import org.eclipse.xtext.ui.refactoring.ui.IRenameElementContext;
 import org.eclipse.xtext.xbase.jvmmodel.IJvmModelAssociations;
@@ -45,12 +46,17 @@ public class JvmModelJdtRenameParticipantContext extends IRenameElementContext.I
 		protected List<? extends IRenameElementContext> createJdtParticipantXtextSourceContexts(EObject indexedJvmElement) {
 			if(operatorMappingUtil.isMappedOperator(indexedJvmElement))
 				return Collections.emptyList();
-			EObject renameTargetElement = associations.getPrimarySourceElement(indexedJvmElement);
+			EObject jvmElement;
+			if(indexedJvmElement instanceof JvmConstructor)
+				jvmElement = ((JvmConstructor) indexedJvmElement).getDeclaringType();
+			else 
+				jvmElement = indexedJvmElement;
+			EObject renameTargetElement = associations.getPrimarySourceElement(jvmElement);
 			if (renameTargetElement != null) {
 				return singletonList(new JvmModelJdtRenameParticipantContext(
 						EcoreUtil2.getNormalizedURI(renameTargetElement), renameTargetElement.eClass()));
 			}
-			return super.createJdtParticipantXtextSourceContexts(indexedJvmElement);
+			return super.createJdtParticipantXtextSourceContexts(jvmElement);
 		}
 	}
 }
