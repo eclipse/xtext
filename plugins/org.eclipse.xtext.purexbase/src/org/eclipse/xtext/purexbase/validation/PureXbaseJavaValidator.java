4cfa970443fed6454858a40f0333bd48edf42977
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2012-05-24 18:27:54 +0200

    [regression] Disabled side-effect validation in pure Xbase again

--- plugins/org.eclipse.xtext.purexbase/src/org/eclipse/xtext/purexbase/validation/PureXbaseJavaValidator.java
+++ plugins/org.eclipse.xtext.purexbase/src/org/eclipse/xtext/purexbase/validation/PureXbaseJavaValidator.java
@@ -1,23 +1,15 @@
 package org.eclipse.xtext.purexbase.validation;
 
 import org.eclipse.xtext.validation.Check;
-import org.eclipse.xtext.xbase.XAbstractWhileExpression;
-import org.eclipse.xtext.xbase.XBlockExpression;
-import org.eclipse.xtext.xbase.XForLoopExpression;
+import org.eclipse.xtext.xbase.XExpression;
 import org.eclipse.xtext.xbase.validation.XbaseJavaValidator;
  
 
 public class PureXbaseJavaValidator extends XbaseJavaValidator {
 
+	@Override
 	@Check
-	public void checkInnerExpressions(XBlockExpression block) {
-	}
-	
-	@Check
-	public void checkForExpression(XForLoopExpression loop) {
-	}
-	
-	@Check
-	public void checkWhileExpression(XAbstractWhileExpression loop) {
+	public void checkInnerExpressions(XExpression expr) {
+		// disabled since pure xbase is meant to be a playground
 	}
 }
