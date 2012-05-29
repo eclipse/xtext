55b3a8c32250c957e1dbab6dcb2f71e23fa8926e
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2012-05-29 11:11:21 +0200

    [smap] Avoid removal of the original file if file extension is not .java
    see https://bugs.eclipse.org/bugs/show_bug.cgi?id=380772

--- plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/smap/SmapInstallingCompilationParticipant.java
+++ plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/smap/SmapInstallingCompilationParticipant.java
@@ -1,11 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2012 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package org.eclipse.xtext.builder.smap;
 
 import java.io.InputStream;
 
+import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
@@ -19,14 +28,21 @@ import org.eclipse.xtext.util.Files;
 
 import com.google.inject.Inject;
 
+/**
+ * @author Sven Efftinge - Initial contribution and API
+ */
 public class SmapInstallingCompilationParticipant extends CompilationParticipant {
 
+	private static final Logger log = Logger.getLogger(SmapInstallingCompilationParticipant.class);
+	
 	@Inject
 	private ITraceInformation traceInformation;
+	
 	@Inject
 	private IResourceServiceProvider.Registry serviceProviderRegistry;
 	
-	@Inject private SmapSupport smapSupport;
+	@Inject 
+	private SmapSupport smapSupport;
 
 	@Override
 	public boolean isActive(IJavaProject project) {
@@ -40,7 +56,7 @@ public class SmapInstallingCompilationParticipant extends CompilationParticipant
 		super.buildStarting(files, isBatch);
 		this.files = files;
 	}
-
+	
 	@Override
 	public void buildFinished(IJavaProject project) {
 		try {
@@ -48,39 +64,60 @@ public class SmapInstallingCompilationParticipant extends CompilationParticipant
 			if (files == null)
 				return;
 			for (BuildContext ctx : files) {
-				IJavaElement element = JavaCore.create(ctx.getFile());
-				if (element != null && element.exists()) {
-					IFile smapFile = findSmapFile(ctx);
-					if (smapFile != null && smapFile.exists()) {
-						try {
-							String smapAsString = Files.readStreamIntoString(smapFile.getContents());
-							// install into bytecode
-							Region region = new Region();
-							region.add(element);
-							IResource[] resources = JavaCore.getGeneratedResources(region, false);
-							for (IResource res : resources) {
-								if (res instanceof IFile) {
-									IFile file = (IFile) res;
-									InputStream newByteCode = smapSupport.getModifiedByteCode(smapAsString, file.getContents());
-									file.setContents(newByteCode, IResource.KEEP_HISTORY, null);
+				try {
+					IJavaElement element = JavaCore.create(ctx.getFile());
+					if (element != null && element.exists()) {
+						IFile smapFile = findSmapFile(ctx);
+						if (smapFile != null && smapFile.exists()) {
+							try {
+								String smapAsString = Files.readStreamIntoString(smapFile.getContents());
+								Region region = new Region();
+								region.add(element);
+								// install into bytecode
+								installSmapInformation(smapAsString, JavaCore.getGeneratedResources(region, false));
+							} catch (Exception e) {
+								log.error(String.format("Could not read and install smap information from %s: %s", smapFile.getFullPath().toString(), e.getMessage()), e);
+							} finally {
+								try {
+									smapFile.delete(true, null);
+								} catch(CoreException e) {
+									log.error(e.getMessage(), e);
 								}
 							}
-						} finally {
-							smapFile.delete(true, null);
 						}
 					}
+				} catch (Exception e) {
+					log.error(String.format("Could not process %s to install smap information: %s", ctx.getFile().getFullPath().toString(), e.getMessage()), e);
 				}
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
 		} finally {
 			files = null;
 		}
 	}
 
+	protected void installSmapInformation(String smap, IResource[] resources) {
+		for (IResource res : resources) {
+			if (res instanceof IFile) {
+				installSmapInformation(smap, (IFile) res);
+			}
+		}
+	}
+
+	protected void installSmapInformation(String smap, IFile compiledFile) {
+		try {
+			InputStream newByteCode = smapSupport.getModifiedByteCode(smap, compiledFile.getContents());
+			compiledFile.setContents(newByteCode, IResource.KEEP_HISTORY, null);
+		} catch (Exception e) {
+			log.error(String.format("Could not install smap information into %s: %s", compiledFile.getFullPath().toString(), e.getMessage()), e);
+		}
+	}
+
 	protected IFile findSmapFile(BuildContext ctx) {
-		final IContainer container = ctx.getFile().getParent();
-		return container.getFile(new Path(ctx.getFile().getName().toString().replace(".java", ".smap")));
+		IFile compiledFile = ctx.getFile();
+		IPath smapPath = compiledFile.getFullPath().removeFileExtension().addFileExtension("smap");
+		IContainer container = compiledFile.getParent();
+		IFile result = container.getFile(smapPath.removeFirstSegments(smapPath.segmentCount() - 1));
+		return result;
 	}
 
 }
