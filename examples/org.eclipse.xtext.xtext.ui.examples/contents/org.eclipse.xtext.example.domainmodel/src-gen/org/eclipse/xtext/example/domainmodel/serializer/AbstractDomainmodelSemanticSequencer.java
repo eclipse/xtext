d9e644fc34e325d96b5c6d5ee92ebdbdade3bb58
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2012-06-05 13:08:59 +0200

    regenerated the domain model example

--- examples/org.eclipse.xtext.xtext.ui.examples/contents/org.eclipse.xtext.example.domainmodel/src-gen/org/eclipse/xtext/example/domainmodel/serializer/AbstractDomainmodelSemanticSequencer.java
+++ examples/org.eclipse.xtext.xtext.ui.examples/contents/org.eclipse.xtext.example.domainmodel/src-gen/org/eclipse/xtext/example/domainmodel/serializer/AbstractDomainmodelSemanticSequencer.java
@@ -196,7 +196,7 @@ public abstract class AbstractDomainmodelSemanticSequencer extends XbaseSemantic
 				   context == grammarAccess.getXRelationalExpressionAccess().getXBinaryOperationLeftOperandAction_1_1_0_0_0() ||
 				   context == grammarAccess.getXRelationalExpressionAccess().getXInstanceOfExpressionExpressionAction_1_0_0_0_0() ||
 				   context == grammarAccess.getXUnaryOperationRule()) {
-					sequence_XAssignment(context, (XAssignment) semanticObject); 
+					sequence_XAssignment_XMemberFeatureCall(context, (XAssignment) semanticObject); 
 					return; 
 				}
 				else break;
@@ -228,7 +228,7 @@ public abstract class AbstractDomainmodelSemanticSequencer extends XbaseSemantic
 				   context == grammarAccess.getXRelationalExpressionAccess().getXBinaryOperationLeftOperandAction_1_1_0_0_0() ||
 				   context == grammarAccess.getXRelationalExpressionAccess().getXInstanceOfExpressionExpressionAction_1_0_0_0_0() ||
 				   context == grammarAccess.getXUnaryOperationRule()) {
-					sequence_XAdditiveExpression(context, (XBinaryOperation) semanticObject); 
+					sequence_XAdditiveExpression_XAndExpression_XAssignment_XEqualityExpression_XMultiplicativeExpression_XOrExpression_XOtherOperatorExpression_XRelationalExpression(context, (XBinaryOperation) semanticObject); 
 					return; 
 				}
 				else break;
