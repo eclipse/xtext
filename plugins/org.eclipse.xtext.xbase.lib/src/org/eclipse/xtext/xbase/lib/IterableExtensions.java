d8a927263cb49577bd73acec4c54c49e966f5ce3
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2012-06-06 10:29:11 +0200

    [xbase][lib] Fixed bogus declaration of IterableExtensions#toList and #toSet
    see https://bugs.eclipse.org/bugs/show_bug.cgi?id=381337

--- plugins/org.eclipse.xtext.xbase.lib/src/org/eclipse/xtext/xbase/lib/IterableExtensions.java
+++ plugins/org.eclipse.xtext.xbase.lib/src/org/eclipse/xtext/xbase/lib/IterableExtensions.java
@@ -659,9 +659,8 @@ public class IterableExtensions {
 	 * @return a list with the same entries as the given iterable. May be the same as the given iterable iff it
 	 *         implements {@link List}, otherwise a copy is returned. Never <code>null</code>.
 	 */
-	public static <T> List<T> toList(Iterable<? extends T> iterable) {
+	public static <T> List<T> toList(Iterable<T> iterable) {
 		if (iterable instanceof List<?>) {
-			@SuppressWarnings("unchecked")
 			List<T> result = (List<T>) iterable;
 			return result;
 		}
@@ -679,9 +678,8 @@ public class IterableExtensions {
 	 * @return a set with the unique entries of the given iterable. May be the same as the given iterable iff it
 	 *         implements {@link Set}, otherwise a copy is returned. Never <code>null</code>.
 	 */
-	public static <T> Set<T> toSet(Iterable<? extends T> iterable) {
+	public static <T> Set<T> toSet(Iterable<T> iterable) {
 		if (iterable instanceof Set<?>) {
-			@SuppressWarnings("unchecked")
 			Set<T> result = (Set<T>) iterable;
 			return result;
 		}
