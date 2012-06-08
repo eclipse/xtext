0c9cdbfe99a8932fdb883c9b9b4b5532abf3a3d8
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2012-06-08 14:31:24 +0200

    regenerated xdoc

--- examples/org.eclipse.xtext.xtext.ui.examples/contents/xbase.tutorial/src-gen/Tutorial.java
+++
@@ -1,7 +0,0 @@
-@SuppressWarnings("all")
-public class Tutorial {
-  public static void main(final String[] args) {
-    try {
-    } catch (Throwable t) {}
-  }
-}
--- examples/org.eclipse.xtext.xtext.ui.examples/contents/xbase.tutorial/src-gen/Xbase04_Casts.java
+++
@@ -1,9 +0,0 @@
-@SuppressWarnings("all")
-public class Xbase04_Casts {
-  public static void main(final String[] args) {
-    try {
-        final CharSequence s = "foo";
-        ((String) s).length();
-    } catch (Throwable t) {}
-  }
-}
