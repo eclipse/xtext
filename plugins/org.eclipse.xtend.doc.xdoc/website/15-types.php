43a643e7664824a7c4c74c865fca0568a33d97a6
Sven Efftinge <sven.efftinge@itemis.de>
2012-06-08 18:39:37 +0200

    [doc] huge update (still reviewing chapter expressions)

--- plugins/org.eclipse.xtend.doc.xdoc/website/15-types.php
+++ plugins/org.eclipse.xtend.doc.xdoc/website/15-types.php
@@ -1,6 +1,6 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	include("include.php");
-	$pageTitle 		= "Types";
+	$pageTitle 		= "Static Typing and Java Interoperability";
 	
 	$html = file_get_contents('15-types.html');
 
