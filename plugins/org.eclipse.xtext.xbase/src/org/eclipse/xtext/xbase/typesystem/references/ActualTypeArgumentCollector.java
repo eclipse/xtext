7a80d0de5e589d3074d7e255f8504a24c2dc7f5f
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2012-06-22 17:29:13 +0200

    [xbase][typesystem] Ported utilities to lightweight reference API

---
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/typesystem/references/ActualTypeArgumentCollector.java
@@ -0,0 +1,132 @@
+/*******************************************************************************
+ * Copyright (c) 2012 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.xbase.typesystem.references;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.xtext.common.types.JvmType;
+import org.eclipse.xtext.common.types.JvmTypeConstraint;
+import org.eclipse.xtext.common.types.JvmTypeParameter;
+import org.eclipse.xtext.common.types.JvmTypeReference;
+import org.eclipse.xtext.common.types.JvmUpperBound;
+import org.eclipse.xtext.xbase.typesystem.util.BoundTypeArgumentSource;
+import org.eclipse.xtext.xbase.typesystem.util.Multimaps2;
+import org.eclipse.xtext.xbase.typesystem.util.VarianceInfo;
+
+import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Sets;
+
+/**
+ * @author Sebastian Zarnekow - Initial contribution and API 
+ * TODO JavaDoc, toString
+ */
+@NonNullByDefault
+public class ActualTypeArgumentCollector extends AbstractTypeReferencePairWalker {
+
+	protected class ActualParameterizedTypeReferenceTraverser extends ParameterizedTypeReferenceTraverser {
+		@Override
+		protected boolean shouldProcessInContextOf(JvmTypeParameter declaredTypeParameter, Set<JvmTypeParameter> boundParameters, Set<JvmTypeParameter> visited) {
+			if (!shouldProcess(declaredTypeParameter)) {
+				if (boundParameters.contains(declaredTypeParameter) && !visited.add(declaredTypeParameter))
+					return false;
+			}
+			return true;
+		}
+	}
+
+	private final ListMultimap<JvmTypeParameter, LightweightBoundTypeArgument> typeParameterMapping;
+	private final List<JvmTypeParameter> parametersToBeMapped;
+	private final BoundTypeArgumentSource defaultSource;
+
+	public ActualTypeArgumentCollector(List<JvmTypeParameter> parametersToBeMapped, BoundTypeArgumentSource defaultSource, TypeReferenceOwner owner) {
+		super(owner);
+		this.parametersToBeMapped = parametersToBeMapped;
+		this.defaultSource = defaultSource;
+		typeParameterMapping = Multimaps2.newLinkedHashListMultimap(parametersToBeMapped.size(), 3);
+	}
+
+	public void populateTypeParameterMapping(LightweightTypeReference declaredType, LightweightTypeReference actualType) {
+		processPairedReferences(declaredType, actualType);
+	}
+	
+	protected LightweightBoundTypeArgument boundByConstraint(LightweightTypeReference reference, Object origin) {
+		return new LightweightBoundTypeArgument(reference, BoundTypeArgumentSource.CONSTRAINT, origin, VarianceInfo.OUT, VarianceInfo.OUT);
+	}
+	
+	protected LightweightBoundTypeArgument boundByDefaultSource(LightweightTypeReference reference) {
+		return new LightweightBoundTypeArgument(reference, defaultSource, getOrigin(), getExpectedVariance(), getActualVariance());
+	}
+	
+	@Override
+	protected ParameterizedTypeReferenceTraverser createParameterizedTypeReferenceTraverser() {
+		return new ActualParameterizedTypeReferenceTraverser();
+	}
+	
+	@Override
+	protected void processTypeParameter(JvmTypeParameter typeParameter, LightweightTypeReference reference) {
+		typeParameterMapping.put(typeParameter, boundByDefaultSource(reference));
+	}
+	
+	public ListMultimap<JvmTypeParameter, LightweightBoundTypeArgument> rawGetTypeParameterMapping() {
+		return typeParameterMapping;
+	}
+	
+	protected List<JvmTypeParameter> getParametersToProcess() {
+		return parametersToBeMapped;
+	}
+	
+	@Override
+	protected boolean shouldProcess(JvmTypeParameter type) {
+		return parametersToBeMapped.contains(type);
+	}
+	
+	@Override
+	public void processPairedReferences(LightweightTypeReference declaredType, LightweightTypeReference actualType) {
+		if (parametersToBeMapped.isEmpty())
+			return;
+		super.processPairedReferences(declaredType, actualType);
+	}
+	
+	public ListMultimap<JvmTypeParameter, LightweightBoundTypeArgument> getTypeParameterMapping() {
+		if (typeParameterMapping.keySet().containsAll(getParametersToProcess())) {
+			return typeParameterMapping;
+		}
+		ListMultimap<JvmTypeParameter, LightweightBoundTypeArgument> result = Multimaps2.newLinkedHashListMultimap(typeParameterMapping);
+		OwnedConverter converter = new OwnedConverter(getOwner());
+		for(JvmTypeParameter pendingParameter: getParametersToProcess()) {
+			if (!result.containsKey(pendingParameter)) {
+				for(JvmTypeConstraint constraint: pendingParameter.getConstraints()) {
+					if (!(constraint instanceof JvmUpperBound)) {
+						throw new IllegalStateException("Type parameters may not be declared with a lower bound");
+					}
+					JvmTypeReference constraintReference = constraint.getTypeReference();
+					if (constraintReference != null) {
+						JvmType constraintType = constraintReference.getType();
+						if (!result.containsKey(constraintType)) {
+							if (!getParametersToProcess().contains(constraintType)) {
+								LightweightTypeReference lightweightReference = converter.toLightweightReference(constraintReference);
+								Map<JvmTypeParameter, LightweightMergedBoundTypeArgument> constraintParameterMapping = new DeclaratorTypeArgumentCollector().getTypeParameterMapping(lightweightReference);
+								LightweightTypeReference resolvedConstraint = lightweightReference.accept(new TypeParameterByConstraintSubstitutor(constraintParameterMapping, getOwner()), Sets.newHashSet(pendingParameter));
+								result.put(pendingParameter, boundByConstraint(resolvedConstraint, pendingParameter));
+							} else {
+								ParameterizedTypeReference lightweightReference = new ParameterizedTypeReference(getOwner(), getOwner().getServices().getTypeReferences().findDeclaredType(Object.class, constraintType));
+								result.put(pendingParameter, boundByConstraint(lightweightReference, pendingParameter));
+							}
+						} else {
+							result.putAll(pendingParameter, result.get((JvmTypeParameter) constraintType));
+						}
+					}
+				}
+			}
+		}
+		return result;
+	}
+}
---
+++ tests/org.eclipse.xtext.xbase.tests/xtend-gen/org/eclipse/xtext/xbase/tests/typesystem/LightweightTypeParameterSubstitutorTest.java
@@ -0,0 +1,45 @@
+package org.eclipse.xtext.xbase.tests.typesystem;
+
+import com.google.inject.Inject;
+import java.util.Map;
+import org.eclipse.xtext.common.types.JvmTypeParameter;
+import org.eclipse.xtext.common.types.JvmTypeReference;
+import org.eclipse.xtext.xbase.lib.Functions.Function0;
+import org.eclipse.xtext.xbase.tests.typesystem.AbstractTypeParameterSubstitutorTest;
+import org.eclipse.xtext.xbase.typesystem.references.DeclaratorTypeArgumentCollector;
+import org.eclipse.xtext.xbase.typesystem.references.LightweightMergedBoundTypeArgument;
+import org.eclipse.xtext.xbase.typesystem.references.LightweightTypeReference;
+import org.eclipse.xtext.xbase.typesystem.references.OwnedConverter;
+import org.eclipse.xtext.xbase.typesystem.references.TypeParameterSubstitutor;
+import org.eclipse.xtext.xbase.typesystem.references.TypeReferenceOwner;
+import org.eclipse.xtext.xbase.typesystem.util.CommonTypeComputationServices;
+
+/**
+ * @author Sebastian Zarnekow
+ */
+@SuppressWarnings("all")
+public class LightweightTypeParameterSubstitutorTest extends AbstractTypeParameterSubstitutorTest implements TypeReferenceOwner {
+  @Inject
+  private CommonTypeComputationServices services;
+  
+  private OwnedConverter _ownedConverter = new Function0<OwnedConverter>() {
+    public OwnedConverter apply() {
+      OwnedConverter _ownedConverter = new OwnedConverter(LightweightTypeParameterSubstitutorTest.this);
+      return _ownedConverter;
+    }
+  }.apply();
+  
+  public String resolve(final JvmTypeReference declaration, final JvmTypeReference reference) {
+    DeclaratorTypeArgumentCollector _declaratorTypeArgumentCollector = new DeclaratorTypeArgumentCollector();
+    LightweightTypeReference _lightweightReference = this._ownedConverter.toLightweightReference(declaration);
+    final Map<JvmTypeParameter,LightweightMergedBoundTypeArgument> mapping = _declaratorTypeArgumentCollector.getTypeParameterMapping(_lightweightReference);
+    TypeParameterSubstitutor _typeParameterSubstitutor = new TypeParameterSubstitutor(mapping, this);
+    LightweightTypeReference _lightweightReference_1 = this._ownedConverter.toLightweightReference(reference);
+    LightweightTypeReference _substitute = _typeParameterSubstitutor.substitute(_lightweightReference_1);
+    return _substitute.toString();
+  }
+  
+  public CommonTypeComputationServices getServices() {
+    return this.services;
+  }
+}
