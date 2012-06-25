861f3c9f40607af382fe6dc99c2572d71af41c64
Jan Koehnlein <jan.koehnlein@itemis.de>
2012-06-25 16:19:34 +0200

    [docs] Fixed nested links

--- xtext.website.generator/src/generator/Documentation.xtend
+++ xtext.website.generator/src/generator/Documentation.xtend
@@ -19,6 +19,7 @@ import org.eclipse.xtext.xdoc.xdoc.ImageRef
 import xdocgen.DocumentLoad
 
 import static extension com.google.common.io.Files.*
+import org.eclipse.xtext.xdoc.xdoc.AbstractSection
 
 class Documentation extends AbstractXdocBaseWebsite {
 	
@@ -133,7 +134,7 @@ class DocumentationBody extends Body {
 		<section id="«chapter.href»" style="padding-top: 68px; margin-top: -68px;">
 			<div class="row">
 				<div class="span8 offset3">
-					<h1 style="padding-top: 20px;">
+					<h1 style="padding-top: 30px;">
 						«chapter.title.toHtmlText»
 					</h1>
 					<hr style="margin-top: 5px; margin-bottom: 5px;">
@@ -147,5 +148,19 @@ class DocumentationBody extends Body {
 			</div>
 		</section>
 	'''
+	
+	override h2(AbstractSection section) '''
+		<!--  section -->
+		<section id="«section.href»" style="padding-top: 68px; margin-top: -68px;">
+		<h2 style="padding-top: 15px;">«section.title.toHtmlText»</h2>
+		«FOR content : section.contents»
+			«content.toHtmlParagraph»
+		«ENDFOR»
+		«FOR subsection: section.sections»
+			«subsection.h3plus(3)»
+		«ENDFOR»
+		</section>
+	'''
+	
 	
 }
--- xtext.website.generator/xtend-gen/generator/DocumentationBody.java
+++ xtext.website.generator/xtend-gen/generator/DocumentationBody.java
@@ -34,7 +34,7 @@ public class DocumentationBody extends Body {
     _builder.append("<div class=\"span8 offset3\">");
     _builder.newLine();
     _builder.append("\t\t\t");
-    _builder.append("<h1 style=\"padding-top: 20px;\">");
+    _builder.append("<h1 style=\"padding-top: 30px;\">");
     _builder.newLine();
     _builder.append("\t\t\t\t");
     TextOrMarkup _title = chapter.getTitle();
@@ -71,6 +71,42 @@ public class DocumentationBody extends Body {
     _builder.append("\t");
     _builder.append("</div>");
     _builder.newLine();
+    _builder.append("</section>");
+    _builder.newLine();
+    return _builder;
+  }
+  
+  public CharSequence h2(final AbstractSection section) {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("<!--  section -->");
+    _builder.newLine();
+    _builder.append("<section id=\"");
+    String _href = this._htmlExtensions.href(section);
+    _builder.append(_href, "");
+    _builder.append("\" style=\"padding-top: 68px; margin-top: -68px;\">");
+    _builder.newLineIfNotEmpty();
+    _builder.append("<h2 style=\"padding-top: 15px;\">");
+    TextOrMarkup _title = section.getTitle();
+    CharSequence _htmlText = this._htmlExtensions.toHtmlText(_title);
+    _builder.append(_htmlText, "");
+    _builder.append("</h2>");
+    _builder.newLineIfNotEmpty();
+    {
+      EList<TextOrMarkup> _contents = section.getContents();
+      for(final TextOrMarkup content : _contents) {
+        CharSequence _htmlParagraph = this._htmlExtensions.toHtmlParagraph(content);
+        _builder.append(_htmlParagraph, "");
+        _builder.newLineIfNotEmpty();
+      }
+    }
+    {
+      Iterable<? extends AbstractSection> _sections = this._xdocExtensions.getSections(section);
+      for(final AbstractSection subsection : _sections) {
+        CharSequence _h3plus = this.h3plus(subsection, 3);
+        _builder.append(_h3plus, "");
+        _builder.newLineIfNotEmpty();
+      }
+    }
     _builder.append("</section>");
     _builder.newLine();
     return _builder;
