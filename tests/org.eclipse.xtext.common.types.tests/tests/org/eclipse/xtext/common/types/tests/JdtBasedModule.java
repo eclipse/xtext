149bd7b72f6f64401045b64cad743b15080ef7e2
Sven Efftinge <sven.efftinge@itemis.de>
2012-09-04 16:10:23 +0200

    [common types] introduced interface to limit dependencies.

--- tests/org.eclipse.xtext.common.types.tests/tests/org/eclipse/xtext/common/types/tests/JdtBasedModule.java
+++ tests/org.eclipse.xtext.common.types.tests/tests/org/eclipse/xtext/common/types/tests/JdtBasedModule.java
@@ -7,10 +7,18 @@
  *******************************************************************************/
 package org.eclipse.xtext.common.types.tests;
 
+import static java.util.Collections.*;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IStorage;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.xtext.common.types.access.IJvmTypeProvider.Factory;
 import org.eclipse.xtext.common.types.access.jdt.IJavaProjectProvider;
+import org.eclipse.xtext.common.types.access.jdt.IWorkingCopyOwnerProvider;
 import org.eclipse.xtext.common.types.access.jdt.JdtTypeProviderFactory;
 import org.eclipse.xtext.common.types.xtext.ui.XtextResourceSetBasedProjectProvider;
+import org.eclipse.xtext.ui.resource.IStorage2UriMapper;
+import org.eclipse.xtext.util.Pair;
 
 /**
  * @author Sven Efftinge - Initial contribution and API
@@ -26,4 +34,21 @@ public class JdtBasedModule extends ClasspathBasedModule {
 		return XtextResourceSetBasedProjectProvider.class;
 	}
 	
+	public IWorkingCopyOwnerProvider bindIWorkingCopyOwnerProvider() {
+		return IWorkingCopyOwnerProvider.DEFAULT;
+	}
+	
+	public static class Storage2UriDummy implements IStorage2UriMapper {
+
+		public Iterable<Pair<IStorage, IProject>> getStorages(URI uri) {
+			return emptyList();
+		}
+
+		public URI getUri(IStorage storage) {
+			return null;
+		}
+		
+	}
 }
+
+
