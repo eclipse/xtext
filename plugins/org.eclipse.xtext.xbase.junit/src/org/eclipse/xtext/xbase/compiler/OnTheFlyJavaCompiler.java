c282c073d29fc946e362a4f2b545c22121c29487
Sven Efftinge <sven.efftinge@itemis.de>
2012-09-06 16:38:28 +0200

    added sysout logging to debug test failure on server

--- plugins/org.eclipse.xtext.xbase.junit/src/org/eclipse/xtext/xbase/compiler/OnTheFlyJavaCompiler.java
+++ plugins/org.eclipse.xtext.xbase.junit/src/org/eclipse/xtext/xbase/compiler/OnTheFlyJavaCompiler.java
@@ -286,8 +286,8 @@ public class OnTheFlyJavaCompiler {
 			sb.append('\"');
 			sb.append(tempDir.getCanonicalPath());
 			sb.append('\"');
-			System.out
-					.println(sb);
+			if (sources.size() > 1)
+				System.out.println(sb);
 			boolean compile = compile(sb.toString());
 			if (!compile)
 				throw new IllegalArgumentException("Couldn't compile : "
