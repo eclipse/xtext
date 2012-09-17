1de62a12608faf6252a59a4a974b1f4fcd666ac1
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2012-09-17 15:34:49 +0200

    [xbase][typesystem] Workaround for https://bugs.eclipse.org/bugs/show_bug.cgi?id=389719
    Disabled annotation value computation for some of the type system tests to improve the runtime

--- tests/org.eclipse.xtext.xbase.tests/src/org/eclipse/xtext/xbase/tests/typesystem/AvoidDeprecatedTypeSystemStandaloneSetup.java
+++ tests/org.eclipse.xtext.xbase.tests/src/org/eclipse/xtext/xbase/tests/typesystem/AvoidDeprecatedTypeSystemStandaloneSetup.java
@@ -7,21 +7,28 @@
  *******************************************************************************/
 package org.eclipse.xtext.xbase.tests.typesystem;
 
+import java.lang.reflect.AnnotatedElement;
+
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.xtext.common.types.JvmAnnotationTarget;
+import org.eclipse.xtext.common.types.access.ClasspathTypeProviderFactory;
+import org.eclipse.xtext.common.types.access.impl.ClasspathTypeProvider;
+import org.eclipse.xtext.common.types.access.impl.DeclaredTypeFactory;
 import org.eclipse.xtext.linking.LinkingScopeProviderBinding;
 import org.eclipse.xtext.resource.SynchronizedXtextResourceSet;
 import org.eclipse.xtext.scoping.IScope;
 import org.eclipse.xtext.scoping.IScopeProvider;
 import org.eclipse.xtext.xbase.XbaseRuntimeModule;
 import org.eclipse.xtext.xbase.XbaseStandaloneSetup;
-import org.eclipse.xtext.xbase.scoping.XbaseScopeProvider;
 import org.eclipse.xtext.xbase.scoping.batch.XbaseBatchScopeProvider;
 import org.eclipse.xtext.xbase.tests.AbstractXbaseTestCase;
 import org.eclipse.xtext.xbase.tests.AbstractXbaseTestCase.SynchronizedXtextResourceSetProvider;
 
 import com.google.inject.Binder;
 import com.google.inject.Guice;
+import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Provider;
 
@@ -54,14 +61,43 @@ public class AvoidDeprecatedTypeSystemStandaloneSetup extends XbaseStandaloneSet
 			public void configureLinkingIScopeProvider(Binder binder) {
 				binder.bind(IScopeProvider.class).annotatedWith(LinkingScopeProviderBinding.class).to(XbaseBatchScopeProvider.class);
 			}
+			
+			@SuppressWarnings("unused")
+			public Class<? extends ClasspathTypeProviderFactory> bindClasspathTypeProviderFactory() {
+				return ClasspathTypeProviderFactoryWithoutAnnotationValues.class;
+			}
 		});
 	}
 	
 	@SuppressWarnings("deprecation")
-	public static class DisabledXbaseScopeProvider extends XbaseScopeProvider {
+	public static class DisabledXbaseScopeProvider extends org.eclipse.xtext.xbase.scoping.XbaseScopeProvider {
 		@Override
 		public IScope getScope(EObject context, EReference reference) {
 			throw new UnsupportedOperationException();
 		}
+	}
+	
+	public static class ClasspathTypeProviderFactoryWithoutAnnotationValues extends ClasspathTypeProviderFactory {
+
+		@Inject
+		public ClasspathTypeProviderFactoryWithoutAnnotationValues(ClassLoader classLoader) {
+			super(classLoader);
+		}
+		
+		@Override
+		protected ClasspathTypeProvider createClasspathTypeProvider(ResourceSet resourceSet) {
+			return new ClasspathTypeProvider(getClassLoader(), resourceSet, getIndexedJvmTypeAccess()) {
+				@Override
+				protected DeclaredTypeFactory createDeclaredTypeFactory() {
+					return new DeclaredTypeFactory(getClassURIHelper()) {
+						@Override
+						protected void createAnnotationValues(AnnotatedElement annotated, JvmAnnotationTarget result) {
+							// disabled
+						}
+					};
+				}
+			};
+		}
+		
 	}
 }
