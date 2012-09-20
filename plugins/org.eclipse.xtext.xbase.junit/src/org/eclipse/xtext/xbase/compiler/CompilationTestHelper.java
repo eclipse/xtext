694d0ea02622732b7fadb671b96103c3670c45e1
Sven Efftinge <sven.efftinge@itemis.de>
2012-09-20 15:34:55 +0200

    [xbase junit] allow compilation of a whole resource set containing different languages.

--- plugins/org.eclipse.xtext.xbase.junit/src/org/eclipse/xtext/xbase/compiler/CompilationTestHelper.java
+++ plugins/org.eclipse.xtext.xbase.junit/src/org/eclipse/xtext/xbase/compiler/CompilationTestHelper.java
@@ -7,20 +7,36 @@
  *******************************************************************************/
 package org.eclipse.xtext.xbase.compiler;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.eclipse.emf.ecore.EObject;
+import org.apache.log4j.Logger;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.xtext.diagnostics.Severity;
 import org.eclipse.xtext.generator.IGenerator;
 import org.eclipse.xtext.generator.InMemoryFileSystemAccess;
-import org.eclipse.xtext.junit4.util.ParseHelper;
-import org.eclipse.xtext.junit4.validation.ValidationTestHelper;
+import org.eclipse.xtext.resource.FileExtensionProvider;
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.resource.XtextResourceSet;
+import org.eclipse.xtext.util.CancelIndicator;
 import org.eclipse.xtext.util.IAcceptor;
+import org.eclipse.xtext.util.StringInputStream;
+import org.eclipse.xtext.validation.CheckMode;
+import org.eclipse.xtext.validation.Issue;
 import org.eclipse.xtext.xbase.lib.IterableExtensions;
+import org.eclipse.xtext.xbase.lib.Pair;
 import org.junit.Assert;
 
 import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+import static java.util.Collections.*;
+
+import static com.google.common.collect.Lists.*;
 
 import static com.google.common.collect.Maps.*;
 
@@ -29,13 +45,13 @@ import static com.google.common.collect.Maps.*;
  */
 public class CompilationTestHelper {
 	
+	private final static Logger log = Logger.getLogger(CompilationTestHelper.class);
+	
 	@Inject private OnTheFlyJavaCompiler javaCompiler;
 	
-	@Inject private ParseHelper<EObject> parseHelper;
+	@Inject private Provider<XtextResourceSet> resourceSetProvider;
 	
-	@Inject private ValidationTestHelper validationTestHelper;
-	
-	@Inject private IGenerator generator;
+	@Inject private FileExtensionProvider extensionProvider;
 	
 	/**
 	 * Asserts that the expected code is generated for the given source.
@@ -43,8 +59,9 @@ public class CompilationTestHelper {
 	 * 
 	 * @param source some valid source code written in the language under test
 	 * @param expected the expected Java source code.
+	 * @throws IOException 
 	 */
-	public void assertCompilesTo(CharSequence source, final CharSequence expected) {
+	public void assertCompilesTo(CharSequence source, final CharSequence expected) throws IOException {
 		final boolean[] called = {false};
 		compile(source, new IAcceptor<CompilationTestHelper.Result>() {
 			public void accept(Result r) {
@@ -67,7 +84,7 @@ public class CompilationTestHelper {
 		
 		String getSingleGeneratedCode();
 
-		Resource getResource();
+		ResourceSet getResourceSet();
 
 		Class<?> getCompiledClass();
 		
@@ -82,13 +99,53 @@ public class CompilationTestHelper {
 	 *  
 	 * @param source some code written in the language under test.
 	 * @param acceptor gets called once for each file generated in {@link IGenerator}
+	 * @throws IOException 
 	 */
-	public void compile(CharSequence source, IAcceptor<Result> acceptor) {
+	@SuppressWarnings("unchecked")
+	public void compile(CharSequence source, IAcceptor<Result> acceptor) throws IOException {
+		String fileName = "MyFile."+extensionProvider.getPrimaryFileExtension();
+		compile(resourceSet(new Pair<String, CharSequence>(fileName, source)), acceptor);
+	}
+	
+	/**
+	 * Parses, validates and compiles the given source. Calls the given acceptor for each
+	 * resource which is generated from the source.
+	 *  
+	 * @param source some code written in the language under test.
+	 * @param resourceSet - the {@link ResourceSet} to use
+	 * @param acceptor gets called once for each file generated in {@link IGenerator}
+	 */
+	public void compile(final ResourceSet resourceSet, IAcceptor<Result> acceptor) {
 		try {
-			final EObject parsed = parseHelper.parse(source);
-			validationTestHelper.assertNoErrors(parsed);
+			boolean hasErrors = false; 
+			List<Resource> resourcesToCheck = newArrayList(resourceSet.getResources());
+			for (Resource resource : resourcesToCheck) {
+				if (resource instanceof XtextResource) {
+					XtextResource xtextResource = (XtextResource) resource;
+					List<Issue> issues = xtextResource.getResourceServiceProvider().getResourceValidator().validate(xtextResource, CheckMode.ALL, CancelIndicator.NullImpl);
+					for (Issue issue : issues) {
+						if (issue.getSeverity() == Severity.ERROR) {
+							hasErrors = true;
+							log.error(issue);
+						} else {
+							log.info(issue);
+						}
+					}
+				}
+			}
+			if (hasErrors) {
+				throw new IllegalStateException("One or more resources contained errors. Check the logs.");
+			}
+			
 			final InMemoryFileSystemAccess access = new InMemoryFileSystemAccess();
-			generator.doGenerate(parsed.eResource(), access);
+			for (Resource resource : resourcesToCheck) {
+				if (resource instanceof XtextResource) {
+					XtextResource xtextResource = (XtextResource) resource;
+					IGenerator generator = xtextResource.getResourceServiceProvider().get(IGenerator.class);
+					if (generator != null)
+						generator.doGenerate(xtextResource, access);
+				}
+			}
 			acceptor.accept(new Result() {
 				
 				private Map<String,Class<?>> compiledClasses;
@@ -106,19 +163,14 @@ public class CompilationTestHelper {
 						generatedCode = newHashMap();
 						for (final Entry<String, CharSequence> e : access.getFiles().entrySet()) {
 							String name = e.getKey().substring("DEFAULT_OUTPUT".length(), e.getKey().length() - ".java".length());
-							generatedCode.put(name, e.getValue().toString());
+							generatedCode.put(name.replace('/', '.'), e.getValue().toString());
 						}
 					}
 					return generatedCode;
 				}
 
 				public String getGeneratedCode(String typeName) {
-					for (final Entry<String, CharSequence> e : access.getFiles().entrySet()) {
-						String name = e.getKey().substring("DEFAULT_OUTPUT".length(), e.getKey().length() - ".java".length());
-						if (name.equals(typeName))
-							return e.getValue().toString();
-					}
-					return null;
+					return getGeneratedCode().get(typeName);
 				}
 				
 				public String getSingleGeneratedCode() {
@@ -127,8 +179,8 @@ public class CompilationTestHelper {
 					return null;
 				}
 
-				public Resource getResource() {
-					return (Resource) parsed.eResource();
+				public ResourceSet getResourceSet() {
+					return resourceSet;
 				}
 
 				public Class<?> getCompiledClass() {
@@ -147,6 +199,25 @@ public class CompilationTestHelper {
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
+	}
+	
+	/**
+	 * creates a fresh resource set with the given resources
+	 * 
+	 * @param resources - pairs of file names and their contents 
+	 * @return a resourceset, containing the given resources.
+	 * @throws IOException 
+	 */
+	public ResourceSet resourceSet(Pair<String,CharSequence> ...resources ) throws IOException {
+		XtextResourceSet result = resourceSetProvider.get();
+		for (Pair<String, CharSequence> entry : resources) {
+			final URI uri = URI.createURI(entry.getKey());
+			Resource resource = result.createResource(uri);
+			if (resource == null)
+				throw new IllegalStateException("Couldn't create resource for URI "+uri+". Resource.Factory not registered?");
+			resource.load(new StringInputStream(entry.getValue().toString()), emptyMap());
+		}
+		return result;
 	}
 	
 }
