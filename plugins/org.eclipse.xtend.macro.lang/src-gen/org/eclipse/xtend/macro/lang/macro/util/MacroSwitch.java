2bcb89ae977e5603463dbd7254e01b093c9bfda5
Sven Efftinge <sven.efftinge@itemis.de>
2012-09-21 10:12:18 +0200

    [macro] regenerated with EMF runtimeVersion 2.5

--- plugins/org.eclipse.xtend.macro.lang/src-gen/org/eclipse/xtend/macro/lang/macro/util/MacroSwitch.java
+++ plugins/org.eclipse.xtend.macro.lang/src-gen/org/eclipse/xtend/macro/lang/macro/util/MacroSwitch.java
@@ -2,10 +2,10 @@
  */
 package org.eclipse.xtend.macro.lang.macro.util;
 
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EPackage;
+import java.util.List;
 
-import org.eclipse.emf.ecore.util.Switch;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
 
 import org.eclipse.xtend.core.xtend.XtendAnnotationTarget;
 import org.eclipse.xtend.core.xtend.XtendMember;
@@ -26,7 +26,7 @@ import org.eclipse.xtend.macro.lang.macro.*;
  * @see org.eclipse.xtend.macro.lang.macro.MacroPackage
  * @generated
  */
-public class MacroSwitch<T> extends Switch<T>
+public class MacroSwitch<T>
 {
   /**
    * The cached model package
@@ -51,17 +51,15 @@ public class MacroSwitch<T> extends Switch<T>
   }
 
   /**
-   * Checks whether this is a switch for the given package.
+   * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
    * <!-- begin-user-doc -->
    * <!-- end-user-doc -->
-   * @parameter ePackage the package in question.
-   * @return whether this is a switch for the given package.
+   * @return the first non-null result returned by a <code>caseXXX</code> call.
    * @generated
    */
-  @Override
-  protected boolean isSwitchFor(EPackage ePackage)
+  public T doSwitch(EObject theEObject)
   {
-    return ePackage == modelPackage;
+    return doSwitch(theEObject.eClass(), theEObject);
   }
 
   /**
@@ -71,7 +69,29 @@ public class MacroSwitch<T> extends Switch<T>
    * @return the first non-null result returned by a <code>caseXXX</code> call.
    * @generated
    */
-  @Override
+  protected T doSwitch(EClass theEClass, EObject theEObject)
+  {
+    if (theEClass.eContainer() == modelPackage)
+    {
+      return doSwitch(theEClass.getClassifierID(), theEObject);
+    }
+    else
+    {
+      List<EClass> eSuperTypes = theEClass.getESuperTypes();
+      return
+        eSuperTypes.isEmpty() ?
+          defaultCase(theEObject) :
+          doSwitch(eSuperTypes.get(0), theEObject);
+    }
+  }
+
+  /**
+   * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the first non-null result returned by a <code>caseXXX</code> call.
+   * @generated
+   */
   protected T doSwitch(int classifierID, EObject theEObject)
   {
     switch (classifierID)
@@ -215,7 +235,6 @@ public class MacroSwitch<T> extends Switch<T>
    * @see #doSwitch(org.eclipse.emf.ecore.EObject)
    * @generated
    */
-  @Override
   public T defaultCase(EObject object)
   {
     return null;
