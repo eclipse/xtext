ae1df563a7d1963cd769db3406691f1001738a7a
Michael Clay <clay@codeworkz.at>
2012-10-03 23:35:08 +0200

    add missing test for [editor] No override indicators in gutter https://bugs.eclipse.org/bugs/show_bug.cgi?id=389892

--- tests/org.eclipse.xtend.ide.tests/src/org/eclipse/xtend/ide/tests/refactoring/RenameStrategyTest.java
+++ tests/org.eclipse.xtend.ide.tests/src/org/eclipse/xtend/ide/tests/refactoring/RenameStrategyTest.java
@@ -7,18 +7,36 @@
  *******************************************************************************/
 package org.eclipse.xtend.ide.tests.refactoring;
 
+import static com.google.common.collect.Iterators.*;
+import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Provider;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.text.source.ISourceViewer;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.actions.WorkspaceModifyOperation;
 import org.eclipse.xtend.core.jvmmodel.IXtendJvmAssociations;
 import org.eclipse.xtend.core.xtend.XtendClass;
 import org.eclipse.xtend.core.xtend.XtendConstructor;
 import org.eclipse.xtend.core.xtend.XtendFunction;
+import org.eclipse.xtend.ide.editor.OverrideIndicatorAnnotation;
 import org.eclipse.xtend.ide.tests.AbstractXtendUITestCase;
 import org.eclipse.xtend.ide.tests.WorkbenchTestHelper;
 import org.eclipse.xtext.common.types.JvmConstructor;
 import org.eclipse.xtext.common.types.JvmGenericType;
 import org.eclipse.xtext.common.types.JvmOperation;
+import org.eclipse.xtext.junit4.ui.util.IResourcesSetupUtil;
+import org.eclipse.xtext.ui.editor.XtextEditor;
 import org.eclipse.xtext.ui.refactoring.IRenameStrategy;
 import org.junit.Test;
 
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
 import com.google.inject.Inject;
 
 /**
@@ -35,6 +53,52 @@ public class RenameStrategyTest extends AbstractXtendUITestCase {
 
 	@Inject
 	private WorkbenchTestHelper testHelper;
+
+	@Test public void testOverrideIndicatorAnnotationAfterFileRename() throws Exception {
+		testHelper.createFile("test/SuperClass","package test\nclass SuperClass { def foo() {}}");
+		final IFile subClassFile = testHelper.createFile("test/SubClass","package test\nclass SubClass extends SuperClass { override foo () {}}");
+		IResourcesSetupUtil.waitForAutoBuild();
+		XtextEditor openEditor = testHelper.openEditor(subClassFile);
+		final OverrideIndicatorAnnotation[] annotationBeforeFileRename = new OverrideIndicatorAnnotation[]{null};
+		final OverrideIndicatorAnnotation[] annotationAfterFileRename = new OverrideIndicatorAnnotation[]{null};
+		final ISourceViewer sourceViewer = openEditor.getInternalSourceViewer();
+		sleepWhile(Predicates.isNull(), new Provider<Object>() {
+
+			public OverrideIndicatorAnnotation get() {
+				annotationBeforeFileRename[0] = getOnlyElement(filter(sourceViewer.getAnnotationModel().getAnnotationIterator(), OverrideIndicatorAnnotation.class), null);
+				return annotationBeforeFileRename[0];
+			}
+		},TimeUnit.SECONDS.toMillis(10));
+		assertNotNull(annotationBeforeFileRename[0]);
+		new WorkspaceModifyOperation() {
+			@Override
+			protected void execute(IProgressMonitor monitor) throws CoreException, InvocationTargetException,
+					InterruptedException {
+				subClassFile.move(subClassFile.getFullPath().removeLastSegments(1).append("Test.xtend"), true, monitor);
+			}
+		}.run(new NullProgressMonitor());
+		IResourcesSetupUtil.waitForAutoBuild();
+		sleepWhile(Predicates.isNull(), new Provider<Object>() {
+
+			public OverrideIndicatorAnnotation get() {
+				annotationAfterFileRename[0] = getOnlyElement(filter(sourceViewer.getAnnotationModel().getAnnotationIterator(), OverrideIndicatorAnnotation.class), null);
+				return annotationAfterFileRename[0];
+			}
+		},TimeUnit.SECONDS.toMillis(10));
+		assertNotNull(annotationAfterFileRename[0]);
+		assertNotSame(annotationBeforeFileRename[0], annotationAfterFileRename[0]);
+	}
+
+	private <T> void sleepWhile(Predicate<T> test, Provider<T> input, long timeOutInMillis) {
+		Display display = Display.getCurrent();
+		long timeToGo = System.currentTimeMillis() + timeOutInMillis;
+		while (System.currentTimeMillis() < timeToGo && test.apply(input.get())) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+		display.update();
+	}
 
 	@Test public void testInferredClassRenamed() throws Exception {
 		XtendClass fooClass = (XtendClass) testHelper.xtendFile("Foo", "class Foo { }").getXtendTypes().get(0);
