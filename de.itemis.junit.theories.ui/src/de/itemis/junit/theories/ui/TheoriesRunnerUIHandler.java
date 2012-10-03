f83cc470a3a3c5173febbf181b5852acdcc4c785
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2012-10-03 14:47:50 +0200

    added example projects for JUnit theories
    Signed-off-by: Moritz Eysholdt <moritz.eysholdt@itemis.de>

---
+++ de.itemis.junit.theories.ui/src/de/itemis/junit/theories/ui/TheoriesRunnerUIHandler.java
@@ -0,0 +1,46 @@
+package de.itemis.junit.theories.ui;
+
+import org.eclipse.jdt.internal.junit.model.TestCaseElement;
+import org.eclipse.jdt.internal.junit.ui.TestRunnerViewPart;
+import org.eclipse.jdt.junit.model.ITestCaseElement;
+import org.eclipse.jdt.junit.model.ITestElement;
+import org.eclipse.jdt.junit.model.ITestSuiteElement;
+import org.eclipse.jdt.junit.runners.IRunnerUIHandler;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.ui.part.ViewPart;
+
+@SuppressWarnings("restriction")
+public class TheoriesRunnerUIHandler implements IRunnerUIHandler {
+
+	@Override
+	public boolean contextMenuAboutToShow(ViewPart part, ITestElement element, IMenuManager menu) {
+		if (element instanceof TestCaseElement) {
+			menu.add(new OpenTestTheoryAction((TestRunnerViewPart) part, (TestCaseElement) element));
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean handleDoubleClick(ViewPart part, ITestElement element) {
+		if (element instanceof TestCaseElement) {
+			Action action = new OpenTestTheoryAction((TestRunnerViewPart) part, (TestCaseElement) element);
+			if (action.isEnabled()) {
+				action.run();
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public String getSimpleLabel(ViewPart part, ITestElement element) {
+		if (element instanceof ITestCaseElement)
+			return ((ITestCaseElement) element).getTestMethodName();
+		if (element instanceof ITestSuiteElement)
+			return ((ITestSuiteElement) element).getSuiteTypeName();
+		return "unknown";
+	}
+
+}
