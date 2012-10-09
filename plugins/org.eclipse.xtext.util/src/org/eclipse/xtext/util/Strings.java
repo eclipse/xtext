d989f116a5c767e085fdf36601ef52214d11f7a9
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2012-10-09 15:01:52 +0200

    [util] Minor: Mark dubious method Strings.equalsIgnoreWhitespace as deprecated
    It should be moved to the test that actually uses it.

--- plugins/org.eclipse.xtext.util/src/org/eclipse/xtext/util/Strings.java
+++ plugins/org.eclipse.xtext.util/src/org/eclipse/xtext/util/Strings.java
@@ -23,6 +23,10 @@ public class Strings {
 
 	public static final String[] EMPTY_ARRAY = new String[0];
 
+	/**
+	 * @deprecated will be moved to JUnit utilities.
+	 */
+	@Deprecated
 	public static boolean equalsIgnoreWhitespace(String left, String right) {
 		String l = left == null ? "" : left.replaceAll("\\s", "");
 		String r = right == null ? "" : right.replaceAll("\\s", "");
