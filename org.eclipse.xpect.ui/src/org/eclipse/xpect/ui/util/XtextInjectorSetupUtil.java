e2c4a86917873d4315761cecc58c6c29c1459812
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2012-10-21 19:45:50 +0200

    look for annotation also in supertypes Signed-off-by: Moritz Eysholdt <moritz.eysholdt@itemis.de>

--- org.eclipse.xpect.ui/src/org/eclipse/xpect/ui/util/XtextInjectorSetupUtil.java
+++ org.eclipse.xpect.ui/src/org/eclipse/xpect/ui/util/XtextInjectorSetupUtil.java
@@ -2,6 +2,8 @@ package org.eclipse.xpect.ui.util;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.Set;
+import java.util.Stack;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
@@ -12,18 +14,20 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.xpect.XpectFile;
-import org.eclipse.xpect.XpectTest;
 import org.eclipse.xpect.registry.ILanguageInfo;
 import org.eclipse.xpect.setup.XtextInjectorSetup;
 import org.eclipse.xpect.ui.editor.AssimilatingModule;
 import org.eclipse.xpect.ui.internal.XpectActivator;
 import org.eclipse.xpect.ui.util.ContentTypeUtil.XpectContentType;
+import org.eclipse.xtext.common.types.JvmDeclaredType;
+import org.eclipse.xtext.common.types.JvmTypeReference;
 import org.eclipse.xtext.resource.IResourceFactory;
 import org.eclipse.xtext.resource.XtextResourceSet;
 import org.eclipse.xtext.ui.shared.Access;
 import org.eclipse.xtext.ui.util.JdtClasspathUriResolver;
 import org.eclipse.xtext.util.Pair;
 
+import com.google.common.collect.Sets;
 import com.google.inject.Injector;
 import com.google.inject.Module;
 
@@ -51,14 +55,26 @@ public class XtextInjectorSetupUtil {
 	}
 
 	private static Class<? extends Module> getWorkbenchModule(XpectFile xpectFile) {
-		if (xpectFile != null) {
-			XpectTest xpectTest = xpectFile.getTest();
-			if (xpectTest != null && !xpectTest.eIsProxy()) {
-				Class<?> testClass = TypeUiUtil.getWorkspaceTypeFromHostPlatform(xpectFile.getTest().getTestClass());
-				if (testClass != null) {
-					XtextInjectorSetup xtextInjectorSetup = testClass.getAnnotation(XtextInjectorSetup.class);
-					if (xtextInjectorSetup != null)
-						return xtextInjectorSetup.workbenchModule();
+		if (xpectFile != null && xpectFile.getTest() != null) {
+			JvmDeclaredType testType = xpectFile.getTest().getTestClass();
+			if (testType != null && !testType.eIsProxy()) {
+				Stack<JvmDeclaredType> candidates = new Stack<JvmDeclaredType>();
+				candidates.add(testType);
+				Set<JvmDeclaredType> visited = Sets.newHashSet(testType);
+				while (!candidates.isEmpty()) {
+					JvmDeclaredType candidate = candidates.pop();
+					Class<?> testClass = TypeUiUtil.getWorkspaceTypeFromHostPlatform(candidate);
+					if (testClass != null) {
+						XtextInjectorSetup xtextInjectorSetup = testClass.getAnnotation(XtextInjectorSetup.class);
+						if (xtextInjectorSetup != null)
+							return xtextInjectorSetup.workbenchModule();
+					}
+					for (JvmTypeReference ref : candidate.getSuperTypes())
+						if (ref.getType() instanceof JvmDeclaredType && !ref.getType().eIsProxy()) {
+							JvmDeclaredType type = (JvmDeclaredType) ref.getType();
+							if (visited.add(type))
+								candidates.push(type);
+						}
 				}
 			}
 		}
