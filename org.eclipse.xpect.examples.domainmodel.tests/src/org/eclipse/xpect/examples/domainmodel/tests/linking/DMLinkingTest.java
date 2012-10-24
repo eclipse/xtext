91adebfa78d7808ec119bfd4e3df4f36ba64de7d
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2012-10-24 16:41:16 +0200

    created a linking test Signed-off-by: Moritz Eysholdt <moritz.eysholdt@itemis.de>

---
+++ org.eclipse.xpect.examples.domainmodel.tests/src/org/eclipse/xpect/examples/domainmodel/tests/linking/DMLinkingTest.java
@@ -0,0 +1,12 @@
+package org.eclipse.xpect.examples.domainmodel.tests.linking;
+
+import org.eclipse.xpect.runner.XpectRunner;
+import org.eclipse.xpect.runner.XpectTestFiles;
+import org.eclipse.xpect.xtext.lib.tests.LinkingTest;
+import org.junit.runner.RunWith;
+
+@RunWith(XpectRunner.class)
+@XpectTestFiles(fileExtensions = "dmodel")
+public class DMLinkingTest extends LinkingTest {
+
+}
