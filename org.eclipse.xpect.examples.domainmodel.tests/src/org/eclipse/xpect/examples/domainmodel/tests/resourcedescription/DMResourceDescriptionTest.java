28c1a4e09d39f5a7df1e70ce8c4460a51b12d7e3
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2012-10-25 13:44:16 +0200

    polished the tests Signed-off-by: Moritz Eysholdt <moritz.eysholdt@itemis.de>

--- org.eclipse.xpect.examples.domainmodel.tests/src/org/eclipse/xpect/examples/domainmodel/tests/resourcedescription/DMResourceDescriptionTest.java
+++ org.eclipse.xpect.examples.domainmodel.tests/src/org/eclipse/xpect/examples/domainmodel/tests/resourcedescription/DMResourceDescriptionTest.java
@@ -1,8 +1,11 @@
 package org.eclipse.xpect.examples.domainmodel.tests.resourcedescription;
 
+import org.eclipse.xpect.runner.XpectRunner;
 import org.eclipse.xpect.runner.XpectTestFiles;
 import org.eclipse.xpect.xtext.lib.tests.ResourceDescriptionTest;
+import org.junit.runner.RunWith;
 
+@RunWith(XpectRunner.class)
 @XpectTestFiles(fileExtensions = "dmodel")
 public class DMResourceDescriptionTest extends ResourceDescriptionTest {
 
