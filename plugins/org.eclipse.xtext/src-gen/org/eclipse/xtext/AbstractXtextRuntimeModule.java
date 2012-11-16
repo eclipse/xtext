2b850b3741e5a391026da64ddea23ce00a094c6a
Jan Koehnlein <jan.koehnlein@itemis.de>
2012-11-16 15:00:11 +0100

    [xtext] bootstrapped to get rid of local compile errors due to bug 394074

--- plugins/org.eclipse.xtext/src-gen/org/eclipse/xtext/AbstractXtextRuntimeModule.java
+++ plugins/org.eclipse.xtext/src-gen/org/eclipse/xtext/AbstractXtextRuntimeModule.java
@@ -35,6 +35,11 @@ public abstract class AbstractXtextRuntimeModule extends DefaultRuntimeModule {
 	}
 	
 	// contributed by org.eclipse.xtext.generator.grammarAccess.GrammarAccessFragment
+	public java.lang.ClassLoader bindClassLoaderToInstance() {
+		return getClass().getClassLoader();
+	}
+
+	// contributed by org.eclipse.xtext.generator.grammarAccess.GrammarAccessFragment
 	public Class<? extends org.eclipse.xtext.IGrammarAccess> bindIGrammarAccess() {
 		return org.eclipse.xtext.services.XtextGrammarAccess.class;
 	}
--- plugins/org.eclipse.xtext/src-gen/org/eclipse/xtext/services/XtextGrammarAccess.java
+++ plugins/org.eclipse.xtext/src-gen/org/eclipse/xtext/services/XtextGrammarAccess.java
@@ -7,6 +7,8 @@ package org.eclipse.xtext.services;
 import com.google.inject.Singleton;
 import com.google.inject.Inject;
 
+import java.util.List;
+
 import org.eclipse.xtext.*;
 import org.eclipse.xtext.service.GrammarProvider;
 import org.eclipse.xtext.service.AbstractElementFinder.*;
@@ -1725,19 +1727,36 @@ public class XtextGrammarAccess extends AbstractGrammarElementFinder {
 	private EnumLiteralsElements pEnumLiterals;
 	private EnumLiteralDeclarationElements pEnumLiteralDeclaration;
 	
-	private final GrammarProvider grammarProvider;
+	private final Grammar grammar;
 
 	private TerminalsGrammarAccess gaTerminals;
 
 	@Inject
 	public XtextGrammarAccess(GrammarProvider grammarProvider,
 		TerminalsGrammarAccess gaTerminals) {
-		this.grammarProvider = grammarProvider;
+		this.grammar = internalFindGrammar(grammarProvider);
 		this.gaTerminals = gaTerminals;
 	}
 	
-	public Grammar getGrammar() {	
-		return grammarProvider.getGrammar(this);
+	protected Grammar internalFindGrammar(GrammarProvider grammarProvider) {
+		Grammar grammar = grammarProvider.getGrammar(this);
+		while (grammar != null) {
+			if ("org.eclipse.xtext.Xtext".equals(grammar.getName())) {
+				return grammar;
+			}
+			List<Grammar> grammars = grammar.getUsedGrammars();
+			if (!grammars.isEmpty()) {
+				grammar = grammars.iterator().next();
+			} else {
+				return null;
+			}
+		}
+		return grammar;
+	}
+	
+	
+	public Grammar getGrammar() {
+		return grammar;
 	}
 	
 
