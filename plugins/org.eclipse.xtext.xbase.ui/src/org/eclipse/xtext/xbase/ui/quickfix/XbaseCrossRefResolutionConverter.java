e573eb6a015bd0968e038403237b2d82fe84b4e3
Holger Schill <Holger.Schill@itemis.de>
2012-11-21 13:25:26 +0100

    [Quickfix] introduced CrossRefResolutionConverter see https://bugs.eclipse.org/bugs/show_bug.cgi?id=390595

---
+++ plugins/org.eclipse.xtext.xbase.ui/src/org/eclipse/xtext/xbase/ui/quickfix/XbaseCrossRefResolutionConverter.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2012 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.xbase.ui.quickfix;
+
+import org.eclipse.xtext.ui.editor.quickfix.DefaultQuickfixProvider.CrossRefResolutionConverter;
+
+/**
+ * @author Holger Schill - Initial contribution and API
+ * @since 2.4
+ */
+public class XbaseCrossRefResolutionConverter extends CrossRefResolutionConverter {
+	@Override
+	public String convertToString(String replacement, String ruleName) {
+		if (ruleName.equals("ID") || ruleName.equals("IdOrSuper"))
+			return super.convertToString(replacement, ruleName);
+		return replacement;
+	}
+}
