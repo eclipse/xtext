bea94699e8d47074a40486a10e5b949875c3b030
Sven Efftinge <sven.efftinge@itemis.de>
2012-11-23 08:27:09 +0100

    ImportNormalizer now returns null when 'resolve' is invoked with an empty string. (see #354473)

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/scoping/impl/ImportNormalizer.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/scoping/impl/ImportNormalizer.java
@@ -56,6 +56,8 @@ public class ImportNormalizer {
 	}
 
 	public QualifiedName resolve(QualifiedName relativeName) {
+		if (relativeName.isEmpty())
+			return null;
 		if (hasWildCard) {
 			return importedNamespacePrefix.append(relativeName);
 		} else {
