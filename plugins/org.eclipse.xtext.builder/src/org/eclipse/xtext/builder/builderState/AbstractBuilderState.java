4a9b464c5184fe19f4000a3274d27f5dee0b87f9
Sven Efftinge <sven.efftinge@itemis.de>
2012-11-29 14:12:54 +0100

    [builder] Some simplifications.removed unnecessary copying and indexing.

--- plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/builderState/AbstractBuilderState.java
+++ plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/builderState/AbstractBuilderState.java
@@ -7,9 +7,10 @@
  *******************************************************************************/
 package org.eclipse.xtext.builder.builderState;
 
+import static com.google.common.collect.Sets.*;
+
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -28,8 +29,6 @@ import org.eclipse.xtext.resource.impl.DefaultResourceDescriptionDelta;
 import org.eclipse.xtext.resource.impl.ResourceDescriptionChangeEvent;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
 import com.google.inject.Inject;
 
 /**
@@ -151,16 +150,15 @@ public abstract class AbstractBuilderState extends AbstractResourceDescriptionCh
 	protected Collection<IResourceDescription.Delta> doClean(Set<URI> toBeRemoved, IProgressMonitor monitor) {
 		SubMonitor subMonitor = SubMonitor.convert(monitor, Messages.AbstractBuilderState_2, toBeRemoved.size());
 		subMonitor.subTask(Messages.AbstractBuilderState_2);
-		Set<URI> toBeDeletedAsSet = Sets.newHashSet(toBeRemoved);
-		Map<URI, IResourceDescription.Delta> result = Maps.newHashMap();
-		for (URI toDelete : toBeDeletedAsSet) {
+		Set<IResourceDescription.Delta> result = newLinkedHashSet();
+		for (URI toDelete : toBeRemoved) {
 			IResourceDescription resourceDescription = getResourceDescription(toDelete);
 			if (resourceDescription != null) {
-				result.put(toDelete, new DefaultResourceDescriptionDelta(resourceDescription, null));
+				result.add(new DefaultResourceDescriptionDelta(resourceDescription, null));
 			}
 			subMonitor.worked(1);
 		}
-		return result.values();
+		return result;
 	}
 
 	public Iterable<IEObjectDescription> getExportedObjects() {
