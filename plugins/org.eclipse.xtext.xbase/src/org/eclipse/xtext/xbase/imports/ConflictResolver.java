3b64ce40c2ae2afdb4f4be3a2ec481d3a4664768
Jan Koehnlein <jan.koehnlein@itemis.de>
2012-12-17 10:28:30 +0100

    [imports] Used 'organize imports' infrastructure form Xtend to Xbase

---
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/imports/ConflictResolver.java
@@ -0,0 +1,107 @@
+/*******************************************************************************
+ * Copyright (c) 2012 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.xbase.imports;
+
+import static com.google.common.collect.Iterables.*;
+import static com.google.common.collect.Maps.*;
+import static org.eclipse.xtext.util.Strings.*;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.eclipse.xtext.common.types.JvmDeclaredType;
+import org.eclipse.xtext.common.types.JvmIdentifiableElement;
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.xbase.jvmmodel.IJvmModelAssociations;
+import org.eclipse.xtext.xtype.XImportSection;
+
+import com.google.common.collect.Multimap;
+import com.google.inject.Inject;
+
+/**
+ * @author Jan Koehnlein - Initial contribution and API
+ */
+public class ConflictResolver {
+
+	@Inject
+	private IJvmModelAssociations associations;
+	
+	@Inject
+	private IImportsConfiguration config;
+	
+	public Map<String, JvmDeclaredType> resolveConflicts(TypeUsages usages, NonOverridableTypesProvider nonOverridableTypesProvider, XtextResource resource) {
+		String packageName = config.getCommonPackageName(resource);
+		ImportCollection importSection = getImportSection(config.getImportSection(resource));
+		Map<String, JvmDeclaredType> locallyDefinedTypes = config.getLocallyDefinedTypes(resource);
+		Map<String, JvmDeclaredType> result = newLinkedHashMap();
+		Multimap<String, JvmDeclaredType> simpleName2Types = usages.getSimpleName2Types();
+		for (String simpleName : simpleName2Types.keySet()) {
+			Collection<JvmDeclaredType> types = simpleName2Types.get(simpleName);
+			JvmDeclaredType locallyDeclaredType = locallyDefinedTypes.get(simpleName);
+			if (locallyDeclaredType != null || isConflictsWithVisibleTypes(types, usages, nonOverridableTypesProvider, simpleName)) {
+				for (JvmDeclaredType type : types) {
+					if (type != locallyDeclaredType)
+						result.put(type.getIdentifier(), type);
+				}
+			} else {
+				if (types.size() == 1) {
+					result.put(simpleName, types.iterator().next());
+				} else {
+					JvmDeclaredType bestMatch = findBestMatch(types, usages, packageName, importSection);
+					for (JvmDeclaredType type : types) {
+						if (type == bestMatch)
+							result.put(simpleName, type);
+						else
+							result.put(type.getIdentifier(), type);
+					}
+				}
+			}
+		}
+		return result;
+	}
+	
+	protected boolean isConflictsWithVisibleTypes(Iterable<JvmDeclaredType> types, TypeUsages usages, 
+			NonOverridableTypesProvider nonOverridableTypesProvider, String simpleName) {
+		for(JvmDeclaredType type: types) {
+			for(TypeUsage usage: usages.getUsages(type)) {
+				JvmIdentifiableElement visibleType = nonOverridableTypesProvider.getVisibleType(usage.getContext(), simpleName);
+				if(visibleType != null && !visibleType.equals(type))
+					return true;
+			}
+		}
+		return false;
+	}
+
+	protected ImportCollection getImportSection(XImportSection xImportSection) {
+		return new ImportCollection(xImportSection);
+	}
+
+	protected JvmDeclaredType findBestMatch(Collection<JvmDeclaredType> types, TypeUsages usages, String packageName,
+			ImportCollection importCollection) {
+		Iterator<JvmDeclaredType> iterator = types.iterator();
+		JvmDeclaredType currentBestMatch = iterator.next();
+		while (iterator.hasNext()) {
+			JvmDeclaredType nextType = iterator.next();
+			if (isBetter(nextType, currentBestMatch, usages, packageName, importCollection)) {
+				currentBestMatch = nextType;
+			}
+		}
+		return currentBestMatch;
+	}
+
+	protected boolean isBetter(JvmDeclaredType candidate, JvmDeclaredType currentBestMatch, TypeUsages usages, String packageName,
+			ImportCollection importSection) {
+		if (equal(packageName,candidate.getPackageName()) && !equal(packageName, currentBestMatch.getPackageName()))
+			return true;
+		if (importSection.getImportedTypes().contains(candidate)
+				&& !importSection.getImportedTypes().contains(currentBestMatch))
+			return true;
+		return size(usages.getUsages(candidate)) > size(usages.getUsages(currentBestMatch));
+	}
+}
