d201a58d2a3820447b5c9a237da06cb51449a72b
Jan Koehnlein <jan.koehnlein@itemis.de>
2012-12-17 10:30:12 +0100

    [xbase][imports] moved import validation and quickfixes from Xtend to Xbase

--- plugins/org.eclipse.xtext.xbase.ui/src/org/eclipse/xtext/xbase/ui/quickfix/XbaseQuickfixProvider.java
+++ plugins/org.eclipse.xtext.xbase.ui/src/org/eclipse/xtext/xbase/ui/quickfix/XbaseQuickfixProvider.java
@@ -1,13 +1,40 @@
 
 package org.eclipse.xtext.xbase.ui.quickfix;
 
-import org.eclipse.xtext.ui.editor.quickfix.AbstractDeclarativeQuickfixProvider;
+import org.eclipse.xtext.ui.editor.model.edit.IModification;
+import org.eclipse.xtext.ui.editor.model.edit.IModificationContext;
+import org.eclipse.xtext.ui.editor.quickfix.DefaultQuickfixProvider;
+import org.eclipse.xtext.ui.editor.quickfix.Fix;
+import org.eclipse.xtext.ui.editor.quickfix.IssueResolutionAcceptor;
+import org.eclipse.xtext.validation.Issue;
+import org.eclipse.xtext.xbase.ui.imports.OrganizeImportsHandler;
+import org.eclipse.xtext.xbase.validation.IssueCodes;
 
-public class XbaseQuickfixProvider extends AbstractDeclarativeQuickfixProvider {
+import com.google.inject.Inject;
 
-//	@Fix(code = IssueConstants.INVALID_TYPE_NAME, label = "Capitalize name", description = "Capitalize name of type")
-//	public void fixName(Type type, IssueContext issue) {
-//		type.setName(type.getName().toUpperCase());
-//	}
+public class XbaseQuickfixProvider extends DefaultQuickfixProvider {
+
+	@Inject
+	private OrganizeImportsHandler organizeImportsHandler;
+
+	@Fix(IssueCodes.IMPORT_DUPLICATE)
+	public void fixDuplicateImport(final Issue issue, IssueResolutionAcceptor acceptor) {
+		organizeImports(issue, acceptor);
+	}
+
+	@Fix(IssueCodes.IMPORT_UNUSED)
+	public void fixUnusedImport(final Issue issue, IssueResolutionAcceptor acceptor) {
+		organizeImports(issue, acceptor);
+	}
+
+	protected void organizeImports(final Issue issue, IssueResolutionAcceptor acceptor) {
+		acceptor.accept(issue, "Organize Imports.",
+				"Organizes the whole import section. Removes wildcard imports as well as duplicates and unused ones.",
+				"fix_indent.gif", new IModification() {
+					public void apply(IModificationContext context) throws Exception {
+						organizeImportsHandler.doOrganizeImports(context.getXtextDocument());
+					}
+				});
+	}
 
 }
