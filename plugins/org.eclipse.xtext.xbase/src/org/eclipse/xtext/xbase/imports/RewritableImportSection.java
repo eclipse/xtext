ccb739a0e5f47774c8b75ec7117fbb9311f4e123
Jan Koehnlein <jan.koehnlein@itemis.de>
2012-12-18 17:51:38 +0100

    [imports] allow to remove imports form rewritable import section

--- plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/imports/RewritableImportSection.java
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/imports/RewritableImportSection.java
@@ -10,15 +10,19 @@ package org.eclipse.xtext.xbase.imports;
 import static com.google.common.collect.Lists.*;
 import static com.google.common.collect.Maps.*;
 import static com.google.common.collect.Sets.*;
-import static java.util.Collections.*;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.xtext.common.types.JvmDeclaredType;
 import org.eclipse.xtext.formatting.IWhitespaceInformationProvider;
+import org.eclipse.xtext.nodemodel.ICompositeNode;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.util.IAcceptor;
+import org.eclipse.xtext.util.ITextRegion;
 import org.eclipse.xtext.util.ReplaceRegion;
 import org.eclipse.xtext.util.TextRegion;
 import org.eclipse.xtext.xtype.XImportDeclaration;
@@ -52,7 +56,9 @@ public class RewritableImportSection {
 
 	private List<XImportDeclaration> originalImportDeclarations = newArrayList();
 
-	private List<XImportDeclaration> newImportDeclarations = newArrayList();
+	private List<XImportDeclaration> addedImportDeclarations = newArrayList();
+
+	private Set<XImportDeclaration> removedImportDeclarations = newLinkedHashSet();
 
 	private Map<String, JvmDeclaredType> plainImports = newHashMap();
 
@@ -94,10 +100,41 @@ public class RewritableImportSection {
 		plainImports.put(type.getSimpleName(), type);
 		XImportDeclaration importDeclaration = XtypeFactory.eINSTANCE.createXImportDeclaration();
 		importDeclaration.setImportedType(type);
-		newImportDeclarations.add(importDeclaration);
+		addedImportDeclarations.add(importDeclaration);
 		return true;
 	}
 	
+	public boolean removeImport(JvmDeclaredType type) {
+		XImportDeclaration importDeclaration = findOriginalImport(type, originalImportDeclarations, false, false);
+		if(importDeclaration != null) 
+			removedImportDeclarations.add(importDeclaration);
+		else {
+			importDeclaration = findOriginalImport(type, addedImportDeclarations, false, false);
+			if(importDeclaration != null)
+				addedImportDeclarations.remove(importDeclaration);
+		}
+		if(importDeclaration != null) {
+			for(Map.Entry<String, JvmDeclaredType> entry: plainImports.entrySet()) {
+				if(entry.getValue() == type) {
+					plainImports.remove(entry.getKey());
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	protected XImportDeclaration findOriginalImport(JvmDeclaredType type, Collection<XImportDeclaration> list,
+			boolean isStatic, boolean isExtension) {
+		for(XImportDeclaration importDeclaration: list) {
+			if(!(isStatic ^ importDeclaration.isStatic())
+					&& !(isExtension ^ importDeclaration.isExtension())
+					&& importDeclaration.getImportedType()==type) 
+				return importDeclaration;
+		}
+		return null;
+	}
+
 	public JvmDeclaredType getType(String simpleName) {
 		return plainImports.get(simpleName);
 	}
@@ -109,9 +146,23 @@ public class RewritableImportSection {
 		XImportDeclaration importDeclaration = XtypeFactory.eINSTANCE.createXImportDeclaration();
 		importDeclaration.setImportedType(type);
 		importDeclaration.setStatic(true);
-		newImportDeclarations.add(importDeclaration);
+		addedImportDeclarations.add(importDeclaration);
 		return true;
 	}
+
+	public boolean removeStaticImport(JvmDeclaredType type) {
+		XImportDeclaration importDeclaration = findOriginalImport(type, originalImportDeclarations, true, false);
+		if(importDeclaration != null) 
+			removedImportDeclarations.add(importDeclaration);
+		else {
+			importDeclaration = findOriginalImport(type, addedImportDeclarations, true, false);
+			if(importDeclaration != null)
+				addedImportDeclarations.remove(importDeclaration);
+		}
+		staticImports.remove(type);
+		return importDeclaration != null;
+	}
+	
 
 	public boolean addStaticExtensionImport(JvmDeclaredType type) {
 		if (staticExtensionImports.contains(type))
@@ -121,13 +172,49 @@ public class RewritableImportSection {
 		importDeclaration.setImportedType(type);
 		importDeclaration.setStatic(true);
 		importDeclaration.setExtension(true);
-		newImportDeclarations.add(importDeclaration);
+		addedImportDeclarations.add(importDeclaration);
 		return true;
 	}
 
+	public boolean removeStaticExtensionImport(JvmDeclaredType type) {
+		XImportDeclaration importDeclaration = findOriginalImport(type, originalImportDeclarations, true, true);
+		if(importDeclaration != null) 
+			removedImportDeclarations.add(importDeclaration);
+		else {
+			importDeclaration = findOriginalImport(type, addedImportDeclarations, true, true);
+			if(importDeclaration != null)
+				addedImportDeclarations.remove(importDeclaration);
+		}
+		staticExtensionImports.remove(type);
+		return importDeclaration != null;
+	}
+
 	public List<ReplaceRegion> rewrite() {
+		final List<ReplaceRegion> replaceRegions = newArrayList();
+		for(XImportDeclaration removedImportDeclaration: removedImportDeclarations) {
+			ICompositeNode node = NodeModelUtils.findActualNodeFor(removedImportDeclaration);
+			if(node != null) {
+				ITextRegion textRegion = new TextRegion(node.getOffset(), node.getLength());
+				if(removedImportDeclaration!=originalImportDeclarations.get(originalImportDeclarations.size()-1) 
+						|| addedImportDeclarations.isEmpty()) {
+					textRegion = regionUtil.addTrailingSingleWhitespace(textRegion, lineSeparator, resource);
+				}
+				replaceRegions.add(new ReplaceRegion(textRegion, ""));
+			}
+		}
+		addSectionToAppend(new IAcceptor<ReplaceRegion>() {
+			public void accept(ReplaceRegion t) {
+				replaceRegions.add(t);
+			}
+		});
+		return replaceRegions;
+	}
+
+	protected void addSectionToAppend(IAcceptor<ReplaceRegion> acceptor) {
 		StringBuilder importDeclarationsToAppend = getImportDeclarationsToAppend();
-		TextRegion region = regionUtil.computeRegion(resource);
+		if(importDeclarationsToAppend.length() ==0) 
+			return;
+		ITextRegion region = regionUtil.computeRegion(resource);
 		region = regionUtil.addLeadingWhitespace(region, resource);
 		int insertOffset = region.getOffset() + region.getLength();
 		if(insertOffset != 0) {
@@ -138,24 +225,29 @@ public class RewritableImportSection {
 		importDeclarationsToAppend.append(lineSeparator);
 		int insertLength = -region.getLength();
 		insertLength += regionUtil.addTrailingWhitespace(region, resource).getLength();
-		return singletonList(new ReplaceRegion(new TextRegion(insertOffset, insertLength), importDeclarationsToAppend.toString()));
+		ReplaceRegion appendDeclarations = new ReplaceRegion(new TextRegion(insertOffset, insertLength), importDeclarationsToAppend.toString());
+		acceptor.accept(appendDeclarations);
 	}
 
 	protected StringBuilder getImportDeclarationsToAppend() {
 		StringBuilder builder = new StringBuilder();
-		for (XImportDeclaration newImportDeclaration : newImportDeclarations) {
-			builder.append("import ");
-			if (newImportDeclaration.isStatic()) {
-				builder.append("static ");
-				if (newImportDeclaration.isExtension()) {
-					builder.append("extension ");
-				}
-			}
-			builder.append(newImportDeclaration.getImportedTypeName());
-			if (newImportDeclaration.isStatic())
-				builder.append(".*");
-			builder.append(lineSeparator);
+		for (XImportDeclaration newImportDeclaration : addedImportDeclarations) {
+			appendImport(builder, newImportDeclaration);
 		}
 		return builder;
+	}
+
+	protected void appendImport(StringBuilder builder, XImportDeclaration newImportDeclaration) {
+		builder.append("import ");
+		if (newImportDeclaration.isStatic()) {
+			builder.append("static ");
+			if (newImportDeclaration.isExtension()) {
+				builder.append("extension ");
+			}
+		}
+		builder.append(newImportDeclaration.getImportedTypeName());
+		if (newImportDeclaration.isStatic())
+			builder.append(".*");
+		builder.append(lineSeparator);
 	}
 }
