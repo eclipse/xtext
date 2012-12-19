2adc773b5dd5ebed622541116d1c0b672b733179
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2012-12-19 17:12:59 +0100

    [typesystem][tests] Added more tests, fixes for type parameter substitution
    Combinations of Type<? super T> and Type<? extends T> work better, now

---
+++ tests/org.eclipse.xtend.core.tests/testdata/bug396879/BooleanProperty.java
@@ -0,0 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2012 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package bug396879;
+
+public abstract class BooleanProperty implements ObservableValue<Boolean> {
+}
