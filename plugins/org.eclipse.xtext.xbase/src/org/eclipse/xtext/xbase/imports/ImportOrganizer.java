c3c760775fd49fb38ea44990f219f02f2a787ebe
Jan Koehnlein <jan.koehnlein@itemis.de>
2013-01-04 18:05:50 +0100

    [xbase][imports] major bug

--- plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/imports/ImportOrganizer.java
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/imports/ImportOrganizer.java
@@ -63,7 +63,7 @@ public class ImportOrganizer {
 			String text = entry.getKey();
 			JvmDeclaredType type = entry.getValue();
 			Iterable<TypeUsage> usages = typeUsages.getUsages(type);
-			if(!needsImport(type, text, nonOverridableTypesProvider, usages)) {
+			if(needsImport(type, text, nonOverridableTypesProvider, usages)) {
 				newImportSection.addImport(type);
 			}
 			for(TypeUsage usage: usages) {
