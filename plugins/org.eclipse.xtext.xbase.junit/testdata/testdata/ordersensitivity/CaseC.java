2a1799a7906659af05393767bb574e8a23bd1bb9
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2013-01-04 10:02:27 +0100

    [xbase][tests] Currently disabled test cases for bug 397424
    see https://bugs.eclipse.org/bugs/show_bug.cgi?id=397424

---
+++ plugins/org.eclipse.xtext.xbase.junit/testdata/testdata/ordersensitivity/CaseC.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2012 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package testdata.ordersensitivity;
+
+/**
+ * @author Sebastian Zarnekow - Initial contribution and API
+ * @since 2.4
+ */
+public class CaseC {
+
+	public interface Runner {
+        public void run();
+    }
+
+    public interface RunnerWithResult<T> {
+        public T run();
+    }
+
+    public void run(Runner runner) {
+        runner.run();
+    }
+
+    public <T> T run(RunnerWithResult<T> runner) {
+        return runner.run();
+    } 
+    
+}
