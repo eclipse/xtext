451ebf4d9838a8d321a0cf627f7f36c6133eb2e9
Sven Efftinge <sven.efftinge@itemis.de>
2013-01-06 18:35:57 +0100

    [xtend/macro] WIP - more progress on modifiable view on JVM types (i.e. Generated<Bla>)

--- plugins/org.eclipse.xtend.lib/src/org/eclipse/xtend/lib/macro/declaration/CompilationUnit.java
+++ plugins/org.eclipse.xtend.lib/src/org/eclipse/xtend/lib/macro/declaration/CompilationUnit.java
@@ -18,6 +18,21 @@ public interface CompilationUnit extends AnnotationTarget {
 	String getDocComment();
 	
 	List<SourceTypeDeclaration> getSourceTypeDeclarations();
+	List<SourceClassDeclaration> getSourceClassDeclarations();
 	
 	List<GeneratedTypeDeclaration> getGeneratedTypeDeclarations();
+	List<GeneratedClassDeclaration> getGeneratedClassDeclarations();
+	
+	// factory and finder methods
+//	GeneratedClassDeclaration modifyClass(String name, Procedures.Procedure1<GeneratedClassDeclaration> initializer);
+//	GeneratedClassDeclaration newClass(String name, Procedures.Procedure1<GeneratedClassDeclaration> initializer);
+//	
+//	
+//	// type references
+//	TypeReference typeRef(String name, TypeReference ... actualArguments);
+//	TypeReference wildCard(TypeReference upperBound, TypeReference lowerBound);
+//	TypeReference primitiveVoid();
+//	TypeReference primitiveBoolean();
+//	TypeReference primitiveInt();
+//	TypeReference primitiveLong();
 }
--- plugins/org.eclipse.xtend.lib/src/org/eclipse/xtend/lib/macro/declaration/GeneratedClassDeclaration.java
+++ plugins/org.eclipse.xtend.lib/src/org/eclipse/xtend/lib/macro/declaration/GeneratedClassDeclaration.java
@@ -1,5 +1,13 @@
 package org.eclipse.xtend.lib.macro.declaration;
 
-public interface GeneratedClassDeclaration extends GeneratedTypeDeclaration, ClassDeclaration {
+import java.util.List;
 
+import org.eclipse.xtend.lib.macro.type.TypeReference;
+
+public interface GeneratedClassDeclaration extends GeneratedTypeDeclaration, ClassDeclaration {
+	public void setSuperclass(TypeReference superclass);
+	public void setImplementedInterfaces(List<? extends TypeReference> superclass);
+	void setAbstract(boolean isAbstract);
+	void setFinal(boolean isFinal);
+	void setStatic(boolean isStatic);
 }
