e01cdaee53a8a542f54746f0892ae9e4087a0cba
Dennis Huebner <dennis.huebner@itemis.de>
2013-01-08 16:58:20 +0100

    [validation-ui] new 'unused stuff' section, added some more settings

--- plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/validation/XbaseConfigurableIssueCodes.java
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/validation/XbaseConfigurableIssueCodes.java
@@ -14,24 +14,42 @@ import java.util.Map;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.xtext.preferences.PreferenceKey;
 import org.eclipse.xtext.validation.ConfigurableIssueCodesProvider;
+import org.eclipse.xtext.validation.SeverityConverter;
 
 /**
+ * This class holds all the configurable issue codes for the Xbase language.<br>
+ * Use {@link #getConfigurableIssueCodes()} to get all registered codes.<br>
+ * 
+ * 
  * @author Sven Efftinge - Initial contribution and API
+ * @author Dennis Huebner
  */
 public class XbaseConfigurableIssueCodes extends ConfigurableIssueCodesProvider {
 
-	public static final PreferenceKey FORBIDDEN_REFERENCE = new PreferenceKey(IssueCodes.FORBIDDEN_REFERENCE, JavaCore.COMPILER_PB_FORBIDDEN_REFERENCE);
-	public static final PreferenceKey DISCOURAGED_REFERENCE = new PreferenceKey(IssueCodes.DISCOURAGED_REFERENCE, JavaCore.COMPILER_PB_DISCOURAGED_REFERENCE);
+	private static final Map<String, PreferenceKey> _allConfigurableCodes = newLinkedHashMap();
+
+
+	public static final PreferenceKey FORBIDDEN_REFERENCE = create(IssueCodes.FORBIDDEN_REFERENCE, JavaCore.COMPILER_PB_FORBIDDEN_REFERENCE);
+	public static final PreferenceKey DISCOURAGED_REFERENCE = create(IssueCodes.DISCOURAGED_REFERENCE, JavaCore.COMPILER_PB_DISCOURAGED_REFERENCE);
+	public static final PreferenceKey IMPORT_WILDCARD_DEPRECATED = create(IssueCodes.IMPORT_WILDCARD_DEPRECATED, SeverityConverter.SEVERITY_WARNING);
+	 
+	public static final PreferenceKey OBSOLETE_INSTANCEOF = create(IssueCodes.OBSOLETE_INSTANCEOF, JavaCore.COMPILER_PB_UNNECESSARY_TYPE_CHECK);
+	public static final PreferenceKey IMPORT_UNUSED = create(IssueCodes.IMPORT_UNUSED, JavaCore.COMPILER_PB_UNUSED_IMPORT);
+	public static final PreferenceKey UNUSED_LOCAL_VARIABLE = create(IssueCodes.UNUSED_LOCAL_VARIABLE, JavaCore.COMPILER_PB_UNUSED_LOCAL);
 	
-	private static final Map<String, PreferenceKey> allConfigurableCodes = newLinkedHashMap();
-	
-	static {
-		allConfigurableCodes.put(FORBIDDEN_REFERENCE.getId(), FORBIDDEN_REFERENCE);
-		allConfigurableCodes.put(DISCOURAGED_REFERENCE.getId(), DISCOURAGED_REFERENCE);
+	/**
+	 * If you would like to a add a new IssueCode use this method,<br>
+	 * this method creates a new {@link PreferenceKey} and adds it to the inner registry map.
+	 */
+	private static PreferenceKey create(String id, String defaultvalue) {
+		PreferenceKey prefKey = new PreferenceKey(id, defaultvalue);
+		_allConfigurableCodes.put(prefKey.getId(), prefKey);
+		return prefKey;
 	}
-	
+
 	@Override
 	public Map<String, PreferenceKey> getConfigurableIssueCodes() {
-		return allConfigurableCodes;
+		return _allConfigurableCodes;
 	}
+
 }
