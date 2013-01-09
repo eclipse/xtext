1287c9c9ad0f4b85ab1b2ba1edf74be583bcc5fc
Dennis Huebner <dennis.huebner@itemis.de>
2013-01-09 17:21:22 +0100

    [validation] Default severity for delegated issue codes

--- plugins/org.eclipse.xtext.xbase.ui/src/org/eclipse/xtext/xbase/ui/validation/XbaseIssueSeveritiesProvider.java
+++ plugins/org.eclipse.xtext.xbase.ui/src/org/eclipse/xtext/xbase/ui/validation/XbaseIssueSeveritiesProvider.java
@@ -9,14 +9,13 @@ package org.eclipse.xtext.xbase.ui.validation;
 
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.xtext.common.types.access.jdt.IJavaProjectProvider;
-import org.eclipse.xtext.diagnostics.Severity;
 import org.eclipse.xtext.preferences.IPreferenceValues;
+import org.eclipse.xtext.util.Pair;
+import org.eclipse.xtext.validation.ConfigurableIssueCodesProvider;
 import org.eclipse.xtext.validation.IssueSeverities;
 import org.eclipse.xtext.validation.IssueSeveritiesProvider;
-import org.eclipse.xtext.validation.SeverityConverter;
-import org.eclipse.xtext.xbase.validation.XbaseConfigurableIssueCodes;
+import org.eclipse.xtext.xbase.validation.XbaseSeverityConverter;
 
 import com.google.inject.Inject;
 
@@ -24,22 +23,35 @@ import com.google.inject.Inject;
  * @author Sven Efftinge - Initial contribution and API
  */
 public class XbaseIssueSeveritiesProvider extends IssueSeveritiesProvider {
-	
-	@Inject private IJavaProjectProvider projectProvider;
-	@Inject private XbaseConfigurableIssueCodes configurableIssueCodes;
+
+	@Inject
+	private IJavaProjectProvider projectProvider;
+	@Inject
+	private ConfigurableIssueCodesProvider configurableIssueCodes;
 
 	@Override
-	public IssueSeverities getIssueSeverities(Resource context) {
-		final IJavaProject project = projectProvider.getJavaProject(context.getResourceSet());
+	public IssueSeverities getIssueSeverities(final Resource context) {
 		IPreferenceValues preferenceValues = getValuesProvider().getPreferenceValues(context);
-		return new IssueSeverities(preferenceValues, configurableIssueCodes.getConfigurableIssueCodes(), new SeverityConverter() {
-			@Override
-			public Severity stringToSeverity(String severityAsString) {
-				if (severityAsString.startsWith(JavaCore.PLUGIN_ID)) {
-					return super.stringToSeverity(project.getOption(severityAsString, true));
-				}
-				return super.stringToSeverity(severityAsString);
-			}
-		});
+		return new IssueSeverities(preferenceValues, configurableIssueCodes.getConfigurableIssueCodes(),
+				new XbaseSeverityConverter() {
+
+					final IJavaProject project = projectProvider.getJavaProject(context.getResourceSet());
+
+					@Override
+					protected String delegatedValue(Pair<String, String> pair) {
+						String jdtSeverity = jdtSeverity(project, pair.getFirst());
+						if (jdtSeverity == null || "".equals(jdtSeverity)) {
+							return super.delegatedValue(pair);
+						}
+						return jdtSeverity;
+					}
+
+					private String jdtSeverity(final IJavaProject project, String severityAsString) {
+						if (project == null) {
+							return null;
+						}
+						return project.getOption(severityAsString, true);
+					}
+				});
 	}
 }
---
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/validation/XbaseSeverityConverter.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2013 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.xbase.validation;
+
+import java.util.List;
+
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.xtext.diagnostics.Severity;
+import org.eclipse.xtext.util.Pair;
+import org.eclipse.xtext.util.Strings;
+import org.eclipse.xtext.util.Tuples;
+import org.eclipse.xtext.validation.SeverityConverter;
+
+/**
+ * @author Dennis Huebner - Initial contribution and API
+ */
+public class XbaseSeverityConverter extends SeverityConverter {
+	
+	private static final char DEFAULT_SEVERITY_SEPARATOR = ':';
+
+	@Override
+	public Severity stringToSeverity(final String severityAsString) {
+		String valueToConvert = severityAsString;
+		if (severityAsString.startsWith(JavaCore.PLUGIN_ID)) {
+			valueToConvert = delegatedValue(decodeDelegationKey(severityAsString));
+		}
+		return super.stringToSeverity(valueToConvert);
+	}
+
+	/**
+	 * Pair contains <delegationKey,defaultValue>
+	 */
+	protected String delegatedValue(Pair<String, String> pair) {
+		return pair.getSecond();
+	}
+
+	public static String encodeDefaultSeverity(String delegationKey, String defaultSeverity) {
+		return new StringBuilder(delegationKey).append(DEFAULT_SEVERITY_SEPARATOR).append(defaultSeverity).toString();
+	}
+
+	public static Pair<String, String> decodeDelegationKey(String encodedValue) {
+		List<String> split = Strings.split(encodedValue, DEFAULT_SEVERITY_SEPARATOR);
+		if (split.size() == 2) {
+			return Tuples.create(split.get(0), split.get(1));
+		} else if (split.size() == 1) {
+			return Tuples.create(split.get(0), SeverityConverter.SEVERITY_WARNING);
+		} else
+			return null;
+	}
+}
