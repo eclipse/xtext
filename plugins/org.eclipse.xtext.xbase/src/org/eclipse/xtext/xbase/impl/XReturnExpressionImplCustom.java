b2f8e0a32326d6fb3bb2aefc4a9b591a30f22b36
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2013-01-10 21:53:11 +0100

    [xbase][ast] Some more #toString implementations on XExpressions

--- plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/impl/XReturnExpressionImplCustom.java
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/impl/XReturnExpressionImplCustom.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 itemis AG (http://www.itemis.eu) and others.
+ * Copyright (c) 2013 itemis AG (http://www.itemis.eu) and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,11 +10,13 @@ package org.eclipse.xtext.xbase.impl;
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
  */
-public class XNullLiteralImplCustom extends XNullLiteralImpl {
+public class XReturnExpressionImplCustom extends XReturnExpressionImpl {
 
 	@Override
 	public String toString() {
-		return "null /*literal*/";
+		if (expression != null)
+			return String.format("return %s;", expression);
+		return "return;";
 	}
 	
 }
