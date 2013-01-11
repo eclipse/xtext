b4364ae50b2f1ffaf7b86162723de81bc3ac7fe5
Sven Efftinge <sven.efftinge@itemis.de>
2013-01-11 16:16:23 +0100

    [xtend macro] fixed failing test

--- plugins/org.eclipse.xtend.lib/src/org/eclipse/xtend/lib/macro/DeclarationFactory.java
+++ plugins/org.eclipse.xtend.lib/src/org/eclipse/xtend/lib/macro/DeclarationFactory.java
@@ -1,20 +1,20 @@
 package org.eclipse.xtend.lib.macro;
 
-import org.eclipse.xtend.lib.macro.declaration.ClassDeclaration;
-import org.eclipse.xtend.lib.macro.declaration.ConstructorDeclaration;
-import org.eclipse.xtend.lib.macro.declaration.FieldDeclaration;
-import org.eclipse.xtend.lib.macro.declaration.MethodDeclaration;
 import org.eclipse.xtend.lib.macro.declaration.MutableClassDeclaration;
 import org.eclipse.xtend.lib.macro.declaration.MutableConstructorDeclaration;
+import org.eclipse.xtend.lib.macro.declaration.MutableExecutableDeclaration;
 import org.eclipse.xtend.lib.macro.declaration.MutableFieldDeclaration;
 import org.eclipse.xtend.lib.macro.declaration.MutableMethodDeclaration;
+import org.eclipse.xtend.lib.macro.declaration.MutableParameterDeclaration;
+import org.eclipse.xtend.lib.macro.declaration.MutableTypeDeclaration;
+import org.eclipse.xtend.lib.macro.type.TypeReference;
 import org.eclipse.xtext.xbase.lib.Procedures;
 
 public interface DeclarationFactory {
 	
-	FieldDeclaration newField(Procedures.Procedure1<MutableFieldDeclaration> initializer);
-	MethodDeclaration newMethod(Procedures.Procedure1<MutableMethodDeclaration> initializer);
-	ConstructorDeclaration newConstructor(Procedures.Procedure1<MutableConstructorDeclaration> initializer);
-	ClassDeclaration newClass(Procedures.Procedure1<MutableClassDeclaration> initializer);
+	MutableFieldDeclaration newField(MutableClassDeclaration declarator, String simpleName, Procedures.Procedure1<MutableFieldDeclaration> initializer);
+	MutableMethodDeclaration newMethod(MutableTypeDeclaration declarator, String simpleName, Procedures.Procedure1<MutableMethodDeclaration> initializer);
+	MutableParameterDeclaration newParameter(MutableExecutableDeclaration declarator, String simpleName, TypeReference type);
+	MutableConstructorDeclaration newConstructor(MutableClassDeclaration declarator, Procedures.Procedure1<MutableConstructorDeclaration> initializer);
 	
 }
