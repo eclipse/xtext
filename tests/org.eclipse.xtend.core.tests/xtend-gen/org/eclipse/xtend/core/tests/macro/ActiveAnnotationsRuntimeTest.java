a46766da83e3ee1091079b90eb5525e46ce6174d
Sven Efftinge <sven.efftinge@itemis.de>
2013-01-22 13:43:41 +0100

    [xtend macros] make runtime tests runnable as Plug-in tests.

--- tests/org.eclipse.xtend.core.tests/xtend-gen/org/eclipse/xtend/core/tests/macro/ActiveAnnotationsRuntimeTest.java
+++ tests/org.eclipse.xtend.core.tests/xtend-gen/org/eclipse/xtend/core/tests/macro/ActiveAnnotationsRuntimeTest.java
@@ -1,6 +1,7 @@
 package org.eclipse.xtend.core.tests.macro;
 
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import org.eclipse.emf.common.util.EList;
@@ -12,6 +13,7 @@ import org.eclipse.xtend.core.tests.RuntimeInjectorProvider;
 import org.eclipse.xtend.core.tests.macro.AbstractActiveAnnotationsTest;
 import org.eclipse.xtend.core.tests.macro.DelegatingClassloader;
 import org.eclipse.xtend.core.xtend.XtendFile;
+import org.eclipse.xtend.lib.macro.declaration.MutableTypeDeclaration;
 import org.eclipse.xtext.junit4.InjectWith;
 import org.eclipse.xtext.junit4.XtextRunner;
 import org.eclipse.xtext.resource.XtextResourceSet;
@@ -23,6 +25,7 @@ import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.IterableExtensions;
 import org.eclipse.xtext.xbase.lib.Pair;
 import org.eclipse.xtext.xbase.lib.Procedures.Procedure1;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -36,11 +39,20 @@ public class ActiveAnnotationsRuntimeTest extends AbstractActiveAnnotationsTest 
   @Inject
   private Provider<CompilationUnitImpl> compilationUnitProvider;
   
+  @Before
+  public void setUp() {
+    this.compiler.setJavaCompilerClassPath(MutableTypeDeclaration.class, IterableExtensions.class, Lists.class);
+  }
+  
   public void assertProcessing(final Pair<String,String> macroFile, final Pair<String,String> clientFile, final Procedure1<? super CompilationUnitImpl> expectations) {
     try {
-      ResourceSet _unLoadedResourceSet = this.compiler.unLoadedResourceSet(clientFile);
-      final XtextResourceSet resourceSet = ((XtextResourceSet) _unLoadedResourceSet);
-      String _value = macroFile.getValue();
+      ResourceSet _unLoadedResourceSet = this.compiler.unLoadedResourceSet(macroFile);
+      final XtextResourceSet macroResourceSet = ((XtextResourceSet) _unLoadedResourceSet);
+      Class<? extends Object> _class = this.getClass();
+      ClassLoader _classLoader = _class.getClassLoader();
+      macroResourceSet.setClasspathURIContext(_classLoader);
+      ResourceSet _unLoadedResourceSet_1 = this.compiler.unLoadedResourceSet(clientFile);
+      final XtextResourceSet resourceSet = ((XtextResourceSet) _unLoadedResourceSet_1);
       final Procedure1<Result> _function = new Procedure1<Result>() {
           public void apply(final Result result) {
             Class<? extends Object> _class = ActiveAnnotationsRuntimeTest.this.getClass();
@@ -55,7 +67,7 @@ public class ActiveAnnotationsRuntimeTest extends AbstractActiveAnnotationsTest 
             resourceSet.setClasspathURIContext(_delegatingClassloader);
           }
         };
-      this.compiler.compile(_value, new IAcceptor<Result>() {
+      this.compiler.compile(macroResourceSet, new IAcceptor<Result>() {
           public void accept(Result t) {
             _function.apply(t);
           }
