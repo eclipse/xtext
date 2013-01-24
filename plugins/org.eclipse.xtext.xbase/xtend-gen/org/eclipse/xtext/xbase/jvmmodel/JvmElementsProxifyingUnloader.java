6f98c079c4356e611e81bf452e6bd3c15864663d
Sven Efftinge <sven.efftinge@itemis.de>
2013-01-24 13:56:35 +0100

    [xbase] do depth-first proxification to avoid populating a list, clear adapters.

--- plugins/org.eclipse.xtext.xbase/xtend-gen/org/eclipse/xtext/xbase/jvmmodel/JvmElementsProxifyingUnloader.java
+++ plugins/org.eclipse.xtext.xbase/xtend-gen/org/eclipse/xtext/xbase/jvmmodel/JvmElementsProxifyingUnloader.java
@@ -1,7 +1,7 @@
 package org.eclipse.xtext.xbase.jvmmodel;
 
 import com.google.common.annotations.Beta;
-import java.util.List;
+import org.eclipse.emf.common.notify.Adapter;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
@@ -9,29 +9,22 @@ import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.xtext.common.types.JvmMember;
 import org.eclipse.xtext.parser.antlr.IReferableElementsUnloader;
-import org.eclipse.xtext.xbase.lib.CollectionLiterals;
 
 @Beta
 @SuppressWarnings("all")
 public class JvmElementsProxifyingUnloader implements IReferableElementsUnloader {
   public void unloadRoot(final EObject root) {
-    final List<JvmMember> toBeProxified = CollectionLiterals.<JvmMember>newLinkedList();
     boolean _matched = false;
     if (!_matched) {
       if (root instanceof JvmMember) {
         final JvmMember _jvmMember = (JvmMember)root;
         _matched=true;
-        this.unloadRecursively(_jvmMember, toBeProxified);
+        this.unloadRecursively(_jvmMember);
       }
-    }
-    for (final JvmMember toProxify : toBeProxified) {
-      URI _uRI = EcoreUtil.getURI(toProxify);
-      ((InternalEObject) toProxify).eSetProxyURI(_uRI);
     }
   }
   
-  protected void unloadRecursively(final JvmMember element, final List<JvmMember> toBeProxified) {
-    toBeProxified.add(element);
+  protected void unloadRecursively(final JvmMember element) {
     EList<EObject> _eContents = element.eContents();
     for (final EObject child : _eContents) {
       boolean _matched = false;
@@ -39,9 +32,13 @@ public class JvmElementsProxifyingUnloader implements IReferableElementsUnloader
         if (child instanceof JvmMember) {
           final JvmMember _jvmMember = (JvmMember)child;
           _matched=true;
-          this.unloadRecursively(_jvmMember, toBeProxified);
+          this.unloadRecursively(_jvmMember);
         }
       }
     }
+    EList<Adapter> _eAdapters = element.eAdapters();
+    _eAdapters.clear();
+    URI _uRI = EcoreUtil.getURI(element);
+    ((InternalEObject) element).eSetProxyURI(_uRI);
   }
 }
