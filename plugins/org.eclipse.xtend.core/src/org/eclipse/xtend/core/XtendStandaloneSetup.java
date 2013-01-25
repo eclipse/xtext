aa21c9e6bb977ad2a103677d72a7f9cd8614b780
Sven Efftinge <sven.efftinge@itemis.de>
2013-01-25 13:22:47 +0100

    [xtend] only create one injector during StandaloneSetup

--- plugins/org.eclipse.xtend.core/src/org/eclipse/xtend/core/XtendStandaloneSetup.java
+++ plugins/org.eclipse.xtend.core/src/org/eclipse/xtend/core/XtendStandaloneSetup.java
@@ -2,8 +2,12 @@
 package org.eclipse.xtend.core;
 
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.xtend.core.xtend.XtendPackage;
-import org.eclipse.xtext.xbase.XbaseStandaloneSetup;
+import org.eclipse.xtext.common.types.TypesPackage;
+import org.eclipse.xtext.xbase.XbasePackage;
+import org.eclipse.xtext.xbase.annotations.xAnnotations.XAnnotationsPackage;
+import org.eclipse.xtext.xtype.XtypePackage;
 
 import com.google.inject.Injector;
 
@@ -15,13 +19,32 @@ public class XtendStandaloneSetup extends XtendStandaloneSetupGenerated{
 	
 	@Override
 	public Injector createInjectorAndDoEMFRegistration() {
-		new XbaseStandaloneSetup().createInjectorAndDoEMFRegistration();
+		// don't call super, since that would create useless injectors for all the super languages.
+		// register default ePackages
+		if (!Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().containsKey("ecore"))
+			Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
+					"ecore", new org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl());
+		if (!Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().containsKey("xmi"))
+			Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
+					"xmi", new org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl());
+		if (!EPackage.Registry.INSTANCE.containsKey(org.eclipse.xtext.XtextPackage.eNS_URI))
+			EPackage.Registry.INSTANCE.put(org.eclipse.xtext.XtextPackage.eNS_URI, org.eclipse.xtext.XtextPackage.eINSTANCE);
+		
+//		// register used EPackages
 		EPackage.Registry.INSTANCE.put(XtendPackage.eINSTANCE.getNsURI(),XtendPackage.eINSTANCE);
-		return super.createInjectorAndDoEMFRegistration();
+		EPackage.Registry.INSTANCE.put(XbasePackage.eINSTANCE.getNsURI(),XbasePackage.eINSTANCE);
+		EPackage.Registry.INSTANCE.put(XAnnotationsPackage.eINSTANCE.getNsURI(),XAnnotationsPackage.eINSTANCE);
+		EPackage.Registry.INSTANCE.put(TypesPackage.eINSTANCE.getNsURI(), TypesPackage.eINSTANCE);
+		EPackage.Registry.INSTANCE.put(XtypePackage.eINSTANCE.getNsURI(), XtypePackage.eINSTANCE);
+		
+		Injector injector = createInjector();
+		register(injector);
+		return injector;
 	}
 
 	public static void doSetup() {
 		new XtendStandaloneSetup().createInjectorAndDoEMFRegistration();
 	}
+
 }
 
