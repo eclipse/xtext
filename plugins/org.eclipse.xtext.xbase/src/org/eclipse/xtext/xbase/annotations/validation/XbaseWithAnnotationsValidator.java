98d7260345d98b66340d25059df488047bb1f439
Jan Koehnlein <jan.koehnlein@itemis.de>
2013-01-31 17:41:05 +0100

    [xbase][generator] Better support for Xbase languages generated with the ValidatorFragment
    Change-Id: Ie5601a23a59a736fea3a46e2c61a611884b43506

---
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/annotations/validation/XbaseWithAnnotationsValidator.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2013 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.xbase.annotations.validation;
+
+import org.eclipse.xtext.generator.validation.JavaValidatorFragment;
+import org.eclipse.xtext.generator.validation.ValidatorFragment;
+
+/**
+ * Allows to use the {@link ValidatorFragment} instead of the {@link JavaValidatorFragment} for Xbase languages.
+ * 
+ * @author Jan Koehnlein - Initial contribution and API
+ */
+public class XbaseWithAnnotationsValidator extends XbaseWithAnnotationsJavaValidator {
+
+}
