c57451ca934305262d61c8664fc45adfec74c490
Holger Schill <Holger.Schill@itemis.de>
2013-02-04 15:00:45 +0100

    Generated Code View is broken see https://bugs.eclipse.org/bugs/show_bug.cgi?id=399465
    Change-Id: Id9b6b81b20a88f1cabd2801b092983a1107f294e

--- plugins/org.eclipse.xtend.ide/src/org/eclipse/xtend/ide/builder/SourceRelativeFileSystemAccess.java
+++ plugins/org.eclipse.xtend.ide/src/org/eclipse/xtend/ide/builder/SourceRelativeFileSystemAccess.java
@@ -7,14 +7,23 @@
  *******************************************************************************/
 package org.eclipse.xtend.ide.builder;
 
+import java.util.Collection;
+import java.util.Set;
+
 import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.xtext.builder.JDTAwareEclipseResourceFileSystemAccess2;
 import org.eclipse.xtext.generator.OutputConfiguration;
 
+import com.google.common.collect.Multimap;
+
 /**
  * @author Sven Efftinge - Initial contribution and API
+ * @author Holger Schill
  */
 public class SourceRelativeFileSystemAccess extends JDTAwareEclipseResourceFileSystemAccess2 {
 	
@@ -33,6 +42,28 @@ public class SourceRelativeFileSystemAccess extends JDTAwareEclipseResourceFileS
 			return folder;
 		}
 		return super.getContainer(outputConfig);
+	}
+
+	/**
+	 * Since sourceTraces are relative the URI has to be computed with the currentSource as context
+	 */
+	@Override
+	public void flushSourceTraces(String generatorName) throws CoreException {
+		Multimap<URI, IPath> sourceTraces = getSourceTraces();
+		if (sourceTraces != null) {
+			Set<URI> keys = sourceTraces.keySet();
+			for(URI uri: keys) {
+				if (uri != null && currentSource != null) {
+					Collection<IPath> paths = sourceTraces.get(uri);
+					IFile sourceFile = currentSource.getFile(uri.toFileString());
+					if (sourceFile.exists()) {
+						IPath[] tracePathArray = paths.toArray(new IPath[paths.size()]);
+						getTraceMarkers().installMarker(sourceFile, generatorName, tracePathArray);
+					}
+				}
+			}
+		}
+		sourceTraces = null;
 	}
 
 }
