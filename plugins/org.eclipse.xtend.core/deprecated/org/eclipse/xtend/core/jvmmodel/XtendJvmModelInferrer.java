eba94906c8ce414600e47ff0d76b15b57ee9370e
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2013-02-06 11:52:37 +0100

    [xtend] Interfaces have super type object if no super interface was given

--- plugins/org.eclipse.xtend.core/deprecated/org/eclipse/xtend/core/jvmmodel/XtendJvmModelInferrer.java
+++ plugins/org.eclipse.xtend.core/deprecated/org/eclipse/xtend/core/jvmmodel/XtendJvmModelInferrer.java
@@ -297,6 +297,9 @@ public class XtendJvmModelInferrer implements IJvmModelInferrer {
 		translateAnnotationsTo(source.getAnnotations(), inferredJvmType);
 		for (JvmTypeReference intf : source.getExtends()) 
 			inferredJvmType.getSuperTypes().add(jvmTypesBuilder.cloneWithProxies(intf));
+		if (inferredJvmType.getSuperTypes().isEmpty()) {
+			inferredJvmType.getSuperTypes().add(typeReferences.getTypeForName(Object.class, source));
+		}
 		copyAndFixTypeParameters(source.getTypeParameters(), inferredJvmType);
 		
 		for (XtendMember member : source.getMembers()) {
