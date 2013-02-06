8d229976e26be5aed2ca0d87d4cd2d2b1697caaa
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2013-02-06 10:25:54 +0100

    [xtend][typesystem] Integrate new type system (WIP)
    Step 3) Fixed some xtext.ide tests

--- plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/typesystem/internal/LogicalContainerAwareBatchTypeResolver.java
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/typesystem/internal/LogicalContainerAwareBatchTypeResolver.java
@@ -7,20 +7,14 @@
  *******************************************************************************/
 package org.eclipse.xtext.xbase.typesystem.internal;
 
-import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.eclipse.xtext.EcoreUtil2;
-import org.eclipse.xtext.common.types.JvmDeclaredType;
-import org.eclipse.xtext.common.types.JvmIdentifiableElement;
 import org.eclipse.xtext.common.types.JvmType;
-import org.eclipse.xtext.xbase.jvmmodel.ILogicalContainerProvider;
 
 import com.google.common.collect.Lists;
-import com.google.inject.Inject;
 
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
@@ -29,43 +23,17 @@ import com.google.inject.Inject;
 @NonNullByDefault
 public class LogicalContainerAwareBatchTypeResolver extends DefaultBatchTypeResolver {
 	
-	@Inject
-	private ILogicalContainerProvider logicalContainerProvider;
-	
 	@Override
 	protected List<EObject> getEntryPoints(EObject object) {
-		if (object.eContainer() == null && object.eResource() != null) {
-			Resource resource = object.eResource();
-			List<EObject> contents = resource.getContents();
-			List<EObject> result = Lists.newArrayList();
-			for(EObject content: contents) {
-				if (content instanceof JvmType) {
-					result.add(content);
-				}
-			}
-			return result;
-		}
-		JvmIdentifiableElement logicalContainer = logicalContainerProvider.getNearestLogicalContainer(object);
-		if (logicalContainer == null) {
-			if (object instanceof JvmIdentifiableElement) {
-				logicalContainer = (JvmIdentifiableElement) object;
-			} else {
-				EObject container = object.eContainer();
-				if (container != null) {
-					return getEntryPoints(container);
-				}
-				throw new IllegalStateException("object is not contained in a logical container: " + object);
+		Resource resource = object.eResource();
+		List<EObject> contents = resource.getContents();
+		List<EObject> result = Lists.newArrayList();
+		for(EObject content: contents) {
+			if (content instanceof JvmType) {
+				result.add(content);
 			}
 		}
-		JvmDeclaredType declaredType = EcoreUtil2.getContainerOfType(logicalContainer, JvmDeclaredType.class);
-		if (declaredType != null) {
-			while(declaredType.getDeclaringType() != null) {
-				declaredType = declaredType.getDeclaringType();
-			}
-		} else {
-			throw new IllegalStateException("logicalContainer is not contained in a declaredType");
-		}
-		return Collections.<EObject>singletonList(declaredType);
+		return result;
 	}
 
 }
