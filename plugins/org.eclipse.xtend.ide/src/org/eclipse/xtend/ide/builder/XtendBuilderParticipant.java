8e6b0121f3ded1efdaf8a64977eff63598d992d0
Holger Schill <Holger.Schill@itemis.de>
2013-02-08 15:50:01 +0100

    [compiler] Generated code for deleted Xtend class is sticky see https://bugs.eclipse.org/bugs/show_bug.cgi?id=400193

--- plugins/org.eclipse.xtend.ide/src/org/eclipse/xtend/ide/builder/XtendBuilderParticipant.java
+++ plugins/org.eclipse.xtend.ide/src/org/eclipse/xtend/ide/builder/XtendBuilderParticipant.java
@@ -8,10 +8,16 @@
 package org.eclipse.xtend.ide.builder;
 
 import static com.google.common.collect.Lists.*;
+import static com.google.common.collect.Maps.*;
+import static org.eclipse.xtext.ui.util.ResourceUtil.*;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IMarker;
@@ -19,13 +25,17 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IStorage;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.internal.core.JavaProject;
 import org.eclipse.xtext.builder.BuilderParticipant;
 import org.eclipse.xtext.builder.EclipseResourceFileSystemAccess2;
+import org.eclipse.xtext.generator.OutputConfiguration;
 import org.eclipse.xtext.resource.IResourceDescription.Delta;
 import org.eclipse.xtext.ui.resource.IStorage2UriMapper;
 import org.eclipse.xtext.ui.util.IssueUtil;
@@ -34,6 +44,8 @@ import org.eclipse.xtext.util.Pair;
 import org.eclipse.xtext.validation.Issue;
 import org.eclipse.xtext.xbase.compiler.ElementIssueProvider;
 
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 
 /**
@@ -119,6 +131,49 @@ public class XtendBuilderParticipant extends BuilderParticipant {
 			}
 		}
 		return null;
+	}
+
+	/**
+	 * @since 2.4
+	 */
+	protected List<IPath> getSourceFolderPathes(IProject project){
+		List<IPath> sourceFolder = Lists.newArrayList();
+		try {
+			if (project.isOpen() && JavaProject.hasJavaNature(project)) {
+				IJavaProject javaProject = JavaCore.create(project);
+				List<IPackageFragmentRoot> packageFragmentRoots = Arrays.asList(javaProject.getAllPackageFragmentRoots());
+				for(IPackageFragmentRoot packageFragmentRoot : packageFragmentRoots){
+					if(packageFragmentRoot.getKind() == IPackageFragmentRoot.K_SOURCE){
+						IPath path = packageFragmentRoot.getPath();
+						sourceFolder.add(path);
+					}
+				}
+			}
+		} catch (JavaModelException e) {
+			LOGGER.error(e.getMessage(), e);
+		}
+		return sourceFolder;
+	}
+
+	@Override
+	protected Map<OutputConfiguration, Iterable<IMarker>> getGeneratorMarkers(IProject builtProject, Collection<OutputConfiguration> outputConfigurations) throws CoreException{
+		Map<OutputConfiguration, Iterable<IMarker>> generatorMarkers = newHashMap();
+		 List<IPath> sourcePath = getSourceFolderPathes(builtProject);
+		 for (OutputConfiguration config : outputConfigurations) {
+			 if (config.isCleanUpDerivedResources()) {
+				List<IContainer> containerToSearchIn = Lists.newArrayList();
+				for(IPath sourceFolder : sourcePath){
+					IContainer sourcePathBasedContainer = builtProject.getWorkspace().getRoot().getFolder(sourceFolder);
+					containerToSearchIn.add(sourcePathBasedContainer);
+				}
+				Iterable<IMarker> markers = Lists.newArrayList();
+				for(IContainer container : containerToSearchIn){
+					markers = Iterables.concat(markers, getDerivedResourceMarkers().findDerivedResourceMarkers(container, getGeneratorIdProvider().getGeneratorIdentifier()));
+				}
+				generatorMarkers.put(config, markers);
+			 }
+		 }
+		 return generatorMarkers;
 	}
 
 }
