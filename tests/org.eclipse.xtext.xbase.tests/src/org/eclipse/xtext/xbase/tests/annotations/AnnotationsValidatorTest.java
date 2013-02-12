d8348de939dfe0f1648a7632947309e9ddb6ed03
Jan Koehnlein <jan.koehnlein@itemis.de>
2013-02-12 11:58:47 +0100

    [xbase][annotations] removed tests referring to old syntax
    Change-Id: I3f81a79774c61d863d81a20f940e67e560f57f53

--- tests/org.eclipse.xtext.xbase.tests/src/org/eclipse/xtext/xbase/tests/annotations/AnnotationsValidatorTest.java
+++ tests/org.eclipse.xtext.xbase.tests/src/org/eclipse/xtext/xbase/tests/annotations/AnnotationsValidatorTest.java
@@ -35,16 +35,6 @@ public class AnnotationsValidatorTest extends AbstractXbaseWithAnnotationsTest {
 		validator.assertNoErrors(annotation);
 	}
 	
-	@Test public void testTypeConformance_03() throws Exception {
-		XAnnotation annotation = annotation("@testdata.Annotation2({'foo'})", false);
-		validator.assertNoErrors(annotation);
-	}
-	
-	@Test public void testTypeConformance_04() throws Exception {
-		XAnnotation annotation = annotation("@testdata.Annotation2(value = {'foo'})", false);
-		validator.assertNoErrors(annotation);
-	}
-	
 	@Test public void testTypeConformance_05() throws Exception {
 		XAnnotation annotation = annotation("@testdata.Annotation2(true)", false);
 		validator.assertError(annotation, XbasePackage.Literals.XBOOLEAN_LITERAL, IssueCodes.INCOMPATIBLE_TYPES, "expected String | String[] but was boolean");
@@ -53,16 +43,6 @@ public class AnnotationsValidatorTest extends AbstractXbaseWithAnnotationsTest {
 	@Test public void testTypeConformance_06() throws Exception {
 		XAnnotation annotation = annotation("@testdata.Annotation2(value = 42)", false);
 		validator.assertError(annotation, XbasePackage.Literals.XNUMBER_LITERAL, IssueCodes.INCOMPATIBLE_TYPES, "String | String[]", "int");
-	}
-	
-	@Test public void testTypeConformance_07() throws Exception {
-		XAnnotation annotation = annotation("@testdata.Annotation2({typeof(String)})", false);
-		validator.assertError(annotation, XbasePackage.Literals.XTYPE_LITERAL, IssueCodes.INCOMPATIBLE_TYPES, "String", "Class<String>");
-	}
-	
-	@Test public void testTypeConformance_08() throws Exception {
-		XAnnotation annotation = annotation("@testdata.Annotation2(value = {true})", false);
-		validator.assertError(annotation, XbasePackage.Literals.XBOOLEAN_LITERAL, IssueCodes.INCOMPATIBLE_TYPES, "String", "boolean");
 	}
 	
 	@Test public void testTypeConformance_09() throws Exception {
@@ -78,12 +58,6 @@ public class AnnotationsValidatorTest extends AbstractXbaseWithAnnotationsTest {
 	@Test public void testTypeConformance_11() throws Exception {
 		XAnnotation annotation = annotation("@testdata.Annotation1(value = true , children = @testdata.Annotation2(true), foo = 'bar' )", false);
 		validator.assertError(annotation, XbasePackage.Literals.XBOOLEAN_LITERAL, IssueCodes.INCOMPATIBLE_TYPES, "String[]", "boolean");
-	}
-	
-	@Test public void testTypeConformance_12() throws Exception {
-		XAnnotation annotation = annotation("@testdata.Annotation2(value = {'', true, 1, ''})", false);
-		validator.assertError(annotation, XbasePackage.Literals.XBOOLEAN_LITERAL, IssueCodes.INCOMPATIBLE_TYPES, "String", "boolean");
-		validator.assertError(annotation, XbasePackage.Literals.XNUMBER_LITERAL, IssueCodes.INCOMPATIBLE_TYPES, "String", "int");
 	}
 	
 	@Test public void testTypeConformance_13() throws Exception {
@@ -120,16 +94,6 @@ public class AnnotationsValidatorTest extends AbstractXbaseWithAnnotationsTest {
 	@Test public void testSideEffect_1() throws Exception {
 		XAnnotation annotation = annotation("@testdata.Annotation2(value = #['foo'.replace('foo', 'bar')])", false);
 		validator.assertError(annotation, XAnnotationsPackage.Literals.XANNOTATION, IssueCodes.ANNOTATIONS_ILLEGAL_ATTRIBUTE, "constant");
-	}
-	
-	@Test public void testEmptyValueList_01() throws Exception {
-		XAnnotation annotation = annotation("@testdata.Annotation2(value = {})", false);
-		validator.assertNoErrors(annotation);
-	}
-	
-	@Test public void testEmptyValueList_02() throws Exception {
-		XAnnotation annotation = annotation("@testdata.Annotation2({})", false);
-		validator.assertNoErrors(annotation);
 	}
 	
 	@Test public void testEmptyValueList_03() throws Exception {
