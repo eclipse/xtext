a7c6fd2a1475c021ba38f3d6cc69a92b1e856d3c
Sven Efftinge <sven.efftinge@itemis.de>
2013-02-25 19:55:29 +0100

    [tests] added test for https://bugs.eclipse.org/bugs/show_bug.cgi?id=388828

--- tests/org.eclipse.xtend.ide.tests/xtend-gen/org/eclipse/xtend/ide/tests/compiler/RebuildAffectedResourcesTest.java
+++ tests/org.eclipse.xtend.ide.tests/xtend-gen/org/eclipse/xtend/ide/tests/compiler/RebuildAffectedResourcesTest.java
@@ -1,11 +1,13 @@
 package org.eclipse.xtend.ide.tests.compiler;
 
 import com.google.inject.Inject;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
@@ -20,11 +22,14 @@ import org.eclipse.xtext.junit4.ui.util.IResourcesSetupUtil;
 import org.eclipse.xtext.util.StringInputStream;
 import org.eclipse.xtext.util.internal.Stopwatches;
 import org.eclipse.xtext.util.internal.Stopwatches.NumbersForTask;
+import org.eclipse.xtext.xbase.lib.Conversions;
 import org.eclipse.xtext.xbase.lib.Exceptions;
 import org.eclipse.xtext.xbase.lib.Functions.Function0;
 import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.IterableExtensions;
+import org.eclipse.xtext.xbase.lib.ListExtensions;
 import org.eclipse.xtext.xbase.lib.ObjectExtensions;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Rule;
@@ -42,6 +47,16 @@ public class RebuildAffectedResourcesTest extends AbstractXtendUITestCase {
       return _stopwatchRule;
     }
   }.apply();
+  
+  @After
+  public void tearDown() {
+    try {
+      IProject _project = this.workbenchTestHelper.getProject();
+      _project.delete(true, null);
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
   
   @Test
   public void testRemoveReferencedType() {
@@ -164,6 +179,107 @@ public class RebuildAffectedResourcesTest extends AbstractXtendUITestCase {
     }
   }
   
+  @Test
+  public void testExtendedClassChanges() {
+    try {
+      StringConcatenation _builder = new StringConcatenation();
+      _builder.append("class Type3 {");
+      _builder.newLine();
+      _builder.append("}");
+      _builder.newLine();
+      final IFile type3 = this.workbenchTestHelper.createFile("Type3.xtend", _builder.toString());
+      StringConcatenation _builder_1 = new StringConcatenation();
+      _builder_1.append("class Type1 extends Type3 {");
+      _builder_1.newLine();
+      _builder_1.append("}");
+      _builder_1.newLine();
+      this.workbenchTestHelper.createFile("Type1.xtend", _builder_1.toString());
+      StringConcatenation _builder_2 = new StringConcatenation();
+      _builder_2.append("class Type2 {");
+      _builder_2.newLine();
+      _builder_2.append("\t");
+      _builder_2.append("def void doStuff(Type1 foo) {");
+      _builder_2.newLine();
+      _builder_2.append("\t\t");
+      _builder_2.append("foo.bar");
+      _builder_2.newLine();
+      _builder_2.append("\t");
+      _builder_2.append("}");
+      _builder_2.newLine();
+      _builder_2.append("}");
+      _builder_2.newLine();
+      final IFile type2 = this.workbenchTestHelper.createFile("Type2.xtend", _builder_2.toString());
+      IResourcesSetupUtil.waitForAutoBuild();
+      this.assertHasErrors(type2, "bar");
+      StringConcatenation _builder_3 = new StringConcatenation();
+      _builder_3.append("class Type3 {");
+      _builder_3.newLine();
+      _builder_3.append("\t");
+      _builder_3.append("def void bar() {}");
+      _builder_3.newLine();
+      _builder_3.append("}");
+      _builder_3.newLine();
+      StringInputStream _stringInputStream = new StringInputStream(_builder_3.toString());
+      type3.setContents(_stringInputStream, true, true, null);
+      IResourcesSetupUtil.waitForAutoBuild();
+      this.assertNoErrorsInWorkspace();
+      StringConcatenation _builder_4 = new StringConcatenation();
+      _builder_4.append("class Type3 {");
+      _builder_4.newLine();
+      _builder_4.append("\t");
+      _builder_4.append("def void baz() {}");
+      _builder_4.newLine();
+      _builder_4.append("}");
+      _builder_4.newLine();
+      StringInputStream _stringInputStream_1 = new StringInputStream(_builder_4.toString());
+      type3.setContents(_stringInputStream_1, true, true, null);
+      IResourcesSetupUtil.waitForAutoBuild();
+      this.assertHasErrors(type2, "bar");
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
+  
+  /**
+   * see https://bugs.eclipse.org/bugs/show_bug.cgi?id=388828
+   */
+  @Test
+  public void testBug388828() {
+    try {
+      StringConcatenation _builder = new StringConcatenation();
+      _builder.append("abstract class A {}");
+      _builder.newLine();
+      final IFile typeA = this.workbenchTestHelper.createFile("A.xtend", _builder.toString());
+      StringConcatenation _builder_1 = new StringConcatenation();
+      _builder_1.append("class B extends A {");
+      _builder_1.newLine();
+      _builder_1.append("}");
+      _builder_1.newLine();
+      final IFile typeB = this.workbenchTestHelper.createFile("B.xtend", _builder_1.toString());
+      IResourcesSetupUtil.waitForAutoBuild();
+      this.assertNoErrorsInWorkspace();
+      StringConcatenation _builder_2 = new StringConcatenation();
+      _builder_2.append("abst ract class A {");
+      _builder_2.newLine();
+      _builder_2.append("}");
+      _builder_2.newLine();
+      StringInputStream _stringInputStream = new StringInputStream(_builder_2.toString());
+      typeA.setContents(_stringInputStream, true, true, null);
+      IResourcesSetupUtil.waitForAutoBuild();
+      this.assertHasErrors(typeA, "abst");
+      this.assertHasErrors(typeB, "A");
+      StringConcatenation _builder_3 = new StringConcatenation();
+      _builder_3.append("abstract class A {}");
+      _builder_3.newLine();
+      StringInputStream _stringInputStream_1 = new StringInputStream(_builder_3.toString());
+      typeA.setContents(_stringInputStream_1, true, true, null);
+      IResourcesSetupUtil.waitForAutoBuild();
+      this.assertNoErrorsInWorkspace();
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
+  
   public void assertNoErrorsInWorkspace() {
     try {
       IWorkspace _workspace = ResourcesPlugin.getWorkspace();
@@ -221,11 +337,21 @@ public class RebuildAffectedResourcesTest extends AbstractXtendUITestCase {
           return;
         }
       }
-      String _plus = ("Exected an error marker containing \'" + msgPart);
+      String _plus = ("Expected an error marker containing \'" + msgPart);
       String _plus_1 = (_plus + "\' on ");
       IPath _fullPath = file.getFullPath();
       String _plus_2 = (_plus_1 + _fullPath);
-      Assert.fail(_plus_2);
+      String _plus_3 = (_plus_2 + " but found ");
+      final Function1<IMarker,String> _function = new Function1<IMarker,String>() {
+          public String apply(final IMarker it) {
+            String _message = MarkerUtilities.getMessage(it);
+            return _message;
+          }
+        };
+      List<String> _map = ListExtensions.<IMarker, String>map(((List<IMarker>)Conversions.doWrapArray(findMarkers)), _function);
+      String _join = IterableExtensions.join(_map, ",");
+      String _plus_4 = (_plus_3 + _join);
+      Assert.fail(_plus_4);
     } catch (Throwable _e) {
       throw Exceptions.sneakyThrow(_e);
     }
