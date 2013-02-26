27071d4ff8859aab5ecc8b40d463e29e8712e53a
Sven Efftinge <sven.efftinge@itemis.de>
2013-02-26 11:38:06 +0100

    [common types] made TypeResource implement ISynchonizable and synchonized computation and resolution in JvmDeclaredTypeImplCustom

--- plugins/org.eclipse.xtext.common.types/src/org/eclipse/xtext/common/types/impl/JvmDeclaredTypeImplCustom.java
+++ plugins/org.eclipse.xtext.common.types/src/org/eclipse/xtext/common/types/impl/JvmDeclaredTypeImplCustom.java
@@ -16,6 +16,7 @@ import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.emf.common.notify.Notifier;
+import org.eclipse.emf.common.util.WrappedException;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.xtext.common.types.JvmArrayType;
 import org.eclipse.xtext.common.types.JvmDeclaredType;
@@ -33,11 +34,14 @@ import org.eclipse.xtext.common.types.access.IMirrorExtension;
 import org.eclipse.xtext.common.types.access.JvmTypeChangeDispatcher;
 import org.eclipse.xtext.common.types.access.TypeResource;
 import org.eclipse.xtext.common.types.util.RawTypeHelper.RawTypeReferenceImplementation;
+import org.eclipse.xtext.resource.ISynchronizable;
 import org.eclipse.xtext.util.Strings;
+import org.eclipse.xtext.util.concurrent.IUnitOfWork;
 
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
+import com.google.inject.Provider;
 
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
@@ -48,30 +52,33 @@ public abstract class JvmDeclaredTypeImplCustom extends JvmDeclaredTypeImpl {
 	public JvmArrayType getArrayType() {
 		JvmArrayType result = super.getArrayType();
 		if (result == null) {
-			result = TypesFactory.eINSTANCE.createJvmArrayType();
-			boolean wasDeliver = eDeliver();
-			eSetDeliver(false);
-			try {
-				setArrayType(result);
-			} finally {
-				eSetDeliver(wasDeliver);
-			}
+			result = doSynchronized(new Provider<JvmArrayType>() {
+				public JvmArrayType get() {
+					JvmArrayType result = TypesFactory.eINSTANCE.createJvmArrayType();
+					boolean wasDeliver = eDeliver();
+					eSetDeliver(false);
+					try {
+						setArrayType(result);
+					} finally {
+						eSetDeliver(wasDeliver);
+					}
+					return result;
+				}
+			});
 		}
 		return result;
 	}
-	
+
 	@Override
-	public Object eGet(int featureID, boolean resolve, boolean coreType)
-	{
-		switch (featureID)
-		{
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
 			case TypesPackage.JVM_DECLARED_TYPE__ARRAY_TYPE:
 				// don't demand-create the array if queried reflectively
 				return arrayType;
 		}
 		return super.eGet(featureID, resolve, coreType);
 	}
-	
+
 	@Override
 	public String getPackageName() {
 		JvmDeclaredType declaringType = getDeclaringType();
@@ -79,7 +86,7 @@ public abstract class JvmDeclaredTypeImplCustom extends JvmDeclaredTypeImpl {
 			return declaringType.getPackageName();
 		return packageName;
 	}
-	
+
 	@Override
 	protected String computeIdentifier() {
 		if (simpleName == null)
@@ -95,7 +102,7 @@ public abstract class JvmDeclaredTypeImplCustom extends JvmDeclaredTypeImpl {
 			return null;
 		return parentName + '$' + simpleName;
 	}
-	
+
 	@Override
 	public String getQualifiedName(char innerClassDelimiter) {
 		if (simpleName == null)
@@ -111,109 +118,145 @@ public abstract class JvmDeclaredTypeImplCustom extends JvmDeclaredTypeImpl {
 			return null;
 		return parentName + innerClassDelimiter + simpleName;
 	}
-	
+
 	@Override
 	public Iterable<JvmField> getDeclaredFields() {
 		return Iterables.filter(getMembers(), JvmField.class);
 	}
-	
+
 	@Override
 	public Iterable<JvmOperation> getDeclaredOperations() {
 		return Iterables.filter(getMembers(), JvmOperation.class);
 	}
-	
+
 	protected Map<String, Set<JvmFeature>> allFeaturesByName;
-	
+
 	protected Set<String> removedOverridden;
-	
+
 	@Override
-	public Iterable<JvmFeature> findAllFeaturesByName(String simpleName) {
-		Map<String, Set<JvmFeature>> allFeaturesByName = getAllFeaturesMap(); 
-		Set<JvmFeature> result = allFeaturesByName.get(simpleName);
-		if (result != null) {
-			if (result.size() <= 1 || (removedOverridden != null && removedOverridden.contains(simpleName))) {
-				return result;
-			}
-			// TODO use the number of parameters as a first fast criteria
-			Set<String> signatures = Sets.newHashSet();
-			Iterator<JvmFeature> iter = result.iterator();
-			while(iter.hasNext()) {
-				JvmFeature next = iter.next();
-				if (next instanceof JvmOperation) {
-					JvmOperation operation = (JvmOperation) next;
-					if (!operation.isStatic()) {
-						/* 
-						 * TODO use upper bound information for cases like
-						 * class MyStringList extends ArrayList<String>
-						 * where add(String) actually overrides add(T/Object)
-						 */
-						List<JvmFormalParameter> parameters = operation.getParameters();
-						StringBuilder signature = new StringBuilder(operation.getSimpleName());
-						if (parameters.isEmpty()) {
-							signature.append("()");
-						} else {
-							signature.append("(");
-							for(JvmFormalParameter parameter: parameters) {
-								JvmType parameterType = getRawType(parameter.getParameterType());
-								if (parameterType != null) {
-									signature.append(parameterType.getIdentifier());
-									signature.append(",");
+	public Iterable<JvmFeature> findAllFeaturesByName(final String simpleName) {
+		return doSynchronized(new Provider<Iterable<JvmFeature>>() {
+			public Iterable<JvmFeature> get() {
+				Map<String, Set<JvmFeature>> allFeaturesByName = getAllFeaturesMap();
+				Set<JvmFeature> result = allFeaturesByName.get(simpleName);
+				if (result != null) {
+					if (result.size() <= 1 || (removedOverridden != null && removedOverridden.contains(simpleName))) {
+						return result;
+					}
+					// TODO use the number of parameters as a first fast criteria
+					Set<String> signatures = Sets.newHashSet();
+					Iterator<JvmFeature> iter = result.iterator();
+					while (iter.hasNext()) {
+						JvmFeature next = iter.next();
+						if (next instanceof JvmOperation) {
+							JvmOperation operation = (JvmOperation) next;
+							if (!operation.isStatic()) {
+								/* 
+								 * TODO use upper bound information for cases like
+								 * class MyStringList extends ArrayList<String>
+								 * where add(String) actually overrides add(T/Object)
+								 */
+								List<JvmFormalParameter> parameters = operation.getParameters();
+								StringBuilder signature = new StringBuilder(operation.getSimpleName());
+								if (parameters.isEmpty()) {
+									signature.append("()");
+								} else {
+									signature.append("(");
+									for (JvmFormalParameter parameter : parameters) {
+										JvmType parameterType = getRawType(parameter.getParameterType());
+										if (parameterType != null) {
+											signature.append(parameterType.getIdentifier());
+											signature.append(",");
+										}
+									}
+									signature.replace(signature.length() - 1, signature.length(), ")");
+								}
+								if (!signatures.add(signature.toString())) {
+									iter.remove();
 								}
 							}
-							signature.replace(signature.length() - 1, signature.length(), ")");
-						}
-						if (!signatures.add(signature.toString())) {
-							iter.remove();
+						} else if (next instanceof JvmField) {
+							JvmField field = (JvmField) next;
+							if (!field.isStatic() && !signatures.add(field.getSimpleName())) {
+								iter.remove();
+							}
 						}
 					}
-				} else if (next instanceof JvmField) {
-					JvmField field = (JvmField) next;
-					if (!field.isStatic() && !signatures.add(field.getSimpleName())) {
-						iter.remove();
+					if (removedOverridden == null) {
+						removedOverridden = Sets.newHashSet(simpleName);
+					} else {
+						removedOverridden.add(simpleName);
 					}
+					return result;
 				}
+				return Collections.emptyList();
 			}
-			// TODO HashSet is not threadsafe
-			if (removedOverridden == null) {
-				removedOverridden = Sets.newHashSet(simpleName);
-			} else {
-				removedOverridden.add(simpleName);
-			}
-			return result;
-		}
-		return Collections.emptyList();
+		});
 	}
-	
+
+	protected <T> T doSynchronized(final Provider<? extends T> provider) {
+		if (eResource() instanceof ISynchronizable<?>) {
+			ISynchronizable<?> eResource = (ISynchronizable<?>) eResource();
+			try {
+				T result = eResource.execute(new IUnitOfWork<T, Object>() {
+					public T exec(Object state) throws Exception {
+						T result = provider.get();
+						return result;
+					}
+				});
+				return result;
+			} catch (RuntimeException e) {
+				throw e;
+			} catch (Exception e) {
+				throw new WrappedException(e);
+			}
+		} else {
+			// not in a resource, don't lock
+			return provider.get();
+		}
+	}
+
 	protected Map<String, Set<JvmFeature>> getAllFeaturesMap() {
 		Set<JvmDeclaredType> processedSuperTypes = Sets.newHashSet();
 		return internalGetAllFeaturesMap(processedSuperTypes);
 	}
-	
-	protected Map<String, Set<JvmFeature>> internalGetAllFeaturesMap(Set<JvmDeclaredType> processedSuperTypes) {
-		Map<String, Set<JvmFeature>> result = allFeaturesByName;
-		if (result == null) {
-			result = Maps.newLinkedHashMap();
-			processMembers(result, getMembers());
-			for(JvmTypeReference superTypeReference: getSuperTypes()) {
-				JvmType superType = getRawType(superTypeReference);
-				if (superType instanceof JvmDeclaredTypeImplCustom && !superType.eIsProxy() && !processedSuperTypes.contains(superType)) {
-					processedSuperTypes.add((JvmDeclaredType) superType);
-					Map<String, Set<JvmFeature>> superTypeFeatureMap = ((JvmDeclaredTypeImplCustom) superType).internalGetAllFeaturesMap(processedSuperTypes);
-					processedSuperTypes.remove(superType);					
-					for(Set<JvmFeature> features: superTypeFeatureMap.values())
-						processMembers(result, features);
+
+	protected Map<String, Set<JvmFeature>> internalGetAllFeaturesMap(final Set<JvmDeclaredType> processedSuperTypes) {
+		if (allFeaturesByName == null) {
+			allFeaturesByName = doSynchronized(new Provider<Map<String, Set<JvmFeature>>>() {
+				public Map<String, Set<JvmFeature>> get() {
+					if (allFeaturesByName != null)
+						return allFeaturesByName;
+					Map<String, Set<JvmFeature>> result = Maps.newLinkedHashMap();
+					processMembers(result, getMembers());
+					for (JvmTypeReference superTypeReference : getSuperTypes()) {
+						JvmType superType = getRawType(superTypeReference);
+						if (superType instanceof JvmDeclaredTypeImplCustom && !superType.eIsProxy()
+								&& !processedSuperTypes.contains(superType)) {
+							processedSuperTypes.add((JvmDeclaredType) superType);
+							Map<String, Set<JvmFeature>> superTypeFeatureMap = ((JvmDeclaredTypeImplCustom) superType)
+									.internalGetAllFeaturesMap(processedSuperTypes);
+							processedSuperTypes.remove(superType);
+							for (Set<JvmFeature> features : superTypeFeatureMap.values())
+								processMembers(result, features);
+						}
+					}
+					Runnable runnable = new Runnable() {
+						public void run() {
+							doSynchronized(new Provider<Object>() {
+								public Object get() {
+									removedOverridden = null;
+									allFeaturesByName = null;
+									return null;
+								}});
+						}
+					};
+					requestNotificationOnChange(runnable);
+					return result;
 				}
-			}
-			Runnable runnable = new Runnable() {
-				public void run() {
-					removedOverridden = null;
-					allFeaturesByName = null;
-				}
-			};
-			requestNotificationOnChange(runnable);
-			allFeaturesByName = result;
+			});
 		}
-		return result;
+		return allFeaturesByName;
 	}
 
 	protected void requestNotificationOnChange(Runnable listener) {
@@ -237,7 +280,7 @@ public abstract class JvmDeclaredTypeImplCustom extends JvmDeclaredTypeImpl {
 	}
 
 	protected void processMembers(Map<String, Set<JvmFeature>> result, Collection<? extends JvmMember> members) {
-		for(JvmMember member: members) {
+		for (JvmMember member : members) {
 			if (member instanceof JvmOperation) {
 				Set<JvmFeature> knownMembers = result.get(member.getSimpleName());
 				if (knownMembers == null) {
@@ -259,22 +302,31 @@ public abstract class JvmDeclaredTypeImplCustom extends JvmDeclaredTypeImpl {
 	}
 
 	protected Set<JvmFeature> allFeatures;
-	
+
 	@Override
 	public Iterable<JvmFeature> getAllFeatures() {
 		if (allFeatures == null) {
-			Set<JvmFeature> result = Sets.newLinkedHashSet();
-			Map<String, Set<JvmFeature>> allFeaturesMap = getAllFeaturesMap();
-			for (String name : allFeaturesMap.keySet()) {
-				Iterables.addAll(result, findAllFeaturesByName(name) );
-			}
-			Runnable runnable = new Runnable() {
-				public void run() {
-					allFeatures = null;
-				}
-			};
-			requestNotificationOnChange(runnable);
-			allFeatures = result;
+			allFeatures = doSynchronized(new Provider<Set<JvmFeature>>() {
+				public Set<JvmFeature> get() {
+					if (allFeatures != null)
+						return allFeatures;
+					Set<JvmFeature> result = Sets.newLinkedHashSet();
+					Map<String, Set<JvmFeature>> allFeaturesMap = getAllFeaturesMap();
+					for (String name : allFeaturesMap.keySet()) {
+						Iterables.addAll(result, findAllFeaturesByName(name));
+					}
+					Runnable runnable = new Runnable() {
+						public void run() {
+							doSynchronized(new Provider<Object>() {
+								public Object get() {
+									allFeatures = null;
+									return null;
+								}});
+						}
+					};
+					requestNotificationOnChange(runnable);
+					return result;
+				}});
 		}
 		return allFeatures;
 	}
@@ -284,5 +336,5 @@ public abstract class JvmDeclaredTypeImplCustom extends JvmDeclaredTypeImpl {
 		JvmTypeReference result = strategy.getRawTypeReference(reference, eResource());
 		return result == null ? null : result.getType();
 	}
-	
+
 }
