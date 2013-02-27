8abf77ebf39f470be9e618fc7cb237c146e9165e
Sven Efftinge <sven.efftinge@itemis.de>
2013-02-27 14:19:54 +0100

    [xtend aa] more javaDoc

--- plugins/org.eclipse.xtend.lib/src/org/eclipse/xtend/lib/macro/RegisterGlobalsParticipant.java
+++ plugins/org.eclipse.xtend.lib/src/org/eclipse/xtend/lib/macro/RegisterGlobalsParticipant.java
@@ -14,13 +14,29 @@ import org.eclipse.xtend.lib.macro.declaration.NamedElement;
 import com.google.common.annotations.Beta;
 
 /**
+ * Call back to participate in the register-globals phase, where Java types are
+ * collected.
+ * 
+ * Clients should implement this to register and create new Java types.
  * 
  * @author Sven Efftinge
- *
- * @param <T> - the type
+ * 
+ * @param <T>
+ *            the type of the annotated elements
  */
 @Beta
 public interface RegisterGlobalsParticipant<T extends NamedElement> {
-	
-	void doRegisterGlobals(List<? extends T> annotatedTargetElements, RegisterGlobalsContext context);
+
+	/**
+	 * Called by the compiler during register-globals phase.
+	 * 
+	 * @param annotatedSourceElements
+	 *            the source elements from the currently processed compilation
+	 *            unit which are annotated with active annotation
+	 * @param context
+	 *            services to register new Java types
+	 * @see RegisterGlobalsContext
+	 */
+	void doRegisterGlobals(List<? extends T> annotatedSourceElements,
+			RegisterGlobalsContext context);
 }
