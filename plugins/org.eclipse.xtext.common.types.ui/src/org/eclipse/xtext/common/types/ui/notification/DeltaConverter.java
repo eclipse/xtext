98cacba6dd84683e8540ec6fdae3d33450cec837
Sven Efftinge <sven.efftinge@itemis.de>
2013-02-28 08:24:53 +0100

    [common types] removed check for structure errors in delta converter (see https://bugs.eclipse.org/bugs/show_bug.cgi?id=401959)
    Change-Id: I15e0547790e03ff298aac2fa528b7f0a6ef2ada0

--- plugins/org.eclipse.xtext.common.types.ui/src/org/eclipse/xtext/common/types/ui/notification/DeltaConverter.java
+++ plugins/org.eclipse.xtext.common.types.ui/src/org/eclipse/xtext/common/types/ui/notification/DeltaConverter.java
@@ -250,8 +250,6 @@ public class DeltaConverter {
 	
 	protected void convertCompilationUnits(IJavaElementDelta delta, List<IResourceDescription.Delta> result) {
 		IJavaElement element = delta.getElement();
-		if (delta.getKind() != IJavaElementDelta.REMOVED && hasStructureErrors(element)) 
-			return;
 		if (element.getElementType() == IJavaElement.COMPILATION_UNIT) {
 			convertCompilationUnit(delta, result);
 		}
@@ -280,16 +278,6 @@ public class DeltaConverter {
 						logger.debug(e, e);
 				}
 			}
-		}
-	}
-
-	protected boolean hasStructureErrors(IJavaElement element) {
-		try {
-			return !element.isStructureKnown();
-		} catch (JavaModelException e) {
-			if (logger.isDebugEnabled())
-				logger.debug(e, e);
-			return true;
 		}
 	}
 
