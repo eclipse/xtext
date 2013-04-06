4c25539b8db4c2f38f22c9b42f0d0b10aafbdc8b
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2013-04-06 13:23:19 +0300

    use IResourceServiceProvider.Provider instead of OverridingGuiceModule Signed-off-by: Moritz Eysholdt <moritz.eysholdt@itemis.de>

--- org.xpect.ui/src/org/xpect/ui/services/XtResourceUIServiceProviderProvider.java
+++ org.xpect.ui/src/org/xpect/ui/services/XtResourceUIServiceProviderProvider.java
@@ -7,12 +7,9 @@
  *******************************************************************************/
 package org.xpect.ui.services;
 
-import org.eclipse.core.resources.IStorage;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.xtext.resource.IResourceServiceProvider;
-import org.eclipse.xtext.ui.resource.DefaultResourceUIServiceProvider;
 import org.eclipse.xtext.ui.resource.IResourceUIServiceProvider;
-import org.xpect.ui.builder.IResourceServiceProviderProvider;
 import org.xpect.ui.internal.XpectActivator;
 import org.xpect.ui.util.XtInjectorSetupUtil;
 import org.xpect.util.URIDelegationHandler;
@@ -22,12 +19,7 @@ import com.google.inject.Injector;
 /**
  * @author Moritz Eysholdt - Initial contribution and API
  */
-public class XtResourceUIServiceProviderProvider extends DefaultResourceUIServiceProvider implements IResourceServiceProviderProvider {
-
-	public XtResourceUIServiceProviderProvider() {
-		super(null);
-	}
-
+public class XtResourceUIServiceProviderProvider implements IResourceServiceProvider.Provider {
 	public IResourceServiceProvider get(URI uri, String contentType) {
 		String ext = new URIDelegationHandler().getOriginalFileExtension(uri.lastSegment());
 		if (ext != null) {
@@ -37,15 +29,4 @@ public class XtResourceUIServiceProviderProvider extends DefaultResourceUIServic
 		}
 		return XpectActivator.getInstance().getInjector(XpectActivator.ORG_XPECT_XPECT).getInstance(IResourceUIServiceProvider.class);
 	}
-
-	@Override
-	public boolean canHandle(URI uri) {
-		return false;
-	}
-
-	@Override
-	public boolean canHandle(URI uri, IStorage storage) {
-		return false;
-	}
-
 }
