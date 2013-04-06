87d6477cff2c90af6d47bfedcc75b4fd44a80959
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2013-04-06 17:12:20 +0100

    work on content assist for the xt language Signed-off-by: Moritz Eysholdt <moritz.eysholdt@itemis.de>

--- org.xpect.ui/src/org/xpect/ui/XtUIModule.java
+++ org.xpect.ui/src/org/xpect/ui/XtUIModule.java
@@ -9,10 +9,12 @@ package org.xpect.ui;
 
 import org.eclipse.xtext.ui.LanguageSpecific;
 import org.eclipse.xtext.ui.editor.IURIEditorOpener;
+import org.eclipse.xtext.ui.editor.contentassist.IContentProposalProvider;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfiguration;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.ISemanticHighlightingCalculator;
 import org.eclipse.xtext.validation.IResourceValidator;
 import org.xpect.registry.AbstractDelegatingModule;
+import org.xpect.ui.contentassist.XtProposalProvider;
 import org.xpect.ui.highlighting.XtHighlightingCalculator;
 import org.xpect.ui.highlighting.XtHighlightingConfiguration;
 import org.xpect.ui.services.XtLanguageSpecificURIEditorOpener;
@@ -30,6 +32,7 @@ public class XtUIModule extends AbstractDelegatingModule {
 		overrideAndBackup(binder, ISemanticHighlightingCalculator.class, XtHighlightingCalculator.class);
 		overrideAndBackup(binder, IResourceValidator.class, XtResourceValidator.class);
 		overrideAndBackup(binder, IURIEditorOpener.class, LanguageSpecific.class, XtLanguageSpecificURIEditorOpener.class);
+		overrideAndBackup(binder, IContentProposalProvider.class, XtProposalProvider.class);
 	}
 
 }
