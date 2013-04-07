79fc8f8713f9145b26c1c4fde9a34a72be6a724d
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2013-04-07 19:41:49 +0100

    fixed content assist for tests in XPECT_SETUP Signed-off-by: Moritz Eysholdt <moritz.eysholdt@itemis.de>

--- org.xpect.ui/src/org/xpect/ui/contentassist/XpectProposalProvider.java
+++ org.xpect.ui/src/org/xpect/ui/contentassist/XpectProposalProvider.java
@@ -7,9 +7,27 @@
  *******************************************************************************/
 package org.xpect.ui.contentassist;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.xtext.resource.IEObjectDescription;
+import org.eclipse.xtext.ui.editor.contentassist.ICompletionProposalAcceptor;
+import org.xpect.XjmPackage;
+import org.xpect.XpectPackage;
+
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+
 /**
  * @author Moritz Eysholdt - Initial contribution and API
  */
 public class XpectProposalProvider extends AbstractXpectProposalProvider {
+
+	protected void lookupCrossReference(EObject model, EReference reference, ICompletionProposalAcceptor acceptor,
+			Predicate<IEObjectDescription> filter, Function<IEObjectDescription, ICompletionProposal> proposalFactory) {
+		if (reference == XpectPackage.Literals.XPECT_TEST__TEST_CLASS_OR_SUITE)
+			reference = XjmPackage.Literals.XJM_CLASS__JVM_CLASS;
+		super.lookupCrossReference(model, reference, acceptor, filter, proposalFactory);
+	}
 
 }
