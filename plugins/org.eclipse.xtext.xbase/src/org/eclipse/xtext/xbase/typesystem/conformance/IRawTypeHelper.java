7515202c04449b200c93252876f32b1c9b9e3d2d
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2013-04-29 10:28:38 +0200

    [xbase][typesystem] Regression: Attempt to fix the smoke tests
    The raw type of a synonym reference is computed from the synonym
    type part rather than its origin, e.g. the raw type of the type
    
    int | Integer
    
    is Integer since that's the type which is used to compute the 
    declared features.

--- plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/typesystem/conformance/IRawTypeHelper.java
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/typesystem/conformance/IRawTypeHelper.java
@@ -18,6 +18,19 @@ import org.eclipse.xtext.xbase.typesystem.references.RawTypeHelper;
 import com.google.inject.ImplementedBy;
 
 /**
+ * The {@link IRawTypeHelper} allows to compute the raw types for a given reference.
+ * 
+ * Semantically the raw type for a reference is considered to be
+ * <ul>
+ *   <li>The primary type for a parameterized type, e.g. {@code List<String>} produces {@code List}</li>
+ *   <li>The constraints in case the type is a type paramater, e.g. {@code CharSequence} is the raw type of
+ *       a type parameter {@code T extends CharSequence}</li>
+ *   <li>Wildcard references determine the raw type from their upper bound</li>
+ *   <li>Multi-types return all the raw types of all of their components.</li>
+ *   <li>Synonyms return the raw type of the synonym since the primary type is only used to track the origin. That 
+ *       means, a type {@code int | Integer} has the raw type {@code Integer}</li>.
+ * </ul>
+ * 
  * @author Sebastian Zarnekow - Initial contribution and API
  */
 @ImplementedBy(RawTypeHelper.class)
