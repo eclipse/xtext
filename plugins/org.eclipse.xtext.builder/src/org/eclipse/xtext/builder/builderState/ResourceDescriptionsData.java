a31ebac75825a6dc1542bdffbb969ce679cc337e
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2013-05-07 19:07:05 +0200

    [builder] Reduced required memory for the ResourceDescriptionsData
    This commit also includes a changed default clustering policy.
    Clustering has been disabled for the generator since the very
    first day and in our workspace, no OOM occurred. So in practice
    there is enough memory available to load all build resources
    into a single resource set. The clustering policy used to cap 
    that one due to short living instances that suggested the VM is
    close to a OOM. So resources have been unloaded unnecessarily.
    Therefore this changed default binding.
    
    The changes in the ResourceDescriptionData reduced the number
    of unnecessary hash structures in the ResourceDescriptionsData 
    
    see https://bugs.eclipse.org/bugs/show_bug.cgi?id=407423

--- plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/builderState/ResourceDescriptionsData.java
+++ plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/builderState/ResourceDescriptionsData.java
@@ -7,8 +7,8 @@
  *******************************************************************************/
 package org.eclipse.xtext.builder.builderState;
 
-import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -19,23 +19,30 @@ import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.naming.QualifiedName;
 import org.eclipse.xtext.resource.IEObjectDescription;
 import org.eclipse.xtext.resource.IResourceDescription;
-import org.eclipse.xtext.resource.ISelectable;
 import org.eclipse.xtext.resource.impl.AbstractCompoundSelectable;
 
 import com.google.common.base.Function;
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.ImmutableSetMultimap;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Maps;
-import com.google.common.collect.Multimap;
+import com.google.common.collect.Sets;
 
 /**
+ * Instances of {@link ResourceDescriptionsData} provide indexed access to
+ * resource descriptions that export elements with a certain name.
+ * 
  * @author Sebastian Zarnekow - Initial contribution and API
  */
 public class ResourceDescriptionsData extends AbstractCompoundSelectable {
 
 	private final Map<URI, IResourceDescription> resourceDescriptionMap;
-	private final Multimap<QualifiedName, IResourceDescription> lookupMap;
+	/**
+	 * The lookup map contains either a single {@link IResourceDescription} or a 
+	 * {@link Set Set&lt;IResourceDescription&gt;} as values. It is maintained by 
+	 * {@link #removeDescription(URI)} and {@link #addDescription(URI, IResourceDescription)} 
+	 * which take care of creating the {@link Set Sets} and discarding them if the number
+	 * of elements becomes small enough.  
+	 */
+	private final Map<QualifiedName, Object> lookupMap;
 
 	public ResourceDescriptionsData(Iterable<IResourceDescription> descriptions) {
 		resourceDescriptionMap = Maps.uniqueIndex(descriptions, new Function<IResourceDescription, URI>() {
@@ -43,17 +50,14 @@ public class ResourceDescriptionsData extends AbstractCompoundSelectable {
 				return from.getURI();
 			}
 		});
-		ImmutableSetMultimap.Builder<QualifiedName, IResourceDescription> builder = ImmutableSetMultimap.builder();
+		// magic number - it is assumend that we export at least 10 entries per resource description
+		lookupMap = Maps.newHashMapWithExpectedSize(resourceDescriptionMap.size() * 10);
 	    for (IResourceDescription description: descriptions) {
-	    	for(IEObjectDescription object: description.getExportedObjects()) {
-	    		QualifiedName lowerCase = object.getName().toLowerCase();
-	    		builder.put(lowerCase, description);
-	    	}
+	    	registerDescription(description, lookupMap);
 	    }
-	    lookupMap = builder.build();
 	}
-	
-	protected ResourceDescriptionsData(Map<URI, IResourceDescription> resourceDescriptionMap, Multimap<QualifiedName, IResourceDescription> lookupMap) {
+
+	protected ResourceDescriptionsData(Map<URI, IResourceDescription> resourceDescriptionMap, Map<QualifiedName, Object> lookupMap) {
 		this.resourceDescriptionMap = resourceDescriptionMap;
 		this.lookupMap = lookupMap;
 	}
@@ -61,7 +65,22 @@ public class ResourceDescriptionsData extends AbstractCompoundSelectable {
 	public ResourceDescriptionsData copy() {
 		return new ResourceDescriptionsData(
 				Maps.newHashMap(resourceDescriptionMap),
-				HashMultimap.create(lookupMap));
+				copyLookupMap());
+	}
+	
+	protected Map<QualifiedName, Object> copyLookupMap() {
+		Map<QualifiedName, Object> result = Maps.newHashMap(lookupMap);
+		for(Map.Entry<QualifiedName, Object> entry: result.entrySet()) {
+			Object value = entry.getValue();
+			if (value instanceof Set<?>) {
+				@SuppressWarnings("unchecked")
+				Set<IResourceDescription> copiedValue = new HashSet<IResourceDescription>((Set<? extends IResourceDescription>) value);
+				if (copiedValue.size() <= 1)
+					throw new IllegalStateException("Unexpected number of elements in the value set: " + copiedValue.size() + " for " + entry.getKey());
+				entry.setValue(copiedValue);
+			}
+		}
+		return result;
 	}
 
 	public Iterable<IResourceDescription> getAllResourceDescriptions() {
@@ -76,19 +95,37 @@ public class ResourceDescriptionsData extends AbstractCompoundSelectable {
 		IResourceDescription oldDescription = resourceDescriptionMap.remove(uri);
 		if (oldDescription != null) {
 			for(IEObjectDescription object: oldDescription.getExportedObjects()) {
-				lookupMap.remove(object.getName().toLowerCase(), oldDescription);
+				QualifiedName objectName = object.getName().toLowerCase();
+				Object existing = lookupMap.get(objectName);
+				if (existing == oldDescription) {
+					lookupMap.remove(objectName);
+				} else if (existing instanceof Set<?>) {
+					Set<?> casted = (Set<?>) existing;
+					if (casted.remove(oldDescription)) {
+						if (casted.size() == 1) {
+							lookupMap.put(objectName, casted.iterator().next());
+						} else if (casted.isEmpty()) {
+							lookupMap.remove(objectName);
+						}
+					}
+				}
 			}
 		}
 	}
 	
 	@Override
 	public Iterable<IEObjectDescription> getExportedObjects(final EClass type, final QualifiedName qualifiedName, final boolean ignoreCase) {
-		if (lookupMap.containsKey(qualifiedName.toLowerCase())) {
-			Collection<IResourceDescription> candidates = lookupMap.get(qualifiedName.toLowerCase());
-			return Iterables.concat(Iterables.transform(candidates, new Function<ISelectable, Iterable<IEObjectDescription>>() {
-				public Iterable<IEObjectDescription> apply(ISelectable from) {
-					if (from != null)
+		Object existing = lookupMap.get(qualifiedName.toLowerCase());
+		if (existing instanceof IResourceDescription) {
+			return ((IResourceDescription) existing).getExportedObjects(type, qualifiedName, ignoreCase);
+		} else if (existing instanceof Set<?>) {
+			@SuppressWarnings("unchecked")
+			Set<IResourceDescription> casted = (Set<IResourceDescription>) existing;
+			return Iterables.concat(Iterables.transform(casted, new Function<IResourceDescription, Iterable<IEObjectDescription>>() {
+				public Iterable<IEObjectDescription> apply(IResourceDescription from) {
+					if (from != null) {
 						return from.getExportedObjects(type, qualifiedName, ignoreCase);
+					}
 					return Collections.emptyList();
 				}
 			}));
@@ -118,10 +155,25 @@ public class ResourceDescriptionsData extends AbstractCompoundSelectable {
 		removeDescription(uri);
 		if (newDescription != null) {
 			resourceDescriptionMap.put(uri, newDescription);
-			for(IEObjectDescription object: newDescription.getExportedObjects()) {
-				lookupMap.put(object.getName().toLowerCase(), newDescription);
-			}
+			registerDescription(newDescription, lookupMap);
 		}
 	}
 
+	protected void registerDescription(IResourceDescription description, Map<QualifiedName, Object> target) {
+		for(IEObjectDescription object: description.getExportedObjects()) {
+			QualifiedName lowerCase = object.getName().toLowerCase();
+			Object existing = target.put(lowerCase, description);
+			if (existing != null && existing != description) {
+				if (existing instanceof IResourceDescription) {
+					Set<IResourceDescription> set = Sets.newHashSetWithExpectedSize(2);
+					set.add((IResourceDescription)existing);
+					set.add(description);
+				} else {
+					@SuppressWarnings("unchecked")
+					Set<IResourceDescription> set = (Set<IResourceDescription>) existing;
+					set.add(description);
+				}
+			}
+		}
+	}
 }
