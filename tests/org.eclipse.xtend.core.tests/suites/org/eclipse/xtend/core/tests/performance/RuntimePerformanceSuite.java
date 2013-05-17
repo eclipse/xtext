bd5f863d55ae468c4505f89efa575599bcbf143a
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2013-05-18 00:32:00 +0200

    [xtend][linking] Specialized linking proxies for Xtend
    This saves around 5 percent in the performance test:
    
    doCompileThreeTimes from xtend.core.tests.performance.PerformanceTest
    is down to around 6.1 secs (3 measurements), was
    6.5 secs
    
    This was implemented along the lines of bug
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=378088
    
    Change-Id: Iafc858911234af3d3e823c621a36f35faa7419dc

---
+++ tests/org.eclipse.xtend.core.tests/suites/org/eclipse/xtend/core/tests/performance/RuntimePerformanceSuite.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2013 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtend.core.tests.performance;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author Sebastian Zarnekow - Initial contribution and API
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+	PerformanceTest.class,
+	PerformanceTest.class,
+	PerformanceTest.class,
+	PerformanceTest.class,
+	PerformanceTest.class,
+	PerformanceTest.class,
+	PerformanceTest.class,
+	PerformanceTest.class,
+	PerformanceTest.class,
+	PerformanceTest.class,
+})
+public class RuntimePerformanceSuite {
+
+}
