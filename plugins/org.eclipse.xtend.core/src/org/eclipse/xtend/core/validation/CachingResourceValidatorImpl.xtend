a5c7aca299c4d306456fe665576934fb3afec807
Jan Koehnlein <jan.koehnlein@itemis.de>
2013-05-23 18:18:44 +0200

    [validation] deal with interrupted validation job and reduce # of unnecessary runs of the job https://bugs.eclipse.org/bugs/show_bug.cgi?id=408787
    Change-Id: I1c3101ea3fef06e9c69d7251d9f490fa130e9fdc

--- plugins/org.eclipse.xtend.core/src/org/eclipse/xtend/core/validation/CachingResourceValidatorImpl.xtend
+++ plugins/org.eclipse.xtend.core/src/org/eclipse/xtend/core/validation/CachingResourceValidatorImpl.xtend
@@ -6,15 +6,23 @@ import org.eclipse.emf.ecore.resource.Resource
 import org.eclipse.xtext.validation.CheckMode
 import org.eclipse.xtext.util.CancelIndicator
 import org.eclipse.xtext.util.OnChangeEvictingCache
+import org.eclipse.core.runtime.OperationCanceledException
 
 class CachingResourceValidatorImpl extends ResourceValidatorImpl {
 
 	@Inject OnChangeEvictingCache cache
 
 	override validate(Resource resource, CheckMode mode, CancelIndicator mon) {
-		return cache.get(mode, resource) [ |
-			super.validate(resource, mode, mon)
-		]
+		try {
+			return cache.get(mode, resource) [ |
+				val result = super.validate(resource, mode, mon)
+				if(mon != null && mon.canceled)
+					throw new OperationCanceledException
+				else
+					result
+			]
+		} catch(OperationCanceledException exc) {
+			return emptyList
+		}
 	}
-
 }
--- plugins/org.eclipse.xtend.core/xtend-gen/org/eclipse/xtend/core/validation/CachingResourceValidatorImpl.java
+++ plugins/org.eclipse.xtend.core/xtend-gen/org/eclipse/xtend/core/validation/CachingResourceValidatorImpl.java
@@ -1,14 +1,18 @@
 package org.eclipse.xtend.core.validation;
 
+import com.google.common.base.Objects;
 import com.google.inject.Provider;
 import java.util.List;
 import javax.inject.Inject;
+import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.xtext.util.CancelIndicator;
 import org.eclipse.xtext.util.OnChangeEvictingCache;
 import org.eclipse.xtext.validation.CheckMode;
 import org.eclipse.xtext.validation.Issue;
 import org.eclipse.xtext.validation.ResourceValidatorImpl;
+import org.eclipse.xtext.xbase.lib.CollectionLiterals;
+import org.eclipse.xtext.xbase.lib.Exceptions;
 
 @SuppressWarnings("all")
 public class CachingResourceValidatorImpl extends ResourceValidatorImpl {
@@ -16,12 +20,40 @@ public class CachingResourceValidatorImpl extends ResourceValidatorImpl {
   private OnChangeEvictingCache cache;
   
   public List<Issue> validate(final Resource resource, final CheckMode mode, final CancelIndicator mon) {
-    final Provider<List<Issue>> _function = new Provider<List<Issue>>() {
-        public List<Issue> get() {
-          List<Issue> _validate = CachingResourceValidatorImpl.super.validate(resource, mode, mon);
-          return _validate;
-        }
-      };
-    return this.cache.<List<Issue>>get(mode, resource, _function);
+    try {
+      final Provider<List<Issue>> _function = new Provider<List<Issue>>() {
+          public List<Issue> get() {
+            List<Issue> _xblockexpression = null;
+            {
+              final List<Issue> result = CachingResourceValidatorImpl.super.validate(resource, mode, mon);
+              List<Issue> _xifexpression = null;
+              boolean _and = false;
+              boolean _notEquals = (!Objects.equal(mon, null));
+              if (!_notEquals) {
+                _and = false;
+              } else {
+                boolean _isCanceled = mon.isCanceled();
+                _and = (_notEquals && _isCanceled);
+              }
+              if (_and) {
+                OperationCanceledException _operationCanceledException = new OperationCanceledException();
+                throw _operationCanceledException;
+              } else {
+                _xifexpression = result;
+              }
+              _xblockexpression = (_xifexpression);
+            }
+            return _xblockexpression;
+          }
+        };
+      return this.cache.<List<Issue>>get(mode, resource, _function);
+    } catch (final Throwable _t) {
+      if (_t instanceof OperationCanceledException) {
+        final OperationCanceledException exc = (OperationCanceledException)_t;
+        return CollectionLiterals.<Issue>emptyList();
+      } else {
+        throw Exceptions.sneakyThrow(_t);
+      }
+    }
   }
 }
