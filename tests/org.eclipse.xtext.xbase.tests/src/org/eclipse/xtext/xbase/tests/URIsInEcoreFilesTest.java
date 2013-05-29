583b20141846438fd87463bd0fb213662664e418
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2013-05-29 09:42:32 +0200

    [tests] Reenabled URI validation in *.ecore files
    see https://bugs.eclipse.org/bugs/show_bug.cgi?id=406460
    
    Change-Id: I646679295d5b7509d031d636c5588eaf4dbc1e6e

--- tests/org.eclipse.xtext.xbase.tests/src/org/eclipse/xtext/xbase/tests/URIsInEcoreFilesTest.java
+++ tests/org.eclipse.xtext.xbase.tests/src/org/eclipse/xtext/xbase/tests/URIsInEcoreFilesTest.java
@@ -24,7 +24,6 @@ import org.eclipse.xtext.resource.XtextResourceSet;
 import org.eclipse.xtext.xbase.lib.Functions;
 import org.eclipse.xtext.xbase.lib.ListExtensions;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.google.common.collect.Lists;
@@ -32,7 +31,6 @@ import com.google.common.collect.Lists;
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
  */
-@Ignore
 public class URIsInEcoreFilesTest extends AbstractXbaseTestCase {
 
 	private ResourceSet resourceSet;
