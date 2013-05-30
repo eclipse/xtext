7e3ff46e9242c40ff61a3953bf5f542755939222
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2013-05-30 10:59:48 +0200

    [xbase.tutorial] Reflect syntax improvements in tutorial
    Change-Id: I230753cfadb772fb51287f55dce4be70fa45d797

--- examples/org.eclipse.xtext.xtext.ui.examples/contents/xbase.tutorial/src-gen/Xbase01_Literals.java
+++ examples/org.eclipse.xtext.xtext.ui.examples/contents/xbase.tutorial/src-gen/Xbase01_Literals.java
@@ -1,28 +1,52 @@
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import org.eclipse.xtext.xbase.lib.CollectionLiterals;
 import org.eclipse.xtext.xbase.lib.Pair;
 
 @SuppressWarnings("all")
 public class Xbase01_Literals {
-  public BigDecimal myMethod() throws Throwable {
-    BigDecimal _xblockexpression = null;
+  public Map<String,String> myMethod() throws Throwable {
+    Map<String,String> _xblockexpression = null;
     {
       /* null */
       /* "Hello World" */
-      /* 42 */
-      /* 0.42e2 */
       /* true */
       /* false */
+      /* 42 */
+      /* 0.42e2 */
+      /* new BigInteger("beefbeefbeefbeefbeef", 16) */
+      /* new BigDecimal("0.123456789123456789123456789123456789e4242") */
       /* String.class */
+      String.class.getField("CASE_INSENSITIVE_ORDER");
+      /* Entry.class */
       /* CollectionLiterals.<Integer>newArrayList(Integer.valueOf(3), Integer.valueOf(5), Integer.valueOf(7)); */
       /* CollectionLiterals.<String>newHashSet("foo", "bar", "baz"); */
       Pair<Integer,String> _mappedTo = Pair.<Integer, String>of(Integer.valueOf(1), "one");
       Pair<Integer,String> _mappedTo_1 = Pair.<Integer, String>of(Integer.valueOf(2), "two");
       Pair<Integer,String> _mappedTo_2 = Pair.<Integer, String>of(Integer.valueOf(3), "three");
       /* CollectionLiterals.<Integer, String>newHashMap(_mappedTo, _mappedTo_1, _mappedTo_2); */
-      /* new BigInteger("beefbeefbeefbeefbeef", 16) */
-      _xblockexpression = (new BigDecimal("0.123456789123456789123456789123456789e4242"));
+      String[] stringArray = { "a", "b", "c" };
+      /* stringArray[1]; */
+      List<String> listOfStrings = Collections.<String>unmodifiableList(Lists.<String>newArrayList("a", "b", "c"));
+      /* listOfStrings; */
+      Set<String> setOfStrings = Collections.<String>unmodifiableSet(Sets.<String>newHashSet("a", "b", "c"));
+      /* setOfStrings; */
+      Map<String,String> _xsetliteral = null;
+      Map<String,String> _tempMap = Maps.<String, String>newHashMap();
+      _tempMap.put("a", "A");
+      _tempMap.put("b", "B");
+      _tempMap.put("c", "C");
+      _xsetliteral = Collections.<String, String>unmodifiableMap(_tempMap);
+      Map<String,String> immutableMap = _xsetliteral;
+      _xblockexpression = (immutableMap);
     }
     return _xblockexpression;
   }
--- examples/org.eclipse.xtext.xtext.ui.examples/contents/xbase.tutorial/src-gen/Xbase11_TypeCoercion.java
+++ examples/org.eclipse.xtext.xtext.ui.examples/contents/xbase.tutorial/src-gen/Xbase11_TypeCoercion.java
@@ -4,24 +4,30 @@ import org.eclipse.xtext.xbase.lib.Conversions;
 
 @SuppressWarnings("all")
 public class Xbase11_TypeCoercion {
-  public void myMethod() throws Throwable {
-    "string".length();
-    final Integer i = Integer.valueOf("string".length());
-    /* i; */
-    int _length = "string".length();
-    Integer.valueOf(_length).toString();
-    "string".toCharArray();
-    final List<Character> l = ((List<Character>)Conversions.doWrapArray("string".toCharArray()));
-    char[] _charArray = "string".toCharArray();
-    /* _charArray[3]; */
-    final Comparator<String> _function = new Comparator<String>() {
-        public int compare(final String a, final String b) {
-          int _length = a.length();
-          int _length_1 = b.length();
-          int _compareTo = Integer.valueOf(_length).compareTo(Integer.valueOf(_length_1));
-          return _compareTo;
-        }
-      };
-    final Comparator<String> comparator = _function;
+  public int myMethod() throws Throwable {
+    int _xblockexpression = (int) 0;
+    {
+      "string".length();
+      final Integer i = Integer.valueOf("string".length());
+      /* i; */
+      int _length = "string".length();
+      Integer.valueOf(_length).toString();
+      "string".toCharArray();
+      char[] _charArray = "string".toCharArray();
+      /* _charArray[3]; */
+      final List<Character> l = ((List<Character>)Conversions.doWrapArray("string".toCharArray()));
+      final Comparator<String> _function = new Comparator<String>() {
+          public int compare(final String a, final String b) {
+            int _length = a.length();
+            int _length_1 = b.length();
+            int _compareTo = Integer.valueOf(_length).compareTo(Integer.valueOf(_length_1));
+            return _compareTo;
+          }
+        };
+      final Comparator<String> comparator = _function;
+      int _compare = comparator.compare("1", " 2");
+      _xblockexpression = (_compare);
+    }
+    return _xblockexpression;
   }
 }
