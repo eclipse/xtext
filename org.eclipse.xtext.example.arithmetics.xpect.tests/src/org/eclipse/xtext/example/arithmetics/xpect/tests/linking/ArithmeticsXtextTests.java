872b14f1db7333d54eb5b2aebd42c01495de7aa6
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2013-06-11 09:38:19 +0200

    added linking test to arithmetics example
    Signed-off-by: Moritz Eysholdt <moritz.eysholdt@itemis.de>

---
+++ org.eclipse.xtext.example.arithmetics.xpect.tests/src/org/eclipse/xtext/example/arithmetics/xpect/tests/linking/ArithmeticsXtextTests.java
@@ -0,0 +1,10 @@
+package org.eclipse.xtext.example.arithmetics.xpect.tests.linking;
+
+import org.junit.runner.RunWith;
+import org.xpect.runner.XpectRunner;
+import org.xpect.xtext.lib.tests.XtextTests;
+
+@RunWith(XpectRunner.class)
+public class ArithmeticsXtextTests extends XtextTests {
+
+}
