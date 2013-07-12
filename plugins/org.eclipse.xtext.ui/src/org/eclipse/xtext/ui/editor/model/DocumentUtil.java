91dd842a3e7dc531bf8b755ecbbc7b1d4551d538
akosyakov <anton.kosyakov@itemis.de>
2013-07-12 11:21:37 +0200

    [410481]: replacing the invocation of IDocument#get(offset, length) method by invocations of String#indexOf and String#lastIndexOf methods
    Change-Id: Ic9fcd8346640e8ef3462d6dde9c71f472f7639a5
    Signed-off-by: akosyakov <anton.kosyakov@itemis.de>

--- plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/model/DocumentUtil.java
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/model/DocumentUtil.java
@@ -41,6 +41,33 @@ public class DocumentUtil {
 		return null;
 	}
 	
+	/**
+	 * searches backwards for the given string within the same partition type
+	 * @return the region of the match or <code>null</code> if no match were found
+	 * @since 2.4
+	 */
+	public IRegion searchBackwardsInSamePartition(String toFind, String documentText, IDocument document, int endOffset) throws BadLocationException {
+		if (endOffset < 0) {
+			return null;
+		}
+		int length = toFind.length();
+		String text = preProcessSearchString(documentText);
+		ITypedRegion partition = document.getPartition(endOffset);
+		int indexOf = text.lastIndexOf(toFind, endOffset - length);
+		while (indexOf >= 0) {
+			ITypedRegion partition2 = document.getPartition(indexOf);
+			if (partition2.getType().equals(partition.getType())) {
+				return new Region(indexOf, length);
+			}
+			indexOf = text.lastIndexOf(toFind, partition2.getOffset() - length);
+		}
+		String trimmed = toFind.trim();
+		if (trimmed.length() > 0 && trimmed.length() != length) {
+			return searchBackwardsInSamePartition(trimmed, documentText, document, endOffset);
+		}
+		return null;
+	}
+	
 	protected String preProcessSearchString(String string) {
 		return string;
 	}
@@ -77,6 +104,48 @@ public class DocumentUtil {
 			return searchInSamePartition(trimmed, document, startOffset);
 		}
 		return null;
+	}
+	
+	/**
+	 * searches for the given string within the same partition type
+	 * 
+	 * @return the region of the match or <code>null</code> if no match were found
+	 * @since 2.4
+	 */
+	public IRegion searchInSamePartition(String toFind, String documentText, IDocument document, int startOffset)
+			throws BadLocationException {
+		if (startOffset >= document.getLength()) {
+			return null;
+		}
+		String text = preProcessSearchString(documentText);
+		ITypedRegion partition = document.getPartition(startOffset);
+		
+		int indexOf = text.indexOf(toFind, getOffset(toFind, startOffset));
+		while (indexOf >= 0 && indexOf < document.getLength()) {
+			ITypedRegion partition2 = document.getPartition(indexOf);
+			if (partition2.getType().equals(partition.getType())) {
+				return new Region(indexOf, toFind.length());
+			}
+			indexOf = text.indexOf(toFind, partition2.getOffset() + partition2.getLength());
+		}
+		String trimmed = toFind.trim();
+		if (trimmed.length() > 0 && trimmed.length() != toFind.length()) {
+			return searchInSamePartition(trimmed, documentText, document, startOffset);
+		}
+		return null;
+	}
+
+	private int getOffset(String toFind, int startOffset) {
+		int whitespacesCount = getWhitespacesCount(toFind);
+		return Math.max(startOffset - whitespacesCount, 0);
+	}
+
+	private int getWhitespacesCount(String toFind) {
+		int whitespacesCount = 0;
+		while (toFind.length() > whitespacesCount && Character.isWhitespace(toFind.charAt(whitespacesCount))) {
+			whitespacesCount++;
+		}
+		return whitespacesCount;
 	}
 
 	public boolean isSameLine(IDocument doc, int offset, int offset2) throws BadLocationException {
