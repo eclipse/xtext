35b84284e764a37e1305d4498eb2bb9c83ff04de
Dennis Huebner <dennis.huebner@itemis.de>
2013-07-18 18:19:36 +0200

    [413156] remove dependency to easymock
    Change-Id: I69e45b1e633124533cb134365a3c3e90230f7154

--- tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/parser/datatyperules/MetamodelTransformationErrorTest.java
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/parser/datatyperules/MetamodelTransformationErrorTest.java
@@ -7,17 +7,14 @@
  *******************************************************************************/
 package org.eclipse.xtext.parser.datatyperules;
 
-import static org.easymock.EasyMock.*;
-
 import java.util.List;
 
-import org.easymock.EasyMock;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.xtext.Grammar;
 import org.eclipse.xtext.XtextStandaloneSetup;
 import org.eclipse.xtext.junit4.AbstractXtextTests;
 import org.eclipse.xtext.resource.XtextResource;
-import org.eclipse.xtext.xtext.ecoreInference.ErrorAcceptor;
+import org.eclipse.xtext.tests.TestErrorAcceptor;
 import org.eclipse.xtext.xtext.ecoreInference.TransformationErrorCode;
 import org.eclipse.xtext.xtext.ecoreInference.Xtext2EcoreTransformer;
 import org.junit.Test;
@@ -30,48 +27,48 @@ public class MetamodelTransformationErrorTest extends AbstractXtextTests {
 	private String model;
 	private XtextResource resource;
 	private Xtext2EcoreTransformer transformer;
-	private ErrorAcceptor errorAcceptor;
+	private TestErrorAcceptor errorAcceptor;
 	private Grammar grammar;
 
 	@Override
 	public void setUp() throws Exception {
 		super.setUp();
 		with(XtextStandaloneSetup.class);
-		model = "grammar datatypetests with org.eclipse.xtext.common.Terminals\n" +
-				"import 'http://www.eclipse.org/emf/2002/Ecore' as ecore\n" +
-				"generate metamodel 'http://fooo'\n" +
-				"Start:\n" +
-				"  id=ValidId id2=ValidId2 failure1=FailureId failure2=Failure2;\n" +
-				"ValidId returns ecore::EString: ID '.' ID;\n" +
-				"ValidId2 returns ecore::EString: ID '.' ValidId;\n" +
-				"FailureId returns ecore::EString: name=ID;\n" +
-				"Failure2 returns ecore::EString: name=Start;";
+		model = "grammar datatypetests with org.eclipse.xtext.common.Terminals\n"
+				+ "import 'http://www.eclipse.org/emf/2002/Ecore' as ecore\n" + "generate metamodel 'http://fooo'\n"
+				+ "Start:\n" + "  id=ValidId id2=ValidId2 failure1=FailureId failure2=Failure2;\n"
+				+ "ValidId returns ecore::EString: ID '.' ID;\n" + "ValidId2 returns ecore::EString: ID '.' ValidId;\n"
+				+ "FailureId returns ecore::EString: name=ID;\n" + "Failure2 returns ecore::EString: name=Start;";
 		resource = getResourceFromStringAndExpect(model, 2);
 		grammar = (Grammar) resource.getContents().get(0);
 		transformer = new Xtext2EcoreTransformer(grammar);
-		errorAcceptor = EasyMock.createMock(ErrorAcceptor.class);
+		errorAcceptor = new TestErrorAcceptor();
 		transformer.setErrorAcceptor(errorAcceptor);
 	}
 
-	@Test public void testSetUp() {
+	@Test
+	public void testSetUp() {
 		assertNotNull(resource);
 		assertNotNull(transformer);
 		assertNotNull(errorAcceptor);
 		assertEquals(2, resource.getErrors().size());
 	}
 
-	@Test public void testErrorMessages() throws Exception {
-		errorAcceptor.acceptError(same(TransformationErrorCode.InvalidDatatypeRule), (String) anyObject(), same(grammar.getRules().get(3)));
-		errorAcceptor.acceptError(same(TransformationErrorCode.InvalidDatatypeRule), (String) anyObject(), same(grammar.getRules().get(4)));
+	@Test
+	public void testErrorMessages() throws Exception {
+		errorAcceptor.acceptError(TransformationErrorCode.InvalidDatatypeRule, TestErrorAcceptor.ANY_STRING, grammar
+				.getRules().get(3));
+		errorAcceptor.acceptError(TransformationErrorCode.InvalidDatatypeRule, TestErrorAcceptor.ANY_STRING, grammar
+				.getRules().get(4));
+		errorAcceptor.replay();
 		transform();
 	}
 
 	private EPackage transform() throws Exception {
-		replay(errorAcceptor);
 		transformer.removeGeneratedPackages();
 		transformer.transform();
 		List<EPackage> metamodels = transformer.getGeneratedPackages();
-		verify(errorAcceptor);
+		errorAcceptor.verify();
 		assertNotNull(metamodels);
 		assertEquals(1, metamodels.size());
 		return metamodels.get(0);
