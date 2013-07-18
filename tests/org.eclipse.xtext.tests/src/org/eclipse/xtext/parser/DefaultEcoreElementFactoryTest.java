d8fbad65ec407915012d7d9a2e8a2945f7280f78
Dennis Huebner <dennis.huebner@itemis.de>
2013-07-18 18:19:36 +0200

    [413156] remove dependency to easymock
    Change-Id: I69e45b1e633124533cb134365a3c3e90230f7154

--- tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/parser/DefaultEcoreElementFactoryTest.java
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/parser/DefaultEcoreElementFactoryTest.java
@@ -8,13 +8,15 @@
  *******************************************************************************/
 package org.eclipse.xtext.parser;
 
-import org.easymock.EasyMock;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EcoreFactory;
 import org.eclipse.xtext.conversion.IValueConverterService;
 import org.eclipse.xtext.conversion.ValueConverterException;
 import org.eclipse.xtext.nodemodel.ILeafNode;
+import org.eclipse.xtext.nodemodel.INode;
 import org.eclipse.xtext.nodemodel.impl.LeafNode;
+import org.eclipse.xtext.xbase.lib.Functions;
+import org.eclipse.xtext.xbase.lib.Functions.Function2;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -23,60 +25,97 @@ import org.junit.Test;
  */
 public class DefaultEcoreElementFactoryTest extends Assert {
 
-    @Test public void testConvertNullInput() throws Exception {
-        EClass eClass = EcoreFactory.eINSTANCE.createEClass();
-        IValueConverterService converter = EasyMock.createMock(IValueConverterService.class);
+	@Test
+	public void testConvertNullInput() throws Exception {
+		EClass eClass = EcoreFactory.eINSTANCE.createEClass();
+		Function2<String, INode, Object> toValueImpl = new Function2<String, INode, Object>() {
 
-        converter.toValue(null, "foo", null);
-        EasyMock.expectLastCall().andReturn("FOO");
+			public Object apply(String lexerRule, INode node) {
+				if ("foo".equals(lexerRule) ) {
+					return "FOO";
+				}
+				return null;
+			}
+		};
+		DefaultEcoreElementFactory factory = new DefaultEcoreElementFactory();
+		factory.setConverterService(new MockedConverterService(toValueImpl));
+		factory.set(eClass, "name", null, "foo", null);
 
-        EasyMock.replay(converter);
-        DefaultEcoreElementFactory factory = new DefaultEcoreElementFactory();
-        factory.setConverterService(converter);
-        factory.set(eClass, "name", null, "foo", null);
+		assertEquals("FOO", eClass.getName());
+	}
 
-        EasyMock.verify(converter);
-        assertEquals("FOO", eClass.getName());
-    }
-    
-    @Test public void testNullInput() throws Exception {
-    	EClass eClass = EcoreFactory.eINSTANCE.createEClass();
-    	ILeafNode node = new LeafNode();
-        IValueConverterService converter = EasyMock.createMock(IValueConverterService.class);
-        converter.toValue(null, "foo", node);
-        EasyMock.expectLastCall().andReturn(null);
-        EasyMock.replay(converter);
-        
-        DefaultEcoreElementFactory factory = new DefaultEcoreElementFactory();
-        factory.setConverterService(converter);
-        try {
-        	factory.set(eClass, "abstract", null, "foo", node);
-        	fail("Expected ValueConverterException");
-        } catch(ValueConverterException ex) {
-        	assertNull(ex.getCause());
-        	assertTrue(ex.getMessage().indexOf("ValueConverter returned null for") >= 0);
-        	assertSame(node, ex.getNode());
-        }
-        EasyMock.verify(converter);
-    }
-    
-    @Test public void testValueConverterException() throws Exception {
-    	EClass eClass = EcoreFactory.eINSTANCE.createEClass();
-    	ILeafNode node = new LeafNode();
-    	ValueConverterException expected = new ValueConverterException("Foo", node, null);
-        IValueConverterService converter = EasyMock.createMock(IValueConverterService.class);
-        converter.toValue(null, "foo", node);
-        EasyMock.expectLastCall().andThrow(expected);
-        EasyMock.replay(converter);
-        
-        DefaultEcoreElementFactory factory = new DefaultEcoreElementFactory();
-        factory.setConverterService(converter);
-        try {
-        	factory.set(eClass, "abstract", null, "foo", node);
-        	fail("Expected ValueConverterException");
-        } catch(ValueConverterException ex) {
-        	assertSame(expected, ex);
-        }
-        EasyMock.verify(converter);
-    }
+	@Test
+	public void testNullInput() throws Exception {
+		EClass eClass = EcoreFactory.eINSTANCE.createEClass();
+		final ILeafNode node = new LeafNode();
+		
+		Function2<String, INode, Object> toValueImpl = new Function2<String, INode, Object>() {
+
+			public Object apply(String lexerRule, INode nodeParam) {
+				if ("foo".equals(lexerRule) && nodeParam.equals(node)) {
+					return null;
+				}
+				fail();
+				return null;
+			}
+		};
+
+		DefaultEcoreElementFactory factory = new DefaultEcoreElementFactory();
+		factory.setConverterService(new MockedConverterService(toValueImpl));
+		try {
+			factory.set(eClass, "abstract", null, "foo", node);
+			fail("Expected ValueConverterException");
+		} catch (ValueConverterException ex) {
+			assertNull(ex.getCause());
+			assertTrue(ex.getMessage().indexOf("ValueConverter returned null for") >= 0);
+			assertSame(node, ex.getNode());
+		}
+	}
+
+	@Test
+	public void testValueConverterException() throws Exception {
+		EClass eClass = EcoreFactory.eINSTANCE.createEClass();
+		final ILeafNode node = new LeafNode();
+		final ValueConverterException expected = new ValueConverterException("Foo", node, null);
+		
+		Function2<String, INode, Object> toValueImpl = new Function2<String, INode, Object>() {
+
+			public Object apply(String lexerRule, INode nodeParam) {
+				if ("foo".equals(lexerRule) && node.equals(nodeParam)) {
+					throw expected;
+				}
+				fail();
+				return null;
+			}
+		};
+
+		DefaultEcoreElementFactory factory = new DefaultEcoreElementFactory();
+		factory.setConverterService(new MockedConverterService(toValueImpl));
+		try {
+			factory.set(eClass, "abstract", null, "foo", node);
+			fail("Expected ValueConverterException");
+		} catch (ValueConverterException ex) {
+			assertSame(expected, ex);
+		}
+	}
+
+	/**
+	 * @author dhuebner - Initial contribution and API
+	 */
+	private final class MockedConverterService implements IValueConverterService {
+		private final Functions.Function2<String, INode, Object> toValueFunction;
+
+		public MockedConverterService(Functions.Function2<String, INode, Object> toValueFunction) {
+			this.toValueFunction = toValueFunction;
+		}
+
+		public Object toValue(String string, String lexerRule, INode nodeParam) throws ValueConverterException {
+			return toValueFunction.apply(lexerRule, nodeParam);
+		}
+
+		public String toString(Object value, String lexerRule) {
+			return null;
+		}
+	}
+
 }
