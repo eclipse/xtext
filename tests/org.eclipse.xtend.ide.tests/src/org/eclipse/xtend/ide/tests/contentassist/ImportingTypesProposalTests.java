2dbbabcee9f4ffb884e2b7460711a428a6c1c490
Jan Koehnlein <jan.koehnlein@itemis.de>
2013-07-25 15:04:14 +0200

    [contentassist] tests for https://bugs.eclipse.org/bugs/show_bug.cgi?id=413711
    Change-Id: Id074c09c1210a01efbfa94cfc506910e35d509e0

--- tests/org.eclipse.xtend.ide.tests/src/org/eclipse/xtend/ide/tests/contentassist/ImportingTypesProposalTests.java
+++ tests/org.eclipse.xtend.ide.tests/src/org/eclipse/xtend/ide/tests/contentassist/ImportingTypesProposalTests.java
@@ -15,11 +15,12 @@ import com.google.inject.Inject;
 /**
  * @author Jan Koehnlein - Initial contribution and API
  */
-public class Bug410060Test extends AbstractXtendContentAssistBugTest {
+public class ImportingTypesProposalTests extends AbstractXtendContentAssistBugTest {
 
 	@Inject 
 	private ILineSeparatorInformation lineSepInfo;  
 	
+	// @see https://bugs.eclipse.org/bugs/show_bug.cgi?id=410060
 	@Test public void testInnerClass() throws Exception {
 		String lineSeparator = lineSepInfo.getLineSeparator();
 		newBuilder()
@@ -30,7 +31,21 @@ public class Bug410060Test extends AbstractXtendContentAssistBugTest {
 					+ lineSeparator 
 					+ "import java.lang.Character.Subset"
 					+ lineSeparator + lineSeparator
-					+ "class Foo { Subset");
+					+ "class Foo { Subset")
+			.assertCursorIsAfter("{ Subset");
 	}
 	
+	// @see https://bugs.eclipse.org/bugs/show_bug.cgi?id=413711
+	@Test public void testTopLevelClass() throws Exception {
+		String lineSeparator = lineSepInfo.getLineSeparator();
+		newBuilder()
+			.append("class Foo { WeakHashMa")
+			.assertProposal("java.util.WeakHashMap")
+			.apply()
+			.expectContent("import java.util.WeakHashMap" 
+					+ lineSeparator + lineSeparator
+					+ "class Foo { WeakHashMap")
+			.assertCursorIsAfter("{ WeakHashMap");
+	}
+
 }
