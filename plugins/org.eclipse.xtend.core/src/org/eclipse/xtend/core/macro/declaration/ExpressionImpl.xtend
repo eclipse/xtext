b20f6a9a8f85fbf02cdd5ba590e71c24a47c5f2f
Sven Efftinge <sven.efftinge@itemis.de>
2013-08-08 14:11:44 +0200

    [xtend aa] better 'toString' for expressions.

--- plugins/org.eclipse.xtend.core/src/org/eclipse/xtend/core/macro/declaration/ExpressionImpl.xtend
+++ plugins/org.eclipse.xtend.core/src/org/eclipse/xtend/core/macro/declaration/ExpressionImpl.xtend
@@ -2,11 +2,12 @@ package org.eclipse.xtend.core.macro.declaration
 
 import org.eclipse.xtend.lib.macro.expression.Expression
 import org.eclipse.xtext.xbase.XExpression
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils
 
 class ExpressionImpl extends AbstractElementImpl<XExpression> implements Expression {
 	
 	override toString() {
-		delegate.toString
+		NodeModelUtils.getNode(delegate).text.trim
 	}
 	
 }
--- plugins/org.eclipse.xtend.core/xtend-gen/org/eclipse/xtend/core/macro/declaration/ExpressionImpl.java
+++ plugins/org.eclipse.xtend.core/xtend-gen/org/eclipse/xtend/core/macro/declaration/ExpressionImpl.java
@@ -2,13 +2,17 @@ package org.eclipse.xtend.core.macro.declaration;
 
 import org.eclipse.xtend.core.macro.declaration.AbstractElementImpl;
 import org.eclipse.xtend.lib.macro.expression.Expression;
+import org.eclipse.xtext.nodemodel.ICompositeNode;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.eclipse.xtext.xbase.XExpression;
 
 @SuppressWarnings("all")
 public class ExpressionImpl extends AbstractElementImpl<XExpression> implements Expression {
   public String toString() {
     XExpression _delegate = this.getDelegate();
-    String _string = _delegate.toString();
-    return _string;
+    ICompositeNode _node = NodeModelUtils.getNode(_delegate);
+    String _text = _node.getText();
+    String _trim = _text.trim();
+    return _trim;
   }
 }
