aeb5458fa182cf093509259994d740353f915600
Jan Koehnlein <jan.koehnlein@itemis.de>
2013-08-09 09:24:46 +0200

    [resource] integrate EagerResourceSetBasedResourceDescriptions with IContainer infrastructure
    Change-Id: I06fb8cd22f44d97563ab28f405d27b2808257039

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/resource/containers/ResourceSetBasedAllContainersStateProvider.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/resource/containers/ResourceSetBasedAllContainersStateProvider.java
@@ -11,6 +11,7 @@ import org.apache.log4j.Logger;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.xtext.resource.IResourceDescriptions;
+import org.eclipse.xtext.resource.impl.EagerResourceSetBasedResourceDescriptions;
 import org.eclipse.xtext.resource.impl.ResourceSetBasedResourceDescriptions;
 
 /**
@@ -21,19 +22,25 @@ public class ResourceSetBasedAllContainersStateProvider implements IAllContainer
 	private final static Logger log = Logger.getLogger(ResourceSetBasedAllContainersStateProvider.class);
 
 	public IAllContainersState get(IResourceDescriptions context) {
-		if (context instanceof ResourceSetBasedResourceDescriptions) {
-			ResourceSetBasedResourceDescriptions resourceDescs = (ResourceSetBasedResourceDescriptions) context;
-			ResourceSet resourceSet = resourceDescs.getResourceSet();
-			IAllContainersState adapter = (IAllContainersState) EcoreUtil.getAdapter(resourceSet.eAdapters(),
-					IAllContainersState.class);
-			if (adapter == null) {
-				return handleAdapterNotFound(resourceSet);
-			}
-			return adapter;
+		ResourceSet resourceSet = getResourceSet(context);
+		IAllContainersState adapter = (IAllContainersState) EcoreUtil.getAdapter(resourceSet.eAdapters(),
+				IAllContainersState.class);
+		if (adapter == null) {
+			return handleAdapterNotFound(resourceSet);
 		}
+		return adapter;
+	}
+	
+	/**
+	 * @since 2.4
+	 */
+	protected ResourceSet getResourceSet(IResourceDescriptions context) {
+		if (context instanceof ResourceSetBasedResourceDescriptions) 
+			return ((ResourceSetBasedResourceDescriptions) context).getResourceSet();
+		else if (context instanceof EagerResourceSetBasedResourceDescriptions) 
+			return ((EagerResourceSetBasedResourceDescriptions) context).getResourceSet();
 		String contextType = context == null ? "null" : context.getClass().getName();
-		throw new IllegalStateException("Passed " + contextType + " not of type "
-				+ ResourceSetBasedResourceDescriptions.class.getName());
+		throw new IllegalStateException("Passed " + contextType + " is not based on a resource set");
 	}
 
 	protected IAllContainersState handleAdapterNotFound(final ResourceSet resourceSet) {
