809d52dcce9c8a7076969dcdd285c556bf247919
akosyakov <anton.kosyakov@itemis.de>
2013-08-13 10:14:16 +0200

    [Bug 403563]: 1. filter processors' errors out from log 2. print a stack trace until the line with AnnotationProcessor class
    Signed-off-by: akosyakov <anton.kosyakov@itemis.de>

--- tests/org.eclipse.xtend.core.tests/xtend-gen/org/eclipse/xtend/core/tests/macro/ActiveAnnotationsRuntimeTest.java
+++ tests/org.eclipse.xtend.core.tests/xtend-gen/org/eclipse/xtend/core/tests/macro/ActiveAnnotationsRuntimeTest.java
@@ -5,27 +5,37 @@ import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import java.io.File;
+import java.util.List;
 import java.util.Map;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.Resource.Diagnostic;
+import org.eclipse.xtend.core.macro.AnnotationProcessor;
 import org.eclipse.xtend.core.macro.ProcessorInstanceForJvmTypeProvider;
 import org.eclipse.xtend.core.macro.declaration.CompilationUnitImpl;
 import org.eclipse.xtend.core.tests.RuntimeInjectorProvider;
 import org.eclipse.xtend.core.tests.macro.AbstractReusableActiveAnnotationTests;
 import org.eclipse.xtend.core.tests.macro.DelegatingClassloader;
+import org.eclipse.xtend.core.validation.IssueCodes;
 import org.eclipse.xtend.core.xtend.XtendFile;
 import org.eclipse.xtend.lib.macro.declaration.MutableTypeDeclaration;
 import org.eclipse.xtend.lib.macro.file.MutableFileSystemSupport;
 import org.eclipse.xtend.lib.macro.file.Path;
+import org.eclipse.xtend2.lib.StringConcatenation;
 import org.eclipse.xtext.junit4.InjectWith;
 import org.eclipse.xtext.junit4.XtextRunner;
 import org.eclipse.xtext.junit4.validation.ValidationTestHelper;
+import org.eclipse.xtext.resource.IResourceServiceProvider;
+import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.resource.XtextResourceSet;
+import org.eclipse.xtext.util.CancelIndicator;
 import org.eclipse.xtext.util.Files;
 import org.eclipse.xtext.util.IAcceptor;
+import org.eclipse.xtext.validation.CheckMode;
+import org.eclipse.xtext.validation.IResourceValidator;
+import org.eclipse.xtext.validation.Issue;
 import org.eclipse.xtext.xbase.compiler.CompilationTestHelper;
 import org.eclipse.xtext.xbase.compiler.CompilationTestHelper.Result;
 import org.eclipse.xtext.xbase.file.ProjectConfig;
@@ -174,35 +184,10 @@ public class ActiveAnnotationsRuntimeTest extends AbstractReusableActiveAnnotati
   }
   
   public void assertProcessing(final Pair<String,String> macroFile, final Pair<String,String> clientFile, final Procedure1<? super CompilationUnitImpl> expectations) {
-    final URI macroURI = this.copyToDisk(this.macroProject, macroFile);
-    final URI clientURI = this.copyToDisk(this.clientProject, clientFile);
-    final XtextResourceSet macroResourceSet = this.resourceSetProvider.get();
-    Class<? extends ActiveAnnotationsRuntimeTest> _class = this.getClass();
-    ClassLoader _classLoader = _class.getClassLoader();
-    macroResourceSet.setClasspathURIContext(_classLoader);
-    macroResourceSet.createResource(macroURI);
-    final XtextResourceSet resourceSet = this.resourceSetProvider.get();
-    resourceSet.createResource(clientURI);
-    final IAcceptor<Result> _function = new IAcceptor<Result>() {
-      public void accept(final Result result) {
-        Class<? extends ActiveAnnotationsRuntimeTest> _class = ActiveAnnotationsRuntimeTest.this.getClass();
-        ClassLoader _classLoader = _class.getClassLoader();
-        final Function1<String,Class<? extends Object>> _function = new Function1<String,Class<? extends Object>>() {
-          public Class<? extends Object> apply(final String it) {
-            Class<? extends Object> _compiledClass = result.getCompiledClass(it);
-            return _compiledClass;
-          }
-        };
-        DelegatingClassloader _delegatingClassloader = new DelegatingClassloader(_classLoader, _function);
-        final DelegatingClassloader classLoader = _delegatingClassloader;
-        resourceSet.setClasspathURIContext(classLoader);
-        ActiveAnnotationsRuntimeTest.this.processorProvider.setClassLoader(classLoader);
-      }
-    };
-    this.compiler.compile(macroResourceSet, _function);
+    final XtextResourceSet resourceSet = this.compileMacroResourceSet(macroFile, clientFile);
     EList<Resource> _resources = resourceSet.getResources();
     final Resource singleResource = IterableExtensions.<Resource>head(_resources);
-    final IAcceptor<Result> _function_1 = new IAcceptor<Result>() {
+    final IAcceptor<Result> _function = new IAcceptor<Result>() {
       public void accept(final Result it) {
         final CompilationUnitImpl unit = ActiveAnnotationsRuntimeTest.this.compilationUnitProvider.get();
         EList<EObject> _contents = singleResource.getContents();
@@ -216,11 +201,131 @@ public class ActiveAnnotationsRuntimeTest extends AbstractReusableActiveAnnotati
         Assert.assertTrue(_isEmpty);
       }
     };
-    this.compiler.compile(resourceSet, _function_1);
+    this.compiler.compile(resourceSet, _function);
+  }
+  
+  public void assertIssues(final Pair<String,String> macroFile, final Pair<String,String> clientFile, final Procedure1<? super List<Issue>> expectations) {
+    try {
+      final XtextResourceSet resourceSet = this.compileMacroResourceSet(macroFile, clientFile);
+      EList<Resource> _resources = resourceSet.getResources();
+      Resource _head = IterableExtensions.<Resource>head(_resources);
+      final XtextResource singleResource = ((XtextResource) _head);
+      boolean _isLoaded = singleResource.isLoaded();
+      boolean _not = (!_isLoaded);
+      if (_not) {
+        Map<Object,Object> _loadOptions = resourceSet.getLoadOptions();
+        singleResource.load(_loadOptions);
+      }
+      IResourceServiceProvider _resourceServiceProvider = singleResource.getResourceServiceProvider();
+      final IResourceValidator validator = _resourceServiceProvider.getResourceValidator();
+      List<Issue> _validate = validator.validate(singleResource, CheckMode.ALL, CancelIndicator.NullImpl);
+      expectations.apply(_validate);
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
+  
+  public XtextResourceSet compileMacroResourceSet(final Pair<String,String> macroFile, final Pair<String,String> clientFile) {
+    XtextResourceSet _xblockexpression = null;
+    {
+      final URI macroURI = this.copyToDisk(this.macroProject, macroFile);
+      final URI clientURI = this.copyToDisk(this.clientProject, clientFile);
+      final XtextResourceSet macroResourceSet = this.resourceSetProvider.get();
+      Class<? extends ActiveAnnotationsRuntimeTest> _class = this.getClass();
+      ClassLoader _classLoader = _class.getClassLoader();
+      macroResourceSet.setClasspathURIContext(_classLoader);
+      macroResourceSet.createResource(macroURI);
+      final XtextResourceSet resourceSet = this.resourceSetProvider.get();
+      resourceSet.createResource(clientURI);
+      final IAcceptor<Result> _function = new IAcceptor<Result>() {
+        public void accept(final Result result) {
+          Class<? extends ActiveAnnotationsRuntimeTest> _class = ActiveAnnotationsRuntimeTest.this.getClass();
+          ClassLoader _classLoader = _class.getClassLoader();
+          final Function1<String,Class<? extends Object>> _function = new Function1<String,Class<? extends Object>>() {
+            public Class<? extends Object> apply(final String it) {
+              Class<? extends Object> _compiledClass = result.getCompiledClass(it);
+              return _compiledClass;
+            }
+          };
+          DelegatingClassloader _delegatingClassloader = new DelegatingClassloader(_classLoader, _function);
+          final DelegatingClassloader classLoader = _delegatingClassloader;
+          resourceSet.setClasspathURIContext(classLoader);
+          ActiveAnnotationsRuntimeTest.this.processorProvider.setClassLoader(classLoader);
+        }
+      };
+      this.compiler.compile(macroResourceSet, _function);
+      _xblockexpression = (resourceSet);
+    }
+    return _xblockexpression;
   }
   
   @Test
   public void testSimpleModification() {
     super.testSimpleModification();
+  }
+  
+  @Test
+  public void testBug403563() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("package myannotation");
+    _builder.newLine();
+    _builder.newLine();
+    _builder.append("import org.eclipse.xtend.lib.macro.Active");
+    _builder.newLine();
+    _builder.append("import org.eclipse.xtend.lib.macro.TransformationContext");
+    _builder.newLine();
+    _builder.append("import org.eclipse.xtend.lib.macro.AbstractClassProcessor");
+    _builder.newLine();
+    _builder.append("import org.eclipse.xtend.lib.macro.declaration.MutableClassDeclaration");
+    _builder.newLine();
+    _builder.newLine();
+    _builder.append("@Active(ThrowExceptionAnnotationProcessor)");
+    _builder.newLine();
+    _builder.append("annotation ThrowExceptionAnnotation{ }");
+    _builder.newLine();
+    _builder.append("class ThrowExceptionAnnotationProcessor extends AbstractClassProcessor {");
+    _builder.newLine();
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("override doTransform(MutableClassDeclaration clazz, extension TransformationContext context) {");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("throw new IllegalStateException");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    Pair<String,String> _mappedTo = Pair.<String, String>of("myannotation/ThrowExceptionAnnotationProcessor.xtend", _builder.toString());
+    StringConcatenation _builder_1 = new StringConcatenation();
+    _builder_1.append("package myusercode");
+    _builder_1.newLine();
+    _builder_1.newLine();
+    _builder_1.append("@myannotation.ThrowExceptionAnnotation class MyClass {");
+    _builder_1.newLine();
+    _builder_1.append("}");
+    _builder_1.newLine();
+    Pair<String,String> _mappedTo_1 = Pair.<String, String>of("myusercode/UserCode.xtend", _builder_1.toString());
+    final Procedure1<List<Issue>> _function = new Procedure1<List<Issue>>() {
+      public void apply(final List<Issue> it) {
+        int _size = it.size();
+        Assert.assertEquals(1, _size);
+        final Issue error = IterableExtensions.<Issue>head(it);
+        Assert.assertNotNull(error);
+        String _code = error.getCode();
+        Assert.assertEquals(IssueCodes.PROCESSING_ERROR, _code);
+        String _message = error.getMessage();
+        String _name = IllegalStateException.class.getName();
+        boolean _contains = _message.contains(_name);
+        Assert.assertTrue(_contains);
+        String _message_1 = error.getMessage();
+        String _name_1 = AnnotationProcessor.class.getName();
+        boolean _contains_1 = _message_1.contains(_name_1);
+        Assert.assertFalse(_contains_1);
+      }
+    };
+    this.assertIssues(_mappedTo, _mappedTo_1, _function);
   }
 }
