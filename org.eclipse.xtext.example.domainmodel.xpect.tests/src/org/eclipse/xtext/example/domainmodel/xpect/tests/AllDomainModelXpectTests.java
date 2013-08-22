19ecdbebb210954a3b5859d175f9c1ace2ca26c1
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2013-08-22 14:49:53 +0200

    added test suite to domainmodel example tests Signed-off-by: Moritz Eysholdt <moritz.eysholdt@itemis.de>

---
+++ org.eclipse.xtext.example.domainmodel.xpect.tests/src/org/eclipse/xtext/example/domainmodel/xpect/tests/AllDomainModelXpectTests.java
@@ -0,0 +1,26 @@
+package org.eclipse.xtext.example.domainmodel.xpect.tests;
+
+import org.eclipse.xtext.example.domainmodel.xpect.tests.jvmmodelinferrer.DMJvmModelInferrerTest;
+import org.eclipse.xtext.example.domainmodel.xpect.tests.linking.DMLinkingTest;
+import org.eclipse.xtext.example.domainmodel.xpect.tests.parser.ParserTest;
+import org.eclipse.xtext.example.domainmodel.xpect.tests.resourcedescription.DMResourceDescriptionTest;
+import org.eclipse.xtext.example.domainmodel.xpect.tests.scoping.DMScopingTest;
+import org.eclipse.xtext.example.domainmodel.xpect.tests.testsuite.DMXtextTests;
+import org.eclipse.xtext.example.domainmodel.xpect.tests.validation.DMValidationTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@SuiteClasses({
+/*    */DMJvmModelInferrerTest.class, //
+		DMLinkingTest.class,//
+		ParserTest.class,//
+		DMResourceDescriptionTest.class,//
+		DMScopingTest.class,//
+		DMXtextTests.class,//
+		DMValidationTest.class //
+})
+@RunWith(Suite.class)
+public class AllDomainModelXpectTests {
+
+}
