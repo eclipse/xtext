b1999137b2ea03d5eafcd0cceffe1af9a424d462
Jan Koehnlein <jan.koehnlein@itemis.de>
2013-09-10 16:05:11 +0200

    [tests] first SWTBot tests
    Change-Id: I26f5cfe5ee828d6f914722e9e31d52f1568044fd

---
+++ tests/org.eclipse.xtend.ide.tests/swtbot/xtend-gen/org/eclipse/xtend/ide/tests/WaitForLinkedModeCondition.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2013 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.xtend.ide.tests;
+
+import com.google.common.base.Objects;
+import org.eclipse.swtbot.swt.finder.waits.DefaultCondition;
+import org.eclipse.xtext.ui.refactoring.ui.RenameLinkedMode;
+import org.eclipse.xtext.ui.refactoring.ui.RenameRefactoringController;
+
+@SuppressWarnings("all")
+public class WaitForLinkedModeCondition extends DefaultCondition {
+  private RenameRefactoringController controller;
+  
+  public WaitForLinkedModeCondition(final RenameRefactoringController controller) {
+    this.controller = controller;
+  }
+  
+  public String getFailureMessage() {
+    return "Linked mode not entered";
+  }
+  
+  public boolean test() throws Exception {
+    RenameLinkedMode _activeLinkedMode = this.controller.getActiveLinkedMode();
+    boolean _notEquals = (!Objects.equal(_activeLinkedMode, null));
+    return _notEquals;
+  }
+}
