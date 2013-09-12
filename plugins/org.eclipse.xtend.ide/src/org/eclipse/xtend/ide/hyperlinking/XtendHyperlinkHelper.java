05fa217789f447a971639fa8c2de77dbbaa59c98
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2013-09-12 17:52:35 +0200

    [xbase][validation] Detect ambiguous feature calls
    Method or constructor invocations may be ambiguous
    if there is no preferable candidate for the given
    argument types. This is now properly validated.
    
    Navigation is customized so it's possible to go
    to all ambiguous candidates. Quick fixes are not
    provided (e.g. one could insert type casts to
    disambiguate a feature call).
    
    TODO: Ambiguously imported extensions should treat
    all the contributing extension imports as 'used'.
    Same for local extensions. Otherwise an 'organize
    import' would accidentally *fix* the ambiguity.
    
    see also tickets:
    - https://bugs.eclipse.org/bugs/show_bug.cgi?id=341243
    - https://bugs.eclipse.org/bugs/show_bug.cgi?id=366359
    - https://bugs.eclipse.org/bugs/show_bug.cgi?id=372251
    - https://bugs.eclipse.org/bugs/show_bug.cgi?id=373685
    - https://bugs.eclipse.org/bugs/show_bug.cgi?id=393402
    - https://bugs.eclipse.org/bugs/show_bug.cgi?id=397424
    - https://bugs.eclipse.org/bugs/show_bug.cgi?id=398168
    
    Change-Id: I012ea2057fe5cfa1545c701fb8f3d53fed06a09d

--- plugins/org.eclipse.xtend.ide/src/org/eclipse/xtend/ide/hyperlinking/XtendHyperlinkHelper.java
+++ plugins/org.eclipse.xtend.ide/src/org/eclipse/xtend/ide/hyperlinking/XtendHyperlinkHelper.java
@@ -7,28 +7,55 @@
  *******************************************************************************/
 package org.eclipse.xtend.ide.hyperlinking;
 
+import java.util.List;
 import java.util.Set;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.text.Region;
+import org.eclipse.jface.text.hyperlink.IHyperlink;
+import org.eclipse.xtext.nodemodel.INode;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.hyperlinking.IHyperlinkAcceptor;
+import org.eclipse.xtext.util.TextRegion;
+import org.eclipse.xtext.xbase.XAbstractFeatureCall;
+import org.eclipse.xtext.xbase.XConstructorCall;
 import org.eclipse.xtext.xbase.jvmmodel.IJvmModelAssociations;
+import org.eclipse.xtext.xbase.typesystem.IBatchTypeResolver;
+import org.eclipse.xtext.xbase.typesystem.computation.IAmbiguousLinkingCandidate;
+import org.eclipse.xtext.xbase.typesystem.computation.IConstructorLinkingCandidate;
+import org.eclipse.xtext.xbase.typesystem.computation.IFeatureLinkingCandidate;
+import org.eclipse.xtext.xbase.typesystem.computation.ILinkingCandidate;
 import org.eclipse.xtext.xbase.ui.navigation.TypeLiteralAwareHyperlinkHelper;
 
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 
 /**
- * Redirects hyperlinks to the source elements if the referenced target is
- * an inferred element.
+ * Customized hyperlinker for Xtend source files.
+ * 
+ * First and foremost, it redirects hyperlinks to the source elements 
+ * if the referenced target is an inferred element. That means, all
+ * contributing dispatch methods are available on hyperlink requests.
+ * 
+ * This implementation also provides hyperlinks to all ambiguous candidates if
+ * a feature or constructor call was resolved to multiple equally valid
+ * features.
  * 
  * @author Jan Koehnlein - Initial contribution and API
+ * @author Sebastian Zarnekow - Navigation to all ambiguous candidates.
  */
 public class XtendHyperlinkHelper extends TypeLiteralAwareHyperlinkHelper {
 
 	@Inject
 	private IJvmModelAssociations associations;
 	
+	@Inject
+	private IBatchTypeResolver typeResolver;
+	
+	/**
+	 * This redirects the hyperlinks to all source elements of an inferred element.
+	 */
 	@Override
 	public void createHyperlinksTo(XtextResource from, Region region, EObject to, IHyperlinkAcceptor acceptor) {
 		Set<EObject> sourceElements = associations.getSourceElements(to);
@@ -38,6 +65,64 @@ public class XtendHyperlinkHelper extends TypeLiteralAwareHyperlinkHelper {
 			for(EObject sourceElement: sourceElements) {
 				super.createHyperlinksTo(from, region, sourceElement, acceptor);
 			}
+		}
+	}
+	
+	/**
+	 * If multiple links are requested, all ambiguous candidates are provided for feature
+	 * and constructor calls. 
+	 */
+	@Override
+	public IHyperlink[] createHyperlinksByOffset(XtextResource resource, int offset, boolean createMultipleHyperlinks) {
+		if (!createMultipleHyperlinks) {
+			return super.createHyperlinksByOffset(resource, offset, createMultipleHyperlinks);
+		}
+		List<IHyperlink> links = Lists.newArrayList();
+		IHyperlinkAcceptor acceptor = new HyperlinkAcceptor(links);
+		
+		createMultipleHyperlinksByOffset(resource, offset, acceptor);
+		if (!links.isEmpty())
+			return Iterables.toArray(links, IHyperlink.class);
+		return null;
+	}
+	
+	protected void createMultipleHyperlinksByOffset(XtextResource resource, int offset, IHyperlinkAcceptor acceptor) {
+		INode crossRefNode = getEObjectAtOffsetHelper().getCrossReferenceNode(resource, new TextRegion(offset, 0));
+		if (crossRefNode == null)
+			return;
+		EObject crossLinkedEObject = getEObjectAtOffsetHelper().getCrossReferencedElement(crossRefNode);
+		if (crossLinkedEObject != null && !crossLinkedEObject.eIsProxy()) {
+			EObject containedElementAt = getEObjectAtOffsetHelper().resolveContainedElementAt(resource, offset);
+			if (containedElementAt instanceof XAbstractFeatureCall) {
+				XAbstractFeatureCall casted = (XAbstractFeatureCall) containedElementAt;
+				if (casted.getFeature() == crossLinkedEObject) {
+					IFeatureLinkingCandidate candidate = typeResolver.resolveTypes(casted).getLinkingCandidate(casted);
+					if (candidate instanceof IAmbiguousLinkingCandidate) {
+						createAmbiguousLinks(resource, crossRefNode, ((IAmbiguousLinkingCandidate) candidate).getAlternatives(), acceptor);
+					} else {
+						createHyperlinksTo(resource, crossRefNode, crossLinkedEObject, acceptor);
+					}
+				}
+			} else if (containedElementAt instanceof XConstructorCall) {
+				XConstructorCall casted = (XConstructorCall) containedElementAt;
+				if (casted.getConstructor() == crossLinkedEObject) {
+					IConstructorLinkingCandidate candidate = typeResolver.resolveTypes(casted).getLinkingCandidate(casted);
+					if (candidate instanceof IAmbiguousLinkingCandidate) {
+						createAmbiguousLinks(resource, crossRefNode, ((IAmbiguousLinkingCandidate) candidate).getAlternatives(), acceptor);
+					} else {
+						createHyperlinksTo(resource, crossRefNode, crossLinkedEObject, acceptor);
+					}
+				}
+			} else {
+				createHyperlinksTo(resource, crossRefNode, crossLinkedEObject, acceptor);
+			}
+		}
+	}
+
+	private void createAmbiguousLinks(XtextResource resource, INode crossRefNode,
+			List<? extends ILinkingCandidate> alternatives, IHyperlinkAcceptor acceptor) {
+		for(ILinkingCandidate alternative: alternatives) {
+			createHyperlinksTo(resource, crossRefNode, alternative.getFeature(), acceptor);
 		}
 	}
 }
