6318ad17360c59a1f15fb98c44b6a5b7b74f6907
Dennis Huebner <dennis.huebner@itemis.de>
2013-09-13 15:14:17 +0200

    [releng] xtend builder
    Change-Id: I8081c86e39ec3a3c805b0d780c61aceabc4351d7

---
+++ plugins/org.eclipse.xtext.junit4/src/org/eclipse/xtext/junit4/internal/InternalBuilderTest.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2013 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.junit4.internal;
+
+import static org.junit.Assert.*;
+
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.junit.Test;
+
+/**
+ * @author dhuebner - Initial contribution and API
+ */
+public class InternalBuilderTest {
+
+	@Test
+	public void test() throws CoreException {
+		ResourcesPlugin.getWorkspace().build(IncrementalProjectBuilder.INCREMENTAL_BUILD, null);
+		assertTrue(
+				"Problems found",
+				ResourcesPlugin.getWorkspace().getRoot().findMarkers(IMarker.PROBLEM, true, IResource.DEPTH_INFINITE).length == 0);
+	}
+
+}
