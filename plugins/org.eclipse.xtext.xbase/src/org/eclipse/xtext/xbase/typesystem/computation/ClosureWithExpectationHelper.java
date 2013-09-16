8124ba646df7348ecc97d39eddb5c752a306c627
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2013-09-16 23:59:11 +0200

    [xbase][linking] Improved disambiguation of overloaded methods
    Overloaded methods that are invoked with lambda arguments
    take the declared parameter and the computed return type
    of the lambda into account
    
    see also https://bugs.eclipse.org/bugs/show_bug.cgi?id=403006
    
    Change-Id: I88f2fbde9dfd44a604a6def96b2791540a1853b9

--- plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/typesystem/computation/ClosureWithExpectationHelper.java
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/typesystem/computation/ClosureWithExpectationHelper.java
@@ -11,7 +11,10 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.xtext.common.types.JvmFormalParameter;
 import org.eclipse.xtext.common.types.JvmGenericType;
 import org.eclipse.xtext.common.types.JvmOperation;
@@ -20,6 +23,7 @@ import org.eclipse.xtext.common.types.JvmTypeParameter;
 import org.eclipse.xtext.common.types.JvmTypeParameterDeclarator;
 import org.eclipse.xtext.common.types.JvmTypeReference;
 import org.eclipse.xtext.xbase.XClosure;
+import org.eclipse.xtext.xbase.XExpression;
 import org.eclipse.xtext.xbase.XbasePackage;
 import org.eclipse.xtext.xbase.scoping.batch.IFeatureNames;
 import org.eclipse.xtext.xbase.typesystem.conformance.ConformanceHint;
@@ -55,7 +59,9 @@ public class ClosureWithExpectationHelper extends AbstractClosureTypeHelper {
 
 	private FunctionTypeReference expectedClosureType;
 	private FunctionTypeReference resultClosureType;
-	
+
+	private boolean validParameterTypes = true;
+
 	protected ClosureWithExpectationHelper(XClosure closure, JvmOperation operation, ITypeExpectation expectation, ITypeComputationState state) {
 		super(closure, expectation, state);
 		this.operation = operation;
@@ -64,24 +70,24 @@ public class ClosureWithExpectationHelper extends AbstractClosureTypeHelper {
 		}
 		prepareComputation();
 	}
-	
+
 	@Override
 	public JvmOperation getOperation() {
 		return operation;
 	}
-	
+
 	@Override
 	public FunctionTypeReference getExpectedClosureType() {
 		return expectedClosureType;
 	}
-	
+
 	@Override
 	protected void computeTypes() {
 		prepareResultType();
 		if (resultClosureType == null) {
-			throw new IllegalStateException("Cannot locate appropriate operation for " + getClosure()); 
+			throw new IllegalStateException("Cannot locate appropriate operation for " + getClosure());
 		}
-		
+
 		LightweightTypeReference expectedReturnType = expectedClosureType.getReturnType();
 		if (expectedReturnType == null) {
 			throw new IllegalStateException("expected return type may not be null");
@@ -89,27 +95,47 @@ public class ClosureWithExpectationHelper extends AbstractClosureTypeHelper {
 		ITypeAssigner typeAssigner = getState().withRootExpectation(expectedReturnType).assignTypes();
 		ITypeComputationState closureBodyTypeComputationState = getClosureBodyTypeComputationState(typeAssigner);
 		ITypeComputationResult expressionResult = closureBodyTypeComputationState.computeTypes(getClosure().getExpression());
-		
-		boolean incompatible = processExpressionType(expressionResult);
-		
+
+		ConformanceHint hint = processExpressionType(expressionResult);
+
 		if (resultClosureType.getReturnType() == null)
 			throw new IllegalStateException("Closure has no return type assigned");
-		// TODO the hint is probably wrong - if we expect a native function type, demand conversion is not true
-		if (expectedClosureType.isFunctionType()) {
-			if (incompatible) {
-				getExpectation().acceptActualType(resultClosureType, ConformanceHint.CHECKED, ConformanceHint.INCOMPATIBLE, ConformanceHint.PROPAGATED_TYPE);
-			} else {
-				getExpectation().acceptActualType(resultClosureType, ConformanceHint.UNCHECKED);
-			}
+		if (!validParameterTypes || hint == ConformanceHint.INCOMPATIBLE) {
+			if (hint == null)
+				markIncompatibleParameterList();
+			else if (validParameterTypes)
+				markCompatibleParameterList();
+			else
+				markIncompatible();
+		} else if (hint == ConformanceHint.LAMBDA_RAW_COMPATIBLE) {
+			markRawCompatible();
 		} else {
-			if (incompatible) {
-				getExpectation().acceptActualType(resultClosureType, ConformanceHint.CHECKED, ConformanceHint.INCOMPATIBLE, ConformanceHint.PROPAGATED_TYPE);
-			} else {
-				getExpectation().acceptActualType(resultClosureType, ConformanceHint.UNCHECKED);
-			}
+			markUncheckedValid();
 		}
 	}
+
+	protected void markUncheckedValid() {
+		getExpectation().acceptActualType(resultClosureType, ConformanceHint.UNCHECKED);
+	}
+
+	protected void markIncompatibleParameterList() {
+		getExpectation().acceptActualType(resultClosureType, ConformanceHint.CHECKED, ConformanceHint.INCOMPATIBLE, ConformanceHint.SEALED);
+	}
 	
+	protected void markCompatibleParameterList() {
+		getExpectation().acceptActualType(resultClosureType, ConformanceHint.CHECKED, ConformanceHint.LAMBDA_PARAMETER_COMPATIBLE, ConformanceHint.SEALED);
+	}
+
+	protected void markIncompatible() {
+		getExpectation().acceptActualType(resultClosureType, ConformanceHint.CHECKED, ConformanceHint.INCOMPATIBLE, ConformanceHint.PROPAGATED_TYPE,
+				ConformanceHint.SEALED);
+	}
+
+	protected void markRawCompatible() {
+		getExpectation().acceptActualType(resultClosureType, ConformanceHint.CHECKED, ConformanceHint.LAMBDA_RAW_COMPATIBLE, ConformanceHint.PROPAGATED_TYPE,
+				ConformanceHint.SEALED);
+	}
+
 	/**
 	 * @noreference This method is not intended to be referenced by clients.
 	 */
@@ -125,19 +151,20 @@ public class ClosureWithExpectationHelper extends AbstractClosureTypeHelper {
 		expectedClosureType = initKnownClosureType(type, operation);
 		deferredBindTypeArgument(expectedType, expectedClosureType, BoundTypeArgumentSource.INFERRED_CONSTRAINT);
 	}
-	
+
 	protected void prepareResultType() {
 		resultClosureType = new FunctionTypeReference(expectedClosureType.getOwner(), expectedClosureType.getType());
-		for(LightweightTypeReference argument: expectedClosureType.getTypeArguments()) {
+		for (LightweightTypeReference argument : expectedClosureType.getTypeArguments()) {
 			resultClosureType.addTypeArgument(argument);
 		}
 	}
-	
+
 	protected FunctionTypeReference initKnownClosureType(JvmType type, JvmOperation operation) {
 		ITypeReferenceOwner owner = getExpectation().getReferenceOwner();
 		FunctionTypeReference result = new FunctionTypeReference(owner, type);
 		OwnedConverter converter = new OwnedConverter(owner);
-		TypeParameterByUnboundSubstitutor substitutor = new TypeParameterByUnboundSubstitutor(Collections.<JvmTypeParameter, LightweightMergedBoundTypeArgument>emptyMap(), owner) {
+		TypeParameterByUnboundSubstitutor substitutor = new TypeParameterByUnboundSubstitutor(
+				Collections.<JvmTypeParameter, LightweightMergedBoundTypeArgument> emptyMap(), owner) {
 			@Override
 			protected UnboundTypeReference createUnboundTypeReference(JvmTypeParameter type) {
 				UnboundTypeReference result = getExpectation().createUnboundTypeReference(getClosure(), type);
@@ -146,7 +173,7 @@ public class ClosureWithExpectationHelper extends AbstractClosureTypeHelper {
 		};
 		if (type instanceof JvmTypeParameterDeclarator) {
 			List<JvmTypeParameter> typeParameters = ((JvmTypeParameterDeclarator) type).getTypeParameters();
-			for(JvmTypeParameter typeParameter: typeParameters) {
+			for (JvmTypeParameter typeParameter : typeParameters) {
 				ParameterizedTypeReference parameterReference = new ParameterizedTypeReference(owner, typeParameter);
 				LightweightTypeReference substituted = substitutor.substitute(parameterReference);
 				result.addTypeArgument(substituted);
@@ -155,7 +182,7 @@ public class ClosureWithExpectationHelper extends AbstractClosureTypeHelper {
 			substitutor.enhanceMapping(definedMapping);
 		}
 		LightweightTypeReference declaredReturnType = converter.toLightweightReference(operation.getReturnType());
-		for(JvmFormalParameter parameter: operation.getParameters()) {
+		for (JvmFormalParameter parameter : operation.getParameters()) {
 			LightweightTypeReference lightweight = converter.toLightweightReference(parameter.getParameterType());
 			LightweightTypeReference lowerBound = lightweight.getLowerBoundSubstitute();
 			LightweightTypeReference substituted = substitutor.substitute(lowerBound);
@@ -170,11 +197,13 @@ public class ClosureWithExpectationHelper extends AbstractClosureTypeHelper {
 	protected ITypeComputationState getClosureBodyTypeComputationState(ITypeAssigner typeAssigner) {
 		List<LightweightTypeReference> operationParameterTypes = expectedClosureType.getParameterTypes();
 		List<JvmFormalParameter> closureParameters = getClosure().getFormalParameters();
-		
+
+		validParameterTypes = closureParameters.size() == operationParameterTypes.size();
+
 		// just in case we have more than 6 closure parameters
 		int paramCount = Math.min(closureParameters.size(), operationParameterTypes.size());
 		// TODO validate parameter count - check against operation if available
-		for(int i = 0; i < paramCount; i++) {
+		for (int i = 0; i < paramCount; i++) {
 			JvmFormalParameter closureParameter = closureParameters.get(i);
 			final LightweightTypeReference operationParameterType = operationParameterTypes.get(i);
 			if (closureParameter.eContainingFeature() != XbasePackage.Literals.XCLOSURE__IMPLICIT_PARAMETER && closureParameter.getParameterType() != null) {
@@ -188,19 +217,24 @@ public class ClosureWithExpectationHelper extends AbstractClosureTypeHelper {
 				}.processPairedReferences(operationParameterType, closureParameterType);
 				typeAssigner.assignType(closureParameter, closureParameterType);
 				resultClosureType.addParameterType(closureParameterType);
+				if (validParameterTypes && !operationParameterType.isAssignableFrom(closureParameterType)) {
+					validParameterTypes = false;
+				}
 			} else {
-				LightweightTypeReference partiallyResolved = new TypeParameterSubstitutor<Object>(Collections.<JvmTypeParameter, LightweightMergedBoundTypeArgument>emptyMap(), typeAssigner.getReferenceOwner()) {
+				LightweightTypeReference partiallyResolved = new TypeParameterSubstitutor<Object>(
+						Collections.<JvmTypeParameter, LightweightMergedBoundTypeArgument> emptyMap(), typeAssigner.getReferenceOwner()) {
 					@Override
 					protected Object createVisiting() {
 						return new Object();
 					}
+
 					@Override
 					protected LightweightTypeReference doVisitUnboundTypeReference(UnboundTypeReference reference, Object param) {
 						if (reference.internalIsResolved()) {
 							return super.doVisitUnboundTypeReference(reference, param);
 						} else {
 							List<LightweightBoundTypeArgument> hints = reference.getAllHints();
-							for(LightweightBoundTypeArgument hint: hints) {
+							for (LightweightBoundTypeArgument hint : hints) {
 								if (hint.getSource() == BoundTypeArgumentSource.INFERRED) {
 									reference.tryResolve();
 									if (reference.internalIsResolved()) {
@@ -211,6 +245,7 @@ public class ClosureWithExpectationHelper extends AbstractClosureTypeHelper {
 						}
 						return reference;
 					}
+
 					@Override
 					public LightweightTypeReference substitute(LightweightTypeReference original) {
 						return original.accept(this, createVisiting());
@@ -220,7 +255,7 @@ public class ClosureWithExpectationHelper extends AbstractClosureTypeHelper {
 				resultClosureType.addParameterType(partiallyResolved);
 			}
 		}
-		for(int i = paramCount; i < closureParameters.size(); i++) {
+		for (int i = paramCount; i < closureParameters.size(); i++) {
 			JvmFormalParameter closureParameter = closureParameters.get(i);
 			JvmTypeReference parameterType = closureParameter.getParameterType();
 			if (parameterType != null) {
@@ -228,7 +263,8 @@ public class ClosureWithExpectationHelper extends AbstractClosureTypeHelper {
 				typeAssigner.assignType(closureParameter, lightweight);
 				resultClosureType.addParameterType(lightweight);
 			} else {
-				LightweightTypeReference objectType = typeAssigner.toLightweightTypeReference(getServices().getTypeReferences().getTypeForName(Object.class, closureParameter));
+				LightweightTypeReference objectType = typeAssigner.toLightweightTypeReference(getServices().getTypeReferences().getTypeForName(Object.class,
+						closureParameter));
 				typeAssigner.assignType(closureParameter, objectType);
 				resultClosureType.addParameterType(objectType);
 			}
@@ -245,8 +281,8 @@ public class ClosureWithExpectationHelper extends AbstractClosureTypeHelper {
 		List<JvmTypeReference> exceptions = operation.getExceptions();
 		if (exceptions.isEmpty())
 			return result;
-		List<LightweightTypeReference> expectedExceptions = Lists.newArrayListWithCapacity(exceptions.size()); 
-		for(JvmTypeReference exception: exceptions) {
+		List<LightweightTypeReference> expectedExceptions = Lists.newArrayListWithCapacity(exceptions.size());
+		for (JvmTypeReference exception : exceptions) {
 			expectedExceptions.add(typeAssigner.toLightweightTypeReference(exception));
 		}
 		return result.withExpectedExceptions(expectedExceptions);
@@ -255,7 +291,8 @@ public class ClosureWithExpectationHelper extends AbstractClosureTypeHelper {
 	/**
 	 * Returns <code>true</code> if the expression result is definitely incompatible to the expected type.
 	 */
-	protected boolean processExpressionType(ITypeComputationResult expressionResult) {
+	@Nullable
+	protected ConformanceHint processExpressionType(ITypeComputationResult expressionResult) {
 		LightweightTypeReference expressionResultType = expressionResult.getReturnType();
 		if (expressionResultType == null || expressionResultType instanceof AnyTypeReference) {
 			LightweightTypeReference returnType = expectedClosureType.getReturnType();
@@ -266,15 +303,48 @@ public class ClosureWithExpectationHelper extends AbstractClosureTypeHelper {
 			LightweightTypeReference expectedReturnType = expectedClosureType.getReturnType();
 			if (expectedReturnType == null)
 				throw new IllegalStateException("expected return type may not be null");
-			if (!expressionResultType.isPrimitiveVoid())
-				deferredBindTypeArgument(expectedReturnType, expressionResultType, BoundTypeArgumentSource.INFERRED);
+			if (!expressionResultType.isPrimitiveVoid()) {
+				if (expectedReturnType.isPrimitiveVoid()) {
+					resultClosureType.setReturnType(expectedReturnType);
+					if (isImplicitReturn(expressionResult.getExpression())) {
+						return null;
+					} else {
+						return ConformanceHint.INCOMPATIBLE;
+					}
+				} else {
+					deferredBindTypeArgument(expectedReturnType, expressionResultType, BoundTypeArgumentSource.INFERRED);
+				}
+			}
 			if (expectedReturnType.isAssignableFrom(expressionResultType)) {
 				resultClosureType.setReturnType(expressionResultType);
+			} else if (expectedReturnType.isPrimitiveVoid()) {
+				resultClosureType.setReturnType(expectedReturnType);
+				return ConformanceHint.INCOMPATIBLE;
 			} else {
 				resultClosureType.setReturnType(expectedReturnType);
-				return true;
+				return ConformanceHint.LAMBDA_RAW_COMPATIBLE;
 			}
 		}
-		return false;
+		return null;
+	}
+
+	protected boolean isImplicitReturn(@Nullable XExpression expression) {
+		if (expression == null) {
+			return true;
+		}
+		if (expression.eClass() == XbasePackage.Literals.XRETURN_EXPRESSION) {
+			return false;
+		}
+		TreeIterator<EObject> contents = expression.eAllContents();
+		while (contents.hasNext()) {
+			EObject next = contents.next();
+			if (next.eClass() == XbasePackage.Literals.XRETURN_EXPRESSION) {
+				return false;
+			}
+			if (next.eClass() == XbasePackage.Literals.XCLOSURE) {
+				contents.prune();
+			}
+		}
+		return true;
 	}
 }
