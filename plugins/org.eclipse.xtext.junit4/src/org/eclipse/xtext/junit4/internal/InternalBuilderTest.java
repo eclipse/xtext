2850360774f2f147f02b15a995def9a6a8dfb941
Dennis Huebner <dennis.huebner@itemis.de>
2013-09-16 16:46:43 +0200

    [tests] Builder run - better problems report
    Change-Id: I7c4b68d4b11b1abfe8b90caa93c483797373a528

--- plugins/org.eclipse.xtext.junit4/src/org/eclipse/xtext/junit4/internal/InternalBuilderTest.java
+++ plugins/org.eclipse.xtext.junit4/src/org/eclipse/xtext/junit4/internal/InternalBuilderTest.java
@@ -9,12 +9,15 @@ package org.eclipse.xtext.junit4.internal;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.ui.texteditor.MarkerUtilities;
+import static org.eclipse.xtext.xbase.lib.IterableExtensions.*;
 import org.junit.Test;
 
 /**
@@ -24,13 +27,24 @@ public class InternalBuilderTest {
 
 	@Test
 	public void test() throws CoreException {
-		System.out.println("Build started");
-		ResourcesPlugin.getWorkspace().build(
-				IncrementalProjectBuilder.FULL_BUILD,  new NullProgressMonitor());
-		System.out.println("Build done");
-		assertTrue(
-				"Problems found",
-				ResourcesPlugin.getWorkspace().getRoot().findMarkers(IMarker.PROBLEM, true, IResource.DEPTH_INFINITE).length == 0);
+		final IMarker[] markers = ResourcesPlugin.getWorkspace().getRoot()
+				.findMarkers(IMarker.PROBLEM, true, IResource.DEPTH_INFINITE);
+		List<String> errors = new ArrayList<String>();
+		for (IMarker marker : markers) {
+			String msg = MarkerUtilities.getMessage(marker);
+			if (MarkerUtilities.getSeverity(marker) == IMarker.SEVERITY_ERROR) {
+				errors.add(msg);
+			}
+		}
+
+		List<String> top10;
+		if (errors.size() > 10) {
+			top10 = toList(take(errors, 10));
+		} else {
+			top10 = errors;
+		}
+		assertTrue("Problems found (" + top10.size() + " from " + errors.size() + "): " + join(errors, ", "),
+				errors.isEmpty());
 	}
 
 }
