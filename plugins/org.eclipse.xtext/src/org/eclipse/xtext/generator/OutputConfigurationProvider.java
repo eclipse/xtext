256b013e2cd00d0a51350f9aa18e118b6c72dfe8
Dennis Huebner <dennis.huebner@itemis.de>
2013-09-19 09:59:52 +0200

    [generator] allow overwriting default output configuration
    Change-Id: I3cde672e59fd8db286ebb97d2349bf11a32b8c83

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/generator/OutputConfigurationProvider.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/generator/OutputConfigurationProvider.java
@@ -11,23 +11,38 @@ import static com.google.common.collect.Sets.*;
 
 import java.util.Set;
 
+import com.google.inject.Singleton;
+
 /**
  * @author Sven Efftinge - Initial contribution and API
  * @since 2.1
  */
+@Singleton
 public class OutputConfigurationProvider implements IOutputConfigurationProvider {
+	private Set<OutputConfiguration> outputConfigurations;
 
 	/**
 	 * @return a set of {@link OutputConfiguration} available for the generator
 	 */
 	public Set<OutputConfiguration> getOutputConfigurations() {
-		OutputConfiguration defaultOutput = new OutputConfiguration(IFileSystemAccess.DEFAULT_OUTPUT);
-		defaultOutput.setDescription("Output Folder");
-		defaultOutput.setOutputDirectory("./src-gen");
-		defaultOutput.setOverrideExistingResources(true);
-		defaultOutput.setCreateOutputDirectory(true);
-		defaultOutput.setCleanUpDerivedResources(true);
-		defaultOutput.setSetDerivedProperty(true);
-		return newHashSet(defaultOutput);
+		if (outputConfigurations == null) {
+			outputConfigurations = newHashSet();
+			OutputConfiguration defaultOutput = new OutputConfiguration(IFileSystemAccess.DEFAULT_OUTPUT);
+			defaultOutput.setDescription("Output Folder");
+			defaultOutput.setOutputDirectory("./src-gen");
+			defaultOutput.setOverrideExistingResources(true);
+			defaultOutput.setCreateOutputDirectory(true);
+			defaultOutput.setCleanUpDerivedResources(true);
+			defaultOutput.setSetDerivedProperty(true);
+			outputConfigurations.add(defaultOutput);
+		}
+		return outputConfigurations;
+	}
+
+	/**
+	 * @since 2.5
+	 */
+	public void setOutputConfigurations(Set<OutputConfiguration> outputConfigurations) {
+		this.outputConfigurations = outputConfigurations;
 	}
 }
