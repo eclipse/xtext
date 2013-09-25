2d9e7a9ad81abb955820d9ad0933257f7cab75b6
Dennis Huebner <dennis.huebner@itemis.de>
2013-09-25 17:57:44 +0200

    [mvn-xtext] xbase IT test
    Change-Id: I3e3e2135a624f414a7cd964288f05aa6d4ad243b

--- plugins/org.eclipse.xtext.maven.plugin/src/test/java/org/eclipse/xtext/maven/plugin/XtextGeneratorIT.java
+++ plugins/org.eclipse.xtext.maven.plugin/src/test/java/org/eclipse/xtext/maven/plugin/XtextGeneratorIT.java
@@ -6,11 +6,18 @@ import java.io.IOException;
 import org.apache.maven.it.VerificationException;
 import org.apache.maven.it.Verifier;
 import org.apache.maven.it.util.ResourceExtractor;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class XtextGeneratorIT {
 
 	private static String ROOT = "/it/generate";
+
+	@BeforeClass
+	static public void setUpOnce() throws IOException, VerificationException {
+		new XtextGeneratorIT().verifyErrorFreeLog(ROOT, "clean", "install");
+	}
 
 	@Test
 	public void simpleLang() throws Exception {
@@ -18,14 +25,23 @@ public class XtextGeneratorIT {
 		verifier.assertFileMatches(verifier.getBasedir() + "/src-gen/greetings.txt", "People to greet\\: Test");
 	}
 
-	private Verifier verifyErrorFreeLog(String pathToTestProject) throws IOException, VerificationException {
-		return verifyErrorFreeLog(pathToTestProject, "verify");
+	@Test
+	public void purexbase() throws Exception {
+		Verifier verifier = verifyErrorFreeLog(ROOT + "/purexbase");
+		verifier.assertFilePresent(verifier.getBasedir() + "/src-gen/IntegrationTestXbase.java");
+		verifier.assertFilePresent(verifier.getBasedir() + "/target/xtext-temp/classes/IntegrationTestXbase.class");
 	}
 
-	private Verifier verifyErrorFreeLog(String pathToTestProject, String goal) throws IOException,
+	private Verifier verifyErrorFreeLog(String pathToTestProject) throws IOException, VerificationException {
+		return verifyErrorFreeLog(pathToTestProject, "clean", "verify");
+	}
+
+	private Verifier verifyErrorFreeLog(String pathToTestProject, String... goals) throws IOException,
 			VerificationException {
 		Verifier verifier = newVerifier(pathToTestProject);
-		verifier.executeGoal(goal);
+		for (String goal : goals) {
+			verifier.executeGoal(goal);
+		}
 		verifier.verifyErrorFreeLog();
 		verifier.resetStreams();
 		return verifier;
@@ -35,4 +51,10 @@ public class XtextGeneratorIT {
 		File testDir = ResourceExtractor.simpleExtractResources(getClass(), pathToTestProject);
 		return new Verifier(testDir.getAbsolutePath());
 	}
+
+	@AfterClass
+	static public void tearDownOnce() throws IOException, VerificationException {
+		new XtextGeneratorIT().verifyErrorFreeLog(ROOT, "clean");
+	}
+
 }
