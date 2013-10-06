12483abdb23b38b09acdb66fe4a993c90def141d
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2013-10-06 21:47:24 +0200

    [xbase][typesystem] Improved type inference for type parameters
    Scenarios where method type parameters are constrained
    with type parameters of the declaring type, work much
    better now.
    
    see https://bugs.eclipse.org/bugs/show_bug.cgi?id=413138
    and https://bugs.eclipse.org/bugs/show_bug.cgi?id=417522
    
    Change-Id: I192eaf086c7cc1090ab6832dae2d6941f37bcc6a

---
+++ tests/org.eclipse.xtend.core.tests/xtend-gen/org/eclipse/xtend/core/tests/compiler/CompilerBug413138Test.java
@@ -0,0 +1,300 @@
+/**
+ * Copyright (c) 2013 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.xtend.core.tests.compiler;
+
+import org.eclipse.xtend.core.tests.compiler.AbstractXtendCompilerTest;
+import org.eclipse.xtend2.lib.StringConcatenation;
+import org.junit.Test;
+
+/**
+ * @author Sebastian Zarnekow - Initial contribution and API
+ */
+@SuppressWarnings("all")
+public class CompilerBug413138Test extends AbstractXtendCompilerTest {
+  @Test
+  public void test_01() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("class C {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("StringRepository repository");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("def m()\t{");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("var string = repository.findByStringId(1L)");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("repository.save(string)");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("public interface StringRepository extends GraphRepository<String> {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("def String findByStringId(Long id);");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("interface GraphRepository<T> {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("def <U extends T> U save(U entity);");
+    _builder.newLine();
+    _builder.append("    ");
+    _builder.append("def <U extends T> Iterable<U> save(Iterable<U> entities);");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    StringConcatenation _builder_1 = new StringConcatenation();
+    _builder_1.append("@SuppressWarnings(\"all\")");
+    _builder_1.newLine();
+    _builder_1.append("public class C {");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("private StringRepository repository;");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("public String m() {");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("String _xblockexpression = null;");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("{");
+    _builder_1.newLine();
+    _builder_1.append("      ");
+    _builder_1.append("String string = this.repository.findByStringId(Long.valueOf(1L));");
+    _builder_1.newLine();
+    _builder_1.append("      ");
+    _builder_1.append("String _save = this.repository.<String>save(string);");
+    _builder_1.newLine();
+    _builder_1.append("      ");
+    _builder_1.append("_xblockexpression = (_save);");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("}");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("return _xblockexpression;");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("}");
+    _builder_1.newLine();
+    _builder_1.append("}");
+    _builder_1.newLine();
+    this.assertCompilesTo(_builder, _builder_1);
+  }
+  
+  @Test
+  public void test_02() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("class C {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("StringRepository repository");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("def m()\t{");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("repository.save(#[])");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("public interface StringRepository extends GraphRepository<String> {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("def String findByStringId(Long id);");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("interface GraphRepository<T> {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("def <U extends T> U save(U entity);");
+    _builder.newLine();
+    _builder.append("    ");
+    _builder.append("def <U extends T> Iterable<U> save(Iterable<U> entities);");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    StringConcatenation _builder_1 = new StringConcatenation();
+    _builder_1.append("import com.google.common.collect.Lists;");
+    _builder_1.newLine();
+    _builder_1.append("import java.util.Collections;");
+    _builder_1.newLine();
+    _builder_1.newLine();
+    _builder_1.append("@SuppressWarnings(\"all\")");
+    _builder_1.newLine();
+    _builder_1.append("public class C {");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("private StringRepository repository;");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("public Iterable<String> m() {");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("Iterable<String> _save = this.repository.<String>save(Collections.<String>unmodifiableList(Lists.<String>newArrayList()));");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("return _save;");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("}");
+    _builder_1.newLine();
+    _builder_1.append("}");
+    _builder_1.newLine();
+    this.assertCompilesTo(_builder, _builder_1);
+  }
+  
+  @Test
+  public void test_03() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("class C {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("CharSeqRepository repository");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("def m()\t{");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("repository.<String>save(newArrayList)");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("public interface CharSeqRepository extends GraphRepository<CharSequence> {");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("interface GraphRepository<T> {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("def <U extends T> U save(U entity);");
+    _builder.newLine();
+    _builder.append("    ");
+    _builder.append("def <U extends T> Iterable<U> save(Iterable<U> entities);");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    StringConcatenation _builder_1 = new StringConcatenation();
+    _builder_1.append("import java.util.ArrayList;");
+    _builder_1.newLine();
+    _builder_1.append("import org.eclipse.xtext.xbase.lib.CollectionLiterals;");
+    _builder_1.newLine();
+    _builder_1.newLine();
+    _builder_1.append("@SuppressWarnings(\"all\")");
+    _builder_1.newLine();
+    _builder_1.append("public class C {");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("private CharSeqRepository repository;");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("public Iterable<String> m() {");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("ArrayList<String> _newArrayList = CollectionLiterals.<String>newArrayList();");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("Iterable<String> _save = this.repository.<String>save(_newArrayList);");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("return _save;");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("}");
+    _builder_1.newLine();
+    _builder_1.append("}");
+    _builder_1.newLine();
+    this.assertCompilesTo(_builder, _builder_1);
+  }
+  
+  @Test
+  public void test_04() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("class C {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("CharSeqRepository repository");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("def m()\t{");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("repository.save(#[\'a\'])");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("public interface CharSeqRepository extends GraphRepository<CharSequence> {");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("interface GraphRepository<T> {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("def <U extends T> U save(U entity);");
+    _builder.newLine();
+    _builder.append("    ");
+    _builder.append("def <U extends T> Iterable<U> save(Iterable<U> entities);");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    StringConcatenation _builder_1 = new StringConcatenation();
+    _builder_1.append("import com.google.common.collect.Lists;");
+    _builder_1.newLine();
+    _builder_1.append("import java.util.Collections;");
+    _builder_1.newLine();
+    _builder_1.newLine();
+    _builder_1.append("@SuppressWarnings(\"all\")");
+    _builder_1.newLine();
+    _builder_1.append("public class C {");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("private CharSeqRepository repository;");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("public Iterable<String> m() {");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("Iterable<String> _save = this.repository.<String>save(Collections.<String>unmodifiableList(Lists.<String>newArrayList(\"a\")));");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("return _save;");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("}");
+    _builder_1.newLine();
+    _builder_1.append("}");
+    _builder_1.newLine();
+    this.assertCompilesTo(_builder, _builder_1);
+  }
+}
