fe05131cc9ccb2a56a1257f8e209efeb95e0d0bf
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2013-10-18 00:36:15 +0200

    [xbase][typesystem] Improved type parameter resolution
    Situations where upper and lower bound expectation / hints
    are present on the instance level are resolved properly:
    
    class C {
      def method(MyIterable<? extends Number> list) {
        list.m[ process(it) ]
      }
      def void process(Number value) {}
    }
    interface MyIterable<T> extends Iterable<T> {
      def void m(Consumer<? super T> c)
    }
    interface Consumer<T> {
      def void consume(T t)
    }
    
    Change-Id: Ic5e945fc1c6898c2ad27203d33dbeea44f905c56

--- tests/org.eclipse.xtend.core.tests/suites/org/eclipse/xtend/core/tests/compiler/CompilerSuite.java
+++ tests/org.eclipse.xtend.core.tests/suites/org/eclipse/xtend/core/tests/compiler/CompilerSuite.java
@@ -38,6 +38,7 @@ import org.junit.runners.Suite.SuiteClasses;
 	ConfiguredCompilerTest.class,
 	ExtensionsCompilerTest.class,
 	DataCompilerTest.class,
+	OverloadedExtensionCompilerTest.class,
 	ParameterizedTypeCompilerTest.class,
 	PropertyCompilerTest.class,
 	XtendCompilerTest.class,
