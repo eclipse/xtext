44145db7241b11f25145f1319a94c7e194854962
Sven Efftinge <sven.efftinge@itemis.de>
2013-10-25 13:54:56 +0200

    [Xtend] organize imports bug (see https://bugs.eclipse.org/bugs/show_bug.cgi?id=416439)
    Change-Id: Ia4f117e53ad9e3e6191dafffb3516add5f6a83c6

---
+++ plugins/org.eclipse.xtend.core/xtend-gen/org/eclipse/xtend/core/documentation/XtendDocumentationProvider.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2013 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.xtend.core.documentation;
+
+import com.google.common.base.Objects;
+import java.util.List;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.xtend.core.xtend.XtendPackage.Literals;
+import org.eclipse.xtend.core.xtend.XtendTypeDeclaration;
+import org.eclipse.xtext.documentation.impl.MultiLineCommentDocumentationProvider;
+import org.eclipse.xtext.nodemodel.INode;
+import org.eclipse.xtext.xbase.lib.CollectionLiterals;
+
+/**
+ * @author Sven Efftinge - Initial contribution and API
+ */
+@SuppressWarnings("all")
+public class XtendDocumentationProvider extends MultiLineCommentDocumentationProvider {
+  public String getDocumentation(final EObject o) {
+    String _xblockexpression = null;
+    {
+      boolean _shouldBeHandeled = this.shouldBeHandeled(o);
+      boolean _not = (!_shouldBeHandeled);
+      if (_not) {
+        return null;
+      }
+      String _documentation = super.getDocumentation(o);
+      _xblockexpression = (_documentation);
+    }
+    return _xblockexpression;
+  }
+  
+  public List<INode> getDocumentationNodes(final EObject o) {
+    List<INode> _xblockexpression = null;
+    {
+      boolean _shouldBeHandeled = this.shouldBeHandeled(o);
+      boolean _not = (!_shouldBeHandeled);
+      if (_not) {
+        return CollectionLiterals.<INode>emptyList();
+      }
+      List<INode> _documentationNodes = super.getDocumentationNodes(o);
+      _xblockexpression = (_documentationNodes);
+    }
+    return _xblockexpression;
+  }
+  
+  /**
+   * The Xtend parser constructs a synthetic nested XtendClass to hold annotations which should be ignored as a documentation provider
+   */
+  public boolean shouldBeHandeled(final EObject o) {
+    boolean _and = false;
+    if (!(o instanceof XtendTypeDeclaration)) {
+      _and = false;
+    } else {
+      EStructuralFeature _eContainingFeature = o.eContainingFeature();
+      boolean _equals = Objects.equal(_eContainingFeature, Literals.XTEND_MEMBER__ANNOTATION_INFO);
+      _and = ((o instanceof XtendTypeDeclaration) && _equals);
+    }
+    boolean _not = (!_and);
+    return _not;
+  }
+}
