25ebd5cee516223abd7a3d85e0a5e833d25d3c16
Jan Koehnlein <jan.koehnlein@itemis.de>
2013-11-06 11:29:42 +0100

    [xtend][quickfix] quick fix for missing constructor parentheses and missing types in other package https://bugs.eclipse.org/bugs/show_bug.cgi?id=402918
    Change-Id: I204bb2bd91abd359f63a467a85829feffc8b4f13

--- plugins/org.eclipse.xtend.ide/src/org/eclipse/xtend/ide/quickfix/CreateXtendTypeQuickfixes.java
+++ plugins/org.eclipse.xtend.ide/src/org/eclipse/xtend/ide/quickfix/CreateXtendTypeQuickfixes.java
@@ -8,6 +8,7 @@
 package org.eclipse.xtend.ide.quickfix;
 
 import static org.eclipse.xtext.ui.util.DisplayRunHelper.*;
+import static org.eclipse.xtext.util.Strings.*;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
@@ -17,6 +18,7 @@ import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.jdt.internal.ui.wizards.NewElementWizard;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.xtend.core.jvmmodel.IXtendJvmAssociations;
+import org.eclipse.xtend.core.xtend.XtendFile;
 import org.eclipse.xtend.core.xtend.XtendPackage;
 import org.eclipse.xtend.core.xtend.XtendTypeDeclaration;
 import org.eclipse.xtend.ide.codebuilder.AbstractAnnotationBuilder;
@@ -33,16 +35,22 @@ import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.common.types.JvmDeclaredType;
 import org.eclipse.xtext.common.types.JvmVisibility;
 import org.eclipse.xtext.common.types.TypesPackage;
+import org.eclipse.xtext.naming.IQualifiedNameConverter;
+import org.eclipse.xtext.naming.QualifiedName;
 import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.scoping.IScope;
+import org.eclipse.xtext.scoping.IScopeProvider;
 import org.eclipse.xtext.ui.editor.model.IXtextDocument;
 import org.eclipse.xtext.ui.editor.model.edit.IModification;
 import org.eclipse.xtext.ui.editor.model.edit.IModificationContext;
 import org.eclipse.xtext.ui.editor.quickfix.IssueResolutionAcceptor;
+import org.eclipse.xtext.util.Pair;
 import org.eclipse.xtext.validation.Issue;
 import org.eclipse.xtext.xbase.XConstructorCall;
 import org.eclipse.xtext.xbase.XbasePackage;
 import org.eclipse.xtext.xbase.annotations.xAnnotations.XAnnotationsPackage;
 import org.eclipse.xtext.xbase.ui.quickfix.CreateJavaTypeQuickfixes;
+import org.eclipse.xtext.xbase.ui.quickfix.TypeNameGuesser;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
@@ -73,45 +81,82 @@ public class CreateXtendTypeQuickfixes extends CreateJavaTypeQuickfixes {
 	@Inject
 	private CodeBuilderQuickfix codeBuilderQuickfix;
 
+	@Inject
+	private TypeNameGuesser typeNameGuesser;
+	
+	@Inject
+	private IScopeProvider scopeProvider;
+	
+	@Inject
+	private IQualifiedNameConverter converter;
+	
 	@Override
 	public void addQuickfixes(Issue issue, IssueResolutionAcceptor issueResolutionAcceptor,
 			IXtextDocument xtextDocument, XtextResource resource, 
 			EObject referenceOwner, EReference unresolvedReference)
 			throws Exception {
-		String typeName = xtextDocument.get(issue.getOffset(), issue.getLength());
+		String typeString = (issue.getData() != null && issue.getData().length > 0) 
+				? issue.getData()[0] 
+				: xtextDocument.get(issue.getOffset(), issue.getLength());
+		Pair<String, String> packageAndType = typeNameGuesser.guessPackageAndTypeName(referenceOwner, typeString);
+		String typeName = packageAndType.getSecond();
+		if(isEmpty(typeName)) 
+			return;
+		String explicitPackage = packageAndType.getFirst();
+		boolean isLocal = isEmpty(explicitPackage) || explicitPackage.equals(getPackage(resource));
+		if(isLocal) 
+			explicitPackage = "";
+		if(isEmpty(packageAndType.getSecond()))
+			return;
 		if (unresolvedReference == XbasePackage.Literals.XCONSTRUCTOR_CALL__CONSTRUCTOR) {
 			if(((XConstructorCall)referenceOwner).getConstructor().eIsProxy()) {
-				newJavaClassQuickfix(typeName, resource, issue, issueResolutionAcceptor);
-				newXtendClassQuickfix(typeName, resource, issue, issueResolutionAcceptor);
-				newLocalXtendClassQuickfix(typeName, resource, issue, issueResolutionAcceptor);
+				if(isTypeMissing(referenceOwner, typeName, explicitPackage)) {
+					newJavaClassQuickfix(typeName, explicitPackage, resource, issue, issueResolutionAcceptor);
+					newXtendClassQuickfix(typeName, explicitPackage, resource, issue, issueResolutionAcceptor);
+					if(isLocal)
+						newLocalXtendClassQuickfix(typeName, resource, issue, issueResolutionAcceptor);
+				}
 			}
 		} else if(unresolvedReference == XbasePackage.Literals.XTYPE_LITERAL__TYPE
 				|| unresolvedReference == TypesPackage.Literals.JVM_PARAMETERIZED_TYPE_REFERENCE__TYPE) {
 			EStructuralFeature eContainingFeature = referenceOwner.eContainingFeature();
 			if(eContainingFeature == XtendPackage.Literals.XTEND_CLASS__EXTENDS) {
-				newJavaClassQuickfix(typeName, resource, issue, issueResolutionAcceptor);
-				newXtendClassQuickfix(typeName, resource, issue, issueResolutionAcceptor);
-				newLocalXtendClassQuickfix(typeName, resource, issue, issueResolutionAcceptor);
+				newJavaClassQuickfix(typeName, explicitPackage, resource, issue, issueResolutionAcceptor);
+				newXtendClassQuickfix(typeName, explicitPackage, resource, issue, issueResolutionAcceptor);
+				if(isLocal)
+					newLocalXtendClassQuickfix(typeName, resource, issue, issueResolutionAcceptor);
 			} else if(eContainingFeature == XtendPackage.Literals.XTEND_CLASS__IMPLEMENTS
 					|| eContainingFeature == XtendPackage.Literals.XTEND_INTERFACE__EXTENDS) {
-				newJavaInterfaceQuickfix(typeName, resource, issue, issueResolutionAcceptor);
-				newXtendInterfaceQuickfix(typeName, resource, issue, issueResolutionAcceptor);
-				newLocalXtendInterfaceQuickfix(typeName, resource, issue, issueResolutionAcceptor);
+				newJavaInterfaceQuickfix(typeName, explicitPackage, resource, issue, issueResolutionAcceptor);
+				newXtendInterfaceQuickfix(typeName, explicitPackage, resource, issue, issueResolutionAcceptor);
+				if(isLocal)
+					newLocalXtendInterfaceQuickfix(typeName, resource, issue, issueResolutionAcceptor);
 			} else {
-				newJavaClassQuickfix(typeName, resource, issue, issueResolutionAcceptor);
-				newJavaInterfaceQuickfix(typeName, resource, issue, issueResolutionAcceptor);
-				newXtendClassQuickfix(typeName, resource, issue, issueResolutionAcceptor);
-				newXtendInterfaceQuickfix(typeName, resource, issue, issueResolutionAcceptor);
-				newLocalXtendClassQuickfix(typeName, resource, issue, issueResolutionAcceptor);				
-				newLocalXtendInterfaceQuickfix(typeName, resource, issue, issueResolutionAcceptor);
+				newJavaClassQuickfix(typeName, explicitPackage, resource, issue, issueResolutionAcceptor);
+				newJavaInterfaceQuickfix(typeName, explicitPackage, resource, issue, issueResolutionAcceptor);
+				newXtendClassQuickfix(typeName, explicitPackage, resource, issue, issueResolutionAcceptor);
+				newXtendInterfaceQuickfix(typeName, explicitPackage, resource, issue, issueResolutionAcceptor);
+				if(isLocal) {
+					newLocalXtendClassQuickfix(typeName, resource, issue, issueResolutionAcceptor);				
+					newLocalXtendInterfaceQuickfix(typeName, resource, issue, issueResolutionAcceptor);
+				}
 			}
 		} else if(unresolvedReference == XAnnotationsPackage.Literals.XANNOTATION__ANNOTATION_TYPE) {
-			newJavaAnnotationQuickfix(typeName, resource, issue, issueResolutionAcceptor);
-			newXtendAnnotationQuickfix(typeName, resource, issue, issueResolutionAcceptor);
-			newLocalXtendAnnotationQuickfix(typeName, resource, issue, issueResolutionAcceptor);
+			newJavaAnnotationQuickfix(typeName, explicitPackage, resource, issue, issueResolutionAcceptor);
+			newXtendAnnotationQuickfix(typeName, explicitPackage, resource, issue, issueResolutionAcceptor);
+			if(isLocal) 
+				newLocalXtendAnnotationQuickfix(typeName, resource, issue, issueResolutionAcceptor);
 		}
 	}
 
+	protected boolean isTypeMissing(EObject referenceOwner, String typeName, String explicitPackage) {
+		QualifiedName typeQName = (!isEmpty(explicitPackage))
+			? converter.toQualifiedName(explicitPackage).append(typeName)
+			: converter.toQualifiedName(typeName);
+		final IScope typeScope = scopeProvider.getScope(referenceOwner, TypesPackage.Literals.JVM_PARAMETERIZED_TYPE_REFERENCE__TYPE);
+		return typeScope.getSingleElement(typeQName) == null;
+	}
+	
 	protected void newLocalXtendClassQuickfix(String typeName, XtextResource resource, Issue issue,
 			IssueResolutionAcceptor issueResolutionAcceptor) {
 		EObject eObject = resource.getEObject(issue.getUriToProblem().fragment());
@@ -169,10 +214,11 @@ public class CreateXtendTypeQuickfixes extends CreateJavaTypeQuickfixes {
 			return containerType;
 	}
 
-	protected void newXtendClassQuickfix(final String typeName, final XtextResource resource, Issue issue,
+	protected void newXtendClassQuickfix(final String typeName, final String explicitPackage, final XtextResource resource, Issue issue,
 			IssueResolutionAcceptor issueResolutionAcceptor) {
-		issueResolutionAcceptor.accept(issue, "Create Xtend class '" + typeName + "'",
-				"Opens the new Xtend class wizard to create the type '" + typeName + "'", "xtend_file.png",
+		String packageDescription = getPackageDescription(explicitPackage);
+		issueResolutionAcceptor.accept(issue, "Create Xtend class '" + typeName + "'" + packageDescription,
+				"Opens the new Xtend class wizard to create the type '" + typeName + "'" + packageDescription, "xtend_file.png",
 				new IModification() {
 					public void apply(@Nullable IModificationContext context) throws Exception {
 						runAsyncInDisplayThread(new Runnable() {
@@ -180,7 +226,7 @@ public class CreateXtendTypeQuickfixes extends CreateJavaTypeQuickfixes {
 								NewElementWizard newXtendClassWizard = newXtendClassWizardProvider.get();
 								WizardDialog dialog = createWizardDialog(newXtendClassWizard);
 								NewXtendClassWizardPage page = (NewXtendClassWizardPage) newXtendClassWizard.getStartingPage();
-								configureWizardPage(page, resource.getURI(), typeName);
+								configureWizardPage(page, resource.getURI(), typeName, explicitPackage);
 								dialog.open();
 							}
 						});
@@ -188,10 +234,11 @@ public class CreateXtendTypeQuickfixes extends CreateJavaTypeQuickfixes {
 				});
 	}
 
-	protected void newXtendInterfaceQuickfix(final String typeName, final XtextResource resource, Issue issue,
+	protected void newXtendInterfaceQuickfix(final String typeName, final String explicitPackage, final XtextResource resource, Issue issue,
 			IssueResolutionAcceptor issueResolutionAcceptor) {
-		issueResolutionAcceptor.accept(issue, "Create Xtend interface '" + typeName + "'",
-				"Opens the new Xtend interface wizard to create the type '" + typeName + "'", "xtend_file.png",
+		String packageDescription = getPackageDescription(explicitPackage);
+		issueResolutionAcceptor.accept(issue, "Create Xtend interface '" + typeName + "'" + packageDescription,
+				"Opens the new Xtend interface wizard to create the type '" + typeName + "'" + packageDescription, "xtend_file.png",
 				new IModification() {
 					public void apply(@Nullable IModificationContext context) throws Exception {
 						runAsyncInDisplayThread(new Runnable() {
@@ -199,7 +246,7 @@ public class CreateXtendTypeQuickfixes extends CreateJavaTypeQuickfixes {
 								NewElementWizard newXtendInterfaceWizard = newXtendInterfaceWizardProvider.get();
 								WizardDialog dialog = createWizardDialog(newXtendInterfaceWizard);
 								NewXtendInterfaceWizardPage page = (NewXtendInterfaceWizardPage) newXtendInterfaceWizard.getStartingPage();
-								configureWizardPage(page, resource.getURI(), typeName);
+								configureWizardPage(page, resource.getURI(), typeName, explicitPackage);
 								dialog.open();
 							}
 						});
@@ -207,10 +254,11 @@ public class CreateXtendTypeQuickfixes extends CreateJavaTypeQuickfixes {
 				});
 	}
 
-	protected void newXtendAnnotationQuickfix(final String typeName, final XtextResource resource, Issue issue,
+	protected void newXtendAnnotationQuickfix(final String typeName, final String explicitPackage, final XtextResource resource, Issue issue,
 			IssueResolutionAcceptor issueResolutionAcceptor) {
-		issueResolutionAcceptor.accept(issue, "Create Xtend annotation '" + typeName + "'",
-				"Opens the new Xtend annotation wizard to create the type '" + typeName + "'", "xtend_file.png",
+		String packageDescription = getPackageDescription(explicitPackage);
+		issueResolutionAcceptor.accept(issue, "Create Xtend annotation '" + typeName + "'" + packageDescription,
+				"Opens the new Xtend annotation wizard to create the type '" + typeName + "'" + packageDescription, "xtend_file.png",
 				new IModification() {
 					public void apply(@Nullable IModificationContext context) throws Exception {
 						runAsyncInDisplayThread(new Runnable() {
@@ -218,7 +266,7 @@ public class CreateXtendTypeQuickfixes extends CreateJavaTypeQuickfixes {
 								NewElementWizard newXtendAnnotationWizard = newXtendAnnotationWizardProvider.get();
 								WizardDialog dialog = createWizardDialog(newXtendAnnotationWizard);
 								NewXtendAnnotationWizardPage page = (NewXtendAnnotationWizardPage) newXtendAnnotationWizard.getStartingPage();
-								configureWizardPage(page, resource.getURI(), typeName);
+								configureWizardPage(page, resource.getURI(), typeName, explicitPackage);
 								dialog.open();
 							}
 						});
@@ -226,4 +274,14 @@ public class CreateXtendTypeQuickfixes extends CreateJavaTypeQuickfixes {
 				});
 	}
 
+	protected String getPackage(XtextResource resource) {
+		if(!resource.getContents().isEmpty()) {
+			final EObject root = resource.getContents().get(0);
+			if(root instanceof XtendFile) {
+				return ((XtendFile) root).getPackage();
+			}
+		}
+		return null;
+	}
+	
 }
