f6e66790c4a6cfdf5058e56344d4205538d6a140
Sven Efftinge <sven.efftinge@itemis.de>
2013-11-15 09:36:41 +0100

    [xbase] improved annotation value validation

--- plugins/org.eclipse.xtext.xbase/xtend-gen/org/eclipse/xtext/xbase/annotations/validation/AnnotationValueValidator.java
+++ plugins/org.eclipse.xtext.xbase/xtend-gen/org/eclipse/xtext/xbase/annotations/validation/AnnotationValueValidator.java
@@ -7,37 +7,25 @@
  */
 package org.eclipse.xtext.xbase.annotations.validation;
 
-import com.google.common.base.Objects;
 import java.util.Arrays;
 import org.eclipse.emf.common.util.EList;
-import org.eclipse.xtext.common.types.JvmEnumerationLiteral;
-import org.eclipse.xtext.common.types.JvmField;
-import org.eclipse.xtext.common.types.JvmIdentifiableElement;
-import org.eclipse.xtext.common.types.JvmPrimitiveType;
-import org.eclipse.xtext.common.types.JvmType;
-import org.eclipse.xtext.common.types.JvmTypeReference;
 import org.eclipse.xtext.validation.ValidationMessageAcceptor;
-import org.eclipse.xtext.xbase.XBooleanLiteral;
+import org.eclipse.xtext.xbase.XAbstractFeatureCall;
 import org.eclipse.xtext.xbase.XExpression;
-import org.eclipse.xtext.xbase.XFeatureCall;
 import org.eclipse.xtext.xbase.XListLiteral;
-import org.eclipse.xtext.xbase.XMemberFeatureCall;
-import org.eclipse.xtext.xbase.XNumberLiteral;
-import org.eclipse.xtext.xbase.XStringLiteral;
-import org.eclipse.xtext.xbase.XTypeLiteral;
 import org.eclipse.xtext.xbase.annotations.xAnnotations.XAnnotation;
-import org.eclipse.xtext.xbase.annotations.xAnnotations.XAnnotationElementValueBinaryOperation;
 import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.IterableExtensions;
+import org.eclipse.xtext.xbase.validation.ConstantExpressionValidator;
 import org.eclipse.xtext.xbase.validation.IssueCodes;
 
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
  */
 @SuppressWarnings("all")
-public class AnnotationValueValidator {
+public class AnnotationValueValidator extends ConstantExpressionValidator {
   public void validateAnnotationValue(final XExpression value, final ValidationMessageAcceptor acceptor) {
-    boolean _isValidAnnotationValue = this.isValidAnnotationValue(value, true);
+    boolean _isValidAnnotationValue = this.isValidAnnotationValue(value);
     boolean _not = (!_isValidAnnotationValue);
     if (_not) {
       acceptor.acceptError("The value for an annotation attribute must be a constant expression", value, null, 
@@ -45,161 +33,60 @@ public class AnnotationValueValidator {
     }
   }
   
-  protected boolean _isValidAnnotationValue(final XExpression expression, final boolean allowNesting) {
-    return false;
+  protected boolean _isValidAnnotationValue(final XExpression expression) {
+    return super.isConstant(expression);
   }
   
-  protected boolean _isValidAnnotationValue(final Void nullValue, final boolean allowNesting) {
+  protected boolean _isValidAnnotationValue(final Void nullValue) {
     return true;
   }
   
-  protected boolean _isValidAnnotationValue(final XBooleanLiteral expression, final boolean allowNesting) {
-    return true;
-  }
-  
-  protected boolean _isValidAnnotationValue(final XNumberLiteral expression, final boolean allowNesting) {
-    return true;
-  }
-  
-  protected boolean _isValidAnnotationValue(final XStringLiteral expression, final boolean allowNesting) {
-    return true;
-  }
-  
-  protected boolean _isValidAnnotationValue(final XTypeLiteral expression, final boolean allowNesting) {
-    return true;
-  }
-  
-  protected boolean _isValidAnnotationValue(final XAnnotation expression, final boolean allowNesting) {
-    return true;
-  }
-  
-  protected boolean _isValidAnnotationValue(final XListLiteral expression, final boolean allowNesting) {
-    if (allowNesting) {
-      EList<XExpression> _elements = expression.getElements();
+  protected boolean _isValidAnnotationValue(final XListLiteral expression) {
+    boolean _or = false;
+    EList<XExpression> _elements = expression.getElements();
+    boolean _isEmpty = _elements.isEmpty();
+    if (_isEmpty) {
+      _or = true;
+    } else {
+      EList<XExpression> _elements_1 = expression.getElements();
       final Function1<XExpression,Boolean> _function = new Function1<XExpression,Boolean>() {
         public Boolean apply(final XExpression it) {
-          boolean _isValidAnnotationValue = AnnotationValueValidator.this.isValidAnnotationValue(it, false);
+          boolean _isValidAnnotationValue = AnnotationValueValidator.this.isValidAnnotationValue(it);
           return Boolean.valueOf(_isValidAnnotationValue);
         }
       };
-      return IterableExtensions.<XExpression>forall(_elements, _function);
+      boolean _forall = IterableExtensions.<XExpression>forall(_elements_1, _function);
+      _or = (_isEmpty || _forall);
     }
-    return false;
+    return _or;
   }
   
-  protected boolean _isValidAnnotationValue(final XFeatureCall expression, final boolean allowNesting) {
-    boolean _or = false;
-    boolean _isTypeLiteral = expression.isTypeLiteral();
-    if (_isTypeLiteral) {
-      _or = true;
-    } else {
-      boolean _isPackageFragment = expression.isPackageFragment();
-      _or = (_isTypeLiteral || _isPackageFragment);
-    }
-    if (_or) {
-      return true;
-    }
-    JvmIdentifiableElement _feature = expression.getFeature();
-    return this.isValidAnnotationValue(_feature);
-  }
-  
-  protected boolean _isValidAnnotationValue(final XMemberFeatureCall expression, final boolean allowNesting) {
-    boolean _or = false;
-    boolean _isTypeLiteral = expression.isTypeLiteral();
-    if (_isTypeLiteral) {
-      _or = true;
-    } else {
-      boolean _isPackageFragment = expression.isPackageFragment();
-      _or = (_isTypeLiteral || _isPackageFragment);
-    }
-    if (_or) {
-      return true;
-    }
-    JvmIdentifiableElement _feature = expression.getFeature();
-    return this.isValidAnnotationValue(_feature);
-  }
-  
-  protected boolean _isValidAnnotationValue(final XAnnotationElementValueBinaryOperation expression, final boolean allowNesting) {
-    boolean _and = false;
-    XExpression _leftOperand = expression.getLeftOperand();
-    boolean _isValidAnnotationValue = this.isValidAnnotationValue(_leftOperand, false);
-    if (!_isValidAnnotationValue) {
-      _and = false;
-    } else {
-      XExpression _rightOperand = expression.getRightOperand();
-      boolean _isValidAnnotationValue_1 = this.isValidAnnotationValue(_rightOperand, false);
-      _and = (_isValidAnnotationValue && _isValidAnnotationValue_1);
-    }
-    return _and;
-  }
-  
-  protected boolean _isValidAnnotationValue(final JvmIdentifiableElement identifiable) {
-    return false;
-  }
-  
-  protected boolean _isValidAnnotationValue(final Void identifiable) {
+  protected boolean _isValidAnnotationValue(final XAnnotation expression) {
     return true;
   }
   
-  protected boolean _isValidAnnotationValue(final JvmField field) {
-    boolean _isStatic = field.isStatic();
-    if (_isStatic) {
-      final JvmTypeReference fieldType = field.getType();
-      final JvmType rawType = fieldType.getType();
-      if ((rawType instanceof JvmPrimitiveType)) {
-        return true;
-      }
-      String _identifier = fieldType.getIdentifier();
-      return Objects.equal(_identifier, "java.lang.String");
+  protected boolean _isValidAnnotationValue(final XAbstractFeatureCall expression) {
+    boolean _isTypeLiteral = expression.isTypeLiteral();
+    if (_isTypeLiteral) {
+      return true;
     }
-    return false;
+    return super.isConstant(expression);
   }
   
-  protected boolean _isValidAnnotationValue(final JvmEnumerationLiteral enumLiteral) {
-    return true;
-  }
-  
-  protected boolean isValidAnnotationValue(final XExpression expression, final boolean allowNesting) {
-    if (expression instanceof XFeatureCall) {
-      return _isValidAnnotationValue((XFeatureCall)expression, allowNesting);
-    } else if (expression instanceof XListLiteral) {
-      return _isValidAnnotationValue((XListLiteral)expression, allowNesting);
-    } else if (expression instanceof XMemberFeatureCall) {
-      return _isValidAnnotationValue((XMemberFeatureCall)expression, allowNesting);
-    } else if (expression instanceof XBooleanLiteral) {
-      return _isValidAnnotationValue((XBooleanLiteral)expression, allowNesting);
-    } else if (expression instanceof XNumberLiteral) {
-      return _isValidAnnotationValue((XNumberLiteral)expression, allowNesting);
-    } else if (expression instanceof XStringLiteral) {
-      return _isValidAnnotationValue((XStringLiteral)expression, allowNesting);
-    } else if (expression instanceof XTypeLiteral) {
-      return _isValidAnnotationValue((XTypeLiteral)expression, allowNesting);
+  protected boolean isValidAnnotationValue(final XExpression expression) {
+    if (expression instanceof XListLiteral) {
+      return _isValidAnnotationValue((XListLiteral)expression);
+    } else if (expression instanceof XAbstractFeatureCall) {
+      return _isValidAnnotationValue((XAbstractFeatureCall)expression);
     } else if (expression instanceof XAnnotation) {
-      return _isValidAnnotationValue((XAnnotation)expression, allowNesting);
-    } else if (expression instanceof XAnnotationElementValueBinaryOperation) {
-      return _isValidAnnotationValue((XAnnotationElementValueBinaryOperation)expression, allowNesting);
+      return _isValidAnnotationValue((XAnnotation)expression);
     } else if (expression != null) {
-      return _isValidAnnotationValue(expression, allowNesting);
+      return _isValidAnnotationValue(expression);
     } else if (expression == null) {
-      return _isValidAnnotationValue((Void)null, allowNesting);
-    } else {
-      throw new IllegalArgumentException("Unhandled parameter types: " +
-        Arrays.<Object>asList(expression, allowNesting).toString());
-    }
-  }
-  
-  protected boolean isValidAnnotationValue(final JvmIdentifiableElement enumLiteral) {
-    if (enumLiteral instanceof JvmEnumerationLiteral) {
-      return _isValidAnnotationValue((JvmEnumerationLiteral)enumLiteral);
-    } else if (enumLiteral instanceof JvmField) {
-      return _isValidAnnotationValue((JvmField)enumLiteral);
-    } else if (enumLiteral != null) {
-      return _isValidAnnotationValue(enumLiteral);
-    } else if (enumLiteral == null) {
       return _isValidAnnotationValue((Void)null);
     } else {
       throw new IllegalArgumentException("Unhandled parameter types: " +
-        Arrays.<Object>asList(enumLiteral).toString());
+        Arrays.<Object>asList(expression).toString());
     }
   }
 }
---
+++ plugins/org.eclipse.xtext.xbase/xtend-gen/org/eclipse/xtext/xbase/validation/ConstantExpressionValidator.java
@@ -0,0 +1,194 @@
+/**
+ * Copyright (c) 2013 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.xtext.xbase.validation;
+
+import com.google.common.base.Objects;
+import com.google.common.collect.Iterables;
+import java.util.Arrays;
+import javax.inject.Inject;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.xtext.common.types.JvmAnnotationReference;
+import org.eclipse.xtext.common.types.JvmAnnotationValue;
+import org.eclipse.xtext.common.types.JvmBooleanAnnotationValue;
+import org.eclipse.xtext.common.types.JvmEnumerationLiteral;
+import org.eclipse.xtext.common.types.JvmField;
+import org.eclipse.xtext.common.types.JvmIdentifiableElement;
+import org.eclipse.xtext.common.types.JvmOperation;
+import org.eclipse.xtext.common.types.JvmTypeReference;
+import org.eclipse.xtext.common.types.util.Primitives;
+import org.eclipse.xtext.common.types.util.TypeReferences;
+import org.eclipse.xtext.xbase.XAbstractFeatureCall;
+import org.eclipse.xtext.xbase.XBooleanLiteral;
+import org.eclipse.xtext.xbase.XCastedExpression;
+import org.eclipse.xtext.xbase.XExpression;
+import org.eclipse.xtext.xbase.XNumberLiteral;
+import org.eclipse.xtext.xbase.XStringLiteral;
+import org.eclipse.xtext.xbase.XTypeLiteral;
+import org.eclipse.xtext.xbase.lib.Extension;
+import org.eclipse.xtext.xbase.lib.Functions.Function1;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
+import org.eclipse.xtext.xbase.util.XExpressionHelper;
+
+/**
+ * Checks whether a given XExpression is a a constant expression.
+ * 
+ * @author Sven Efftinge - Initial contribution and API
+ */
+@SuppressWarnings("all")
+public class ConstantExpressionValidator {
+  @Inject
+  @Extension
+  private TypeReferences _typeReferences;
+  
+  @Inject
+  @Extension
+  private Primitives _primitives;
+  
+  @Inject
+  @Extension
+  private XExpressionHelper _xExpressionHelper;
+  
+  protected boolean _isConstant(final XExpression expression) {
+    return false;
+  }
+  
+  protected boolean _isConstant(final XStringLiteral expression) {
+    return true;
+  }
+  
+  protected boolean _isConstant(final XTypeLiteral expression) {
+    return true;
+  }
+  
+  protected boolean _isConstant(final XNumberLiteral expression) {
+    return true;
+  }
+  
+  protected boolean _isConstant(final XBooleanLiteral expression) {
+    return true;
+  }
+  
+  protected boolean _isConstant(final XCastedExpression expression) {
+    boolean _or = false;
+    JvmTypeReference _type = expression.getType();
+    boolean _isPrimitive = this._primitives.isPrimitive(_type);
+    if (_isPrimitive) {
+      _or = true;
+    } else {
+      JvmTypeReference _type_1 = expression.getType();
+      boolean _is = this._typeReferences.is(_type_1, String.class);
+      _or = (_isPrimitive || _is);
+    }
+    return _or;
+  }
+  
+  protected boolean _isConstant(final XAbstractFeatureCall expression) {
+    JvmIdentifiableElement _feature = expression.getFeature();
+    final JvmIdentifiableElement feature = _feature;
+    boolean _matched = false;
+    if (!_matched) {
+      if (feature instanceof JvmField) {
+        _matched=true;
+        boolean _and = false;
+        boolean _isStatic = ((JvmField)feature).isStatic();
+        if (!_isStatic) {
+          _and = false;
+        } else {
+          boolean _isFinal = ((JvmField)feature).isFinal();
+          _and = (_isStatic && _isFinal);
+        }
+        return _and;
+      }
+    }
+    if (!_matched) {
+      if (feature instanceof JvmEnumerationLiteral) {
+        _matched=true;
+        return true;
+      }
+    }
+    if (!_matched) {
+      if (feature instanceof JvmOperation) {
+        _matched=true;
+        final JvmAnnotationReference annotationReference = this._xExpressionHelper.findInlineAnnotation(expression);
+        boolean _equals = Objects.equal(annotationReference, null);
+        if (_equals) {
+          return false;
+        }
+        EList<JvmAnnotationValue> _values = annotationReference.getValues();
+        Iterable<JvmBooleanAnnotationValue> _filter = Iterables.<JvmBooleanAnnotationValue>filter(_values, JvmBooleanAnnotationValue.class);
+        final Function1<JvmBooleanAnnotationValue,Boolean> _function = new Function1<JvmBooleanAnnotationValue,Boolean>() {
+          public Boolean apply(final JvmBooleanAnnotationValue it) {
+            boolean _and = false;
+            String _valueName = it.getValueName();
+            boolean _equals = Objects.equal(_valueName, "constantExpression");
+            if (!_equals) {
+              _and = false;
+            } else {
+              EList<Boolean> _values = it.getValues();
+              Boolean _head = IterableExtensions.<Boolean>head(_values);
+              boolean _booleanValue = _head.booleanValue();
+              _and = (_equals && _booleanValue);
+            }
+            return Boolean.valueOf(_and);
+          }
+        };
+        boolean _exists = IterableExtensions.<JvmBooleanAnnotationValue>exists(_filter, _function);
+        if (_exists) {
+          boolean _xifexpression = false;
+          XExpression _actualReceiver = expression.getActualReceiver();
+          boolean _equals_1 = Objects.equal(_actualReceiver, null);
+          if (_equals_1) {
+            _xifexpression = true;
+          } else {
+            XExpression _actualReceiver_1 = expression.getActualReceiver();
+            boolean _isConstant = this.isConstant(_actualReceiver_1);
+            _xifexpression = _isConstant;
+          }
+          final boolean receiverConstant = _xifexpression;
+          boolean _and = false;
+          if (!receiverConstant) {
+            _and = false;
+          } else {
+            EList<XExpression> _actualArguments = expression.getActualArguments();
+            final Function1<XExpression,Boolean> _function_1 = new Function1<XExpression,Boolean>() {
+              public Boolean apply(final XExpression it) {
+                boolean _isConstant = ConstantExpressionValidator.this.isConstant(it);
+                return Boolean.valueOf(_isConstant);
+              }
+            };
+            boolean _forall = IterableExtensions.<XExpression>forall(_actualArguments, _function_1);
+            _and = (receiverConstant && _forall);
+          }
+          return _and;
+        }
+      }
+    }
+    return false;
+  }
+  
+  public boolean isConstant(final XExpression expression) {
+    if (expression instanceof XAbstractFeatureCall) {
+      return _isConstant((XAbstractFeatureCall)expression);
+    } else if (expression instanceof XBooleanLiteral) {
+      return _isConstant((XBooleanLiteral)expression);
+    } else if (expression instanceof XCastedExpression) {
+      return _isConstant((XCastedExpression)expression);
+    } else if (expression instanceof XNumberLiteral) {
+      return _isConstant((XNumberLiteral)expression);
+    } else if (expression instanceof XStringLiteral) {
+      return _isConstant((XStringLiteral)expression);
+    } else if (expression instanceof XTypeLiteral) {
+      return _isConstant((XTypeLiteral)expression);
+    } else if (expression != null) {
+      return _isConstant(expression);
+    } else {
+      throw new IllegalArgumentException("Unhandled parameter types: " +
+        Arrays.<Object>asList(expression).toString());
+    }
+  }
+}
