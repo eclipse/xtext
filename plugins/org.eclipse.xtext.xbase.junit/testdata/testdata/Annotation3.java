5a6f406d4ca344d1799189b1c562fc79319f7b58
Sven Efftinge <sven.efftinge@itemis.de>
2013-11-15 09:36:41 +0100

    [xbase] improved annotation value validation

---
+++ plugins/org.eclipse.xtext.xbase.junit/testdata/testdata/Annotation3.java
@@ -0,0 +1,22 @@
+package testdata;
+
+/**
+ * @since 2.5
+ */
+public @interface Annotation3 {
+	boolean booleanValue() default true;
+	int intValue() default 1;
+	long longValue() default 42;
+	String stringValue() default "foo";
+	
+	boolean[] booleanArrayValue() default {true};
+	int[] intArrayValue() default {42};
+	long[] longArrayValue() default {99};
+	String[] stringArrayValue() default {"foo"};
+	
+	Class<?> typeValue() default String.class;
+	Class<?>[] typeArrayValue() default {String.class};
+	
+	Annotation2 annotation2Value() default @Annotation2({"foo"});
+	Annotation2[] annotation2ArrayValue() default {@Annotation2({"foo"})};
+}
