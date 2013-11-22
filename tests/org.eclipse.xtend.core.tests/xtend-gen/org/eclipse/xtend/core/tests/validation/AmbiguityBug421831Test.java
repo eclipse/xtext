2b2a8bf9d262d33d28a5eb769fd5d66dfb0e3639
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2013-11-22 21:07:43 +0100

    [xbase][validation] Added more tests to AmbiguitySuite
    see https://bugs.eclipse.org/bugs/show_bug.cgi?id=421831

---
+++ tests/org.eclipse.xtend.core.tests/xtend-gen/org/eclipse/xtend/core/tests/validation/AmbiguityBug421831Test.java
@@ -0,0 +1,152 @@
+/**
+ * Copyright (c) 2013 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.xtend.core.tests.validation;
+
+import org.eclipse.xtend.core.tests.validation.AmbiguityValidationTest;
+import org.eclipse.xtend2.lib.StringConcatenation;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author Sebastian Zarnekow - Initial contribution and API
+ */
+@SuppressWarnings("all")
+public class AmbiguityBug421831Test extends AmbiguityValidationTest {
+  @Test
+  public void testAmbiguousStaticImports_01() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("import static MockitoMatchers.* ");
+    _builder.newLine();
+    _builder.append("import static HarmcrestMatchers.*");
+    _builder.newLine();
+    _builder.newLine();
+    _builder.append("class Bug {");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("def static accept(Bug bug) {");
+    _builder.newLine();
+    _builder.append("    ");
+    _builder.append("any(Bug)");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("class MockitoMatchers {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("static def <T> Matcher<T> any(Class<T> type) {}");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("class Matcher<T> {}");
+    _builder.newLine();
+    _builder.append("class HarmcrestMatchers {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("static def <T> T any(Class<T> clazz) {}");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    StringConcatenation _builder_1 = new StringConcatenation();
+    _builder_1.append("Ambiguous feature call.");
+    _builder_1.newLine();
+    _builder_1.append("The methods");
+    _builder_1.newLine();
+    _builder_1.append("\t");
+    _builder_1.append("<T> any(Class<T>) in MockitoMatchers and");
+    _builder_1.newLine();
+    _builder_1.append("\t");
+    _builder_1.append("<T> any(Class<T>) in HarmcrestMatchers");
+    _builder_1.newLine();
+    _builder_1.append("both match.");
+    this.assertAmbiguous(_builder, _builder_1.toString());
+  }
+  
+  @Ignore("TODO fix me")
+  @Test
+  public void testAmbiguousStaticImports_02() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("import static MockitoMatchers.* ");
+    _builder.newLine();
+    _builder.append("import static HarmcrestMatchers.*");
+    _builder.newLine();
+    _builder.newLine();
+    _builder.append("class Bug {");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("def static accept(Bug bug) {");
+    _builder.newLine();
+    _builder.append("    ");
+    _builder.append("any(Bug)");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("class MockitoMatchers {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("static def <T> Matcher<T> any(Class<T> type) {}");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("class Matcher<T> {}");
+    _builder.newLine();
+    _builder.append("class HarmcrestMatchers {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("static def <T extends Bug> T any(Class<T> clazz) {}");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    this.assertUnambiguous(_builder);
+  }
+  
+  @Test
+  public void testUnambiguousStaticImports_01() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("import static MockitoMatchers.* ");
+    _builder.newLine();
+    _builder.append("import static HarmcrestMatchers.*");
+    _builder.newLine();
+    _builder.newLine();
+    _builder.append("class Bug {");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("def static accept(Bug bug) {");
+    _builder.newLine();
+    _builder.append("    ");
+    _builder.append("any(Bug)");
+    _builder.newLine();
+    _builder.append("  ");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("class MockitoMatchers {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("static def <T extends Number> Matcher<T> any(Class<T> type) {}");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("class Matcher<T> {}");
+    _builder.newLine();
+    _builder.append("class HarmcrestMatchers {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("static def <T> T any(Class<T> clazz) {}");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    this.assertUnambiguous(_builder);
+  }
+}
