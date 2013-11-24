82f1abcb81fc3a68c648049e0b38f8f457aad751
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2013-11-24 19:28:25 +0100

    improved the outline view Signed-off-by: Moritz Eysholdt <moritz.eysholdt@itemis.de>

--- org.xpect.ui/src/org/xpect/ui/outline/XpectOutlineTreeProvider.java
+++ org.xpect.ui/src/org/xpect/ui/outline/XpectOutlineTreeProvider.java
@@ -7,11 +7,46 @@
  *******************************************************************************/
 package org.xpect.ui.outline;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.common.types.JvmDeclaredType;
+import org.eclipse.xtext.ui.editor.outline.IOutlineNode;
 import org.eclipse.xtext.ui.editor.outline.impl.DefaultOutlineTreeProvider;
+import org.xpect.XjmTest;
+import org.xpect.XpectFile;
+import org.xpect.XpectInvocation;
+import org.xpect.XpectJavaModel;
+import org.xpect.XpectTest;
 
 /**
  * @author Moritz Eysholdt - Initial contribution and API
  */
 public class XpectOutlineTreeProvider extends DefaultOutlineTreeProvider {
+	protected void _createChildren(IOutlineNode parentNode, XpectFile file) {
+		for (EObject childElement : file.getInvocations())
+			createNode(parentNode, childElement);
+	}
 
+	protected void _createChildren(IOutlineNode parentNode, XpectInvocation inv) {
+	}
+
+	public String _text(XpectFile file) {
+		String filename = file.eResource().getURI().lastSegment();
+		XpectTest test = file.getTest();
+		if (!test.eIsProxy()) {
+			XpectJavaModel model = test.getTestClassOrSuite();
+			if (model != null && !model.eIsProxy()) {
+				XjmTest suite = model.getTestOrSuite();
+				if (suite != null && !suite.eIsProxy()) {
+					JvmDeclaredType jvmClass = suite.getJvmClass();
+					if (jvmClass != null && !jvmClass.eIsProxy())
+						return filename + " - " + jvmClass.getSimpleName();
+				}
+			}
+		}
+		return filename;
+	}
+
+	public String _text(XpectInvocation inv) {
+		return inv.getMethodName();
+	}
 }
