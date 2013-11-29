87712d3c984e031021a6a079772573f186901e04
akosyakov <anton.kosyakov@itemis.de>
2013-11-29 11:33:45 +0100

    [bug 421989]: added quick fix for ambiguous implicit method call
    Change-Id: Ieab0cd3887aeeb980b28db47d0abee32bb761f27
    Signed-off-by: akosyakov <anton.kosyakov@itemis.de>

--- plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/typesystem/internal/AmbiguousFeatureLinkingCandidate.java
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/typesystem/internal/AmbiguousFeatureLinkingCandidate.java
@@ -7,14 +7,22 @@
  *******************************************************************************/
 package org.eclipse.xtext.xbase.typesystem.internal;
 
+import java.util.List;
+import java.util.Set;
+
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.xtext.common.types.JvmIdentifiableElement;
 import org.eclipse.xtext.xbase.XAbstractFeatureCall;
 import org.eclipse.xtext.xbase.XBinaryOperation;
 import org.eclipse.xtext.xbase.XExpression;
 import org.eclipse.xtext.xbase.XUnaryOperation;
 import org.eclipse.xtext.xbase.XbasePackage;
 import org.eclipse.xtext.xbase.typesystem.computation.IFeatureLinkingCandidate;
+import org.eclipse.xtext.xbase.typesystem.computation.ILinkingCandidate;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
@@ -69,6 +77,22 @@ public class AmbiguousFeatureLinkingCandidate extends AbstractAmbiguousLinkingCa
 	@Override
 	protected EStructuralFeature getFeatureToMark() {
 		return XbasePackage.Literals.XABSTRACT_FEATURE_CALL__FEATURE;
+	}
+
+	@Override
+	protected String[] getDiagnosticData() {
+		FeatureLinkingCandidate primaryCandidate = getPrimaryCandidate();
+		XAbstractFeatureCall expression = primaryCandidate.getExpression();
+		if (expression.isExplicitOperationCallOrBuilderSyntax()) {
+			return null;
+		}
+		Set<String> data = Sets.newHashSet();
+		for (ILinkingCandidate candidate : getAlternatives()) {
+			JvmIdentifiableElement feature = candidate.getFeature();
+			String simpleName = feature.getSimpleName();
+			data.add(simpleName + "()");
+		}
+		return data.toArray(new String[0]);
 	}
 
 }
