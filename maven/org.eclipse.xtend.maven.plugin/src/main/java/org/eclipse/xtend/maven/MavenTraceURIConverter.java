ae515da7b66155a7f15ddaf2351f62144c76b4d9
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2013-12-04 11:51:32 +0100

    [xtend][mvn] Regression: Fixed compile problem

--- maven/org.eclipse.xtend.maven.plugin/src/main/java/org/eclipse/xtend/maven/MavenTraceURIConverter.java
+++ maven/org.eclipse.xtend.maven.plugin/src/main/java/org/eclipse/xtend/maven/MavenTraceURIConverter.java
@@ -6,14 +6,19 @@ import java.io.IOException;
 import org.apache.maven.project.MavenProject;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.xtext.generator.trace.DefaultTraceURIConverter;
+import org.eclipse.xtext.generator.trace.TraceURIHelper;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.util.RuntimeIOException;
 
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
+import com.google.inject.Inject;
 
 public class MavenTraceURIConverter extends DefaultTraceURIConverter {
 
+	@Inject
+	private TraceURIHelper traceURIHelper;
+	
 	@Override
 	public URI getURIForTrace(XtextResource context) {
 		MavenProject project = MavenProjectAdapter.get(context.getResourceSet());
@@ -30,7 +35,7 @@ public class MavenTraceURIConverter extends DefaultTraceURIConverter {
 				String canonicalPath = new File(rootString).getCanonicalPath();
 				canonicalPath += "/";
 				root = URI.createFileURI(canonicalPath);
-				if (isPrefix(root, uri))
+				if (traceURIHelper.isPrefix(root, uri))
 					return uri.deresolve(root);
 			} catch (IOException e) {
 				throw new RuntimeIOException(e);
