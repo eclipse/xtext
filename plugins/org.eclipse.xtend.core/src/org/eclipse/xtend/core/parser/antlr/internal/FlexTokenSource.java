84c982ee9db6a771ed4a0f810b4b1b6b3d40846a
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2013-12-13 16:05:33 +0100

    [xtend][lexing] Improved lexing performance
    Antlr: Use human readable token names
    Use custom lexer
    
    see https://bugs.eclipse.org/bugs/show_bug.cgi?id=423203
    
    Change-Id: Ibbfb37587f1a0c51adb18c5821c7366c8e696d56

---
+++ plugins/org.eclipse.xtend.core/src/org/eclipse/xtend/core/parser/antlr/internal/FlexTokenSource.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2013 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtend.core.parser.antlr.internal;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import org.antlr.runtime.CommonToken;
+import org.antlr.runtime.Token;
+import org.antlr.runtime.TokenSource;
+
+/**
+ * @author Sebastian Zarnekow - Initial contribution and API
+ */
+public class FlexTokenSource implements TokenSource {
+
+	private final InternalXtendFlexer flexer;
+	private int offset;
+
+	public FlexTokenSource(Reader reader) {
+		flexer = new InternalXtendFlexer(reader);
+		offset = 0;
+	}
+	
+	public void reset(Reader reader) {
+		flexer.yyreset(reader);
+		offset = 0;
+	}
+
+	public Token nextToken() {
+		try {
+			int type = flexer.advance();
+			if (type == Token.EOF) {
+				return Token.EOF_TOKEN;
+			}
+			int length = flexer.getTokenLength();
+			final String tokenText = flexer.yytext();
+			CommonToken result = new CommonTokenWithText(tokenText, type, Token.DEFAULT_CHANNEL, offset);
+			offset += length;
+			return result;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public String getSourceName() {
+		return "FlexTokenSource";
+	}
+	
+	public static class CommonTokenWithText extends CommonToken {
+
+		private static final long serialVersionUID = 1L;
+
+		public CommonTokenWithText(String tokenText, int type, int defaultChannel, int offset) {
+			super(null, type, defaultChannel, offset, offset + tokenText.length() - 1);
+			this.text = tokenText;
+		}
+		
+	}
+
+}
