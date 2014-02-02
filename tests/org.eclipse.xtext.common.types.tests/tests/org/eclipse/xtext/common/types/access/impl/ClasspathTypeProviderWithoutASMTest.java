8ada289a8eeea9b3625076ef40429f1001ce4e79
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2014-02-02 16:38:56 +0100

    [common.types] Fallback to java.lang.reflect if ASM is not available
    Re-exporting ASM in a specific version is not recommended
    since this is the type of library that's often used in different
    versions.
    
    see https://bugs.eclipse.org/bugs/show_bug.cgi?id=427135

---
+++ tests/org.eclipse.xtext.common.types.tests/tests/org/eclipse/xtext/common/types/access/impl/ClasspathTypeProviderWithoutASMTest.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2014 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.common.types.access.impl;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.xtext.common.types.JvmDeclaredType;
+import org.eclipse.xtext.common.types.access.IMirror;
+import org.eclipse.xtext.common.types.access.binary.BinaryClass;
+import org.eclipse.xtext.common.types.access.binary.BinaryClassMirror;
+import org.eclipse.xtext.common.types.access.binary.asm.ClassFileBytesAccess;
+import org.eclipse.xtext.common.types.access.reflect.ReflectionTypeProviderTest;
+import org.junit.Test;
+
+/**
+ * @author Sebastian Zarnekow - Initial contribution and API
+ */
+public class ClasspathTypeProviderWithoutASMTest extends ReflectionTypeProviderTest {
+
+	@Override
+	protected AbstractRuntimeJvmTypeProvider createTypeProvider() {
+		return new ClasspathTypeProvider(getClass().getClassLoader(), resourceSet, indexedJvmTypeAccess) {
+			@Override
+			protected ITypeFactory<BinaryClass, JvmDeclaredType> createDeclaredTypeFactory(ClassFileBytesAccess readerAccess, ClassLoader classLoader) {
+				return new DeclaredTypeFactory(readerAccess, classLoader, false);
+			}
+		};
+	}
+	
+	@Override
+	@Test
+	public void testCreateMirror_01() {
+		URI uri = URI.createURI("java:/Objects/java.util.Map");
+		IMirror mirror = getTypeProvider().createMirror(uri);
+		assertNotNull(mirror);
+		assertTrue(mirror instanceof BinaryClassMirror);
+		assertEquals("java.util.Map", ((BinaryClassMirror) mirror).getMirroredClass().getName());
+	}
+}
+
