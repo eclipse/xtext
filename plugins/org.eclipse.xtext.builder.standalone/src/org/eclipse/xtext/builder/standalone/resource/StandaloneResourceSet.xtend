b81bd55092f723a98a94e1429b3e173e68a619c8
Sven Efftinge <sven.efftinge@itemis.de>
2014-02-10 11:32:33 +0100

    [bug 427544][standalone builder]: Fixed issue with onDemand==true.
    If onDemand is set to true, it will now first try with onDemand==false, then check for the last segment and only then call super.getResource(uri, true). It will potentially cause trouble, when there are multiple different resources with the same simple name. One could check that by trying to resolve the URI before looking for the last segment.

--- plugins/org.eclipse.xtext.builder.standalone/src/org/eclipse/xtext/builder/standalone/resource/StandaloneResourceSet.xtend
+++ plugins/org.eclipse.xtext.builder.standalone/src/org/eclipse/xtext/builder/standalone/resource/StandaloneResourceSet.xtend
@@ -13,18 +13,43 @@ import org.eclipse.xtext.resource.XtextResourceSet
 
 /**
  * @author Anton Kosyakov - Initial contribution and API
+ * @author Sven Efftinge
  */
 class StandaloneResourceSet extends XtextResourceSet {
 
 	static val LOG = Logger.getLogger(StandaloneResourceSet)
 
 	override getResource(URI uri, boolean loadOnDemand) {
-		val superResource = super.getResource(uri, loadOnDemand)
-		if (superResource != null) {
-			superResource
+		if (loadOnDemand) {
+			// try to find existing
+			val resource = super.getResource(uri, false);
+			if (resource != null) {
+				if (!resource.loaded) {
+					demandLoadHelper(resource);
+				}
+				return resource;
+			}
+			// try find with same simple name
+			val resourceWithSameLastSegment = getResourceByLastSegment(uri)
+			if (resourceWithSameLastSegment != null) {
+				if (!resourceWithSameLastSegment.loaded) {
+					demandLoadHelper(resourceWithSameLastSegment);
+				}
+				LOG.warn('''Failed to resolve a resource by URI: '«uri»'; Using similar URI instead: '«resourceWithSameLastSegment.URI»'.''');
+				return resourceWithSameLastSegment;
+			}
+			return super.getResource(uri, true);
 		} else {
-			LOG.warn('''Failed to resolve a resource by URI: '«uri»'; trying to load it by the URI's last segment: '«uri?.lastSegment»'.''');
-			uri.resourceByLastSegment
+			val superResource = super.getResource(uri, false)
+			if (superResource != null) {
+				return superResource
+			} else {
+				val resourceWithSameLastSegment = getResourceByLastSegment(uri)
+				if (resourceWithSameLastSegment != null) {
+					LOG.warn('''Failed to resolve a resource by URI: '«uri»'; Using similar URI instead: '«resourceWithSameLastSegment.URI»'.''');
+				}
+				return resourceWithSameLastSegment
+			}
 		}
 	}
 
@@ -33,13 +58,12 @@ class StandaloneResourceSet extends XtextResourceSet {
 		if (lastSegment == null) {
 			return null
 		}
-		val resources = resources.filter[lastSegment == URI?.lastSegment]
+		val resources = resources.filter[lastSegment == URI?.lastSegment].toList
 		if (resources.empty) {
 			return null
 		}
 		if (resources.size > 1) {
-			LOG.error('''Failed to resolve a resource by the URI's last segment: '«uri?.lastSegment»'. Several resources are matched: «FOR resourceURI : resources.map[URI] SEPARATOR ','»'«resourceURI»'«ENDFOR».''');
-			return null
+			throw new IllegalArgumentException('''Failed to resolve a resource by the URI's last segment: '«uri?.lastSegment»'. Several resources are matched: «FOR resourceURI : resources.map[URI] SEPARATOR ','»'«resourceURI»'«ENDFOR».''');
 		}
 		return resources.head
 	}
--- plugins/org.eclipse.xtext.builder.standalone/xtend-gen/org/eclipse/xtext/builder/standalone/resource/StandaloneResourceSet.java
+++ plugins/org.eclipse.xtext.builder.standalone/xtend-gen/org/eclipse/xtext/builder/standalone/resource/StandaloneResourceSet.java
@@ -8,6 +8,7 @@
 package org.eclipse.xtext.builder.standalone.resource;
 
 import com.google.common.base.Objects;
+import java.util.List;
 import org.apache.log4j.Logger;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -15,43 +16,68 @@ import org.eclipse.xtend2.lib.StringConcatenation;
 import org.eclipse.xtext.resource.XtextResourceSet;
 import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.IterableExtensions;
+import org.eclipse.xtext.xbase.lib.ListExtensions;
 
 /**
  * @author Anton Kosyakov - Initial contribution and API
+ * @author Sven Efftinge
  */
 @SuppressWarnings("all")
 public class StandaloneResourceSet extends XtextResourceSet {
   private final static Logger LOG = Logger.getLogger(StandaloneResourceSet.class);
   
   public Resource getResource(final URI uri, final boolean loadOnDemand) {
-    Resource _xblockexpression = null;
-    {
-      final Resource superResource = super.getResource(uri, loadOnDemand);
-      Resource _xifexpression = null;
-      boolean _notEquals = (!Objects.equal(superResource, null));
+    if (loadOnDemand) {
+      final Resource resource = super.getResource(uri, false);
+      boolean _notEquals = (!Objects.equal(resource, null));
       if (_notEquals) {
-        _xifexpression = superResource;
-      } else {
-        Resource _xblockexpression_1 = null;
-        {
-          StringConcatenation _builder = new StringConcatenation();
-          _builder.append("Failed to resolve a resource by URI: \'");
-          _builder.append(uri, "");
-          _builder.append("\'; trying to load it by the URI\'s last segment: \'");
-          String _lastSegment = null;
-          if (uri!=null) {
-            _lastSegment=uri.lastSegment();
-          }
-          _builder.append(_lastSegment, "");
-          _builder.append("\'.");
-          StandaloneResourceSet.LOG.warn(_builder);
-          _xblockexpression_1 = (this.getResourceByLastSegment(uri));
+        boolean _isLoaded = resource.isLoaded();
+        boolean _not = (!_isLoaded);
+        if (_not) {
+          this.demandLoadHelper(resource);
         }
-        _xifexpression = _xblockexpression_1;
+        return resource;
       }
-      _xblockexpression = (_xifexpression);
+      final Resource resourceWithSameLastSegment = this.getResourceByLastSegment(uri);
+      boolean _notEquals_1 = (!Objects.equal(resourceWithSameLastSegment, null));
+      if (_notEquals_1) {
+        boolean _isLoaded_1 = resourceWithSameLastSegment.isLoaded();
+        boolean _not_1 = (!_isLoaded_1);
+        if (_not_1) {
+          this.demandLoadHelper(resourceWithSameLastSegment);
+        }
+        StringConcatenation _builder = new StringConcatenation();
+        _builder.append("Failed to resolve a resource by URI: \'");
+        _builder.append(uri, "");
+        _builder.append("\'; Using similar URI instead: \'");
+        URI _uRI = resourceWithSameLastSegment.getURI();
+        _builder.append(_uRI, "");
+        _builder.append("\'.");
+        StandaloneResourceSet.LOG.warn(_builder);
+        return resourceWithSameLastSegment;
+      }
+      return super.getResource(uri, true);
+    } else {
+      final Resource superResource = super.getResource(uri, false);
+      boolean _notEquals_2 = (!Objects.equal(superResource, null));
+      if (_notEquals_2) {
+        return superResource;
+      } else {
+        final Resource resourceWithSameLastSegment_1 = this.getResourceByLastSegment(uri);
+        boolean _notEquals_3 = (!Objects.equal(resourceWithSameLastSegment_1, null));
+        if (_notEquals_3) {
+          StringConcatenation _builder_1 = new StringConcatenation();
+          _builder_1.append("Failed to resolve a resource by URI: \'");
+          _builder_1.append(uri, "");
+          _builder_1.append("\'; Using similar URI instead: \'");
+          URI _uRI_1 = resourceWithSameLastSegment_1.getURI();
+          _builder_1.append(_uRI_1, "");
+          _builder_1.append("\'.");
+          StandaloneResourceSet.LOG.warn(_builder_1);
+        }
+        return resourceWithSameLastSegment_1;
+      }
     }
-    return _xblockexpression;
   }
   
   public Resource getResourceByLastSegment(final URI uri) {
@@ -74,12 +100,13 @@ public class StandaloneResourceSet extends XtextResourceSet {
         return Boolean.valueOf(Objects.equal(lastSegment, _lastSegment));
       }
     };
-    final Iterable<Resource> resources = IterableExtensions.<Resource>filter(this.resources, _function);
-    boolean _isEmpty = IterableExtensions.isEmpty(resources);
+    Iterable<Resource> _filter = IterableExtensions.<Resource>filter(this.resources, _function);
+    final List<Resource> resources = IterableExtensions.<Resource>toList(_filter);
+    boolean _isEmpty = resources.isEmpty();
     if (_isEmpty) {
       return null;
     }
-    int _size = IterableExtensions.size(resources);
+    int _size = resources.size();
     boolean _greaterThan = (_size > 1);
     if (_greaterThan) {
       StringConcatenation _builder = new StringConcatenation();
@@ -96,7 +123,7 @@ public class StandaloneResourceSet extends XtextResourceSet {
             return it.getURI();
           }
         };
-        Iterable<URI> _map = IterableExtensions.<Resource, URI>map(resources, _function_1);
+        List<URI> _map = ListExtensions.<Resource, URI>map(resources, _function_1);
         boolean _hasElements = false;
         for(final URI resourceURI : _map) {
           if (!_hasElements) {
@@ -110,8 +137,7 @@ public class StandaloneResourceSet extends XtextResourceSet {
         }
       }
       _builder.append(".");
-      StandaloneResourceSet.LOG.error(_builder);
-      return null;
+      throw new IllegalArgumentException(_builder.toString());
     }
     return IterableExtensions.<Resource>head(resources);
   }
