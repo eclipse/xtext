a395203e1a667d3467d6f25f6e58a770221edff2
akosyakov <anton.kosyakov@itemis.de>
2014-02-20 09:55:55 +0100

    [bug 428359]: Regression: switch over enum breaks on null values
    Signed-off-by: akosyakov <anton.kosyakov@itemis.de>

--- plugins/org.eclipse.xtext.xbase.junit/src/org/eclipse/xtext/xbase/junit/evaluation/AbstractXbaseEvaluationTest.java
+++ plugins/org.eclipse.xtext.xbase.junit/src/org/eclipse/xtext/xbase/junit/evaluation/AbstractXbaseEvaluationTest.java
@@ -2407,6 +2407,114 @@ public abstract class AbstractXbaseEvaluationTest extends Assert {
 	@Test public void testSwitchExpression_55() throws Exception {
 		assertEvaluatesTo(null, "{ switch (Thread.State x : Thread.State.NEW) { default: { } } }");
 	}
+
+	@Test public void testSwitchExpressionOverEnum() throws Exception {
+		assertEvaluatesTo(1, 
+				"{\n" +
+					"val java.lang.Thread.State x = null\n" +
+					"var result = 1\n" +
+					"switch x { case NEW: { result = 2 } }\n" +
+					"result\n" +
+				" }");
+	}
+	
+	@Test public void testSwitchExpressionOverEnum_2() throws Exception {
+		assertEvaluatesTo(2, 
+				"{\n" +
+					"val java.lang.Thread.State x = Thread.State.NEW\n" +
+					"var result = 1\n" +
+					"switch x { case NEW: { result = 2 } }\n" +
+					"result\n" +
+				" }");
+	}
+	
+	@Test public void testSwitchExpressionOverEnum_3() throws Exception {
+		assertEvaluatesTo(2, 
+				"{\n" +
+					"val java.lang.Thread.State x = null\n" +
+					"var result = 1\n" +
+					"switch x { case NEW: { result = 3 } default: { result = 2 } }\n" +
+					"result\n" +
+				" }");
+	}
+	
+	@Test public void testSwitchExpressionOverEnum_4() throws Exception {
+		assertEvaluatesTo(3, 
+				"{\n" +
+					"val java.lang.Thread.State x = Thread.State.NEW\n" +
+					"var result = 1\n" +
+					"switch x { case NEW: { result = 3 } default: { result = 2 } }\n" +
+					"result\n" +
+				" }");
+	}
+	
+	@Test public void testSwitchExpressionOverEnum_5() throws Exception {
+		assertEvaluatesTo(2, 
+				"{\n" +
+					"val java.lang.Thread.State x = Thread.State.RUNNABLE\n" +
+					"var result = 1\n" +
+					"switch x { case NEW: { result = 3 } default: { result = 2 } }\n" +
+					"result\n" +
+				" }");
+	}
+	
+	@Test public void testSwitchExpressionOverInteger() throws Exception {
+		assertEvaluatesTo(1, 
+				"{\n" +
+					"val Integer x = null\n" +
+					"var result = 1\n" +
+					"switch x { case 3: { result = 2 } }\n" +
+					"result\n" +
+				" }");
+	}
+	
+	@Test public void testSwitchExpressionOveInteger_2() throws Exception {
+		assertEvaluatesTo(2, 
+				"{\n" +
+					"val Integer x = Integer.valueOf('3')\n" +
+					"var result = 1\n" +
+					"switch x { case 3: { result = 2 } }\n" +
+					"result\n" +
+				" }");
+	}
+	
+	@Test public void testSwitchExpressionOverInteger_3() throws Exception {
+		assertEvaluatesTo(2, 
+				"{\n" +
+					"val Integer x = null\n" +
+					"var result = 1\n" +
+					"switch x { case 3: { result = 3 } default: { result = 2 } }\n" +
+					"result\n" +
+				" }");
+	}
+	
+	@Test public void testSwitchExpressionOveInteger_4() throws Exception {
+		assertEvaluatesTo(3, 
+				"{\n" +
+					"val Integer x = Integer.valueOf('3')\n" +
+					"var result = 1\n" +
+					"switch x { case 3: { result = 3 } default: { result = 2 } }\n" +
+					"result\n" +
+				" }");
+	}
+	
+	@Test public void testSwitchExpressionOveInteger_5() throws Exception {
+		assertEvaluatesTo(2, 
+				"{\n" +
+					"val Integer x = Integer.valueOf('4')\n" +
+					"var result = 1\n" +
+					"switch x { case 3: { result = 3 } default: { result = 2 } }\n" +
+					"result\n" +
+				" }");
+	}
+	
+	@Test public void testSwitchExpressionOveInteger_6() throws Exception {
+		assertEvaluatesTo("bar", 
+				"{\n" +
+					"val Integer x = Integer.valueOf('4')\n" +
+					"switch x { case 3: { 'foo' } default: if ('x'.length == 1) { 'bar' } }\n" +
+				" }");
+	}
 	
 	@Test public void testCastedExpression_01() throws Exception {
 		assertEvaluatesTo("literal", "'literal' as String");
