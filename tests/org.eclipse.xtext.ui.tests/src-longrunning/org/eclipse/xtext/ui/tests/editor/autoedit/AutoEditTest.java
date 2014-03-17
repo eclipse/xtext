b94606a15898f9d09b3f238fed2cd6271957b80e
Stefan Oehme <stefan.oehme@itemis.de>
2014-03-17 11:22:30 +0100

    fixed new-line related test failures on windows fixed performance test (don't use currentTimeMillis for measurements, it is too inaccurate)
    Change-Id: Ib8018159bd078d9af2d76c17f14cdea22c5f25be
    Signed-off-by: Stefan Oehme <stefan.oehme@itemis.de>

--- tests/org.eclipse.xtext.ui.tests/src-longrunning/org/eclipse/xtext/ui/tests/editor/autoedit/AutoEditTest.java
+++ tests/org.eclipse.xtext.ui.tests/src-longrunning/org/eclipse/xtext/ui/tests/editor/autoedit/AutoEditTest.java
@@ -7,15 +7,31 @@
  *******************************************************************************/
 package org.eclipse.xtext.ui.tests.editor.autoedit;
 
+import java.util.Collections;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.swt.SWT;
 import org.eclipse.xtext.junit4.ui.AbstractCStyleLanguageAutoEditTest;
+import org.eclipse.xtext.junit4.ui.util.JavaProjectSetupUtil;
+import org.eclipse.xtext.ui.XtextProjectHelper;
 import org.eclipse.xtext.ui.editor.XtextEditor;
+import org.eclipse.xtext.ui.util.PluginProjectFactory;
 import org.junit.Test;
+
+import com.google.inject.Injector;
 
 /**
  * @author Sven Efftinge - Initial contribution and API
  */
 public class AutoEditTest extends AbstractCStyleLanguageAutoEditTest {
+	
+	private static final String TESTPROJECT_NAME = "foo";
+
+	private IProject autoEditTestProject;
 
 	@Override
 	protected String getEditorId() {
@@ -52,6 +68,33 @@ public class AutoEditTest extends AbstractCStyleLanguageAutoEditTest {
 		XtextEditor editor = openEditor("{|\n}'foo'");
 		pressKey(editor, '\n');
 		assertState("{\n\t|\n}'foo'", editor);
+	}
+	
+	@Override
+	protected XtextEditor openEditor(IFile file) throws Exception {
+		JavaProjectSetupUtil.setUnixLineEndings(file.getProject());
+		XtextEditor editor = super.openEditor(file);
+		return editor;
+	}
+	
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		autoEditTestProject = ResourcesPlugin.getWorkspace().getRoot().getProject(TESTPROJECT_NAME);
+		if (!autoEditTestProject.exists())
+			createPluginProject(TESTPROJECT_NAME);
+	}
+
+	protected IProject createPluginProject(String name) throws CoreException {
+		Injector injector = org.eclipse.xtext.ui.tests.Activator.getInstance().getInjector("org.eclipse.xtext.ui.tests.editor.bracketmatching.BmTestLanguage");
+		PluginProjectFactory projectFactory = injector.getInstance(PluginProjectFactory.class);
+		projectFactory.setProjectName(name);
+		projectFactory.addFolders(Collections.singletonList("src"));
+		projectFactory.addBuilderIds(XtextProjectHelper.BUILDER_ID);
+		projectFactory.addProjectNatures(XtextProjectHelper.NATURE_ID);
+		IProject result = projectFactory.createProject(new NullProgressMonitor(), null);
+		JavaProjectSetupUtil.setUnixLineEndings(result);
+		return result;
 	}
 
 }
