b76b49b7fabd332b5d7db7510506fd3e68d23aa3
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2014-04-09 00:11:52 +0200

    [xtext][contentAssist] Fixed regression
    Off by one, damnit.

--- plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/contentassist/antlr/internal/AbstractInternalContentAssistParser.java
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/contentassist/antlr/internal/AbstractInternalContentAssistParser.java
@@ -291,7 +291,7 @@ public abstract class AbstractInternalContentAssistParser extends Parser impleme
 		if (mismatch) {
 			delegate = createMismatchStrategy();
 		} else if (!state.errorRecovery) {
-			if (marked >= 0 && state.syntaxErrors > 0 && state.lastErrorIndex >= firstMarker) {
+			if (marked > 0 && state.syntaxErrors > 0 && state.lastErrorIndex >= firstMarker) {
 				delegate = createNoOpStrategy();
 				return;
 			} else {
