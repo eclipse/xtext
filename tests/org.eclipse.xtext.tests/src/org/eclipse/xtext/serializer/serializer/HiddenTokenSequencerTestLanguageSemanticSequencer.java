cd97421f2df462261f67ae2643abf777df3262ee
Stefan Oehme <stefan.oehme@itemis.de>
2014-04-14 13:56:07 +0200

    [424027] keep comments and whitespace when serializing models with semantic changes
    Change-Id: I30bf0930e72091b40d660b671b7c641fa3fc081c
    Signed-off-by: Stefan Oehme <stefan.oehme@itemis.de>

---
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/serializer/serializer/HiddenTokenSequencerTestLanguageSemanticSequencer.java
@@ -0,0 +1,4 @@
+package org.eclipse.xtext.serializer.serializer;
+
+public class HiddenTokenSequencerTestLanguageSemanticSequencer extends AbstractHiddenTokenSequencerTestLanguageSemanticSequencer {
+}
