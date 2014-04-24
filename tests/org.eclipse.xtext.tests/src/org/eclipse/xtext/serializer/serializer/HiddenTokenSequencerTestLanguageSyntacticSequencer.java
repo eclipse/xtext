8edbb539560a4cbbb5d4bded77d883ddea888d0f
Stefan Oehme <stefan.oehme@itemis.de>
2014-04-24 08:33:38 +0200

    [427449] fixed state explosion in NfaToProduction added two patterns that merge states which where too eagerly consumed
    Change-Id: Iee00bd96ff7326c4df7f84ada1be060bba2e6266
    Signed-off-by: Stefan Oehme <stefan.oehme@itemis.de>

--- tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/serializer/serializer/HiddenTokenSequencerTestLanguageSyntacticSequencer.java
+++
@@ -1,4 +0,0 @@
-package org.eclipse.xtext.serializer.serializer;
-
-public class HiddenTokenSequencerTestLanguageSyntacticSequencer extends AbstractHiddenTokenSequencerTestLanguageSyntacticSequencer {
-}
