1b02de3c29f52faa2414407675417b9540321430
Stefan Oehme <stefan.oehme@itemis.de>
2014-05-14 13:18:59 +0200

    corrected xtype base class names

---
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/scoping/XtypeScopeProvider.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2014 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.xbase.scoping;
+
+import org.eclipse.xtext.scoping.impl.AbstractDeclarativeScopeProvider;
+
+/**
+ * @author Stefan Oehme - Initial contribution and API
+ */
+public class XtypeScopeProvider extends AbstractDeclarativeScopeProvider {
+
+}
