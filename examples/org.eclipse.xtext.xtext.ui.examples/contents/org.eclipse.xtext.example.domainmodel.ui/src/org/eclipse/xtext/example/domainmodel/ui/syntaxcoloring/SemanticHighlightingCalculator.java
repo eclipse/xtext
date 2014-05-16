74bc2a82a6120ba94508c6c89d0d3cd1fe4fb180
Stefan Oehme <stefan.oehme@itemis.de>
2014-05-16 13:46:28 +0200

    moved convenience methods from XbaseHighlightingCalculator to the new superclass
    Change-Id: Iaea278b3c5d483fa0a0fbf6d25c51ee8403e3447

--- examples/org.eclipse.xtext.xtext.ui.examples/contents/org.eclipse.xtext.example.domainmodel.ui/src/org/eclipse/xtext/example/domainmodel/ui/syntaxcoloring/SemanticHighlightingCalculator.java
+++ examples/org.eclipse.xtext.xtext.ui.examples/contents/org.eclipse.xtext.example.domainmodel.ui/src/org/eclipse/xtext/example/domainmodel/ui/syntaxcoloring/SemanticHighlightingCalculator.java
@@ -11,39 +11,23 @@ package org.eclipse.xtext.example.domainmodel.ui.syntaxcoloring;
 import java.util.Iterator;
 
 import org.eclipse.xtext.CrossReference;
-import org.eclipse.xtext.nodemodel.ILeafNode;
 import org.eclipse.xtext.nodemodel.INode;
 import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.DefaultSemanticHighlightingCalculator;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightedPositionAcceptor;
-import org.eclipse.xtext.ui.editor.syntaxcoloring.ISemanticHighlightingCalculator;
 
-public class SemanticHighlightingCalculator implements ISemanticHighlightingCalculator {
+public class SemanticHighlightingCalculator extends DefaultSemanticHighlightingCalculator {
 	
-	public void provideHighlightingFor(final XtextResource resource, IHighlightedPositionAcceptor acceptor) {
-		if (resource == null)
-			return;
-		
+	@Override
+	public void doProvideHighlightingFor(final XtextResource resource, IHighlightedPositionAcceptor acceptor) {
 		Iterator<INode> allNodes = resource.getParseResult().getRootNode().getAsTreeIterable().iterator();
 		while(allNodes.hasNext()) {
 			INode node = allNodes.next();
 			if (node.getGrammarElement() instanceof CrossReference) {
-				highlightNode(node, SemanticHighlightingConfiguration.CROSS_REF, acceptor);
+				highlightNode(acceptor, node, SemanticHighlightingConfiguration.CROSS_REF);
 			}
 		}
-	}
-	
-	private void highlightNode(INode node, String id, IHighlightedPositionAcceptor acceptor) {
-		if (node == null)
-			return;
-		if (node instanceof ILeafNode) {
-			acceptor.addPosition(node.getOffset(), node.getLength(), id);
-		} else {
-			for(ILeafNode leaf: node.getLeafNodes()) {
-				if (!leaf.isHidden()) {
-					acceptor.addPosition(leaf.getOffset(), leaf.getLength(), id);
-				}
-			}
-		}
+		super.doProvideHighlightingFor(resource, acceptor);
 	}
 
 }
