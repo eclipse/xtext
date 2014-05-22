04586731b1ea3c368c0c0dd2b961fad55ed0f407
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2014-05-22 09:12:09 +0200

    [contentAssist] Updated outdated documentation comment

--- tests/org.eclipse.xtext.common.types.tests/src/org/eclipse/xtext/common/types/xtext/ui/ui/contentassist/ContentAssistTestLanguageProposalProvider.java
+++ tests/org.eclipse.xtext.common.types.tests/src/org/eclipse/xtext/common/types/xtext/ui/ui/contentassist/ContentAssistTestLanguageProposalProvider.java
@@ -17,7 +17,7 @@ import org.eclipse.xtext.ui.editor.contentassist.ICompletionProposalAcceptor;
 
 import com.google.inject.Inject;
 /**
- * see http://www.eclipse.org/Xtext/documentation/latest/xtext.html#contentAssist on how to customize content assistant
+ * see http://www.eclipse.org/Xtext/documentation.html#contentAssist on how to customize content assistant
  */
 public class ContentAssistTestLanguageProposalProvider extends AbstractContentAssistTestLanguageProposalProvider {
 
