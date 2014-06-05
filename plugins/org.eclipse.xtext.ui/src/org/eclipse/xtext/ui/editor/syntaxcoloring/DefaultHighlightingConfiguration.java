b9568e5228eb72c33e53ef2aee781108a7d16e6d
Jan Koehnlein <jan.koehnlein@itemis.de>
2014-06-05 14:54:54 +0200

    [syntaxighlighting] don't use black as default foreground color

--- plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/syntaxcoloring/DefaultHighlightingConfiguration.java
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/syntaxcoloring/DefaultHighlightingConfiguration.java
@@ -43,7 +43,7 @@ public class DefaultHighlightingConfiguration implements IHighlightingConfigurat
 	public TextStyle defaultTextStyle() {
 		TextStyle textStyle = new TextStyle();
 	//textStyle.setBackgroundColor(new RGB(255, 255, 255));
-		textStyle.setColor(new RGB(0, 0, 0));
+	//	textStyle.setColor(new RGB(0, 0, 0));
 		return textStyle;
 	}
 	
