241bc00af2ea0831c5cd8d287aec8e50677fd997
Stefan Oehme <stefan.oehme@itemis.de>
2014-06-20 08:29:21 +0200

    fixed and simplified handling of project-relative paths in m2e adapters only overwrite settings via m2e if we actually have a custom value for them

--- plugins/org.eclipse.xtend.m2e/src/org/eclipse/xtend/m2e/XtendProjectConfigurator.java
+++ plugins/org.eclipse.xtend.m2e/src/org/eclipse/xtend/m2e/XtendProjectConfigurator.java
@@ -2,26 +2,13 @@ package org.eclipse.xtend.m2e;
 
 import static org.eclipse.xtext.builder.EclipseOutputConfigurationProvider.HIDE_LOCAL_SYNTHETIC_VARIABLES;
 import static org.eclipse.xtext.builder.EclipseOutputConfigurationProvider.INSTALL_DSL_AS_PRIMARY_SOURCE;
-import static org.eclipse.xtext.builder.EclipseOutputConfigurationProvider.OUTPUT_CLEANUP_DERIVED;
-import static org.eclipse.xtext.builder.EclipseOutputConfigurationProvider.OUTPUT_CLEAN_DIRECTORY;
-import static org.eclipse.xtext.builder.EclipseOutputConfigurationProvider.OUTPUT_CREATE_DIRECTORY;
-import static org.eclipse.xtext.builder.EclipseOutputConfigurationProvider.OUTPUT_DERIVED;
-import static org.eclipse.xtext.builder.EclipseOutputConfigurationProvider.OUTPUT_DESCRIPTION;
-import static org.eclipse.xtext.builder.EclipseOutputConfigurationProvider.OUTPUT_DIRECTORY;
-import static org.eclipse.xtext.builder.EclipseOutputConfigurationProvider.OUTPUT_KEEP_LOCAL_HISTORY;
-import static org.eclipse.xtext.builder.EclipseOutputConfigurationProvider.OUTPUT_NAME;
-import static org.eclipse.xtext.builder.EclipseOutputConfigurationProvider.OUTPUT_OVERRIDE;
 import static org.eclipse.xtext.builder.EclipseOutputConfigurationProvider.USE_OUTPUT_PER_SOURCE_FOLDER;
-import static org.eclipse.xtext.builder.preferences.BuilderPreferenceAccess.PREF_AUTO_BUILDING;
-import static org.eclipse.xtext.builder.preferences.BuilderPreferenceAccess.getIgnoreSourceFolderKey;
 import static org.eclipse.xtext.builder.preferences.BuilderPreferenceAccess.getKey;
 import static org.eclipse.xtext.builder.preferences.BuilderPreferenceAccess.getOutputForSourceFolderKey;
 
 import java.io.File;
-import java.io.IOException;
 
 import org.apache.maven.plugin.MojoExecution;
-import org.codehaus.plexus.util.StringUtils;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ProjectScope;
 import org.eclipse.core.runtime.CoreException;
@@ -37,6 +24,7 @@ import org.eclipse.xtext.ui.preferences.OptionsConfigurationBlock;
 import org.eclipse.xtext.util.RuntimeIOException;
 import org.osgi.service.prefs.BackingStoreException;
 
+import com.google.common.base.CharMatcher;
 import com.google.common.base.Strings;
 
 public class XtendProjectConfigurator extends AbstractProjectConfigurator {
@@ -48,7 +36,6 @@ public class XtendProjectConfigurator extends AbstractProjectConfigurator {
 
 		OutputConfiguration config = new XtendOutputConfigurationProvider()
 				.getOutputConfigurations().iterator().next();
-		config.setUseOutputPerSourceFolder(true);
 
 		for (MojoExecution execution : getMojoExecutions(request, monitor)) {
 			String goal = execution.getGoal();
@@ -73,26 +60,6 @@ public class XtendProjectConfigurator extends AbstractProjectConfigurator {
 				.getNode("org.eclipse.xtend.core.Xtend");
 		languagePreferences.putBoolean(
 				OptionsConfigurationBlock.IS_PROJECT_SPECIFIC, true);
-		languagePreferences.putBoolean(PREF_AUTO_BUILDING, true);
-		languagePreferences.put(getKey(configuration, OUTPUT_NAME),
-				configuration.getName());
-		languagePreferences.put(getKey(configuration, OUTPUT_DESCRIPTION),
-				configuration.getDescription());
-		languagePreferences.put(getKey(configuration, OUTPUT_DIRECTORY),
-				configuration.getOutputDirectory());
-		languagePreferences.putBoolean(getKey(configuration, OUTPUT_DERIVED),
-				configuration.isSetDerivedProperty());
-		languagePreferences.putBoolean(
-				getKey(configuration, OUTPUT_CREATE_DIRECTORY),
-				configuration.isCreateOutputDirectory());
-		languagePreferences.putBoolean(
-				getKey(configuration, OUTPUT_CLEAN_DIRECTORY),
-				configuration.isCanClearOutputDirectory());
-		languagePreferences.putBoolean(getKey(configuration, OUTPUT_OVERRIDE),
-				configuration.isOverrideExistingResources());
-		languagePreferences.putBoolean(
-				getKey(configuration, OUTPUT_CLEANUP_DERIVED),
-				configuration.isCleanUpDerivedResources());
 		languagePreferences.putBoolean(
 				getKey(configuration, INSTALL_DSL_AS_PRIMARY_SOURCE),
 				configuration.isInstallDslAsPrimarySource());
@@ -100,20 +67,13 @@ public class XtendProjectConfigurator extends AbstractProjectConfigurator {
 				getKey(configuration, HIDE_LOCAL_SYNTHETIC_VARIABLES),
 				configuration.isHideSyntheticLocalVariables());
 		languagePreferences.putBoolean(
-				getKey(configuration, OUTPUT_KEEP_LOCAL_HISTORY),
-				configuration.isKeepLocalHistory());
-		languagePreferences.putBoolean(
 				getKey(configuration, USE_OUTPUT_PER_SOURCE_FOLDER),
-				configuration.isUseOutputPerSourceFolder());
+				true);
 		for (SourceMapping sourceMapping : configuration.getSourceMappings()) {
 			languagePreferences.put(
 					getOutputForSourceFolderKey(configuration,
 							sourceMapping.getSourceFolder()),
 					Strings.nullToEmpty(sourceMapping.getOutputDirectory()));
-			languagePreferences.putBoolean(
-					getIgnoreSourceFolderKey(configuration,
-							sourceMapping.getSourceFolder()),
-					sourceMapping.isIgnore());
 		}
 
 		try {
@@ -153,22 +113,17 @@ public class XtendProjectConfigurator extends AbstractProjectConfigurator {
 		}
 	}
 
-	private String makeProjectRelative(String fileName,
-			ProjectConfigurationRequest request) {
-		try {
-			String baseDir = request.getMavenProject().getBasedir().getCanonicalPath();
-			File file = new File(fileName);
-			String relativePath;
-			if (file.isAbsolute()) {
-				relativePath = StringUtils.prechomp(file.getCanonicalPath(), baseDir);
-			} else {
-				relativePath = file.getPath();
-			}
-			String unixDelimited = relativePath.replaceAll("\\\\", "/");
-			return StringUtils.prechomp(unixDelimited, "/");
-		} catch (IOException e) {
-			throw new RuntimeIOException(e);
+	private String makeProjectRelative(String fileName, ProjectConfigurationRequest request) {
+		File baseDir = request.getMavenProject().getBasedir();
+		File file = new File(fileName);
+		String relativePath;
+		if (file.isAbsolute()) {
+			relativePath = baseDir.toURI().relativize(file.toURI()).getPath();
+		} else {
+			relativePath = file.getPath();
 		}
+		String unixDelimited = relativePath.replaceAll("\\\\", "/");
+		return CharMatcher.is('/').trimFrom(unixDelimited);
 	}
 
 	private void readDebugInfoConfig(OutputConfiguration config,
