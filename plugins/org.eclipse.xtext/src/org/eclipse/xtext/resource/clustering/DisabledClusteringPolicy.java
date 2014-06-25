678255969faa2ff47c41eb6e6382319c46ce5784
Holger Schill <Holger.Schill@itemis.de>
2014-06-25 16:39:13 +0200

    [standalone builder] Add support for clustering see https://bugs.eclipse.org/bugs/show_bug.cgi?id=427340
    Change-Id: I6b65260dc04b1b090de28ebeb9b58d42d7011a95

---
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/resource/clustering/DisabledClusteringPolicy.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2013 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.resource.clustering;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+
+/**
+ * A clustering policy that effectively disables the clustering.
+ * 
+ * @author Sebastian Zarnekow - Initial contribution and API
+ * @since 2.7
+ */
+public class DisabledClusteringPolicy implements IResourceClusteringPolicy  {
+
+	/**
+	 * Returns always {@code true} to continue the processing in the very same resource set.
+	 * @return {@code true}.
+	 */
+	public boolean continueProcessing(ResourceSet resourceSet, URI next, int alreadyProcessed) {
+		return true;
+	}
+
+}
