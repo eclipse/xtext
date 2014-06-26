423d8359c82bc0c0e5c1adb3c120df8ed351856e
Jan Koehnlein <jan.koehnlein@itemis.de>
2014-06-26 14:35:05 +0200

    [xbase][editor] Reconciler should infer JVM model and resolve types
    Change-Id: Ib1ea026aa0bdb9f2e545b505435811423291278c

--- plugins/org.eclipse.xtend.ide/src/org/eclipse/xtend/ide/XtendUiModule.java
+++ plugins/org.eclipse.xtend.ide/src/org/eclipse/xtend/ide/XtendUiModule.java
@@ -85,6 +85,7 @@ import org.eclipse.xtext.resource.impl.ResourceDescriptionsProvider;
 import org.eclipse.xtext.service.SingletonBinding;
 import org.eclipse.xtext.ui.LexerUIBindings;
 import org.eclipse.xtext.ui.codetemplates.ui.highlighting.TemplateBodyHighlighter;
+import org.eclipse.xtext.ui.editor.DirtyStateEditorSupport;
 import org.eclipse.xtext.ui.editor.IXtextEditorCallback;
 import org.eclipse.xtext.ui.editor.XtextEditor;
 import org.eclipse.xtext.ui.editor.XtextSourceViewer;
@@ -122,6 +123,7 @@ import org.eclipse.xtext.ui.editor.outline.impl.OutlineNodeFactory;
 import org.eclipse.xtext.ui.editor.outline.quickoutline.QuickOutlineFilterAndSorter;
 import org.eclipse.xtext.ui.editor.preferences.IPreferenceStoreAccess;
 import org.eclipse.xtext.ui.editor.preferences.IPreferenceStoreInitializer;
+import org.eclipse.xtext.ui.editor.reconciler.XtextDocumentReconcileStrategy;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.AbstractAntlrTokenToAttributeIdMapper;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfiguration;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.ISemanticHighlightingCalculator;
@@ -139,6 +141,8 @@ import org.eclipse.xtext.xbase.file.WorkspaceConfig;
 import org.eclipse.xtext.xbase.formatting.IFormattingPreferenceValuesProvider;
 import org.eclipse.xtext.xbase.ui.contentassist.ParameterContextInformationProvider;
 import org.eclipse.xtext.xbase.ui.contentassist.XbaseContentProposalPriorities;
+import org.eclipse.xtext.xbase.ui.editor.XbaseDirtyStateEditorSupport;
+import org.eclipse.xtext.xbase.ui.editor.XbaseDocumentReconcileStrategy;
 import org.eclipse.xtext.xbase.ui.editor.XbaseEditor;
 import org.eclipse.xtext.xbase.ui.editor.XbaseResourceForEditorInputFactory;
 import org.eclipse.xtext.xbase.ui.file.EclipseFileSystemSupportImpl;
@@ -511,5 +515,13 @@ public class XtendUiModule extends org.eclipse.xtend.ide.AbstractXtendUiModule {
 	
 	public Class<? extends OutlineNodeFactory> bindOutlineNodeFactory() {
 		return XtendOutlineNodeFactory.class;
+	}
+	
+	public Class<? extends DirtyStateEditorSupport> bindDirtyStateEditorSupport() {
+		return XbaseDirtyStateEditorSupport.class;
+	}
+	
+	public Class<? extends XtextDocumentReconcileStrategy> bindXtextDocumentReconcileStrategy() {
+		return XbaseDocumentReconcileStrategy.class;
 	}
 }
