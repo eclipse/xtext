ecd862d822b392a18b13e1ab856a51d024c5cb66
Stefan Oehme <stefan.oehme@itemis.de>
2014-07-07 14:45:14 +0200

    added a test case for creating jvm associations in active annotations made Tracability API a bit easier to use added more documentation to Associator

--- plugins/org.eclipse.xtend.lib.macro/src/org/eclipse/xtend/lib/macro/services/Associator.java
+++ plugins/org.eclipse.xtend.lib.macro/src/org/eclipse/xtend/lib/macro/services/Associator.java
@@ -10,10 +10,30 @@ import org.eclipse.xtend.lib.macro.declaration.NamedElement;
  * @see Tracability
  */
 public interface Associator {
+	
 	/**
-	 * @param generatedElement the generated Java element
-	 * @param sourceElement the source Xtend element
-	 * @throws IllegalArgumentException if generatedElement is note a Java element or sourceElement is not an Xtend element 
+	 * Associates the generate element with the Xtend source code. Every generated element should have
+	 * a source element. This information is used by features like "Go to Declaration" and the outline view.
+	 * A typical use case (generating a getter for a field) would look like this:
+	 * 
+	 * <pre>
+	 * myClass.declaredFields.forEach[field|
+	 *  myClass.addMethod("get" + field.simpleName.toFirstUpper) [
+	 *   primarySourceElement = field
+	 *   // return type, body etc...
+	 *  ]
+	 * ]
+	 * </pre>
+	 * 
+	 * This way, the getter generated in the active annotation above would be associated with the field in the Xtend source code.
+	 * Notice that we passed the field from the Java AST as a source element. This is a shortcut for
+	 * 
+	 * <pre>
+	 * primarySourceElement = field.primarySourceElement
+	 * </pre>
+	 * 
+	 * @param primaryElement the source element that the secondaryElement was derived from
+	 * @param secondaryElement the generated Java element 
 	 */
-	void setPrimarySourceElement(MutableNamedElement generatedElement, NamedElement sourceElement);
+	void setPrimarySourceElement(MutableNamedElement primaryElement, NamedElement secondaryElement);
 }
