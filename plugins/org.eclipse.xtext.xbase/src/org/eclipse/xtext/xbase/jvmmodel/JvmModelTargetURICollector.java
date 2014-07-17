42bf0cf15e327300f331bfed695328e5a10b6e15
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2014-07-17 08:34:31 +0200

    [findReferences] Runtime API for find references
    see https://bugs.eclipse.org/bugs/show_bug.cgi?id=440021
    
    Change-Id: I474ca0383a8f91b97ca1dbfe23abd6b1657a79ca

---
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/jvmmodel/JvmModelTargetURICollector.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2014 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.xbase.jvmmodel;
+
+import java.util.Set;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.findReferences.TargetURICollector;
+import org.eclipse.xtext.findReferences.TargetURIs;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+/**
+ * @author Sebastian Zarnekow - Initial contribution and API
+ */
+@Singleton
+public class JvmModelTargetURICollector extends TargetURICollector {
+
+	@Inject
+	private IJvmModelAssociations associations;
+
+	@Override
+	protected void doAdd(EObject object, TargetURIs targetURIs) {
+		super.doAdd(object, targetURIs);
+		Set<EObject> jvmElements = associations.getJvmElements(object);
+		for(EObject jvmElement: jvmElements) {
+			super.doAdd(jvmElement, targetURIs);
+		}
+	}
+	
+}
