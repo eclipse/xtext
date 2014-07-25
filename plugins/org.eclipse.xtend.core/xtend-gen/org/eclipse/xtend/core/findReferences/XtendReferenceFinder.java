62e6a6eb8ac17be977aececf1cab00a4d2b54e23
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2014-07-25 09:30:46 +0200

    [xtend][findRefs] Fixed copy'n'paste error
    This should fix the broken rename refactoring tests
    
    Change-Id: Id77697aa8bb61a3f792b962f703b36e58805959f

--- plugins/org.eclipse.xtend.core/xtend-gen/org/eclipse/xtend/core/findReferences/XtendReferenceFinder.java
+++ plugins/org.eclipse.xtend.core/xtend-gen/org/eclipse/xtend/core/findReferences/XtendReferenceFinder.java
@@ -63,7 +63,7 @@ public class XtendReferenceFinder extends ReferenceFinder {
   @Inject
   private Declarators declarators;
   
-  public void findReferences(final TargetURIs targetURIs, final IResourceDescription resourceDescription, final IReferenceFinder.IResourceAccess resourceAccess, final IReferenceFinder.Acceptor acceptor, final IProgressMonitor monitor) {
+  public void findReferencesInDescription(final TargetURIs targetURIs, final IResourceDescription resourceDescription, final IReferenceFinder.IResourceAccess resourceAccess, final IReferenceFinder.Acceptor acceptor, final IProgressMonitor monitor) {
     Declarators.DeclaratorsData _declaratorData = this.declarators.getDeclaratorData(targetURIs, resourceAccess);
     final Set<QualifiedName> names = _declaratorData.getDeclaratorNames();
     boolean _isCanceled = monitor.isCanceled();
