63b865147a1f45ee58f866ef806f41f4f15ce796
Stefan Oehme <stefan.oehme@itemis.de>
2014-07-25 10:20:19 +0200

    added javadoc which modifiers can be expected on which declarations Change-Id: Ieaee5c14d3f1f02b1aac689a533698640a42a62b

--- plugins/org.eclipse.xtend.lib.macro/src/org/eclipse/xtend/lib/macro/declaration/Modifier.java
+++ plugins/org.eclipse.xtend.lib.macro/src/org/eclipse/xtend/lib/macro/declaration/Modifier.java
@@ -5,21 +5,87 @@ package org.eclipse.xtend.lib.macro.declaration;
  * Xtend source code
  */
 public enum Modifier {
+	/**
+	 * The public visibility modifier, can be present on
+	 * {@link FieldDeclaration fields}, {@link TypeDeclaration types} and
+	 * {@link ExecutableDeclaration executables}
+	 */
 	PUBLIC,
+	/**
+	 * The private visibility modifier, can be present on
+	 * {@link FieldDeclaration fields}, {@link TypeDeclaration types} and
+	 * {@link ExecutableDeclaration executables}
+	 */
 	PRIVATE,
+	/**
+	 * The protected visibility modifier, can be present on
+	 * {@link FieldDeclaration fields}, {@link TypeDeclaration types} and
+	 * {@link ExecutableDeclaration executables}
+	 */
 	PROTECTED,
+	/**
+	 * The package private visibility modifier, can be present on
+	 * {@link FieldDeclaration fields}, {@link TypeDeclaration types} and
+	 * {@link ExecutableDeclaration executables}
+	 */
 	PACKAGE,
+	/**
+	 * The abstract modifier, can be present on {@link ClassDeclaration classes}
+	 * and {@link MethodDeclaration methods}
+	 */
 	ABSTRACT,
+	/**
+	 * The static modifier, can be present on {@link TypeDeclaration types},
+	 * {@link FieldDeclaration fields} and {@link MethodDeclaration methods}
+	 */
 	STATIC,
+	/**
+	 * The dispatch modifier, can be present on {@link MethodDeclaration
+	 * methods}
+	 */
 	DISPATCH,
+	/**
+	 * The final modifier, can be present on {@link ClassDeclaration classes},
+	 * {@link FieldDeclaration fields} and {@link MethodDeclaration methods}
+	 */
 	FINAL,
+	/**
+	 * The strict floating point modifier, can be present on
+	 * {@link ClassDeclaration classes} and {@link MethodDeclaration methods}
+	 */
 	STRICTFP,
+	/**
+	 * The native modifier, can be present on {@link MethodDeclaration methods}
+	 */
 	NATIVE,
+	/**
+	 * The volatile modifier, can be present on {@link FieldDeclaration fields}
+	 */
 	VOLATILE,
+	/**
+	 * The synchronied modifier, can be present on {@link MethodDeclaration
+	 * methods}
+	 */
 	SYNCHRONIZED,
+	/**
+	 * The transient modifier, can be present on {@link FieldDeclaration fields}
+	 */
 	TRANSIENT,
+	/**
+	 * The val modifier, can be present on {@link FieldDeclaration fields}
+	 */
 	VAL,
+	/**
+	 * The var modifier, can be present on {@link FieldDeclaration fields}
+	 */
 	VAR,
+	/**
+	 * The def modifier, can be present on {@link MethodDeclaration methods}
+	 */
 	DEF,
+	/**
+	 * The override modifier, can be present on {@link MethodDeclaration
+	 * methods}
+	 */
 	OVERRIDE
 }
