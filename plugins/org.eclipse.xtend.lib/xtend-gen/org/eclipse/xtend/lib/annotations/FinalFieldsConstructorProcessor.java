fd3c14ac3793c8c7c2cb87a067521e0096c15d36
Stefan Oehme <stefan.oehme@itemis.de>
2014-08-05 14:28:20 +0200

    guard stock AAs against null types
    e.g. @Accessors class Foo {val foo}

--- plugins/org.eclipse.xtend.lib/xtend-gen/org/eclipse/xtend/lib/annotations/FinalFieldsConstructorProcessor.java
+++ plugins/org.eclipse.xtend.lib/xtend-gen/org/eclipse/xtend/lib/annotations/FinalFieldsConstructorProcessor.java
@@ -254,7 +254,8 @@ public class FinalFieldsConstructorProcessor implements TransformationParticipan
           p.markAsInitializedBy(it);
           String _simpleName = p.getSimpleName();
           TypeReference _type = p.getType();
-          final MutableParameterDeclaration param = it.addParameter(_simpleName, _type);
+          TypeReference _orVoid = Util.this.orVoid(_type);
+          final MutableParameterDeclaration param = it.addParameter(_simpleName, _orVoid);
           fieldToParameter.put(p, param);
         }
       };
@@ -307,6 +308,17 @@ public class FinalFieldsConstructorProcessor implements TransformationParticipan
       } else {
         return null;
       }
+    }
+    
+    private TypeReference orVoid(final TypeReference ref) {
+      TypeReference _xifexpression = null;
+      boolean _tripleEquals = (ref == null);
+      if (_tripleEquals) {
+        _xifexpression = this.context.getPrimitiveVoid();
+      } else {
+        _xifexpression = ref;
+      }
+      return _xifexpression;
     }
   }
   
