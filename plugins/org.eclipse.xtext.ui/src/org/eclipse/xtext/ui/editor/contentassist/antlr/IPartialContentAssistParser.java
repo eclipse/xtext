98c3c10a6d6570944eceb67b29e6f84b0ff308e9
Stefan Oehme <stefan.oehme@itemis.de>
2014-08-05 11:37:37 +0200

    generalized partial content assist parsing from Xtend
    Change-Id: I8517aee7b2527933526748a3d28c15c900062f24

---
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/contentassist/antlr/IPartialContentAssistParser.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2014 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.ui.editor.contentassist.antlr;
+
+import java.util.Collection;
+
+import org.eclipse.xtext.parser.IParseResult;
+
+/**
+ * A content assist parser that can skip parts of the document for faster parsing
+ * @since 2.7
+ */
+public interface IPartialContentAssistParser extends IContentAssistParser {
+
+	/**
+	 * @param strict if <code>true</code>
+	 * the parser will not use error recovery on the very last token of the input.
+	 */
+	public Collection<FollowElement> getFollowElements(IParseResult parseResult, int offset, boolean strict);
+
+}
--- plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/contentassist/antlr/ParserBasedContentAssistContextFactory.java
+++ plugins/org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/contentassist/antlr/ParserBasedContentAssistContextFactory.java
@@ -708,6 +708,62 @@ public class ParserBasedContentAssistContextFactory extends AbstractContentAssis
 			return result;
 		}
 	}
+	
+	/**
+	 * @since 2.7
+	 */
+	public static class PartialStatefulFactory extends StatefulFactory {
+		@Override
+		protected void createContextsForLastCompleteNode(EObject previousModel, boolean strict)
+				throws BadLocationException {
+			String currentNodePrefix = getPrefix(currentNode);
+			if (!Strings.isEmpty(currentNodePrefix) && !currentNode.getText().equals(currentNodePrefix)) {
+				lexer.setCharStream(new ANTLRStringStream(currentNodePrefix));
+				Token token = lexer.nextToken();
+				if (token == Token.EOF_TOKEN) {
+					return;
+				}
+				while (token != Token.EOF_TOKEN) {
+					if (isErrorToken(token)) {
+						return;
+					}
+					token = lexer.nextToken();
+				}
+			}
+			String prefix = "";
+			Collection<FollowElement> followElements = parseFollowElements(completionOffset, strict);
+			doCreateContexts(lastCompleteNode, currentNode, prefix, previousModel, followElements);
+		}
+
+		protected Collection<FollowElement> parseFollowElements(int offset, boolean strict) {
+			return ((IPartialContentAssistParser) parser).getFollowElements(parseResult, offset, strict);
+		}
+
+		@Override
+		protected void handleLastCompleteNodeAsPartOfDatatypeNode() throws BadLocationException {
+			String prefix = getPrefix(datatypeNode);
+			Collection<FollowElement> followElements = parseFollowElements(datatypeNode.getOffset(), false);
+			INode lastCompleteNodeBeforeDatatype = getLastCompleteNodeByOffset(rootNode, datatypeNode.getTotalOffset());
+			doCreateContexts(lastCompleteNodeBeforeDatatype, datatypeNode, prefix, currentModel, followElements);
+		}
+
+		@Override
+		protected void handleLastCompleteNodeIsAtEndOfDatatypeNode() throws BadLocationException {
+			String prefix = getPrefix(lastCompleteNode);
+			INode previousNode = getLastCompleteNodeByOffset(rootNode, lastCompleteNode.getOffset());
+			EObject previousModel = previousNode.getSemanticElement();
+			INode currentDatatypeNode = getContainingDatatypeRuleNode(currentNode);
+			Collection<FollowElement> followElements = parseFollowElements(lastCompleteNode.getOffset(), false);
+			int prevSize = contextBuilders.size();
+			doCreateContexts(previousNode, currentDatatypeNode, prefix, previousModel, followElements);
+
+			if (lastCompleteNode instanceof ILeafNode && lastCompleteNode.getGrammarElement() == null
+					&& contextBuilders.size() != prevSize) {
+				handleLastCompleteNodeHasNoGrammarElement(contextBuilders.subList(prevSize, contextBuilders.size()),
+						previousModel);
+			}
+		}
+	}
 
 	public ContentAssistContext[] create(ITextViewer viewer, int offset, XtextResource resource) {
 		try { 
