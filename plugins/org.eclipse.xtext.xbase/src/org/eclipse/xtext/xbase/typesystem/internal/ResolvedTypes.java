eb79ddacfad7e0fdd24428f9400a3f84e2d15a57
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2014-08-08 17:05:26 +0200

    [xbase][typesystem] Further improvements to #getFlattenedReassignedTypes
    Change-Id: Id2ee9a97f0f30162bbe928761543ec4c21551084

--- plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/typesystem/internal/ResolvedTypes.java
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/typesystem/internal/ResolvedTypes.java
@@ -9,6 +9,7 @@ package org.eclipse.xtext.xbase.typesystem.internal;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -1418,7 +1419,14 @@ public abstract class ResolvedTypes implements IResolvedTypes {
 
 	/* @Nullable */
 	protected Map<JvmIdentifiableElement, LightweightTypeReference> getFlattenedReassignedTypes() {
-		return reassignedTypes == null ? null : Maps.newHashMap(reassignedTypes);
+		if (reassignedTypes == null || reassignedTypes.isEmpty()) {
+			return null;
+		}
+		if (reassignedTypes.size() == 1) {
+			Map.Entry<JvmIdentifiableElement, LightweightTypeReference> singleEntry = reassignedTypes.entrySet().iterator().next();
+			return Collections.singletonMap(singleEntry.getKey(), singleEntry.getValue());
+		}
+		return new HashMap<JvmIdentifiableElement, LightweightTypeReference>(reassignedTypes);
 	}
 	
 }
