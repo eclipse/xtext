eeef93db4c09bb3e7f3399bd0a56b8de8ba06836
Dennis Huebner <dennis.huebner@itemis.de>
2014-08-13 15:29:15 +0200

    [xtend] Show errors from derived Java files in Xtend - 2 - Bug 408083
    Change-Id: I9454b6ab37fa4d9d78f40077e0429db62729eb2e
    Signed-off-by: Dennis Huebner <dennis.huebner@itemis.de>

--- plugins/org.eclipse.xtext.xbase.ui/src/org/eclipse/xtext/xbase/ui/editor/StacktraceBasedEditorDecider.java
+++ plugins/org.eclipse.xtext.xbase.ui/src/org/eclipse/xtext/xbase/ui/editor/StacktraceBasedEditorDecider.java
@@ -8,9 +8,8 @@
 package org.eclipse.xtext.xbase.ui.editor;
 
 /**
- * Inspects the caller stack to decide whether it is ok to use an Xbase editor to 
- * edit a java file or whether the user really expects the Java editor, e.g. by selecting
- * a generated Java file explicitly in the package explorer.
+ * Inspects the caller stack to decide whether it is ok to use an Xbase editor to edit a java file or whether the user
+ * really expects the Java editor, e.g. by selecting a generated Java file explicitly in the package explorer.
  * 
  * @author Sebastian Zarnekow - Initial contribution and API
  * @noextend This class is not intended to be subclassed by clients.
@@ -20,9 +19,9 @@ package org.eclipse.xtext.xbase.ui.editor;
 public class StacktraceBasedEditorDecider {
 
 	public enum Decision {
-		FORCE_JAVA, /* FORCE_XBASE, */ FAVOR_XBASE
+		FORCE_JAVA, /* FORCE_XBASE, */FAVOR_XBASE
 	}
-	
+
 	/**
 	 * @noreference This method is not intended to be referenced by clients.
 	 * @nooverride This method is not intended to be re-implemented or extended by clients.
@@ -33,9 +32,9 @@ public class StacktraceBasedEditorDecider {
 
 	protected Decision doDecideAccordingToCaller(boolean respectPackageExplorer) {
 		StackTraceElement[] trace = Thread.currentThread().getStackTrace();
-		for(StackTraceElement element: trace) {
-//			if (isOpenTypeAction(element))
-//				return Decision.FORCE_XBASE;
+		for (StackTraceElement element : trace) {
+			//	if (isOpenTypeAction(element))
+			//	return Decision.FORCE_XBASE;
 			if (isSourceLookup(element))
 				return Decision.FORCE_JAVA;
 			if (isNavigationHistory(element))
@@ -48,10 +47,17 @@ public class StacktraceBasedEditorDecider {
 				return Decision.FORCE_JAVA;
 			if (isOpenCompareEditor(element))
 				return Decision.FORCE_JAVA;
+			if (isOpenSelectedMarkers(element))
+				return Decision.FORCE_JAVA;
 		}
 		return Decision.FAVOR_XBASE;
 	}
-	
+
+	protected boolean isOpenSelectedMarkers(StackTraceElement element) {
+		return "org.eclipse.ui.internal.views.markers.ExtendedMarkersView".equals(element
+				.getClassName()) && "openSelectedMarkers".equals(element.getMethodName());
+	}
+
 	/**
 	 * @noreference This method is not intended to be referenced by clients.
 	 * @nooverride This method is not intended to be re-implemented or extended by clients.
@@ -59,28 +65,29 @@ public class StacktraceBasedEditorDecider {
 	public Decision decideAccordingToCallerForSimpleFileName() {
 		return doDecideAccordingToCaller(false);
 	}
-	
+
 	protected boolean isNavigationHistory(StackTraceElement element) {
 		return "org.eclipse.ui.internal.NavigationHistory".equals(element.getClassName());
 	}
-	
+
 	protected boolean isLineBasedOpenEditorAction() {
 		StackTraceElement[] trace = new Exception().getStackTrace();
 		if (trace.length > 3) {
 			StackTraceElement element = trace[2];
-			boolean result =
-					("org.eclipse.jdt.internal.junit.ui.OpenTestAction".equals(element.getClassName()) || 
-					"org.eclipse.jdt.internal.junit.ui.OpenEditorAtLineAction".equals(element.getClassName())) && "reveal".equals(element.getMethodName());
+			boolean result = ("org.eclipse.jdt.internal.junit.ui.OpenTestAction".equals(element.getClassName()) || "org.eclipse.jdt.internal.junit.ui.OpenEditorAtLineAction"
+					.equals(element.getClassName())) && "reveal".equals(element.getMethodName());
 			if (result)
 				return result;
-			result = "org.eclipse.jdt.internal.debug.ui.console.JavaStackTraceHyperlink".equals(element.getClassName()) && "processSearchResult".equals(element.getMethodName());
+			result = "org.eclipse.jdt.internal.debug.ui.console.JavaStackTraceHyperlink".equals(element.getClassName())
+					&& "processSearchResult".equals(element.getMethodName());
 			return result;
 		}
 		return false;
 	}
 
 	/**
-	 * Happens when one select a stack element in the debugger. Since wie use JSR-45 we don't want to modify JDT's behavior.
+	 * Happens when one select a stack element in the debugger. Since wie use JSR-45 we don't want to modify JDT's
+	 * behavior.
 	 * 
 	 * @nooverride This method is not intended to be re-implemented or extended by clients.
 	 */
@@ -94,22 +101,22 @@ public class StacktraceBasedEditorDecider {
 	 */
 	public boolean isCalledFromFindReferences() {
 		StackTraceElement[] trace = new Exception().getStackTrace();
-		for(StackTraceElement element: trace) {
+		for (StackTraceElement element : trace) {
 			if (isFindReferences(element))
 				return true;
 		}
 		return false;
 	}
-	
+
 	public boolean isEditorUtilityIsOpenInEditor() {
 		StackTraceElement[] trace = new Exception().getStackTrace();
-		for(StackTraceElement element: trace) {
+		for (StackTraceElement element : trace) {
 			if (isEditorUtilityIsOpenInEditor(element))
 				return true;
 		}
 		return false;
 	}
-	
+
 	public boolean isJDI() {
 		StackTraceElement[] trace = new Exception().getStackTrace();
 		for (StackTraceElement element : trace) {
@@ -118,7 +125,7 @@ public class StacktraceBasedEditorDecider {
 		}
 		return false;
 	}
-	
+
 	public boolean isJavaStackTraceHyperlink() {
 		StackTraceElement[] trace = new Exception().getStackTrace();
 		for (StackTraceElement element : trace) {
@@ -142,7 +149,8 @@ public class StacktraceBasedEditorDecider {
 	 * @nooverride This method is not intended to be re-implemented or extended by clients.
 	 */
 	protected boolean isEditorUtilityIsOpenInEditor(StackTraceElement element) {
-		return "org.eclipse.jdt.internal.ui.javaeditor.EditorUtility".equals(element.getClassName()) && ("isOpenInEditor".equals(element.getMethodName()) );
+		return "org.eclipse.jdt.internal.ui.javaeditor.EditorUtility".equals(element.getClassName())
+				&& ("isOpenInEditor".equals(element.getMethodName()));
 	}
 
 	// org.eclipse.jdt.internal.junit.ui.OpenEditorAction.run()
@@ -151,7 +159,7 @@ public class StacktraceBasedEditorDecider {
 	// if there is no invocation
 	public boolean isOpenEditorAction() {
 		StackTraceElement[] trace = new Exception().getStackTrace();
-		for(StackTraceElement element: trace) {
+		for (StackTraceElement element : trace) {
 			if (isOpenEditorAction(element))
 				return true;
 		}
@@ -163,43 +171,50 @@ public class StacktraceBasedEditorDecider {
 	 * @nooverride This method is not intended to be re-implemented or extended by clients.
 	 */
 	protected boolean isOpenEditorAction(StackTraceElement element) {
-		return "org.eclipse.jdt.internal.junit.ui.OpenEditorAction".equals(element.getClassName()) && ("run".equals(element.getMethodName()) );
+		return "org.eclipse.jdt.internal.junit.ui.OpenEditorAction".equals(element.getClassName())
+				&& ("run".equals(element.getMethodName()));
 	}
-	
+
 	// It is currently not possible to supersede an open editor
 	// thus if a Java editor for Foo.java is open and the user wants to
 	// open Foo.xtend, the Open Type dialog will always reveal Foo.java since
 	// the Editor input for Foo.java matches a currently open editor. In other words:
 	// No new editor will be opened anyway so such a guard is pointless.
-//	protected boolean isOpenTypeAction(StackTraceElement element) {
-//		return "org.eclipse.jdt.internal.ui.actions.OpenTypeAction".equals(element.getClassName());
-//	}
-	
+	//	protected boolean isOpenTypeAction(StackTraceElement element) {
+	//		return "org.eclipse.jdt.internal.ui.actions.OpenTypeAction".equals(element.getClassName());
+	//	}
+
 	/**
 	 * @noreference This method is not intended to be referenced by clients.
 	 * @nooverride This method is not intended to be re-implemented or extended by clients.
 	 */
 	protected boolean isFindReferences(StackTraceElement element) {
-		return "org.eclipse.jdt.internal.ui.search.JavaSearchResultPage".equals(element.getClassName()) && "showMatch".equals(element.getMethodName());
+		return "org.eclipse.jdt.internal.ui.search.JavaSearchResultPage".equals(element.getClassName())
+				&& "showMatch".equals(element.getMethodName());
 	}
-	
+
 	/**
 	 * @noreference This method is not intended to be referenced by clients.
 	 * @nooverride This method is not intended to be re-implemented or extended by clients.
 	 */
 	protected boolean isPackageExplorerOrNavigator(StackTraceElement element) {
-		return "org.eclipse.jdt.internal.ui.packageview.PackageExplorerActionGroup".equals(element.getClassName()) && "handleOpen".equals(element.getMethodName()) 
-				||	"org.eclipse.ui.navigator.CommonViewer".equals(element.getClassName()) && "handleOpen".equals(element.getMethodName())
-				||	"org.eclipse.jdt.internal.ui.packageview.PackageExplorerPart".equals(element.getClassName()) && "linkToEditor".equals(element.getMethodName())
-				||	"org.eclipse.jdt.internal.ui.navigator.JavaFileLinkHelper".equals(element.getClassName()) && "activateEditor".equals(element.getMethodName());
+		return "org.eclipse.jdt.internal.ui.packageview.PackageExplorerActionGroup".equals(element.getClassName())
+				&& "handleOpen".equals(element.getMethodName())
+				|| "org.eclipse.ui.navigator.CommonViewer".equals(element.getClassName())
+				&& "handleOpen".equals(element.getMethodName())
+				|| "org.eclipse.jdt.internal.ui.packageview.PackageExplorerPart".equals(element.getClassName())
+				&& "linkToEditor".equals(element.getMethodName())
+				|| "org.eclipse.jdt.internal.ui.navigator.JavaFileLinkHelper".equals(element.getClassName())
+				&& "activateEditor".equals(element.getMethodName());
 	}
-	
+
 	/**
 	 * @noreference This method is not intended to be referenced by clients.
 	 * @nooverride This method is not intended to be re-implemented or extended by clients.
 	 */
 	protected boolean isOpenResource(StackTraceElement element) {
-		return "org.eclipse.ui.internal.ide.handlers.OpenResourceHandler".equals(element.getClassName()) && "execute".equals(element.getMethodName());
+		return "org.eclipse.ui.internal.ide.handlers.OpenResourceHandler".equals(element.getClassName())
+				&& "execute".equals(element.getMethodName());
 	}
 
 	/**
@@ -207,7 +222,8 @@ public class StacktraceBasedEditorDecider {
 	 * @nooverride This method is not intended to be re-implemented or extended by clients.
 	 */
 	protected boolean isFileSearch(StackTraceElement element) {
-		return "org.eclipse.search.internal.ui.text.FileSearchPage".equals(element.getClassName()) && "showMatch".equals(element.getMethodName());
+		return "org.eclipse.search.internal.ui.text.FileSearchPage".equals(element.getClassName())
+				&& "showMatch".equals(element.getMethodName());
 	}
 
 	/**
@@ -215,8 +231,8 @@ public class StacktraceBasedEditorDecider {
 	 * @nooverride This method is not intended to be re-implemented or extended by clients.
 	 */
 	protected boolean isOpenCompareEditor(StackTraceElement element) {
-		return "org.eclipse.compare.CompareUI".equals(element.getClassName()) && "openCompareEditor".equals(element.getMethodName());
+		return "org.eclipse.compare.CompareUI".equals(element.getClassName())
+				&& "openCompareEditor".equals(element.getMethodName());
 	}
-
 
 }
