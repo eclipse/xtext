e5a59f101af95c3528290ab8d1749a197a0f8b8f
Dennis Huebner <dennis.huebner@itemis.de>
2014-08-14 17:20:05 +0200

    [marker] Cleanup marker created with DerivedResourceMarkerCopier
    Change-Id: I914c53193b981d992746e54cb5c98c74186d9b77
    Signed-off-by: Dennis Huebner <dennis.huebner@itemis.de>

--- plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/smap/DerivedResourceMarkerCopier.java
+++ plugins/org.eclipse.xtext.builder/src/org/eclipse/xtext/builder/smap/DerivedResourceMarkerCopier.java
@@ -8,17 +8,14 @@
 package org.eclipse.xtext.builder.smap;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
 import org.eclipse.jdt.core.IJavaModelMarker;
 import org.eclipse.xtext.generator.trace.ILocationInResource;
 import org.eclipse.xtext.generator.trace.ITrace;
@@ -42,6 +39,8 @@ import com.google.inject.Inject;
  */
 public class DerivedResourceMarkerCopier {
 
+	public static final String COPIED_FROM_FILE = "fromFile";
+
 	@Inject
 	private IResourceServiceProvider.Registry serviceProviderRegistry;
 
@@ -56,16 +55,20 @@ public class DerivedResourceMarkerCopier {
 	 *            - Java file to inspect
 	 */
 	public void reflectErrorMarkerInSource(IFile javaFile) throws CoreException {
-		if (javaFile.findMaxProblemSeverity(IJavaModelMarker.JAVA_MODEL_PROBLEM_MARKER, true, IResource.DEPTH_ZERO) < IMarker.SEVERITY_ERROR) {
-			return;
-		}
 		ITrace traceToSource = traceInformation.getTraceToSource(javaFile);
 		if (traceToSource != null) {
+			IMarker[] errorsInJavaFile = javaFile.findMarkers(IJavaModelMarker.JAVA_MODEL_PROBLEM_MARKER, true,
+					IResource.DEPTH_ZERO);
+			// Any errors in target -> clean up source and return
+			if (errorsInJavaFile.length == 0) {
+				cleanUpCreatedMarkers(javaFile, traceToSource);
+				return;
+			}
+			
+			// Copy marker otherwise
 			IWorkspace workspace = javaFile.getWorkspace();
 			Set<Triple<IFile, String, ILocationInResource>> markers = new HashSet<Triple<IFile, String, ILocationInResource>>();
-
-			for (IMarker marker : javaFile.findMarkers(IJavaModelMarker.JAVA_MODEL_PROBLEM_MARKER, true,
-					IResource.DEPTH_ZERO)) {
+			for (IMarker marker : errorsInJavaFile) {
 				String message = (String) marker.getAttribute(IMarker.MESSAGE);
 				if (message == null) {
 					continue;
@@ -77,9 +80,7 @@ public class DerivedResourceMarkerCopier {
 						charStart, charEnd - charStart));
 				if (associatedLocation != null) {
 					IFile srcFile = workspace.getRoot().getFile(associatedLocation.getStorage().getFullPath());
-
-					if (srcFile.findMaxProblemSeverity(MarkerTypes.ANY_VALIDATION, true, IResource.DEPTH_ZERO) < IMarker.SEVERITY_ERROR) {
-
+					if (!hasXtextErrorMarker(srcFile)) {
 						Triple<IFile, String, ILocationInResource> markerData = Tuples.create(srcFile, message,
 								associatedLocation);
 						markers.add(markerData);
@@ -87,42 +88,54 @@ public class DerivedResourceMarkerCopier {
 				}
 			}
 			if (markers.size() > 0) {
-				createMarkers(markers, workspace);
+				createMarkers(markers, javaFile);
 			}
 		}
 	}
 
-	private void createMarkers(final Set<Triple<IFile, String, ILocationInResource>> markers, IWorkspace workspace) {
+	private boolean hasXtextErrorMarker(IFile srcFile) throws CoreException {
+		return srcFile.findMaxProblemSeverity(MarkerTypes.ANY_VALIDATION, true, IResource.DEPTH_ZERO) >= IMarker.SEVERITY_ERROR;
+	}
+
+	private void cleanUpCreatedMarkers(IFile javaFile, ITrace traceToSource) throws CoreException {
+		Iterator<ILocationInResource> iterator = traceToSource.getAllAssociatedLocations().iterator();
+		if (iterator.hasNext()) {
+			ILocationInResource srcLocation = iterator.next();
+			IWorkspace workspace = javaFile.getWorkspace();
+			IFile srcFile = workspace.getRoot().getFile(srcLocation.getStorage().getFullPath());
+			if (hasXtextErrorMarker(srcFile)) {
+				for (IMarker iMarker : srcFile.findMarkers(MarkerTypes.ANY_VALIDATION, true, IResource.DEPTH_ZERO)) {
+					if (javaFile.getFullPath().toString().equals(iMarker.getAttribute(COPIED_FROM_FILE, ""))) {
+						iMarker.delete();
+					}
+				}
+			}
+		}
+	}
+
+	private void createMarkers(final Set<Triple<IFile, String, ILocationInResource>> markers, IFile derivedResource)
+			throws CoreException {
 		final Issue.IssueImpl issue = new Issue.IssueImpl();
 		issue.setType(CheckType.NORMAL);
+		for (Triple<IFile, String, ILocationInResource> markerData : markers) {
+			ILocationInResource locationInSrc = markerData.getThird();
+			IResourceServiceProvider serviceProvider = serviceProviderRegistry.getResourceServiceProvider(locationInSrc
+					.getAbsoluteResourceURI());
+			if (serviceProvider == null)
+				continue;
 
-		WorkspaceJob job = new WorkspaceJob("Show derived issues") {
+			MarkerTypeProvider typeProvider = serviceProvider.get(MarkerTypeProvider.class);
 
-			@Override
-			public IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException {
-				for (Triple<IFile, String, ILocationInResource> markerData : markers) {
-					ILocationInResource locationInSrc = markerData.getThird();
-					IResourceServiceProvider serviceProvider = serviceProviderRegistry
-							.getResourceServiceProvider(locationInSrc.getAbsoluteResourceURI());
-					if (serviceProvider == null)
-						continue;
-
-					MarkerTypeProvider typeProvider = serviceProvider.get(MarkerTypeProvider.class);
-
-					IMarker xtendMarker = markerData.getFirst().createMarker(typeProvider.getMarkerType(issue));
-					xtendMarker.setAttribute(IMarker.MESSAGE, markerData.getSecond());
-					xtendMarker.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_ERROR);
-					ITextRegionWithLineInformation region = locationInSrc.getTextRegion();
-					xtendMarker.setAttribute(IMarker.LINE_NUMBER, region.getLineNumber());
-					xtendMarker.setAttribute(IMarker.CHAR_START, region.getOffset());
-					xtendMarker.setAttribute(IMarker.CHAR_END, region.getOffset() + region.getLength());
-					xtendMarker.setAttribute(IMarker.SOURCE_ID, "JDT");
-				}
-				return Status.OK_STATUS;
-			}
-		};
-		job.setRule(workspace.getRuleFactory().markerRule(workspace.getRoot()));
-		job.schedule();
+			IFile file = markerData.getFirst();
+			IMarker xtendMarker = file.createMarker(typeProvider.getMarkerType(issue));
+			xtendMarker.setAttribute(IMarker.MESSAGE, markerData.getSecond());
+			xtendMarker.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_ERROR);
+			ITextRegionWithLineInformation region = locationInSrc.getTextRegion();
+			xtendMarker.setAttribute(IMarker.LINE_NUMBER, region.getLineNumber());
+			xtendMarker.setAttribute(IMarker.CHAR_START, region.getOffset());
+			xtendMarker.setAttribute(IMarker.CHAR_END, region.getOffset() + region.getLength());
+			xtendMarker.setAttribute(COPIED_FROM_FILE, derivedResource.getFullPath().toString());
+		}
 	}
 
 }
