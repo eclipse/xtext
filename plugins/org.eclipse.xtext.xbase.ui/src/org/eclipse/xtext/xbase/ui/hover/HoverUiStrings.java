fa829c5d5ace5a3e9b786d14507c4317a51bf567
sgalland <galland@arakhne.org>
2014-08-19 15:43:05 +0200

    [xbase][validation] Yield a warning when referencing @Deprecated elements.
    Warnings won't be issued if the referenced elements is from the same resource
    or if the piece of code that uses a deprecated element is deprecated, too.
    
    see https://bugs.eclipse.org/bugs/show_bug.cgi?id=437689
    
    Change-Id: I1619d18e448152a0b9b36aebbac6b4c21fbcffa1
    Signed-off-by: sgalland <galland@arakhne.org>

--- plugins/org.eclipse.xtext.xbase.ui/src/org/eclipse/xtext/xbase/ui/hover/HoverUiStrings.java
+++ plugins/org.eclipse.xtext.xbase.ui/src/org/eclipse/xtext/xbase/ui/hover/HoverUiStrings.java
@@ -9,16 +9,12 @@ package org.eclipse.xtext.xbase.ui.hover;
 
 import static com.google.common.collect.Iterables.*;
 
-import java.util.Iterator;
 import java.util.List;
 
-import org.eclipse.xtext.common.types.JvmAnyTypeReference;
 import org.eclipse.xtext.common.types.JvmFormalParameter;
-import org.eclipse.xtext.common.types.JvmGenericArrayTypeReference;
 import org.eclipse.xtext.common.types.JvmType;
 import org.eclipse.xtext.common.types.JvmTypeConstraint;
 import org.eclipse.xtext.common.types.JvmTypeParameter;
-import org.eclipse.xtext.common.types.JvmTypeReference;
 import org.eclipse.xtext.xbase.validation.UIStrings;
 
 /**
@@ -62,40 +58,8 @@ public class HoverUiStrings extends UIStrings {
 		return "";
 	}
 	
-	protected String formalParametersToString(Iterable<? extends JvmFormalParameter> elements, boolean isVarArgs) {
-		StringBuilder buffer = new StringBuilder();
-		boolean needsSeparator = false;
-		Iterator<? extends JvmFormalParameter> iterator = elements.iterator();
-		while (iterator.hasNext()) {
-			JvmFormalParameter parameter = iterator.next();
-			if (needsSeparator)
-				buffer.append(", ");
-			needsSeparator = true;
-			JvmTypeReference typeRef = parameter.getParameterType();
-			if(typeRef != null) {
-				if (typeRef instanceof JvmAnyTypeReference)
-					buffer.append("Object");
-				else {
-					if (isVarArgs && !iterator.hasNext()) {
-						if (typeRef instanceof JvmGenericArrayTypeReference) {
-							buffer.append(((JvmGenericArrayTypeReference) typeRef).getComponentType().getSimpleName());
-						} else {
-							buffer.append(typeRef.getSimpleName());
-						}
-						buffer.append("...");
-					} else {
-						buffer.append(typeRef.getSimpleName());
-					}
-				}
-			} else 
-				buffer.append("[null]");
-			buffer.append(" " + parameter.getName());
-		}
-		return buffer.toString();
-	}
-	
 	@Override
 	protected String parameterTypes(Iterable<JvmFormalParameter> parameters, boolean isVarArgs) {
-		return formalParametersToString(parameters, isVarArgs);
+		return parametersToString(parameters, isVarArgs, true);
 	}
 }
