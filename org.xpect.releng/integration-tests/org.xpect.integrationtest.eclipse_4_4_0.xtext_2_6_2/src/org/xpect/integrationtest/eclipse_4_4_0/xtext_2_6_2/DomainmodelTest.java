96c92d098d4f50f9ef5b48a17c2e533614d828c6
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2014-08-28 10:23:04 +0200

    added target platforms for Xtext 2.6.2 and Xtext nightly Signed-off-by: Moritz Eysholdt <moritz.eysholdt@itemis.de>

---
+++ org.xpect.releng/integration-tests/org.xpect.integrationtest.eclipse_4_4_0.xtext_2_6_2/src/org/xpect/integrationtest/eclipse_4_4_0/xtext_2_6_2/DomainmodelTest.java
@@ -0,0 +1,12 @@
+package org.xpect.integrationtest.eclipse_4_4_0.xtext_2_6_2;
+
+import org.eclipse.xtext.example.domainmodel.xpect.tests.AllDomainModelXpectTests;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses(AllDomainModelXpectTests.class)
+public class DomainmodelTest {
+
+}
