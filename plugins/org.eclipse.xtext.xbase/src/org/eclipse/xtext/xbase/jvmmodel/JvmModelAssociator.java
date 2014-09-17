8e0055fc0ee05047e4a26de43da0980a2f264c0f
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2014-09-17 15:38:51 +0200

    [xbase][smoke] Another attempt to improve robustness and simplified #isHandled
    Change-Id: I25d31bf55bf133d8f0a400c5626373617995bf0d

--- plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/jvmmodel/JvmModelAssociator.java
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/jvmmodel/JvmModelAssociator.java
@@ -170,11 +170,13 @@ public class JvmModelAssociator implements IJvmModelAssociations, IJvmModelAssoc
 		if (container == null)
 			return;
 		final Map<EObject, JvmIdentifiableElement> mapping = getLogicalContainerMapping(container.eResource());
-		Iterator<Entry<EObject, JvmIdentifiableElement>> iterator = mapping.entrySet().iterator();
-		while (iterator.hasNext()) {
-			Entry<EObject, JvmIdentifiableElement> next = iterator.next();
-			if (next.getValue() == container) {
-				iterator.remove();
+		if (!mapping.isEmpty()) {
+			Iterator<Entry<EObject, JvmIdentifiableElement>> iterator = mapping.entrySet().iterator();
+			while (iterator.hasNext()) {
+				Entry<EObject, JvmIdentifiableElement> next = iterator.next();
+				if (next.getValue() == container) {
+					iterator.remove();
+				}
 			}
 		}
 	}
@@ -429,6 +431,8 @@ public class JvmModelAssociator implements IJvmModelAssociations, IJvmModelAssoc
 			Set<EObject> targets = sourceToTargetMap.get(sourceElement);
 			targets.remove(jvmElement);
 		}
+		if (jvmElement instanceof JvmIdentifiableElement)
+			removeLogicalChildAssociation((JvmIdentifiableElement) jvmElement);
 	}
 
 	@SuppressWarnings("deprecation")
