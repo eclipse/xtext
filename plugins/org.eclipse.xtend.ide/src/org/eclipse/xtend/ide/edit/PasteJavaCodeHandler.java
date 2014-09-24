742efc3e5d0454c602467ff015f00c1f840da122
Dennis Huebner <dennis.huebner@itemis.de>
2014-09-24 19:27:01 +0200

    [java2xtend] Better override detection. More test cases. Arrays.
    [java2xtend] Added context menu action to handle compilation units
    
    [java2xtend] Work around a 0x1L problem
    
    [java2xtend] comments for problematic code in Xtend
    
    Change-Id: I147932520fd7f39a1926dbdae9d903c62b183582

--- plugins/org.eclipse.xtend.ide/src/org/eclipse/xtend/ide/edit/PasteJavaCodeHandler.java
+++ plugins/org.eclipse.xtend.ide/src/org/eclipse/xtend/ide/edit/PasteJavaCodeHandler.java
@@ -10,65 +10,58 @@ package org.eclipse.xtend.ide.edit;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.dom.ASTParser;
-import org.eclipse.jdt.core.dom.BodyDeclaration;
-import org.eclipse.jdt.core.dom.Modifier;
-import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jdt.internal.corext.refactoring.reorg.JavaElementTransfer;
 import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.dnd.Clipboard;
 import org.eclipse.swt.dnd.DND;
-import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.xtend.core.conversion.JavaConverter;
-import org.eclipse.xtend.core.conversion.JavaASTFlattener;
+import org.eclipse.xtend.core.conversion.JavaConverter.ConversionResult;
 import org.eclipse.xtext.ui.editor.XtextEditor;
 import org.eclipse.xtext.ui.editor.copyqualifiedname.ClipboardUtil;
 import org.eclipse.xtext.ui.editor.utils.EditorUtils;
+import org.eclipse.xtext.ui.util.SWTUtil;
 
 import com.google.inject.Inject;
-import com.google.inject.Injector;
 
 /**
  * @author dhuebner - Initial contribution and API
  */
+@SuppressWarnings("restriction")
 public class PasteJavaCodeHandler extends AbstractHandler {
-	private @Inject Injector injector;
+	private @Inject JavaConverter java2xtendConverter;
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		XtextEditor activeXtextEditor = EditorUtils.getActiveXtextEditor(event);
 		if (activeXtextEditor == null) {
 			return null;
 		}
-		Object clipboardContent = ClipboardUtil.getClipboardContent(DND.CLIPBOARD);
-		System.out.println("PasteJavaCodeHandler.execute()");
-		System.out.println(clipboardContent);
-		StyledText textWidget = activeXtextEditor.getInternalSourceViewer().getTextWidget();
-		Point selection = textWidget.getSelection();
-		JavaConverter converter = new JavaConverter() {
-			@Override
-			protected String doConvert(ASTNode ast) {
-				JavaASTFlattener flattener = new JavaASTFlattener() {
-					@Override
-					public boolean visit(TypeDeclaration it) {
-						if ("MISSING".equals(it.getName().getIdentifier()) && !it.isInterface()
-								&& it.getModifiers() == 0) {
-							for (BodyDeclaration body : (Iterable<BodyDeclaration>) it.bodyDeclarations()) {
-								body.accept(this);
-							}
-							return false;
-						}
-						return super.visit(it);
-					}
-				};
-				injector.injectMembers(flattener);
-				ast.accept(flattener);
-				return flattener.getResult();
+		Clipboard clipboard = null;
+		JavaElementTransfer javaElementTransfer = JavaElementTransfer.getInstance();
+		Object contents = null;
+		try {
+			Display display = SWTUtil.getStandardDisplay();
+			clipboard = new Clipboard(display);
+			contents = clipboard.getContents(javaElementTransfer, DND.CLIPBOARD);
+		} finally {
+			if (clipboard != null) {
+				clipboard.dispose();
 			}
-		};
-		injector.injectMembers(converter);
-		String xtendCode = converter.toXtend(clipboardContent.toString(), ASTParser.K_CLASS_BODY_DECLARATIONS);
-		System.out.println(xtendCode);
+		}
+		Object clipboardContent = ClipboardUtil.getClipboardContent(DND.CLIPBOARD);
+		int sourceKind = ASTParser.K_CLASS_BODY_DECLARATIONS;
+		//TODO		if (contents != null) {
+		//			IJavaElement iJavaElement = ((org.eclipse.jdt.core.IJavaElement[]) contents)[0];
+		//			if (iJavaElement.getElementType() == IJavaElement.COMPILATION_UNIT) {
+		//				sourceKind = ASTParser.K_COMPILATION_UNIT;
+		//			}
+		//		}
+		StyledText textWidget = activeXtextEditor.getInternalSourceViewer().getTextWidget();
+		ConversionResult conversionResult = java2xtendConverter.toXtend("SNIPPET",clipboardContent.toString(), sourceKind);
+		String xtendCode = conversionResult.getXtendCode();
 		textWidget.insert(xtendCode);
 		return null;
 	}
-
 }
