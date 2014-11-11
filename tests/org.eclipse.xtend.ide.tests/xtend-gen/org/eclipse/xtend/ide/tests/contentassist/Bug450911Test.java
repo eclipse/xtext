0e2933b57761a6cefc8590ed8fd011fca291c607
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2014-11-11 10:45:26 +0100

    [xtend][contentAssist] Fixed exception on CA
    Also removed unnecessarily duplicated code.
    
    see https://bugs.eclipse.org/bugs/show_bug.cgi?id=450911
    
    Change-Id: Ia15598ad78de93b90e91576e69606dde602a1632

---
+++ tests/org.eclipse.xtend.ide.tests/xtend-gen/org/eclipse/xtend/ide/tests/contentassist/Bug450911Test.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2014 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.xtend.ide.tests.contentassist;
+
+import org.eclipse.xtend.ide.tests.contentassist.AbstractXtendContentAssistBugTest;
+import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipse.xtext.junit4.ui.ContentAssistProcessorTestBuilder;
+import org.junit.Test;
+
+/**
+ * @author Sebastian Zarnekow - Initial contribution and API
+ */
+@SuppressWarnings("all")
+public class Bug450911Test extends AbstractXtendContentAssistBugTest {
+  @Test
+  public void test_01() throws Exception {
+    ContentAssistProcessorTestBuilder _newBuilder = this.newBuilder();
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("class C {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("def m() {");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("if (!println(true)) {");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    ContentAssistProcessorTestBuilder _append = _newBuilder.append(_builder.toString());
+    _append.assertTextAtCursorPosition("println", 1, "println", "print()", "println()");
+  }
+}
