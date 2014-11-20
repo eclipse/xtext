a5064f99db035a1d456dc75e5e10b5740da67885
Stefan Oehme <stefan.oehme@itemis.de>
2014-11-20 08:49:37 +0100

    intial creation of xtext.ide bundle

---
+++ plugins/org.eclipse.xtext.ide/src/org/eclipse/xtext/ide/internal/Activator.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2014 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+package org.eclipse.xtext.ide.internal;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator implements BundleActivator {
+
+	public void start(BundleContext context) throws Exception {
+	}
+
+	public void stop(BundleContext context) throws Exception {
+	}
+
+}
