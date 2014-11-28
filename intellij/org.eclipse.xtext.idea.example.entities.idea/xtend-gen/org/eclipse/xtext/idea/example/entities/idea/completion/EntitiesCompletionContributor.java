615fa8b46b33ef6c055da13acc2ba4fa58bd96ae
Stefan Oehme <stefan.oehme@itemis.de>
2014-11-28 10:57:35 +0100

    added an example on how to use the intellij completion APIs

--- intellij/org.eclipse.xtext.idea.example.entities.idea/xtend-gen/org/eclipse/xtext/idea/example/entities/idea/completion/EntitiesCompletionContributor.java
+++ intellij/org.eclipse.xtext.idea.example.entities.idea/xtend-gen/org/eclipse/xtext/idea/example/entities/idea/completion/EntitiesCompletionContributor.java
@@ -1,11 +1,45 @@
 package org.eclipse.xtext.idea.example.entities.idea.completion;
 
+import com.intellij.codeInsight.completion.CompletionParameters;
+import com.intellij.codeInsight.completion.CompletionProvider;
+import com.intellij.codeInsight.completion.CompletionResultSet;
+import com.intellij.codeInsight.completion.CompletionType;
+import com.intellij.codeInsight.template.impl.LiveTemplateLookupElementImpl;
+import com.intellij.codeInsight.template.impl.TemplateImpl;
+import com.intellij.codeInsight.template.impl.TextExpression;
+import com.intellij.patterns.PlatformPatterns;
+import com.intellij.patterns.PsiElementPattern;
+import com.intellij.psi.PsiElement;
+import com.intellij.util.ProcessingContext;
 import org.eclipse.xtext.idea.completion.AbstractCompletionContributor;
+import org.eclipse.xtext.idea.example.entities.idea.lang.EntitiesElementTypeProvider;
 import org.eclipse.xtext.idea.example.entities.idea.lang.EntitiesLanguage;
+import org.eclipse.xtext.xbase.lib.ObjectExtensions;
+import org.eclipse.xtext.xbase.lib.Procedures.Procedure1;
 
 @SuppressWarnings("all")
 public class EntitiesCompletionContributor extends AbstractCompletionContributor {
   public EntitiesCompletionContributor() {
     super(EntitiesLanguage.INSTANCE);
+    PsiElementPattern.Capture<PsiElement> _psiElement = PlatformPatterns.psiElement();
+    PsiElementPattern.Capture<PsiElement> _psiElement_1 = PlatformPatterns.psiElement(EntitiesElementTypeProvider.Entity_NameValidIDParserRuleCall_1_0_ELEMENT_TYPE);
+    PsiElementPattern.Capture<PsiElement> _withParent = _psiElement.withParent(_psiElement_1);
+    final CompletionProvider<CompletionParameters> _function = new CompletionProvider<CompletionParameters>() {
+      @Override
+      protected void addCompletions(final CompletionParameters parameters, final ProcessingContext context, final CompletionResultSet result) {
+        TemplateImpl _templateImpl = new TemplateImpl("entity name", "");
+        final Procedure1<TemplateImpl> _function = new Procedure1<TemplateImpl>() {
+          public void apply(final TemplateImpl it) {
+            TextExpression _textExpression = new TextExpression("Name");
+            it.addVariable(_textExpression, true);
+          }
+        };
+        final TemplateImpl template = ObjectExtensions.<TemplateImpl>operator_doubleArrow(_templateImpl, _function);
+        LiveTemplateLookupElementImpl _liveTemplateLookupElementImpl = new LiveTemplateLookupElementImpl(template, false);
+        EntitiesCompletionContributor.this._completionExtensions.operator_add(result, _liveTemplateLookupElementImpl);
+      }
+    };
+    this.extend(
+      CompletionType.BASIC, _withParent, _function);
   }
 }
