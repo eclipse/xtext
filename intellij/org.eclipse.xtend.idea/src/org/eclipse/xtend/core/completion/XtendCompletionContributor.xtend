83044dd1a6a8a01e053dbf2dd2969df8bde02e3f
Stefan Oehme <stefan.oehme@itemis.de>
2014-11-28 14:01:43 +0100

    add xtend.ide.common, enables keyword completion in intellij

---
+++ intellij/org.eclipse.xtend.idea/src/org/eclipse/xtend/core/completion/XtendCompletionContributor.xtend
@@ -0,0 +1,10 @@
+package org.eclipse.xtend.core.completion
+
+import org.eclipse.xtend.core.idea.lang.XtendLanguage
+import org.eclipse.xtext.idea.completion.AbstractCompletionContributor
+
+class XtendCompletionContributor extends AbstractCompletionContributor {
+	new() {
+		super(XtendLanguage.INSTANCE)
+	}
+}
---
+++ plugins/org.eclipse.xtend.ide.common/src/org/eclipse/xtend/ide/common/contentassist/antlr/FlexerBasedPartialXtendContentAssistParser.java
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ * Copyright (c) 2014 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtend.ide.common.contentassist.antlr;
+
+import java.io.StringReader;
+import java.util.Collection;
+
+import org.antlr.runtime.CharStream;
+import org.antlr.runtime.TokenSource;
+import org.eclipse.xtend.ide.common.contentassist.antlr.internal.ContentAssistFlexerFactory;
+import org.eclipse.xtend.ide.common.contentassist.antlr.internal.InternalXtendParser;
+import org.eclipse.xtext.ide.editor.contentassist.antlr.FollowElement;
+import org.eclipse.xtext.ide.editor.contentassist.antlr.ObservableXtextTokenStream;
+import org.eclipse.xtext.ide.editor.contentassist.antlr.internal.AbstractInternalContentAssistParser;
+import org.eclipse.xtext.ide.editor.contentassist.antlr.internal.InfiniteRecursion;
+import org.eclipse.xtext.parser.antlr.ITokenDefProvider;
+import org.eclipse.xtext.parser.antlr.IUnorderedGroupHelper;
+
+import com.google.common.collect.Lists;
+import com.google.inject.Inject;
+
+/**
+ * @author dhuebner - Initial contribution and API
+ */
+public class FlexerBasedPartialXtendContentAssistParser extends PartialXtendContentAssistParser {
+
+	@Inject
+	private ITokenDefProvider tokenDefProvider;
+	
+	@Inject
+	private ContentAssistFlexerFactory flexerFactory;
+	
+	@Override
+	protected InternalXtendParser createParser() {
+		FlexerBasedInternalContentAssistParser result = new FlexerBasedInternalContentAssistParser(tokenDefProvider);
+		result.setGrammarAccess(getGrammarAccess());
+		return result;
+	}
+	
+	@Override
+	protected TokenSource createLexer(CharStream stream) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Collection<FollowElement> getFollowElements(String input, boolean strict) {
+		TokenSource tokenSource = flexerFactory.createTokenSource(new StringReader(input));
+		AbstractInternalContentAssistParser parser = createParser();
+		parser.setStrict(strict);
+		ObservableXtextTokenStream tokens = new ObservableXtextTokenStream(tokenSource, parser);
+		tokens.setInitialHiddenTokens(getInitialHiddenTokens());
+		parser.setTokenStream(tokens);
+		IUnorderedGroupHelper helper = getUnorderedGroupHelper().get();
+		parser.setUnorderedGroupHelper(helper);
+		helper.initializeWith(parser);
+		tokens.setListener(parser);
+		try {
+			return Lists.newArrayList(getFollowElements(parser));
+		} catch(InfiniteRecursion infinite) {
+			return Lists.newArrayList(parser.getFollowElements());
+		}
+	}
+}
