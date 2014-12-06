7ee2ee6a9f50217978e7ec7fe9821701a15c19d2
Stéphane Galland <galland@arakhne.org>
2014-12-06 19:07:03 +0100

    [xbase][lib] Add operations in MapExtensions.
    see https://bugs.eclipse.org/bugs/show_bug.cgi?id=454319
    
    Change-Id: I50f390fb9230fa16706c90572b06fbcf64cc3aa7
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

--- plugins/org.eclipse.xtext.xbase.lib/src/org/eclipse/xtext/xbase/lib/MapExtensions.java
+++ plugins/org.eclipse.xtext.xbase.lib/src/org/eclipse/xtext/xbase/lib/MapExtensions.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 itemis AG (http://www.itemis.eu) and others.
+ * Copyright (c) 2014 itemis AG (http://www.itemis.eu) and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,7 +7,14 @@
  *******************************************************************************/
 package org.eclipse.xtext.xbase.lib;
 
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.concurrent.ConcurrentMap;
 
 import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.Functions.Function2;
@@ -16,6 +23,7 @@ import org.eclipse.xtext.xbase.lib.Procedures.Procedure3;
 import org.eclipse.xtext.xbase.lib.internal.FunctionDelegate;
 
 import com.google.common.annotations.GwtCompatible;
+import com.google.common.base.Objects;
 import com.google.common.base.Predicate;
 import com.google.common.collect.Maps;
 
@@ -23,6 +31,7 @@ import com.google.common.collect.Maps;
  * This is an extension library for {@link Map maps}.
  * 
  * @author Sebastian Zarnekow - Initial contribution and API
+ * @author Stephane Galland - Add operator definitions.
  */
 @GwtCompatible public class MapExtensions {
 
@@ -109,4 +118,294 @@ import com.google.common.collect.Maps;
 		return Maps.transformValues(original, new FunctionDelegate<V1, V2>(transformation));
 	}
 	
+	private static <K, V> Map<K, V> newMapInstance(Map<K, V> oldMap) {
+		if (oldMap instanceof TreeMap<?, ?>) {
+			Comparator<? super K> cmp = ((TreeMap<K, V>) oldMap).comparator();
+			if (cmp != null) {
+				return Maps.newTreeMap(cmp);
+			}
+			return new TreeMap<K, V>();
+		}
+		if (oldMap instanceof LinkedHashMap<?, ?>) {
+			return Maps.newLinkedHashMap();
+		}
+		if (oldMap instanceof ConcurrentMap<?, ?>) {
+			return Maps.newConcurrentMap();
+		}
+		return Maps.newHashMap();
+	}
+
+	/** Add the given pair into the map.
+	 *
+	 * If the pair key already exists in the map, its value is replaced
+	 * by the value in the pair, and the old value in the map is returned.
+	 *
+	 * @param <K> - type of the map keys.
+	 * @param <V> - type of the map values.
+	 * @param map - the map to update.
+	 * @param entry - the entry (key, value) to add into the map.
+	 * @return the value previously associated to the key, or <code>null</code>
+	 * if the key was not present in the map before the addition.
+	 * @since 2.8
+	 */
+	@Inline(value = "($1.put($2.getKey(), $2.getValue()))", statementExpression=true)
+	public static <K, V> V operator_add(Map<K, V> map, Pair<? extends K, ? extends V> entry) {
+		return map.put(entry.getKey(), entry.getValue());
+	}
+
+	/** Add the given entries of the input map into the output map.
+	 *
+	 * If a key in the inputMap already exists in the outputMap, its value is
+	 * replaced in the outputMap by the value from the inputMap.
+	 *
+	 * @param <K> - type of the map keys.
+	 * @param <V> - type of the map values.
+	 * @param outputMap - the map to update.
+	 * @param inputMap - the entries to add.
+	 * @return the outputMap
+	 * @since 2.8
+	 */
+	public static <K, V> Map<K, V> operator_add(Map<K, V> outputMap, Map<? extends K, ? extends V> inputMap) {
+		outputMap.putAll(inputMap);
+		return outputMap;
+	}
+
+	/** Add the given pair to a given map for obtaining a new map.
+	 *
+	 * If the pair key already exists in the map, its value is replaced
+	 * by the value in the pair, and the old value in the map is returned.
+	 *
+	 * @param <K> - type of the map keys.
+	 * @param <V> - type of the map values.
+	 * @param map - the map to consider.
+	 * @param entry - the entry (key, value) to add into the map.
+	 * @return a map with the content of the map and with the given entry.
+	 * @since 2.8
+	 */
+	@Pure
+	public static <K, V> Map<K, V> operator_plus(Map<K, V> map, Pair<? extends K, ? extends V> entry) {
+		Map<K, V> newMap = newMapInstance(map);
+		newMap.putAll(map);
+		newMap.put(entry.getKey(), entry.getValue());
+		return newMap;
+	}
+
+	/** Add the given pair to a given map for obtaining a new map.
+	 *
+	 * If the pair key already exists in the map, its value is replaced
+	 * by the value in the pair, and the old value in the map is returned.
+	 *
+	 * @param <K> - type of the map keys.
+	 * @param <V> - type of the map values.
+	 * @param entry - the entry (key, value) to add into the map.
+	 * @param map - the map to consider.
+	 * @return a map with the content of the map and with the given entry.
+	 * @since 2.8
+	 */
+	@Pure
+	public static <K, V> Map<K, V> operator_plus(Pair<? extends K, ? extends V> entry, Map<K, V> map) {
+		Map<K, V> newMap = newMapInstance(map);
+		newMap.putAll(map);
+		newMap.put(entry.getKey(), entry.getValue());
+		return newMap;
+	}
+
+	/** Merge the given maps.
+	 *
+	 * If a key in the map2 already exists in the map2, its value is
+	 * replaced in the map1 by the value from the map2.
+	 *
+	 * @param <K> - type of the map keys.
+	 * @param <V> - type of the map values.
+	 * @param map1 - the first map.
+	 * @param map2 - the second map.
+	 * @return a map with the merged contents from the two maps.
+	 * @since 2.8
+	 */
+	@Pure
+	public static <K, V> Map<K, V> operator_plus(Map<K, V> map1, Map<? extends K, ? extends V> map2) {
+		Map<K, V> newMap = newMapInstance(map1);
+		newMap.putAll(map1);
+		newMap.putAll(map2);
+		return newMap;
+	}
+
+	/** Remove a pair from the given map.
+	 *
+	 * The pair is removed only if the key and the value are associated in the map.
+	 * If the key exists in the map, but it is not associated with the given value,
+	 * then the map stays unchanged.
+	 *
+	 * @param <K> - type of the map keys.
+	 * @param <V> - type of the map values.
+	 * @param map - the map to update.
+	 * @param pair - the pair to remove.
+	 * @return the map.
+	 * @since 2.8
+	 */
+	public static <K, V> Map<K, V> operator_remove(Map<K, V> map, Pair<? super K, ? super V> pair) {
+		V value = map.get(pair.getKey());
+		if (Objects.equal(value, pair.getValue())) {
+			map.remove(pair.getKey());
+		}
+		return map;
+	}
+
+	/** Remove a key from the given map.
+	 *
+	 * @param <K> - type of the map keys.
+	 * @param <V> - type of the map values.
+	 * @param map - the map to update.
+	 * @param key - the key to remove.
+	 * @return the removed value, or <code>null</code> if the key was not
+	 * present in the map.
+	 * @since 2.8
+	 */
+	@Inline(value="($1.remove($2))", statementExpression=true)
+	public static <K, V> V operator_remove(Map<K, V> map, K key) {
+		return map.remove(key);
+	}
+
+	/** Remove pairs from the given map.
+	 *
+	 * The pair is removed only if the key and the value are associated in the map.
+	 * If the key exists in the map, but it is not associated with the given value,
+	 * then the map stays unchanged.
+	 *
+	 * @param <K> - type of the map keys.
+	 * @param <V> - type of the map values.
+	 * @param map - the map to update.
+	 * @param pairsToRemove - the pairs to remove.
+	 * @return the map.
+	 * @since 2.8
+	 */
+	public static <K, V> Map<K, V> operator_remove(Map<K, V> map, Map<? super K, ? super V> pairsToRemove) {
+		for (Entry<? super K, ? super V> pair : pairsToRemove.entrySet()) {
+			V value = map.get(pair.getKey());
+			if (Objects.equal(value, pair.getValue())) {
+				map.remove(pair.getKey());
+			}
+		}
+		return map;
+	}
+
+	/** Remove pairs with the given keys from the map.
+	 *
+	 *
+	 * @param <K> - type of the map keys.
+	 * @param <V> - type of the map values.
+	 * @param map - the map to update.
+	 * @param keysToRemove - the keys of the pairs to remove.
+	 * @return the map.
+	 * @since 2.8
+	 */
+	public static <K, V> Map<K, V> operator_remove(Map<K, V> map, Iterable<? super K> keysToRemove) {
+		for (Object key : keysToRemove) {
+			map.remove(key);
+		}
+		return map;
+	}
+
+	/** Create a copy of the given map without the pair with given key.
+	 *
+	 * @param <K> - type of the map keys.
+	 * @param <V> - type of the map values.
+	 * @param map - the map to update.
+	 * @param key - the key to remove.
+	 * @return the map with the content of the map except the key.
+	 * @since 2.8
+	 */
+	@Pure
+	public static <K, V> Map<K, V> operator_minus(Map<K, V> map, K key) {
+		Map<K, V> newMap = newMapInstance(map);
+		for (Entry<K, V> pair : map.entrySet()) {
+			if (!Objects.equal(key, pair.getKey())) {
+				newMap.put(pair.getKey(), pair.getValue());
+			}
+		}
+		return newMap;
+	}
+	
+	/** Create a copy of the given map without the given pair.
+	 *
+	 * The pair is removed only if the key and the value are associated in the map.
+	 * If the key exists in the map, but it is not associated with the given value,
+	 * then the map stays unchanged.
+	 *
+	 * @param <K> - type of the map keys.
+	 * @param <V> - type of the map values.
+	 * @param map - the map to update.
+	 * @param pair - the pair to remove.
+	 * @return the map with the content of the map except the pair.
+	 * @since 2.8
+	 */
+	@Pure
+	public static <K, V> Map<K, V> operator_minus(Map<K, V> map, Pair<? super K, ? super V> pair) {
+		Map<K, V> newMap = newMapInstance(map);
+		for (Entry<K, V> opair : map.entrySet()) {
+			if (!Objects.equal(pair.getKey(), opair.getKey())
+				|| !Objects.equal(pair.getValue(), opair.getValue())) {
+				newMap.put(opair.getKey(), opair.getValue());
+			}
+		}
+		return newMap;
+	}
+
+	/** Create a copy of the given map without the given pairs.
+	 *
+	 * The pair is removed only if the key and the value are associated in the map.
+	 * If the key exists in the map, but it is not associated with the given value,
+	 * then the map stays unchanged.
+	 *
+	 * @param <K> - type of the map keys.
+	 * @param <V> - type of the map values.
+	 * @param map1 - the map to update.
+	 * @param map2 - the pairs to remove.
+	 * @return the map with the content of the map except the pairs.
+	 * @since 2.8
+	 */
+	@Pure
+	public static <K, V> Map<K, V> operator_minus(Map<K, V> map1, Map<? super K, ? super V> map2) {
+		Map<K, V> newMap = newMapInstance(map1);
+		for (Entry<K, V> opair : map1.entrySet()) {
+			Object value = map2.get(opair.getKey());
+			if (!Objects.equal(value, opair.getValue())) {
+				newMap.put(opair.getKey(), opair.getValue());
+			}
+		}
+		return newMap;
+	}
+
+	/** Create a copy of the given map without the given pairs.
+	 *
+	 * @param <K> - type of the map keys.
+	 * @param <V> - type of the map values.
+	 * @param map - the map to update.
+	 * @param pairs - the pairs to remove.
+	 * @return the map with the content of the map except the pairs.
+	 * @since 2.8
+	 */
+	@Pure
+	public static <K, V> Map<K, V> operator_minus(Map<K, V> map, Iterable<? super K> pairs) {
+		Map<K, V> newMap = newMapInstance(map);
+		Set<Object> keys = null;
+		for (Entry<K, V> opair : map.entrySet()) {
+			if (keys == null) {
+				boolean isFound = false;
+				for (Object key : pairs) {
+					keys.add(key);
+					if (Objects.equal(key, opair.getKey())) {
+						isFound = true;
+					}
+				}
+				if (!isFound) {
+					newMap.put(opair.getKey(), opair.getValue());
+				}
+			} else if (!keys.contains(opair.getKey())) {
+				newMap.put(opair.getKey(), opair.getValue());
+			}
+		}
+		return newMap;
+	}
+
 }
---
+++ tests/org.eclipse.xtext.xbase.tests/src/org/eclipse/xtext/xbase/tests/lib/MapExtensionsTest.java
@@ -0,0 +1,396 @@
+/*******************************************************************************
+ * Copyright (c) 2014 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.xbase.tests.lib;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.eclipse.xtext.xbase.lib.Pair;
+
+import static org.eclipse.xtext.xbase.lib.MapExtensions.*;
+
+/**
+ * @author Stephane Galland - Initial contribution and API
+ */
+public class MapExtensionsTest extends Assert {
+
+	private Map<String, String> map;
+	private String key1;
+	private String key2;
+	private String value1;
+	private String value2;
+
+	@Before
+	public void setUp() {
+		this.map = new HashMap<String, String>();
+		this.key1 = "k1"; //$NON-NLS-1$
+		this.key2 = "k2"; //$NON-NLS-1$
+		this.value1 = UUID.randomUUID().toString();
+		this.value2 = UUID.randomUUID().toString();
+		this.map.put(this.key1, this.value1);
+		this.map.put(this.key2, this.value2);
+	}
+
+	@After
+	public void tearDown() {
+		this.key1 = this.value1 = null;
+		this.key2 = this.value2 = null;
+		this.map = null;
+	}
+
+	@Test
+	public void operator_addMapPair_0() {
+		String v = UUID.randomUUID().toString();
+		String o;
+
+		o = operator_add(this.map, new Pair<String, String>("k3", v));
+		assertNull(o);
+		assertEquals(3, this.map.size());
+		assertEquals(this.value1, this.map.get("k1"));
+		assertEquals(this.value2, this.map.get("k2"));
+		assertEquals(v, this.map.get("k3"));
+	}
+
+	@Test
+	public void operator_addMapPair_1() {
+		String v = UUID.randomUUID().toString();
+		String o;
+
+		o = operator_add(this.map, new Pair<String, String>("k2", v));
+		assertEquals(this.value2, o);
+		assertEquals(2, this.map.size());
+		assertEquals(this.value1, this.map.get("k1"));
+		assertEquals(v, this.map.get("k2"));
+	}
+
+	@Test
+	public void operator_addMapMap_0() {
+		String v1 = UUID.randomUUID().toString();
+		String v2 = UUID.randomUUID().toString();
+		String v3 = UUID.randomUUID().toString();
+		Map<String, String> o;
+
+		Map<String, String> tmp = new HashMap<String, String>();
+		tmp.put("k3", v1);
+		tmp.put("k4", v2);
+		tmp.put("k5", v3);
+
+		o = operator_add(this.map, tmp);
+		assertSame(this.map, o);
+		assertEquals(5, this.map.size());
+		assertEquals(this.value1, this.map.get("k1"));
+		assertEquals(this.value2, this.map.get("k2"));
+		assertEquals(v1, this.map.get("k3"));
+		assertEquals(v2, this.map.get("k4"));
+		assertEquals(v3, this.map.get("k5"));
+	}
+
+	@Test
+	public void operator_addMapMap_1() {
+		String v1 = UUID.randomUUID().toString();
+		String v2 = UUID.randomUUID().toString();
+		String v3 = UUID.randomUUID().toString();
+		Map<String, String> o;
+
+		Map<String, String> tmp = new HashMap<String, String>();
+		tmp.put("k3", v1);
+		tmp.put("k2", v2);
+		tmp.put("k5", v3);
+
+		o = operator_add(this.map, tmp);
+		assertSame(this.map, o);
+		assertEquals(4, this.map.size());
+		assertEquals(this.value1, this.map.get("k1"));
+		assertEquals(v2, this.map.get("k2"));
+		assertEquals(v1, this.map.get("k3"));
+		assertEquals(v3, this.map.get("k5"));
+	}
+
+	@Test
+	public void operator_pluMapPair_0() {
+		String v = UUID.randomUUID().toString();
+		Map<String, String> o;
+
+		o = operator_plus(this.map, new Pair<String, String>("k3", v));
+		assertNotNull(o);
+		assertNotSame(this.map, o);
+		assertEquals(2, this.map.size());
+		assertEquals(this.value1, this.map.get("k1"));
+		assertEquals(this.value2, this.map.get("k2"));
+		assertEquals(3, o.size());
+		assertEquals(this.value1, o.get("k1"));
+		assertEquals(this.value2, o.get("k2"));
+		assertEquals(v, o.get("k3"));
+	}
+
+	@Test
+	public void operator_pluMapPair_1() {
+		String v = UUID.randomUUID().toString();
+		Map<String, String> o;
+
+		o = operator_plus(this.map, new Pair<String, String>("k2", v));
+		assertNotNull(o);
+		assertNotSame(this.map, o);
+		assertEquals(2, this.map.size());
+		assertEquals(this.value1, this.map.get("k1"));
+		assertEquals(this.value2, this.map.get("k2"));
+		assertEquals(2, o.size());
+		assertEquals(this.value1, o.get("k1"));
+		assertEquals(v, o.get("k2"));
+	}
+
+	@Test
+	public void operator_plusPairMap_0() {
+		String v = UUID.randomUUID().toString();
+		Map<String, String> o;
+
+		o = operator_plus(new Pair<String, String>("k3", v), this.map);
+		assertNotNull(o);
+		assertNotSame(this.map, o);
+		assertEquals(2, this.map.size());
+		assertEquals(this.value1, this.map.get("k1"));
+		assertEquals(this.value2, this.map.get("k2"));
+		assertEquals(3, o.size());
+		assertEquals(this.value1, o.get("k1"));
+		assertEquals(this.value2, o.get("k2"));
+		assertEquals(v, o.get("k3"));
+	}
+
+	@Test
+	public void operator_pluPairMap() {
+		String v = UUID.randomUUID().toString();
+		Map<String, String> o;
+
+		o = operator_plus(new Pair<String, String>("k2", v), this.map);
+		assertNotNull(o);
+		assertNotSame(this.map, o);
+		assertEquals(2, this.map.size());
+		assertEquals(this.value1, this.map.get("k1"));
+		assertEquals(this.value2, this.map.get("k2"));
+		assertEquals(2, o.size());
+		assertEquals(this.value1, o.get("k1"));
+		assertEquals(v, o.get("k2"));
+	}
+
+	@Test
+	public void operator_plusMapMap_0() {
+		String v1 = UUID.randomUUID().toString();
+		String v2 = UUID.randomUUID().toString();
+		String v3 = UUID.randomUUID().toString();
+		Map<String, String> o;
+
+		Map<String, String> tmp = new HashMap<String, String>();
+		tmp.put("k3", v1);
+		tmp.put("k4", v2);
+		tmp.put("k5", v3);
+
+		o = operator_plus(this.map, tmp);
+		assertNotNull(o);
+		assertNotSame(this.map, o);
+		assertNotSame(tmp, o);
+		assertEquals(2, this.map.size());
+		assertEquals(this.value1, this.map.get("k1"));
+		assertEquals(this.value2, this.map.get("k2"));
+		assertEquals(3, tmp.size());
+		assertEquals(v1, tmp.get("k3"));
+		assertEquals(v2, tmp.get("k4"));
+		assertEquals(v3, tmp.get("k5"));
+		assertEquals(5, o.size());
+		assertEquals(this.value1, o.get("k1"));
+		assertEquals(this.value2, o.get("k2"));
+		assertEquals(v1, o.get("k3"));
+		assertEquals(v2, o.get("k4"));
+		assertEquals(v3, o.get("k5"));
+	}
+
+	@Test
+	public void operator_plusMapMap_1() {
+		String v1 = UUID.randomUUID().toString();
+		String v2 = UUID.randomUUID().toString();
+		String v3 = UUID.randomUUID().toString();
+		Map<String, String> o;
+
+		Map<String, String> tmp = new HashMap<String, String>();
+		tmp.put("k3", v1);
+		tmp.put("k4", v2);
+		tmp.put("k5", v3);
+
+		o = operator_plus(tmp, this.map);
+		assertNotNull(o);
+		assertNotSame(this.map, o);
+		assertNotSame(tmp, o);
+		assertEquals(2, this.map.size());
+		assertEquals(this.value1, this.map.get("k1"));
+		assertEquals(this.value2, this.map.get("k2"));
+		assertEquals(3, tmp.size());
+		assertEquals(v1, tmp.get("k3"));
+		assertEquals(v2, tmp.get("k4"));
+		assertEquals(v3, tmp.get("k5"));
+		assertEquals(5, o.size());
+		assertEquals(this.value1, o.get("k1"));
+		assertEquals(this.value2, o.get("k2"));
+		assertEquals(v1, o.get("k3"));
+		assertEquals(v2, o.get("k4"));
+		assertEquals(v3, o.get("k5"));
+	}
+
+	@Test
+	public void operator_removeMapK_0() {
+		String o = operator_remove(this.map, "k0");
+		assertEquals(2, this.map.size());
+		assertEquals(this.value1, this.map.get("k1"));
+		assertEquals(this.value2, this.map.get("k2"));
+		assertNull(o);
+	}
+
+	@Test
+	public void operator_removeMapK_1() {
+		String o = operator_remove(this.map, "k1");
+		assertEquals(1, this.map.size());
+		assertNull(this.map.get("k1"));
+		assertEquals(this.value2, this.map.get("k2"));
+		assertEquals(this.value1, o);
+	}
+
+	@Test
+	public void operator_removeMapK_2() {
+		String o = operator_remove(this.map, "k2");
+		assertEquals(1, this.map.size());
+		assertEquals(this.value1, this.map.get("k1"));
+		assertNull(this.map.get("k2"));
+		assertEquals(this.value2, o);
+	}
+
+	@Test
+	public void operator_removeMapK_3() {
+		String o1 = operator_remove(this.map, "k2");
+		String o2 = operator_remove(this.map, "k1");
+		assertEquals(0, this.map.size());
+		assertNull(this.map.get("k1"));
+		assertNull(this.map.get("k2"));
+		assertEquals(this.value2, o1);
+		assertEquals(this.value1, o2);
+	}
+
+	@Test
+	public void operator_removeMapPair_0() {
+		Map<String, String> o = operator_remove(this.map, new Pair<String, String>("k0", "v0"));
+		assertEquals(2, this.map.size());
+		assertEquals(this.value1, this.map.get("k1"));
+		assertEquals(this.value2, this.map.get("k2"));
+		assertSame(this.map, o);
+	}
+
+	@Test
+	public void operator_removeMapPair_1() {
+		Map<String, String> o = operator_remove(this.map, new Pair<String, String>("k1", "v0"));
+		assertEquals(2, this.map.size());
+		assertEquals(this.value1, this.map.get("k1"));
+		assertEquals(this.value2, this.map.get("k2"));
+		assertSame(this.map, o);
+	}
+
+	@Test
+	public void operator_removeMapPair_2() {
+		Map<String, String> o = operator_remove(this.map, new Pair<String, String>("k1", this.value1));
+		assertEquals(1, this.map.size());
+		assertNull(this.map.get("k1"));
+		assertEquals(this.value2, this.map.get("k2"));
+		assertSame(this.map, o);
+	}
+
+	@Test
+	public void operator_removeMapMap_0() {
+		Map<String, String> m = new HashMap<String, String>();
+		m.put("k0", "v0");
+		m.put("k1", "v0");
+		Map<String, String> o = operator_remove(this.map, m);
+		assertEquals(2, this.map.size());
+		assertEquals(this.value1, this.map.get("k1"));
+		assertEquals(this.value2, this.map.get("k2"));
+		assertSame(this.map, o);
+	}
+
+	@Test
+	public void operator_removeMapMap_1() {
+		Map<String, String> m = new HashMap<String, String>();
+		m.put("k0", "v0");
+		m.put("k1", this.value1);
+		Map<String, String> o = operator_remove(this.map, m);
+		assertEquals(1, this.map.size());
+		assertNull(this.map.get("k1"));
+		assertEquals(this.value2, this.map.get("k2"));
+		assertSame(this.map, o);
+	}
+
+	@Test
+	public void operator_removeMapIterable() {
+		List<String> m = new ArrayList<String>();
+		m.add("k0");
+		m.add("k1");
+		Map<String, String> o = operator_remove(this.map, m);
+		assertEquals(1, this.map.size());
+		assertNull(this.map.get("k1"));
+		assertEquals(this.value2, this.map.get("k2"));
+		assertSame(this.map, o);
+	}
+
+	@Test
+	public void operator_minusMapK_0() {
+		Map<String, String> o = operator_minus(this.map, "k0");
+		assertEquals(2, this.map.size());
+		assertEquals(this.value1, this.map.get("k1"));
+		assertEquals(this.value2, this.map.get("k2"));
+		assertNotNull(o);
+		assertNotSame(this.map, o);
+		assertEquals(2, o.size());
+		assertEquals(this.value1, o.get("k1"));
+		assertEquals(this.value2, o.get("k2"));
+	}
+
+	@Test
+	public void operator_minusMapK_1() {
+		Map<String, String> o = operator_minus(this.map, "k2");
+		assertEquals(2, this.map.size());
+		assertEquals(this.value1, this.map.get("k1"));
+		assertEquals(this.value2, this.map.get("k2"));
+		assertNotNull(o);
+		assertNotSame(this.map, o);
+		assertEquals(1, o.size());
+		assertEquals(this.value1, o.get("k1"));
+		assertNull(o.get("k2"));
+	}
+
+	@Test
+	public void operator_minusMapK_2() {
+		Map<String, String> o1 = operator_minus(this.map, "k2");
+		Map<String, String> o2 = operator_minus(o1, "k1");
+		assertEquals(2, this.map.size());
+		assertEquals(this.value1, this.map.get("k1"));
+		assertEquals(this.value2, this.map.get("k2"));
+		assertNotNull(o1);
+		assertNotSame(this.map, o1);
+		assertEquals(1, o1.size());
+		assertEquals(this.value1, o1.get("k1"));
+		assertNull(o1.get("k2"));
+		assertNotNull(o2);
+		assertNotSame(this.map, o2);
+		assertEquals(0, o2.size());
+		assertNull(o2.get("k1"));
+		assertNull(o2.get("k2"));
+	}
+	
+}
