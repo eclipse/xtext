69d82b0756501c92c42c3c3060f1aab254887d23
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2014-12-17 22:30:38 +0100

    [xtext][ui] Another attempt to fix the document modification test
    This reverts "[xtext][ui] Fixed document test"
    (commit 9702bdd066667344624824149642c4979e45f046)
    
    Change-Id: I6cc5683fc014650a2d8ca18630d7421073e31f05

--- tests/org.eclipse.xtext.ui.tests/tests/org/eclipse/xtext/ui/tests/editor/model/edit/RealXtextDocumentModifyTest.java
+++ tests/org.eclipse.xtext.ui.tests/tests/org/eclipse/xtext/ui/tests/editor/model/edit/RealXtextDocumentModifyTest.java
@@ -11,16 +11,14 @@ import static org.eclipse.xtext.junit4.ui.util.IResourcesSetupUtil.*;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.emf.common.util.URI;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.xtext.junit4.ui.AbstractEditorTest;
-import org.eclipse.xtext.resource.IResourceServiceProvider;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.XtextProjectHelper;
-import org.eclipse.xtext.ui.editor.ObservableEditorCallback;
-import org.eclipse.xtext.ui.editor.IXtextEditorCallback;
 import org.eclipse.xtext.ui.editor.XtextEditor;
 import org.eclipse.xtext.ui.editor.model.IXtextDocument;
 import org.eclipse.xtext.ui.editor.model.IXtextModelListener;
+import org.eclipse.xtext.ui.editor.reconciler.XtextReconciler;
 import org.eclipse.xtext.util.concurrent.IUnitOfWork;
 import org.junit.Test;
 
@@ -36,46 +34,33 @@ public class RealXtextDocumentModifyTest extends AbstractEditorTest {
 	
 	// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=406811
 	@Test public void testModelListenerNotNotified() throws Exception {
-		IResourceServiceProvider serviceProvider = IResourceServiceProvider.Registry.INSTANCE.getResourceServiceProvider(URI.createURI("a.xtext"));
-		ObservableEditorCallback listenable = serviceProvider.get(ObservableEditorCallback.class);
+		final String grammar = "grammar foo.Foo\n" 
+				+ "generate foo \"http://foo.net/foo\"\n"
+				+ "Foo: 'foo';"; 
+		final IXtextDocument document = createDocument(grammar);
+		// wait for the forced reconcile operation to finish
+		Job.getJobManager().join(XtextReconciler.class.getName(), null);
 		
 		final int[] changeCounter = new int[] { 0 };
-		IXtextEditorCallback.NullImpl listener = new IXtextEditorCallback.NullImpl() {
+		document.addModelListener(new IXtextModelListener() {
 			@Override
-			public void afterCreatePartControl(XtextEditor xtextEditor) {
-				xtextEditor.getDocument().addModelListener(new IXtextModelListener() {
-					@Override
-					public void modelChanged(XtextResource resource) {
-						++changeCounter[0]; 
-					}
-				});
+			public void modelChanged(XtextResource resource) {
+				++changeCounter[0]; 
 			}
-		};
-		try {
-			listenable.addCallback(listener);
-			
-			final String grammar = "grammar foo.Foo\n" 
-					+ "generate foo \"http://foo.net/foo\"\n"
-					+ "Foo: 'foo';"; 
-			final IXtextDocument document = createDocument(grammar);
-			// the outline will trigger a synchronous #readOnly thus there is already a first #readOnly event
-			assertEquals("IXtextModelListener should have been directly notified after editor was opened", 1, changeCounter[0]);
-			document.modify(new IUnitOfWork.Void<XtextResource>() {
-				@Override
-				public void process(XtextResource state) throws Exception {
-					document.replace(grammar.indexOf("Foo"), 3, "Bar");
-				}
-			});
-			assertEquals("IXtextModelListener should not have been directly notified after #modify", 1, changeCounter[0]);
-			document.readOnly(new IUnitOfWork.Void<XtextResource>() {
-				@Override
-				public void process(XtextResource state) throws Exception {
-				}
-			});
-			assertEquals("Reconciler should have notified IXtextModelListener", 2, changeCounter[0]);
-		} finally {
-			listenable.removeCallback(listener);
-		}
+		});
+		document.modify(new IUnitOfWork.Void<XtextResource>() {
+			@Override
+			public void process(XtextResource state) throws Exception {
+				document.replace(grammar.indexOf("Foo"), 3, "Bar");
+			}
+		});
+		assertEquals("IXtextModelListener should not have been directly notified", 0, changeCounter[0]);
+		document.readOnly(new IUnitOfWork.Void<XtextResource>() {
+			@Override
+			public void process(XtextResource state) throws Exception {
+			}
+		});
+		assertEquals("Reconciler should have notified IXtextModelListener", 1, changeCounter[0]);
 	}
 
 	private IXtextDocument createDocument(String grammar) throws Exception {
