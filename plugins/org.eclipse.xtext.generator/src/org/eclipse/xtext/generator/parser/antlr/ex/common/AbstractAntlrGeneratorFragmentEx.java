bb18fe72baf4498af7d197a39ff28b39d03c3f93
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2015-01-06 18:18:45 +0100

    [xtext][parser] Work on infrastructure for indentation aware languages
    see https://bugs.eclipse.org/bugs/show_bug.cgi?id=451838
    
    Change-Id: I2aa288db306d5c6ac2b8c06b1970447e8a370318

--- plugins/org.eclipse.xtext.generator/src/org/eclipse/xtext/generator/parser/antlr/ex/common/AbstractAntlrGeneratorFragmentEx.java
+++ plugins/org.eclipse.xtext.generator/src/org/eclipse/xtext/generator/parser/antlr/ex/common/AbstractAntlrGeneratorFragmentEx.java
@@ -10,7 +10,11 @@ package org.eclipse.xtext.generator.parser.antlr.ex.common;
 import java.util.List;
 
 import org.eclipse.xtext.Grammar;
+import org.eclipse.xtext.generator.Naming;
 import org.eclipse.xtext.generator.parser.antlr.AbstractAntlrGeneratorFragment;
+import org.eclipse.xtext.generator.parser.antlr.ex.wsAware.SyntheticTerminalAwareFragmentHelper;
+import org.eclipse.xtext.parser.antlr.AbstractIndentationTokenSource;
+import org.eclipse.xtext.parser.antlr.AbstractSplittingTokenSource;
 
 import com.google.common.collect.ImmutableList;
 
@@ -21,15 +25,37 @@ import com.google.common.collect.ImmutableList;
 public abstract class AbstractAntlrGeneratorFragmentEx extends AbstractAntlrGeneratorFragment {
 
 	private AntlrFragmentHelper fragmentHelper;
+	
+	/**
+	 * @since 2.8
+	 */
+	private boolean detectSyntheticTerminals = true;
 
+	/**
+	 * Allows to customize the detection of synthetic terminal symbols in the grammar.
+	 * @see SyntheticTerminalAwareFragmentHelper
+	 */
 	public void setFragmentHelper(AntlrFragmentHelper fragmentHelper) {
 		this.fragmentHelper = fragmentHelper;
 	}
 
 	public AntlrFragmentHelper getFragmentHelper() {
-		if (fragmentHelper == null)
-			fragmentHelper = new AntlrFragmentHelper(getNaming());
+		if (fragmentHelper == null) {
+			if (detectSyntheticTerminals) {
+				fragmentHelper = new SyntheticTerminalAwareFragmentHelper(getNaming());
+			} else {
+				fragmentHelper = new AntlrFragmentHelper(getNaming());
+			}
+		}
 		return fragmentHelper;
+	}
+	
+	@Override
+	public void registerNaming(Naming naming) {
+		if (fragmentHelper != null) {
+			fragmentHelper.registerNaming(naming);
+		}
+		super.registerNaming(naming);
 	}
 
 	@Override
@@ -45,5 +71,26 @@ public abstract class AbstractAntlrGeneratorFragmentEx extends AbstractAntlrGene
 	@Override
 	protected String getLexerFileNameSuffix() {
 		return ".java";
+	}
+	
+	/**
+	 * @since 2.8
+	 */
+	public boolean isDetectSyntheticTerminals() {
+		return detectSyntheticTerminals;
+	}
+	
+	/**
+	 * Set to false if synthetic terminal should be ignored. Synthetic terminals
+	 * have the form {@code terminal ABC: 'synthetic:ABC';} in the grammar
+	 * and require a customized {@link AbstractSplittingTokenSource token source}.
+	 * 
+	 * @see SyntheticTerminalAwareFragmentHelper
+	 * @see AbstractIndentationTokenSource
+	 * @see AbstractSplittingTokenSource
+	 * @since 2.8
+	 */
+	public void setDetectSyntheticTerminals(boolean detectSyntheticTerminals) {
+		this.detectSyntheticTerminals = detectSyntheticTerminals;
 	}
 }
