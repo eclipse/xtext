9d61238a82d6013ee9a613f9f121dc4066eb1b2b
akosyakov <anton.kosyakov@itemis.de>
2015-01-06 09:44:54 +0100

    [IDEA] regenerated test languages to add support for completion contribution, braces matching, type and call hierarchies
    Change-Id: I170029c1739219894cdbda390051961de32e164b
    Signed-off-by: akosyakov <anton.kosyakov@itemis.de>

--- intellij/org.eclipse.xtext.idea.common.types.tests/src-gen/org/eclipse/xtext/idea/common/types/parser/antlr/RefactoringTestLanguageAntlrTokenFileProvider.java
+++ intellij/org.eclipse.xtext.idea.common.types.tests/src-gen/org/eclipse/xtext/idea/common/types/parser/antlr/RefactoringTestLanguageAntlrTokenFileProvider.java
@@ -8,6 +8,7 @@ import org.eclipse.xtext.parser.antlr.IAntlrTokenFileProvider;
 
 public class RefactoringTestLanguageAntlrTokenFileProvider implements IAntlrTokenFileProvider {
 	
+	@Override
 	public InputStream getAntlrTokenFile() {
 		ClassLoader classLoader = getClass().getClassLoader();
     	return classLoader.getResourceAsStream("org/eclipse/xtext/idea/common/types/parser/antlr/internal/InternalRefactoringTestLanguage.tokens");
