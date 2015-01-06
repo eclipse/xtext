61faedbc87878b18ff7376e49d7ebfd85bf6592b
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2015-01-06 18:18:45 +0100

    [xtext][parser] Work on infrastructure for indentation aware languages
    see https://bugs.eclipse.org/bugs/show_bug.cgi?id=451838
    
    Change-Id: I2aa288db306d5c6ac2b8c06b1970447e8a370318

--- plugins/org.eclipse.xtext.generator/src/org/eclipse/xtext/generator/parser/antlr/ex/common/AntlrFragmentHelper.java
+++ plugins/org.eclipse.xtext.generator/src/org/eclipse/xtext/generator/parser/antlr/ex/common/AntlrFragmentHelper.java
@@ -23,6 +23,7 @@ import org.eclipse.xtext.ParserRule;
 import org.eclipse.xtext.TerminalRule;
 import org.eclipse.xtext.UnorderedGroup;
 import org.eclipse.xtext.generator.Naming;
+import org.eclipse.xtext.generator.NamingAware;
 
 import com.google.common.base.Predicate;
 import com.google.common.collect.Collections2;
@@ -33,12 +34,26 @@ import com.google.common.collect.Collections2;
  * extensions.
  * @author Sebastian Zarnekow - Initial contribution and API
  */
-public class AntlrFragmentHelper {
+public class AntlrFragmentHelper implements NamingAware {
 
-	private final Naming naming;
+	private Naming naming;
 
 	public AntlrFragmentHelper(Naming naming) {
 		this.naming = naming;
+	}
+
+	/**
+	 * @since 2.8
+	 */
+	public AntlrFragmentHelper() {
+	}
+	
+	/**
+	 * @since 2.8
+	 */
+	@Override
+	public void registerNaming(Naming n) {
+		this.naming = n;
 	}
 
 	public String getAntlrTokenFileProviderClassName(Grammar grammar) {
