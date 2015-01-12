5a1f0230348f9a4225273cdd9dd53dd2e5a61f1d
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2015-01-12 13:34:36 +0100

    [xtext][junit4] Convenience for tests that work with resources
    ParseHelper and ValidationTestHelper focussed on EObjects as context.
    Added ResourceHelper to obtain a resource and added new methods to ValidationTestHelper.
    
    Change-Id: I99182ae28ecea053921a3984a93bbedefc5c52bc

--- plugins/org.eclipse.xtext.junit4/src/org/eclipse/xtext/junit4/validation/ValidationTestHelper.java
+++ plugins/org.eclipse.xtext.junit4/src/org/eclipse/xtext/junit4/validation/ValidationTestHelper.java
@@ -16,6 +16,7 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.xtext.diagnostics.Severity;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.util.CancelIndicator;
@@ -35,39 +36,19 @@ import com.google.common.collect.Iterables;
 public class ValidationTestHelper {
 
 	public List<Issue> validate(EObject model) {
-		IResourceValidator validator = ((XtextResource) model.eResource()).getResourceServiceProvider()
-				.getResourceValidator();
-		return validator.validate(model.eResource(), CheckMode.ALL, CancelIndicator.NullImpl);
+		return validate(model.eResource());
 	}
 	
 	public void assertNoIssues(final EObject model) {
-		final List<Issue> validate = validate(model);
-		if (!isEmpty(validate))
-			fail("Expected no issues, but got :" + getIssuesAsString(model, validate, new StringBuilder()));
+		assertNoIssues(model.eResource());
 	}
 	
 	public void assertNoErrors(final EObject model) {
-		final List<Issue> validate = validate(model);
-		Iterable<Issue> issues = filter(validate, new Predicate<Issue>() {
-			@Override
-			public boolean apply(Issue input) {
-				return Severity.ERROR == input.getSeverity();
-			}
-		});
-		if (!isEmpty(issues))
-			fail("Expected no errors, but got :" + getIssuesAsString(model, issues, new StringBuilder()));
+		assertNoErrors(model.eResource());
 	}
 
-	public void assertNoError(final EObject model, final String issuecode) {
-		final List<Issue> validate = validate(model);
-		Iterable<Issue> issues = filter(validate, new Predicate<Issue>() {
-			@Override
-			public boolean apply(Issue input) {
-				return issuecode.equals(input.getCode());
-			}
-		});
-		if (!isEmpty(issues))
-			fail("Expected no error '" + issuecode + "' but got " + getIssuesAsString(model, issues, new StringBuilder()));
+	public void assertNoError(final EObject model, final String issueCode) {
+		assertNoError(model.eResource(), issueCode);
 	}
 
 	/**
@@ -88,42 +69,15 @@ public class ValidationTestHelper {
 	/**
 	 * @since 2.4
 	 */
-	public void assertNoIssues(final EObject root, final EClass objectType) {
-		final List<Issue> validate = validate(root);
-		Iterable<Issue> issues = filter(validate, new Predicate<Issue>() {
-			@Override
-			public boolean apply(Issue input) {
-				EObject object = root.eResource().getEObject(input.getUriToProblem().fragment());
-				if (objectType.isInstance(object)) {
-					return true;
-				}
-				return false;
-			}
-		});
-		if (!isEmpty(issues))
-			fail("Expected no error on instances of  '" + objectType.getName() + "' but got " + getIssuesAsString(root, issues, new StringBuilder()));
+	public void assertNoIssues(final EObject model, final EClass objectType) {
+		assertNoIssues(model.eResource(), objectType);
 	}
 	
 	/**
 	 * @since 2.4
 	 */
-	public void assertNoIssue(final EObject root, final EClass objectType, final String issuecode) {
-		final List<Issue> validate = validate(root);
-		Iterable<Issue> issues = filter(validate, new Predicate<Issue>() {
-			@Override
-			public boolean apply(Issue input) {
-				if (issuecode.equals(input.getCode())) {
-					EObject object = root.eResource().getEObject(input.getUriToProblem().fragment());
-					if (objectType.isInstance(object)) {
-						return true;
-					}
-				}
-				return false;
-			}
-		});
-		if (!isEmpty(issues)) {
-			fail("Expected no error '" + issuecode + "' but got " + getIssuesAsString(root, issues, new StringBuilder()));
-		}
+	public void assertNoIssue(final EObject model, final EClass objectType, final String issueCode) {
+		assertNoIssue(model.eResource(), objectType, issueCode);
 	}
 
 	/**
@@ -150,26 +104,9 @@ public class ValidationTestHelper {
 	/**
 	 * @since 2.4
 	 */
-	public void assertIssue(final EObject model, final EClass objectType, final String code, final int offset, final int length,  final Severity severity,
-			final String... messageParts) {
-		final List<Issue> allIssues = validate(model);
-		final Iterable<Issue> validate = matchIssues(model, objectType, code, offset, length, severity, allIssues, messageParts);
-		if (Iterables.isEmpty(validate)) {
-			StringBuilder message = new StringBuilder("Expected ")
-				.append(severity)
-				.append(" '")
-				.append(code)
-				.append("' on ")
-				.append(objectType.getName())
-				.append(" at [")
-				.append(offset)
-				.append(":")
-				.append(length)
-				.append("] but got\n");
-			getIssuesAsString(model, allIssues, message);
-			assertEquals(Joiner.on('\n').join(messageParts), message.toString());
-			fail(message.toString());
-		}
+	public void assertIssue(EObject model, EClass objectType, String issueCode, int offset, int length,  Severity severity,
+			String... messageParts) {
+		assertIssue(model.eResource(), objectType, issueCode, offset, length, severity, messageParts);
 	}
 	
 	/**
@@ -183,72 +120,9 @@ public class ValidationTestHelper {
 	/**
 	 * @since 2.4
 	 */
-	public void assertNoIssues(final EObject model, final EClass objectType, final String code, final int offset, final int length,  final Severity severity,
-			final String... messageParts) {
-		final List<Issue> allIssues = validate(model);
-		final Iterable<Issue> validate = matchIssues(model, objectType, code, offset, length, severity, allIssues, messageParts);
-		if (!Iterables.isEmpty(validate)) {
-			StringBuilder message = new StringBuilder("Expected no ")
-				.append(severity)
-				.append(" '")
-				.append(code)
-				.append("' on ")
-				.append(objectType.getName())
-				.append(" but got\n");
-			getIssuesAsString(model, allIssues, message);
-			assertEquals(Joiner.on('\n').join(messageParts), message.toString());
-			fail(message.toString());
-		}
-	}
-
-	/**
-	 * @since 2.4
-	 */
-	protected Iterable<Issue> matchIssues(final EObject model, final EClass objectType, final String code,
-			final int offset, final int length, final Severity severity, final List<Issue> validate,
-			final String... messageParts) {
-		return Iterables.filter(validate, new Predicate<Issue>() {
-			@Override
-			public boolean apply(Issue input) {
-				if (Strings.equal(input.getCode(), code) && input.getSeverity()==severity) {
-					if ((offset < 0 || offset == input.getOffset()) && (length < 0 || length == input.getLength())) {
-						EObject object = model.eResource().getResourceSet().getEObject(input.getUriToProblem(), true);
-						if (objectType.isInstance(object)) {
-							for (String messagePart : messageParts) {
-								if (!input.getMessage().toLowerCase().contains(messagePart.toLowerCase())) {
-									return false;
-								}
-							}
-							return true;
-						}
-					}
-				}
-				return false;
-			}
-		});
-	}
-
-	/**
-	 * @since 2.4
-	 */
-	protected StringBuilder getIssuesAsString(final EObject model, final Iterable<Issue> issues, StringBuilder result) {
-		for (Issue issue : issues) {
-			URI uri = issue.getUriToProblem();
-			result.append(issue.getSeverity());
-			result.append(" (");
-			result.append(issue.getCode());
-			result.append(") '");
-			result.append(issue.getMessage());
-			result.append("'");
-			if (uri != null) {
-				EObject eObject = model.eResource().getResourceSet().getEObject(uri, true);
-				result.append(" on ");
-				result.append(eObject.eClass().getName());
-			}
-			result.append(", offset " + issue.getOffset() + ", length " + issue.getLength());
-			result.append("\n");
-		}
-		return result;
+	public void assertNoIssues(EObject model, EClass objectType, String issueCode, int offset, int length,  Severity severity,
+			String... messageParts) {
+		assertNoIssues(model.eResource(), objectType, issueCode, offset, length, severity, messageParts);
 	}
 
 	public void assertWarning(final EObject model, final EClass objectType, final String code,
@@ -272,4 +146,302 @@ public class ValidationTestHelper {
 		assertIssue(model, objectType, code, offset, length, Severity.WARNING, messageParts);
 	}
 
+	/**
+	 * @since 2.8
+	 */
+	public List<Issue> validate(Resource resource) {
+		IResourceValidator validator = ((XtextResource) resource).getResourceServiceProvider()
+				.getResourceValidator();
+		return validator.validate(resource, CheckMode.ALL, CancelIndicator.NullImpl);
+	}
+	
+	/**
+	 * @since 2.8
+	 */
+	public void assertNoIssues(final Resource resource) {
+		final List<Issue> validate = validate(resource);
+		if (!isEmpty(validate))
+			fail("Expected no issues, but got :" + getIssuesAsString(resource, validate, new StringBuilder()));
+	}
+	
+	/**
+	 * @since 2.8
+	 */
+	public void assertNoErrors(final Resource resource) {
+		final List<Issue> validate = validate(resource);
+		Iterable<Issue> issues = filter(validate, new Predicate<Issue>() {
+			@Override
+			public boolean apply(Issue input) {
+				return Severity.ERROR == input.getSeverity();
+			}
+		});
+		if (!isEmpty(issues))
+			fail("Expected no errors, but got :" + getIssuesAsString(resource, issues, new StringBuilder()));
+	}
+
+	/**
+	 * @since 2.8
+	 */
+	public void assertNoError(final Resource resource, final String issuecode) {
+		final List<Issue> validate = validate(resource);
+		Iterable<Issue> issues = filter(validate, new Predicate<Issue>() {
+			@Override
+			public boolean apply(Issue input) {
+				return issuecode.equals(input.getCode());
+			}
+		});
+		if (!isEmpty(issues))
+			fail("Expected no error '" + issuecode + "' but got " + getIssuesAsString(resource, issues, new StringBuilder()));
+	}
+
+	/**
+	 * @since 2.8
+	 */
+	public void assertNoErrors(final Resource resource, final EClass objectType, final String code,
+			final String... messageParts) {
+		assertNoIssues(resource, objectType, code, Severity.ERROR, messageParts);
+	}
+	
+	/**
+	 * @since 2.8
+	 */
+	public void assertNoErrors(final Resource resource, final String code) {
+		assertNoIssues(resource, EcorePackage.Literals.EOBJECT, code, Severity.ERROR);
+	}
+	
+	/**
+	 * @since 2.8
+	 */
+	public void assertNoIssues(final Resource resource, final EClass objectType) {
+		final List<Issue> validate = validate(resource);
+		Iterable<Issue> issues = filter(validate, new Predicate<Issue>() {
+			@Override
+			public boolean apply(Issue input) {
+				EObject object = resource.getEObject(input.getUriToProblem().fragment());
+				if (objectType.isInstance(object)) {
+					return true;
+				}
+				return false;
+			}
+		});
+		if (!isEmpty(issues))
+			fail("Expected no error on instances of  '" + objectType.getName() + "' but got " + getIssuesAsString(resource, issues, new StringBuilder()));
+	}
+	
+	/**
+	 * @since 2.8
+	 */
+	public void assertNoIssue(final Resource resource, final EClass objectType, final String issuecode) {
+		final List<Issue> validate = validate(resource);
+		Iterable<Issue> issues = filter(validate, new Predicate<Issue>() {
+			@Override
+			public boolean apply(Issue input) {
+				if (issuecode.equals(input.getCode())) {
+					EObject object = resource.getEObject(input.getUriToProblem().fragment());
+					if (objectType.isInstance(object)) {
+						return true;
+					}
+				}
+				return false;
+			}
+		});
+		if (!isEmpty(issues)) {
+			fail("Expected no error '" + issuecode + "' but got " + getIssuesAsString(resource, issues, new StringBuilder()));
+		}
+	}
+
+	/**
+	 * @since 2.8
+	 */
+	public void assertError(final Resource resource, final EClass objectType, final String code, int offset, int length, 
+			final String... messageParts) {
+		assertIssue(resource, objectType, code, offset, length, Severity.ERROR, messageParts);
+	}
+
+	/**
+	 * @since 2.8
+	 */
+	public void assertError(final Resource resource, final EClass objectType, final String code,
+			final String... messageParts) {
+		assertIssue(resource, objectType, code, Severity.ERROR, messageParts);
+	}
+	
+	/**
+	 * @since 2.8
+	 */
+	public void assertIssue(final Resource resource, final EClass objectType, final String code, final Severity severity,
+			final String... messageParts) {
+		assertIssue(resource, objectType, code, -1, -1, severity, messageParts);
+	}
+	
+	/**
+	 * @since 2.8
+	 */
+	public void assertIssue(final Resource resource, final EClass objectType, final String code, final int offset, final int length,  final Severity severity,
+			final String... messageParts) {
+		final List<Issue> allIssues = validate(resource);
+		final Iterable<Issue> validate = matchIssues(resource, objectType, code, offset, length, severity, allIssues, messageParts);
+		if (Iterables.isEmpty(validate)) {
+			StringBuilder message = new StringBuilder("Expected ")
+				.append(severity)
+				.append(" '")
+				.append(code)
+				.append("' on ")
+				.append(objectType.getName())
+				.append(" at [")
+				.append(offset)
+				.append(":")
+				.append(length)
+				.append("] but got\n");
+			getIssuesAsString(resource, allIssues, message);
+			assertEquals(Joiner.on('\n').join(messageParts), message.toString());
+			fail(message.toString());
+		}
+	}
+	
+	/**
+	 * @since 2.8
+	 */
+	public void assertNoIssues(final Resource resource, final EClass objectType, final String code, final Severity severity,
+			final String... messageParts) {
+		assertNoIssues(resource, objectType, code, -1, -1, severity, messageParts);
+	}
+	
+	/**
+	 * @since 2.8
+	 */
+	public void assertNoIssues(final Resource resource, final EClass objectType, final String code, final int offset, final int length,  final Severity severity,
+			final String... messageParts) {
+		final List<Issue> allIssues = validate(resource);
+		final Iterable<Issue> validate = matchIssues(resource, objectType, code, offset, length, severity, allIssues, messageParts);
+		if (!Iterables.isEmpty(validate)) {
+			StringBuilder message = new StringBuilder("Expected no ")
+				.append(severity)
+				.append(" '")
+				.append(code)
+				.append("' on ")
+				.append(objectType.getName())
+				.append(" but got\n");
+			getIssuesAsString(resource, allIssues, message);
+			assertEquals(Joiner.on('\n').join(messageParts), message.toString());
+			fail(message.toString());
+		}
+	}
+
+	/**
+	 * @since 2.8
+	 */
+	public void assertWarning(final Resource resource, final EClass objectType, final String code,
+			final String... messageParts) {
+		assertIssue(resource, objectType, code, Severity.WARNING, messageParts);
+	}
+
+	/**
+	 * @since 2.8
+	 */
+	public void assertNoWarnings(final Resource resource, final EClass objectType, final String code,
+			final String... messageParts) {
+		assertNoIssues(resource, objectType, code, Severity.WARNING, messageParts);
+	}
+
+	/**
+	 * @since 2.8
+	 */
+	public void assertWarning(final Resource resource, final EClass objectType, final String code, int offset, int length, 
+			final String... messageParts) {
+		assertIssue(resource, objectType, code, offset, length, Severity.WARNING, messageParts);
+	}
+
+	/**
+	 * @since 2.4
+	 */
+	protected Iterable<Issue> matchIssues(final EObject model, final EClass objectType, final String code,
+			final int offset, final int length, final Severity severity, final List<Issue> validate,
+			final String... messageParts) {
+		return doMatchIssues(model.eResource(), objectType, code, offset, length, severity, validate, messageParts);
+	}
+	
+	/**
+	 * @since 2.8
+	 */
+	protected Iterable<Issue> matchIssues(final Resource resource, final EClass objectType, final String code,
+			final int offset, final int length, final Severity severity, final List<Issue> validate,
+			final String... messageParts) {
+		// keep the original impl of #matchIssues(EObject, ..) in the call graph  
+		List<EObject> contents = resource.getContents();
+		if (contents.size() > 1) {
+			return matchIssues(contents.get(0), objectType, code, offset, length, severity, validate, messageParts);
+		}
+		return doMatchIssues(resource, objectType, code, offset, length, severity, validate, messageParts);
+	}
+
+	/**
+	 * @since 2.8
+	 */
+	protected Iterable<Issue> doMatchIssues(final Resource resource, final EClass objectType, final String code,
+			final int offset, final int length, final Severity severity, final List<Issue> validate,
+			final String... messageParts) {
+		return Iterables.filter(validate, new Predicate<Issue>() {
+			@Override
+			public boolean apply(Issue input) {
+				if (Strings.equal(input.getCode(), code) && input.getSeverity()==severity) {
+					if ((offset < 0 || offset == input.getOffset()) && (length < 0 || length == input.getLength())) {
+						EObject object = resource.getResourceSet().getEObject(input.getUriToProblem(), true);
+						if (objectType.isInstance(object)) {
+							for (String messagePart : messageParts) {
+								if (!input.getMessage().toLowerCase().contains(messagePart.toLowerCase())) {
+									return false;
+								}
+							}
+							return true;
+						}
+					}
+				}
+				return false;
+			}
+		});
+	}
+	
+	/**
+	 * @since 2.4
+	 */
+	protected StringBuilder getIssuesAsString(final EObject model, final Iterable<Issue> issues, StringBuilder result) {
+		return doGetIssuesAsString(model.eResource(), issues, result);
+	}
+	
+	/**
+	 * @since 2.8
+	 */
+	protected StringBuilder getIssuesAsString(final Resource resource, final Iterable<Issue> issues, StringBuilder result) {
+		// keep the original impl of #getIssuesAsString(EObject, ..) in the call graph  
+		List<EObject> contents = resource.getContents();
+		if (contents.size() > 1) {
+			return getIssuesAsString(contents.get(0), issues, result);
+		}
+		return doGetIssuesAsString(resource, issues, result);
+	}
+
+	/**
+	 * @since 2.8
+	 */
+	protected StringBuilder doGetIssuesAsString(Resource resource, final Iterable<Issue> issues, StringBuilder result) {
+		for (Issue issue : issues) {
+			URI uri = issue.getUriToProblem();
+			result.append(issue.getSeverity());
+			result.append(" (");
+			result.append(issue.getCode());
+			result.append(") '");
+			result.append(issue.getMessage());
+			result.append("'");
+			if (uri != null) {
+				EObject eObject = resource.getResourceSet().getEObject(uri, true);
+				result.append(" on ");
+				result.append(eObject.eClass().getName());
+			}
+			result.append(", offset " + issue.getOffset() + ", length " + issue.getLength());
+			result.append("\n");
+		}
+		return result;
+	}
+	
 }
