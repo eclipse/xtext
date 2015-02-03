ef1dd4949b5f5b329013d0fdc6db1a514a266214
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2015-02-03 19:46:36 +0100

    [serializer] improve debug output and comments MyLangSyntacticSequencer
    Change-Id: I6bfcdc0e6b6beef0c4427828c4f6280e6d3fe0a6

--- plugins/org.eclipse.xtext.util/src/org/eclipse/xtext/util/formallang/ProductionFormatter.java
+++ plugins/org.eclipse.xtext.util/src/org/eclipse/xtext/util/formallang/ProductionFormatter.java
@@ -27,6 +27,8 @@ public class ProductionFormatter<ELEMENT, TOKEN> implements Function<Production<
 
 	protected int autoWrapChars = 140;
 
+	protected int autoWrapChildren = 5;
+
 	protected String indent = "    ";
 
 	protected Function<TOKEN, String> tokenToString = new ObjToStrFunction<TOKEN>();
@@ -54,18 +56,18 @@ public class ProductionFormatter<ELEMENT, TOKEN> implements Function<Production<
 		}
 		Iterable<ELEMENT> alternative = adapter.getAlternativeChildren(grammarElement);
 		if (alternative != null)
-			return format(adapter, grammarElement, alternative, " | ", false, needParenthesis, 5);
+			return format(adapter, grammarElement, alternative, " | ", false, needParenthesis);
 		Iterable<ELEMENT> group = adapter.getSequentialChildren(grammarElement);
 		if (group != null)
-			return format(adapter, grammarElement, group, " ", false, needParenthesis, 5);
+			return format(adapter, grammarElement, group, " ", false, needParenthesis);
 		Iterable<ELEMENT> ungroup = adapter.getUnorderedChildren(grammarElement);
 		if (ungroup != null)
-			return format(adapter, grammarElement, ungroup, " & ", false, needParenthesis, 5);
+			return format(adapter, grammarElement, ungroup, " & ", false, needParenthesis);
 		return "<unknown>";
 	}
 
 	protected String format(Production<ELEMENT, TOKEN> adapter, ELEMENT element, Iterable<ELEMENT> children,
-			String separator, boolean needWrap, boolean needParenthesis, int maxChildren) {
+			String separator, boolean needWrap, boolean needParenthesis) {
 		List<String> childStrs2 = Lists.newArrayList();
 		int width2 = 0;
 		for (ELEMENT child : children) {
@@ -75,7 +77,7 @@ public class ProductionFormatter<ELEMENT, TOKEN> implements Function<Production<
 			if (childStr.contains("\n"))
 				needWrap = true;
 		}
-		if (childStrs2.size() > maxChildren)
+		if (childStrs2.size() > autoWrapChildren)
 			needWrap = true;
 		if (width2 > autoWrapChars)
 			needWrap = true;
@@ -101,6 +103,11 @@ public class ProductionFormatter<ELEMENT, TOKEN> implements Function<Production<
 
 	public ProductionFormatter<ELEMENT, TOKEN> setAutoWrapChars(int autoWrapChars) {
 		this.autoWrapChars = autoWrapChars;
+		return this;
+	}
+
+	public ProductionFormatter<ELEMENT, TOKEN> setAutoWrapChildren(int autoWrapChildren) {
+		this.autoWrapChildren = autoWrapChildren;
 		return this;
 	}
 
