f9ba162ef3eae130a33fbd27a9e5a87ca1377360
Holger Schill <Holger.Schill@itemis.de>
2015-02-04 10:46:35 +0100

    Cosmetic change see comments on https://git.eclipse.org/r/#/c/40909/4

--- plugins/org.eclipse.xtext.common.types.ui/src/org/eclipse/xtext/common/types/ui/editor/JvmTypesAwareDirtyStateEditorSupport.java
+++ plugins/org.eclipse.xtext.common.types.ui/src/org/eclipse/xtext/common/types/ui/editor/JvmTypesAwareDirtyStateEditorSupport.java
@@ -7,24 +7,16 @@
  *******************************************************************************/
 package org.eclipse.xtext.common.types.ui.editor;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.Set;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.URIConverter;
 import org.eclipse.xtext.common.types.TypesPackage;
 import org.eclipse.xtext.common.types.access.impl.URIHelperConstants;
 import org.eclipse.xtext.resource.IEObjectDescription;
 import org.eclipse.xtext.resource.IResourceDescription;
-import org.eclipse.xtext.resource.XtextResource;
-import org.eclipse.xtext.resource.IResourceDescription.Event;
 import org.eclipse.xtext.ui.editor.DirtyStateEditorSupport;
-
-import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
 
 /**
  * @author Holger Schill - Initial contribution and API
@@ -32,34 +24,16 @@ import com.google.common.collect.Sets;
 public class JvmTypesAwareDirtyStateEditorSupport extends DirtyStateEditorSupport {
 	
 	@Override
-	protected Collection<Resource> collectAffectedResources(XtextResource resource, Event event) {
-		List<Resource> result = Lists.newArrayListWithExpectedSize(4);
-		ResourceSet resourceSet = resource.getResourceSet();
-		URIConverter converter = resourceSet.getURIConverter();
-		Set<URI> normalizedURIs = Sets.newHashSetWithExpectedSize(event.getDeltas().size());
-		for(IResourceDescription.Delta delta: event.getDeltas()) {
-			normalizedURIs.add(converter.normalize(delta.getUri()));
-			if(delta.getNew() != null){
-				Iterable<IEObjectDescription> exportedJvmTypes = delta.getNew().getExportedObjectsByType(TypesPackage.Literals.JVM_GENERIC_TYPE);
-				for(IEObjectDescription jvmTypeDesc : exportedJvmTypes){
-					normalizedURIs.add(URIHelperConstants.OBJECTS_URI.appendSegment(jvmTypeDesc.getQualifiedName().toString()));
-				}
+	protected void processDelta(IResourceDescription.Delta delta, ResourceSet resourceSet, Set<URI> normalizedURIs) {
+		super.processDelta(delta, resourceSet, normalizedURIs);
+		if(delta.getNew() != null){
+			Iterable<IEObjectDescription> exportedJvmTypes = delta.getNew().getExportedObjectsByType(TypesPackage.Literals.JVM_GENERIC_TYPE);
+			for(IEObjectDescription jvmTypeDesc : exportedJvmTypes){
+				URI uriToJvmType = URIHelperConstants.OBJECTS_URI.appendSegment(jvmTypeDesc.getQualifiedName().toString());
+				Resource jvmResourceInResourceSet = resourceSet.getResource(uriToJvmType, false);
+				if(jvmResourceInResourceSet != null)
+					normalizedURIs.add(jvmResourceInResourceSet.getURI());
 			}
 		}
-		List<Resource> resources = resourceSet.getResources();
-		for(int i = 0; i< resources.size(); i++) {
-			Resource res = resources.get(i);
-			if (res != resource && res != null) {
-				URI uri = res.getURI();
-				try {
-					uri = converter.normalize(uri);
-				} catch (org.eclipse.xtext.resource.ClasspathUriResolutionException e) {
-					// ignore, since the classpath might be broken.
-				}
-				if (normalizedURIs.contains(uri))
-					result.add(res);
-			}
-		}
-		return result;
 	}
 }
