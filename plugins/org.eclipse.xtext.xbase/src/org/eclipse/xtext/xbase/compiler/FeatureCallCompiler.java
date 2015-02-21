25327ab93d6ebeed668ed8d17b552874406986f1
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2015-02-21 18:24:31 +0100

    [460480]: [xtext] Fixed License headers
    see https://bugs.eclipse.org/bugs/show_bug.cgi?id=460480
    
    Change-Id: I9407c8ad9b42f12de05e2124d7ba4d2a3939a6e4

--- plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/compiler/FeatureCallCompiler.java
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/compiler/FeatureCallCompiler.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
-is * Copyright (c) 2011 itemis AG (http://www.itemis.eu) and others.
+ * Copyright (c) 2011 itemis AG (http://www.itemis.eu) and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse protected License v1.0
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
