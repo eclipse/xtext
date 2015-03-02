4708fde10b4aa10cd57f72ace5547b8fb9af392e
akosyakov <anton.kosyakov@itemis.de>
2015-03-02 14:23:38 +0100

    [intellij] Generated an intellij plugin for Xtext language
    Change-Id: Iaa62544501f605d113ef978b3945e465201ce28e
    Signed-off-by: akosyakov <anton.kosyakov@itemis.de>

---
+++ intellij/org.eclipse.xtext.lang.idea/src-gen/org/eclipse/xtext/idea/XtextExtensionFactory.java
@@ -0,0 +1,19 @@
+package org.eclipse.xtext.idea;
+
+import org.eclipse.xtext.idea.lang.XtextLanguage;
+
+import com.intellij.openapi.extensions.ExtensionFactory;
+
+public class XtextExtensionFactory implements ExtensionFactory {
+
+	public Object createInstance(final String factoryArgument, final String implementationClass) {
+		Class<?> clazz;
+		try {
+			clazz = Class.forName(implementationClass);
+		} catch (ClassNotFoundException e) {
+			throw new IllegalArgumentException("Couldn't load "+implementationClass, e);
+		}
+		return XtextLanguage.INSTANCE.<Object> getInstance(clazz);
+	}
+
+}
---
+++ intellij/org.eclipse.xtext.lang.idea/src-gen/org/eclipse/xtext/idea/lang/parser/XtextParserDefinition.java
@@ -0,0 +1,92 @@
+package org.eclipse.xtext.idea.lang.parser;
+
+import org.eclipse.xtext.psi.impl.PsiEObjectReference;
+import org.eclipse.xtext.idea.lang.XtextElementTypeProvider;
+import org.eclipse.xtext.idea.lang.psi.impl.XtextFileImpl;
+import org.eclipse.xtext.common.idea.lang.parser.TerminalsParserDefinition;
+import org.eclipse.xtext.psi.impl.PsiNamedEObjectImpl;
+
+import com.google.inject.Inject;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.FileViewProvider;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.tree.IElementType;
+
+public class XtextParserDefinition extends TerminalsParserDefinition {
+
+	@Inject 
+	private XtextElementTypeProvider elementTypeProvider;
+
+	public PsiFile createFile(FileViewProvider viewProvider) {
+		return new XtextFileImpl(viewProvider);
+	}
+
+	@Override
+	@SuppressWarnings("rawtypes")
+	public PsiElement createElement(ASTNode node) {
+		IElementType elementType = node.getElementType();
+		if (elementType == elementTypeProvider.getAbstractMetamodelDeclaration_GeneratedMetamodelParserRuleCall_0ElementType()) {
+			return new PsiNamedEObjectImpl(node,
+				elementTypeProvider.getGeneratedMetamodel_NameIDTerminalRuleCall_1_0ElementType()
+			);
+		}
+		if (elementType == elementTypeProvider.getAbstractRule_ParserRuleParserRuleCall_0ElementType()) {
+			return new PsiNamedEObjectImpl(node,
+				elementTypeProvider.getParserRule_NameIDTerminalRuleCall_0_0ElementType()
+			);
+		}
+		if (elementType == elementTypeProvider.getAbstractRule_TerminalRuleParserRuleCall_1ElementType()) {
+			return new PsiNamedEObjectImpl(node,
+				elementTypeProvider.getTerminalRule_NameIDTerminalRuleCall_1_0_1_0ElementType(),
+				elementTypeProvider.getTerminalRule_NameIDTerminalRuleCall_1_1_0_0ElementType()
+			);
+		}
+		if (elementType == elementTypeProvider.getAbstractRule_EnumRuleParserRuleCall_2ElementType()) {
+			return new PsiNamedEObjectImpl(node,
+				elementTypeProvider.getEnumRule_NameIDTerminalRuleCall_1_0ElementType()
+			);
+		}
+		if (elementType == elementTypeProvider.getGrammar_UsedGrammarsGrammarCrossReference_2_1_0ElementType()) {
+			return new PsiEObjectReference(node);
+		}
+		if (elementType == elementTypeProvider.getGrammar_UsedGrammarsGrammarCrossReference_2_2_1_0ElementType()) {
+			return new PsiEObjectReference(node);
+		}
+		if (elementType == elementTypeProvider.getGrammar_HiddenTokensAbstractRuleCrossReference_3_2_0_0ElementType()) {
+			return new PsiEObjectReference(node);
+		}
+		if (elementType == elementTypeProvider.getGrammar_HiddenTokensAbstractRuleCrossReference_3_2_1_1_0ElementType()) {
+			return new PsiEObjectReference(node);
+		}
+		if (elementType == elementTypeProvider.getGeneratedMetamodel_EPackageEPackageCrossReference_2_0ElementType()) {
+			return new PsiEObjectReference(node);
+		}
+		if (elementType == elementTypeProvider.getReferencedMetamodel_EPackageEPackageCrossReference_1_0ElementType()) {
+			return new PsiEObjectReference(node);
+		}
+		if (elementType == elementTypeProvider.getParserRule_HiddenTokensAbstractRuleCrossReference_2_2_0_0ElementType()) {
+			return new PsiEObjectReference(node);
+		}
+		if (elementType == elementTypeProvider.getParserRule_HiddenTokensAbstractRuleCrossReference_2_2_1_1_0ElementType()) {
+			return new PsiEObjectReference(node);
+		}
+		if (elementType == elementTypeProvider.getTypeRef_MetamodelAbstractMetamodelDeclarationCrossReference_0_0_0ElementType()) {
+			return new PsiEObjectReference(node);
+		}
+		if (elementType == elementTypeProvider.getTypeRef_ClassifierEClassifierCrossReference_1_0ElementType()) {
+			return new PsiEObjectReference(node);
+		}
+		if (elementType == elementTypeProvider.getRuleCall_RuleAbstractRuleCrossReference_0ElementType()) {
+			return new PsiEObjectReference(node);
+		}
+		if (elementType == elementTypeProvider.getPredicatedRuleCall_RuleAbstractRuleCrossReference_1_0ElementType()) {
+			return new PsiEObjectReference(node);
+		}
+		if (elementType == elementTypeProvider.getEnumLiteralDeclaration_EnumLiteralEEnumLiteralCrossReference_0_0ElementType()) {
+			return new PsiEObjectReference(node);
+		}
+		return super.createElement(node);
+	}
+
+}
