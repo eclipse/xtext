bede3bf12013a473f0d17d72f943f20e367a6ac2
Karsten Thoms <karsten.thoms@itemis.de>
2015-03-25 15:33:52 +0100

    [Bug 406066] Add additional test cases
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- tests/org.eclipse.xtend.core.tests/xtend-gen/org/eclipse/xtend/core/tests/compiler/CompilerBug406066Test.java
+++ tests/org.eclipse.xtend.core.tests/xtend-gen/org/eclipse/xtend/core/tests/compiler/CompilerBug406066Test.java
@@ -50,4 +50,86 @@ public class CompilerBug406066Test extends AbstractXtendCompilerTest {
     _builder_1.newLine();
     this.assertCompilesTo(_builder, _builder_1);
   }
+  
+  @Test
+  public void testBug406066_02() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("class Error {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("def static err(Integer i) {");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("i.toString");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("val java.lang.Error e = new AssertionError()");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    StringConcatenation _builder_1 = new StringConcatenation();
+    _builder_1.append("@SuppressWarnings(\"all\")");
+    _builder_1.newLine();
+    _builder_1.append("public class Error {");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("public static void err(final Integer i) {");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("i.toString();");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("final java.lang.Error e = new AssertionError();");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("}");
+    _builder_1.newLine();
+    _builder_1.append("}");
+    _builder_1.newLine();
+    this.assertCompilesTo(_builder, _builder_1);
+  }
+  
+  @Test
+  public void testBug406066_03() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("class Error2 {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("def static err(Integer i) {");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("i.toString");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("val Error e = new AssertionError()");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    StringConcatenation _builder_1 = new StringConcatenation();
+    _builder_1.append("@SuppressWarnings(\"all\")");
+    _builder_1.newLine();
+    _builder_1.append("public class Error2 {");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("public static void err(final Integer i) {");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("i.toString();");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("final Error e = new AssertionError();");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("}");
+    _builder_1.newLine();
+    _builder_1.append("}");
+    _builder_1.newLine();
+    this.assertCompilesTo(_builder, _builder_1);
+  }
 }
