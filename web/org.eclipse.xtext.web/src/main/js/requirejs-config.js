83759c240ff05a380bec32e10f21047494590a37
Miro Spönemann <miro.spoenemann@itemis.de>
2015-05-21 18:13:09 +0200

    [web] The Orion Editor library is now packaged together with Xtext as workaround for the Orion bugs 467849 and 467376
    Signed-off-by: Miro Spönemann <miro.spoenemann@itemis.de>

--- web/org.eclipse.xtext.web/src/main/js/requirejs-config.js
+++ web/org.eclipse.xtext.web/src/main/js/requirejs-config.js
@@ -3,15 +3,9 @@
 	name: "xtext/xtext",
 	paths: {
 		"jquery": "empty:",
-		"orion/Deferred": "empty:",
-		"orion/keyBinding": "empty:",
-		"orion/editor/textView": "empty:",
-		"orion/editor/textModel": "empty:",
-		"orion/editor/textTheme": "empty:",
-		"orion/editor/projectionTextModel": "empty:",
-		"orion/editor/editor": "empty:",
-		"orion/editor/editorFeatures": "empty:",
-		"orion/editor/contentAssist": "empty:",
-		"orion/editor/textStyler": "empty:"
-	}
+		"text": "../../../build/tmp/js/requirejs-plugins/text",
+		"i18n": "../../../build/tmp/js/requirejs-plugins/i18n"
+	},
+	include: [ "orion/editor/stylers/lib/syntax" ],
+	exclude: [ "jquery", "text" ]
 })
