e9f0e284c42cda97ea57086c31ba2f5160049b26
Miro Spönemann <miro.spoenemann@itemis.de>
2015-05-28 08:55:34 +0200

    [web] Removed declarative content assist proposal provider
    Signed-off-by: Miro Spönemann <miro.spoenemann@itemis.de>

--- web/org.eclipse.xtext.web/src/test/xtend-gen/org/eclipse/xtext/web/server/test/languages/contentassist/StatemachineWebContentProposalProvider.java
+++ web/org.eclipse.xtext.web/src/test/xtend-gen/org/eclipse/xtext/web/server/test/languages/contentassist/StatemachineWebContentProposalProvider.java
@@ -8,108 +8,169 @@
 package org.eclipse.xtext.web.server.test.languages.contentassist;
 
 import com.google.common.base.Objects;
+import com.google.inject.Inject;
+import java.util.Arrays;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.AbstractElement;
+import org.eclipse.xtext.AbstractRule;
+import org.eclipse.xtext.Assignment;
+import org.eclipse.xtext.CrossReference;
+import org.eclipse.xtext.Keyword;
+import org.eclipse.xtext.RuleCall;
+import org.eclipse.xtext.TerminalRule;
 import org.eclipse.xtext.ide.editor.contentassist.ContentAssistContext;
 import org.eclipse.xtext.naming.QualifiedName;
 import org.eclipse.xtext.resource.IEObjectDescription;
 import org.eclipse.xtext.scoping.IScope;
 import org.eclipse.xtext.scoping.IScopeProvider;
 import org.eclipse.xtext.util.IAcceptor;
+import org.eclipse.xtext.web.example.statemachine.services.StatemachineGrammarAccess;
 import org.eclipse.xtext.web.example.statemachine.statemachine.StatemachinePackage;
-import org.eclipse.xtext.web.server.contentassist.AbstractDeclarativeWebContentProposalProvider;
 import org.eclipse.xtext.web.server.contentassist.ContentAssistResult;
-import org.eclipse.xtext.web.server.contentassist.Proposals;
+import org.eclipse.xtext.web.server.contentassist.WebContentProposalProvider;
+import org.eclipse.xtext.xbase.lib.Extension;
 import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.IterableExtensions;
 import org.eclipse.xtext.xbase.lib.ObjectExtensions;
 import org.eclipse.xtext.xbase.lib.Procedures.Procedure1;
 
 @SuppressWarnings("all")
-public class StatemachineWebContentProposalProvider extends AbstractDeclarativeWebContentProposalProvider {
-  @Proposals(rule = "BOOLEAN")
-  public void completeBoolean(final ContentAssistContext context, final IAcceptor<ContentAssistResult.Entry> acceptor) {
-    String _prefix = context.getPrefix();
-    ContentAssistResult.Entry _entry = new ContentAssistResult.Entry(ContentAssistResult.TERMINAL, _prefix);
-    final Procedure1<ContentAssistResult.Entry> _function = new Procedure1<ContentAssistResult.Entry>() {
-      @Override
-      public void apply(final ContentAssistResult.Entry it) {
-        it.setProposal("true");
-      }
-    };
-    ContentAssistResult.Entry _doubleArrow = ObjectExtensions.<ContentAssistResult.Entry>operator_doubleArrow(_entry, _function);
-    acceptor.accept(_doubleArrow);
-    String _prefix_1 = context.getPrefix();
-    ContentAssistResult.Entry _entry_1 = new ContentAssistResult.Entry(ContentAssistResult.TERMINAL, _prefix_1);
-    final Procedure1<ContentAssistResult.Entry> _function_1 = new Procedure1<ContentAssistResult.Entry>() {
-      @Override
-      public void apply(final ContentAssistResult.Entry it) {
-        it.setProposal("false");
-      }
-    };
-    ContentAssistResult.Entry _doubleArrow_1 = ObjectExtensions.<ContentAssistResult.Entry>operator_doubleArrow(_entry_1, _function_1);
-    acceptor.accept(_doubleArrow_1);
-  }
+public class StatemachineWebContentProposalProvider extends WebContentProposalProvider {
+  @Inject
+  @Extension
+  private StatemachineGrammarAccess _statemachineGrammarAccess;
   
-  @Proposals(rule = "Event", feature = "signal")
-  public void completeEventSignal(final ContentAssistContext context, final IAcceptor<ContentAssistResult.Entry> acceptor) {
-    IScopeProvider _scopeProvider = this.getScopeProvider();
-    EObject _currentModel = context.getCurrentModel();
-    final IScope scope = _scopeProvider.getScope(_currentModel, StatemachinePackage.Literals.EVENT__SIGNAL);
-    Iterable<IEObjectDescription> _allElements = scope.getAllElements();
-    final Function1<IEObjectDescription, Boolean> _function = new Function1<IEObjectDescription, Boolean>() {
-      @Override
-      public Boolean apply(final IEObjectDescription it) {
-        EClass _eClass = it.getEClass();
-        return Boolean.valueOf(Objects.equal(_eClass, StatemachinePackage.Literals.INPUT_SIGNAL));
+  @Override
+  protected void _createProposals(final RuleCall ruleCall, final ContentAssistContext context, final IAcceptor<ContentAssistResult.Entry> acceptor) {
+    AbstractRule _rule = ruleCall.getRule();
+    boolean _matched = false;
+    if (!_matched) {
+      TerminalRule _bOOLEANRule = this._statemachineGrammarAccess.getBOOLEANRule();
+      if (Objects.equal(_rule, _bOOLEANRule)) {
+        _matched=true;
+        String _prefix = context.getPrefix();
+        ContentAssistResult.Entry _entry = new ContentAssistResult.Entry(ContentAssistResult.TERMINAL, _prefix);
+        final Procedure1<ContentAssistResult.Entry> _function = new Procedure1<ContentAssistResult.Entry>() {
+          @Override
+          public void apply(final ContentAssistResult.Entry it) {
+            it.setProposal("true");
+          }
+        };
+        ContentAssistResult.Entry _doubleArrow = ObjectExtensions.<ContentAssistResult.Entry>operator_doubleArrow(_entry, _function);
+        acceptor.accept(_doubleArrow);
+        String _prefix_1 = context.getPrefix();
+        ContentAssistResult.Entry _entry_1 = new ContentAssistResult.Entry(ContentAssistResult.TERMINAL, _prefix_1);
+        final Procedure1<ContentAssistResult.Entry> _function_1 = new Procedure1<ContentAssistResult.Entry>() {
+          @Override
+          public void apply(final ContentAssistResult.Entry it) {
+            it.setProposal("false");
+          }
+        };
+        ContentAssistResult.Entry _doubleArrow_1 = ObjectExtensions.<ContentAssistResult.Entry>operator_doubleArrow(_entry_1, _function_1);
+        acceptor.accept(_doubleArrow_1);
       }
-    };
-    Iterable<IEObjectDescription> _filter = IterableExtensions.<IEObjectDescription>filter(_allElements, _function);
-    for (final IEObjectDescription description : _filter) {
-      String _prefix = context.getPrefix();
-      ContentAssistResult.Entry _entry = new ContentAssistResult.Entry(ContentAssistResult.CROSSREF, _prefix);
-      final Procedure1<ContentAssistResult.Entry> _function_1 = new Procedure1<ContentAssistResult.Entry>() {
-        @Override
-        public void apply(final ContentAssistResult.Entry it) {
-          QualifiedName _name = description.getName();
-          String _string = _name.toString();
-          it.setProposal(_string);
-          it.setDescription("input signal");
-        }
-      };
-      ContentAssistResult.Entry _doubleArrow = ObjectExtensions.<ContentAssistResult.Entry>operator_doubleArrow(_entry, _function_1);
-      acceptor.accept(_doubleArrow);
+    }
+    if (!_matched) {
+      super._createProposals(ruleCall, context, acceptor);
     }
   }
   
-  @Proposals(rule = "Command", feature = "signal")
-  public void completeCommandSignal(final ContentAssistContext context, final IAcceptor<ContentAssistResult.Entry> acceptor) {
-    IScopeProvider _scopeProvider = this.getScopeProvider();
-    EObject _currentModel = context.getCurrentModel();
-    final IScope scope = _scopeProvider.getScope(_currentModel, StatemachinePackage.Literals.COMMAND__SIGNAL);
-    Iterable<IEObjectDescription> _allElements = scope.getAllElements();
-    final Function1<IEObjectDescription, Boolean> _function = new Function1<IEObjectDescription, Boolean>() {
-      @Override
-      public Boolean apply(final IEObjectDescription it) {
-        EClass _eClass = it.getEClass();
-        return Boolean.valueOf(Objects.equal(_eClass, StatemachinePackage.Literals.OUTPUT_SIGNAL));
-      }
-    };
-    Iterable<IEObjectDescription> _filter = IterableExtensions.<IEObjectDescription>filter(_allElements, _function);
-    for (final IEObjectDescription description : _filter) {
-      String _prefix = context.getPrefix();
-      ContentAssistResult.Entry _entry = new ContentAssistResult.Entry(ContentAssistResult.CROSSREF, _prefix);
-      final Procedure1<ContentAssistResult.Entry> _function_1 = new Procedure1<ContentAssistResult.Entry>() {
-        @Override
-        public void apply(final ContentAssistResult.Entry it) {
-          QualifiedName _name = description.getName();
-          String _string = _name.toString();
-          it.setProposal(_string);
-          it.setDescription("output signal");
+  @Override
+  protected void _createProposals(final Assignment assignment, final ContentAssistContext context, final IAcceptor<ContentAssistResult.Entry> acceptor) {
+    boolean _matched = false;
+    if (!_matched) {
+      StatemachineGrammarAccess.EventElements _eventAccess = this._statemachineGrammarAccess.getEventAccess();
+      Assignment _signalAssignment_0 = _eventAccess.getSignalAssignment_0();
+      if (Objects.equal(assignment, _signalAssignment_0)) {
+        _matched=true;
+        IScopeProvider _scopeProvider = this.getScopeProvider();
+        EObject _currentModel = context.getCurrentModel();
+        final IScope scope = _scopeProvider.getScope(_currentModel, StatemachinePackage.Literals.EVENT__SIGNAL);
+        Iterable<IEObjectDescription> _allElements = scope.getAllElements();
+        final Function1<IEObjectDescription, Boolean> _function = new Function1<IEObjectDescription, Boolean>() {
+          @Override
+          public Boolean apply(final IEObjectDescription it) {
+            EClass _eClass = it.getEClass();
+            return Boolean.valueOf(Objects.equal(_eClass, StatemachinePackage.Literals.INPUT_SIGNAL));
+          }
+        };
+        Iterable<IEObjectDescription> _filter = IterableExtensions.<IEObjectDescription>filter(_allElements, _function);
+        for (final IEObjectDescription description : _filter) {
+          String _prefix = context.getPrefix();
+          ContentAssistResult.Entry _entry = new ContentAssistResult.Entry(ContentAssistResult.CROSSREF, _prefix);
+          final Procedure1<ContentAssistResult.Entry> _function_1 = new Procedure1<ContentAssistResult.Entry>() {
+            @Override
+            public void apply(final ContentAssistResult.Entry it) {
+              QualifiedName _name = description.getName();
+              String _string = _name.toString();
+              it.setProposal(_string);
+              it.setDescription("input signal");
+            }
+          };
+          ContentAssistResult.Entry _doubleArrow = ObjectExtensions.<ContentAssistResult.Entry>operator_doubleArrow(_entry, _function_1);
+          acceptor.accept(_doubleArrow);
         }
-      };
-      ContentAssistResult.Entry _doubleArrow = ObjectExtensions.<ContentAssistResult.Entry>operator_doubleArrow(_entry, _function_1);
-      acceptor.accept(_doubleArrow);
+      }
+    }
+    if (!_matched) {
+      StatemachineGrammarAccess.CommandElements _commandAccess = this._statemachineGrammarAccess.getCommandAccess();
+      Assignment _signalAssignment_1 = _commandAccess.getSignalAssignment_1();
+      if (Objects.equal(assignment, _signalAssignment_1)) {
+        _matched=true;
+        IScopeProvider _scopeProvider_1 = this.getScopeProvider();
+        EObject _currentModel_1 = context.getCurrentModel();
+        final IScope scope_1 = _scopeProvider_1.getScope(_currentModel_1, StatemachinePackage.Literals.COMMAND__SIGNAL);
+        Iterable<IEObjectDescription> _allElements_1 = scope_1.getAllElements();
+        final Function1<IEObjectDescription, Boolean> _function_2 = new Function1<IEObjectDescription, Boolean>() {
+          @Override
+          public Boolean apply(final IEObjectDescription it) {
+            EClass _eClass = it.getEClass();
+            return Boolean.valueOf(Objects.equal(_eClass, StatemachinePackage.Literals.OUTPUT_SIGNAL));
+          }
+        };
+        Iterable<IEObjectDescription> _filter_1 = IterableExtensions.<IEObjectDescription>filter(_allElements_1, _function_2);
+        for (final IEObjectDescription description_1 : _filter_1) {
+          String _prefix_1 = context.getPrefix();
+          ContentAssistResult.Entry _entry_1 = new ContentAssistResult.Entry(ContentAssistResult.CROSSREF, _prefix_1);
+          final Procedure1<ContentAssistResult.Entry> _function_3 = new Procedure1<ContentAssistResult.Entry>() {
+            @Override
+            public void apply(final ContentAssistResult.Entry it) {
+              QualifiedName _name = description_1.getName();
+              String _string = _name.toString();
+              it.setProposal(_string);
+              it.setDescription("output signal");
+            }
+          };
+          ContentAssistResult.Entry _doubleArrow_1 = ObjectExtensions.<ContentAssistResult.Entry>operator_doubleArrow(_entry_1, _function_3);
+          acceptor.accept(_doubleArrow_1);
+        }
+      }
+    }
+    if (!_matched) {
+      super._createProposals(assignment, context, acceptor);
+    }
+  }
+  
+  public void createProposals(final AbstractElement assignment, final ContentAssistContext context, final IAcceptor<ContentAssistResult.Entry> acceptor) {
+    if (assignment instanceof Assignment) {
+      _createProposals((Assignment)assignment, context, acceptor);
+      return;
+    } else if (assignment instanceof CrossReference) {
+      _createProposals((CrossReference)assignment, context, acceptor);
+      return;
+    } else if (assignment instanceof Keyword) {
+      _createProposals((Keyword)assignment, context, acceptor);
+      return;
+    } else if (assignment instanceof RuleCall) {
+      _createProposals((RuleCall)assignment, context, acceptor);
+      return;
+    } else if (assignment != null) {
+      _createProposals(assignment, context, acceptor);
+      return;
+    } else {
+      throw new IllegalArgumentException("Unhandled parameter types: " +
+        Arrays.<Object>asList(assignment, context, acceptor).toString());
     }
   }
 }
