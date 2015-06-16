5110c838daaf7f7d62667e24cd985c072355163a
Miro Spönemann <miro.spoenemann@itemis.de>
2015-06-16 08:32:11 +0200

    [web] Moved Orion dependencies into 'orion' subfolder
    Signed-off-by: Miro Spönemann <miro.spoenemann@itemis.de>

--- web/org.eclipse.xtext.web/src/main/js/requirejs-orion-config.js
+++ web/org.eclipse.xtext.web/src/main/js/requirejs-orion-config.js
@@ -4,7 +4,12 @@
 	paths: {
 		"jquery": "empty:",
 		"text": "../../../build/tmp/js/requirejs-plugins/text",
-		"i18n": "../../../build/tmp/js/requirejs-plugins/i18n"
+		"i18n": "../../../build/tmp/js/requirejs-plugins/i18n",
+		"webtools/cssContentAssist": "orion/webtools/cssContentAssist",
+		"webtools/htmlContentAssist": "orion/webtools/htmlContentAssist",
+		"webtools/util": "orion/webtools/util",
+		"javascript/compilationUnit": "orion/javascript/compilationUnit",
+		"csslint": "orion/csslint"
 	},
 	include: [ "orion/editor/stylers/lib/syntax" ],
 	exclude: [ "jquery", "text" ]
