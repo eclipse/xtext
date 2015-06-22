96f531cad94e628b428ddd3791672a55d3d8c1cd
akosyakov <anton.kosyakov@itemis.de>
2015-06-22 15:55:07 +0200

    [idea] Added extensions points to register packages, resource factories and resource service providers
    Change-Id: I92c6072f2853ee546e04e0bf8129e17caf7959c0
    Signed-off-by: akosyakov <anton.kosyakov@itemis.de>

---
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/internal/AbstractResourceServiceProviderDescriptor.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2015 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.internal;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.xtext.resource.IResourceServiceProvider;
+
+/**
+ * @author kosyakov - Initial contribution and API
+ */
+public abstract class AbstractResourceServiceProviderDescriptor implements IResourceServiceProvider.Provider {
+
+	@Override
+	public IResourceServiceProvider get(URI uri, String contentType) {
+		Object extension = getExtension();
+		if (extension instanceof IResourceServiceProvider.Provider) {
+			IResourceServiceProvider.Provider provider = ((IResourceServiceProvider.Provider) extension);
+			IResourceServiceProvider result = provider.get(uri, contentType);
+			return result;
+		} else if (extension instanceof IResourceServiceProvider) {
+			return (IResourceServiceProvider) extension;
+		}
+		String type = extension == null ? "null" : extension.getClass().getName();
+		String valid = IResourceServiceProvider.class + " or " + IResourceServiceProvider.Provider.class;
+		String suffix = "";
+		String contributor = getContributor();
+		if (contributor != null) {
+			suffix += " contributed by " + contributor;
+		}
+		throw new ClassCastException("The type " + type + " is not a valid " + valid + suffix);
+	}
+
+	protected abstract String getContributor();
+
+	protected abstract Object getExtension();
+
+}
