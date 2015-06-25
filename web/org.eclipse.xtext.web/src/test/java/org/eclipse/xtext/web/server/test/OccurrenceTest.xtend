49b2bdb63d83d881a38f84a6a1bd823eb47469da
Jan Koehnlein <jan.koehnlein@itemis.de>
2015-06-25 09:08:51 +0200

    [web] added occurrence tests
    Change-Id: I2e8344a4514e87bd081f1ccf531bc03643a4b171

---
+++ web/org.eclipse.xtext.web/src/test/java/org/eclipse/xtext/web/server/test/OccurrenceTest.xtend
@@ -0,0 +1,92 @@
+package org.eclipse.xtext.web.server.test
+
+import org.eclipse.xtext.web.server.occurrences.OccurrencesResult
+import org.junit.Test
+
+class OccurrenceTest extends AbstractWebServerTest {
+	def protected getOccurrences(CharSequence resourceContent) {
+		val content = resourceContent.toString
+		val offset = content.indexOf('#')
+		val occurrences = getService(#{
+				'requestType' -> 'occurrences',
+				'fullText' -> content.replace('#', ''),
+				'offset' -> offset.toString
+			})
+		assertTrue(occurrences.hasTextInput)
+		occurrences.service.apply() as OccurrencesResult
+	}
+	
+	def protected assertOccurrences(CharSequence resourceContent, CharSequence expectation) {
+		expectation.toString.trim.assertEquals(resourceContent.occurrences.toString.trim)
+	}
+	
+	@Test 
+	def void testNoOccurrenceOnEmptyFile() {
+		'#'.occurrences.assertNull
+	}
+	
+	@Test 
+	def void testNoOccurrencesOnKeyword() {
+		'''
+			#state foo
+		''' .occurrences
+			.assertNull
+	}
+	
+	@Test 
+	def void testSingleWriteOccurrence() {
+		'''
+			state #foo
+		''' .assertOccurrences('''
+			OccurrencesResult [
+			  writeRegions = ArrayList (
+			    TextRegionWithLineInformation [6:3][lineNumber=0, endLineNumber=0]
+			  )
+			  readRegions = ArrayList ()
+			]
+		''')
+	}
+
+	@Test 
+	def void testReadAndWriteOccurrence() {
+		'''
+			input signal #foo
+			state bar 
+				set foo == true
+				set foo == true
+			end
+		''' .assertOccurrences('''
+			OccurrencesResult [
+			  writeRegions = ArrayList (
+			    TextRegionWithLineInformation [13:3][lineNumber=0, endLineNumber=0]
+			  )
+			  readRegions = ArrayList (
+			    TextRegionWithLineInformation [33:3][lineNumber=2, endLineNumber=2],
+			    TextRegionWithLineInformation [50:3][lineNumber=3, endLineNumber=3]
+			  )
+			]
+		''')
+	}
+	
+	@Test 
+	def void testReadAndWriteOccurrence_2() {
+		'''
+			input signal foo
+			state bar 
+				set #foo == true
+				set foo == true
+			end
+		''' .assertOccurrences('''
+			OccurrencesResult [
+			  writeRegions = ArrayList (
+			    TextRegionWithLineInformation [13:3][lineNumber=0, endLineNumber=0]
+			  )
+			  readRegions = ArrayList (
+			    TextRegionWithLineInformation [33:3][lineNumber=2, endLineNumber=2],
+			    TextRegionWithLineInformation [50:3][lineNumber=3, endLineNumber=3]
+			  )
+			]
+		''')
+	}
+	
+}
---
+++ web/org.eclipse.xtext.web/src/test/xtend-gen/org/eclipse/xtext/web/server/test/OccurrenceTest.java
@@ -0,0 +1,174 @@
+package org.eclipse.xtext.web.server.test;
+
+import java.util.Collections;
+import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipse.xtext.web.server.IServiceResult;
+import org.eclipse.xtext.web.server.XtextServiceDispatcher;
+import org.eclipse.xtext.web.server.occurrences.OccurrencesResult;
+import org.eclipse.xtext.web.server.test.AbstractWebServerTest;
+import org.eclipse.xtext.xbase.lib.CollectionLiterals;
+import org.eclipse.xtext.xbase.lib.Functions.Function0;
+import org.eclipse.xtext.xbase.lib.Pair;
+import org.junit.Assert;
+import org.junit.Test;
+
+@SuppressWarnings("all")
+public class OccurrenceTest extends AbstractWebServerTest {
+  protected OccurrencesResult getOccurrences(final CharSequence resourceContent) {
+    OccurrencesResult _xblockexpression = null;
+    {
+      final String content = resourceContent.toString();
+      final int offset = content.indexOf("#");
+      Pair<String, String> _mappedTo = Pair.<String, String>of("requestType", "occurrences");
+      String _replace = content.replace("#", "");
+      Pair<String, String> _mappedTo_1 = Pair.<String, String>of("fullText", _replace);
+      String _string = Integer.valueOf(offset).toString();
+      Pair<String, String> _mappedTo_2 = Pair.<String, String>of("offset", _string);
+      final XtextServiceDispatcher.ServiceDescriptor occurrences = this.getService(
+        Collections.<String, String>unmodifiableMap(CollectionLiterals.<String, String>newHashMap(_mappedTo, _mappedTo_1, _mappedTo_2)));
+      boolean _isHasTextInput = occurrences.isHasTextInput();
+      Assert.assertTrue(_isHasTextInput);
+      Function0<? extends IServiceResult> _service = occurrences.getService();
+      IServiceResult _apply = _service.apply();
+      _xblockexpression = ((OccurrencesResult) _apply);
+    }
+    return _xblockexpression;
+  }
+  
+  protected void assertOccurrences(final CharSequence resourceContent, final CharSequence expectation) {
+    String _string = expectation.toString();
+    String _trim = _string.trim();
+    OccurrencesResult _occurrences = this.getOccurrences(resourceContent);
+    String _string_1 = _occurrences.toString();
+    String _trim_1 = _string_1.trim();
+    Assert.assertEquals(_trim, _trim_1);
+  }
+  
+  @Test
+  public void testNoOccurrenceOnEmptyFile() {
+    OccurrencesResult _occurrences = this.getOccurrences("#");
+    Assert.assertNull(_occurrences);
+  }
+  
+  @Test
+  public void testNoOccurrencesOnKeyword() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("#state foo");
+    _builder.newLine();
+    OccurrencesResult _occurrences = this.getOccurrences(_builder);
+    Assert.assertNull(_occurrences);
+  }
+  
+  @Test
+  public void testSingleWriteOccurrence() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("state #foo");
+    _builder.newLine();
+    StringConcatenation _builder_1 = new StringConcatenation();
+    _builder_1.append("OccurrencesResult [");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("writeRegions = ArrayList (");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("TextRegionWithLineInformation [6:3][lineNumber=0, endLineNumber=0]");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append(")");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("readRegions = ArrayList ()");
+    _builder_1.newLine();
+    _builder_1.append("]");
+    _builder_1.newLine();
+    this.assertOccurrences(_builder, _builder_1);
+  }
+  
+  @Test
+  public void testReadAndWriteOccurrence() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("input signal #foo");
+    _builder.newLine();
+    _builder.append("state bar ");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("set foo == true");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("set foo == true");
+    _builder.newLine();
+    _builder.append("end");
+    _builder.newLine();
+    StringConcatenation _builder_1 = new StringConcatenation();
+    _builder_1.append("OccurrencesResult [");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("writeRegions = ArrayList (");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("TextRegionWithLineInformation [13:3][lineNumber=0, endLineNumber=0]");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append(")");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("readRegions = ArrayList (");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("TextRegionWithLineInformation [33:3][lineNumber=2, endLineNumber=2],");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("TextRegionWithLineInformation [50:3][lineNumber=3, endLineNumber=3]");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append(")");
+    _builder_1.newLine();
+    _builder_1.append("]");
+    _builder_1.newLine();
+    this.assertOccurrences(_builder, _builder_1);
+  }
+  
+  @Test
+  public void testReadAndWriteOccurrence_2() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("input signal foo");
+    _builder.newLine();
+    _builder.append("state bar ");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("set #foo == true");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("set foo == true");
+    _builder.newLine();
+    _builder.append("end");
+    _builder.newLine();
+    StringConcatenation _builder_1 = new StringConcatenation();
+    _builder_1.append("OccurrencesResult [");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("writeRegions = ArrayList (");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("TextRegionWithLineInformation [13:3][lineNumber=0, endLineNumber=0]");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append(")");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append("readRegions = ArrayList (");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("TextRegionWithLineInformation [33:3][lineNumber=2, endLineNumber=2],");
+    _builder_1.newLine();
+    _builder_1.append("    ");
+    _builder_1.append("TextRegionWithLineInformation [50:3][lineNumber=3, endLineNumber=3]");
+    _builder_1.newLine();
+    _builder_1.append("  ");
+    _builder_1.append(")");
+    _builder_1.newLine();
+    _builder_1.append("]");
+    _builder_1.newLine();
+    this.assertOccurrences(_builder, _builder_1);
+  }
+}
