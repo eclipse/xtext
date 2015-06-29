fba8a01cb7ef7d33f42884562b8412856533c5d0
Miro Spönemann <miro.spoenemann@itemis.de>
2015-06-29 14:07:49 +0200

    [generator] Created new project org.eclipse.xtext.xtext.generator
    Signed-off-by: Miro Spönemann <miro.spoenemann@itemis.de>

---
+++ plugins/org.eclipse.xtext.xtext.generator/xtend-gen/org/eclipse/xtext/xtext/generator/GeneratedClassAnnotation.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2015 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.xtext.xtext.generator;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.eclipse.xtend.lib.annotations.Accessors;
+import org.eclipse.xtext.util.Strings;
+import org.eclipse.xtext.xbase.lib.Pure;
+import org.eclipse.xtext.xtext.generator.IClassAnnotation;
+import org.eclipse.xtext.xtext.generator.XtextGenerator;
+
+/**
+ * A class annotation configuration for the <code>@Generated</code> annotation.
+ */
+@SuppressWarnings("all")
+public class GeneratedClassAnnotation implements IClassAnnotation {
+  @Accessors
+  private boolean includeDate = false;
+  
+  @Accessors
+  private String comment;
+  
+  @Override
+  public String toString() {
+    final StringBuilder stringBuilder = new StringBuilder("@Generated(");
+    boolean _or = false;
+    if (this.includeDate) {
+      _or = true;
+    } else {
+      boolean _isEmpty = Strings.isEmpty(this.comment);
+      boolean _not = (!_isEmpty);
+      _or = _not;
+    }
+    if (_or) {
+      this.operator_add(stringBuilder, "value = ");
+    }
+    this.operator_add(stringBuilder, "\"");
+    String _generatorName = this.getGeneratorName();
+    this.operator_add(stringBuilder, _generatorName);
+    this.operator_add(stringBuilder, "\"");
+    if (this.includeDate) {
+      final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd\'T\'HH:mmZ");
+      Date _date = new Date();
+      final String date = dateFormat.format(_date);
+      this.operator_add(stringBuilder, ", date = \"");
+      this.operator_add(stringBuilder, date);
+      this.operator_add(stringBuilder, "\"");
+    }
+    boolean _isEmpty_1 = Strings.isEmpty(this.comment);
+    boolean _not_1 = (!_isEmpty_1);
+    if (_not_1) {
+      final String convertedComment = Strings.convertToJavaString(this.comment);
+      this.operator_add(stringBuilder, ", comments = \"");
+      this.operator_add(stringBuilder, convertedComment);
+      this.operator_add(stringBuilder, "\"");
+    }
+    this.operator_add(stringBuilder, ")");
+    return stringBuilder.toString();
+  }
+  
+  protected String getGeneratorName() {
+    return XtextGenerator.class.getName();
+  }
+  
+  @Override
+  public String getAnnotationImport() {
+    return "javax.annotation.Generated";
+  }
+  
+  private void operator_add(final StringBuilder stringBuilder, final String s) {
+    stringBuilder.append(s);
+  }
+  
+  @Pure
+  public boolean isIncludeDate() {
+    return this.includeDate;
+  }
+  
+  public void setIncludeDate(final boolean includeDate) {
+    this.includeDate = includeDate;
+  }
+  
+  @Pure
+  public String getComment() {
+    return this.comment;
+  }
+  
+  public void setComment(final String comment) {
+    this.comment = comment;
+  }
+}
