9f5046d9767c479e21b0b97a3fb7fa991dda041a
Sven Efftinge <sven.efftinge@itemis.de>
2015-07-02 10:37:21 +0200

    [idea facets] regenerated test languages

--- intellij/org.eclipse.xtext.core.idea.tests/src/org/eclipse/xtext/parser/unorderedGroups/idea/facet/BacktrackingBug325745TestLanguageFacetConfiguration.java
+++ intellij/org.eclipse.xtext.core.idea.tests/src/org/eclipse/xtext/parser/unorderedGroups/idea/facet/BacktrackingBug325745TestLanguageFacetConfiguration.java
@@ -1,7 +1,9 @@
 package org.eclipse.xtext.parser.unorderedGroups.idea.facet;
 
 import org.eclipse.xtext.idea.facet.AbstractFacetConfiguration;
+import org.eclipse.xtext.idea.facet.GeneratorConfigurationState;
 
+import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
 import com.intellij.openapi.components.StoragePathMacros;
@@ -11,6 +13,6 @@ import com.intellij.openapi.components.StorageScheme;
 		@Storage(id = "default", file = StoragePathMacros.PROJECT_FILE),
 		@Storage(id = "dir", file = StoragePathMacros.PROJECT_CONFIG_DIR
 				+ "/BacktrackingBug325745TestLanguageGeneratorConfig.xml", scheme = StorageScheme.DIRECTORY_BASED)})
-public class BacktrackingBug325745TestLanguageFacetConfiguration extends AbstractFacetConfiguration {
+public class BacktrackingBug325745TestLanguageFacetConfiguration extends AbstractFacetConfiguration  implements PersistentStateComponent<GeneratorConfigurationState>{
 	
 }
