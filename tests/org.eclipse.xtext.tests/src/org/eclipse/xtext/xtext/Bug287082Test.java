e064c7b1b6a61f3d67d77942991b27e5bdfa4b5f
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2015-07-02 22:12:37 +0200

    Use SuppressWarnings[..] rather than Suppress[..]

--- tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/xtext/Bug287082Test.java
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/xtext/Bug287082Test.java
@@ -40,7 +40,7 @@ public class Bug287082Test extends AbstractValidationMessageAcceptingTestCase {
 	public void acceptWarning(String message, EObject object, EStructuralFeature feature, int index, String code,
 			String... issueData) {
 		if (code.equals(OverriddenValueInspector.ISSUE_CODE)) {
-			String expectation = "";
+			String expectation = "The assigned value of feature 'feature' will possibly override itself because it is used inside of a loop.";
 			assertEquals(expectation, message);
 		} else {
 			super.acceptWarning(message, object, feature, index, code, issueData);
