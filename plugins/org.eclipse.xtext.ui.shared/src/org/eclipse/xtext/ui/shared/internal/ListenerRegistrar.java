53775d56d20061d5d980e52eb8728edd12ee42f6
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2015-07-03 15:29:39 +0200

    Trigger build for newly opened projects in non-empty workspace

--- plugins/org.eclipse.xtext.ui.shared/src/org/eclipse/xtext/ui/shared/internal/ListenerRegistrar.java
+++ plugins/org.eclipse.xtext.ui.shared/src/org/eclipse/xtext/ui/shared/internal/ListenerRegistrar.java
@@ -7,7 +7,12 @@
  *******************************************************************************/
 package org.eclipse.xtext.ui.shared.internal;
 
+import java.util.Arrays;
+
 import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.xtext.builder.builderState.IBuilderState;
+import org.eclipse.xtext.builder.impl.BuildScheduler;
+import org.eclipse.xtext.builder.impl.IBuildFlag;
 import org.eclipse.xtext.builder.impl.ProjectOpenedOrClosedListener;
 import org.eclipse.xtext.ui.shared.contribution.IEagerContribution;
 
@@ -22,10 +27,23 @@ public class ListenerRegistrar implements IEagerContribution {
 	private ProjectOpenedOrClosedListener listener;
 	@Inject
 	private IWorkspace workspace;
-
+	@Inject
+	private BuildScheduler buildManager;
+	@Inject
+	private IBuilderState builderState;
+	
 	@Override
 	public void initialize() {
 		workspace.addResourceChangeListener(listener);
+		
+		// If there are no open projects in a workspace after a restart, a newly opened Xtext project will
+		// trigger the infrastructure's initialization procedure. Since the listeners are than registered
+		// after the fact, the project-open event is already done. Thus no build is triggered for the newly
+		// opened project and the index state is corrupt.
+		// thus we trigger a recovery build here
+		if (builderState.isEmpty()) {
+			buildManager.scheduleBuildIfNecessary(Arrays.asList(workspace.getRoot().getProjects()), IBuildFlag.RECOVERY_BUILD);
+		}
 	}
 
 	@Override
