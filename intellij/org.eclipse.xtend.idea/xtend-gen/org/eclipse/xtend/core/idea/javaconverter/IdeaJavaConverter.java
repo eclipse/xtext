ea58ff43d430438767db1fc41515216adefa6d10
Dennis Huebner <dennis.huebner@itemis.de>
2015-07-27 10:04:54 +0200

    [idea] Printout classloader path to find out what goes wrong on server

--- intellij/org.eclipse.xtend.idea/xtend-gen/org/eclipse/xtend/core/idea/javaconverter/IdeaJavaConverter.java
+++ intellij/org.eclipse.xtend.idea/xtend-gen/org/eclipse/xtend/core/idea/javaconverter/IdeaJavaConverter.java
@@ -7,8 +7,17 @@
  */
 package org.eclipse.xtend.core.idea.javaconverter;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.List;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.xtend.core.javaconverter.JavaConverter;
+import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipse.xtext.xbase.lib.Conversions;
+import org.eclipse.xtext.xbase.lib.Functions.Function1;
+import org.eclipse.xtext.xbase.lib.InputOutput;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
+import org.eclipse.xtext.xbase.lib.ListExtensions;
 
 /**
  * @author dhuebner - Initial contribution and API
@@ -17,6 +26,20 @@ import org.eclipse.xtend.core.javaconverter.JavaConverter;
 public class IdeaJavaConverter extends JavaConverter {
   @Override
   protected void provideCustomEnvironment(final ASTParser parser) {
+    final ClassLoader sysClassLoader = ClassLoader.getSystemClassLoader();
+    URL[] _uRLs = ((URLClassLoader) sysClassLoader).getURLs();
+    final Function1<URL, String> _function = new Function1<URL, String>() {
+      @Override
+      public String apply(final URL it) {
+        return it.getFile();
+      }
+    };
+    final List<String> cpEntries = ListExtensions.<URL, String>map(((List<URL>)Conversions.doWrapArray(_uRLs)), _function);
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("|");
+    _builder.newLine();
+    String _join = IterableExtensions.join(cpEntries, _builder);
+    InputOutput.<String>println(_join);
     parser.setEnvironment(null, null, null, false);
   }
 }
