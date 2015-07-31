51f7691979163075eaa9bb2526123b492f0b6665
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2015-07-31 14:23:04 +0200

    [473079] Removed wrong guard condition

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/generator/trace/CharSequenceTraceWrapper.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/generator/trace/CharSequenceTraceWrapper.java
@@ -37,9 +37,6 @@ public class CharSequenceTraceWrapper {
 	}
 	
 	public CharSequence wrapWithTraceData(CharSequence sequence, SourceRelativeURI originResourceURI, int originOffset, int originLength, int originLineNumber, int originEndLineNumber) {
-		if (!originResourceURI.getURI().isPlatformResource()) {
-			throw new IllegalArgumentException("URI has to be a platform resource uri but was: " + originResourceURI+ ". Use #wrapWithTraceData(CharSequence, URI, String, int, int) instead.");
-		}
 		return wrapWithTraceData(sequence, originResourceURI, originResourceURI.getURI().segment(1), originOffset, originLength, originLineNumber, originEndLineNumber);
 	}
 	
