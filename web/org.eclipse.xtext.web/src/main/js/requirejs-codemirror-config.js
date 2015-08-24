e614634e2d97ecca06655f8c7ce2a46ba1ee1567
Miro Spönemann <miro.spoenemann@itemis.de>
2015-08-24 16:30:55 +0200

    [web] Added support for CodeMirror https://codemirror.net/
    Signed-off-by: Miro Spönemann <miro.spoenemann@itemis.de>

---
+++ web/org.eclipse.xtext.web/src/main/js/requirejs-codemirror-config.js
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2015 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+({
+	baseUrl: ".",
+	name: "xtext/xtext-codemirror",
+	paths: {
+		"jquery": "empty:",
+		"codemirror": "empty:",
+		"codemirror/addon/hint/show-hint": "empty:"
+	},
+	exclude: [ "jquery", "codemirror", "codemirror/addon/hint/show-hint" ]
+})
