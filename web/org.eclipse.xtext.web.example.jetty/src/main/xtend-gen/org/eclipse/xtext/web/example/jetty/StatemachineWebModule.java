30251521753efd511da46e75a3ad669eaf074490
Sven Efftinge <sven.efftinge@itemis.de>
2015-09-16 10:00:03 +0200

    regenerated statemachine web language

---
+++ web/org.eclipse.xtext.web.example.jetty/src/main/xtend-gen/org/eclipse/xtext/web/example/jetty/StatemachineWebModule.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2015 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.xtext.web.example.jetty;
+
+import com.google.inject.Binder;
+import com.google.inject.Provider;
+import com.google.inject.binder.AnnotatedBindingBuilder;
+import java.util.concurrent.ExecutorService;
+import org.eclipse.xtext.ide.editor.contentassist.IdeContentProposalProvider;
+import org.eclipse.xtext.ide.editor.syntaxcoloring.ISemanticHighlightingCalculator;
+import org.eclipse.xtext.ide.labels.IImageDescriptionProvider;
+import org.eclipse.xtext.web.example.jetty.AbstractStatemachineWebModule;
+import org.eclipse.xtext.web.example.jetty.contentassist.StatemachineWebContentProposalProvider;
+import org.eclipse.xtext.web.example.statemachine.ide.StatemachineImageDescriptionProvider;
+import org.eclipse.xtext.web.example.statemachine.ide.StatemachineSemanticHighlightingCalculator;
+import org.eclipse.xtext.web.server.persistence.FileResourceHandler;
+import org.eclipse.xtext.web.server.persistence.IResourceBaseProvider;
+import org.eclipse.xtext.web.server.persistence.IServerResourceHandler;
+
+/**
+ * Use this class to register components to be used within the web application.
+ */
+@SuppressWarnings("all")
+public class StatemachineWebModule extends AbstractStatemachineWebModule {
+  private IResourceBaseProvider resourceBaseProvider;
+  
+  public StatemachineWebModule(final Provider<ExecutorService> executorServiceProvider) {
+    super(executorServiceProvider);
+  }
+  
+  public void setResourceBaseProvider(final IResourceBaseProvider resourceBaseProvider) {
+    this.resourceBaseProvider = resourceBaseProvider;
+  }
+  
+  public void configureResourceBaseProvider(final Binder binder) {
+    if ((this.resourceBaseProvider != null)) {
+      AnnotatedBindingBuilder<IResourceBaseProvider> _bind = binder.<IResourceBaseProvider>bind(IResourceBaseProvider.class);
+      _bind.toInstance(this.resourceBaseProvider);
+    }
+  }
+  
+  public Class<? extends IServerResourceHandler> bindIServerResourceHandler() {
+    return FileResourceHandler.class;
+  }
+  
+  public Class<? extends IdeContentProposalProvider> bindIdeContentProposalProvider() {
+    return StatemachineWebContentProposalProvider.class;
+  }
+  
+  public Class<? extends IImageDescriptionProvider> bindIImageDescriptionProvider() {
+    return StatemachineImageDescriptionProvider.class;
+  }
+  
+  public Class<? extends ISemanticHighlightingCalculator> bindISemanticHighlightingCalculator() {
+    return StatemachineSemanticHighlightingCalculator.class;
+  }
+}
