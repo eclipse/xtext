1669ffb483886897d7d9e0d662f4597b8370f5aa
Sven Efftinge <sven.efftinge@itemis.de>
2015-09-25 10:48:08 +0200

    [idea] Added icons for Xtext

---
+++ intellij/org.eclipse.xtext.xtext.idea/src/org/eclipse/xtext/idea/XtextXtextIcons.xtend
@@ -0,0 +1,8 @@
+package org.eclipse.xtext.idea
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+class XtextXtextIcons {
+	public static final Icon GRAMMAR_FILE_TYPE = IconLoader.getIcon("/icons/filetype_xtext.png"); // 16x16
+}
---
+++ intellij/org.eclipse.xtext.xtext.idea/xtend-gen/org/eclipse/xtext/idea/XtextXtextIcons.java
@@ -0,0 +1,9 @@
+package org.eclipse.xtext.idea;
+
+import com.intellij.openapi.util.IconLoader;
+import javax.swing.Icon;
+
+@SuppressWarnings("all")
+public class XtextXtextIcons {
+  public final static Icon GRAMMAR_FILE_TYPE = IconLoader.getIcon("/icons/filetype_xtext.png");
+}
