dc330732f4f0fcf3da4764f78e3cce7bbad3a1c8
Sven Efftinge <sven.efftinge@itemis.de>
2015-09-30 11:33:04 +0200

    [idea/xtext] enabled XtextBracePairProvider

--- intellij/org.eclipse.xtext.xtext.idea/src/org/eclipse/xtext/idea/XtextIdeaModule.java
+++ intellij/org.eclipse.xtext.xtext.idea/src/org/eclipse/xtext/idea/XtextIdeaModule.java
@@ -1,5 +1,7 @@
 package org.eclipse.xtext.idea;
 
+import org.eclipse.xtext.ide.editor.bracketmatching.IBracePairProvider;
+import org.eclipse.xtext.ide.editor.bracketmatching.XtextBracePairProvider;
 import org.eclipse.xtext.validation.ResourceValidatorImpl;
 import org.eclipse.xtext.xtext.ReducedXtextResourceValidator;
 
@@ -7,6 +9,11 @@ public class XtextIdeaModule extends AbstractXtextIdeaModule {
 
 	public Class<? extends ResourceValidatorImpl> bindResourceValidator() {
 		return ReducedXtextResourceValidator.class;
+	}
+	
+	@Override
+	public Class<? extends IBracePairProvider> bindIBracePairProvider() {
+		return XtextBracePairProvider.class;
 	}
 	
 }
