8da354340b5688a4b1a0a206c1df688d28e775e5
Stefan Oehme <stefan.oehme@itemis.de>
2015-10-09 10:37:53 +0200

    don't generate Java stub for Xbase label provider

--- plugins/org.eclipse.xtext.xbase/generator/org/eclipse/xtext/xbase/GenerateXbase.java
+++ plugins/org.eclipse.xtext.xbase/generator/org/eclipse/xtext/xbase/GenerateXbase.java
@@ -158,7 +158,9 @@ final class GenerateXbase {
 				addFragment(new BuilderIntegrationFragment2());
 				addFragment(new Formatter2Fragment2());
 				addFragment(new QuickfixProviderFragment2());
-				addFragment(new LabelProviderFragment2());
+				LabelProviderFragment2 label = new LabelProviderFragment2();
+				label.setGenerateStub(false);
+				addFragment(label);
 				addFragment(new OutlineTreeProviderFragment2());
 				addFragment(new ContentAssistFragment2());
 				XtextAntlrUiGeneratorFragment antlrUI = new XtextAntlrUiGeneratorFragment();
