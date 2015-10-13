d96e1cc1e8ccef0fb3a23098aec73f4ed40c6127
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2015-10-13 13:54:54 +0200

    [serializer] introduce IContext
    Previously, the context was of type EObject which actually was a
    ParserRule or assigned Action. 
    
    This refactoring paves the road for more advanced contexts, such
    as ParserRules with parameter values.
    
    Signed-off-by: Moritz Eysholdt <moritz.eysholdt@itemis.de>

--- tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/serializer/AssignmentFinderTest.java
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/serializer/AssignmentFinderTest.java
@@ -7,13 +7,19 @@
  *******************************************************************************/
 package org.eclipse.xtext.serializer;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.AbstractElement;
+import org.eclipse.xtext.GrammarUtil;
 import org.eclipse.xtext.grammaranalysis.impl.GrammarElementTitleSwitch;
 import org.eclipse.xtext.junit4.AbstractXtextTests;
 import org.eclipse.xtext.junit4.util.ParseHelper;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
+import org.eclipse.xtext.serializer.analysis.GrammarElementDeclarationOrder;
+import org.eclipse.xtext.serializer.analysis.IContext;
+import org.eclipse.xtext.serializer.analysis.SerializationContext;
 import org.eclipse.xtext.serializer.assignmentFinderTest.ContainmentRef;
 import org.eclipse.xtext.serializer.assignmentFinderTest.CrossRef;
 import org.eclipse.xtext.serializer.assignmentFinderTest.EnumBool;
@@ -40,8 +46,10 @@ import org.eclipse.xtext.serializer.services.AssignmentFinderTestLanguageGrammar
 import org.junit.Test;
 
 import com.google.common.base.Joiner;
+import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
 import com.google.inject.Inject;
 
 /**
@@ -65,26 +73,34 @@ public class AssignmentFinderTest extends AbstractXtextTests {
 	private AssignmentFinderTestLanguageGrammarAccess grammar;
 
 	private String findAssignments(EObject obj, Object value, AbstractElement... assignments) {
-		List<AbstractElement> ass = Lists.newArrayList(assignments);
-		Iterable<AbstractElement> found = finder.findAssignmentsByValue(obj, ass, value, null);
+		AbstractElement element = (AbstractElement) NodeModelUtils.getNode(obj).getGrammarElement();
+		IContext ctx = SerializationContext.forChild(null, element, obj);
+		Multimap<AbstractElement, IContext> ass = ArrayListMultimap.create();
+		for (AbstractElement e : assignments)
+			ass.put(e, ctx);
+		List<AbstractElement> found = Lists.newArrayList(finder.findAssignmentsByValue(obj, ass, value, null));
+		Collections.sort(found, GrammarElementDeclarationOrder.get(GrammarUtil.getGrammar(assignments[0])));
 		return Joiner.on(", ").join(Iterables.transform(found, new GrammarElementTitleSwitch().showAssignments()));
 	}
 
-	@Test public void testKeywordVal1() throws Exception {
+	@Test
+	public void testKeywordVal1() throws Exception {
 		KeywordValElements g = grammar.getKeywordValAccess();
 		KeywordVal model = parser.parse("#00 kw1").getKeywordVal();
 		String ass = findAssignments(model, model.getKw(), g.getKwKw1Keyword_0_0(), g.getKwKw2Keyword_1_0());
 		assertEquals("kw='kw1'", ass);
 	}
 
-	@Test public void testKeywordVal2() throws Exception {
+	@Test
+	public void testKeywordVal2() throws Exception {
 		KeywordValElements g = grammar.getKeywordValAccess();
 		KeywordVal model = parser.parse("#00 kw2").getKeywordVal();
 		String ass = findAssignments(model, model.getKw(), g.getKwKw1Keyword_0_0(), g.getKwKw2Keyword_1_0());
 		assertEquals("kw='kw2'", ass);
 	}
 
-	@Test public void testTerminalVal1() throws Exception {
+	@Test
+	public void testTerminalVal1() throws Exception {
 		TerminalValElements g = grammar.getTerminalValAccess();
 		TerminalVal model = parser.parse("#01 %BAR").getTerminalVal();
 		String ass = findAssignments(model, model.getTerm(), g.getTermTerminal1TerminalRuleCall_0_0(),
@@ -92,7 +108,8 @@ public class AssignmentFinderTest extends AbstractXtextTests {
 		assertEquals("term=Terminal1", ass);
 	}
 
-	@Test public void testTerminalVal2() throws Exception {
+	@Test
+	public void testTerminalVal2() throws Exception {
 		TerminalValElements g = grammar.getTerminalValAccess();
 		TerminalVal model = parser.parse("#01 %bar").getTerminalVal();
 		String ass = findAssignments(model, model.getTerm(), g.getTermTerminal1TerminalRuleCall_0_0(),
@@ -100,7 +117,8 @@ public class AssignmentFinderTest extends AbstractXtextTests {
 		assertEquals("term=Terminal2", ass);
 	}
 
-	@Test public void testTerminalVal3() throws Exception {
+	@Test
+	public void testTerminalVal3() throws Exception {
 		TerminalValElements g = grammar.getTerminalValAccess();
 		TerminalVal model = parser.parse("#01 %foo").getTerminalVal();
 		String ass = findAssignments(model, model.getTerm(), g.getTermTerminal1TerminalRuleCall_0_0(),
@@ -108,7 +126,8 @@ public class AssignmentFinderTest extends AbstractXtextTests {
 		assertEquals("term='%foo'", ass);
 	}
 
-	@Test public void testEnumVal1() throws Exception {
+	@Test
+	public void testEnumVal1() throws Exception {
 		EnumValElements g = grammar.getEnumValAccess();
 		EnumVal model = parser.parse("#02 lit1").getEnumVal();
 		String ass = findAssignments(model, model.getEn(), g.getEnEnum1EnumRuleCall_0_0(),
@@ -116,7 +135,8 @@ public class AssignmentFinderTest extends AbstractXtextTests {
 		assertEquals("en=Enum1", ass);
 	}
 
-	@Test public void testEnumVal2() throws Exception {
+	@Test
+	public void testEnumVal2() throws Exception {
 		EnumValElements g = grammar.getEnumValAccess();
 		EnumVal model = parser.parse("#02 lit2").getEnumVal();
 		String ass = findAssignments(model, model.getEn(), g.getEnEnum1EnumRuleCall_0_0(),
@@ -124,14 +144,16 @@ public class AssignmentFinderTest extends AbstractXtextTests {
 		assertEquals("en=Enum2", ass);
 	}
 
-	@Test public void testKeywordBool() throws Exception {
+	@Test
+	public void testKeywordBool() throws Exception {
 		KeywordBoolElements g = grammar.getKeywordBoolAccess();
 		KeywordBool model = parser.parse("#03 kw1").getKeywordBool();
 		String ass = findAssignments(model, model.isKw(), g.getKwKw1Keyword_0_0(), g.getKwKw2Keyword_1_0());
 		assertEquals("kw?='kw1', kw?='kw2'", ass);
 	}
 
-	@Test public void testTerminalBool() throws Exception {
+	@Test
+	public void testTerminalBool() throws Exception {
 		TerminalBoolElements g = grammar.getTerminalBoolAccess();
 		TerminalBool model = parser.parse("#04 %BAR").getTerminalBool();
 		String ass = findAssignments(model, model.isTerm(), g.getTermTerminal1TerminalRuleCall_0_0(),
@@ -139,7 +161,8 @@ public class AssignmentFinderTest extends AbstractXtextTests {
 		assertEquals("term?=Terminal1, term?=Terminal2, term?='%foo'", ass);
 	}
 
-	@Test public void testEnumBool() throws Exception {
+	@Test
+	public void testEnumBool() throws Exception {
 		EnumBoolElements g = grammar.getEnumBoolAccess();
 		EnumBool model = parser.parse("#05 lit1").getEnumBool();
 		String ass = findAssignments(model, model.isEn(), g.getEnEnum1EnumRuleCall_0_0(),
@@ -147,7 +170,8 @@ public class AssignmentFinderTest extends AbstractXtextTests {
 		assertEquals("en?=Enum1, en?=Enum2", ass);
 	}
 
-	@Test public void testMixedBool1() throws Exception {
+	@Test
+	public void testMixedBool1() throws Exception {
 		MixedBoolElements g = grammar.getMixedBoolAccess();
 		MixedBool model = parser.parse("#06 true").getMixedBool();
 		String ass = findAssignments(model, model.isVal(), g.getValKw1Keyword_0_0(),
@@ -155,7 +179,8 @@ public class AssignmentFinderTest extends AbstractXtextTests {
 		assertEquals("val?='kw1', val=Boolean", ass);
 	}
 
-	@Test public void testMixedBool2() throws Exception {
+	@Test
+	public void testMixedBool2() throws Exception {
 		MixedBoolElements g = grammar.getMixedBoolAccess();
 		MixedBool model = parser.parse("#06 false").getMixedBool();
 		String ass = findAssignments(model, model.isVal(), g.getValKw1Keyword_0_0(),
@@ -163,7 +188,8 @@ public class AssignmentFinderTest extends AbstractXtextTests {
 		assertEquals("val=Boolean", ass);
 	}
 
-	@Test public void testMixedValue1() throws Exception {
+	@Test
+	public void testMixedValue1() throws Exception {
 		MixedValueElements g = grammar.getMixedValueAccess();
 		MixedValue model = parser.parse("#07 lit1").getMixedValue();
 		String ass = findAssignments(model, model.getVal(), g.getValDatEnumParserRuleCall_1_0(),
@@ -171,7 +197,8 @@ public class AssignmentFinderTest extends AbstractXtextTests {
 		assertEquals("val=Enum1", ass);
 	}
 
-	@Test public void testMixedValue2() throws Exception {
+	@Test
+	public void testMixedValue2() throws Exception {
 		MixedValueElements g = grammar.getMixedValueAccess();
 		MixedValue model = parser.parse("#07 lit3").getMixedValue();
 		String ass = findAssignments(model, model.getVal(), g.getValDatEnumParserRuleCall_1_0(),
@@ -179,7 +206,8 @@ public class AssignmentFinderTest extends AbstractXtextTests {
 		assertEquals("val=DatEnum", ass);
 	}
 
-	@Test public void testContainmentRef1() throws Exception {
+	@Test
+	public void testContainmentRef1() throws Exception {
 		ContainmentRefElements g = grammar.getContainmentRefAccess();
 		ContainmentRef model = parser.parse("#08 kw1 foo").getContainmentRef();
 		String ass = findAssignments(model, model.getCtx(), g.getCtxContainmentRef1ParserRuleCall_0_0(),
@@ -187,7 +215,8 @@ public class AssignmentFinderTest extends AbstractXtextTests {
 		assertEquals("ctx=ContainmentRef1", ass);
 	}
 
-	@Test public void testContainmentRef2() throws Exception {
+	@Test
+	public void testContainmentRef2() throws Exception {
 		ContainmentRefElements g = grammar.getContainmentRefAccess();
 		ContainmentRef model = parser.parse("#08 kw2 foo").getContainmentRef();
 		String ass = findAssignments(model, model.getCtx(), g.getCtxContainmentRef1ParserRuleCall_0_0(),
@@ -195,7 +224,8 @@ public class AssignmentFinderTest extends AbstractXtextTests {
 		assertEquals("ctx=ContainmentRef2", ass);
 	}
 
-	@Test public void testCrossRef1() throws Exception {
+	@Test
+	public void testCrossRef1() throws Exception {
 		CrossRefElements g = grammar.getCrossRefAccess();
 		CrossRef model = parser.parse("#09 %BAR %BAR").getCrossRef();
 		String ass = findAssignments(model, model.getCrossRef(),
@@ -204,7 +234,8 @@ public class AssignmentFinderTest extends AbstractXtextTests {
 		assertEquals("crossRef=[CrossRef|Terminal1]", ass);
 	}
 
-	@Test public void testCrossRef2() throws Exception {
+	@Test
+	public void testCrossRef2() throws Exception {
 		CrossRefElements g = grammar.getCrossRefAccess();
 		CrossRef model = parser.parse("#09 %bar %bar").getCrossRef();
 		String ass = findAssignments(model, model.getCrossRef(),
