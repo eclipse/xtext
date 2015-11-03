7dd71d9e2b32c0c08c80d4c9d13c7267a96704ba
Christian Schneider <christian.schneider@itemis.de>
2015-11-03 10:51:57 +0100

    [xtext generator] added 'generateXtendStub' field and corresponding getter to 'AbstractStubGeneratingFragment' enabling to force the stub generation in Xtend or Java beyond CodeConfig's 'preferXtendStubs'
    Signed-off-by: Christian Schneider <christian.schneider@itemis.de>

--- plugins/org.eclipse.xtext.xtext.generator/xtend-gen/org/eclipse/xtext/xtext/generator/AbstractStubGeneratingFragment.java
+++ plugins/org.eclipse.xtext.xtext.generator/xtend-gen/org/eclipse/xtext/xtext/generator/AbstractStubGeneratingFragment.java
@@ -7,14 +7,37 @@
  */
 package org.eclipse.xtext.xtext.generator;
 
+import com.google.common.base.Objects;
+import com.google.inject.Inject;
+import org.eclipse.xtend.lib.annotations.AccessorType;
 import org.eclipse.xtend.lib.annotations.Accessors;
+import org.eclipse.xtext.xbase.lib.Extension;
 import org.eclipse.xtext.xbase.lib.Pure;
 import org.eclipse.xtext.xtext.generator.AbstractXtextGeneratorFragment;
+import org.eclipse.xtext.xtext.generator.CodeConfig;
 
 @SuppressWarnings("all")
 public abstract class AbstractStubGeneratingFragment extends AbstractXtextGeneratorFragment {
+  @Inject
+  @Extension
+  private CodeConfig _codeConfig;
+  
   @Accessors
   private boolean generateStub = true;
+  
+  @Accessors(AccessorType.PUBLIC_SETTER)
+  private Boolean generateXtendStub = null;
+  
+  public boolean isGenerateXtendStub() {
+    boolean _xifexpression = false;
+    boolean _notEquals = (!Objects.equal(this.generateXtendStub, null));
+    if (_notEquals) {
+      _xifexpression = this.generateXtendStub.booleanValue();
+    } else {
+      _xifexpression = this._codeConfig.isPreferXtendStubs();
+    }
+    return _xifexpression;
+  }
   
   @Pure
   public boolean isGenerateStub() {
@@ -23,5 +46,9 @@ public abstract class AbstractStubGeneratingFragment extends AbstractXtextGenera
   
   public void setGenerateStub(final boolean generateStub) {
     this.generateStub = generateStub;
+  }
+  
+  public void setGenerateXtendStub(final Boolean generateXtendStub) {
+    this.generateXtendStub = generateXtendStub;
   }
 }
