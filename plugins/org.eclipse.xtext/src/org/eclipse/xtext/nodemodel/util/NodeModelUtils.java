353474259ed87f35b7f61fad7198c7219a02d06e
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2015-11-09 17:04:09 +0100

    [NodeModel] Wrong result from findActualNode with actions and fragments

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/nodemodel/util/NodeModelUtils.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/nodemodel/util/NodeModelUtils.java
@@ -234,8 +234,13 @@ public class NodeModelUtils extends InternalNodeModelUtils {
 	public static ICompositeNode findActualNodeFor(/* @Nullable */ EObject semanticObject) {
 		ICompositeNode node = getNode(semanticObject);
 		if (node != null) {
-			while (GrammarUtil.containingAssignment(node.getGrammarElement()) == null && node.getParent() != null && !node.getParent().hasDirectSemanticElement()) {
-				node = node.getParent();
+			while(GrammarUtil.containingAssignment(node.getGrammarElement()) == null) {
+				ICompositeNode parent = node.getParent();
+				if (parent != null && !parent.hasDirectSemanticElement() && !GrammarUtil.isEObjectFragmentRuleCall(parent.getGrammarElement())) {
+					node = parent;
+				} else {
+					break;
+				}
 			}
 		}
 		return node;
