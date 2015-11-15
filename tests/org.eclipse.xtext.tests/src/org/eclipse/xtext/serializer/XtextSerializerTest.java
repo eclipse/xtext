1a4770f6d50e66cbbf0f976fa36aa36898c54294
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2015-11-15 18:14:47 +0100

    Added test for grammar access fragments

--- tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/serializer/XtextSerializerTest.java
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/serializer/XtextSerializerTest.java
@@ -8,17 +8,27 @@
 package org.eclipse.xtext.serializer;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.xtext.AbstractRule;
 import org.eclipse.xtext.Grammar;
+import org.eclipse.xtext.GrammarUtil;
+import org.eclipse.xtext.Group;
+import org.eclipse.xtext.RuleCall;
+import org.eclipse.xtext.TerminalRule;
 import org.eclipse.xtext.XtextStandaloneSetup;
+import org.eclipse.xtext.common.services.TerminalsGrammarAccess;
+import org.eclipse.xtext.grammarinheritance.services.BaseInheritanceTestLanguageGrammarAccess;
+import org.eclipse.xtext.grammarinheritance.services.InheritanceTestLanguageGrammarAccess;
 import org.eclipse.xtext.junit4.AbstractXtextTests;
 import org.eclipse.xtext.junit4.serializer.SerializerTester;
 import org.eclipse.xtext.resource.XtextResourceSet;
+import org.eclipse.xtext.service.GrammarProvider;
 import org.eclipse.xtext.services.XtextGrammarAccess;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import com.google.inject.Inject;
+import com.google.inject.Provider;
 
 /**
  * @author Moritz Eysholdt - Initial contribution and API
@@ -61,5 +71,36 @@ public class XtextSerializerTest extends AbstractXtextTests {
 		XtextGrammarAccess grammarAccess = get(XtextGrammarAccess.class);
 		String string = get(ISerializer.class).serialize(grammarAccess.getGrammarAccess().getGroup_2());
 		Assert.assertEquals("(\"with\" usedGrammars+=[Grammar|GrammarID] (\",\" usedGrammars+=[Grammar|GrammarID])*)?", string);
+	}
+	
+	@Test
+	public void testFQNInSuper_01() {
+		GrammarProvider grammarProvider = new GrammarProvider("org.eclipse.xtext.grammarinheritance.InheritanceTestLanguage", new Provider<XtextResourceSet>() {
+			@Override
+			public XtextResourceSet get() {
+				return XtextSerializerTest.this.get(XtextResourceSet.class);
+			}
+		});
+		TerminalsGrammarAccess gaTerminals = new TerminalsGrammarAccess(grammarProvider);
+		BaseInheritanceTestLanguageGrammarAccess gaBaseInheritanceTestLanguage = new BaseInheritanceTestLanguageGrammarAccess(grammarProvider, gaTerminals);
+		InheritanceTestLanguageGrammarAccess grammarAccess = new InheritanceTestLanguageGrammarAccess(grammarProvider, gaBaseInheritanceTestLanguage, gaTerminals);
+		String string = get(ISerializer.class).serialize(grammarAccess.getFQNRule().getAlternatives());
+		Assert.assertEquals("ID (\".\" ID)*", string);
+	}
+	
+	@Ignore("Serialization does not have the correct context information")
+	@Test
+	public void testFQNInSuper_02() {
+		Grammar grammar = load(URI.createURI("classpath:/org/eclipse/xtext/grammarinheritance/InheritanceTestLanguage.xtext"));
+		AbstractRule rule = GrammarUtil.findRuleForName(grammar, "FQN");
+		Assert.assertNotNull(rule);
+		Group group = (Group) rule.getAlternatives();
+		RuleCall ruleCall = (RuleCall) group.getElements().get(0);
+		TerminalRule id = (TerminalRule) ruleCall.getRule();
+		Assert.assertSame(grammar, GrammarUtil.getGrammar(id));
+		String string = get(ISerializer.class).serialize(rule.getAlternatives());
+		Assert.assertEquals("ID (\".\" ID)*", string);
+		// currently wrong result is 
+		Assert.assertEquals("super::ID (\".\" super::ID)*", string);
 	}
 }
