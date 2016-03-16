cec5c38b8274b23ead31dd7f0497b5a7ac258907
akosyakov <anton.kosyakov@typefox.io>
2016-03-16 13:44:19 +0100

    [bug 489547] Allow to provide offset and length for SyntheticLinkingLeafNode
    Change-Id: I4d723aa426e4f09a48cbee2187e98c5d60900920
    Signed-off-by: akosyakov <anton.kosyakov@typefox.io>

--- tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/linking/Bug437669Test.java
+++ tests/org.eclipse.xtext.tests/src/org/eclipse/xtext/linking/Bug437669Test.java
@@ -16,7 +16,10 @@ import org.eclipse.xtext.linking.importedURI.ImportedURIPackage;
 import org.eclipse.xtext.linking.importedURI.Main;
 import org.eclipse.xtext.linking.importedURI.Type;
 import org.eclipse.xtext.linking.lazy.SyntheticLinkingSupport;
+import org.eclipse.xtext.nodemodel.INode;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.eclipse.xtext.resource.XtextResourceSet;
+import org.eclipse.xtext.util.LineAndColumn;
 import org.junit.Test;
 
 /**
@@ -43,32 +46,64 @@ public class Bug437669Test extends AbstractXtextTests {
 	}
 
 	@Test
-	public void testUnresolved() {
+	public void testUnresolved_01() {
 		Type type = resolve("BlaBlaBla");
 		Resource resource = type.eResource();
 		assertEquals(resource.getErrors().toString(), 1, resource.getErrors().size());
 
 		Diagnostic diagnostic = (Diagnostic) resource.getErrors().get(0);
-		assertEquals(-1, diagnostic.getLine());
 		assertEquals(0, diagnostic.getOffset());
 		assertEquals(1, diagnostic.getLength());
+		assertEquals(1, diagnostic.getLine());
+		assertEquals(1, diagnostic.getColumn());
 		assertEquals("Couldn't resolve reference to Type 'BlaBlaBla'.", diagnostic.getMessage());
 	}
 
-	private Type resolve(String text) {
+	@Test
+	public void testUnresolved_02() {
+		Type type = getContext();
+		INode nameNode = NodeModelUtils.findNodesForFeature(type, ImportedURIPackage.Literals.TYPE__NAME).get(0);
+		resolve(type, "BlaBlaBla", nameNode.getOffset(), nameNode.getLength());
+		Resource resource = type.eResource();
+		assertEquals(resource.getErrors().toString(), 1, resource.getErrors().size());
+		
+		LineAndColumn lineAndColumn = NodeModelUtils.getLineAndColumn(nameNode, nameNode.getOffset());
+
+		Diagnostic diagnostic = (Diagnostic) resource.getErrors().get(0);
+		assertEquals(nameNode.getOffset(), diagnostic.getOffset());
+		assertEquals(nameNode.getLength(), diagnostic.getLength());
+		assertEquals(lineAndColumn.getLine(), diagnostic.getLine());
+		assertEquals(lineAndColumn.getColumn(), diagnostic.getColumn());
+		assertEquals("Couldn't resolve reference to Type 'BlaBlaBla'.", diagnostic.getMessage());
+	}
+
+	protected Type resolve(String text) {
+		Type type = getContext();
+		return resolve(type, text);
+	}
+	
+	protected Type resolve(Type type, String text) {
+		SyntheticLinkingSupport syntheticLinkingSupport = get(SyntheticLinkingSupport.class);
+		syntheticLinkingSupport.createAndSetProxy(type, ImportedURIPackage.Literals.TYPE__EXTENDS, text);
+		EcoreUtil.resolveAll(type.eResource().getResourceSet());
+		return type;
+	}
+	
+	protected Type resolve(Type type, String text, int offset, int length) {
+		SyntheticLinkingSupport syntheticLinkingSupport = get(SyntheticLinkingSupport.class);
+		syntheticLinkingSupport.createAndSetProxy(type, ImportedURIPackage.Literals.TYPE__EXTENDS, text, offset, length);
+		EcoreUtil.resolveAll(type.eResource().getResourceSet());
+		return type;
+	}
+	
+	protected Type getContext() {
 		XtextResourceSet resourceSet = get(XtextResourceSet.class);
 		resourceSet.setClasspathURIContext(getClass().getClassLoader());
 
 		URI uri = URI.createURI("classpath:/org/eclipse/xtext/linking/02.importuritestlanguage");
 		Resource resource = resourceSet.getResource(uri, true);
 		Main main = (Main) resource.getContents().get(0);
-		Type type = main.getTypes().get(0);
-		
-		SyntheticLinkingSupport syntheticLinkingSupport = get(SyntheticLinkingSupport.class);
-		syntheticLinkingSupport.createAndSetProxy(type, ImportedURIPackage.Literals.TYPE__EXTENDS, text);
-
-		EcoreUtil.resolveAll(resourceSet);
-		return type;
+		return main.getTypes().get(0);
 	}
 
 }
