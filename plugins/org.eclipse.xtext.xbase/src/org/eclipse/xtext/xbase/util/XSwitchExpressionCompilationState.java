dde2f6dc19d2a3c4a10506cafba2d08006bd0e72
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2016-04-05 17:54:07 +0200

    470586: XSwitchExpressionCompilationState
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=470586
    Signed-off-by: Lorenzo Bettini <lorenzo.bettini@gmail.com>

---
+++ plugins/org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/util/XSwitchExpressionCompilationState.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2016 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.xbase.util;
+
+/**
+ * Utility class for compiling switch expressions into if statements avoiding useless if check for the first switch's case.
+ * 
+ * See https://bugs.eclipse.org/bugs/show_bug.cgi?id=470586
+ * 
+ * @author Lorenzo Bettini - Initial contribution and API
+ */
+public class XSwitchExpressionCompilationState {
+
+	private int caseProcessedCount = 0;
+
+	public void startProcessingCase() {
+		caseProcessedCount++;
+	}
+
+	public void finishProcessingCase() {
+		caseProcessedCount--;
+	}
+
+	public boolean caseNeedsIfCheck() {
+		return caseProcessedCount > 1;
+	}
+}
---
+++ tests/org.eclipse.xtext.xbase.tests/src/org/eclipse/xtext/xbase/tests/util/XSwitchExpressionCompilationStateTest.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2016 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.xbase.tests.util;
+
+import org.eclipse.xtext.xbase.util.XSwitchExpressionCompilationState;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Lorenzo Bettini - Initial contribution and API
+ */
+public class XSwitchExpressionCompilationStateTest {
+
+	private XSwitchExpressionCompilationState state;
+
+	@Before
+	public void setup() {
+		state = new XSwitchExpressionCompilationState();
+	}
+
+	@Test
+	public void testStateWithSingleCase() {
+		state.startProcessingCase();
+		assertNeedsIfCheck(false);
+		state.finishProcessingCase();
+		assertNeedsIfCheck(false);
+	}
+
+	@Test
+	public void testStateWithSeveralCases() {
+		state.startProcessingCase();
+		assertNeedsIfCheck(false);
+		state.startProcessingCase();
+		assertNeedsIfCheck(true);
+		state.finishProcessingCase();
+		assertNeedsIfCheck(false);
+		state.finishProcessingCase();
+		assertNeedsIfCheck(false);
+	}
+
+	private void assertNeedsIfCheck(boolean expected) {
+		Assert.assertEquals(expected, state.caseNeedsIfCheck());
+	}
+}
