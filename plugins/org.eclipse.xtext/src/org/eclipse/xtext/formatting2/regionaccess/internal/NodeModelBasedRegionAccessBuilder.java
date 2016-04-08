6a3a141e8fbd850369ed1634838de89ca79adbee
Moritz Eysholdt <moritz.eysholdt@typefox.io>
2016-04-08 10:22:13 +0200

    [formatter/textregions] make sure the root has a proper grammar element
    Even though the root has been created via an {Action}, the grammar 
    element should be the root parser rule.
    
    Signed-off-by: Moritz Eysholdt <moritz.eysholdt@typefox.io>

--- plugins/org.eclipse.xtext/src/org/eclipse/xtext/formatting2/regionaccess/internal/NodeModelBasedRegionAccessBuilder.java
+++ plugins/org.eclipse.xtext/src/org/eclipse/xtext/formatting2/regionaccess/internal/NodeModelBasedRegionAccessBuilder.java
@@ -191,7 +191,10 @@ public class NodeModelBasedRegionAccessBuilder {
 		if (tokens.getSemanticElement() == null) {
 			if (node.getParent() == null) {
 				tokens.setSemantcElement(resource.getContents().get(0));
-				tokens.setGrammarElement(node.getGrammarElement());
+				EObject element = node.getGrammarElement();
+				if (element instanceof Action)
+					element = ((ICompositeNode) node).getFirstChild().getGrammarElement();
+				tokens.setGrammarElement(element);
 			} else if (node.hasDirectSemanticElement()) {
 				tokens.setSemantcElement(node.getSemanticElement());
 				tokens.setGrammarElement(findGrammarElement(node, tokens.getSemanticElement()));
