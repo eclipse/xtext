edeb6c1fac73e2d072ff0efa1f288877500c48dd
Christian Schneider <christian.schneider@typefox.io>
2016-05-10 12:15:38 +0200

    [xbase.ide + .ui] introduced a bunch of further syntax coloring configs being conform with those of JDT

--- plugins/org.eclipse.xtext.xbase.ui/src/org/eclipse/xtext/xbase/ui/highlighting/XbaseHighlightingConfiguration.java
+++ plugins/org.eclipse.xtext.xbase.ui/src/org/eclipse/xtext/xbase/ui/highlighting/XbaseHighlightingConfiguration.java
@@ -18,6 +18,7 @@ import org.eclipse.xtext.xbase.ide.highlighting.XbaseHighlightingStyles;
 /**
  * @author Sven Efftinge - Initial contribution and API
  * @author Holger Schill
+ * @author Christian Schneider
  */
 public class XbaseHighlightingConfiguration extends DefaultHighlightingConfiguration {
 	
@@ -30,53 +31,146 @@ public class XbaseHighlightingConfiguration extends DefaultHighlightingConfigura
 
 	@Override
 	public void configure(IHighlightingConfigurationAcceptor acceptor) {
-		acceptor.acceptDefaultHighlighting(STATIC_METHOD_INVOCATION, "Static method invocation", staticMethodInvocation());
-		acceptor.acceptDefaultHighlighting(STATIC_FIELD, "Static Field", staticField());
-		acceptor.acceptDefaultHighlighting(FIELD, "Field", field());
-		acceptor.acceptDefaultHighlighting(ANNOTATION, "Annotation", annotation());
-		acceptor.acceptDefaultHighlighting(EXTENSION_METHOD_INVOCATION, "Extension method invocation", extensionMethodInvocation());
+		acceptor.acceptDefaultHighlighting(STATIC_METHOD_INVOCATION, "Static method invocations", staticMethodInvocation());
+		acceptor.acceptDefaultHighlighting(STATIC_FIELD, "Static fields", staticField());
+		acceptor.acceptDefaultHighlighting(FIELD, "Fields", field());
+		acceptor.acceptDefaultHighlighting(ANNOTATION, "Annotations", annotation());
+		acceptor.acceptDefaultHighlighting(EXTENSION_METHOD_INVOCATION, "Extension method invocations", extensionMethodInvocation());
 		acceptor.acceptDefaultHighlighting(DEPRECATED_MEMBERS, "Deprecated members", deprecatedMembers());
-	
+		
+		acceptor.acceptDefaultHighlighting(XbaseHighlightingStyles.INTERFACE, "Interfaces", interfaces());
+		acceptor.acceptDefaultHighlighting(XbaseHighlightingStyles.ENUM, "Enums", enums());
+		acceptor.acceptDefaultHighlighting(XbaseHighlightingStyles.CLASS, "Classes", classes());
+		acceptor.acceptDefaultHighlighting(XbaseHighlightingStyles.ABSTRACT_CLASS, "Abstract classes", abstractClasses());
+		
+		acceptor.acceptDefaultHighlighting(XbaseHighlightingStyles.STATIC_FINAL_FIELD, "Static final fields", staticFinalField());
+		
+		// not supported yet
+		// acceptor.acceptDefaultHighlighting(XbaseHighlightingStyles.INHERITED_FIELD, "Inherited fields", inheritedField());
+		
+		acceptor.acceptDefaultHighlighting(XbaseHighlightingStyles.METHOD, "Methods", method());
+		acceptor.acceptDefaultHighlighting(XbaseHighlightingStyles.ABSTRACT_METHOD_INVOCATION, "Abstract method invocations", abstractMethodInvocation());
+		
+		// not supported yet
+		// acceptor.acceptDefaultHighlighting(XbaseHighlightingStyles.INHERITED_METHOD_INVOCATION, "Inherited method invocations", inheritedMethodInvocation());
+		
+		acceptor.acceptDefaultHighlighting(XbaseHighlightingStyles.LOCAL_VARIABLE, "Local variables", localVariable());
+		acceptor.acceptDefaultHighlighting(XbaseHighlightingStyles.LOCAL_VARIABLE_DECLARATION, "Local variable declarations", localVariableDecl());
+		acceptor.acceptDefaultHighlighting(XbaseHighlightingStyles.LOCAL_FINAL_VARIABLE, "Local final variables", localFinalVariable());
+		acceptor.acceptDefaultHighlighting(XbaseHighlightingStyles.LOCAL_FINAL_VARIABLE_DECLARATION, "Local final variable declarations", localFinalVariableDecl());
+		acceptor.acceptDefaultHighlighting(XbaseHighlightingStyles.PARAMETER_VARIABLE, "Parameter variables", parameterVariable());
+		acceptor.acceptDefaultHighlighting(XbaseHighlightingStyles.TYPE_VARIABLE, "Type variables", typeVariable());
+		
+		acceptor.acceptDefaultHighlighting(XbaseHighlightingStyles.TYPE_ARGUMENT, "Type arguments", typeArgument());
+		
+		acceptor.acceptDefaultHighlighting(XbaseHighlightingStyles.AUTOBOXING, "Auto(un)boxed expressions", autoboxing());
+		
 		super.configure(acceptor);
 	}
 	
 
-	public TextStyle staticMethodInvocation(){
-		TextStyle textStyle = defaultTextStyle().copy();
+	public TextStyle staticMethodInvocation() {
+		TextStyle textStyle = method().copy();
 		textStyle.setStyle(SWT.ITALIC);
 		return textStyle;
 	}
 	
-	public TextStyle staticField(){
-		TextStyle textStyle = defaultTextStyle().copy();
+	public TextStyle staticField() {
+		TextStyle textStyle = field().copy();
 		textStyle.setStyle(SWT.ITALIC);
 		textStyle.setColor(new RGB(0, 0, 192));
 		return textStyle;
 	}
 	
-	public TextStyle field(){
+	public TextStyle field() {
 		TextStyle textStyle = defaultTextStyle().copy();
 		textStyle.setColor(new RGB(0, 26, 171));
 		return textStyle;
 	}
 	
-	public TextStyle annotation(){
+	public TextStyle annotation() {
 		TextStyle textStyle = defaultTextStyle().copy();
 		textStyle.setColor(new RGB(100,100,100));
 		return textStyle;
 	}
 	
-	public TextStyle extensionMethodInvocation(){
+	public TextStyle extensionMethodInvocation() {
 		TextStyle textStyle = defaultTextStyle().copy();
 		textStyle.setColor(new RGB(171, 48, 0));
 		return textStyle;
 	}
 	
-	public TextStyle deprecatedMembers(){
+	public TextStyle deprecatedMembers() {
 		TextStyle textStyle = defaultTextStyle().copy();
 		textStyle.setStyle(TextAttribute.STRIKETHROUGH);
 		return textStyle;
 	}
-	
 
+	public TextStyle interfaces() {
+		return defaultTextStyle().copy();
+	}
+
+	public TextStyle enums() {
+		return defaultTextStyle().copy();
+	}
+	
+	public TextStyle classes() {
+		return defaultTextStyle().copy();
+	}
+	
+	public TextStyle abstractClasses() {
+		return classes().copy();
+	}
+	
+	public TextStyle staticFinalField() {
+		return staticField().copy();
+	}
+	
+	public TextStyle inheritedField() {
+		return field().copy();
+	}
+	
+	public TextStyle method() {
+		return defaultTextStyle().copy();
+	}
+	
+	public TextStyle abstractMethodInvocation() {
+		return method().copy();
+	}
+	
+	public TextStyle inheritedMethodInvocation() {
+		return method().copy();
+	}
+	
+	public TextStyle localVariable() {
+		return defaultTextStyle().copy();
+	}
+	
+	public TextStyle localVariableDecl() {
+		return localVariable().copy();
+	}
+	
+	public TextStyle localFinalVariable() {
+		return localVariable().copy();
+	}
+	
+	public TextStyle localFinalVariableDecl() {
+		return localFinalVariable().copy();
+	}
+	
+	public TextStyle parameterVariable() {
+		return localVariable().copy();
+	}
+		
+	public TextStyle typeVariable() {
+		return typeArgument().copy();
+	}
+	
+	public TextStyle typeArgument() {
+		return defaultTextStyle().copy();
+	}
+	
+	public TextStyle autoboxing() {
+		return defaultTextStyle().copy();
+	}
 }
