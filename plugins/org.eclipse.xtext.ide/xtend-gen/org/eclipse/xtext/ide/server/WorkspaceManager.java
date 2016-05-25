99e5b12483a92ad22fe2aa09bfd0b533d63d6ffe
Sven Efftinge <sven.efftinge@typefox.io>
2016-05-25 08:38:45 +0200

    [lsp] initial support for multi project builds

--- plugins/org.eclipse.xtext.ide/xtend-gen/org/eclipse/xtext/ide/server/WorkspaceManager.java
+++ plugins/org.eclipse.xtext.ide/xtend-gen/org/eclipse/xtext/ide/server/WorkspaceManager.java
@@ -23,26 +23,20 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.xtext.build.BuildRequest;
 import org.eclipse.xtext.build.IncrementalBuilder;
 import org.eclipse.xtext.build.IndexState;
-import org.eclipse.xtext.build.Source2GeneratedMapping;
 import org.eclipse.xtext.ide.server.Document;
-import org.eclipse.xtext.ide.server.IFileSystemScanner;
+import org.eclipse.xtext.ide.server.ProjectManager;
 import org.eclipse.xtext.resource.IExternalContentSupport;
 import org.eclipse.xtext.resource.IResourceDescription;
-import org.eclipse.xtext.resource.IResourceServiceProvider;
-import org.eclipse.xtext.resource.XtextResourceSet;
-import org.eclipse.xtext.resource.impl.ChunkedResourceDescriptions;
-import org.eclipse.xtext.resource.impl.ProjectDescription;
 import org.eclipse.xtext.resource.impl.ResourceDescriptionsData;
-import org.eclipse.xtext.util.IAcceptor;
 import org.eclipse.xtext.validation.Issue;
 import org.eclipse.xtext.xbase.lib.CollectionLiterals;
 import org.eclipse.xtext.xbase.lib.Functions.Function1;
-import org.eclipse.xtext.xbase.lib.ObjectExtensions;
-import org.eclipse.xtext.xbase.lib.Procedures.Procedure1;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
+import org.eclipse.xtext.xbase.lib.ListExtensions;
 import org.eclipse.xtext.xbase.lib.Procedures.Procedure2;
 
 /**
@@ -51,41 +45,52 @@ import org.eclipse.xtext.xbase.lib.Procedures.Procedure2;
 @SuppressWarnings("all")
 public class WorkspaceManager {
   @Inject
-  protected IncrementalBuilder incrementalBuilder;
+  private Provider<ProjectManager> projectManagerProvider;
   
-  @Inject
-  protected Provider<XtextResourceSet> resourceSetProvider;
-  
-  @Inject
-  protected IResourceServiceProvider.Registry languagesRegistry;
-  
-  @Inject
-  protected IFileSystemScanner fileSystemScanner;
-  
-  @Inject
-  protected IExternalContentSupport externalContentSupport;
-  
-  private IndexState indexState = new IndexState();
+  private Map<URI, ProjectManager> baseDir2ProjectManager = CollectionLiterals.<URI, ProjectManager>newHashMap();
   
   private URI baseDir;
   
-  private Procedure2<? super URI, ? super Iterable<Issue>> issueAcceptor;
-  
   private Map<URI, Document> openDocuments = CollectionLiterals.<URI, Document>newHashMap();
   
+  private final IExternalContentSupport.IExternalContentProvider openedDocumentsContentProvider = new IExternalContentSupport.IExternalContentProvider() {
+    @Override
+    public IExternalContentSupport.IExternalContentProvider getActualContentProvider() {
+      return this;
+    }
+    
+    @Override
+    public String getContent(final URI uri) {
+      Document _get = WorkspaceManager.this.openDocuments.get(uri);
+      String _contents = null;
+      if (_get!=null) {
+        _contents=_get.getContents();
+      }
+      return _contents;
+    }
+    
+    @Override
+    public boolean hasContent(final URI uri) {
+      return WorkspaceManager.this.openDocuments.containsKey(uri);
+    }
+  };
+  
+  private Map<String, ResourceDescriptionsData> fullIndex = CollectionLiterals.<String, ResourceDescriptionsData>newHashMap();
+  
   public void initialize(final URI baseDir, final Procedure2<? super URI, ? super Iterable<Issue>> acceptor) {
-    final ArrayList<URI> uris = CollectionLiterals.<URI>newArrayList();
     this.baseDir = baseDir;
-    this.issueAcceptor = acceptor;
-    final IAcceptor<URI> _function = new IAcceptor<URI>() {
+    final ProjectManager projectManager = this.projectManagerProvider.get();
+    final Provider<Map<String, ResourceDescriptionsData>> _function = new Provider<Map<String, ResourceDescriptionsData>>() {
       @Override
-      public void accept(final URI it) {
-        uris.add(it);
+      public Map<String, ResourceDescriptionsData> get() {
+        return WorkspaceManager.this.fullIndex;
       }
     };
-    this.fileSystemScanner.scan(baseDir, _function);
-    List<URI> _emptyList = CollectionLiterals.<URI>emptyList();
-    this.doBuild(uris, _emptyList);
+    final IncrementalBuilder.Result indexResult = projectManager.initialize(baseDir, acceptor, this.openedDocumentsContentProvider, _function);
+    this.baseDir2ProjectManager.put(baseDir, projectManager);
+    IndexState _indexState = indexResult.getIndexState();
+    ResourceDescriptionsData _resourceDescriptions = _indexState.getResourceDescriptions();
+    this.fullIndex.put("DEFAULT", _resourceDescriptions);
   }
   
   public void didChangeWatchedFiles(final DidChangeWatchedFilesParams fileChanges) {
@@ -109,16 +114,85 @@ public class WorkspaceManager {
   }
   
   public void doBuild(final List<URI> dirtyFiles, final List<URI> deletedFiles) {
-    BuildRequest _newBuildRequest = this.newBuildRequest(dirtyFiles, deletedFiles);
-    final Function1<URI, IResourceServiceProvider> _function = new Function1<URI, IResourceServiceProvider>() {
+    final ArrayList<URI> allDirty = new ArrayList<URI>(dirtyFiles);
+    Set<Map.Entry<URI, ProjectManager>> _entrySet = this.baseDir2ProjectManager.entrySet();
+    for (final Map.Entry<URI, ProjectManager> entry : _entrySet) {
+      {
+        ProjectManager _value = entry.getValue();
+        final Function1<URI, Boolean> _function = new Function1<URI, Boolean>() {
+          @Override
+          public Boolean apply(final URI it) {
+            URI _key = entry.getKey();
+            return Boolean.valueOf(WorkspaceManager.this.isPrefix(it, _key));
+          }
+        };
+        Iterable<URI> _filter = IterableExtensions.<URI>filter(allDirty, _function);
+        List<URI> _list = IterableExtensions.<URI>toList(_filter);
+        final Function1<URI, Boolean> _function_1 = new Function1<URI, Boolean>() {
+          @Override
+          public Boolean apply(final URI it) {
+            URI _key = entry.getKey();
+            return Boolean.valueOf(WorkspaceManager.this.isPrefix(it, _key));
+          }
+        };
+        Iterable<URI> _filter_1 = IterableExtensions.<URI>filter(deletedFiles, _function_1);
+        List<URI> _list_1 = IterableExtensions.<URI>toList(_filter_1);
+        final IncrementalBuilder.Result result = _value.doBuild(_list, _list_1);
+        List<IResourceDescription.Delta> _affectedResources = result.getAffectedResources();
+        final Function1<IResourceDescription.Delta, URI> _function_2 = new Function1<IResourceDescription.Delta, URI>() {
+          @Override
+          public URI apply(final IResourceDescription.Delta it) {
+            return it.getUri();
+          }
+        };
+        List<URI> _map = ListExtensions.<IResourceDescription.Delta, URI>map(_affectedResources, _function_2);
+        allDirty.addAll(_map);
+      }
+    }
+  }
+  
+  public URI getProjectBaseDir(final URI candidate) {
+    Set<URI> _keySet = this.baseDir2ProjectManager.keySet();
+    final Function1<URI, Integer> _function = new Function1<URI, Integer>() {
       @Override
-      public IResourceServiceProvider apply(final URI it) {
-        return WorkspaceManager.this.languagesRegistry.getResourceServiceProvider(it);
+      public Integer apply(final URI it) {
+        String _string = it.toString();
+        int _length = _string.length();
+        return Integer.valueOf((-_length));
       }
     };
-    final IncrementalBuilder.Result result = this.incrementalBuilder.build(_newBuildRequest, _function);
-    IndexState _indexState = result.getIndexState();
-    this.indexState = _indexState;
+    List<URI> _sortBy = IterableExtensions.<URI, Integer>sortBy(_keySet, _function);
+    for (final URI projectBaseDir : _sortBy) {
+      boolean _isPrefix = this.isPrefix(candidate, projectBaseDir);
+      if (_isPrefix) {
+        return projectBaseDir;
+      }
+    }
+    return this.baseDir;
+  }
+  
+  protected boolean isPrefix(final URI candidate, final URI prefix) {
+    String _string = candidate.toString();
+    String _string_1 = prefix.toString();
+    return _string.startsWith(_string_1);
+  }
+  
+  public ProjectManager getProjectManager(final URI uri) {
+    final URI projectBaseDir = this.getProjectBaseDir(uri);
+    return this.baseDir2ProjectManager.get(projectBaseDir);
+  }
+  
+  public URI toUri(final String path) {
+    String _string = this.baseDir.toString();
+    String _plus = (_string + path);
+    return URI.createURI(_plus);
+  }
+  
+  public String toPath(final URI uri) {
+    String _string = uri.toString();
+    String _string_1 = this.baseDir.toString();
+    int _length = _string_1.length();
+    return _string.substring(_length);
   }
   
   public void didChange(final DidChangeTextDocumentParams changeEvent) {
@@ -147,19 +221,6 @@ public class WorkspaceManager {
     this.doBuild(Collections.<URI>unmodifiableList(CollectionLiterals.<URI>newArrayList(uri)), _newArrayList);
   }
   
-  public URI toUri(final String path) {
-    String _string = this.baseDir.toString();
-    String _plus = (_string + path);
-    return URI.createURI(_plus);
-  }
-  
-  public String toPath(final URI uri) {
-    String _string = uri.toString();
-    String _string_1 = this.baseDir.toString();
-    int _length = _string_1.length();
-    return _string.substring(_length);
-  }
-  
   public void didClose(final DidCloseTextDocumentParams changeEvent) {
     TextDocumentIdentifier _textDocument = changeEvent.getTextDocument();
     String _uri = _textDocument.getUri();
@@ -171,84 +232,5 @@ public class WorkspaceManager {
   
   public Object didSave(final DidSaveTextDocumentParams changeEvent) {
     return null;
-  }
-  
-  protected BuildRequest newBuildRequest(final List<URI> changedFiles, final List<URI> deletedFiles) {
-    BuildRequest _buildRequest = new BuildRequest();
-    final Procedure1<BuildRequest> _function = new Procedure1<BuildRequest>() {
-      @Override
-      public void apply(final BuildRequest it) {
-        it.setBaseDir(WorkspaceManager.this.baseDir);
-        List<IResourceDescription> _emptyList = CollectionLiterals.<IResourceDescription>emptyList();
-        ResourceDescriptionsData _resourceDescriptionsData = new ResourceDescriptionsData(_emptyList);
-        XtextResourceSet _createFreshResourceSet = WorkspaceManager.this.createFreshResourceSet(_resourceDescriptionsData);
-        it.setResourceSet(_createFreshResourceSet);
-        ResourceDescriptionsData _resourceDescriptions = WorkspaceManager.this.indexState.getResourceDescriptions();
-        ResourceDescriptionsData _copy = _resourceDescriptions.copy();
-        Source2GeneratedMapping _fileMappings = WorkspaceManager.this.indexState.getFileMappings();
-        Source2GeneratedMapping _copy_1 = _fileMappings.copy();
-        IndexState _indexState = new IndexState(_copy, _copy_1);
-        it.setState(_indexState);
-        IndexState _state = it.getState();
-        ResourceDescriptionsData _resourceDescriptions_1 = _state.getResourceDescriptions();
-        XtextResourceSet _createFreshResourceSet_1 = WorkspaceManager.this.createFreshResourceSet(_resourceDescriptions_1);
-        it.setResourceSet(_createFreshResourceSet_1);
-        it.setDirtyFiles(changedFiles);
-        it.setDeletedFiles(deletedFiles);
-        final BuildRequest.IPostValidationCallback _function = new BuildRequest.IPostValidationCallback() {
-          @Override
-          public boolean afterValidate(final URI uri, final Iterable<Issue> issues) {
-            WorkspaceManager.this.issueAcceptor.apply(uri, issues);
-            return true;
-          }
-        };
-        it.setAfterValidate(_function);
-      }
-    };
-    return ObjectExtensions.<BuildRequest>operator_doubleArrow(_buildRequest, _function);
-  }
-  
-  protected XtextResourceSet createFreshResourceSet(final ResourceDescriptionsData newIndex) {
-    XtextResourceSet _get = this.resourceSetProvider.get();
-    final Procedure1<XtextResourceSet> _function = new Procedure1<XtextResourceSet>() {
-      @Override
-      public void apply(final XtextResourceSet it) {
-        ProjectDescription _projectDescription = new ProjectDescription();
-        final Procedure1<ProjectDescription> _function = new Procedure1<ProjectDescription>() {
-          @Override
-          public void apply(final ProjectDescription it) {
-            it.setName("test-project");
-          }
-        };
-        final ProjectDescription projectDescription = ObjectExtensions.<ProjectDescription>operator_doubleArrow(_projectDescription, _function);
-        projectDescription.attachToEmfObject(it);
-        Map<String, ResourceDescriptionsData> _emptyMap = CollectionLiterals.<String, ResourceDescriptionsData>emptyMap();
-        final ChunkedResourceDescriptions index = new ChunkedResourceDescriptions(_emptyMap, it);
-        String _name = projectDescription.getName();
-        index.setContainer(_name, newIndex);
-        WorkspaceManager.this.externalContentSupport.configureResourceSet(it, new IExternalContentSupport.IExternalContentProvider() {
-          @Override
-          public IExternalContentSupport.IExternalContentProvider getActualContentProvider() {
-            return this;
-          }
-          
-          @Override
-          public String getContent(final URI uri) {
-            Document _get = WorkspaceManager.this.openDocuments.get(uri);
-            String _contents = null;
-            if (_get!=null) {
-              _contents=_get.getContents();
-            }
-            return _contents;
-          }
-          
-          @Override
-          public boolean hasContent(final URI uri) {
-            return WorkspaceManager.this.openDocuments.containsKey(uri);
-          }
-        });
-      }
-    };
-    return ObjectExtensions.<XtextResourceSet>operator_doubleArrow(_get, _function);
   }
 }
