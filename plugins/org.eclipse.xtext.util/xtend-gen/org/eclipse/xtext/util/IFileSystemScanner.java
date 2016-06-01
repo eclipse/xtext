254f9c55bce871e374a126ac00a9d6e89856f743
Dennis Huebner <dennis.huebner@gmail.com>
2016-06-01 14:16:05 +0200

    [misc] Fixed project setup where possible

--- plugins/org.eclipse.xtext.util/xtend-gen/org/eclipse/xtext/util/IFileSystemScanner.java
+++ plugins/org.eclipse.xtext.util/xtend-gen/org/eclipse/xtext/util/IFileSystemScanner.java
@@ -14,6 +14,7 @@ import org.eclipse.xtext.util.IAcceptor;
 
 /**
  * @author Sven Efftinge - Initial contribution and API
+ * @since 2.11
  */
 @ImplementedBy(IFileSystemScanner.JavaIoFileSystemScanner.class)
 @SuppressWarnings("all")
