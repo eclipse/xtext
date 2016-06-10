e5854e0cfc3286e24685f4b4d19a216da46f3ac5
Miro Spönemann <miro.spoenemann@typefox.io>
2016-06-10 14:01:43 +0200

    Create branches before doing filter-branch and a added a task that combines all filter-branch tasks

--- splitting/git-filter-branch.sh
+++ splitting/git-filter-branch.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# TODO change source branch to 'master'
+git checkout -B split-$3 msp/separateBuilds
+
 REMOVALS=`cat $1 | tr '\n' ' '`
 git filter-branch -f --prune-empty \
     --index-filter "git rm -qrf --cached --ignore-unmatch $REMOVALS" \
