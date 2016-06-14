4270986cba1dc9d811dad148f93e11bbc7193fa2
Miro Spönemann <miro.spoenemann@typefox.io>
2016-06-14 08:30:25 +0200

    Moved projects to root of repository

--- splitting/git-filter-branch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git checkout -B split-$3 refs/remotes/origin/repo_splitting
-
-REMOVALS=`cat $1 | tr '\n' ' '`
-git filter-branch -f --prune-empty \
-    --index-filter "git rm -qrf --cached --ignore-unmatch $REMOVALS" \
-    --parent-filter $2/parent-filter.sh \
-    HEAD
