8468d39ed2ad71fae9d006e3be87780d09fd70a0
Miro Spönemann <miro.spoenemann@typefox.io>
2016-07-08 15:55:22 +0200

    Migrated workflows to new generator

---
+++ org.eclipse.xtext.tests/src/org/eclipse/xtext/tests/TestsEMFGeneratorFragment.xtend
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2016 TypeFox GmbH (http://www.typefox.io) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.tests
+
+import org.eclipse.xtend.lib.annotations.Accessors
+import org.eclipse.xtext.Grammar
+import org.eclipse.xtext.xtext.generator.ecore.EMFGeneratorFragment2
+
+/**
+ * @author spoenemann - Initial contribution and API
+ */
+class TestsEMFGeneratorFragment extends EMFGeneratorFragment2 {
+	
+	@Accessors(PUBLIC_SETTER)
+	String ecoreModel
+	
+	String genModel
+	
+	override setGenModel(String genModel) {
+		this.genModel = genModel
+		super.setGenModel(genModel)
+	}
+	
+	override protected getEcoreFilePath(Grammar grammar) {
+		if (!ecoreModel.nullOrEmpty)
+			ecoreModel
+		else
+			javaModelDirectory
+				+ '/' + grammar.name.substring(0, grammar.name.lastIndexOf('.')).replace('.', '/')
+				+ grammar.modelName + '.ecore'
+	}
+	
+	override protected getGenModelPath(Grammar grammar) {
+		if (!genModel.nullOrEmpty)
+			genModel
+		else
+			javaModelDirectory
+				+ '/' + grammar.name.substring(0, grammar.name.lastIndexOf('.')).replace('.', '/')
+				+ grammar.modelName + '.genmodel'
+	}
+	
+}
---
+++ org.eclipse.xtext.tests/xtend-gen/org/eclipse/xtext/tests/TestsEMFGeneratorFragment.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2016 TypeFox GmbH (http://www.typefox.io) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.xtext.tests;
+
+import org.eclipse.xtend.lib.annotations.AccessorType;
+import org.eclipse.xtend.lib.annotations.Accessors;
+import org.eclipse.xtext.Grammar;
+import org.eclipse.xtext.xbase.lib.StringExtensions;
+import org.eclipse.xtext.xtext.generator.ecore.EMFGeneratorFragment2;
+
+/**
+ * @author spoenemann - Initial contribution and API
+ */
+@SuppressWarnings("all")
+public class TestsEMFGeneratorFragment extends EMFGeneratorFragment2 {
+  @Accessors(AccessorType.PUBLIC_SETTER)
+  private String ecoreModel;
+  
+  private String genModel;
+  
+  @Override
+  public void setGenModel(final String genModel) {
+    this.genModel = genModel;
+    super.setGenModel(genModel);
+  }
+  
+  @Override
+  protected String getEcoreFilePath(final Grammar grammar) {
+    String _xifexpression = null;
+    boolean _isNullOrEmpty = StringExtensions.isNullOrEmpty(this.ecoreModel);
+    boolean _not = (!_isNullOrEmpty);
+    if (_not) {
+      _xifexpression = this.ecoreModel;
+    } else {
+      String _javaModelDirectory = this.getJavaModelDirectory();
+      String _plus = (_javaModelDirectory + "/");
+      String _name = grammar.getName();
+      String _name_1 = grammar.getName();
+      int _lastIndexOf = _name_1.lastIndexOf(".");
+      String _substring = _name.substring(0, _lastIndexOf);
+      String _replace = _substring.replace(".", "/");
+      String _plus_1 = (_plus + _replace);
+      String _modelName = this.getModelName(grammar);
+      String _plus_2 = (_plus_1 + _modelName);
+      _xifexpression = (_plus_2 + ".ecore");
+    }
+    return _xifexpression;
+  }
+  
+  @Override
+  protected String getGenModelPath(final Grammar grammar) {
+    String _xifexpression = null;
+    boolean _isNullOrEmpty = StringExtensions.isNullOrEmpty(this.genModel);
+    boolean _not = (!_isNullOrEmpty);
+    if (_not) {
+      _xifexpression = this.genModel;
+    } else {
+      String _javaModelDirectory = this.getJavaModelDirectory();
+      String _plus = (_javaModelDirectory + "/");
+      String _name = grammar.getName();
+      String _name_1 = grammar.getName();
+      int _lastIndexOf = _name_1.lastIndexOf(".");
+      String _substring = _name.substring(0, _lastIndexOf);
+      String _replace = _substring.replace(".", "/");
+      String _plus_1 = (_plus + _replace);
+      String _modelName = this.getModelName(grammar);
+      String _plus_2 = (_plus_1 + _modelName);
+      _xifexpression = (_plus_2 + ".genmodel");
+    }
+    return _xifexpression;
+  }
+  
+  public void setEcoreModel(final String ecoreModel) {
+    this.ecoreModel = ecoreModel;
+  }
+}
