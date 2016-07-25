1f8e10b62763c74a61a581d7204a2d04b5e0e303
Karsten Thoms <karsten.thoms@itemis.de>
2016-07-25 23:24:16 +0200

    Adding generated sources for example projects: - org.eclipse.xtext.example.arithmetics.ide/src-gen
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

---
+++ org.eclipse.xtext.xtext.ui.examples/projects/org.eclipse.xtext.example.arithmetics.ide/src-gen/org/eclipse/xtext/example/arithmetics/ide/AbstractArithmeticsIdeModule.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2015 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.example.arithmetics.ide;
+
+import com.google.inject.Binder;
+import com.google.inject.name.Names;
+import org.eclipse.xtext.example.arithmetics.ide.contentassist.antlr.ArithmeticsParser;
+import org.eclipse.xtext.example.arithmetics.ide.contentassist.antlr.internal.InternalArithmeticsLexer;
+import org.eclipse.xtext.ide.DefaultIdeModule;
+import org.eclipse.xtext.ide.LexerIdeBindings;
+import org.eclipse.xtext.ide.editor.contentassist.antlr.IContentAssistParser;
+import org.eclipse.xtext.ide.editor.contentassist.antlr.internal.Lexer;
+
+/**
+ * Manual modifications go to {@link ArithmeticsIdeModule}.
+ */
+@SuppressWarnings("all")
+public abstract class AbstractArithmeticsIdeModule extends DefaultIdeModule {
+
+	// contributed by org.eclipse.xtext.xtext.generator.parser.antlr.XtextAntlrGeneratorFragment2
+	public void configureContentAssistLexer(Binder binder) {
+		binder.bind(Lexer.class)
+			.annotatedWith(Names.named(LexerIdeBindings.CONTENT_ASSIST))
+			.to(InternalArithmeticsLexer.class);
+	}
+	
+	// contributed by org.eclipse.xtext.xtext.generator.parser.antlr.XtextAntlrGeneratorFragment2
+	public Class<? extends IContentAssistParser> bindIContentAssistParser() {
+		return ArithmeticsParser.class;
+	}
+	
+}
