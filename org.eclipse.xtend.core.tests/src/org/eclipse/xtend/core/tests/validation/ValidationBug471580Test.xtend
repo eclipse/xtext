565de48bdc2e5599aaf2cd0a9397da9f5229fadb
Christian Dietrich <christian.dietrich@itemis.de>
2016-08-03 14:08:11 +0200

    test for https://bugs.eclipse.org/bugs/show_bug.cgi?id=471580
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

---
+++ org.eclipse.xtend.core.tests/src/org/eclipse/xtend/core/tests/validation/ValidationBug471580Test.xtend
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2016 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtend.core.tests.validation
+
+import javax.inject.Inject
+import org.eclipse.xtend.core.tests.AbstractXtendTestCase
+import org.eclipse.xtend.core.xtend.XtendFile
+import org.eclipse.xtext.junit4.util.ParseHelper
+import org.eclipse.xtext.junit4.validation.ValidationTestHelper
+import org.eclipse.xtext.xbase.XbasePackage
+import org.eclipse.xtext.xbase.validation.IssueCodes
+import org.junit.Test
+
+/**
+ * @author dietrich - Initial contribution and API
+ */
+class ValidationBug471580Test extends AbstractXtendTestCase {
+
+	@Inject extension ValidationTestHelper
+	@Inject extension ParseHelper<XtendFile>
+
+	@Test	
+	def void testBug471580() {
+		val file = '''
+		import java.util.Vector
+		class Foo {
+			def m(Vector<?> items) {
+			 	m(#['a'])
+			}
+		}
+		'''.parse
+		file.assertError(
+			XbasePackage.Literals.XLIST_LITERAL,
+			IssueCodes.INCOMPATIBLE_TYPES,
+			'Type mismatch: cannot convert from List<Object> to Vector<?>'
+		)
+	}
+
+}
---
+++ org.eclipse.xtend.core.tests/xtend-gen/org/eclipse/xtend/core/tests/validation/ValidationBug471580Test.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2016 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.xtend.core.tests.validation;
+
+import javax.inject.Inject;
+import org.eclipse.xtend.core.tests.AbstractXtendTestCase;
+import org.eclipse.xtend.core.xtend.XtendFile;
+import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipse.xtext.junit4.util.ParseHelper;
+import org.eclipse.xtext.junit4.validation.ValidationTestHelper;
+import org.eclipse.xtext.xbase.XbasePackage;
+import org.eclipse.xtext.xbase.lib.Exceptions;
+import org.eclipse.xtext.xbase.lib.Extension;
+import org.eclipse.xtext.xbase.validation.IssueCodes;
+import org.junit.Test;
+
+/**
+ * @author dietrich - Initial contribution and API
+ */
+@SuppressWarnings("all")
+public class ValidationBug471580Test extends AbstractXtendTestCase {
+  @Inject
+  @Extension
+  private ValidationTestHelper _validationTestHelper;
+  
+  @Inject
+  @Extension
+  private ParseHelper<XtendFile> _parseHelper;
+  
+  @Test
+  public void testBug471580() {
+    try {
+      StringConcatenation _builder = new StringConcatenation();
+      _builder.append("import java.util.Vector");
+      _builder.newLine();
+      _builder.append("class Foo {");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.append("def m(Vector<?> items) {");
+      _builder.newLine();
+      _builder.append("\t \t");
+      _builder.append("m(#[\'a\'])");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.append("}");
+      _builder.newLine();
+      _builder.append("}");
+      _builder.newLine();
+      final XtendFile file = this._parseHelper.parse(_builder);
+      this._validationTestHelper.assertError(file, 
+        XbasePackage.Literals.XLIST_LITERAL, 
+        IssueCodes.INCOMPATIBLE_TYPES, 
+        "Type mismatch: cannot convert from List<Object> to Vector<?>");
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
+}
