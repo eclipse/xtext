d92792cbeb51e56aac8929f349dfecd80236c7d9
Karsten Thoms <karsten.thoms@itemis.de>
2016-09-29 04:44:35 +0200

    [378821] Add option to generate main method
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- org.eclipse.xtend.ide/src/org/eclipse/xtend/ide/wizards/NewXtendClassWizardPage.java
+++ org.eclipse.xtend.ide/src/org/eclipse/xtend/ide/wizards/NewXtendClassWizardPage.java
@@ -8,19 +8,34 @@
 package org.eclipse.xtend.ide.wizards;
 
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.jdt.internal.ui.wizards.NewWizardMessages;
+import org.eclipse.jdt.internal.ui.wizards.dialogfields.DialogField;
+import org.eclipse.jdt.internal.ui.wizards.dialogfields.LayoutUtil;
+import org.eclipse.jdt.internal.ui.wizards.dialogfields.SelectionButtonDialogFieldGroup;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 
 /**
  * @author Robert von Massow - Initial contribution and API
  * @author Holger Schill
  * @author Anton Kosyakov - https://bugs.eclipse.org/bugs/show_bug.cgi?id=379220
+ * @author Karsten Thoms - bug#378821
  */
+@SuppressWarnings("restriction")
 public class NewXtendClassWizardPage extends AbstractNewXtendElementWizardPage {
+	private SelectionButtonDialogFieldGroup fMethodStubsButtons;
 
 	public NewXtendClassWizardPage() {
 		super(CLASS_TYPE, NewXtendClassWizard.TITLE);
 		this.setTitle(NewXtendClassWizard.TITLE);
 		this.setDescription(Messages.XTEND_CLASS_WIZARD_DESCRIPTION);
+
+		// from org.eclipse.jdt.ui.wizards.NewClassWizardPage
+		// TODO: Enable further options (constructors, abstract methods, comments)
+		String[] buttonNames = new String[] { Messages.METHODS_MAIN };
+		fMethodStubsButtons= new SelectionButtonDialogFieldGroup(SWT.CHECK, buttonNames, 1);
+		fMethodStubsButtons.setLabelText(NewWizardMessages.NewClassWizardPage_methods_label);
 	}
 
 	@Override
@@ -28,6 +43,7 @@ public class NewXtendClassWizardPage extends AbstractNewXtendElementWizardPage {
 		Composite composite = createCommonControls(parent);
 		createSuperClassControls(composite, COLS);
 		createSuperInterfacesControls(composite, COLS);
+		createMethodStubSelectionControls(composite, COLS);
 		setControl(composite);
 	}
 
@@ -50,7 +66,8 @@ public class NewXtendClassWizardPage extends AbstractNewXtendElementWizardPage {
 
 	@Override
 	protected String getTypeContent(String indentation, String lineSperator) {
-		return XtendTypeCreatorUtil.createClassContent(getTypeName(), getSuperClass(), getSuperInterfaces(), indentation, lineSperator);
+		return XtendTypeCreatorUtil.createClassContent(getTypeName(), getSuperClass(), getSuperInterfaces(), indentation, lineSperator,
+				isCreateMain());
 	}
 
 	@Override
@@ -58,6 +75,23 @@ public class NewXtendClassWizardPage extends AbstractNewXtendElementWizardPage {
 		return Messages.ERROR_CREATING_CLASS;
 	}
 	
+	private void createMethodStubSelectionControls(Composite composite, int nColumns) {
+		Control labelControl= fMethodStubsButtons.getLabelControl(composite);
+		LayoutUtil.setHorizontalSpan(labelControl, nColumns);
+
+		DialogField.createEmptySpace(composite);
+
+		Control buttonGroup= fMethodStubsButtons.getSelectionButtonsGroup(composite);
+		LayoutUtil.setHorizontalSpan(buttonGroup, nColumns - 1);
+	}
 	
-	
+	/**
+	 * Returns the current selection state of the 'Create Main' checkbox.
+	 *
+	 * @return the selection state of the 'Create Main' checkbox
+	 */
+	private boolean isCreateMain() {
+		return fMethodStubsButtons.isSelected(0);
+	}
+
 }
