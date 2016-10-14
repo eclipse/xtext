5c5a08a1c2a02939cb8f0cd707304c7efd0cbd11
Sven Efftinge <sven.efftinge@typefox.io>
2016-10-14 10:55:23 +0200

    [LSP] Moved creation of CompletionList to language-specific implementation

--- org.eclipse.xtext.ide/src/org/eclipse/xtext/ide/server/contentassist/ContentAssistService.xtend
+++ org.eclipse.xtext.ide/src/org/eclipse/xtext/ide/server/contentassist/ContentAssistService.xtend
@@ -7,14 +7,27 @@
  *******************************************************************************/
 package org.eclipse.xtext.ide.server.contentassist
 
+import com.google.common.base.Strings
 import com.google.inject.Inject
 import com.google.inject.Provider
 import com.google.inject.Singleton
+import io.typefox.lsapi.CompletionItemKind
+import io.typefox.lsapi.CompletionList
+import io.typefox.lsapi.TextDocumentPositionParams
+import io.typefox.lsapi.builders.CompletionListBuilder
+import io.typefox.lsapi.impl.CompletionItemImpl
+import io.typefox.lsapi.impl.PositionImpl
+import io.typefox.lsapi.impl.RangeImpl
+import io.typefox.lsapi.impl.TextEditImpl
 import java.util.concurrent.ExecutorService
+import org.eclipse.xtext.ide.editor.contentassist.ContentAssistEntry
 import org.eclipse.xtext.ide.editor.contentassist.IIdeContentProposalAcceptor
+import org.eclipse.xtext.ide.editor.contentassist.IdeContentProposalAcceptor
 import org.eclipse.xtext.ide.editor.contentassist.IdeContentProposalProvider
 import org.eclipse.xtext.ide.editor.contentassist.antlr.ContentAssistContextFactory
+import org.eclipse.xtext.ide.server.Document
 import org.eclipse.xtext.resource.XtextResource
+import org.eclipse.xtext.service.OperationCanceledManager
 import org.eclipse.xtext.util.CancelIndicator
 import org.eclipse.xtext.util.TextRegion
 
@@ -25,49 +38,86 @@ import org.eclipse.xtext.util.TextRegion
 @Singleton
 class ContentAssistService {
 
-	public static val DEFAULT_PROPOSALS_LIMIT = 1000
+    public static val DEFAULT_PROPOSALS_LIMIT = 1000
 
-	@Inject Provider<ContentAssistContextFactory> contextFactoryProvider
+    @Inject Provider<ContentAssistContextFactory> contextFactoryProvider
 
-	@Inject ExecutorService executorService
+    @Inject ExecutorService executorService
 
-	@Inject IdeContentProposalProvider proposalProvider
-	
-	def void createProposals(
-		String document,
-		int caretOffset,
-		XtextResource resource,
-		IIdeContentProposalAcceptor acceptor,
-		CancelIndicator cancelIndicator
-	) {
-		val selection = new TextRegion(caretOffset, 0)
-		createProposals(document, selection, caretOffset, resource, acceptor, cancelIndicator)
-	}
+    @Inject IdeContentProposalProvider proposalProvider
 
-	def void createProposals(
-		String document,
-		TextRegion selection,
-		int caretOffset,
-		XtextResource resource,
-		IIdeContentProposalAcceptor acceptor,
-		CancelIndicator cancelIndicator
-	) {
-		createProposals(document, selection, caretOffset, resource, acceptor)
-	}
+    @Inject Provider<IdeContentProposalAcceptor> proposalAcceptorProvider
 
-	def void createProposals(
-		String document,
-		TextRegion selection,
-		int caretOffset,
-		XtextResource resource,
-		IIdeContentProposalAcceptor acceptor
-	) {
-		if(caretOffset > document.length) return;
+    @Inject OperationCanceledManager operationCanceledManager
 
-		val contextFactory = contextFactoryProvider.get
-		contextFactory.pool = executorService
-		val contexts = contextFactory.create(document, selection, caretOffset, resource)
-		proposalProvider.createProposals(contexts, acceptor)
-	}
+    def CompletionList createCompletionList(Document document, XtextResource resource,
+        TextDocumentPositionParams params, CancelIndicator cancelIndicator) {
+        val result = new CompletionListBuilder()
+        // we set isInComplete to true, so we get asked always, which is the best match to the expected behavior in Xtext
+        result.incomplete(true);
 
+        val acceptor = proposalAcceptorProvider.get
+        val caretOffset = document.getOffSet(params.position)
+        val caretPosition = new PositionImpl(params.position as PositionImpl)
+        val position = new TextRegion(caretOffset, 0)
+        try {
+            createProposals(document.contents, position, caretOffset, resource, acceptor)
+        } catch (Throwable t) {
+            if (!operationCanceledManager.isOperationCanceledException(t)) {
+                throw t
+            }
+        }
+        acceptor.getEntries().forEach [ it, idx |
+            val item = toCompletionItem(caretOffset, caretPosition, document)
+            item.sortText = Strings.padStart(Integer.toString(idx), 5, "0")
+            result.item(item)
+        ]
+        return result.build
+    }
+
+    protected def void createProposals(String document, TextRegion selection, int caretOffset, XtextResource resource, IIdeContentProposalAcceptor acceptor) {
+        if(caretOffset > document.length) return;
+
+        val contextFactory = contextFactoryProvider.get
+        contextFactory.pool = executorService
+        val contexts = contextFactory.create(document, selection, caretOffset, resource)
+        proposalProvider.createProposals(contexts, acceptor)
+    }
+
+    protected def CompletionItemImpl toCompletionItem(ContentAssistEntry entry, int caretOffset,
+        PositionImpl caretPosition, Document document) {
+        val completionItem = new CompletionItemImpl
+        completionItem.label = entry.label ?: entry.proposal
+        completionItem.detail = entry.description
+        completionItem.documentation = entry.documentation
+        val prefixOffset = caretOffset - (entry.prefix ?: '').length
+        val prefixPosition = document.getPosition(prefixOffset)
+        completionItem.textEdit = new TextEditImpl(new RangeImpl(prefixPosition, caretPosition), entry.proposal)
+        completionItem.kind = translateKind(entry)
+        return completionItem
+    }
+
+    protected def translateKind(ContentAssistEntry entry) {
+        switch entry.kind {
+            case ContentAssistEntry.KIND_CLASS: CompletionItemKind.Class
+            case ContentAssistEntry.KIND_COLOR: CompletionItemKind.Color
+            case ContentAssistEntry.KIND_CONSTRUCTOR: CompletionItemKind.Constructor
+            case ContentAssistEntry.KIND_ENUM: CompletionItemKind.Enum
+            case ContentAssistEntry.KIND_FIELD: CompletionItemKind.Field
+            case ContentAssistEntry.KIND_FILE: CompletionItemKind.File
+            case ContentAssistEntry.KIND_FUNCTION: CompletionItemKind.Function
+            case ContentAssistEntry.KIND_INTERFACE: CompletionItemKind.Interface
+            case ContentAssistEntry.KIND_KEYWORD: CompletionItemKind.Keyword
+            case ContentAssistEntry.KIND_METHOD: CompletionItemKind.Method
+            case ContentAssistEntry.KIND_MODULE: CompletionItemKind.Module
+            case ContentAssistEntry.KIND_PROPERTY: CompletionItemKind.Property
+            case ContentAssistEntry.KIND_REFERENCE: CompletionItemKind.Reference
+            case ContentAssistEntry.KIND_SNIPPET: CompletionItemKind.Snippet
+            case ContentAssistEntry.KIND_TEXT: CompletionItemKind.Text
+            case ContentAssistEntry.KIND_UNIT: CompletionItemKind.Unit
+            case ContentAssistEntry.KIND_VALUE: CompletionItemKind.Value
+            case ContentAssistEntry.KIND_VARIABLE: CompletionItemKind.Variable
+            default: CompletionItemKind.Value
+        }
+    }
 }
--- org.eclipse.xtext.ide/xtend-gen/org/eclipse/xtext/ide/server/LanguageServerImpl.java
+++ org.eclipse.xtext.ide/xtend-gen/org/eclipse/xtext/ide/server/LanguageServerImpl.java
@@ -7,7 +7,6 @@
  */
 package org.eclipse.xtext.ide.server;
 
-import com.google.common.base.Strings;
 import com.google.common.collect.Iterables;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
@@ -16,7 +15,6 @@ import io.typefox.lsapi.CodeLens;
 import io.typefox.lsapi.CodeLensParams;
 import io.typefox.lsapi.Command;
 import io.typefox.lsapi.CompletionItem;
-import io.typefox.lsapi.CompletionItemKind;
 import io.typefox.lsapi.CompletionList;
 import io.typefox.lsapi.DiagnosticSeverity;
 import io.typefox.lsapi.DidChangeConfigurationParams;
@@ -55,8 +53,7 @@ import io.typefox.lsapi.TextEdit;
 import io.typefox.lsapi.VersionedTextDocumentIdentifier;
 import io.typefox.lsapi.WorkspaceEdit;
 import io.typefox.lsapi.WorkspaceSymbolParams;
-import io.typefox.lsapi.impl.CompletionItemImpl;
-import io.typefox.lsapi.impl.CompletionListImpl;
+import io.typefox.lsapi.builders.CompletionListBuilder;
 import io.typefox.lsapi.impl.CompletionOptionsImpl;
 import io.typefox.lsapi.impl.DiagnosticImpl;
 import io.typefox.lsapi.impl.HoverImpl;
@@ -83,8 +80,6 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.xtend.lib.annotations.Accessors;
 import org.eclipse.xtend.lib.annotations.FinalFieldsConstructor;
 import org.eclipse.xtext.diagnostics.Severity;
-import org.eclipse.xtext.ide.editor.contentassist.ContentAssistEntry;
-import org.eclipse.xtext.ide.editor.contentassist.IdeContentProposalAcceptor;
 import org.eclipse.xtext.ide.server.Document;
 import org.eclipse.xtext.ide.server.UriExtensions;
 import org.eclipse.xtext.ide.server.WorkspaceManager;
@@ -104,7 +99,6 @@ import org.eclipse.xtext.service.OperationCanceledManager;
 import org.eclipse.xtext.util.CancelIndicator;
 import org.eclipse.xtext.validation.Issue;
 import org.eclipse.xtext.xbase.lib.CollectionLiterals;
-import org.eclipse.xtext.xbase.lib.Exceptions;
 import org.eclipse.xtext.xbase.lib.Extension;
 import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.Functions.Function2;
@@ -443,154 +437,17 @@ public class LanguageServerImpl implements LanguageServer, WorkspaceService, Win
         _get=resourceServiceProvider.<ContentAssistService>get(ContentAssistService.class);
       }
       final ContentAssistService contentAssistService = _get;
-      final CompletionListImpl result = new CompletionListImpl();
       if ((contentAssistService == null)) {
-        return result;
+        CompletionListBuilder _completionListBuilder = new CompletionListBuilder();
+        return _completionListBuilder.build();
       }
-      final Function2<Document, XtextResource, ArrayList<CompletionItemImpl>> _function_1 = (Document document, XtextResource resource) -> {
-        try {
-          IResourceServiceProvider _resourceServiceProvider = resource.getResourceServiceProvider();
-          final IdeContentProposalAcceptor acceptor = _resourceServiceProvider.<IdeContentProposalAcceptor>get(IdeContentProposalAcceptor.class);
-          Position _position = params.getPosition();
-          final int caretOffset = document.getOffSet(_position);
-          Position _position_1 = params.getPosition();
-          final PositionImpl caretPosition = new PositionImpl(((PositionImpl) _position_1));
-          try {
-            String _contents = document.getContents();
-            contentAssistService.createProposals(_contents, caretOffset, resource, acceptor, cancelIndicator);
-          } catch (final Throwable _t) {
-            if (_t instanceof Throwable) {
-              final Throwable t = (Throwable)_t;
-              boolean _isOperationCanceledException = this.operationCanceledManager.isOperationCanceledException(t);
-              boolean _not = (!_isOperationCanceledException);
-              if (_not) {
-                throw t;
-              } else {
-                result.setIncomplete(true);
-              }
-            } else {
-              throw Exceptions.sneakyThrow(_t);
-            }
-          }
-          final ArrayList<CompletionItemImpl> completionItems = CollectionLiterals.<CompletionItemImpl>newArrayList();
-          Iterable<ContentAssistEntry> _entries = acceptor.getEntries();
-          final Procedure2<ContentAssistEntry, Integer> _function_2 = (ContentAssistEntry it, Integer idx) -> {
-            final CompletionItemImpl item = this.toCompletionItem(it, caretOffset, caretPosition, document);
-            String _string = Integer.toString((idx).intValue());
-            String _padStart = Strings.padStart(_string, 5, '0');
-            item.setSortText(_padStart);
-            completionItems.add(item);
-          };
-          IterableExtensions.<ContentAssistEntry>forEach(_entries, _function_2);
-          return completionItems;
-        } catch (Throwable _e) {
-          throw Exceptions.sneakyThrow(_e);
-        }
+      final Function2<Document, XtextResource, CompletionList> _function_1 = (Document document, XtextResource resource) -> {
+        return contentAssistService.createCompletionList(document, resource, params, cancelIndicator);
       };
-      ArrayList<CompletionItemImpl> _doRead = this.workspaceManager.<ArrayList<CompletionItemImpl>>doRead(uri, _function_1);
-      result.setItems(_doRead);
+      final CompletionList result = this.workspaceManager.<CompletionList>doRead(uri, _function_1);
       return result;
     };
     return this.requestManager.<CompletionList>runRead(_function);
-  }
-  
-  protected CompletionItemImpl toCompletionItem(final ContentAssistEntry entry, final int caretOffset, final PositionImpl caretPosition, final Document document) {
-    final CompletionItemImpl completionItem = new CompletionItemImpl();
-    String _elvis = null;
-    String _label = entry.getLabel();
-    if (_label != null) {
-      _elvis = _label;
-    } else {
-      String _proposal = entry.getProposal();
-      _elvis = _proposal;
-    }
-    completionItem.setLabel(_elvis);
-    String _description = entry.getDescription();
-    completionItem.setDetail(_description);
-    String _documentation = entry.getDocumentation();
-    completionItem.setDocumentation(_documentation);
-    String _elvis_1 = null;
-    String _prefix = entry.getPrefix();
-    if (_prefix != null) {
-      _elvis_1 = _prefix;
-    } else {
-      _elvis_1 = "";
-    }
-    int _length = _elvis_1.length();
-    final int prefixOffset = (caretOffset - _length);
-    final PositionImpl prefixPosition = document.getPosition(prefixOffset);
-    RangeImpl _rangeImpl = new RangeImpl(prefixPosition, caretPosition);
-    String _proposal_1 = entry.getProposal();
-    TextEditImpl _textEditImpl = new TextEditImpl(_rangeImpl, _proposal_1);
-    completionItem.setTextEdit(_textEditImpl);
-    CompletionItemKind _translateKind = this.translateKind(entry);
-    completionItem.setKind(_translateKind);
-    return completionItem;
-  }
-  
-  protected CompletionItemKind translateKind(final ContentAssistEntry entry) {
-    CompletionItemKind _switchResult = null;
-    String _kind = entry.getKind();
-    switch (_kind) {
-      case ContentAssistEntry.KIND_CLASS:
-        _switchResult = CompletionItemKind.Class;
-        break;
-      case ContentAssistEntry.KIND_COLOR:
-        _switchResult = CompletionItemKind.Color;
-        break;
-      case ContentAssistEntry.KIND_CONSTRUCTOR:
-        _switchResult = CompletionItemKind.Constructor;
-        break;
-      case ContentAssistEntry.KIND_ENUM:
-        _switchResult = CompletionItemKind.Enum;
-        break;
-      case ContentAssistEntry.KIND_FIELD:
-        _switchResult = CompletionItemKind.Field;
-        break;
-      case ContentAssistEntry.KIND_FILE:
-        _switchResult = CompletionItemKind.File;
-        break;
-      case ContentAssistEntry.KIND_FUNCTION:
-        _switchResult = CompletionItemKind.Function;
-        break;
-      case ContentAssistEntry.KIND_INTERFACE:
-        _switchResult = CompletionItemKind.Interface;
-        break;
-      case ContentAssistEntry.KIND_KEYWORD:
-        _switchResult = CompletionItemKind.Keyword;
-        break;
-      case ContentAssistEntry.KIND_METHOD:
-        _switchResult = CompletionItemKind.Method;
-        break;
-      case ContentAssistEntry.KIND_MODULE:
-        _switchResult = CompletionItemKind.Module;
-        break;
-      case ContentAssistEntry.KIND_PROPERTY:
-        _switchResult = CompletionItemKind.Property;
-        break;
-      case ContentAssistEntry.KIND_REFERENCE:
-        _switchResult = CompletionItemKind.Reference;
-        break;
-      case ContentAssistEntry.KIND_SNIPPET:
-        _switchResult = CompletionItemKind.Snippet;
-        break;
-      case ContentAssistEntry.KIND_TEXT:
-        _switchResult = CompletionItemKind.Text;
-        break;
-      case ContentAssistEntry.KIND_UNIT:
-        _switchResult = CompletionItemKind.Unit;
-        break;
-      case ContentAssistEntry.KIND_VALUE:
-        _switchResult = CompletionItemKind.Value;
-        break;
-      case ContentAssistEntry.KIND_VARIABLE:
-        _switchResult = CompletionItemKind.Variable;
-        break;
-      default:
-        _switchResult = CompletionItemKind.Value;
-        break;
-    }
-    return _switchResult;
   }
   
   @Override
