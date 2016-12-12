b7ec4d2584ff06fa83580ccd0074bdb366dcb8c6
Karsten Thoms <karsten.thoms@itemis.de>
2016-12-12 13:17:04 +0100

    Use ToStringBuilder for toString
    Objects#toStringBuilder is marked for removal and its replacement
    MoreObjects#toStringBuilder is only available from Guava 18+
    
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- org.eclipse.xtext/src/org/eclipse/xtext/formatting2/regionaccess/internal/TextSegment.java
+++ org.eclipse.xtext/src/org/eclipse/xtext/formatting2/regionaccess/internal/TextSegment.java
@@ -8,9 +8,7 @@
 package org.eclipse.xtext.formatting2.regionaccess.internal;
 
 import org.eclipse.xtext.formatting2.regionaccess.ITextRegionAccess;
-
-import com.google.common.base.MoreObjects;
-import com.google.common.base.MoreObjects.ToStringHelper;
+import org.eclipse.xtext.xbase.lib.util.ToStringBuilder;
 
 /**
  * @author Moritz Eysholdt - Initial contribution and API
@@ -45,10 +43,10 @@ public class TextSegment extends AbstractTextSegment {
 
 	@Override
 	public String toString() {
-		ToStringHelper helper = MoreObjects.toStringHelper(this).add("offset", offset).add("length", length);
+		ToStringBuilder builder = new ToStringBuilder(this).singleLine().add("offset", offset).add("length", length);
 		if (regionAccess != null)
-			helper.add("text", getText());
-		return helper.toString();
+			builder.add("text", getText());
+		return builder.toString();
 	}
 
 }
