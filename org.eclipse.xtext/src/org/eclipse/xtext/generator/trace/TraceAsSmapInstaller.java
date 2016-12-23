4cc8c07641da5a424bb004a8ca762ab3cb15d797
Sven Efftinge <sven.efftinge@typefox.io>
2016-12-23 09:01:35 +0100

    [smap] made old smap installer try to delegate to new impl. This will allow users to fix this just by adding the corresponding dependency to the classpath.

--- org.eclipse.xtext/src/org/eclipse/xtext/generator/trace/TraceAsSmapInstaller.java
+++ org.eclipse.xtext/src/org/eclipse/xtext/generator/trace/TraceAsSmapInstaller.java
@@ -8,8 +8,13 @@
 package org.eclipse.xtext.generator.trace;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import org.apache.log4j.Logger;
+
+import com.google.inject.Inject;
+import com.google.inject.Injector;
 
 /**
  * @author Sven Efftinge - Initial contribution and API
@@ -17,19 +22,44 @@ import org.apache.log4j.Logger;
 public class TraceAsSmapInstaller implements ITraceToBytecodeInstaller {
 	
 	private static final Logger LOG = Logger.getLogger(TraceAsSmapInstaller.class);
-	private static boolean hasLogged = false;
+	private static final String DELEGATE_CLASS = "org.eclipse.xtext.smap.TraceAsSmapInstaller";
+	Object delegate;
+	
+	@Inject
+	protected void setInjector(Injector injector) {
+		try {
+			Class<?> loadClass = getClass().getClassLoader().loadClass(DELEGATE_CLASS);
+			delegate = injector.getInstance(loadClass);
+		} catch (ClassNotFoundException e) {
+			LOG.warn("Cannot install debug information into bytecode, because 'org.eclipse.xtext.smap' is missing on the classpath. Please add 'org.eclipse.xtext.smap' to your plugin classpath.");
+		}
+	}
 
 	@Override
 	public byte[] installTrace(byte[] javaClassBytecode) throws IOException {
-		if (!hasLogged) {
-			hasLogged = true;
-			LOG.error("The class "+getClass().getName()+" has been moved to 'org.eclipse.xtext.smap'. Please update your dependency.");
+		if (delegate == null) {
+			return javaClassBytecode;
+		}
+		try {
+			Method method = delegate.getClass().getMethod("installTrace", javaClassBytecode.getClass());
+			Object result = method.invoke(delegate, javaClassBytecode);
+			return (byte[]) result;
+		} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			LOG.error("Error installing trace information.", e);
 		}
 		return javaClassBytecode;
 	}
 
 	@Override
 	public void setTrace(String javaFileName, AbstractTraceRegion trace) {
+		if (delegate == null) {
+			return;
+		}
+		try {
+			Method method = delegate.getClass().getMethod("setTrace", String.class, AbstractTraceRegion.class);
+			method.invoke(delegate, javaFileName, trace);
+		} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			LOG.error("Error setting trace information.", e);
+		}
 	}
-
 }
