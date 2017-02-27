bae8cad78a21cabbd3d91ffdb48fc67241f26fdb
Christian Dietrich <christian.dietrich@itemis.de>
2017-02-27 16:53:58 +0100

    fixed missing gwt sources. #40 (#43) (#44)
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtend.lib.gwt.test/src/org/eclipse/xtend/lib/test/TestEntryPoint.java
+++ org.eclipse.xtend.lib.gwt.test/src/org/eclipse/xtend/lib/test/TestEntryPoint.java
@@ -1,8 +1,11 @@
 package org.eclipse.xtend.lib.test;
 
+import org.eclipse.xtend2.lib.StringConcatenation;
+
 import com.google.gwt.core.client.EntryPoint;
 
 public class TestEntryPoint implements EntryPoint {
   public void onModuleLoad() {
+	  StringConcatenation demo = null;
   }
 }
