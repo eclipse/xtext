85fa43381196b9b281fe764e4ee69e380dff497c
Christian Dietrich <christian.dietrich@itemis.de>
2017-03-16 14:21:02 +0100

    allow multiple quickfixes. #2
    added test for multiple  @Fix Annotations. #2
    
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtext.ui.tests/xtend-gen/org/eclipse/xtext/ui/tests/editor/quickfix/DeclarativeQuickfixProviderTest.java
+++ org.eclipse.xtext.ui.tests/xtend-gen/org/eclipse/xtext/ui/tests/editor/quickfix/DeclarativeQuickfixProviderTest.java
@@ -14,6 +14,7 @@ import org.eclipse.xtext.ui.editor.model.edit.IModification;
 import org.eclipse.xtext.ui.editor.model.edit.IssueModificationContext;
 import org.eclipse.xtext.ui.editor.quickfix.AbstractDeclarativeQuickfixProvider;
 import org.eclipse.xtext.ui.editor.quickfix.Fix;
+import org.eclipse.xtext.ui.editor.quickfix.Fixes;
 import org.eclipse.xtext.ui.editor.quickfix.IssueResolution;
 import org.eclipse.xtext.ui.editor.quickfix.IssueResolutionAcceptor;
 import org.eclipse.xtext.validation.Issue;
@@ -31,6 +32,8 @@ import org.junit.Test;
 public class DeclarativeQuickfixProviderTest extends Assert {
   private final static String DUMMY_CODE = "DeclarativeQuickfixProviderTest.DummyCode";
   
+  private final static String DUMMY_CODE2 = "DeclarativeQuickfixProviderTest.DummyCode2";
+  
   @Test
   public void testHasResolutions() throws Exception {
     abstract class __DeclarativeQuickfixProviderTest_1 extends AbstractDeclarativeQuickfixProvider {
@@ -47,14 +50,47 @@ public class DeclarativeQuickfixProviderTest extends Assert {
   }
   
   @Test
-  public void testGetResolutions() throws Exception {
+  public void testHasResolutions2() throws Exception {
     abstract class __DeclarativeQuickfixProviderTest_2 extends AbstractDeclarativeQuickfixProvider {
+      public abstract void signature(final Issue i, final IssueResolutionAcceptor acceptor);
+    }
+    
+    AbstractDeclarativeQuickfixProvider provider = new __DeclarativeQuickfixProviderTest_2() {
+      @Fix(DeclarativeQuickfixProviderTest.DUMMY_CODE)
+      @Fix(DeclarativeQuickfixProviderTest.DUMMY_CODE2)
+      public void signature(final Issue i, final IssueResolutionAcceptor acceptor) {
+      }
+    };
+    Assert.assertFalse(provider.hasResolutionFor((DeclarativeQuickfixProviderTest.DUMMY_CODE + Integer.valueOf(1))));
+    Assert.assertTrue(provider.hasResolutionFor(DeclarativeQuickfixProviderTest.DUMMY_CODE));
+    Assert.assertTrue(provider.hasResolutionFor(DeclarativeQuickfixProviderTest.DUMMY_CODE2));
+  }
+  
+  @Test
+  public void testHasResolutions3() throws Exception {
+    abstract class __DeclarativeQuickfixProviderTest_3 extends AbstractDeclarativeQuickfixProvider {
+      public abstract void signature(final Issue i, final IssueResolutionAcceptor acceptor);
+    }
+    
+    AbstractDeclarativeQuickfixProvider provider = new __DeclarativeQuickfixProviderTest_3() {
+      @Fixes({ @Fix(DeclarativeQuickfixProviderTest.DUMMY_CODE), @Fix(DeclarativeQuickfixProviderTest.DUMMY_CODE2) })
+      public void signature(final Issue i, final IssueResolutionAcceptor acceptor) {
+      }
+    };
+    Assert.assertFalse(provider.hasResolutionFor((DeclarativeQuickfixProviderTest.DUMMY_CODE + Integer.valueOf(1))));
+    Assert.assertTrue(provider.hasResolutionFor(DeclarativeQuickfixProviderTest.DUMMY_CODE));
+    Assert.assertTrue(provider.hasResolutionFor(DeclarativeQuickfixProviderTest.DUMMY_CODE2));
+  }
+  
+  @Test
+  public void testGetResolutions() throws Exception {
+    abstract class __DeclarativeQuickfixProviderTest_4 extends AbstractDeclarativeQuickfixProvider {
       public abstract void fixError1(final Issue i, final IssueResolutionAcceptor acceptor);
       
       public abstract void fixError2(final Issue i, final IssueResolutionAcceptor acceptor);
     }
     
-    AbstractDeclarativeQuickfixProvider provider = new __DeclarativeQuickfixProviderTest_2() {
+    AbstractDeclarativeQuickfixProvider provider = new __DeclarativeQuickfixProviderTest_4() {
       @Fix(DeclarativeQuickfixProviderTest.DUMMY_CODE)
       public void fixError1(final Issue i, final IssueResolutionAcceptor acceptor) {
         acceptor.accept(i, "fixError1", "", "", IModification.NULL);
@@ -86,6 +122,73 @@ public class DeclarativeQuickfixProviderTest extends Assert {
       return Boolean.valueOf(Objects.equal(_label, "fixError2"));
     };
     Assert.assertTrue(IterableExtensions.<IssueResolution>exists(resolutions, _function_2));
+  }
+  
+  @Test
+  public void testGetResolutions2() throws Exception {
+    abstract class __DeclarativeQuickfixProviderTest_5 extends AbstractDeclarativeQuickfixProvider {
+      public abstract void fixError1(final Issue i, final IssueResolutionAcceptor acceptor);
+      
+      public abstract void fixError2(final Issue i, final IssueResolutionAcceptor acceptor);
+      
+      public abstract void fixError3(final Issue i, final IssueResolutionAcceptor acceptor);
+    }
+    
+    AbstractDeclarativeQuickfixProvider provider = new __DeclarativeQuickfixProviderTest_5() {
+      @Fix(DeclarativeQuickfixProviderTest.DUMMY_CODE)
+      @Fix(DeclarativeQuickfixProviderTest.DUMMY_CODE2)
+      public void fixError1(final Issue i, final IssueResolutionAcceptor acceptor) {
+        acceptor.accept(i, "fixError1", "", "", IModification.NULL);
+      }
+      
+      @Fix(DeclarativeQuickfixProviderTest.DUMMY_CODE)
+      public void fixError2(final Issue i, final IssueResolutionAcceptor acceptor) {
+        acceptor.accept(i, "fixError2", "", "", IModification.NULL);
+      }
+      
+      @Fixes({ @Fix(DeclarativeQuickfixProviderTest.DUMMY_CODE), @Fix(DeclarativeQuickfixProviderTest.DUMMY_CODE2) })
+      public void fixError3(final Issue i, final IssueResolutionAcceptor acceptor) {
+        acceptor.accept(i, "fixError3", "", "", IModification.NULL);
+      }
+    };
+    final Provider<IssueResolutionAcceptor> _function = () -> {
+      final IssueModificationContext.Factory _function_1 = (Issue it) -> {
+        return null;
+      };
+      return new IssueResolutionAcceptor(_function_1);
+    };
+    provider.setIssueResolutionAcceptorProvider(_function);
+    List<IssueResolution> resolutionsIterable = provider.getResolutions(this.createIssue((DeclarativeQuickfixProviderTest.DUMMY_CODE + Integer.valueOf(1))));
+    Assert.assertNull(IterableExtensions.<IssueResolution>head(resolutionsIterable));
+    List<IssueResolution> resolutions = provider.getResolutions(this.createIssue(DeclarativeQuickfixProviderTest.DUMMY_CODE));
+    Assert.assertEquals(3, resolutions.size());
+    final Function1<IssueResolution, Boolean> _function_1 = (IssueResolution it) -> {
+      String _label = it.getLabel();
+      return Boolean.valueOf(Objects.equal(_label, "fixError1"));
+    };
+    Assert.assertTrue(IterableExtensions.<IssueResolution>exists(resolutions, _function_1));
+    final Function1<IssueResolution, Boolean> _function_2 = (IssueResolution it) -> {
+      String _label = it.getLabel();
+      return Boolean.valueOf(Objects.equal(_label, "fixError2"));
+    };
+    Assert.assertTrue(IterableExtensions.<IssueResolution>exists(resolutions, _function_2));
+    final Function1<IssueResolution, Boolean> _function_3 = (IssueResolution it) -> {
+      String _label = it.getLabel();
+      return Boolean.valueOf(Objects.equal(_label, "fixError3"));
+    };
+    Assert.assertTrue(IterableExtensions.<IssueResolution>exists(resolutions, _function_3));
+    List<IssueResolution> resolutions2 = provider.getResolutions(this.createIssue(DeclarativeQuickfixProviderTest.DUMMY_CODE2));
+    Assert.assertEquals(2, resolutions2.size());
+    final Function1<IssueResolution, Boolean> _function_4 = (IssueResolution it) -> {
+      String _label = it.getLabel();
+      return Boolean.valueOf(Objects.equal(_label, "fixError2"));
+    };
+    Assert.assertTrue(IterableExtensions.<IssueResolution>exists(resolutions, _function_4));
+    final Function1<IssueResolution, Boolean> _function_5 = (IssueResolution it) -> {
+      String _label = it.getLabel();
+      return Boolean.valueOf(Objects.equal(_label, "fixError3"));
+    };
+    Assert.assertTrue(IterableExtensions.<IssueResolution>exists(resolutions, _function_5));
   }
   
   protected Issue createIssue(final String code) {
--- org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/quickfix/Fixes.java
+++ org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/quickfix/Fixes.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 itemis AG (http://www.itemis.eu) and others.
+ * Copyright (c) 2017 itemis AG (http://www.itemis.de) and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,18 +8,20 @@
 package org.eclipse.xtext.ui.editor.quickfix;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * @author Knut Wannheden - Initial contribution and API
- * @author Jan Koehnlein
+ * Annotation Container for {@link Fix} Annotations
+ * 
+ * @author dietrich - Initial contribution and API
+ * @since 2.12
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
-@Repeatable(Fixes.class)
-public @interface Fix {
-    String value();
+public @interface Fixes {
+	
+	Fix[] value();
+
 }
