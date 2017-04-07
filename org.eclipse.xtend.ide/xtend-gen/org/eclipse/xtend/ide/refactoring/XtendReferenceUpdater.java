3e1070a627233072a70f7c4abf0dffd93f7c88cc
Christian Dietrich <christian.dietrich@itemis.de>
2017-04-07 10:07:38 +0200

    Sort References on Rename Refactoring to Prevent NPE when Anon Inner Class Constructors are Involved. Fixes https://github.com/eclipse/xtext-core/issues/300
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtend.ide/xtend-gen/org/eclipse/xtend/ide/refactoring/XtendReferenceUpdater.java
+++ org.eclipse.xtend.ide/xtend-gen/org/eclipse/xtend/ide/refactoring/XtendReferenceUpdater.java
@@ -7,13 +7,17 @@
  */
 package org.eclipse.xtend.ide.refactoring;
 
+import java.util.Comparator;
+import java.util.List;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.xtend.core.xtend.AnonymousClass;
 import org.eclipse.xtext.common.types.JvmType;
+import org.eclipse.xtext.resource.IReferenceDescription;
 import org.eclipse.xtext.ui.refactoring.IRefactoringUpdateAcceptor;
 import org.eclipse.xtext.xbase.XConstructorCall;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
 import org.eclipse.xtext.xbase.ui.refactoring.XbaseReferenceUpdater;
 
 /**
@@ -29,5 +33,33 @@ public class XtendReferenceUpdater extends XbaseReferenceUpdater {
       return;
     }
     super.createReferenceUpdate(referringElement, referringResourceURI, reference, indexInList, newTargetElement, updateAcceptor);
+  }
+  
+  @Override
+  protected Iterable<IReferenceDescription> getNotImportTypeReferences(final Iterable<IReferenceDescription> referenceDescriptions) {
+    List<IReferenceDescription> _xblockexpression = null;
+    {
+      final List<IReferenceDescription> result = IterableExtensions.<IReferenceDescription>toList(super.getNotImportTypeReferences(referenceDescriptions));
+      final String localClassesFragmentPart = "@localClasses.";
+      final Comparator<IReferenceDescription> _function = (IReferenceDescription o1, IReferenceDescription o2) -> {
+        final String f1 = o1.getSourceEObjectUri().fragment();
+        final String f2 = o2.getSourceEObjectUri().fragment();
+        if ((f1.contains(localClassesFragmentPart) && f2.contains(localClassesFragmentPart))) {
+          return f1.compareTo(f2);
+        }
+        boolean _contains = f1.contains(localClassesFragmentPart);
+        if (_contains) {
+          return 1;
+        }
+        boolean _contains_1 = f2.contains(localClassesFragmentPart);
+        if (_contains_1) {
+          return (-1);
+        }
+        return f1.compareTo(f2);
+      };
+      result.sort(_function);
+      _xblockexpression = result;
+    }
+    return _xblockexpression;
   }
 }
