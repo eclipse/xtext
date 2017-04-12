8fa6676c72a3b1c7fb77fd049b1ff87cac781ba4
Christian Dietrich <christian.dietrich@itemis.de>
2017-04-12 09:00:57 +0200

    added support for creating annotation properties in xtend active annotations. https://github.com/eclipse/xtext-xtend/issues/200
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtend.lib.macro/src/org/eclipse/xtend/lib/macro/declaration/MutableAnnotationTypeElementDeclaration.java
+++ org.eclipse.xtend.lib.macro/src/org/eclipse/xtend/lib/macro/declaration/MutableAnnotationTypeElementDeclaration.java
@@ -7,6 +7,9 @@
  *******************************************************************************/
 package org.eclipse.xtend.lib.macro.declaration;
 
+import org.eclipse.xtend.lib.macro.expression.Expression;
+import org.eclipse.xtend2.lib.StringConcatenationClient;
+
 import com.google.common.annotations.Beta;
 
 /**
@@ -17,5 +20,47 @@ import com.google.common.annotations.Beta;
  */
 @Beta
 public interface MutableAnnotationTypeElementDeclaration extends MutableMemberDeclaration, AnnotationTypeElementDeclaration {
+	
+	/**
+	 * Setting an expression will alter the scope of it accordingly.
+	 * Each expression can only be assigned to one element, if this {@link MutableExecutableDeclaration} has already set 
+	 * another expression or compilation strategy the old one will be detached.
+	 * 
+	 * If the given expression is already assigned to another {@link MutableExecutableDeclaration} or {@link MutableFieldDeclaration}
+	 * it will be automatically detached from it.
+	 * 
+	 * @param value the expression to set as the default value of this annotation property
+	 */
+	void setDefaultValueExpression(Expression value);
+	
+	/**
+	 * Sets the given {@link CompilationStrategy} as the default value.
+	 * 
+	 * <b>The given {@link CompilationStrategy} will be executed later during code generation, not immediately.</b>
+	 * 
+	 * If this {@link MutableExecutableDeclaration} has already set another expression or compilation strategy the old one will be detached.
+	 * 
+	 * @param compilationStrategy the compilation strategy, must not be <code>null</code>
+	 * @exception IllegalArgumentException if <code>compilationStrategy</code> is <code>null</code>
+	 */
+	void setDefaultValueExpression(CompilationStrategy compilationStrategy);
+	
+	/**
+	 * Sets the given {@link StringConcatenationClient} as the default value.
+	 * 
+	 * <b>The given {@link StringConcatenationClient} will be executed later during code generation, not immediately.</b>
+	 * 
+	 * If this {@link MutableExecutableDeclaration} has already set another expression or compilation strategy the old one will be detached.
+	 * 
+	 * @param compilationTemplate the compilation strategy, must not be <code>null</code>
+	 * @exception IllegalArgumentException if <code>compilationStrategy</code> is <code>null</code>
+	 */
+	void setDefaultValueExpression(StringConcatenationClient compilationTemplate);
+	
+	/**
+	 * @param type the type of this annotation property, must be not <code>null</code>
+	 * @exception IllegalArgumentException if the <code>type</code> is <code>null</code>
+	 */
+	void setType(TypeReference type);
 	
 }
