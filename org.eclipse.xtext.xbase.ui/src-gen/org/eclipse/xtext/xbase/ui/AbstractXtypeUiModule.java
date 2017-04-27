9f2e5bab4ab759837b003319fa16e18b148a5ee1
Miro Spönemann <miro.spoenemann@typefox.io>
2017-04-27 14:43:47 +0200

    Regenerated Xbase language

--- org.eclipse.xtext.xbase.ui/src-gen/org/eclipse/xtext/xbase/ui/AbstractXtypeUiModule.java
+++ org.eclipse.xtext.xbase.ui/src-gen/org/eclipse/xtext/xbase/ui/AbstractXtypeUiModule.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010-2015 itemis AG (http://www.itemis.eu) and others.
+ * Copyright (c) 2010-2017 itemis AG (http://www.itemis.eu) and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -42,7 +42,7 @@ public abstract class AbstractXtypeUiModule extends DefaultUiModule {
 	}
 	
 	// contributed by org.eclipse.xtext.xtext.generator.ImplicitFragment
-	public Provider<IAllContainersState> provideIAllContainersState() {
+	public Provider<? extends IAllContainersState> provideIAllContainersState() {
 		return Access.getJavaProjectsState();
 	}
 	
