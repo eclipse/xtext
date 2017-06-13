14b2f3568401604a587b69037cdfa7e225976e7e
Sebastian Zarnekow <Sebastian.Zarnekow@itemis.de>
2017-06-13 19:32:11 +0200

    Yet another attempt (tm)

---
+++ org.eclipse.xtext.tests/xtend-gen/org/eclipse/xtext/parser/indentation/IndentationAwareLanguageSuite.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2015 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.xtext.parser.indentation;
+
+import org.eclipse.xtext.parser.indentation.IndentationAwareLanguageTest;
+import org.eclipse.xtext.parser.indentation.NodeModelTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ IndentationAwareLanguageTest.class, NodeModelTest.class })
+@SuppressWarnings("all")
+public class IndentationAwareLanguageSuite {
+}
--- org.eclipse.xtext.tests/xtend-gen/org/eclipse/xtext/parser/indentation/IndentationAwarePartialParsingTests.java
+++ org.eclipse.xtext.tests/xtend-gen/org/eclipse/xtext/parser/indentation/IndentationAwarePartialParsingTests.java
@@ -7,20 +7,17 @@
  */
 package org.eclipse.xtext.parser.indentation;
 
-import org.eclipse.xtext.parser.indentation.IndentationAwareLanguageTest;
-import org.eclipse.xtext.parser.indentation.NodeModelTest;
+import org.eclipse.xtext.parser.indentation.IndentationAwareLanguageSuite;
 import org.eclipse.xtext.testing.smoketest.ProcessedBy;
 import org.eclipse.xtext.testing.smoketest.XtextSmokeTestRunner;
 import org.eclipse.xtext.testing.smoketest.processors.PartialParsingProcessor;
 import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
  */
 @RunWith(XtextSmokeTestRunner.class)
 @ProcessedBy(value = PartialParsingProcessor.class, processInParallel = true)
-@Suite.SuiteClasses({ IndentationAwareLanguageTest.class, NodeModelTest.class })
 @SuppressWarnings("all")
-public class IndentationAwarePartialParsingTest {
+public class IndentationAwarePartialParsingTests extends IndentationAwareLanguageSuite {
 }
