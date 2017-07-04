b3f48fda5956b1437768a5b8a8ef4a4916f2b670
Karsten Thoms <karsten.thoms@itemis.de>
2017-07-04 14:03:05 +0200

    [462906] error handling when no Injector can be retrieved
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/guice/AbstractGuiceAwareExecutableExtensionFactory.java
+++ org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/guice/AbstractGuiceAwareExecutableExtensionFactory.java
@@ -15,6 +15,7 @@ import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExecutableExtension;
 import org.eclipse.core.runtime.IExecutableExtensionFactory;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 import org.osgi.framework.Bundle;
 
@@ -49,15 +50,35 @@ public abstract class AbstractGuiceAwareExecutableExtensionFactory implements IE
 		try {
 			final Class<?> clazz = getBundle().loadClass(clazzName);
 			final Injector injector = getInjector();
+			if (injector == null) {
+				throw handleCreationError(null);
+			}
 			final Object result = injector.getInstance(clazz);
 			if (result instanceof IExecutableExtension)
 				((IExecutableExtension) result).setInitializationData(config, null, null);
 			return result;
+		} catch (Throwable e) {
+			throw handleCreationError(e);
 		}
-		catch (Exception e) {
-			log.error(e.getMessage(), e);
-			throw new CoreException(new Status(IStatus.ERROR, getBundle().getSymbolicName(), e.getMessage() + " ExtensionFactory: "+ getClass().getName(), e));
+	}
+	
+	private CoreException handleCreationError (Throwable cause) {
+		Bundle bundle = null;
+		String bundleName = null;
+		try {
+			bundle = getBundle();
+		} catch (NullPointerException e) {
+			// subclasses generated with Xtext <2.13 will raise a NPE when the UI bundle wqs not activated
+			// try to guess the bundle from the EEF's package
+			bundle = Platform.getBundle(getClass().getPackage().getName());
 		}
+		bundleName = bundle != null ? bundle.getSymbolicName() : "UNKNOWN";
+		
+		if (bundle != null && bundle.getState()!=Bundle.ACTIVE) {
+			return new CoreException(new Status(IStatus.ERROR, bundleName, "Bundle "+ bundleName+" not activated. Please check that all dependencies could be resolved and 'Bundle-ActivationPolicy: lazy' is configured in the bundle's MANIFEST.MF. Check also the error log."));	
+		}
+		
+		return new CoreException(new Status(IStatus.ERROR, bundleName, cause.getMessage() + " ExtensionFactory: "+ getClass().getName(), cause));
 	}
 	
 	protected abstract Bundle getBundle();
