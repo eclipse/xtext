b3b2ac87c8d2b33476727d43ae6dc657d2f7524f
Christian Dietrich <christian.dietrich@itemis.de>
2017-08-17 16:05:16 +0200

    restored ClasspathScannerTest #16
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

---
+++ org.eclipse.xtext.xbase.tests/xtend-gen/org/eclipse/xtext/xbase/tests/util/ClasspathScannerTest.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2015 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.xtext.xbase.tests.util;
+
+import com.google.common.base.Objects;
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Collections;
+import org.eclipse.xtext.xbase.ide.types.ClasspathScanner;
+import org.eclipse.xtext.xbase.ide.types.ITypeDescriptor;
+import org.eclipse.xtext.xbase.lib.CollectionLiterals;
+import org.eclipse.xtext.xbase.lib.Exceptions;
+import org.eclipse.xtext.xbase.lib.Functions.Function1;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
+import org.junit.Assert;
+import org.junit.Test;
+
+@SuppressWarnings("all")
+public class ClasspathScannerTest {
+  private final ClasspathScanner scanner = new ClasspathScanner();
+  
+  @Test
+  public void testBootClasspathScanning() {
+    final Iterable<ITypeDescriptor> javaUtil = this.scanner.getBootClasspathDescriptors(Collections.<String>unmodifiableList(CollectionLiterals.<String>newArrayList("java.util")));
+    final Function1<ITypeDescriptor, Boolean> _function = (ITypeDescriptor it) -> {
+      String _simpleName = it.getSimpleName();
+      return Boolean.valueOf(Objects.equal(_simpleName, "BitSet"));
+    };
+    Assert.assertTrue(IterableExtensions.<ITypeDescriptor>exists(javaUtil, _function));
+  }
+  
+  @Test
+  public void testClasspathScanning() {
+    try {
+      final File bootstrapJar = new File("./somelib/sample.jar");
+      URL _uRL = bootstrapJar.toURI().toURL();
+      final URLClassLoader classloader = new URLClassLoader(new URL[] { _uRL });
+      final Iterable<ITypeDescriptor> utilPackage = this.scanner.getDescriptors(classloader, Collections.<String>unmodifiableList(CollectionLiterals.<String>newArrayList("sample")));
+      final Function1<ITypeDescriptor, Boolean> _function = (ITypeDescriptor it) -> {
+        String _name = it.getName();
+        return Boolean.valueOf(Objects.equal(_name, "sample.Sample"));
+      };
+      Assert.assertTrue(IterableExtensions.<ITypeDescriptor>exists(utilPackage, _function));
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
+}
