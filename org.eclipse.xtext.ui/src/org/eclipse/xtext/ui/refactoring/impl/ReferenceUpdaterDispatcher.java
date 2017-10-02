rameshsampenga/xtext-eclipse

GH-284 use fragemnt path URIs for rename refactoring
Signed-off-by: Jan Koehnlein <jan.koehnlein@typefox.io>

org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/refactoring/impl/ReferenceUpdaterDispatcher.java
@@ -18,6 +18,7 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.findReferences.IReferenceFinder;
 import org.eclipse.xtext.resource.IReferenceDescription;
 import org.eclipse.xtext.resource.IResourceDescriptions;
@@ -122,7 +123,7 @@ public class ReferenceUpdaterDispatcher {
 		@Override
 		public void accept(EObject source, URI sourceURI, EReference eReference, int index, EObject targetOrProxy,
 				URI targetURI) {
-			accept(new DefaultReferenceDescription(sourceURI, targetURI, eReference, index, null));
+			accept(new DefaultReferenceDescription(EcoreUtil2.getPathURI(source), targetURI, eReference, index, null));
 		}
 
 		protected void handleNoReferenceUpdater(URI sourceResourceURI, StatusWrapper status) {

org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/refactoring/ui/DefaultLinkedPositionGroupCalculator.java
@@ -31,6 +31,7 @@ import org.eclipse.jface.text.link.LinkedPositionGroup;
 import org.eclipse.ltk.core.refactoring.Change;
 import org.eclipse.text.edits.ReplaceEdit;
 import org.eclipse.text.edits.TextEdit;
+import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.findReferences.IReferenceFinder;
 import org.eclipse.xtext.resource.IGlobalServiceProvider;
 import org.eclipse.xtext.resource.IReferenceDescription;
@@ -158,7 +159,7 @@ public class DefaultLinkedPositionGroupCalculator implements ILinkedPositionGrou
 			@Override
 			public void accept(EObject source, URI sourceURI, EReference eReference, int index, EObject targetOrProxy,
 					URI targetURI) {
-				referenceDescriptions.add(new DefaultReferenceDescription(sourceURI, targetURI, eReference, index, null));
+				referenceDescriptions.add(new DefaultReferenceDescription(EcoreUtil2.getPathURI(source), targetURI, eReference, index, null));
 			}
 		};
 		if (monitor.isCanceled()) {

eclipse/xtext

org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/refactoring/impl/ReferenceUpdaterDispatcher.java
@@ -18,6 +18,7 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.findReferences.IReferenceFinder;
 import org.eclipse.xtext.resource.IReferenceDescription;
 import org.eclipse.xtext.resource.IResourceDescriptions;
@@ -122,7 +123,7 @@ public class ReferenceUpdaterDispatcher {
 		@Override
 		public void accept(EObject source, URI sourceURI, EReference eReference, int index, EObject targetOrProxy,
 				URI targetURI) {
-			accept(new DefaultReferenceDescription(sourceURI, targetURI, eReference, index, null));
+			accept(new DefaultReferenceDescription(EcoreUtil2.getFragmentPathURI(source), targetURI, eReference, index, null));
 		}
 
 		protected void handleNoReferenceUpdater(URI sourceResourceURI, StatusWrapper status) {

org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/refactoring/ui/DefaultLinkedPositionGroupCalculator.java
@@ -31,6 +31,7 @@ import org.eclipse.jface.text.link.LinkedPositionGroup;
 import org.eclipse.ltk.core.refactoring.Change;
 import org.eclipse.text.edits.ReplaceEdit;
 import org.eclipse.text.edits.TextEdit;
+import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.findReferences.IReferenceFinder;
 import org.eclipse.xtext.resource.IGlobalServiceProvider;
 import org.eclipse.xtext.resource.IReferenceDescription;
@@ -158,7 +159,7 @@ public class DefaultLinkedPositionGroupCalculator implements ILinkedPositionGrou
 			@Override
 			public void accept(EObject source, URI sourceURI, EReference eReference, int index, EObject targetOrProxy,
 					URI targetURI) {
-				referenceDescriptions.add(new DefaultReferenceDescription(sourceURI, targetURI, eReference, index, null));
+				referenceDescriptions.add(new DefaultReferenceDescription(EcoreUtil2.getFragmentPathURI(source), targetURI, eReference, index, null));
 			}
 		};
 		if (monitor.isCanceled()) {