a57d43d08387123b30ff125d1c81c08046654386
Sebastian Zarnekow <sebastian.zarnekow@gmail.com>
2017-12-07 15:58:20 +0100

    Avoid duplicated code in CA infrastructure

--- org.eclipse.xtext.ui/deprecated/org/eclipse/xtext/ui/editor/contentassist/antlr/internal/DFA.java
+++ org.eclipse.xtext.ui/deprecated/org/eclipse/xtext/ui/editor/contentassist/antlr/internal/DFA.java
@@ -7,51 +7,8 @@
  *******************************************************************************/
 package org.eclipse.xtext.ui.editor.contentassist.antlr.internal;
 
-import org.antlr.runtime.IntStream;
-import org.antlr.runtime.NoViableAltException;
-import org.antlr.runtime.RecognitionException;
-import org.antlr.runtime.Token;
-import org.eclipse.xtext.parser.antlr.IUnorderedGroupHelper;
-import org.eclipse.xtext.parser.antlr.XtextTokenStream;
-
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
  */
-public class DFA extends org.antlr.runtime.DFA {
-	
-	@Override
-	public int predict(IntStream input) throws RecognitionException {
-		try {
-			getRecognizer().beginDFAPrediction();
-			return super.predict(input);
-		} finally {
-			getRecognizer().endDFAPrediction();
-		}
-	}
-	
-	protected AbstractInternalContentAssistParser getRecognizer() {
-		return (AbstractInternalContentAssistParser) recognizer;
-	}
-	
-	/**
-	 * @since 2.4
-	 */
-	protected IUnorderedGroupHelper getUnorderedGroupHelper() {
-		return getRecognizer().getUnorderedGroupHelper();
-	}
-	
-	/**
-	 * @since 2.1
-	 */
-	@Override
-	protected void error(NoViableAltException nvae) {
-		if (nvae.token == Token.EOF_TOKEN) {
-			int lookAheadAddOn = getRecognizer().lookAheadAddOn;
-			int lookAhead = ((XtextTokenStream)nvae.input).getCurrentLookAhead();
-			if ((lookAhead >= lookAheadAddOn && lookAheadAddOn > 0) || (lookAhead == 0 && lookAheadAddOn > 0) || lookAhead == -1)
-				getRecognizer().failedPredicateAtEOF = true;
-		}
-		super.error(nvae);
-	}
-
+public class DFA extends org.eclipse.xtext.ide.editor.contentassist.antlr.internal.DFA {
 }
