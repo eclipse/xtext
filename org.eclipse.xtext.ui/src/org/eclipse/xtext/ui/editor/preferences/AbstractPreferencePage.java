b45a62e6e98cd3ece4d8d64ae8a939e1fadd9723
Karsten Thoms <karsten.thoms@itemis.de>
2018-01-16 13:33:45 +0100

    [eclipse/xtext#1160] Update copyright date range
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/preferences/AbstractPreferencePage.java
+++ org.eclipse.xtext.ui/src/org/eclipse/xtext/ui/editor/preferences/AbstractPreferencePage.java
@@ -1,7 +1,7 @@
 package org.eclipse.xtext.ui.editor.preferences;
 
 /*******************************************************************************
- * Copyright (c) 2008, 2012 itemis AG (http://www.itemis.eu) and others.
+ * Copyright (c) 2008, 2017 itemis AG (http://www.itemis.eu) and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
--- org.eclipse.xtext.xbase.junit/src/org/eclipse/xtext/xbase/compiler/OnTheFlyJavaCompiler.java
+++ org.eclipse.xtext.xbase.junit/src/org/eclipse/xtext/xbase/compiler/OnTheFlyJavaCompiler.java
@@ -1,7 +1,7 @@
 package org.eclipse.xtext.xbase.compiler;
 
 /*******************************************************************************
- * Copyright (c) 2011 itemis AG (http://www.itemis.eu) and others.
+ * Copyright (c) 2011, 2017 itemis AG (http://www.itemis.eu) and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
