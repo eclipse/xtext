eclipse/xtext-maven

made auto fill of platform resource map skippable introduced manual mapping for platform resource map added test
Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

org.eclipse.xtext.maven.plugin/src/main/java/org/eclipse/xtext/maven/ProjectMapping.java
@@ -1,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2018 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.xtext.maven;
+
+/**
+ * 
+ * @author Christian Dietrich - Initial contribution and API
+ *
+ */
+public class ProjectMapping {
+
+	/**
+	 * @property
+	 * @required
+	 */
+	private String projectName;
+
+	/**
+	 * @property
+	 * @required
+	 */
+	private String path;
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+}

eclipse/xtext

org.eclipse.xtext.maven.plugin/src/main/java/org/eclipse/xtext/maven/ProjectMapping.java
@@ -1,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2018 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.xtext.maven;
+
+import java.io.File;
+
+/**
+ * 
+ * @author Christian Dietrich - Initial contribution and API
+ *
+ */
+public class ProjectMapping {
+
+	/**
+	 * @property
+	 * @required
+	 */
+	private String projectName;
+
+	/**
+	 * @property
+	 * @required
+	 */
+	private File path;
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public File getPath() {
+		return path;
+	}
+
+	public void setPath(File path) {
+		this.path = path;
+	}
+
+}