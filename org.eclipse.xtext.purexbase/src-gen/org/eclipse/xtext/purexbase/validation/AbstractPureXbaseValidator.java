80c6ea494185562387e5a2a882501fa34f7e4e5f
Christian Dietrich <christian.dietrich@itemis.de>
2018-08-24 11:36:46 +0200

    [eclipse/xtext#1227] regen purexbase
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtext.purexbase/src-gen/org/eclipse/xtext/purexbase/validation/AbstractPureXbaseValidator.java
+++ org.eclipse.xtext.purexbase/src-gen/org/eclipse/xtext/purexbase/validation/AbstractPureXbaseValidator.java
@@ -19,5 +19,4 @@ public abstract class AbstractPureXbaseValidator extends XbaseValidator {
 		result.add(EPackage.Registry.INSTANCE.getEPackage("http://www.eclipse.org/xtext/xbase/Xtype"));
 		return result;
 	}
-	
 }
