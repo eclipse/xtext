40333a72574192ee5fc5f78edaa1e2acb74c36b1
Arne Deutsch <Arne.Deutsch@itemis.de>
2018-08-29 10:48:58 +0200

    [eclipse/xtext-eclipse#809] Cache task marker pattern for performance
    Signed-off-by: Arne Deutsch <Arne.Deutsch@itemis.de>

--- org.eclipse.xtext/xtend-gen/org/eclipse/xtext/tasks/TaskTags.java
+++ org.eclipse.xtext/xtend-gen/org/eclipse/xtext/tasks/TaskTags.java
@@ -9,9 +9,15 @@ package org.eclipse.xtext.tasks;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Pattern;
+import org.eclipse.xtend.lib.annotations.AccessorType;
 import org.eclipse.xtend.lib.annotations.Accessors;
+import org.eclipse.xtend2.lib.StringConcatenation;
 import org.eclipse.xtext.tasks.TaskTag;
 import org.eclipse.xtext.xbase.lib.CollectionLiterals;
+import org.eclipse.xtext.xbase.lib.Functions.Function1;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
+import org.eclipse.xtext.xbase.lib.ListExtensions;
 import org.eclipse.xtext.xbase.lib.Pure;
 
 /**
@@ -25,9 +31,31 @@ public class TaskTags implements Iterable<TaskTag> {
   
   private final List<TaskTag> taskTags = CollectionLiterals.<TaskTag>newArrayList();
   
+  @Accessors(AccessorType.NONE)
+  private Pattern pattern;
+  
   @Override
   public Iterator<TaskTag> iterator() {
     return this.taskTags.iterator();
+  }
+  
+  public Pattern toPattern() {
+    if ((this.pattern == null)) {
+      int flags = Pattern.MULTILINE;
+      if ((!this.caseSensitive)) {
+        flags = ((flags | Pattern.CASE_INSENSITIVE) | Pattern.UNICODE_CASE);
+      }
+      StringConcatenation _builder = new StringConcatenation();
+      _builder.append("^.*((");
+      final Function1<TaskTag, String> _function = (TaskTag it) -> {
+        return Pattern.quote(it.getName());
+      };
+      String _join = IterableExtensions.join(ListExtensions.<TaskTag, String>map(this.taskTags, _function), "|");
+      _builder.append(_join);
+      _builder.append(")(.*)?)$");
+      this.pattern = Pattern.compile(_builder.toString(), flags);
+    }
+    return this.pattern;
   }
   
   @Pure
