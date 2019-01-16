0a9b92aa18db9f07debb20f502970d668d037559
Holger Schill <Holger.Schill@itemis.de>
2019-01-16 14:40:02 +0100

    [#677] remove missing blank lines in example class

--- org.eclipse.xtend.ide.tests.data/src/org/eclipse/xtend/ide/tests/data/contentassist/StaticClassExample.java
+++ org.eclipse.xtend.ide.tests.data/src/org/eclipse/xtend/ide/tests/data/contentassist/StaticClassExample.java
@@ -1,15 +1,8 @@
 package org.eclipse.xtend.ide.tests.data.contentassist;
 
 public class StaticClassExample {
-	
-	
-	public static void staticMethod(String a) {
-		
-	}
-	
-	public static void staticMethodWithoutParameter() {
-		
-	}
+	public static void staticMethod(String a) {}
+	public static void staticMethodWithoutParameter() {}
 	public static String STATICFIELD = "";
 
 }
