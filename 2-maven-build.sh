88235162f082594809fec52f8b99c4b2ac03c9f0
Karsten Thoms <karsten.thoms@itemis.de>
2019-01-21 16:53:42 +0100

    [eclipse/xtext#1309] Provide Jenkinsfile for Eclipse CBI
    - externalized build steps into shell scripts for convenient local usage
    - added Slack notifications
    - configure Maven default goal
    
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

---
+++ 2-maven-build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+if [ -z "$JENKINS_URL" ]; then
+  # if not set in environment use default
+  JENKINS_URL=https://ci.eclipse.org/xtext/
+fi
+
+mvn \
+  -f releng \
+  --batch-mode \
+  --update-snapshots \
+  -Dmaven.repo.local=.m2/repository \
+  -DJENKINS_URL=$JENKINS_URL \
+  -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
+  $@
+
