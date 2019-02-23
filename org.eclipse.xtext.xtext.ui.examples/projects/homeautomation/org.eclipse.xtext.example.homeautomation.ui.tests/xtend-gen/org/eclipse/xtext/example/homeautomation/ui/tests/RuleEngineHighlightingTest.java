9c63ca2ed24050d4fa9fba431f7442f0e903925a
Tamas Miklossy <miklossy@itemis.de>
2019-02-23 13:49:46 +0100

    [#980] Ensure the AbstractHighlightingTest works properly.
    - Modify the AbstractHighlightingTest to properly wait for the semantic
    highlighting to be completed before asserting the result.
    - Modify the AbstractHighlightingTest to be more extensible by clients.
    - Add corresponding DomainmodelHighlightingTest,
    RuleEngineHighlightingTest, StatemachineHighlightingTest test cases.
    
    Signed-off-by: Tamas Miklossy <miklossy@itemis.de>

--- org.eclipse.xtext.xtext.ui.examples/projects/homeautomation/org.eclipse.xtext.example.homeautomation.ui.tests/xtend-gen/org/eclipse/xtext/example/homeautomation/ui/tests/RuleEngineHighlightingTest.java
+++ org.eclipse.xtext.xtext.ui.examples/projects/homeautomation/org.eclipse.xtext.example.homeautomation.ui.tests/xtend-gen/org/eclipse/xtext/example/homeautomation/ui/tests/RuleEngineHighlightingTest.java
@@ -5,9 +5,12 @@ import org.eclipse.xtend2.lib.StringConcatenation;
 import org.eclipse.xtext.example.homeautomation.ui.tests.RuleEngineUiInjectorProvider;
 import org.eclipse.xtext.testing.InjectWith;
 import org.eclipse.xtext.testing.XtextRunner;
-import org.eclipse.xtext.ui.editor.syntaxcoloring.DefaultHighlightingConfiguration;
 import org.eclipse.xtext.ui.testing.AbstractHighlightingTest;
+import org.eclipse.xtext.ui.testing.util.JavaProjectSetupUtil;
+import org.eclipse.xtext.xbase.lib.Exceptions;
 import org.eclipse.xtext.xbase.lib.Extension;
+import org.eclipse.xtext.xbase.ui.highlighting.XbaseHighlightingConfiguration;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -20,14 +23,23 @@ import org.junit.runner.RunWith;
 public class RuleEngineHighlightingTest extends AbstractHighlightingTest {
   @Inject
   @Extension
-  private DefaultHighlightingConfiguration _defaultHighlightingConfiguration;
+  private XbaseHighlightingConfiguration _xbaseHighlightingConfiguration;
+  
+  @Before
+  public void setup() {
+    try {
+      JavaProjectSetupUtil.createJavaProject(this.getProjectName());
+    } catch (Throwable _e) {
+      throw Exceptions.sneakyThrow(_e);
+    }
+  }
   
   @Test
   public void device_keyword() {
     StringConcatenation _builder = new StringConcatenation();
     _builder.append("Device Window can be open, closed");
     _builder.newLine();
-    this.testHighlighting(_builder, "Device", this._defaultHighlightingConfiguration.keywordTextStyle());
+    this.testHighlighting(_builder, "Device", this._xbaseHighlightingConfiguration.keywordTextStyle());
   }
   
   @Test
@@ -35,7 +47,7 @@ public class RuleEngineHighlightingTest extends AbstractHighlightingTest {
     StringConcatenation _builder = new StringConcatenation();
     _builder.append("Device Window can be open, closed");
     _builder.newLine();
-    this.testHighlighting(_builder, "can", this._defaultHighlightingConfiguration.keywordTextStyle());
+    this.testHighlighting(_builder, "can", this._xbaseHighlightingConfiguration.keywordTextStyle());
   }
   
   @Test
@@ -43,7 +55,7 @@ public class RuleEngineHighlightingTest extends AbstractHighlightingTest {
     StringConcatenation _builder = new StringConcatenation();
     _builder.append("Device Window can be open, closed");
     _builder.newLine();
-    this.testHighlighting(_builder, "be", this._defaultHighlightingConfiguration.keywordTextStyle());
+    this.testHighlighting(_builder, "be", this._xbaseHighlightingConfiguration.keywordTextStyle());
   }
   
   @Test
@@ -59,7 +71,7 @@ public class RuleEngineHighlightingTest extends AbstractHighlightingTest {
     _builder.append("\t");
     _builder.append("fire(Heater.off)");
     _builder.newLine();
-    this.testHighlighting(_builder, "Rule", this._defaultHighlightingConfiguration.keywordTextStyle());
+    this.testHighlighting(_builder, "Rule", this._xbaseHighlightingConfiguration.keywordTextStyle());
   }
   
   @Test
@@ -75,7 +87,7 @@ public class RuleEngineHighlightingTest extends AbstractHighlightingTest {
     _builder.append("\t");
     _builder.append("fire(Heater.off)");
     _builder.newLine();
-    this.testHighlighting(_builder, "when", this._defaultHighlightingConfiguration.keywordTextStyle());
+    this.testHighlighting(_builder, "when", this._xbaseHighlightingConfiguration.keywordTextStyle());
   }
   
   @Test
@@ -91,7 +103,7 @@ public class RuleEngineHighlightingTest extends AbstractHighlightingTest {
     _builder.append("\t");
     _builder.append("fire(Heater.off)");
     _builder.newLine();
-    this.testHighlighting(_builder, "then", this._defaultHighlightingConfiguration.keywordTextStyle());
+    this.testHighlighting(_builder, "then", this._xbaseHighlightingConfiguration.keywordTextStyle());
   }
   
   @Test
@@ -122,7 +134,7 @@ public class RuleEngineHighlightingTest extends AbstractHighlightingTest {
     _builder.append("\t\t\t");
     _builder.append("fire(Heater.on)");
     _builder.newLine();
-    this.testHighlighting(_builder, "switch", this._defaultHighlightingConfiguration.keywordTextStyle());
+    this.testHighlighting(_builder, "switch", this._xbaseHighlightingConfiguration.keywordTextStyle());
   }
   
   @Test
@@ -153,7 +165,7 @@ public class RuleEngineHighlightingTest extends AbstractHighlightingTest {
     _builder.append("\t\t\t");
     _builder.append("fire(Heater.on)");
     _builder.newLine();
-    this.testHighlighting(_builder, "case", this._defaultHighlightingConfiguration.keywordTextStyle());
+    this.testHighlighting(_builder, "case", this._xbaseHighlightingConfiguration.keywordTextStyle());
   }
   
   @Test
@@ -184,7 +196,7 @@ public class RuleEngineHighlightingTest extends AbstractHighlightingTest {
     _builder.append("\t\t\t");
     _builder.append("fire(Heater.on)");
     _builder.newLine();
-    this.testHighlighting(_builder, "default", this._defaultHighlightingConfiguration.keywordTextStyle());
+    this.testHighlighting(_builder, "default", this._xbaseHighlightingConfiguration.keywordTextStyle());
   }
   
   @Test
@@ -215,7 +227,7 @@ public class RuleEngineHighlightingTest extends AbstractHighlightingTest {
     _builder.append("\t\t\t");
     _builder.append("fire(Heater.on)");
     _builder.newLine();
-    this.testHighlighting(_builder, "1", this._defaultHighlightingConfiguration.numberTextStyle());
+    this.testHighlighting(_builder, "1", this._xbaseHighlightingConfiguration.numberTextStyle());
   }
   
   @Test
@@ -231,7 +243,7 @@ public class RuleEngineHighlightingTest extends AbstractHighlightingTest {
     _builder.append("\t");
     _builder.append("fire(Heater.off)");
     _builder.newLine();
-    this.testHighlighting(_builder, ".", this._defaultHighlightingConfiguration.punctuationTextStyle());
+    this.testHighlighting(_builder, ".", this._xbaseHighlightingConfiguration.punctuationTextStyle());
   }
   
   @Test
@@ -247,7 +259,7 @@ public class RuleEngineHighlightingTest extends AbstractHighlightingTest {
     _builder.append("\t");
     _builder.append("fire(Heater.off)");
     _builder.newLine();
-    this.testHighlighting(_builder, "rule1", this._defaultHighlightingConfiguration.stringTextStyle());
+    this.testHighlighting(_builder, "rule1", this._xbaseHighlightingConfiguration.stringTextStyle());
   }
   
   @Test
@@ -263,7 +275,39 @@ public class RuleEngineHighlightingTest extends AbstractHighlightingTest {
     _builder.append("\t");
     _builder.append("fire(Heater.off)");
     _builder.newLine();
-    this.testHighlighting(_builder, "rule1", this._defaultHighlightingConfiguration.stringTextStyle());
+    this.testHighlighting(_builder, "rule1", this._xbaseHighlightingConfiguration.stringTextStyle());
+  }
+  
+  @Test
+  public void fire_method_invocation() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("Device Window can be open, closed");
+    _builder.newLine();
+    _builder.append("Device Heater can be on, off, error");
+    _builder.newLine();
+    _builder.newLine();
+    _builder.append("Rule \"rule1\" when Window.open then");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("fire(Heater.off)");
+    _builder.newLine();
+    this.testHighlighting(_builder, "fire", this._xbaseHighlightingConfiguration.staticMethodInvocation());
+  }
+  
+  @Test
+  public void device_state_access() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("Device Window can be open, closed");
+    _builder.newLine();
+    _builder.append("Device Heater can be on, off, error");
+    _builder.newLine();
+    _builder.newLine();
+    _builder.append("Rule \"rule1\" when Window.open then");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("fire(Heater.off)");
+    _builder.newLine();
+    this.testHighlighting(_builder, "off", this._xbaseHighlightingConfiguration.staticField());
   }
   
   @Test
@@ -282,7 +326,7 @@ public class RuleEngineHighlightingTest extends AbstractHighlightingTest {
     _builder.append("\t");
     _builder.append("fire(Heater.off)");
     _builder.newLine();
-    this.testHighlighting(_builder, "A language for home automation systems", this._defaultHighlightingConfiguration.commentTextStyle());
+    this.testHighlighting(_builder, "A language for home automation systems", this._xbaseHighlightingConfiguration.commentTextStyle());
   }
   
   @Test
@@ -322,6 +366,59 @@ public class RuleEngineHighlightingTest extends AbstractHighlightingTest {
     _builder_1.append(" ");
     _builder_1.append("*/");
     _builder_1.newLine();
-    this.testHighlighting(_builder, _builder_1.toString(), this._defaultHighlightingConfiguration.commentTextStyle());
+    this.testHighlighting(_builder, _builder_1.toString(), this._xbaseHighlightingConfiguration.commentTextStyle());
+  }
+  
+  @Test
+  public void fixme_task_in_comment() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("/**");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("* FIXME");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("*/");
+    _builder.newLine();
+    _builder.append("Device Window can be open, closed");
+    _builder.newLine();
+    this.testHighlighting(_builder, "FIXME", this._xbaseHighlightingConfiguration.taskTextStyle());
+  }
+  
+  @Test
+  public void todo_task_in_comment() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("/**");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("* TODO");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("*/");
+    _builder.newLine();
+    _builder.append("Device Window can be open, closed");
+    _builder.newLine();
+    this.testHighlighting(_builder, "TODO", this._xbaseHighlightingConfiguration.taskTextStyle());
+  }
+  
+  @Test
+  public void xxx_task_in_comment() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("/**");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("* XXX");
+    _builder.newLine();
+    _builder.append(" ");
+    _builder.append("*/");
+    _builder.newLine();
+    _builder.append("Device Window can be open, closed");
+    _builder.newLine();
+    this.testHighlighting(_builder, "XXX", this._xbaseHighlightingConfiguration.taskTextStyle());
+  }
+  
+  @Override
+  protected int getStartPosition(final String content, final String text) {
+    return content.lastIndexOf(text);
   }
 }
