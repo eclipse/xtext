1462f1d9c86e03bb987e68ee05cce83aa7df55ab
Sebastian Zarnekow <sebastian.zarnekow@gmail.com>
2019-02-27 13:14:04 +0100

    Revert changes to XtendIDEInjectorProvider

--- org.eclipse.xtend.ide.tests/src/org/eclipse/xtend/ide/tests/XtendIDEInjectorProvider.xtend
+++ org.eclipse.xtend.ide.tests/src/org/eclipse/xtend/ide/tests/XtendIDEInjectorProvider.xtend
@@ -1,33 +1,12 @@
 package org.eclipse.xtend.ide.tests
 
 import org.eclipse.xtend.ide.internal.XtendActivator
-import org.eclipse.xtext.testing.GlobalRegistries
-import org.eclipse.xtext.testing.GlobalRegistries.GlobalStateMemento
 import org.eclipse.xtext.testing.IInjectorProvider
-import org.eclipse.xtext.testing.IRegistryConfigurator
 
-class XtendIDEInjectorProvider implements IInjectorProvider, IRegistryConfigurator {
-
-	protected GlobalStateMemento stateAfterInjectorCreation
+class XtendIDEInjectorProvider implements IInjectorProvider {
 
 	override getInjector() {
-		val result = XtendActivator::instance.getInjector(XtendActivator::ORG_ECLIPSE_XTEND_CORE_XTEND)
-		if (stateAfterInjectorCreation === null) {
-			stateAfterInjectorCreation = GlobalRegistries.makeCopyOfGlobalState();
-		}
-		return result
+		XtendActivator::instance.getInjector(XtendActivator::ORG_ECLIPSE_XTEND_CORE_XTEND)
 	}
-
-	override void restoreRegistry() {
-		stateAfterInjectorCreation.restoreGlobalState();
-	}
-
-	override void setupRegistry() {
-		if (stateAfterInjectorCreation === null) {
-			getInjector()
-		} else {
-			stateAfterInjectorCreation.restoreGlobalState()
-		}
-	}
-
+	
 }
--- org.eclipse.xtend.ide.tests/xtend-gen/org/eclipse/xtend/ide/tests/XtendIDEInjectorProvider.java
+++ org.eclipse.xtend.ide.tests/xtend-gen/org/eclipse/xtend/ide/tests/XtendIDEInjectorProvider.java
@@ -2,34 +2,12 @@ package org.eclipse.xtend.ide.tests;
 
 import com.google.inject.Injector;
 import org.eclipse.xtend.ide.internal.XtendActivator;
-import org.eclipse.xtext.testing.GlobalRegistries;
 import org.eclipse.xtext.testing.IInjectorProvider;
-import org.eclipse.xtext.testing.IRegistryConfigurator;
 
 @SuppressWarnings("all")
-public class XtendIDEInjectorProvider implements IInjectorProvider, IRegistryConfigurator {
-  protected GlobalRegistries.GlobalStateMemento stateAfterInjectorCreation;
-  
+public class XtendIDEInjectorProvider implements IInjectorProvider {
   @Override
   public Injector getInjector() {
-    final Injector result = XtendActivator.getInstance().getInjector(XtendActivator.ORG_ECLIPSE_XTEND_CORE_XTEND);
-    if ((this.stateAfterInjectorCreation == null)) {
-      this.stateAfterInjectorCreation = GlobalRegistries.makeCopyOfGlobalState();
-    }
-    return result;
-  }
-  
-  @Override
-  public void restoreRegistry() {
-    this.stateAfterInjectorCreation.restoreGlobalState();
-  }
-  
-  @Override
-  public void setupRegistry() {
-    if ((this.stateAfterInjectorCreation == null)) {
-      this.getInjector();
-    } else {
-      this.stateAfterInjectorCreation.restoreGlobalState();
-    }
+    return XtendActivator.getInstance().getInjector(XtendActivator.ORG_ECLIPSE_XTEND_CORE_XTEND);
   }
 }
