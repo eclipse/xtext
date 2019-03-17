c44068d01b5c3cd6f503c35cd235a16c438d66b1
Christian Dietrich <christian.dietrich@itemis.de>
2019-03-17 16:28:58 +0100

    [eclipse/xtext#1412] build against 2019-03 release
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtext.xtext.ui.examples/projects/domainmodel/org.eclipse.xtext.example.domainmodel/xtend-gen/org/eclipse/xtext/example/domainmodel/serializer/DomainmodelSyntacticSequencer.java
+++ org.eclipse.xtext.xtext.ui.examples/projects/domainmodel/org.eclipse.xtext.example.domainmodel/xtend-gen/org/eclipse/xtext/example/domainmodel/serializer/DomainmodelSyntacticSequencer.java
@@ -3,8 +3,18 @@
  */
 package org.eclipse.xtext.example.domainmodel.serializer;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.RuleCall;
 import org.eclipse.xtext.example.domainmodel.serializer.AbstractDomainmodelSyntacticSequencer;
+import org.eclipse.xtext.nodemodel.INode;
 
 @SuppressWarnings("all")
 public class DomainmodelSyntacticSequencer extends AbstractDomainmodelSyntacticSequencer {
+  @Override
+  protected String getArrayBracketsToken(final EObject semanticObject, final RuleCall ruleCall, final INode node) {
+    if ((node != null)) {
+      return this.getTokenText(node);
+    }
+    return "[]";
+  }
 }
