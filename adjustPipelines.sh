9a8ee6705f142cbb59cef46dfd0f3f2e74e3fc97
Karsten Thoms <karsten.thoms@itemis.de>
2019-04-02 15:16:19 +0200

    [eclipse/xtext#1424] Change default Jenkins
    Use https://ci.eclipse.org/xtext as default Jenkins URL
    
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- adjustPipelines.sh
+++ adjustPipelines.sh
@@ -41,7 +41,7 @@ sed_inplace() {
 }
 
 if [[ -z "${JENKINS_URL}" ]]; then
-	JENKINS_URL="https://services.typefox.io/open-source/jenkins"
+	JENKINS_URL="https://ci.eclipse.org/xtext"
 fi
 echo "JENKINS_URL=$JENKINS_URL"
 
