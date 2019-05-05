373f71e46371a7bbcfc6aac166d6e5cbde45afdd
Christian Dietrich <christian.dietrich@itemis.de>
2019-05-05 11:25:01 +0200

    [eclipse/xtext#1427] disable p2 mirrors
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- 2-maven-build.sh
+++ 2-maven-build.sh
@@ -9,6 +9,7 @@ mvn \
   --batch-mode \
   --update-snapshots \
   -Dmaven.repo.local=.m2/repository \
+  -Dtycho.disableP2Mirrors=true \
   -DJENKINS_URL=$JENKINS_URL \
   -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
   $@
