e18d7fc35d2ebdbc8dcc3d057e0c3d3047916dc5
Christian Dietrich <christian.dietrich@itemis.de>
2019-05-16 14:43:27 +0200

    update to guava 27.1 Fixes eclipse/xtext#1398 and eclipse/xtext-lib#111
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtext.common.types.tests/tests/org/eclipse/xtext/common/types/access/impl/AbstractTypeProviderTest.java
+++ org.eclipse.xtext.common.types.tests/tests/org/eclipse/xtext/common/types/access/impl/AbstractTypeProviderTest.java
@@ -3544,10 +3544,7 @@ public abstract class AbstractTypeProviderTest extends Assert {
 				.getOnlyElement(type.findAllFeaturesByName("containsValue"));
 		assertNotNull(containsValue);
 		JvmFormalParameter firstParam = containsValue.getParameters().get(0);
-		assertEquals(1, firstParam.getAnnotations().size());
-		JvmAnnotationReference annotationReference = firstParam.getAnnotations().get(0);
-		JvmAnnotationType annotationType = annotationReference.getAnnotation();
-		assertEquals("java:/Objects/javax.annotation.Nullable", EcoreUtil.getURI(annotationType).trimFragment().toString());
+		assertEquals(0, firstParam.getAnnotations().size());
 	}
 
 	@Test
