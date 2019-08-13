0c96ad3c7107a571e0f9a5e1ccc3931f972b8bdf
Christian Dietrich <christian.dietrich@itemis.de>
2019-08-13 09:26:09 +0200

    [eclipse/xtext#1530] adapted wizard to new junit 5 version in orbit
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtext.xtext.wizard/xtend-gen/org/eclipse/xtext/xtext/wizard/TargetPlatformProject.java
+++ org.eclipse.xtext.xtext.wizard/xtend-gen/org/eclipse/xtext/xtext/wizard/TargetPlatformProject.java
@@ -194,25 +194,25 @@ public class TargetPlatformProject extends ProjectDescriptor {
       boolean _equals = Objects.equal(_junitVersion, JUnitVersion.JUNIT_5);
       if (_equals) {
         _builder.append("\t\t\t");
-        _builder.append("<unit id=\"org.junit.jupiter.api\" version=\"5.4.0.v20190212-2109\"/>");
+        _builder.append("<unit id=\"org.junit.jupiter.api\" version=\"5.5.1.v20190812-1854\"/>");
         _builder.newLine();
         _builder.append("\t\t\t");
-        _builder.append("<unit id=\"org.junit.jupiter.engine\" version=\"5.4.0.v20190212-2109\"/>");
+        _builder.append("<unit id=\"org.junit.jupiter.engine\" version=\"5.5.1.v20190812-1854\"/>");
         _builder.newLine();
         _builder.append("\t\t\t");
-        _builder.append("<unit id=\"org.junit.platform.commons\" version=\"1.4.0.v20190212-2109\"/>");
+        _builder.append("<unit id=\"org.junit.platform.commons\" version=\"1.5.1.v20190812-1854\"/>");
         _builder.newLine();
         _builder.append("\t\t\t");
-        _builder.append("<unit id=\"org.junit.platform.engine\" version=\"1.4.0.v20190212-2109\"/>");
+        _builder.append("<unit id=\"org.junit.platform.engine\" version=\"1.5.1.v20190812-1854\"/>");
         _builder.newLine();
         _builder.append("\t\t\t");
-        _builder.append("<unit id=\"org.junit.platform.launcher\" version=\"1.4.0.v20190212-2109\"/>");
+        _builder.append("<unit id=\"org.junit.platform.launcher\" version=\"1.5.1.v20190812-1854\"/>");
         _builder.newLine();
         _builder.append("\t\t\t");
-        _builder.append("<unit id=\"org.junit.platform.runner\" version=\"1.4.0.v20190212-2109\"/>");
+        _builder.append("<unit id=\"org.junit.platform.runner\" version=\"1.5.1.v20190812-1854\"/>");
         _builder.newLine();
         _builder.append("\t\t\t");
-        _builder.append("<unit id=\"org.opentest4j\" version=\"1.1.1.v20190212-2109\"/>");
+        _builder.append("<unit id=\"org.opentest4j\" version=\"1.2.0.v20190812-1854\"/>");
         _builder.newLine();
       }
     }
