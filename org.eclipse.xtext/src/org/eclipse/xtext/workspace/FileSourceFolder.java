85db097d5634607a48f10202feb50d802fb436bd
Karsten Thoms <karsten.thoms@itemis.de>
2019-10-28 21:01:50 +0100

    [eclipse/xtext#1569] Refactor Xtend to Java
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

---
+++ org.eclipse.xtext/src/org/eclipse/xtext/workspace/FileSourceFolder.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2015, 2017 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.xtext.workspace;
+
+import org.eclipse.emf.common.util.URI;
+
+import com.google.common.base.Objects;
+
+public class FileSourceFolder implements ISourceFolder {
+	private final FileProjectConfig parent;
+
+	private final String name;
+
+	public FileSourceFolder(FileProjectConfig parent, String name) {
+		this.parent = parent;
+		this.name = name;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public URI getPath() {
+		final URI result = URI.createFileURI(name).resolve(parent.getPath());
+		if (result.hasTrailingPathSeparator()) {
+			return result;
+		} else {
+			return result.appendSegment("");
+		}
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (obj instanceof FileSourceFolder) {
+			URI path1 = this.getPath();
+			URI path2 = ((FileSourceFolder) obj).getPath();
+			return Objects.equal(path1, path2);
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return this.getPath().hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return name + " (" + getPath() + ")";
+	}
+
+}
