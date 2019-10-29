4eba252aa92391d9b1a820a00d2fa31639ae3ca3
Karsten Thoms <karsten.thoms@itemis.de>
2019-10-29 06:41:15 +0100

    [eclipse/xtext#1569] Refactor Xtend to Java
    The usage of Xtend in the Xtext code base should be reduced where the
    usage of Xtend is not super beneficial. Replaced Xtend classes by the
    compiled code and refactored the code to more readable Java.
    
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

---
+++ org.eclipse.xtext/src/org/eclipse/xtext/tasks/DefaultTaskTagProvider.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2014 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.xtext.tasks;
+
+import org.eclipse.emf.ecore.resource.Resource;
+
+/**
+ * @author Stefan Oehme - Initial contribution and API
+ * @since 2.6
+ */
+public class DefaultTaskTagProvider implements ITaskTagProvider {
+	@Override
+	public TaskTags getTaskTags(Resource resource) {
+		TaskTags taskTags = new TaskTags();
+		taskTags.setCaseSensitive(true);
+		taskTags.getTaskTags().add(toTaskTag("TODO", Priority.NORMAL));
+		taskTags.getTaskTags().add(toTaskTag("FIXME", Priority.HIGH));
+		taskTags.getTaskTags().add(toTaskTag("XXX", Priority.NORMAL));
+		return taskTags;
+	}
+
+	private TaskTag toTaskTag(String name, Priority priority) {
+		TaskTag tag = new TaskTag();
+		tag.setName(name);
+		tag.setPriority(priority);
+		return tag;
+	}
+}
