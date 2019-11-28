e1161dbe033cf2e6086da87b45a37f4e1eff9408
Karsten Thoms <karsten.thoms@itemis.de>
2019-11-28 11:57:17 +0100

    [#1304] Add test case for regression in getCurrent()
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- org.eclipse.xtext.tests/src/org/eclipse/xtext/util/XtextVersionTests.java
+++ org.eclipse.xtext.tests/src/org/eclipse/xtext/util/XtextVersionTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 itemis AG (http://www.itemis.eu) and others.
+ * Copyright (c) 2015, 2019 itemis AG (http://www.itemis.eu) and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,12 +9,15 @@ package org.eclipse.xtext.util;
 
 import static org.junit.Assert.*;
 
+import java.util.regex.Pattern;
+
 import org.junit.Test;
 
 /**
  * @author dhuebner - Initial contribution and API
  */
 public class XtextVersionTests {
+	private static Pattern VERSION_MATCHER = Pattern.compile("\\d\\.\\d+\\.\\d+(?:-SNAPSHOT)?");
 
 	@Test
 	public void testVersionKinds() {
@@ -46,5 +49,24 @@ public class XtextVersionTests {
 		assertFalse(version.isSnapshot());
 		assertFalse(version.isStable());
 		
+	}
+	
+	@Test
+	public void test_getCurrent() {
+		// just to show what the pattern matches
+		assertTrue(VERSION_MATCHER.matcher("2.20.0").matches());
+		assertTrue(VERSION_MATCHER.matcher("2.20.0-SNAPSHOT").matches());
+		
+		assertFalse(VERSION_MATCHER.matcher("2.20.0.qualifier").matches());
+		assertFalse(VERSION_MATCHER.matcher("2.20.0.vSomething").matches());
+		assertFalse(VERSION_MATCHER.matcher("2.20").matches());
+		
+		XtextVersion xtextVersion = XtextVersion.getCurrent();
+		assertNotNull(xtextVersion);
+		String version = xtextVersion.getVersion();
+		assertNotNull(version);
+		if (!"unknown".equals(version)) {
+			assertTrue(VERSION_MATCHER.matcher(version).matches());
+		}
 	}
 }
