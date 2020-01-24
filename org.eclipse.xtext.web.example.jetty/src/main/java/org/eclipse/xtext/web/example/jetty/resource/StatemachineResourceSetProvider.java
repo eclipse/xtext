85791fe8dc83d24adc9914b9f94f1dabf593419e
Christian Dietrich <christian.dietrich@itemis.de>
2020-01-24 13:36:21 +0100

    moved more code to java
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

---
+++ org.eclipse.xtext.web.example.jetty/src/main/java/org/eclipse/xtext/web/example/jetty/resource/StatemachineResourceSetProvider.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2015, 2020 itemis AG (http://www.itemis.eu) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.xtext.web.example.jetty.resource;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.xtext.xbase.lib.Pair;
+import org.eclipse.xtext.web.server.IServiceContext;
+import org.eclipse.xtext.web.server.model.IWebResourceSetProvider;
+
+/**
+ * Resources that run in multi-resource mode share the same resource set.
+ */
+public class StatemachineResourceSetProvider implements IWebResourceSetProvider {
+	
+	public static final String MULTI_RESOURCE_PREFIX = "multi-resource";
+		
+	@Inject 
+	private Provider<ResourceSet> provider;
+	
+	@Override
+	public ResourceSet get(String resourceId, IServiceContext serviceContext) {
+		if (resourceId != null && resourceId.startsWith(MULTI_RESOURCE_PREFIX)) {
+			int pathEnd = Math.max(resourceId.indexOf('/'), MULTI_RESOURCE_PREFIX.length());
+			return serviceContext.getSession().get(Pair.of(ResourceSet.class, resourceId.substring(0, pathEnd)), ()->provider.get());
+		} else
+			return provider.get();
+	}
+	
+}
