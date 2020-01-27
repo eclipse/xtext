c6b1db5d067ce02809bffa3399c41f44c044b2e4
Christian Dietrich <christian.dietrich@itemis.de>
2020-01-27 15:41:33 +0100

    [eclipse/xtext#1629] ported more Xtend code to Java
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtext.xbase.testing/src/org/eclipse/xtext/xbase/testing/typesystem/PublicStackedResolvedTypes.java
+++ org.eclipse.xtext.xbase.testing/src/org/eclipse/xtext/xbase/testing/typesystem/PublicStackedResolvedTypes.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012, 2017 itemis AG (http://www.itemis.eu) and others.
+ * Copyright (c) 2012, 2020 itemis AG (http://www.itemis.eu) and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,9 +13,8 @@ import org.eclipse.xtext.xbase.typesystem.internal.StackedResolvedTypes;
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
  */
-@SuppressWarnings("all")
 public class PublicStackedResolvedTypes extends StackedResolvedTypes {
-  public PublicStackedResolvedTypes(final ResolvedTypes parent) {
-    super(parent);
-  }
+	public PublicStackedResolvedTypes(ResolvedTypes parent) {
+		super(parent);
+	}
 }
