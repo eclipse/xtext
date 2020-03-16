24ab236af1e509465aeca367f34b1ba252e87b83
Christian Dietrich <christian.dietrich@itemis.de>
2020-03-16 13:25:18 +0100

    [eclipse/xtext#1685] use 2020-06 update site
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtext.xtext.wizard/xtend-gen/org/eclipse/xtext/xtext/wizard/TargetPlatformProject.java
+++ org.eclipse.xtext.xtext.wizard/xtend-gen/org/eclipse/xtext/xtext/wizard/TargetPlatformProject.java
@@ -116,7 +116,7 @@ public class TargetPlatformProject extends ProjectDescriptor {
     _builder.append("<unit id=\"org.eclipse.emf.sdk.feature.group\" version=\"0.0.0\"/>");
     _builder.newLine();
     _builder.append("\t\t\t");
-    _builder.append("<repository location=\"https://download.eclipse.org/releases/2020-03\"/>");
+    _builder.append("<repository location=\"https://download.eclipse.org/releases/2020-06\"/>");
     _builder.newLine();
     _builder.append("\t\t");
     _builder.append("</location>");
