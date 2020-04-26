beb804b96618eb7fc176776b80f95164c40970aa
Christian Dietrich <christian.dietrich@itemis.de>
2020-04-26 11:01:56 +0200

    fixed unused imports
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtext.ide/src/org/eclipse/xtext/ide/editor/syntaxcoloring/DefaultAntlrTokenToAttributeIdMapper.java
+++ org.eclipse.xtext.ide/src/org/eclipse/xtext/ide/editor/syntaxcoloring/DefaultAntlrTokenToAttributeIdMapper.java
@@ -11,7 +11,6 @@ package org.eclipse.xtext.ide.editor.syntaxcoloring;
 import com.google.inject.Singleton;
 import java.util.regex.Pattern;
 import org.eclipse.xtext.ide.editor.syntaxcoloring.AbstractAntlrTokenToAttributeIdMapper;
-import org.eclipse.xtext.ide.editor.syntaxcoloring.HighlightingStyles;
 
 /**
  * @author Anton Kosyakov
