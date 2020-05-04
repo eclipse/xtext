2aaf5f0772c9f845eac6755014b0a21f320647a8
Sebastian Zarnekow <Sebastian.Zarnekow@gmail.com>
2020-05-04 13:48:35 +0200

    [#1462] Regenerated test-languages (#233)
    see eclipse/xtext-core#1462

--- org.eclipse.xtext.web.example.entities/src-gen/org/eclipse/xtext/web/example/entities/parser/antlr/internal/InternalEntitiesParser.java
+++ org.eclipse.xtext.web.example.entities/src-gen/org/eclipse/xtext/web/example/entities/parser/antlr/internal/InternalEntitiesParser.java
@@ -6530,7 +6530,7 @@ public class InternalEntitiesParser extends AbstractInternalAntlrParser {
             	              										if (current==null) {
             	              											current = createModelElement(grammarAccess.getXMemberFeatureCallRule());
             	              										}
-            	              										setWithLastConsumed(current, "explicitStatic", true, "::");
+            	              										setWithLastConsumed(current, "explicitStatic", lv_explicitStatic_3_0 != null, "::");
             	              									
             	            }
 
@@ -6725,7 +6725,7 @@ public class InternalEntitiesParser extends AbstractInternalAntlrParser {
             	              										if (current==null) {
             	              											current = createModelElement(grammarAccess.getXMemberFeatureCallRule());
             	              										}
-            	              										setWithLastConsumed(current, "nullSafe", true, "?.");
+            	              										setWithLastConsumed(current, "nullSafe", lv_nullSafe_9_0 != null, "?.");
             	              									
             	            }
 
@@ -6757,7 +6757,7 @@ public class InternalEntitiesParser extends AbstractInternalAntlrParser {
             	              										if (current==null) {
             	              											current = createModelElement(grammarAccess.getXMemberFeatureCallRule());
             	              										}
-            	              										setWithLastConsumed(current, "explicitStatic", true, "::");
+            	              										setWithLastConsumed(current, "explicitStatic", lv_explicitStatic_10_0 != null, "::");
             	              									
             	            }
 
@@ -6965,7 +6965,7 @@ public class InternalEntitiesParser extends AbstractInternalAntlrParser {
             	              								if (current==null) {
             	              									current = createModelElement(grammarAccess.getXMemberFeatureCallRule());
             	              								}
-            	              								setWithLastConsumed(current, "explicitOperationCall", true, "(");
+            	              								setWithLastConsumed(current, "explicitOperationCall", lv_explicitOperationCall_17_0 != null, "(");
             	              							
             	            }
 
@@ -8828,7 +8828,7 @@ public class InternalEntitiesParser extends AbstractInternalAntlrParser {
                       							if (current==null) {
                       								current = createModelElement(grammarAccess.getXClosureRule());
                       							}
-                      							setWithLastConsumed(current, "explicitSyntax", true, "|");
+                      							setWithLastConsumed(current, "explicitSyntax", lv_explicitSyntax_5_0 != null, "|");
                       						
                     }
 
@@ -9306,7 +9306,7 @@ public class InternalEntitiesParser extends AbstractInternalAntlrParser {
               							if (current==null) {
               								current = createModelElement(grammarAccess.getXShortClosureRule());
               							}
-              							setWithLastConsumed(current, "explicitSyntax", true, "|");
+              							setWithLastConsumed(current, "explicitSyntax", lv_explicitSyntax_4_0 != null, "|");
               						
             }
 
@@ -10499,7 +10499,7 @@ public class InternalEntitiesParser extends AbstractInternalAntlrParser {
                       						if (current==null) {
                       							current = createModelElement(grammarAccess.getXCasePartRule());
                       						}
-                      						setWithLastConsumed(current, "fallThrough", true, ",");
+                      						setWithLastConsumed(current, "fallThrough", lv_fallThrough_6_0 != null, ",");
                       					
                     }
 
@@ -12075,7 +12075,7 @@ public class InternalEntitiesParser extends AbstractInternalAntlrParser {
                       						if (current==null) {
                       							current = createModelElement(grammarAccess.getXVariableDeclarationRule());
                       						}
-                      						setWithLastConsumed(current, "writeable", true, "var");
+                      						setWithLastConsumed(current, "writeable", lv_writeable_1_0 != null, "var");
                       					
                     }
 
@@ -12951,7 +12951,7 @@ public class InternalEntitiesParser extends AbstractInternalAntlrParser {
                       						if (current==null) {
                       							current = createModelElement(grammarAccess.getXFeatureCallRule());
                       						}
-                      						setWithLastConsumed(current, "explicitOperationCall", true, "(");
+                      						setWithLastConsumed(current, "explicitOperationCall", lv_explicitOperationCall_7_0 != null, "(");
                       					
                     }
 
@@ -13813,7 +13813,7 @@ public class InternalEntitiesParser extends AbstractInternalAntlrParser {
                       						if (current==null) {
                       							current = createModelElement(grammarAccess.getXConstructorCallRule());
                       						}
-                      						setWithLastConsumed(current, "explicitConstructorCall", true, "(");
+                      						setWithLastConsumed(current, "explicitConstructorCall", lv_explicitConstructorCall_8_0 != null, "(");
                       					
                     }
 
@@ -14181,7 +14181,7 @@ public class InternalEntitiesParser extends AbstractInternalAntlrParser {
                       						if (current==null) {
                       							current = createModelElement(grammarAccess.getXBooleanLiteralRule());
                       						}
-                      						setWithLastConsumed(current, "isTrue", true, "true");
+                      						setWithLastConsumed(current, "isTrue", lv_isTrue_2_0 != null, "true");
                       					
                     }
 
@@ -18735,7 +18735,7 @@ public class InternalEntitiesParser extends AbstractInternalAntlrParser {
                       							if (current==null) {
                       								current = createModelElement(grammarAccess.getXImportDeclarationRule());
                       							}
-                      							setWithLastConsumed(current, "static", true, "static");
+                      							setWithLastConsumed(current, "static", lv_static_1_0 != null, "static");
                       						
                     }
 
@@ -18769,7 +18769,7 @@ public class InternalEntitiesParser extends AbstractInternalAntlrParser {
                               							if (current==null) {
                               								current = createModelElement(grammarAccess.getXImportDeclarationRule());
                               							}
-                              							setWithLastConsumed(current, "extension", true, "extension");
+                              							setWithLastConsumed(current, "extension", lv_extension_2_0 != null, "extension");
                               						
                             }
 
@@ -18853,7 +18853,7 @@ public class InternalEntitiesParser extends AbstractInternalAntlrParser {
                               								if (current==null) {
                               									current = createModelElement(grammarAccess.getXImportDeclarationRule());
                               								}
-                              								setWithLastConsumed(current, "wildcard", true, "*");
+                              								setWithLastConsumed(current, "wildcard", lv_wildcard_4_0 != null, "*");
                               							
                             }
 
