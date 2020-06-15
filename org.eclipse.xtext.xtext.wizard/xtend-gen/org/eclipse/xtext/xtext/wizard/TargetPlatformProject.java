7c14947d460e78e3d0dbf691d7a60db65f86b9e0
Christian Dietrich <christian.dietrich@itemis.de>
2020-06-15 14:16:32 +0200

    [eclipse/xtext#1781] update orbit to 2020-09
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtext.xtext.wizard/xtend-gen/org/eclipse/xtext/xtext/wizard/TargetPlatformProject.java
+++ org.eclipse.xtext.xtext.wizard/xtend-gen/org/eclipse/xtext/xtext/wizard/TargetPlatformProject.java
@@ -216,7 +216,7 @@ public class TargetPlatformProject extends ProjectDescriptor {
     _builder.append("<unit id=\"io.github.classgraph\" version=\"4.8.35.v20190528-1517\"/>");
     _builder.newLine();
     _builder.append("\t\t\t");
-    _builder.append("<repository location=\"https://download.eclipse.org/tools/orbit/downloads/2020-06\"/>");
+    _builder.append("<repository location=\"https://download.eclipse.org/tools/orbit/downloads/2020-09\"/>");
     _builder.newLine();
     _builder.append("\t\t");
     _builder.append("</location>");
