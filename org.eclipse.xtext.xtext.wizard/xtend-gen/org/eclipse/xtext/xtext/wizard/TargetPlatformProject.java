246b86e30447356f51f4b84019a678866d99c08d
Christian Dietrich <christian.dietrich@itemis.de>
2020-10-26 12:00:17 +0100

    [eclipse/xtext#1855] update to junit 5.7
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtext.xtext.wizard/xtend-gen/org/eclipse/xtext/xtext/wizard/TargetPlatformProject.java
+++ org.eclipse.xtext.xtext.wizard/xtend-gen/org/eclipse/xtext/xtext/wizard/TargetPlatformProject.java
@@ -197,22 +197,22 @@ public class TargetPlatformProject extends ProjectDescriptor {
       boolean _equals = Objects.equal(_junitVersion, JUnitVersion.JUNIT_5);
       if (_equals) {
         _builder.append("\t\t\t");
-        _builder.append("<unit id=\"org.junit.jupiter.api\" version=\"5.6.0.v20200203-2009\"/>");
+        _builder.append("<unit id=\"org.junit.jupiter.api\" version=\"5.7.0.v20201026-1537\"/>");
         _builder.newLine();
         _builder.append("\t\t\t");
-        _builder.append("<unit id=\"org.junit.jupiter.engine\" version=\"5.6.0.v20200203-2009\"/>");
+        _builder.append("<unit id=\"org.junit.jupiter.engine\" version=\"5.7.0.v20201026-1537\"/>");
         _builder.newLine();
         _builder.append("\t\t\t");
-        _builder.append("<unit id=\"org.junit.platform.commons\" version=\"1.6.0.v20200203-2009\"/>");
+        _builder.append("<unit id=\"org.junit.platform.commons\" version=\"1.7.0.v20201026-1537\"/>");
         _builder.newLine();
         _builder.append("\t\t\t");
-        _builder.append("<unit id=\"org.junit.platform.engine\" version=\"1.6.0.v20200203-2009\"/>");
+        _builder.append("<unit id=\"org.junit.platform.engine\" version=\"1.7.0.v20201026-1537\"/>");
         _builder.newLine();
         _builder.append("\t\t\t");
-        _builder.append("<unit id=\"org.junit.platform.launcher\" version=\"1.6.0.v20200203-2009\"/>");
+        _builder.append("<unit id=\"org.junit.platform.launcher\" version=\"1.7.0.v20201026-1537\"/>");
         _builder.newLine();
         _builder.append("\t\t\t");
-        _builder.append("<unit id=\"org.junit.platform.runner\" version=\"1.6.0.v20200203-2009\"/>");
+        _builder.append("<unit id=\"org.junit.platform.runner\" version=\"1.7.0.v20201026-1537\"/>");
         _builder.newLine();
         _builder.append("\t\t\t");
         _builder.append("<unit id=\"org.opentest4j\" version=\"1.2.0.v20190826-0900\"/>");
