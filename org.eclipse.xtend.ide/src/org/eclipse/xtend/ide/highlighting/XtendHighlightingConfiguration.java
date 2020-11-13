ea55182ed14fb54820b3da877248480c3040d36e
Karsten Thoms <karsten.thoms@karakun.com>
2020-11-13 08:48:44 +0100

    [#1078] Register theming in XtendHighlightingConfiguration
    Avoid the need to initialize theming in generated activator.
    Preferences are only already set when switching to dark mode. When
    switching back to default this does not work somehow. As a workaround
    preferences are reset with the highlighting configuration.
    
    Added the default theme anyway. Might be useful later.
    
    Configured access rules as ThemeEngine and ITheme are internal API.
    
    Signed-off-by: Karsten Thoms <karsten.thoms@karakun.com>

--- org.eclipse.xtend.ide/src/org/eclipse/xtend/ide/highlighting/XtendHighlightingConfiguration.java
+++ org.eclipse.xtend.ide/src/org/eclipse/xtend/ide/highlighting/XtendHighlightingConfiguration.java
@@ -8,17 +8,36 @@
  *******************************************************************************/
 package org.eclipse.xtend.ide.highlighting;
 
+import java.util.Optional;
+
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.e4.core.services.events.IEventBroker;
+import org.eclipse.e4.ui.css.swt.theme.ITheme;
+import org.eclipse.e4.ui.css.swt.theme.IThemeEngine;
 import org.eclipse.swt.graphics.RGB;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.xtend.ide.common.highlighting.XtendHighlightingStyles;
+import org.eclipse.xtend.ide.internal.XtendActivator;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfigurationAcceptor;
 import org.eclipse.xtext.ui.editor.utils.TextStyle;
 import org.eclipse.xtext.xbase.ui.highlighting.XbaseHighlightingConfiguration;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventHandler;
 
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
  * @author Holger Schill
  */
 public class XtendHighlightingConfiguration extends XbaseHighlightingConfiguration implements XtendHighlightingStyles {
+
+	private static final String THEME_ID_DARK = "org.eclipse.e4.ui.css.theme.e4_dark";
+
+	public XtendHighlightingConfiguration () {
+		initThemeManaging();
+	}
+	
 
 	@Override
 	public void configure(IHighlightingConfigurationAcceptor acceptor) {
@@ -31,8 +50,10 @@ public class XtendHighlightingConfiguration extends XbaseHighlightingConfigurati
 		
 		acceptor.acceptDefaultHighlighting(SINGLE_LINE_COMMENT, "Single Line Comments", commentTextStyle());
 		acceptor.acceptDefaultHighlighting(MULTI_LINE_COMMENT, "Multi Line Comments", commentTextStyle());
+		
 	}
 	
+
 	public TextStyle richTextStyle() {
 		TextStyle textStyle = stringTextStyle().copy();
 		textStyle.setBackgroundColor(new RGB(220, 220, 220));
@@ -54,4 +75,33 @@ public class XtendHighlightingConfiguration extends XbaseHighlightingConfigurati
 		return textStyle;
 	}
 
+	private void initThemeManaging() {
+		if (PlatformUI.isWorkbenchRunning()) {
+			IWorkbench workbench = PlatformUI.getWorkbench();
+			IEventBroker eventBroker = workbench.getService(IEventBroker.class);
+			if (eventBroker != null) {
+				EventHandler eventHandler = evt -> initTheme(evt);
+				eventBroker.subscribe("org/eclipse/e4/ui/css/swt/theme/ThemeManager/themeChanged", eventHandler);
+				initTheme(null);
+			}
+		}
+	}
+
+	private void initTheme(Event evt) {
+		IThemeEngine themeEngine = PlatformUI.getWorkbench().getService(IThemeEngine.class);
+		ITheme theme = themeEngine.getActiveTheme();
+
+		IEclipsePreferences preferences = InstanceScope.INSTANCE.getNode(XtendActivator.ORG_ECLIPSE_XTEND_CORE_XTEND);
+		
+		if (THEME_ID_DARK.equals(theme.getId())) {
+			themeEngine.applyStyles(preferences, false);
+		} else {
+			configure((id,name,style) -> {
+				RGB color = Optional.ofNullable(style.getColor()).orElse(new RGB(0,0,0));
+				preferences.put(String.format("%s.syntaxColorer.tokenStyles.%s.color", XtendActivator.ORG_ECLIPSE_XTEND_CORE_XTEND, id), String.format("%d,%d,%d", color.red, color.green, color.blue));
+			}
+			);
+			themeEngine.applyStyles(preferences, false);
+		}
+	}
 }
