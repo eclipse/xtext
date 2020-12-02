e104c4331fe133f831e5646548bacffbbbafcdf7
Christian Dietrich <christian.dietrich@itemis.de>
2020-12-02 09:23:26 +0100

    [eclipse/xtext#1882][2021-03] Update Orbit
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtext.xtext.wizard/xtend-gen/org/eclipse/xtext/xtext/wizard/TargetPlatformProject.java
+++ org.eclipse.xtext.xtext.wizard/xtend-gen/org/eclipse/xtext/xtext/wizard/TargetPlatformProject.java
@@ -241,7 +241,7 @@ public class TargetPlatformProject extends ProjectDescriptor {
     _builder.append("<unit id=\"io.github.classgraph\" version=\"4.8.35.v20190528-1517\"/>");
     _builder.newLine();
     _builder.append("\t\t\t");
-    _builder.append("<repository location=\"https://download.eclipse.org/tools/orbit/downloads/2020-12\"/>");
+    _builder.append("<repository location=\"https://download.eclipse.org/tools/orbit/downloads/2021-03\"/>");
     _builder.newLine();
     _builder.append("\t\t");
     _builder.append("</location>");
