8ff246abc128887595e2fc8dfe9785d1c0518fc3
Christian Dietrich <christian.dietrich@itemis.de>
2021-02-04 14:02:26 +0100

    updated versions in wizard
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtext.xtext.wizard/xtend-gen/org/eclipse/xtext/xtext/wizard/IdeProjectDescriptor.java
+++ org.eclipse.xtext.xtext.wizard/xtend-gen/org/eclipse/xtext/xtext/wizard/IdeProjectDescriptor.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2015, 2019 itemis AG (http://www.itemis.eu) and others.
+ * Copyright (c) 2015, 2021 itemis AG (http://www.itemis.eu) and others.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -373,7 +373,7 @@ public class IdeProjectDescriptor extends ProjectDescriptor {
               _builder.newLine();
               _builder.append("\t\t");
               _builder.append("\t");
-              _builder.append("<version>3.1.1</version>");
+              _builder.append("<version>3.1.2</version>");
               _builder.newLine();
               _builder.append("\t\t");
               _builder.append("\t");
@@ -655,7 +655,7 @@ public class IdeProjectDescriptor extends ProjectDescriptor {
               _builder.newLine();
               _builder.append("\t\t");
               _builder.append("\t");
-              _builder.append("<version>3.2.1</version>");
+              _builder.append("<version>3.2.4</version>");
               _builder.newLine();
               _builder.append("\t\t");
               _builder.append("\t");
@@ -1109,7 +1109,7 @@ public class IdeProjectDescriptor extends ProjectDescriptor {
           _builder.append("plugins {");
           _builder.newLine();
           _builder.append("\t");
-          _builder.append("id \'com.github.johnrengelman.shadow\' version \'5.1.0\'");
+          _builder.append("id \'com.github.johnrengelman.shadow\' version \'5.2.0\'");
           _builder.newLine();
           _builder.append("}");
           _builder.newLine();
