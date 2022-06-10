d16c2d52be4ce5aa91ba99ce0da7d466bebea954
Christian Dietrich <christian.dietrich@itemis.de>
2022-06-10 14:46:37 +0200

    fixed some spotbugs issues
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtext.web/src/test/java/org/eclipse/xtext/web/server/test/GeneratorTest.java
+++ org.eclipse.xtext.web/src/test/java/org/eclipse/xtext/web/server/test/GeneratorTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2015, 2020 itemis AG (http://www.itemis.eu) and others.
+ * Copyright (c) 2015, 2022 itemis AG (http://www.itemis.eu) and others.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -42,11 +42,13 @@ public class GeneratorTest extends AbstractWebServerTest {
 		public void doGenerate(Resource input, IFileSystemAccess2 fsa, IGeneratorContext ctx) {
 			invocationCount++;
 			Statemachine statemachine = Iterables.getFirst(Iterables.filter(input.getContents(), Statemachine.class), null);
-			StringConcatenation builder = new StringConcatenation();
-			builder.append(statemachine.getStates().stream().map(State::getName).collect(Collectors.joining(",")));
-			builder.newLineIfNotEmpty();
-			fsa.generateFile("/DEFAULT_ARTIFACT", builder);
-			fsa.generateFile("/test.txt", "hello, world!");
+			if (statemachine != null) {
+				StringConcatenation builder = new StringConcatenation();
+				builder.append(statemachine.getStates().stream().map(State::getName).collect(Collectors.joining(",")));
+				builder.newLineIfNotEmpty();
+				fsa.generateFile("/DEFAULT_ARTIFACT", builder);
+				fsa.generateFile("/test.txt", "hello, world!");
+			}
 		}
 	}
 
