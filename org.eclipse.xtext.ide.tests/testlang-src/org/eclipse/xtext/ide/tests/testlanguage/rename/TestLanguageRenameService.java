48ec4a1fd4469613da27832b9dedf8d7c859a3bc
Christian Dietrich <christian.dietrich@itemis.de>
2022-07-13 18:45:06 +0200

    fix unused imports
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtext.ide.tests/testlang-src/org/eclipse/xtext/ide/tests/testlanguage/rename/TestLanguageRenameService.java
+++ org.eclipse.xtext.ide.tests/testlang-src/org/eclipse/xtext/ide/tests/testlanguage/rename/TestLanguageRenameService.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2019, 2020 TypeFox and others.
+ * Copyright (c) 2019, 2022 TypeFox and others.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -9,7 +9,6 @@
 package org.eclipse.xtext.ide.tests.testlanguage.rename;
 
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.xtext.Keyword;
 import org.eclipse.xtext.ide.server.rename.RenameService2;
 import org.eclipse.xtext.naming.IQualifiedNameProvider;
 import org.eclipse.xtext.naming.QualifiedName;
