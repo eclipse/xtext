3870faab9c97382463af9cfc052c0a8b3a34ade7
Christian Dietrich <christian.dietrich@itemis.de>
2022-07-26 07:12:25 +0200

    [#1362] workaround for changed internal api in jdt
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtend.ide/src/org/eclipse/xtend/ide/formatting/preferences/AbstractModifyDialogTab.java
+++ org.eclipse.xtend.ide/src/org/eclipse/xtend/ide/formatting/preferences/AbstractModifyDialogTab.java
@@ -97,4 +97,16 @@ public abstract class AbstractModifyDialogTab extends ModifyDialogTabPage {
 				Boolean.FALSE.toString(), Boolean.TRUE.toString() });
 	}
 
+	public void doSetAll(boolean value) {
+		
+	}
+
+	public void resetValues() {
+		
+	}
+
+	public void setDefaults() {
+		
+	}
+
 }
