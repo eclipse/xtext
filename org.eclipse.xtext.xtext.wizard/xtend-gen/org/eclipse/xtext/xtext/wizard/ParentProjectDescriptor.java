4255b6e64f1e4a204ea40142cfaa9117aed977dc
Christian Dietrich <christian.dietrich@itemis.de>
2022-09-11 16:51:56 +0200

    [eclipse/xtext#2096] update to tycho 2.7.5
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtext.xtext.wizard/xtend-gen/org/eclipse/xtext/xtext/wizard/ParentProjectDescriptor.java
+++ org.eclipse.xtext.xtext.wizard/xtend-gen/org/eclipse/xtext/xtext/wizard/ParentProjectDescriptor.java
@@ -106,7 +106,7 @@ public class ParentProjectDescriptor extends ProjectDescriptor {
   }
 
   public String getTychoVersion() {
-    return "2.7.4";
+    return "2.7.5";
   }
 
   public String getTychoVersionJ8() {
