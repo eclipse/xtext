ac8e7582a946996092587fa9841aed2c90e50ff1
Christian Dietrich <christian.dietrich@itemis.de>
2022-10-20 11:18:22 +0200

    [#1393] added missing super.tearDown to test Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtend.ide.tests/xtend-gen/org/eclipse/xtend/ide/tests/quickfix/QuickfixTest.java
+++ org.eclipse.xtend.ide.tests/xtend-gen/org/eclipse/xtend/ide/tests/quickfix/QuickfixTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2013, 2020 itemis AG (http://www.itemis.eu) and others.
+ * Copyright (c) 2013, 2022 itemis AG (http://www.itemis.eu) and others.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -51,6 +51,7 @@ public class QuickfixTest extends AbstractXtendUITestCase {
     try {
       this.builder.tearDown();
       this.workbenchTestHelper.tearDown();
+      super.tearDown();
     } catch (Throwable _e) {
       throw Exceptions.sneakyThrow(_e);
     }
