57615ec663fe5d8e20b645bf733955e42d19cbd6
Christian Dietrich <christian.dietrich@itemis.de>
2022-11-16 20:16:11 +0100

    fix some warnings
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtend.core.tests/src/org/eclipse/xtend/core/tests/typesystem/RawIterable.java
+++ org.eclipse.xtend.core.tests/src/org/eclipse/xtend/core/tests/typesystem/RawIterable.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012, 2016 itemis AG (http://www.itemis.eu) and others.
+ * Copyright (c) 2012, 2022 itemis AG (http://www.itemis.eu) and others.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -8,6 +8,6 @@
  */
 package org.eclipse.xtend.core.tests.typesystem;
 
-@SuppressWarnings("all")
+@SuppressWarnings("rawtypes")
 public abstract class RawIterable implements Iterable {
 }
