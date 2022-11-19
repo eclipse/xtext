0a25e8eaff10f43e2808fae41b437b27516c4de8
Christian Dietrich <christian.dietrich@itemis.de>
2022-11-19 10:12:28 +0100

    also mark test as deprecated
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtext.tests/src/org/eclipse/xtext/util/LineFilterOutputStreamTest.java
+++ org.eclipse.xtext.tests/src/org/eclipse/xtext/util/LineFilterOutputStreamTest.java
@@ -18,7 +18,7 @@ import org.junit.Test;
 /**
  * @author Sebastian Zarnekow - Initial contribution and API
  */
-@SuppressWarnings("deprecation")
+@Deprecated
 public class LineFilterOutputStreamTest extends Assert {
 
 	private ByteArrayOutputStream result;
