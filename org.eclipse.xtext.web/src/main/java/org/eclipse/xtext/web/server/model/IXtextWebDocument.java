2ddd3cff2bac5d850497a45fb373e4f0987c43f1
Christian Dietrich <christian.dietrich@itemis.de>
2023-02-16 06:12:24 +0100

    cleanup after xtend 2 java mig
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- org.eclipse.xtext.web/src/main/java/org/eclipse/xtext/web/server/model/IXtextWebDocument.java
+++ org.eclipse.xtext.web/src/main/java/org/eclipse/xtext/web/server/model/IXtextWebDocument.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2015 itemis AG (http://www.itemis.eu) and others.
+ * Copyright (c) 2015, 2023 itemis AG (http://www.itemis.eu) and others.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -13,7 +13,6 @@ import org.eclipse.xtext.resource.XtextResource;
 /**
  * Interface for accessing a document. Depending on the access type, some methods may not be supported.
  */
-@SuppressWarnings("all")
 public interface IXtextWebDocument {
   XtextResource getResource();
   
