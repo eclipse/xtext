acae65b38fe6dfbb08300302dec23520c448ea50
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-03-09 11:11:28 +0100

    maven tycho (#515)
    * xtext-dev-bom as a Maven project
    
    * parent POM with starting target platform
    
    * ignore target
    
    * .xbase.lib compiles
    
    * .xbase.lib Maven/Tycho project
    
    * build .xbase.lib
    
    * fixed version in BOM
    
    * GitHub Actions workflow
    
    * xtend.lib.macro compiles
    
    * .xtend.lib.macro as Maven/Tycho project
    
    * build .xtend.lib.macro
    
    * .xtend.lib compiles
    
    * .xtend.lib as Maven/Tycho project
    
    * build .xtend.lib
    
    * guava-testlib in TP as Maven artifact
    
    * .xbase.lib.tests compiles
    
    * .xtend.lib.tests as Maven/Tycho project
    
    * build xbase.lib.tests, tests are green
    
    * xtend.lib.tests compiles
    
    * xtend.lib.tests Maven/Tycho project
    
    * build xtend.lib.tests, tests are green
    
    * xbase.lib.gwt removed gradle nature
    
    * added POM to xbase.lib.gwt
    
    taking inspiration from
    https://github.com/eclipse/xtext/tree/v2.10.0/plugins/org.eclipse.xtext.xbase.lib.gwt
    
    * xbase.lib.gwt as Maven/tycho project
    
    * xbase.lib.gwt removed gradle nature
    
    * xtend.lib.gwt as Maven/tycho project
    
    * .xtend.lib.gwt compiles as Maven project
    
    * fixed xtend.lib.gwt.test
    
    * build xbase.lib.feature
    
    * build xtend.lib.feature
    
    * added names to features' POMs
    
    * p2 repository
    
    * removed old releng directory
    
    * updated Jenkins build
    
    * removed features from the p2 repository
    
    * Revert "removed features from the p2 repository"
    
    This reverts commit 8f2a288c895f2f8f8d13c897b2906aa0c8b57d92.
    
    * updated project configuration
    
    * changed groupId of features
    
    Because they have the same artifactId of bundles
    
    * xbase.lib.tests is a pure Maven project
    
    No need to have guava-testlib in the target platform, since we consume
    it as a Maven dependency
    
    * put comment in the right place in the POM
    
    * eclipse-jarsigner-plugin
    
    * eclipse-sign profile in Jenkins
    
    * separate projects for p2 and maven
    
    * harmonized parent with xtext-core parent
    
    * harmonized parent with xtext-extras parent
    
    * harmonized parent with xtext-xtend parent
    
    * prepared sonatype-oss-release profile
    
    * distributionManagement
    
    * prepared Jenkinsfile for Maven deploy
    
    * skipTests in profile sonatype-oss-release
    
    * reference to Maven releng project
    
    * distributionManagement in dev-bom
    
    because it does not inherit from the parent
    
    * updated distributionManagement release repository
    
    * removed wrong configuration option of javadoc plugin
    
    * eclipse-sign also when deploying Maven artifacts
    
    * renamed maven aggregator artifactId
    
    * removed blank line
    
    * removed organization for Sebastian
    
    * removed dep com.google.code.findbugs:jsr305
    
    * versions-maven-plugin
    
    * a profile mentioning releng children
    
    * removed old xtext-current-release-version
    
    * xtend-maven-plugin-version 2.30.0.M2
    
    * harmonized parent POM with other projects
    
    * harmonized Jenkins script with other projects
    
    separate maven build, tycho test and tycho sign
    
    * coinfigured maven-jar-plugin
    
    * configured maven-source-plugin
    
    * set execution bit
    
    * skip main "compile" goal of xtend-maven-plugin
    
    to avoid generated sources to be considered as main sources by the
    maven-compiler-plugin
    
    * profile buildP2Repository
    
    * configured exec-maven-plugin
    
    * added source bundles in features
    
    As it used to be in the original
    
    * update to lsp4j 0.20.0
    
    * disable p2 sign in Jenkinsfile
    
    * bumped 2.31.0 in POMs
    
    * xtend-maven-plugin-version 2.30.0
    
    * updated other eclipse metadata to 2.31.0
    
    * removed gradle files

---
+++ maven-build.sh
@@ -0,0 +1,18 @@
+if [ -z "$JENKINS_URL" ]; then
+  # if not set in environment use default
+  JENKINS_URL=https://ci.eclipse.org/xtext/
+fi
+
+mvn \
+  -f org.eclipse.xtext.maven.releng \
+  -DskipTests=true \
+  clean deploy \
+  -P useJenkinsSnapshots \
+  -P eclipse-sign \
+  --batch-mode \
+  --update-snapshots \
+  -Dmaven.repo.local=.m2/repository \
+  -DaltDeploymentRepository=local::default::file:./build/maven-repository \
+  -DJENKINS_URL=$JENKINS_URL \
+  -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
+  $@
---
+++ tycho-sign.sh
@@ -0,0 +1,18 @@
+if [ -z "$JENKINS_URL" ]; then
+  # if not set in environment use default
+  JENKINS_URL=https://ci.eclipse.org/xtext/
+fi
+
+# THIS SIGNS, SKIPPING TESTS
+
+mvn \
+  -f org.eclipse.xtext.p2.releng \
+  package \
+  -P useJenkinsSnapshots \
+  -P eclipse-sign,buildP2Repository \
+  --batch-mode \
+  --update-snapshots \
+  -Dmaven.repo.local=.m2/repository \
+  -DJENKINS_URL=$JENKINS_URL \
+  -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
+  $@
