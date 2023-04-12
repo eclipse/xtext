3dd109828ac0227cd01d4c01f3f306fff02c9090
Hannes Wellmann <wellmann.hannes1@gmx.net>
2023-04-12 22:29:12 +0200

    Unify build workflows and argument specification
    - Merge the external-pr workflow into the maven workflow
    - Create .mvn/maven.config and common build arguments to it
    - Remove unused environment variable 'WORKSPACE' and inline
    'JENKINS_URL' env-variable.

--- tycho-sign.sh
+++ tycho-sign.sh
@@ -1,8 +1,3 @@
-if [ -z "$JENKINS_URL" ]; then
-  # if not set in environment use default
-  JENKINS_URL=https://ci.eclipse.org/xtext/
-fi
-
 # THIS SIGNS, SKIPPING TESTS
 
 mvn \
@@ -11,8 +6,6 @@ mvn \
   -P useJenkinsSnapshots \
   -P eclipse-sign \
   --batch-mode \
-  --update-snapshots \
   -Dmaven.repo.local=.m2/repository \
-  -DJENKINS_URL=$JENKINS_URL \
   -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
   $@
