662b87ed37f40d4dd3ba1d9594ecb90dcedd919b
Florian Pirchner <florian.pirchner@gmail.com>
2023-07-21 09:25:51 +0200

    Renamed javax.servelt to jakarta.servlet Signed-off-by: Florian Pirchner <florian.pirchner@gmail.com>

--- org.eclipse.xtext.web.servlet/src/main/java/org/eclipse/xtext/web/servlet/XtextServlet.java
+++ org.eclipse.xtext.web.servlet/src/main/java/org/eclipse/xtext/web/servlet/XtextServlet.java
@@ -11,10 +11,10 @@ package org.eclipse.xtext.web.servlet;
 import java.io.IOException;
 import java.util.Set;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.eclipse.emf.common.util.URI;
