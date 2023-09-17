ee1ae239ec3e0378549689d5c59f92733e31b237
Christian Dietrich <christian.dietrich.opensource@gmail.com>
2023-09-17 15:19:23 +0200

    [#2754] update to orbit 2023-12
    Signed-off-by: Christian Dietrich <christian.dietrich.opensource@gmail.com>

--- org.eclipse.xtext.xtext.wizard/xtend-gen/org/eclipse/xtext/xtext/wizard/TargetPlatformProject.java
+++ org.eclipse.xtext.xtext.wizard/xtend-gen/org/eclipse/xtext/xtext/wizard/TargetPlatformProject.java
@@ -254,7 +254,7 @@ public class TargetPlatformProject extends ProjectDescriptor {
     _builder.append("<unit id=\"io.github.classgraph\" version=\"4.8.149.v20220915-0556\"/>");
     _builder.newLine();
     _builder.append("\t\t\t");
-    _builder.append("<repository location=\"https://download.eclipse.org/tools/orbit/simrel/orbit-aggregation/2023-09\"/>");
+    _builder.append("<repository location=\"https://download.eclipse.org/tools/orbit/simrel/orbit-aggregation/2023-12\"/>");
     _builder.newLine();
     _builder.append("\t\t");
     _builder.append("</location>");
