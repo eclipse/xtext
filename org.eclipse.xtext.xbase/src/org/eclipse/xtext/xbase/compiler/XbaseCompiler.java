04808b6e63cac0d8e9b805c3e5536f52ab6d330f
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2024-01-02 07:57:34 +0100

    simplified appendConstructedTypeName
    use getNamedType instead of manually retrieving the last element of
    supertypes

--- org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/compiler/XbaseCompiler.java
+++ org.eclipse.xtext.xbase/src/org/eclipse/xtext/xbase/compiler/XbaseCompiler.java
@@ -99,7 +99,6 @@ import org.eclipse.xtext.xbase.util.XbaseUsageCrossReferencer;
 
 import com.google.common.base.Objects;
 import com.google.common.base.Preconditions;
-import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 
@@ -1133,12 +1132,12 @@ public class XbaseCompiler extends FeatureCallCompiler {
 
 	protected void appendConstructedTypeName(XConstructorCall constructorCall, ITreeAppendable typeAppendable) {
 		JvmDeclaredType type = constructorCall.getConstructor().getDeclaringType();
+		IResolvedTypes resolvedTypes = batchTypeResolver.resolveTypes(constructorCall);
+		LightweightTypeReference actualType = resolvedTypes.getActualType(constructorCall);
 		if (type instanceof JvmGenericType && ((JvmGenericType) type).isAnonymous()) {
-			typeAppendable.append(Iterables.getLast(type.getSuperTypes()).getType());
+			typeAppendable.append(actualType.getNamedType().getRawTypeReference());
 		} else {
-			IResolvedTypes resolvedTypes = batchTypeResolver.resolveTypes(constructorCall);
-			LightweightTypeReference actualType = resolvedTypes.getActualType(constructorCall).getRawTypeReference();
-			typeAppendable.append(actualType);
+			typeAppendable.append(actualType.getRawTypeReference());
 		}
 	}
 	
