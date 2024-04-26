ec1ab025d158ea20970fd0f48003ef42d5f36743
Christian Dietrich <christian.dietrich.opensource@gmail.com>
2024-04-26 10:31:42 +0200

    update to mwe 2.18.0.M2

--- org.eclipse.xtext.util/src/org/eclipse/xtext/util/XtextVersion.java
+++ org.eclipse.xtext.util/src/org/eclipse/xtext/util/XtextVersion.java
@@ -52,14 +52,14 @@ public class XtextVersion {
 	 * @noreference
 	 */
 	public String getMweVersion() {
-		return "2.18.0.M1";
+		return "2.18.0.M2";
 	}
 
 	/**
 	 * @noreference
 	 */
 	public String getMweBuildNumber() {
-		return "S202404050451";
+		return "S202404260736";
 	}
 
 	public String getAntlrGeneratorVersion() {
