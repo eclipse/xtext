3e5dacd563945eb722e2fcde0209e4746ce87c7d
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2024-05-16 12:10:07 +0200

    regenerated examples

--- org.eclipse.xtext.xtext.ui.examples/projects/domainmodel/org.eclipse.xtext.example.domainmodel.tests/src-gen/org/eclipse/xtext/example/domainmodel/tests/DomainmodelInjectorProvider.java
+++ org.eclipse.xtext.xtext.ui.examples/projects/domainmodel/org.eclipse.xtext.example.domainmodel.tests/src-gen/org/eclipse/xtext/example/domainmodel/tests/DomainmodelInjectorProvider.java
@@ -48,10 +48,12 @@ public class DomainmodelInjectorProvider implements IInjectorProvider, IRegistry
 	protected DomainmodelRuntimeModule createRuntimeModule() {
 		// make it work also with Maven/Tycho and OSGI
 		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=493672
+		// allows for bindClassLoaderToInstance to get the class loader of the bundle
+		// containing the instance of the injector provider (possibly inherited)
 		return new DomainmodelRuntimeModule() {
 			@Override
 			public ClassLoader bindClassLoaderToInstance() {
-				return DomainmodelInjectorProvider.class
+				return DomainmodelInjectorProvider.this.getClass()
 						.getClassLoader();
 			}
 		};
--- org.eclipse.xtext.xtext.ui.examples/projects/homeautomation/org.eclipse.xtext.example.homeautomation.tests/src-gen/org/eclipse/xtext/example/homeautomation/tests/RuleEngineInjectorProvider.java
+++ org.eclipse.xtext.xtext.ui.examples/projects/homeautomation/org.eclipse.xtext.example.homeautomation.tests/src-gen/org/eclipse/xtext/example/homeautomation/tests/RuleEngineInjectorProvider.java
@@ -48,10 +48,12 @@ public class RuleEngineInjectorProvider implements IInjectorProvider, IRegistryC
 	protected RuleEngineRuntimeModule createRuntimeModule() {
 		// make it work also with Maven/Tycho and OSGI
 		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=493672
+		// allows for bindClassLoaderToInstance to get the class loader of the bundle
+		// containing the instance of the injector provider (possibly inherited)
 		return new RuleEngineRuntimeModule() {
 			@Override
 			public ClassLoader bindClassLoaderToInstance() {
-				return RuleEngineInjectorProvider.class
+				return RuleEngineInjectorProvider.this.getClass()
 						.getClassLoader();
 			}
 		};
