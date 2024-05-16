569d266957742c583ea523480bf7cd0888cf8ca9
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2024-05-16 11:14:11 +0200

    updated JvmGenericTypeValidatorTestLangInjectorProvider

--- org.eclipse.xtext.xbase.testlanguages/src-gen/org/eclipse/xtext/xbase/testlanguages/tests/JvmGenericTypeValidatorTestLangInjectorProvider.java
+++ org.eclipse.xtext.xbase.testlanguages/src-gen/org/eclipse/xtext/xbase/testlanguages/tests/JvmGenericTypeValidatorTestLangInjectorProvider.java
@@ -48,10 +48,12 @@ public class JvmGenericTypeValidatorTestLangInjectorProvider implements IInjecto
 	protected JvmGenericTypeValidatorTestLangRuntimeModule createRuntimeModule() {
 		// make it work also with Maven/Tycho and OSGI
 		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=493672
+		// allows for bindClassLoaderToInstance to get the class loader of the bundle
+		// containing the instance of the injector provider (possibly inherited)
 		return new JvmGenericTypeValidatorTestLangRuntimeModule() {
 			@Override
 			public ClassLoader bindClassLoaderToInstance() {
-				return JvmGenericTypeValidatorTestLangInjectorProvider.class
+				return JvmGenericTypeValidatorTestLangInjectorProvider.this.getClass()
 						.getClassLoader();
 			}
 		};
