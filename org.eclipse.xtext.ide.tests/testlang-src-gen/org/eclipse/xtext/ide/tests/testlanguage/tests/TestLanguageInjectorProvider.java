2e10445967c56a6a096c5386b9bb01f2a3b5aa3a
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2024-05-16 09:24:55 +0200

    regenerated org.eclipse.xtext.ide.tests

--- org.eclipse.xtext.ide.tests/testlang-src-gen/org/eclipse/xtext/ide/tests/testlanguage/tests/TestLanguageInjectorProvider.java
+++ org.eclipse.xtext.ide.tests/testlang-src-gen/org/eclipse/xtext/ide/tests/testlanguage/tests/TestLanguageInjectorProvider.java
@@ -48,10 +48,12 @@ public class TestLanguageInjectorProvider implements IInjectorProvider, IRegistr
 	protected TestLanguageRuntimeModule createRuntimeModule() {
 		// make it work also with Maven/Tycho and OSGI
 		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=493672
+		// allows for bindClassLoaderToInstance to get the class loader of the bundle
+		// containing the instance of the injector provider (possibly inherited)
 		return new TestLanguageRuntimeModule() {
 			@Override
 			public ClassLoader bindClassLoaderToInstance() {
-				return TestLanguageInjectorProvider.class
+				return TestLanguageInjectorProvider.this.getClass()
 						.getClassLoader();
 			}
 		};
