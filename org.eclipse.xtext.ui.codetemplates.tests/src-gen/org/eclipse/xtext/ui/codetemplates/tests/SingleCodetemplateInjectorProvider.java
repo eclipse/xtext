b073505bb7c3f10345ee2d3b6cd06d69167bd438
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2024-05-16 12:04:56 +0200

    regenerated xtext.ui.codetemplates.tests

--- org.eclipse.xtext.ui.codetemplates.tests/src-gen/org/eclipse/xtext/ui/codetemplates/tests/SingleCodetemplateInjectorProvider.java
+++ org.eclipse.xtext.ui.codetemplates.tests/src-gen/org/eclipse/xtext/ui/codetemplates/tests/SingleCodetemplateInjectorProvider.java
@@ -48,10 +48,12 @@ public class SingleCodetemplateInjectorProvider implements IInjectorProvider, IR
 	protected SingleCodetemplateRuntimeModule createRuntimeModule() {
 		// make it work also with Maven/Tycho and OSGI
 		// see https://bugs.eclipse.org/bugs/show_bug.cgi?id=493672
+		// allows for bindClassLoaderToInstance to get the class loader of the bundle
+		// containing the instance of the injector provider (possibly inherited)
 		return new SingleCodetemplateRuntimeModule() {
 			@Override
 			public ClassLoader bindClassLoaderToInstance() {
-				return SingleCodetemplateInjectorProvider.class
+				return SingleCodetemplateInjectorProvider.this.getClass()
 						.getClassLoader();
 			}
 		};
