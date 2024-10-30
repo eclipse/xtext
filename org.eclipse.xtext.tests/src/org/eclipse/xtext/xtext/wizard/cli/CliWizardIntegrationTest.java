0c251d3bad21b9c11b0182907b3f072d504f8a18
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2024-10-30 07:57:55 +0100

    updated project wizard tests

--- org.eclipse.xtext.tests/src/org/eclipse/xtext/xtext/wizard/cli/CliWizardIntegrationTest.java
+++ org.eclipse.xtext.tests/src/org/eclipse/xtext/xtext/wizard/cli/CliWizardIntegrationTest.java
@@ -139,7 +139,7 @@ public class CliWizardIntegrationTest {
 				project.getIdeProject().setEnabled(true);
 				project.getWebProject().setEnabled(true);
 			}), CliWizardIntegrationTest.newProjectConfig(project -> {
-				project.setBaseName("mavenTychoJ17");
+				project.setBaseName("mavenTychoJUnit5");
 				project.setPreferredBuildSystem(BuildSystem.MAVEN);
 				project.setSourceLayout(SourceLayout.PLAIN);
 				project.setProjectLayout(ProjectLayout.HIERARCHICAL);
@@ -148,7 +148,6 @@ public class CliWizardIntegrationTest {
 				project.getUiProject().getTestProject().setEnabled(true);
 				project.getIdeProject().setEnabled(true);
 				project.getWebProject().setEnabled(true);
-				project.setJavaVersion(JavaVersion.JAVA17);
 				project.setJunitVersion(JUnitVersion.JUNIT_5);
 			}), CliWizardIntegrationTest.newProjectConfig(project -> {
 				project.setBaseName("gradle");
@@ -180,18 +179,6 @@ public class CliWizardIntegrationTest {
 				project.getP2Project().setEnabled(true);
 			}), CliWizardIntegrationTest.newProjectConfig(project -> {
 				project.setBaseName("mavenTychoP2");
-				project.setPreferredBuildSystem(BuildSystem.MAVEN);
-				project.setSourceLayout(SourceLayout.PLAIN);
-				project.setProjectLayout(ProjectLayout.HIERARCHICAL);
-				project.getRuntimeProject().getTestProject().setEnabled(true);
-				project.getUiProject().setEnabled(true);
-				project.getUiProject().getTestProject().setEnabled(true);
-				project.getIdeProject().setEnabled(true);
-				project.getWebProject().setEnabled(true);
-				project.getP2Project().setEnabled(true);
-			}), CliWizardIntegrationTest.newProjectConfig(project -> {
-				project.setBaseName("mavenTychoP2J17");
-				project.setJavaVersion(JavaVersion.JAVA17);
 				project.setPreferredBuildSystem(BuildSystem.MAVEN);
 				project.setSourceLayout(SourceLayout.PLAIN);
 				project.setProjectLayout(ProjectLayout.HIERARCHICAL);
